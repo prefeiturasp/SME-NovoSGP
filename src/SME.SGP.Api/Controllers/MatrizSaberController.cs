309fc5c641b5cb2337072028fea62740464e83cf
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-08-08 15:58:12 -0300

    Listagem de matrizes do saber

---
+++ src/SME.SGP.Api/Controllers/MatrizSaberController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/matrizes-saber")]
+    [ValidaDto]
+    public class MatrizSaberController : ControllerBase
+    {
+        private readonly IConsultasMatrizSaber consultasMatrizSaber;
+
+        public MatrizSaberController(IConsultasMatrizSaber consultasMatrizSaber)
+        {
+            this.consultasMatrizSaber = consultasMatrizSaber ?? throw new System.ArgumentNullException(nameof(consultasMatrizSaber));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(consultasMatrizSaber.Listar());
+        }
+    }
+}
