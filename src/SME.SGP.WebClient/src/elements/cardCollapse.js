75b57d728825f8fbb3efcef5f7790841175811cc
Flávio Ricardo <flavioricardo@live.com>
2019-08-08 19:19:21 -0300

    Componente de Card com Collapse

--- src/SME.SGP.WebClient/src/elements/cardCollapse.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CardCollapse() {
-  return (
-    <div className="card">
-
-    </div>
-  );
-}
--- src/SME.SGP.WebClient/src/pages/Planejamento/PlanoAnual/planoAnual.js
+++ src/SME.SGP.WebClient/src/pages/Planejamento/PlanoAnual/planoAnual.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import CardCollapse from '../../../elements/cardCollapse';
+import shortid from 'shortid';
+import CardCollapse from '../../../componentes/cardCollapse';
 
 export default function PlanoAnual() {
+  const bimestres = [{ nome: '1º Bimestre' }, { nome: '2º Bimestre' }];
+
   return (
     <div>
       <h1>Plano Anual</h1>
-      <CardCollapse />
+      {bimestres.length > 0
+        ? bimestres.map(bimestre => {
+            return (
+              <CardCollapse
+                key={shortid.generate()}
+                titulo={bimestre.nome}
+                indice={shortid.generate()}
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
