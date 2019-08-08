3539c756d100915eb8fc64824419d6364420f285
Flávio Ricardo <flavioricardo@live.com>
2019-08-08 15:18:55 -0300

    Plano anual

---
+++ src/SME.SGP.WebClient/src/pages/Planejamento/PlanoAnual/planoAnual.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardCollapse from '../../../elements/cardCollapse';
+
+export default function PlanoAnual() {
+  return (
+    <div>
+      <h1>Plano Anual</h1>
+      <CardCollapse />
+    </div>
+  );
+}
--- src/SME.SGP.WebClient/src/routes/index.js
+++ src/SME.SGP.WebClient/src/routes/index.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Principal from '../pages/Principal/principal';
 import PlanoCiclo from '../pages/Planejamento/PlanoCiclo/planoCiclo';
+import PlanoAnual from '../pages/Planejamento/PlanoAnual/planoAnual';
 
 export default function Rotas() {
   return (
     <Switch>
       <Route path="/" exact component={Principal} />
       <Route path="/planejamento/plano-ciclo" component={PlanoCiclo} />
+      <Route path="/planejamento/plano-anual" component={PlanoAnual} />
     </Switch>
   );
 }
