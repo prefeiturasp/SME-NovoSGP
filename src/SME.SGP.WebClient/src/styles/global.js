c08135961dbbd07f92ecca606a620cef4eb1dd65
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2019-08-08 07:35:17 -0300

    rotas

---
+++ src/SME.SGP.WebClient/src/styles/global.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`  
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  *:focus {
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
