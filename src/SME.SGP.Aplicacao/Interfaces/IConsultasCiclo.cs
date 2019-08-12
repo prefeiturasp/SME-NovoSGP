0ea3e76e504ed301dbc531047b79090f3f76433f
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-08-12 15:21:28 -0300

    Criando api que retorna a listagem de ciclo por ids das turmas do professor

---
+++ src/SME.SGP.Aplicacao/Interfaces/IConsultasCiclo.cs
@@ -0,0 +1,10 @@
+﻿using SME.SGP.Dto;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public interface IConsultasCiclo
+    {
+        IEnumerable<CicloDto> Listar(IEnumerable<int> idsTurmas);
+    }
+}
