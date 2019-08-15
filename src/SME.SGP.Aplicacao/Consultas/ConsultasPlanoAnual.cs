3be3cec617b4e7b08cfb1f1f966e3de4af204434
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-08-15 11:34:08 -0300

    Iniciando plano anual

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasPlanoAnual.cs
@@ -0,0 +1,19 @@
+﻿using SME.SGP.Dominio.Interfaces;
+
+namespace SME.SGP.Aplicacao.Consultas
+{
+    public class ConsultasPlanoAnual
+    {
+        private readonly IRepositorioPlanoAnual repositorioPlanoAnual;
+
+        public ConsultasPlanoAnual(IRepositorioPlanoAnual repositorioPlanoAnual)
+        {
+            this.repositorioPlanoAnual = repositorioPlanoAnual ?? throw new System.ArgumentNullException(nameof(repositorioPlanoAnual));
+        }
+
+        public void Listar()
+        {
+            var planosAnuais = repositorioPlanoAnual.Listar();
+        }
+    }
+}
