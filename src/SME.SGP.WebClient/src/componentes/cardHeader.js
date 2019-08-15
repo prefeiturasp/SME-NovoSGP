90388bbb532f8e8e0c2d187d55d56a1915f17d62
Flávio Ricardo <flavioricardo@live.com>
2019-08-15 19:18:04 -0300

    Refinamentos plano anual

--- src/SME.SGP.WebClient/src/componentes/cardHeader.js
+++ src/SME.SGP.WebClient/src/componentes/cardHeader.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { Base } from './colors';
 
 const CardHeader = props => {
   const { children } = props;
 
   const Header = styled.div`
-    border-left: 4px solid #0a3da3;
+    border-left: 8px solid ${Base.AzulBordaCard};
+    font-size: 16px;
   `;
 
   return (
-    <Header className="card-header rounded bg-white d-flex align-items-center">
+    <Header className="card-header shadow-sm rounded bg-white d-flex align-items-center py-4">
       {children}
     </Header>
   );
