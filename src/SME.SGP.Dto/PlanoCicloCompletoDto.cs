e43c568a91e3fa914227c542ba33c336c47e9a50
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-08-18 17:34:03 -0300

    Ajuste para retornar o id ciclo na consulta do plano de ciclo

--- src/SME.SGP.Dto/PlanoCicloCompletoDto.cs
+++ src/SME.SGP.Dto/PlanoCicloCompletoDto.cs
@@ -6,6 +6,7 @@ namespace SME.SGP.Dto
 {
     public class PlanoCicloCompletoDto
     {
+        public long CicloId { get; set; }
         public string Descricao { get; set; }
         public long Id { get; set; }
         public IEnumerable<long> IdsMatrizesSaber => MatrizesSaber?.Split(',').Select(c => Convert.ToInt64(c));
