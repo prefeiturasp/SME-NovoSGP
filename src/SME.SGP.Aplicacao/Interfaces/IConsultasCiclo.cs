cdd4c3c858fecb2094841aefc9ea85005312d4fb
Caique Latorre <caique.siqueira@amcom.com.br>
2019-08-19 15:31:38 -0300

    Método para consultar ciclo por ano

--- src/SME.SGP.Aplicacao/Interfaces/IConsultasCiclo.cs
+++ src/SME.SGP.Aplicacao/Interfaces/IConsultasCiclo.cs
@@ -1,10 +1,9 @@
 ﻿using SME.SGP.Dto;
-using System.Collections.Generic;
 
 namespace SME.SGP.Aplicacao
 {
     public interface IConsultasCiclo
     {
-        IEnumerable<CicloDto> Listar(IEnumerable<int> idsTurmas);
+        CicloDto Selecionar(int ano);
     }
 }
---
+++ src/SME.SGP.Dados/Mapeamentos/CicloMap.cs
@@ -0,0 +1,12 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class CicloMap : BaseMap<Ciclo>
+    {
+        public CicloMap()
+        {
+            ToTable("tipo_ciclo");
+        }
+    }
+}
