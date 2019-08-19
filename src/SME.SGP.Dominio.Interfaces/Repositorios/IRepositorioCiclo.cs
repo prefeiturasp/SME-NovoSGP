1e84e3182c76f299e3cbf1260bc49bb6d9377bfa
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-08-19 19:00:22 -0300

    Ajustes para retornar sugestão do ciclo + ciclos vindo da base de dados

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioCiclo.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioCiclo.cs
@@ -1,9 +1,12 @@
 ﻿using SME.SGP.Dto;
+using System.Collections.Generic;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioCiclo : IRepositorioBase<Ciclo>
     {
         CicloDto ObterCicloPorAno(int ano);
+
+        IEnumerable<CicloDto> ObterCiclosPorTurma(IEnumerable<int> turmas);
     }
 }
