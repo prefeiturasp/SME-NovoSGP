2d93f1a11afdb59224f9f5cd052ac94cc558d0ba
Flávio Ricardo <flavioricardo@live.com>
2019-08-19 16:52:01 -0300

    Plano de ciclo

--- src/SME.SGP.WebClient/src/rotas/rotas.js
+++ src/SME.SGP.WebClient/src/rotas/rotas.js
@@ -4,13 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 import Principal from '../paginas/Principal/principal';
 import PlanoCiclo from '../paginas/Planejamento/PlanoCiclo/planoCiclo';
 import PlanoAnual from '../paginas/Planejamento/PlanoAnual/planoAnual';
-import Monica from '../paginas/Planejamento/PlanoAnual/monica';
 
 export default function Rotas() {
   return (
     <Switch>
       <Route path="/" exact component={Principal} />
-      {/* <Route
+      <Route
         path="/planejamento/plano-ciclo/:ano/:cicloId/:escolaId"
         exact
         component={PlanoCiclo}
@@ -19,10 +18,8 @@ export default function Rotas() {
         path="/planejamento/plano-ciclo/:ano"
         exact
         component={PlanoCiclo}
-      /> */}
-      <Route path="/planejamento/plano-ciclo" component={PlanoCiclo} />
+      />
       <Route path="/planejamento/plano-anual" component={PlanoAnual} />
-      <Route path="/monica" component={Monica} />
     </Switch>
   );
 }
