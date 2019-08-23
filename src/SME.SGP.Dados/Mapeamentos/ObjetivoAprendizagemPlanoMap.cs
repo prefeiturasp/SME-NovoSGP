1303cddee29bc6a13b6cf2cc3df70914c3eb4b4e
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-08-23 12:17:33 -0300

    Ajustando entidades de plano anual

--- src/SME.SGP.Dados/Mapeamentos/ObjetivoAprendizagemPlanoMap.cs
+++ src/SME.SGP.Dados/Mapeamentos/ObjetivoAprendizagemPlanoMap.cs
@@ -7,7 +7,7 @@ namespace SME.SGP.Dados.Mapeamentos
         public ObjetivoAprendizagemPlanoMap()
         {
             ToTable("objetivo_desenvolvimento_plano");
-            Map(c => c.ObjetivoAprendizagemId).ToColumn("objetivo_desenvolvimento_id");
+            Map(c => c.ObjetivoAprendizagemId).ToColumn("objetivo_aprendizagem_id");
             Map(c => c.PlanoId).ToColumn("plano_id");
         }
     }
