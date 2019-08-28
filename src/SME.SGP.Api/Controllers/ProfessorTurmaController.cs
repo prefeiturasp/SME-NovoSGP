d50fba7d29a6c0137cee0349325d86f5611cfa21
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-08-28 09:24:07 -0300

    mock de turmas por professor

---
+++ src/SME.SGP.Api/Controllers/ProfessorTurmaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao;
+
+namespace SME.SGP.Api.Controllers
+{
+    [Route("api/v1/professor-turma")]
+    [ApiController]
+    [ValidaDto]
+    public class ProfessorTurmaController : ControllerBase
+    {
+        private readonly IConsultasProfessorTurma consultasProfessorTurma;
+
+        public ProfessorTurmaController(IConsultasProfessorTurma consultasProfessorTurma)
+        {
+            this.consultasProfessorTurma = consultasProfessorTurma;
+        }
+
+        [HttpGet]
+        [Route("{codigoRf}/turmas")]
+        public IActionResult Get(string codigoRf)
+        {
+            return Ok(consultasProfessorTurma.Listar(codigoRf));
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Interfaces/IConsultasProfessorTurma.cs
@@ -0,0 +1,10 @@
+﻿using SME.SGP.Dto;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public interface IConsultasProfessorTurma
+    {
+        IEnumerable<ProfessorTurmaDto> Listar(string codigoRf);
+    }
+}
