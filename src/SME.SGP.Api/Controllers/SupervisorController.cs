b7b5aabf1a42d365fd009dbc974ca3a321506905
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-08-30 16:25:30 -0300

    Fontes iniciais
    AB#4288

---
+++ src/SME.SGP.Api/Controllers/SupervisorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dto;
+using System.Collections.Generic;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/supervisores")]
+    [ValidaDto]
+    public class SupervisorController : ControllerBase
+    {
+        private readonly IConsultasCiclo consultasCiclo;
+
+        public SupervisorController(IConsultasCiclo consultasCiclo)
+        {
+            this.consultasCiclo = consultasCiclo ?? throw new System.ArgumentNullException(nameof(consultasCiclo));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CicloDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public IActionResult Get()
+        {
+            return Ok(consultasCiclo.Listar(new List<int>()));
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/SupervisorEscolaDre.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SME.SGP.Dominio
+{
+    public class SupervisorEscolaDre : EntidadeBase
+    {
+        public long SupervisorId { get; set; }
+        public long EscolaId { get; set; }
+        public long DreId { get; set; }
+    }
+}
