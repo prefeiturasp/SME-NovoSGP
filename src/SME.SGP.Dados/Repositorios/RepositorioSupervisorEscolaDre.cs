010c7789a09720e8e0843ee7ef8b56a1205f08d3
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-08-30 18:15:28 -0300

    Início Repositório
    AB#4288

--- src/SME.SGP.Dados/Repositorios/RepositorioSupervisorEscolaDre.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioSupervisorEscolaDre.cs
@@ -1,15 +1,30 @@
-﻿using SME.SGP.Dados.Contexto;
+﻿using Dapper;
+using SME.SGP.Dados.Contexto;
 using SME.SGP.Dominio;
-using System;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Dto;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SME.SGP.Dados.Repositorios
 {
-    public class RepositorioSupervisorEscolaDre : RepositorioBase<SupervisorEscolaDre>
+    public class RepositorioSupervisorEscolaDre : RepositorioBase<SupervisorEscolaDre>, IRepositorioSupervisorEscolaDre
     {
         public RepositorioSupervisorEscolaDre(ISgpContext conexao) : base(conexao)
         {
+        }
+
+        public IEnumerable<SupervisorEscolasDto> ObtemSupervisoresEscola(long dreId, long supervisorId)
+        {
+            StringBuilder query = new StringBuilder();
+
+            query.AppendLine("select *");
+            query.AppendLine("from supervisor_escola_dre");
+
+            //query.AppendLine("where");
+            //query.AppendLine("tca.ano = @ano");
+
+            return database.Conexao.Query<SupervisorEscolasDto>(query.ToString()).AsList();
         }
     }
 }
---
+++ src/SME.SGP.Dto/UnidadeEscolarDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Dto
+{
+    public class UnidadeEscolarDto
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
