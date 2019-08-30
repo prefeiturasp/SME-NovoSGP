573f22e5e9876a980ef17a42e1bf26c2e140914b
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-08-30 16:25:30 -0300

    Fontes iniciais
    AB#4288

---
+++ scripts/V5__SUPERVISOR_ESCOLA.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE IF NOT EXISTS public.supervisor_escola_dre
+(
+    id bigint NOT NULL,
+    id_supervisor bigint not null,
+    id_escola bigint not null,
+    id_dre bigint not null,
+    criado_em timestamp without time zone NOT NULL,
+    criado_por character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    alterado_em timestamp without time zone,
+    alterado_por character varying(200) COLLATE pg_catalog."default",
+    criado_rf character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    alterado_rf character varying(200) COLLATE pg_catalog."default",
+    CONSTRAINT supervisor_escola_pk PRIMARY KEY (id),
+    constraint supervisor_escola_dre_ck  UNIQUE (id_supervisor, id_escola, id_dre)
+)
---
+++ src/SME.SGP.Dados/Repositorios/RepositorioSupervisorEscolaDre.cs
@@ -0,0 +1,15 @@
+﻿using SME.SGP.Dados.Contexto;
+using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioSupervisorEscolaDre : RepositorioBase<SupervisorEscolaDre>
+    {
+        public RepositorioSupervisorEscolaDre(ISgpContext conexao) : base(conexao)
+        {
+        }
+    }
+}
