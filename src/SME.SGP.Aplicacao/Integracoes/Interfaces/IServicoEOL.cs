f9f0cea575b673fd981ce1d7af826ab93d99e897
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-08-31 12:30:08 -0300

    Iniciando busca de turmas atribuidas ao professor por ano letivo e escola AB#4252

--- src/SME.SGP.Aplicacao/Integracoes/Interfaces/IServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Integracoes/Interfaces/IServicoEOL.cs
@@ -1,6 +1,11 @@
-﻿namespace SME.SGP.Aplicacao.Integracoes
+﻿using SME.SGP.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Integracoes
 {
     public interface IServicoEOL
     {
+        Task<IEnumerable<TurmaDto>> ObterTurmasAtribuidasAoProfessorPorEscolaEAnoLetivo(string rfProfessor, long codigoEscola, int anoLetivo);
     }
 }
--- src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
@@ -1,4 +1,8 @@
-﻿using System.Net.Http;
+﻿using Newtonsoft.Json;
+using SME.SGP.Dto;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao.Integracoes
 {
@@ -9,6 +13,18 @@ namespace SME.SGP.Aplicacao.Integracoes
         public ServicoEOL(HttpClient httpClient)
         {
             this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<TurmaDto>> ObterTurmasAtribuidasAoProfessorPorEscolaEAnoLetivo(string rfProfessor, long codigoEscola, int anoLetivo)
+        {
+            var resposta = await httpClient.GetAsync($"professores/{rfProfessor}/escolas/{codigoEscola}/turmas/anos_letivos/{anoLetivo}");
+
+            if (resposta.IsSuccessStatusCode)
+            {
+                var json = await resposta.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<TurmaDto>>(json);
+            }
+            return null;
         }
     }
 }
