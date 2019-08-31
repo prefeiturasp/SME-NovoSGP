c461ead09fb9b4d49bdf1fbaa4cfc01f4cf8fc88
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-08-31 18:48:40 -0300

    Teste integrado do endpoitn de consulta por dre
    AB#4288

---
+++ teste/SME.SGP.Integracao.Teste/SupervisorTeste.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using SME.SGP.Dto;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Extensions.Ordering;
+
+namespace SME.SGP.Integracao.Teste
+{
+    public class SupervisorTeste : IClassFixture<TestServerFixture>
+    {
+        private readonly TestServerFixture _fixture;
+
+        public SupervisorTeste(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact, Order(3)]
+        public void Deve_Consultar_Escolas_Por_Dre()
+        {
+            _fixture._clientApi.DefaultRequestHeaders.Clear();
+
+            var postResult = _fixture._clientApi.GetAsync("api/v1/supervisores/dre/108100").Result;
+
+            if (postResult.IsSuccessStatusCode)
+            {
+                var supervisorEscolasDto = JsonConvert.DeserializeObject<List<SupervisorEscolasDto>>(postResult.Content.ReadAsStringAsync().Result);
+                Assert.True(supervisorEscolasDto.Count > 0);
+            }
+        }
+
+        //TODO FAZER TESTE COM CONSULTA DO SUPERVISOR, POREM AINDA NÃO HÁ ENDPOINT DE CADASTRO.
+
+        private string TransformarEmJson(object model)
+        {
+            return JsonConvert.SerializeObject(model);
+        }
+    }
+}
