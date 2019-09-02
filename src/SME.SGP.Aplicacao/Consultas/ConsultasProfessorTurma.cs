cd1718e74472162a77f417683cda33a4be02101e
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-09-02 17:01:45 -0300

    Consulta de turmas por professor AB#4497

--- src/SME.SGP.Aplicacao/Consultas/ConsultasProfessorTurma.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasProfessorTurma.cs
@@ -1,180 +1,47 @@
-﻿using SME.SGP.Dto;
+﻿using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Aplicacao.Integracoes.Respostas;
+using SME.SGP.Dto;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SME.SGP.Aplicacao
 {
     public class ConsultasProfessor : IConsultasProfessor
     {
-        public ConsultasProfessor()
+        private readonly IServicoEOL servicoEOL;
+
+        public ConsultasProfessor(IServicoEOL servicoEOL)
         {
+            this.servicoEOL = servicoEOL ?? throw new System.ArgumentNullException(nameof(servicoEOL));
         }
 
         public IEnumerable<ProfessorTurmaDto> Listar(string codigoRf)
         {
-            string escola1 = "Professor Caíque Ferreira";
-            string escola1Abrev = "Prof. Caíque F.";
-            string escola2 = "Professor Massato Kano";
-            string escola2Abrev = "Prof.r MK";
-            string escola3 = "DR. Ruy Pereira Mendonça";
-            string escola3Abrev = "Prof. PM";
-            var lista = new List<ProfessorTurmaDto>
+            return MapearParaDto(servicoEOL.ObterListaTurmasPorProfessor(codigoRf));
+        }
+
+        private IEnumerable<ProfessorTurmaDto> MapearParaDto(IEnumerable<ProfessorTurmaReposta> turmas)
+        {
+            return turmas?.Select(m => new ProfessorTurmaDto()
             {
-                new ProfessorTurmaDto
-                {
-                    Ano = 1,
-                    AnoLetivo = 2019,
-                    CodModalidade = 5,
-                    Modalidade = "Fundamental",
-                    TipoSemestre = 2,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola1,
-                    UeAbrev = escola1Abrev,
-                    NomeTurma = "1A",
-                    TipoUE = "EF"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 3,
-                    Modalidade = "EJA",
-                    TipoSemestre = 2,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "Dre CL",
-                    Ue = escola1,
-                    UeAbrev = escola1Abrev,
-                    NomeTurma = "2B",
-                    TipoUE = "EJA"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 3,
-                    Modalidade = "EJA",
-                    TipoSemestre = 2,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola1,
-                    UeAbrev = escola1Abrev,
-                    NomeTurma = "2C",
-                    TipoUE = "EJA"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 5,
-                    Modalidade = "Fundamental",
-                    TipoSemestre = 1,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola2,
-                    UeAbrev = escola2Abrev,
-                    NomeTurma = "2B",
-                    TipoUE = "EF"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 1,
-                    AnoLetivo = 2019,
-                    CodModalidade = 5,
-                    Modalidade = "Fundamental",
-                    TipoSemestre = 1,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola2,
-                    UeAbrev = escola2Abrev,
-                    NomeTurma = "1A",
-                    TipoUE = "EF"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 3,
-                    Modalidade = "EJA",
-                    TipoSemestre = 2,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola2,
-                    UeAbrev = escola2Abrev,
-                    NomeTurma = "2B",
-                    TipoUE = "EJA"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 3,
-                    Modalidade = "EJA",
-                    TipoSemestre = 2,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola2,
-                    UeAbrev = escola2Abrev,
-                    NomeTurma = "2B",
-                    TipoUE = "EJA"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 5,
-                    Modalidade = "Fundamental",
-                    TipoSemestre = 1,
-                    Dre = "DRE Campo Limpo",
-                    DreAbrev = "DRE CL",
-                    Ue = escola2,
-                    UeAbrev = escola2Abrev,
-                    NomeTurma = "2A",
-                    TipoUE = "EF"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 2,
-                    AnoLetivo = 2019,
-                    CodModalidade = 5,
-                    Modalidade = "Fundamental",
-                    TipoSemestre = 1,
-                    Dre = "DRE São Judas",
-                    DreAbrev = "DRE SJ",
-                    Ue = escola3,
-                    UeAbrev = escola3Abrev,
-                    NomeTurma = "2A",
-                    TipoUE = "EF"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 3,
-                    AnoLetivo = 2019,
-                    CodModalidade = 5,
-                    Modalidade = "Fundamental",
-                    TipoSemestre = 1,
-                    Dre = "DRE São Judas",
-                    DreAbrev = "DRE SJ",
-                    Ue = escola3,
-                    UeAbrev = escola3Abrev,
-                    NomeTurma = "3A",
-                    TipoUE = "EF"
-                },
-                new ProfessorTurmaDto
-                {
-                    Ano = 1,
-                    AnoLetivo = 2019,
-                    CodModalidade = 3,
-                    Modalidade = "EJA",
-                    TipoSemestre = 2,
-                    Dre = "DRE São Judas",
-                    DreAbrev = "DRE SJ",
-                    Ue = escola3,
-                    UeAbrev = escola3Abrev,
-                    NomeTurma = "1A",
-                    TipoUE = "EJA"
-                }
-            };
-            return lista;
+                Ano = m.Ano,
+                AnoLetivo = m.AnoLetivo,
+                CodDre = m.CodDre,
+                CodEscola = m.CodEscola,
+                CodModalidade = m.CodModalidade,
+                CodTipoEscola = m.CodTipoEscola,
+                CodTipoUE = m.CodTipoUE,
+                CodTurma = m.CodTurma,
+                Dre = m.Dre,
+                DreAbrev = m.DreAbrev,
+                Modalidade = m.Modalidade,
+                NomeTurma = m.NomeTurma,
+                TipoEscola = m.TipoEscola,
+                TipoSemestre = m.TipoSemestre,
+                TipoUE = m.TipoUE,
+                Ue = m.Ue,
+                UeAbrev = m.UeAbrev
+            });
         }
     }
 }
