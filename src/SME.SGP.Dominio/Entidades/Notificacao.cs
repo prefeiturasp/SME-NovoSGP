0f19e35e2a4de66a0689339255537e41d6cfd800
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-09-03 17:27:54 -0300

    Modelagem da entidade Notificação
    AB#4306

---
+++ src/SME.SGP.Dominio/Entidades/Notificacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio.Entidades
+{
+    public class Notificacao :EntidadeBase
+    {
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+        public string UsuarioId { get; set; }
+        public NotificacaoStatus Status { get; set; }
+        public NotificacaoCategoria Categoria { get; set; }
+        public string EscolaId { get; set; }
+        public string DreId { get; set; }
+        public bool PodeRemover { get; set; }
+
+    }
+}
