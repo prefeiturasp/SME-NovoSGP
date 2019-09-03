cc7b955cc6dc169c7d7c15bd92ccaa63b745e9fd
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-09-03 17:27:54 -0300

    Modelagem da entidade Notificação
    AB#4306

---
+++ src/SME.SGP.Dominio/Enumerados/NotificacaoStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio
+{
+    public enum NotificacaoStatus
+    {
+        Enviada = 1,
+        Lida = 2
+    }
+}
---
+++ teste/SME.SGP.Integracao.Teste/NotificacaoTeste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Extensions.Ordering;
+
+namespace SME.SGP.Integracao.Teste
+{
+    [Collection("Testserver collection")]
+    public class NotificacaoTeste
+    {
+        private readonly TestServerFixture _fixture;
+
+        public NotificacaoTeste(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact, Order(3)]
+        public void Teste_Notificacao()
+        {
+            Assert.True(true);
+        }
+    }
+}
