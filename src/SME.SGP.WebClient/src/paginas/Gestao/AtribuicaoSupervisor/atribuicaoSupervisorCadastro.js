ff04b370344ea81a2d68135d2b123f53daeb351b
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-09-05 14:49:57 -0300

    Adicionando componente de select AB#4275

--- src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorCadastro.js
+++ src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorCadastro.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import SelectList from '../../../componentes/selectList';
+import SelectList from '~/componentes/selectList';
+import Card from '~/componentes/card';
+import SelectComponent from 'componentes/select';
 
 export default function AtribuicaoSupervisorCadastro() {
   const [listaUES, setListaUES] = useState([]);
+  const [listaDres, setListaDres] = useState([{ id: 1, descricao: 'teste' }]);
   const [listaUESSelecionadas, setListaUESSelecionadas] = useState([]);
+  const [dreSelecionada, setDreSelecionada] = useState([]);
 
   useEffect(() => {
     function carregarListas() {
-      // TODO - Mock
       setListaUES([
         {
           key: 1,
@@ -21,24 +24,56 @@ export default function AtribuicaoSupervisorCadastro() {
         },
       ]);
     }
+    console.log(dreSelecionada);
 
     carregarListas();
   }, []);
 
   function handleChange(targetKeys) {
-    console.log(targetKeys);
     setListaUESSelecionadas(targetKeys);
   }
 
+  function selecionaDre(e) {
+    setDreSelecionada(e);
+  }
   return (
     <>
-      <SelectList
-        id="select-list-supervisor"
-        mockData={listaUES}
-        targetKeys={listaUESSelecionadas}
-        handleChange={handleChange}
-        titulos={["UE'S SEM ATRIBUIÇÃO", "UE'S ATRIBUIDAS AO SUPERVISOR"]}
-      />
+      <h3>Atribuição de Supervisor</h3>
+      <Card>
+        <div className="col-xs-12 col-md-6 col-lg-6">
+          <SelectComponent
+            label="SELECIONE A DRE:"
+            className="col-md-12"
+            name="dre"
+            id="dre"
+            lista={listaDres}
+            valueOption="id"
+            valueText="descricao"
+            onChange={selecionaDre}
+            valueSelect={dreSelecionada}
+          />
+        </div>
+        <div className="col-xs-12 col-md-6 col-lg-6">
+          <SelectComponent
+            label="SUPERVISOR:"
+            className="col-md-12"
+            name="dre"
+            id="dre"
+            lista={listaDres}
+            valueOption="id"
+            valueText="descricao"
+            onChange={selecionaDre}
+            valueSelect={dreSelecionada}
+          />
+        </div>
+        <SelectList
+          id="select-list-supervisor"
+          mockData={listaUES}
+          targetKeys={listaUESSelecionadas}
+          handleChange={handleChange}
+          titulos={["UE'S SEM ATRIBUIÇÃO", "UE'S ATRIBUIDAS AO SUPERVISOR"]}
+        />
+      </Card>
     </>
   );
 }
