b33cf5724f496863f1f700065a71f64270942fbc
Flávio Ricardo <flavioricardo@live.com>
2019-09-05 15:22:57 -0300

    Navbar, cabeçalho AB#4255

--- src/SME.SGP.WebClient/src/componentes-sgp/navbar.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/navbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Filtro from './filtro';
+import styled from 'styled-components';
+import LogoSGP from '../recursos/LogoSGP.svg';
 
 const Navbar = () => {
+  const Nav = styled.nav`
+    height: 70px;
+  `;
+
+  const collapsed = true;
+
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
-      <Link className="navbar-brand col-sm-3 col-md-2 mr-0" to="/">
+    <Nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
+      <Link
+        className={`navbar-brand ${} mr-0`}
+        to="/"
+      >
         SGP
       </Link>
       <Filtro />
-    </nav>
+    </Nav>
   );
 };
 
