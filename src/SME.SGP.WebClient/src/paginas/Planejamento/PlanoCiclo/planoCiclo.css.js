26ca24018b69e3868a472201ea48b8c18e9823c4
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2019-09-05 18:43:20 -0300

    plano ciclo - ajustes AB#4727

--- src/SME.SGP.WebClient/src/paginas/Planejamento/PlanoCiclo/planoCiclo.css.js
+++ src/SME.SGP.WebClient/src/paginas/Planejamento/PlanoCiclo/planoCiclo.css.js
@@ -81,4 +81,36 @@ export const InseridoAlterado = styled.div`
   }
 `;
 
+export const Titulo = styled.div`
+  object-fit: contain;
+  font-family: Roboto;
+  font-size: 24px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #42474a;
+  margin-bottom: -23px;
+`;
+
+export const TituloAno = styled.span`
+  font-weight: bold;
+  font-size: 16px;
+  color: ${Base.Roxo};
+`;
+
+export const Planejamento = styled.div`
+  object-fit: contain;
+  font-family: Roboto;
+  font-size: 11px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #c8c8c8;
+  padding-top: 6px;
+`;
+
 export const Container = styled.div``;
