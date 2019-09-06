6d18f95c61c36c1c702caaf67fd5ed63b301086b
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-09-05 22:25:01 -0300

    Endpoint de detalhes da notificação
    AB#4836

--- src/SME.SGP.Aplicacao/Consultas/ConsultasNotificacao.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasNotificacao.cs
@@ -1,4 +1,5 @@
-﻿using SME.SGP.Dominio.Interfaces;
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Dto;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace SME.SGP.Aplicacao
         public IEnumerable<NotificacaoBasicaDto> Listar(NotificacaoFiltroDto filtroNotificacaoDto)
         {
             var retorno = repositorioNotificacao.ObterPorDreOuEscolaOuStatusOuTurmoOuUsuarioOuTipo(filtroNotificacaoDto.DreId,
-                filtroNotificacaoDto.EscolaId, (int)filtroNotificacaoDto.Status, filtroNotificacaoDto.TurmaId, filtroNotificacaoDto.UsuarioId, 
+                filtroNotificacaoDto.EscolaId, (int)filtroNotificacaoDto.Status, filtroNotificacaoDto.TurmaId, filtroNotificacaoDto.UsuarioId,
                 (int)filtroNotificacaoDto.Tipo, (int)filtroNotificacaoDto.Categoria);
 
             return from r in retorno
@@ -29,6 +30,39 @@ namespace SME.SGP.Aplicacao
                        Status = r.Status.ToString(),
                        Tipo = r.Tipo.ToString()
                    };
+        }
+
+        public NotificacaoDetalheDto Obter(long notificacaoId)
+        {
+            var notificacao = repositorioNotificacao.ObterPorId(notificacaoId);
+
+            if (notificacao == null)
+                throw new NegocioException($"Notificação de Id: '{notificacaoId}' não localizada.");
+
+            if (notificacao.Status != NotificacaoStatus.Lida && notificacao.DeveSerMarcadoComoLidaAoObterDetalhe())
+                repositorioNotificacao.Salvar(notificacao);
+
+            var detalheDto = MapearEntidadeParaDetalheDto(notificacao);
+            detalheDto.MostrarBotaoMarcarComoLido = notificacao.DeveMarcarComoLido;
+            detalheDto.MostrarBotoesDeAprovacao = notificacao.DeveAprovar;
+
+            return detalheDto;
+        }
+
+        private static NotificacaoDetalheDto MapearEntidadeParaDetalheDto(Dominio.Notificacao retorno)
+        {
+            return new NotificacaoDetalheDto()
+            {
+                AlteradoEm = retorno.AlteradoEm.ToString(),
+                AlteradoPor = retorno.AlteradoPor,
+                CriadoEm = retorno.CriadoEm.ToString(),
+                CriadoPor = retorno.CriadoPor,
+                Id = retorno.Id,
+                Mensagem = retorno.Mensagem,
+                Situacao = retorno.Status.ToString(),
+                Tipo = retorno.Tipo.ToString(),
+                Titulo = retorno.Titulo
+            };
         }
     }
 }
---
+++ src/SME.SGP.Dto/NotificacaoDetalheDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SME.SGP.Dto
+{
+    public class NotificacaoDetalheDto
+    {
+        public string AlteradoEm { get; set; }
+        public string AlteradoPor { get; set; }
+        public string CriadoEm { get; set; }
+        public string CriadoPor { get; set; }
+        public long Id { get; set; }
+        public string Mensagem { get; set; }
+        public bool MostrarBotaoMarcarComoLido { get; set; }
+        public bool MostrarBotoesDeAprovacao { get; set; }
+        public string Situacao { get; set; }
+        public string Tipo { get; set; }
+        public string Titulo { get; set; }
+    }
+}
