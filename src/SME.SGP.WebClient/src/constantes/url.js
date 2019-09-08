d71181dba14c4700a965d1242613a45132abc44e
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-09-07 22:19:55 -0300

    Correções de bugs no frontend
    AB#4939
    AB#4935
    AB#4934
    AB#4931
    AB#4963
    AB#4964
    AB#4968
    AB#4969

--- src/SME.SGP.WebClient/src/constantes/url.js
+++ src/SME.SGP.WebClient/src/constantes/url.js
@@ -1,4 +1,4 @@
-
-export const URL_PLANO_ANUAL = "/planejamento/plano-anual";
-export const URL_PLANO_CICLO = "/planejamento/plano-ciclo/2019/1";
-export const URL_FREQ_PLANO_AULA = "/";
+export const URL_PLANO_ANUAL = '/planejamento/plano-anual';
+export const URL_PLANO_CICLO = '/planejamento/plano-ciclo/2019/1';
+export const URL_FREQ_PLANO_AULA = '/';
+export const URL_HOME = '/';
