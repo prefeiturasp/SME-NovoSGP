5f4c50d424fb49592cb92b08b1e8a97434f3700a
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-09-08 19:15:46 -0300

    Removendo código comentado

--- src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorCadastro.js
+++ src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorCadastro.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import SelectList from '~/componentes/selectList';
 import Card from '~/componentes/card';
 import SelectComponent from '~/componentes/select';
