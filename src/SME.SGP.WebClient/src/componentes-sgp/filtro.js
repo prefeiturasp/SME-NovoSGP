545e1f9bc9b263c052c4feaa8783913f99297149
Flávio Ricardo <flavioricardo@live.com>
2019-09-09 13:23:06 -0300

    CodEscola na lista de turmas do professor

--- src/SME.SGP.WebClient/src/componentes-sgp/filtro.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/filtro.js
@@ -213,6 +213,7 @@ const Filtro = () => {
           codigo: dado.codTurma,
           ano: dado.ano,
           turma: dado.nomeTurma,
+          codEscola: dado.codEscola,
         });
       }
     });
