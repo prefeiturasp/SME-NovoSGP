8eed4e554610e7dca113461330eaae4678e26ae0
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-09-11 17:06:36 -0300

    Inicio da tela de detalhes da notificação AB#5192

---
+++ src/SME.SGP.WebClient/src/paginas/Notificacoes/Detalhes/estiloDetalhe.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const EstiloDetalhe = styled.div`
+  width: 100%;
+  border: 1px solid #dadada;
+  border-radius: 4px;
+  margin: 20px;
+  padding: 20px;
+
+  .bg-id {
+    background-color: #d9d8d8;
+    border-radius: 4px;
+    font-size: 14px;
+    height: 72px;
+    font-weight: bold;
+    justify-content: center;
+    justify-items: center;
+    display: flex;
+    padding: 10px;
+  }
+`;
