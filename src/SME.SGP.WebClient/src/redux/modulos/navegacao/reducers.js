605d39bbd9125eb88ba5546d79a2f9882378a88f
Eliton Santos <eliton.santos@amcom.com.br>
2019-09-11 19:29:54 -0300

    correções breadcrumb AB#5287

--- src/SME.SGP.WebClient/src/redux/modulos/navegacao/reducers.js
+++ src/SME.SGP.WebClient/src/redux/modulos/navegacao/reducers.js
@@ -1,55 +1,6 @@
 import produce from 'immer';
-import Principal from '../../../paginas/Principal/principal';
-import PlanoCiclo from '../../../paginas/Planejamento/PlanoCiclo/planoCiclo';
-import PlanoAnual from '../../../paginas/Planejamento/PlanoAnual/planoAnual';
-import AtribuicaoSupervisorLista from '~/paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorLista';
-import AtribuicaoSupervisorCadastro from '~/paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorCadastro';
 
 const rotas = new Map();
-rotas.set('/', {
-  params: ':rf?',
-  breadcrumbName: 'Home',
-  parent: null,
-  component: Principal,
-  exact: true,
-  limpaSelecaoMenu: true
-});
-rotas.set('/planejamento/plano-ciclo', {
-  breadcrumbName: 'Plano de Ciclo',
-  menu: 'Planejamento',
-  parent: '/',
-  component: PlanoCiclo,
-  exact: true,
-});
-rotas.set('/planejamento/plano-anual', {
-  breadcrumbName: 'Plano Anual',
-  menu: 'Planejamento',
-  parent: '/',
-  component: PlanoAnual,
-  exact: false,
-});
-rotas.set('/gestao/atribuicao-supervisor-lista', {
-  breadcrumbName: 'Atribuição de Supervisor',
-  menu: 'Gestão',
-  parent: '/',
-  component: AtribuicaoSupervisorLista,
-  exact: true,
-});
-rotas.set('/gestao/atribuicao-supervisor', {
-  breadcrumbName: 'Atribuição de Supervisor',
-  menu: 'Gestão',
-  parent: '/',
-  component: AtribuicaoSupervisorCadastro,
-  exact: true,
-});
-rotas.set('/gestao/atribuicao-supervisor/:dreId/:supervisorId', {
-  breadcrumbName: 'Atribuição de Supervisor',
-  menu: 'Gestão',
-  parent: '/',
-  component: AtribuicaoSupervisorCadastro,
-  exact: true,
-});
-
 const inicial = {
   collapsed: false,
   activeRoute: '/',
