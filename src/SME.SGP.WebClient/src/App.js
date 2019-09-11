d40f1681dd1e4064bf0bf75165f5ab38640d19cf
Eliton Santos <eliton.santos@amcom.com.br>
2019-09-11 20:17:54 -0300

    Alterações breadcrumb AB#5287

--- src/SME.SGP.WebClient/src/App.js
+++ src/SME.SGP.WebClient/src/App.js
@@ -8,8 +8,14 @@ import Navbar from './componentes-sgp/navbar';
 import Sider from './componentes-sgp/sider';
 import { store } from './redux';
 import Conteudo from './componentes-sgp/conteudo';
+import { activeRoute } from './redux/modulos/navegacao/actions';
 
 function App() {
+
+  history.listen((location) => {
+    store.dispatch(activeRoute(location.pathname));
+    localStorage.setItem('rota-atual', location.pathname);
+  });
 
   return (
     <Provider store={store}>
