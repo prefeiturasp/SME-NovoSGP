1d3be3be271ce8c279f5ee78651561e1afafdd57
Eliton Santos <eliton.santos@amcom.com.br>
2019-09-11 19:29:54 -0300

    correções breadcrumb AB#5287

--- src/SME.SGP.WebClient/src/rotas/rotas.js
+++ src/SME.SGP.WebClient/src/rotas/rotas.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import history from '../servicos/history';
-import { activeRoute } from '../redux/modulos/navegacao/actions'
+import { activeRoute, getRotas } from '../redux/modulos/navegacao/actions'
 import { store } from '../redux';
+import Principal from '../paginas/Principal/principal';
+import PlanoCiclo from '../paginas/Planejamento/PlanoCiclo/planoCiclo';
+import PlanoAnual from '../paginas/Planejamento/PlanoAnual/planoAnual';
+import AtribuicaoSupervisorLista from '../paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorLista';
+import AtribuicaoSupervisorCadastro from '../paginas/Gestao/AtribuicaoSupervisor/atribuicaoSupervisorCadastro';
 
 export default function Rotas() {
-  const NavegacaoStore = useSelector(store => store.navegacao);
-  const rotas = NavegacaoStore.rotas;
+  const rotas = new Map();
 
+  rotas.set('/', {
+    params: ':rf?',
+    breadcrumbName: 'Home',
+    parent: null,
+    component: Principal,
+    exact: true,
+    limpaSelecaoMenu: true
+  });
+  rotas.set('/planejamento/plano-ciclo', {
+    breadcrumbName: 'Plano de Ciclo',
+    menu: 'Planejamento',
+    parent: '/',
+    component: PlanoCiclo,
+    exact: true,
+  });
+  rotas.set('/planejamento/plano-anual', {
+    breadcrumbName: 'Plano Anual',
+    menu: 'Planejamento',
+    parent: '/',
+    component: PlanoAnual,
+    exact: false,
+  });
+  rotas.set('/gestao/atribuicao-supervisor-lista', {
+    breadcrumbName: 'Atribuição de Supervisor',
+    menu: 'Gestão',
+    parent: '/',
+    component: AtribuicaoSupervisorLista,
+    exact: true,
+  });
+  rotas.set('/gestao/atribuicao-supervisor', {
+    breadcrumbName: 'Atribuição de Supervisor',
+    parent: '/gestao/atribuicao-supervisor-lista',
+    component: AtribuicaoSupervisorCadastro,
+    exact: true,
+  });
+  rotas.set('/gestao/atribuicao-supervisor/:dreId/:supervisorId', {
+    breadcrumbName: 'Atribuição de Supervisor',
+    parent: '/gestao/atribuicao-supervisor-lista',
+    component: AtribuicaoSupervisorCadastro,
+    exact: true,
+  });
+
+  const rotasParaRedux = new Map();
 
   history.listen((location) => {
     store.dispatch(activeRoute(location.pathname));
@@ -20,7 +66,15 @@ export default function Rotas() {
     const rota = value;
     rota.path = key + (value.params ? value.params : '');
     rotasArray.push(rota);
+    const rotaRedux = {
+      params: value.params,
+      breadcrumbName: value.breadcrumbName,
+      parent: value.parent
+    }
+    rotasParaRedux.set(value.path, rotaRedux)
   }
+
+  store.dispatch(getRotas(rotasParaRedux));
 
   return (
     <div>
