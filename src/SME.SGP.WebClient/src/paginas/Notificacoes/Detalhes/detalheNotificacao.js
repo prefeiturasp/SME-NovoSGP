d21f26a225e01539e3c40f89fcc2067fcd013e8a
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-09-12 20:29:40 -0300

    Consumindo endpoint para obter uma notificação AB#4305

--- src/SME.SGP.WebClient/src/paginas/Notificacoes/Detalhes/detalheNotificacao.js
+++ src/SME.SGP.WebClient/src/paginas/Notificacoes/Detalhes/detalheNotificacao.js
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '~/componentes/card';
 import { EstiloDetalhe } from './estiloDetalhe';
+import api from '~/servicos/api';
+import { erros } from '~/servicos/alertas';
+import * as cores from '~/componentes/colors';
+import Button from '~/componentes/button';
+import Cabecalho from '~/componentes-sgp/cabecalho';
+import Textarea from '~/componentes/textarea';
 
-const DetalheNotificacao = () => {
+const DetalheNotificacao = ({ match }) => {
+  const [observacao, setObservacao] = useState('teste');
+  const [notificacao, setNotificacao] = useState({
+    alteradoEm: '',
+    alteradoPor: null,
+    criadoEm: '',
+    criadoPor: '',
+    id: 1,
+    mensagem: '',
+    mostrarBotaoMarcarComoLido: false,
+    mostrarBotoesDeAprovacao: true,
+    situacao: '',
+    tipo: '',
+    titulo: '',
+  });
+
+  useEffect(() => {
+    const buscaNotificacao = id => {
+      api
+        .get(`v1/notificacoes/${id}`)
+        .then(resposta => setNotificacao(resposta.data))
+        .catch(listaErros => erros(listaErros));
+    };
+    if (match.params.id) {
+      buscaNotificacao(match.params.id);
+    }
+  }, [match.params.id]);
+
   return (
     <>
-      <h3>Notificações</h3>
-      <Card>
+      <Cabecalho pagina="Notificações" />
+      <Card mtop="mt-2">
+        <div className="col-md-12 d-flex justify-content-end pb-3">
+          {notificacao.mostrarBotoesDeAprovacao && (
+            <>
+              <Button
+                label="Aceitar"
+                color={cores.Colors.Roxo}
+                className="mr-2"
+                onClick={() => true}
+              />
+              <Button
+                label="Recusar"
+                color={cores.Colors.Roxo}
+                border
+                className="mr-2"
+                onClick={() => true}
+              />
+            </>
+          )}
+          {notificacao.mostrarBotaoMarcarComoLido && (
+            <Button
+              label="Marcar como lida"
+              color={cores.Colors.Azul}
+              border
+              className="mr-2"
+              onClick={() => true}
+            />
+          )}
+          <Button
+            label="Excluir"
+            color={cores.Colors.Vermelho}
+            border
+            className="mr-2"
+            border
+            onClick={() => true}
+          />
+        </div>
         <EstiloDetalhe>
           <div className="col-xs-12 col-md-12 col-lg-12">
             <div className="row mg-bottom">
@@ -16,7 +85,7 @@ const DetalheNotificacao = () => {
                     ID
                   </div>
                   <div className="id-notificacao col-xs-12 col-md-12 col-lg-12 text-center">
-                    00000010
+                    {notificacao.id}
                   </div>
                 </div>
               </div>
@@ -24,20 +93,28 @@ const DetalheNotificacao = () => {
                 <div className="row">
                   <div className="col-xs-12 col-md-12 col-lg-12">
                     <div className="notificacao-horario">
-                      Notificação automática 15/08/2019, 09:20
+                      Notificação automática {notificacao.criadoEm}
                     </div>
                   </div>
                   <div className="col-xs-12 col-md-12 col-lg-12">
                     <div className="row">
                       <div className="col-xs-12 col-md-12 col-lg-4 titulo-coluna">
-                        Tipo <div className="conteudo-coluna">Frequência</div>
+                        Tipo{' '}
+                        <div className="conteudo-coluna">
+                          {notificacao.tipo}
+                        </div>
                       </div>
                       <div className="col-xs-12 col-md-12 col-lg-6 titulo-coluna">
-                        Título <div className="conteudo-coluna">Frequência</div>
+                        Título
+                        <div className="conteudo-coluna">
+                          {notificacao.titulo}
+                        </div>
                       </div>
                       <div className="col-xs-12 col-md-12 col-lg-2 titulo-coluna">
                         Situação
-                        <div className="conteudo-coluna">APROVADO</div>
+                        <div className="conteudo-coluna">
+                          {notificacao.situacao}
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -48,22 +125,17 @@ const DetalheNotificacao = () => {
           <hr className="mt-hr" />
           <div className="row">
             <div className="col-xs-12 col-md-12 col-lg-12 mensagem">
-              MENSAGEM: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do et eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad mini venia dolor et. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do et eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad mini venia
-              dolor et.
+              MENSAGEM: {notificacao.mensagem}
             </div>
           </div>
           <div className="row">
             <div className="col-xs-12 col-md-12 col-lg-12 obs">
               <label>Observações</label>
-              <textarea className="form-control" rows="5">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-              </textarea>
+              <Textarea
+                value={observacao}
+                onChange={setObservacao}
+                name="Observacao"
+              />
             </div>
           </div>
         </EstiloDetalhe>
