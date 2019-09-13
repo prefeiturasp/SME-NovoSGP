3a6ec452e499cd11c6878c38d6f1bb5be71116c4
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-09-13 15:36:50 -0300

    Adicionado endpoints para retornar enumerados;
    AB#4838
    AB#5194

--- src/SME.SGP.Dominio/Enumerados/NotificacaoCategoria.cs
+++ src/SME.SGP.Dominio/Enumerados/NotificacaoCategoria.cs
@@ -1,9 +1,16 @@
-﻿namespace SME.SGP.Dominio
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SME.SGP.Dominio
 {
     public enum NotificacaoCategoria
     {
+        [Display(Name = "Alerta")]
         Alerta = 1,
+
+        [Display(Name = "Ação")]
         Workflow_Aprovacao = 2,
+
+        [Display(Name = "Aviso")]
         Aviso = 3
     }
 }
