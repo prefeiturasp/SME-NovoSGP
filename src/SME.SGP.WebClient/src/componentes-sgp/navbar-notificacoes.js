877c334664b065136f0f50bde1808de67dc6dce6
Flávio Ricardo <flavioricardo@live.com>
2019-09-13 18:27:33 -0300

    Finalizando botão de notificações AB#5204

--- src/SME.SGP.WebClient/src/componentes-sgp/navbar-notificacoes.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/navbar-notificacoes.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { Badge } from 'antd';
 import styled from 'styled-components';
 import shortid from 'shortid';
-import { Base } from '../componentes/colors';
-import Button from '~/componentes/button';
+import { Base, Colors } from '../componentes/colors';
+import Button from '../componentes/button';
 
 const NavbarNotificacoes = props => {
   const { Botao, Icone, Texto } = props;
@@ -24,12 +24,66 @@ const NavbarNotificacoes = props => {
 
   const Lista = styled.div`
     font-size: 9px !important;
-    min-width: 360px;
-    right: 0;
+    margin-top: 5px !important;
+    min-width: 360px !important;
+    right: 0 !important;
+    z-index: 0 !important;
+  `;
+
+  const Tr = styled.tr`
+    &:hover {
+      cursor: pointer;
+    }
+    &:first-child {
+      th,
+      td {
+        border-top: 0 none !important;
+      }
+    }
+    td:first-child {
+      color: ${Base.CinzaIconeNotificacao} !important;
+    }
+    th,
+    td {
+      border-color: ${Base.CinzaDesabilitado} !important;
+      padding-bottom: 0.5rem !important;
+      padding-bottom: 0.5rem !important;
+      ${props =>
+        !props.status &&
+        `
+        background: ${Base.RoxoNotificacao} !important;
+        font-weight: bold !important;
+        &.status {
+            color: ${Base.VermelhoNotificacao} !important;
+            text-transform: uppercase !important;
+        }`}
+      &.w-75 {
+        width: 160px !important;
+      }
+      &.w-25 {
+        width: 50px !important;
+      }
+    }
+  `;
+
+  // eslint-disable-next-line no-unused-vars
+  const Aba = styled.div`
+    background: ${Base.Branco} !important;
+    border-left: 1px solid ${Base.CinzaDesabilitado} !important;
+    border-top: 1px solid ${Base.CinzaDesabilitado} !important;
+    display: block !important;
+    height: 21px !important;
+    position: absolute !important;
+    top: -50% !important;
+    transform: rotate(45deg) translateX(303px) translateY(-169px) !important;
+    z-index: -1 !important;
+    width: 21px !important;
   `;
 
   const [mostraNotificacoes, setMostraNotificacoes] = useState(false);
   const [notificacoes, setNotificacoes] = useState([]);
+
+  const statusLista = ['Pendente', 'Aprovado'];
 
   useEffect(() => {
     setNotificacoes([
@@ -48,7 +102,19 @@ const NavbarNotificacoes = props => {
       {
         id: '0000008',
         status: 1,
-        mensagem: 'Validação da frequencia da turma 5A',
+        mensagem: 'Validação da frequencia da turma 4C',
+        data: '14/08/2019 12:05',
+      },
+      {
+        id: '0000007',
+        status: 1,
+        mensagem: 'Validação da frequencia da turma 4D',
+        data: '14/08/2019 12:05',
+      },
+      {
+        id: '0000006',
+        status: 1,
+        mensagem: 'Validação da frequencia da turma 4E',
         data: '14/08/2019 12:05',
       },
     ]);
@@ -61,51 +127,54 @@ const NavbarNotificacoes = props => {
   return (
     <div className="position-relative">
       <Botao className="text-center" onClick={onClickBotao}>
-        <Count count={15} overflowCount={99}>
+        <Count count={notificacoes.length} overflowCount={99}>
           <Icone className="fa fa-bell fa-lg" />
         </Count>
-        <Texto className="d-block mt-1">Notificações</Texto>
+        <Texto
+          className={`d-block mt-1 ${mostraNotificacoes && 'font-weight-bold'}`}
+        >
+          Notificações
+        </Texto>
       </Botao>
-      {mostraNotificacoes && (
+      {mostraNotificacoes && notificacoes.length > 0 && (
         <Lista className="container position-absolute rounded border bg-white shadow p-0">
-          {notificacoes.length > 0 && (
-            <>
-              <table className="table mb-0">
-                <tbody>
-                  {notificacoes.map((notificacao, indice) => {
-                    return (
-                      <tr key={shortid.generate()}>
-                        <td className={`${indice === 0 && 'border-top-0'}`}>
-                          <i className="fa fa-info-circle" />
-                        </td>
-                        <th
-                          className={`${indice === 0 && 'border-top-0'}`}
-                          scope="row"
-                        >
-                          {notificacao.id}
-                        </th>
-                        <td className={`${indice === 0 && 'border-top-0'}`}>
-                          {notificacao.mensagem}
-                        </td>
-                        <td className={`${indice === 0 && 'border-top-0'}`}>
-                          {notificacao.status}
-                        </td>
-                        <td className={`${indice === 0 && 'border-top-0'}`}>
-                          {notificacao.data}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-              <Button
-                label="Ver tudo"
-                className="block"
-                color={Base.Roxo}
-                border
-              />
-            </>
-          )}
+          <table className="table mb-0">
+            <tbody>
+              {notificacoes.map(notificacao => {
+                return (
+                  <Tr key={shortid.generate()} status={notificacao.status}>
+                    <td className="py-1 pl-2 pr-1 text-center align-middle">
+                      <i className="fa fa-info-circle" />
+                    </td>
+                    <th
+                      className="py-1 px-1 text-center align-middle"
+                      scope="row"
+                    >
+                      {notificacao.id}
+                    </th>
+                    <td className="py-1 px-1 align-middle w-75">
+                      {notificacao.mensagem}
+                    </td>
+                    <td className="py-1 px-1 text-center align-middle status">
+                      {statusLista[notificacao.status]}
+                    </td>
+                    <td className="py-1 px-2 align-middle w-25 text-right">
+                      {notificacao.data}
+                    </td>
+                  </Tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <Button
+            label="Ver tudo"
+            className="btn-block"
+            color={Colors.Roxo}
+            fontSize="12px"
+            customRadius="border-top-right-radius: 0 !important; border-top-left-radius: 0 !important;"
+            border
+            bold
+          />
         </Lista>
       )}
     </div>
