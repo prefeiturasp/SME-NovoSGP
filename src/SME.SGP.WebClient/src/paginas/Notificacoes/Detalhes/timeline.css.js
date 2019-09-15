c74457058daf79d1e26ed534fe0ef081e6600891
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-09-15 14:39:27 -0300

    Ajustando componente de timeline AB#4304

--- src/SME.SGP.WebClient/src/paginas/Notificacoes/Detalhes/timeline.css.js
+++ src/SME.SGP.WebClient/src/paginas/Notificacoes/Detalhes/timeline.css.js
@@ -1,53 +1,67 @@
 import styled from 'styled-components';
 const EstiloTimeline = styled.div`
   width: 100%;
+
   .timeline {
-    margin-left: 31px;
+    overflow-x: hidden;
+    padding: 20px 0;
+  }
+
+  .timeline ol {
+    width: 100%;
+    transition: all 1s;
+    margin: 0;
+    padding: 0;
     display: flex;
-    flex-direction: row;
+    justify-content: space-between;
 
-    .timeline-item {
-      margin-right: 20px;
-      margin-top: 10px;
-      width: 100%;
-      text-align: center;
-      max-width: 95.5px;
-      align-items: center;
-      .bar {
-        width: 250px;
-        height: 3px;
-        background-color: #297805;
-        position: relative;
-        top: -14px;
-        left: 55px;
-      }
-      span {
-        font-family: Roboto;
-        font-size: 11px;
-        font-weight: bold;
-        line-height: 1.33;
-        letter-spacing: normal;
-        color: #a8a8a8;
-      }
-      .fa-check-circle {
-        width: 16.1px;
-        height: 23px;
-        font-size: 18px;
-        margin-top: 5px;
-        color: #297805;
-      }
-
-      div {
-        display: flex;
-        justify-items: center;
-        justify-content: center;
-        max-width: 88px;
-      }
-      span.timeline-data {
-        color: #297805;
-        font-size: 10px;
-      }
+    i {
+      position: relative;
+      top: -12px;
+      left: 10px;
+      font-size: 18px;
+      color: #297805;
+      background-color: white;
     }
+  }
+
+  .timeline ol li {
+    list-style: none;
+    position: relative;
+    text-align: center;
+    flex-grow: 1;
+    flex-basis: 0;
+    padding: 0 5px;
+  }
+
+  .timeline ol li:before {
+    content: '';
+    position: relative;
+    top: 4px;
+    width: 100%;
+    height: 3px;
+    display: block;
+    background: #297805;
+    margin: 0 auto 5px auto;
+  }
+  .timeline ol li:last-child:before {
+    width: 0px;
+  }
+
+  .timeline ol li:first-child:before {
+    width: 40% !important;
+  }
+  .timeline ol li:not(:last-child)::after {
+    content: '';
+    width: calc(100% - 34px);
+    height: 3px;
+    display: block;
+    background: #297805;
+    margin: 0;
+    position: absolute;
+    top: 4px;
+    left: calc(50% + 7px);
+    overflow: hidden;
   }
 `;
 
