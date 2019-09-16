314da69781ecd42d6eeea1b3c77bb0cdce7fc634
Flávio Ricardo <flavioricardo@live.com>
2019-09-16 19:33:17 -0300

    Salvando dados no redux AB#5203

--- src/SME.SGP.WebClient/src/redux/modulos/reducers.js
+++ src/SME.SGP.WebClient/src/redux/modulos/reducers.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 
+import notificacoesLista from './notificacoes/reducers';
 import notificacoes from './alertas/reducers';
 import bimestres from './planoAnual/reducers';
 import usuario from './usuario/reducers';
-import navegacao from './navegacao/reducers'
+import navegacao from './navegacao/reducers';
 
 export default combineReducers({
-  notificacoes, 
+  notificacoesLista,
+  notificacoes,
   bimestres,
   usuario,
-  navegacao
+  navegacao,
 });
