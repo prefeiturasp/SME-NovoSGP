85ccd6a6c835377fe80d5d8e278b19c47747221d
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-09-19 20:06:06 -0300

    Update index.js

--- src/SME.SGP.WebClient/src/paginas/Login/index.js
+++ src/SME.SGP.WebClient/src/paginas/Login/index.js
@@ -1,4 +1,38 @@
 import React from 'react';
+import { Row } from 'antd';
+import styled from 'styled-components';
+import FundoLogin from '~/recursos/FundoLogin.svg';
+import LogoSgp from '~/recursos/LogoSgpTexto.svg';
+import Card from '~/componentes/cardBootstrap';
+import Grid from '~/componentes/grid';
+import { Base } from '~/componentes/colors';
+import CardBody from '~/componentes/cardBody';
+import CampoTexto from '~/componentes/campoTexto';
+import FormGroup from '~/componentes/formGroup';
+import Button from '~/componentes/button';
+
+const Fundo = styled(Row)`
+  background: url(${FundoLogin});
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const Logo = styled.img`
+  max-height: 131px !important;
+  max-width: 339px !important;
+`;
+
+const Cartao = styled(Card)`
+  display: flex;
+  height: 100vh;
+  border-radius: 6px;
+`;
+const Centralizar = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default function Login() {
   return (
