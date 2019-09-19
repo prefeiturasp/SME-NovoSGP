82d9787fe78683c7b98fba639456b6580d14e4ff
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-09-19 20:05:39 -0300

    Iniciando Desenvolvimento tela login
    AB#5534 AB#4924

--- src/SME.SGP.WebClient/src/paginas/Login/index.js
+++ src/SME.SGP.WebClient/src/paginas/Login/index.js
@@ -1,5 +1,44 @@
 import React from 'react';
 
 export default function Login() {
-  return <h5>Teste 123</h5>;
+  return (
+    <Fundo>
+      <Grid cols={12} className="d-flex justify-content-end p-0">
+        <Cartao className="col-md-6">
+          <CardBody>
+            <Grid cols={12}>
+              <Logo
+                src={LogoSgp}
+                alt="SGP"
+                className="mx-auto d-block col-md-6"
+              />
+            </Grid>
+            <Centralizar>
+              <Grid cols={6} className="p-0">
+                <FormGroup className="col-md-12 p-0">
+                  <label htmlFor="Usuario">Usuário</label>
+                  <input
+                    id="Usuario"
+                    placeholder="Insira seu usuário ou RF"
+                    className="col-md-12 form-control"
+                  />
+                </FormGroup>
+                <FormGroup className="col-md-12 p-0">
+                  <label htmlFor="Senha">Senha</label>
+                  <input
+                    id="Senha"
+                    placeholder="Insira sua senha"
+                    className="col-md-12 form-control"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Button style="primary" label="Acessar" color={Base.Roxo} />
+                </FormGroup>
+              </Grid>
+            </Centralizar>
+          </CardBody>
+        </Cartao>
+      </Grid>
+    </Fundo>
+  );
 }
