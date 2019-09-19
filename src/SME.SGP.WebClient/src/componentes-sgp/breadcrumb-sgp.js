167a691c72d93c5bc28a62b21a886947038751b2
Eliton Santos <eliton.santos@amcom.com.br>
2019-09-19 11:19:20 -0300

    ajuste

--- src/SME.SGP.WebClient/src/componentes-sgp/breadcrumb-sgp.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/breadcrumb-sgp.js
@@ -10,7 +10,7 @@ import { rotaAtiva } from '../redux/modulos/navegacao/actions';
 const BreadcrumbSgp = () => {
   const BreadcrumbBody = styled.div`
     padding: 10px 0 5px 15px !important;
-    font-size: 10px;
+    font-size: 12px;
     a, a:hover {
       color: ${Base.AzulBreadcrumb};
     }
