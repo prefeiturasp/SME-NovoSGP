2615cad60ac46541a85020dca8dcc3db1bcec221
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-09-18 21:52:56 -0300

    Iniciando criação da tabela de prioridades dos perfis AB#5567

---
+++ src/SME.SGP.Dominio/Entidades/PrioridadePerfil.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio.Entidades
+{
+    public class PrioridadePerfil : EntidadeBase
+    {
+        public string CodigoPerfil { get; set; }
+        public string NomePerfil { get; set; }
+        public int Ordem { get; set; }
+    }
+}
