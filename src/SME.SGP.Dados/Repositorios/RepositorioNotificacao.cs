f64209279490328d092bd0edcde9150bc2865f5b
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-09-20 19:52:56 -0300

    Removendo comentário do código

--- src/SME.SGP.Dados/Repositorios/RepositorioNotificacao.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioNotificacao.cs
@@ -63,27 +63,6 @@ namespace SME.SGP.Dados.Repositorios
             return database.Conexao.Query<Notificacao>(query.ToString(), new { dreId, ueId, turmaId, statusId, tipoId, usuarioRf, categoriaId, titulo, codigo, anoLetivo });
         }
 
-        //public (Notificacao, string) ObterDetalhePorId(long idNotificacao)
-        //{
-        //    var query = new StringBuilder();
-
-        //    query.AppendLine("select n.*, u.* from notificacao n");
-        //    query.AppendLine("where excluida = false ");
-        //    query.AppendLine("and n.id = @idNotificacao; ");
-
-        //    //query.AppendLine("select wfn.observacao from wf_aprovacao_nivel wfn");
-        //    //query.AppendLine("inner join wf_aprovacao_nivel_notificacao wfnn");
-        //    //query.AppendLine("on  wfn.id = wfnn.wf_aprovacao_nivel_id");
-        //    //query.AppendLine("where wfnn.notificacao_id = @idNotificacao");
-
-        //    //using (var multi = database.Conexao.QueryMultiple(query.ToString(), new { idNotificacao }))
-        //    //{
-        //    //    var notificacao = multi.Read<Notificacao>().First();
-        //    //    var observacao = multi.Read<string>().First();
-
-        //    //    return (notificacao, observacao);
-        //    //}
-        //}
         public override Notificacao ObterPorId(long id)
         {
             var query = new StringBuilder();
