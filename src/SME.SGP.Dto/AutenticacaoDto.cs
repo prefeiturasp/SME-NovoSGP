48994400a772c02e23311bd20727bcb54656f915
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-09-22 13:23:14 -0300

    Modificando formdata para body json
    AB#5168

---
+++ src/SME.SGP.Dto/AutenticacaoDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SME.SGP.Dto
+{
+    public class AutenticacaoDto
+    {
+        [Required(ErrorMessage = "É necessário informar usuário ou código Rf.")]
+        [MinLength(5, ErrorMessage = "O usuário ou código Rf deve conter no mínimo 5 caracteres.")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar a senha.")]
+        [MinLength(6, ErrorMessage = "A senha deve conter no mínimo 6 caracteres.")]
+        public string Senha { get; set; }
+    }
+}
