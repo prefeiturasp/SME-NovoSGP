ad17c1c890d43a83fd5e7be3fd201ef3bf0089be
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2019-09-22 20:02:25 -0300

    Validação de usuario e senha e inicando comunicação com a API
    AB#5535
    AB#5536

--- src/SME.SGP.WebClient/src/paginas/Login/index.js
+++ src/SME.SGP.WebClient/src/paginas/Login/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import LoginController from './controller';
 
 export default function Login() {
+
+
+    useEffect(() =>{
+
+      console.log(LoginController.enviarDados({usuario: "danieli.amcom",senha: "Sgp1234"}));
+
+    })
+
   return <h5>Teste 123</h5>;
 }
