9012bcd0e99c15d97346ee7690f20b74b679fb94
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-09-22 13:23:14 -0300

    Modificando formdata para body json
    AB#5168

--- teste/SME.SGP.Integracao.Teste/AutenticacaoTeste.cs
+++ teste/SME.SGP.Integracao.Teste/AutenticacaoTeste.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using SME.SGP.Dto;
+using System;
 using System.Net.Http;
+using System.Text;
 using Xunit;
 
 namespace SME.SGP.Integracao.Teste
@@ -15,16 +17,16 @@ namespace SME.SGP.Integracao.Teste
             this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
         }
 
+        [Fact]
         public void Deve_Retornar_Nao_Autorizado()
         {
             fixture._clientApi.DefaultRequestHeaders.Clear();
 
-            IList<KeyValuePair<string, string>> nameValueCollection = new List<KeyValuePair<string, string>> {
-                { new KeyValuePair<string, string>("login", "4/P7q7W91a-oMsCeLvIaQm6bTrgtp7") },
-                { new KeyValuePair<string, string>("senha", "ABCD1234") }
-                                                                                                    };
+            var autenticacaoDto = new AutenticacaoDto() { Login = "teste", Senha = "senha123" };
 
-            var postResult = fixture._clientApi.PostAsync($"api/v1/autenticacao", new FormUrlEncodedContent(nameValueCollection)).Result;
+            var jsonParaPost = new StringContent(JsonConvert.SerializeObject(autenticacaoDto), UnicodeEncoding.UTF8, "application/json");
+
+            var postResult = fixture._clientApi.PostAsync($"api/v1/autenticacao", jsonParaPost).Result;
             Assert.True(!postResult.IsSuccessStatusCode);
             Assert.True(postResult.StatusCode == System.Net.HttpStatusCode.Unauthorized);
         }
