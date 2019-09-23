48501fe23b69f40f6d4ff7516bb8c15ff9ff3033
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-09-23 15:57:27 -0300

    AB#5535

--- src/SME.SGP.WebClient/src/servicos/Paginas/LoginServices.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/LoginServices.js
@@ -1,28 +1,39 @@
-import Axios from "axios"
-import api from "../api";
+import Axios from 'axios';
+import api from '../api';
 
 class LoginService {
+  autenticar = async Login => {
+    return await api
+      .post(this.obtenhaUrlAutenticacao(), {
+        login: Login.usuario,
+        senha: Login.senha,
+      })
+      .then(res => {
+        return {
+          sucesso: true,
+          mensagem: 'Usuario Logado com sucesso',
+          dados: res.data,
+        };
+      })
+      .catch(err => {
+        const status = err.response.status;
 
-    autenticar = async (Login) => {
+        if (status === 401)
+          return { sucesso: false, erroGeral: 'Usuário e/ou senha invalida' };
 
-        return await api.post(this.obtenhaUrlAutenticacao(), { login: Login.usuario, senha: Login.senha })
-            .then(res => { return { sucesso: true, mensagem: "Usuario Logado com sucesso", dados: res.data } })
-            .catch(err => {
-                
-                const status = err.response.status;
+        return {
+          sucesso: false,
+          errosModal:
+            err.data && err.data.mensagens
+              ? err.data.mensagens
+              : 'Falha ao tentar autenticar o servidor',
+        };
+      });
+  };
 
-                if(status === 401)
-                    return { sucesso: false, erroGeral: "Usuário e/ou senha invalida" }; 
-                
-                return { sucesso: false, errosModal: err.data.mensagens }; 
-            
-            });
-    };
-
-    obtenhaUrlAutenticacao = () =>{
-        return "v1/autenticacao";
-    };
-
+  obtenhaUrlAutenticacao = () => {
+    return 'v1/autenticacao';
+  };
 }
 
 export default new LoginService();
