cd5f9d4ad6ca2ca1594fb43258e909b7421e4d84
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-09-26 14:16:14 -0300

    AB#5556

---
+++ teste/SME.SGP.Dominio.Teste/UsuarioTeste.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SME.SGP.Dominio.Teste
+{
+    public class UsuarioTeste
+    {
+        [Fact]
+        public void DeveValidarSenha()
+        {
+            var Usuario = new Usuario();
+
+            Usuario.CodigoRf = "7777710";
+
+            Usuario.Senha = "1aA";
+
+            Usuario.validarSenha();
+
+            Usuario.Senha = "Aa@";
+
+            Usuario.validarSenha();
+            
+            Usuario.Senha = " 1a@";
+
+            Assert.Throws<NegocioException>(Usuario.validarSenha);
+
+            Usuario.Senha = @"1a@82193490!@#$%&*()";
+
+            Assert.Throws<NegocioException>(Usuario.validarSenha);
+
+            Usuario.Senha = "7710";
+
+            Assert.Throws<NegocioException>(Usuario.validarSenha);
+
+            Usuario.Senha = "Sgp7710";
+
+            Assert.Throws<NegocioException>(Usuario.validarSenha);
+
+            Assert.Throws<NegocioException>(() => { Usuario.validarSenha(new List<string> { "Sgp7710" }); });
+
+            Usuario.Senha = "Sg7710";
+
+            Usuario.validarSenha(new List<string> { "7710" });
+        }
+    }
+}
