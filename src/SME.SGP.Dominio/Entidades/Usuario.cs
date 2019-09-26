e7e9e39fad1ff866edc6413b5f7bfba067801e56
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-09-26 14:16:14 -0300

    AB#5556

--- src/SME.SGP.Dominio/Entidades/Usuario.cs
+++ src/SME.SGP.Dominio/Entidades/Usuario.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SME.SGP.Dominio
 {
     public class Usuario : EntidadeBase
     {
         public string CodigoRf { get; set; }
+        public string CPF { get; set; }
         public IEnumerable<Notificacao> Notificacoes { get { return notificacoes; } }
         private IList<Notificacao> notificacoes { get; set; }
+        public string Senha { get; set; }
+
+        public void validarSenha()
+        {
+            validarSenha(null);
+        }
+
+        public void validarSenha(List<string> ultimasSenhas)
+        {
+            var regexSenha = new Regex(@"(?=.*?[A-Z])(?=.*?[a-z])(?=((?=.*[!@#$\-%&/\\\[\]|*()_=+])|(?=.*?[0-9]+)))");
+
+            var regexEspacoBranco = new Regex(@"([\s])");
+
+            var RFCPF = string.IsNullOrWhiteSpace(CodigoRf) ? CPF : CodigoRf;
+
+            var SenhasPadrao = new List<string> { $"Sgp{RFCPF.Substring(RFCPF.Length - 4)}", RFCPF.Substring(RFCPF.Length - 4) };
+
+            if (SenhasPadrao.FirstOrDefault(x => x.Equals(Senha)) != null)
+                throw new NegocioException("Não pode ser informado a senha padrão");
+
+            if (regexEspacoBranco.IsMatch(Senha))
+                throw new NegocioException("A senhão não pode conter espaço em branco");
+
+            if (!regexSenha.IsMatch(Senha))
+                throw new NegocioException("A senha deve conter pelo menos 1 letra Maiuscula, 1 minuscula, 1 numero e/ou caractere especial");
+
+            if (ultimasSenhas != null && ultimasSenhas.FirstOrDefault(x => x.Equals(Senha)) != null)
+                throw new NegocioException("A senha informada não pode ser uma das ultimas 5 senhas");
+        }
 
         public void Adicionar(Notificacao notificacao)
         {
             if (notificacao != null)
                 notificacoes.Add(notificacao);
         }
+
+
 
     }
 }
--- src/SME.SGP.Dto/PrimeiroAcessoDto.cs
+++ src/SME.SGP.Dto/PrimeiroAcessoDto.cs
@@ -1,5 +1,4 @@
-﻿using Foolproof;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -12,11 +11,12 @@ namespace SME.SGP.Dto
         [MinLength(5, ErrorMessage = "O usuário ou código Rf deve conter no mínimo 5 caracteres.")]
         public string Usuario { get; set; }
 
-        public string RF { get; set; }
+        [Required(ErrorMessage = "É necessario informar o RF do usuário.")]
+        [MinLength(7, ErrorMessage = "O RFCPF tem no minimo 7 Digitos")]
+        [MaxLength(11, ErrorMessage = "O RFCPF tem no Maximo 11 Digitos")]
+        public string RFCPF { get; set; }
 
-        [RequiredIf]
-        public string CPF { get; set; }
-
+        public bool UsuarioExterno { get; set; }
 
         [Required(ErrorMessage = "É necessário informar a nova senha.")]
         [MinLength(8, ErrorMessage = "A senha deve ter no minimo 8 caracteres.")]
