780bdda172f02b98ba6e230d900043351e5139b1
Flávio Ricardo <flavioricardo@live.com>
2019-10-04 10:37:09 -0300

    Revertendo código perdido no merge

--- src/SME.SGP.Aplicacao/Comandos/ComandosUsuario.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosUsuario.cs
@@ -1,9 +1,4 @@
-﻿using SME.SGP.Aplicacao.Integracoes;
-using SME.SGP.Aplicacao.Servicos;
-using SME.SGP.Dominio;
-using SME.SGP.Dominio.Interfaces;
-using SME.SGP.Dto;
-using System;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,22 +6,64 @@ namespace SME.SGP.Aplicacao
 {
     public class ComandosUsuario : IComandosUsuario
     {
+        private readonly IConfiguration configuration;
         private readonly IRepositorioUsuario repositorioUsuario;
         private readonly IServicoAutenticacao servicoAutenticacao;
+        private readonly IServicoEmail servicoEmail;
         private readonly IServicoEOL servicoEOL;
         private readonly IServicoPerfil servicoPerfil;
         private readonly IServicoTokenJwt servicoTokenJwt;
         private readonly IServicoUsuario servicoUsuario;
 
-        public ComandosUsuario(IRepositorioUsuario repositorioUsuario, IServicoAutenticacao servicoAutenticacao, IServicoUsuario servicoUsuario,
-            IServicoPerfil servicoPerfil, IServicoEOL servicoEOL, IServicoTokenJwt servicoTokenJwt)
+        public ComandosUsuario(IRepositorioUsuario repositorioUsuario,
+            IServicoAutenticacao servicoAutenticacao,
+            IServicoUsuario servicoUsuario,
+            IServicoPerfil servicoPerfil,
+            IServicoEOL servicoEOL,
+            IServicoTokenJwt servicoTokenJwt,
+            IServicoEmail servicoEmail,
+            IConfiguration configuration)
         {
-            this.repositorioUsuario = repositorioUsuario ?? throw new System.ArgumentNullException(nameof(repositorioUsuario));
-            this.servicoAutenticacao = servicoAutenticacao ?? throw new System.ArgumentNullException(nameof(servicoAutenticacao));
-            this.servicoUsuario = servicoUsuario ?? throw new System.ArgumentNullException(nameof(servicoUsuario));
-            this.servicoPerfil = servicoPerfil ?? throw new System.ArgumentNullException(nameof(servicoPerfil));
-            this.servicoEOL = servicoEOL ?? throw new System.ArgumentNullException(nameof(servicoEOL));
-            this.servicoTokenJwt = servicoTokenJwt ?? throw new System.ArgumentNullException(nameof(servicoTokenJwt));
+            this.repositorioUsuario = repositorioUsuario ??
+                throw new System.ArgumentNullException(nameof(repositorioUsuario));
+            this.servicoAutenticacao = servicoAutenticacao ??
+                throw new System.ArgumentNullException(nameof(servicoAutenticacao));
+            this.servicoUsuario = servicoUsuario ??
+                throw new System.ArgumentNullException(nameof(servicoUsuario));
+            this.servicoPerfil = servicoPerfil ??
+                throw new System.ArgumentNullException(nameof(servicoPerfil));
+            this.servicoEOL = servicoEOL ??
+                throw new System.ArgumentNullException(nameof(servicoEOL));
+            this.servicoTokenJwt = servicoTokenJwt ??
+                throw new System.ArgumentNullException(nameof(servicoTokenJwt));
+            this.servicoEmail = servicoEmail ?? throw new ArgumentNullException(nameof(servicoEmail));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task AlterarSenhaComTokenRecuperacao(RecuperacaoSenhaDto recuperacaoSenhaDto)
+        {
+            Usuario usuario = repositorioUsuario.ObterPorTokenRecuperacaoSenha(recuperacaoSenhaDto.Token);
+            if (usuario == null)
+            {
+                throw new NegocioException("Usuário não encontrado.");
+            }
+
+            if (!usuario.TokenRecuperacaoSenhaEstaValido())
+            {
+                throw new NegocioException("Este link expirou. Clique em continuar para solicitar um novo link de recuperação de senha.", 403);
+            }
+
+            usuario.ValidarSenha(recuperacaoSenhaDto.NovaSenha);
+
+            var retornoApi = await servicoEOL.AlterarSenha(usuario.Login, recuperacaoSenhaDto.NovaSenha);
+
+            if (!retornoApi.SenhaAlterada)
+            {
+                throw new NegocioException(retornoApi.Mensagem, retornoApi.StatusRetorno);
+            }
+
+            usuario.FinalizarRecuperacaoSenha();
+            repositorioUsuario.Salvar(usuario);
         }
 
         public async Task<UsuarioAutenticacaoRetornoDto> Autenticar(string login, string senha)
@@ -58,6 +95,38 @@ namespace SME.SGP.Aplicacao
                 }
             }
             return retornoAutenticacaoEol.Item1;
+        }
+
+        public string SolicitarRecuperacaoSenha(string login)
+        {
+            var usuario = repositorioUsuario.ObterPorCodigoRfLogin(null, login);
+            if (usuario == null)
+            {
+                throw new NegocioException("Usuário não encontrado.");
+            }
+            usuario.IniciarRecuperacaoDeSenha();
+            repositorioUsuario.Salvar(usuario);
+            EnviarEmailRecuperacao(usuario);
+            return usuario.Email;
+        }
+
+        private void EnviarEmailRecuperacao(Usuario usuario)
+        {
+            string caminho = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"ModelosEmail\RecuperacaoSenha.txt");
+            var textoArquivo = File.ReadAllText(caminho);
+            var urlFrontEnd = configuration["UrlFrontEnt"];
+            var textoEmail = textoArquivo
+                .Replace("#NOME", usuario.Nome)
+                .Replace("#RF", usuario.CodigoRf)
+                .Replace("#URL_BASE#", urlFrontEnd)
+                .Replace("#LINK", $"{urlFrontEnd}redefinir-senha/{usuario.TokenRecuperacaoSenha.ToString()}");
+            servicoEmail.Enviar(usuario.Email, "Recuperação de senha do SGP", textoEmail);
+        }
+
+        public bool TokenRecuperacaoSenhaEstaValido(Guid token)
+        {
+            Usuario usuario = repositorioUsuario.ObterPorTokenRecuperacaoSenha(token);
+            return usuario != null && usuario.TokenRecuperacaoSenhaEstaValido();
         }
 
         public async Task<string> ModificarPerfil(string guid)
