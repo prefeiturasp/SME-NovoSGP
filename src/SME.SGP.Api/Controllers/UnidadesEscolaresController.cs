f703f833066ea2eb82e1c0173d1a2b2ef417e8f3
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-10-05 19:51:59 -0300

    Endpoint de funcionários por Ue.

---
+++ src/SME.SGP.Api/Controllers/UnidadesEscolaresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Aplicacao;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/unidades-escolares")]
+    [Authorize("Bearer")]
+    public class UnidadesEscolaresController : ControllerBase
+    {
+        [Route("{ueId}/funcionarios")]
+        [HttpGet]
+        public async Task<IActionResult> ObterFuncionariosPorUe([FromServices]IConsultasUnidadesEscolares consultasUnidadesEscolares, string ueId)
+        {
+            return Ok(await consultasUnidadesEscolares.ObtemFuncionariosPorUe(ueId));
+        }
+    }
+}
