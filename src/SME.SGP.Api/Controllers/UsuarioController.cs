a2941b88af25c79635cfe48a1553e95dae50896b
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-10-06 20:19:52 -0300

    Iniciando testes de integração da api de alteração de e-mail do usuário AB#5424

--- src/SME.SGP.Api/Controllers/UsuarioController.cs
+++ src/SME.SGP.Api/Controllers/UsuarioController.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Aplicacao;
 using SME.SGP.Dto;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/usuarios")]
+    [Authorize("Bearer")]
     public class UsuarioController : ControllerBase
     {
-        [Route("imagens/perfil")]
-        [HttpPost]
-        public IActionResult AlterarEmail([FromBody])
+        private readonly IComandosUsuario comandosUsuario;
+
+        public UsuarioController(IComandosUsuario comandosUsuario)
         {
-            return Ok("https://telegramic.org/media/avatars/stickers/52cae315e8a464eb80a3.png");
+            this.comandosUsuario = comandosUsuario;
+        }
+
+        [Route("autenticado/email")]
+        [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public async Task<IActionResult> AlterarEmailUsuarioLogado([FromBody]AlterarEmailDto alterarEmailDto)
+        {
+            await comandosUsuario.AlterarEmailUsuarioLogado(alterarEmailDto.NovoEmail);
+            return Ok();
         }
 
         [Route("imagens/perfil")]
