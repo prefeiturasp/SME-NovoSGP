0cd85c1d9991973bb438c26b97b6c0a24a1589ec
Alexsander Camargo <csalexsander@gmail.com>
2019-10-06 17:46:44 -0300

    Ajustes e correções pos perda de codigo  AB#660

--- src/SME.SGP.Dto/PrimeiroAcessoDto.cs
+++ src/SME.SGP.Dto/PrimeiroAcessoDto.cs
@@ -11,13 +11,6 @@ namespace SME.SGP.Dto
         [MinLength(5, ErrorMessage = "O usuário ou código Rf deve conter no mínimo 5 caracteres.")]
         public string Usuario { get; set; }
 
-        [Required(ErrorMessage = "É necessario informar o RF do usuário.")]
-        [MinLength(7, ErrorMessage = "O RFCPF tem no minimo 7 Digitos")]
-        [MaxLength(11, ErrorMessage = "O RFCPF tem no Maximo 11 Digitos")]
-        public string RFCPF { get; set; }
-
-        public bool UsuarioExterno { get; set; }
-
         [Required(ErrorMessage = "É necessário informar a nova senha.")]
         [MinLength(8, ErrorMessage = "A senha deve ter no minimo 8 caracteres.")]
         [MaxLength(12, ErrorMessage = "A senha deve ter no máximo 12 caracteres.")]
