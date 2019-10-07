9db32e50feac46bd6f8546edb54a40ae6419a6f6
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-10-07 19:35:21 -0300

    AB#6307
    Iniciando desenvolvimento do periodo escolar

---
+++ src/SME.SGP.Dominio/Entidades/PeriodoEscolar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio.Entidades
+{
+    public class PeriodoEscolar : EntidadeBase
+    {
+        public int TipoCalendario { get; set; }
+        public int Bimestre { get; set; }
+        public DateTime PeriodoInicio { get; set; }
+        public DateTime PeriodoFim { get; set; }
+
+        public void Validar()
+        {
+            if (PeriodoFim < PeriodoInicio)
+                throw new NegocioException($"{Bimestre}º Bimestre: A data de inicio não pode ser posterior a data de fim do periodo");
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/PeriodoEscolarLista.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SME.SGP.Dominio.Entidades
+{
+    public class PeriodoEscolarLista
+    {
+        public PeriodoEscolarLista()
+        {
+            Periodos = new List<PeriodoEscolar>();
+        }
+
+        public List<PeriodoEscolar> Periodos { get; set; }
+        public bool Eja { get; set; }
+
+        private int quantidade => Eja ? 2 : 4;
+
+        public void Validar()
+        {
+            ValidarQuantidadePeriodos();
+
+            ValidarBimestresRepetidos();
+
+            ValidarInicioAposFim();
+
+            ValidarInicioPeriodoAntesFimPeriodoAnterior();
+        }
+
+        private void ValidarBimestresRepetidos()
+        {
+            var bimestres = Periodos.Select(x => x.Bimestre).GroupBy(x => x).Where(x => x.Count() > 1);            
+        }
+
+        private void ValidarInicioPeriodoAntesFimPeriodoAnterior()
+        {
+            for (int i = 1; i < Periodos.Count - 1; i++)
+            {
+                if (Periodos[i + 1].PeriodoInicio < Periodos[i].PeriodoFim)
+                    throw new NegocioException($"O inicio do {i + 1}º Bimestre não pode ser anterior ao fim do {i}º Bimestre");
+            }
+        }
+
+        private void ValidarInicioAposFim()
+        {
+            foreach (var periodo in Periodos)            
+                periodo.Validar();            
+        }
+
+        private void ValidarQuantidadePeriodos()
+        {
+            bool valido = Periodos.Count == quantidade;
+
+            if(!valido)            
+                throw new NegocioException($"Para periodo {(Eja ? "semestral" : "anual")} devem ser informados {quantidade} bimestres");            
+        }        
+
+    }
+}
