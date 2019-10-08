ba6cfd1dba6b50cc4321d23ba52515308217b509
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-10-08 15:54:39 -0300

    Criando api de teste do email

--- src/SME.SGP.Aplicacao/Interfaces/IComandosUsuario.cs
+++ src/SME.SGP.Aplicacao/Interfaces/IComandosUsuario.cs
@@ -7,10 +7,15 @@ namespace SME.SGP.Aplicacao
     public interface IComandosUsuario
     {
         Task AlterarSenhaComTokenRecuperacao(RecuperacaoSenhaDto recuperacaoSenhaDto);
+
         Task<UsuarioAutenticacaoRetornoDto> Autenticar(string login, string senha);
+
+        Task<string> ModificarPerfil(string guid);
+
         string SolicitarRecuperacaoSenha(string login);
 
+        void TesteEmail();
+
         bool TokenRecuperacaoSenhaEstaValido(Guid token);
-        Task<string> ModificarPerfil(string guid);
     }
 }
---
+++ teste/SME.SGP.Integracao.Teste/EmailTeste.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Xunit.Extensions.Ordering;
+
+namespace SME.SGP.Integracao.Teste
+{
+    [Collection("Testserver collection")]
+    public class EmailTeste
+    {
+        private readonly TestServerFixture _fixture;
+
+        public EmailTeste(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact, Order(1)]
+        public void Email()
+        {
+            _fixture._clientApi.DefaultRequestHeaders.Clear();
+
+            var postResult = _fixture._clientApi.PostAsync($"api/v1/autenticacao/testeemail", null).Result;
+
+            Assert.True(postResult.IsSuccessStatusCode);
+        }
+    }
+}
