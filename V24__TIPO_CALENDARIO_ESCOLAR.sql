cd782578d42d3dfa8a208cf3470747bfbb720f88
Eliton Santos <eliton.santos@amcom.com.br>
2019-10-09 13:21:32 -0300

    Criação de scripts AB#6288 AB#4164

---
+++ V24__TIPO_CALENDARIO_ESCOLAR.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE IF NOT EXISTS public.tipo_calendario_escolar (
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	criado_em timestamp NOT NULL,
+	criado_por varchar(200) NOT NULL,
+	alterado_em timestamp NULL,
+	alterado_por varchar(200) NULL,
+	criado_rf varchar(200) NOT NULL,
+	alterado_rf varchar(200) NULL,
+	ano_letivo int4 not  null,
+	nome varchar(50) not null,
+	periodo int not null,
+	modalidade int not null,
+	situacao int not null default 1
+);
