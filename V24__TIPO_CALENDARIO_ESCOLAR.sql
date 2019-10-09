6066dc4509de28b1c1c3f81a30b620b88c7db203
Eliton Santos <eliton.santos@amcom.com.br>
2019-10-09 13:30:00 -0300

    Ajuste script AB#4164 AB#6288

--- V24__TIPO_CALENDARIO_ESCOLAR.sql
+++ V24__TIPO_CALENDARIO_ESCOLAR.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE IF NOT EXISTS public.tipo_calendario_escolar (
-	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,	
+	ano_letivo int4 not  null,
+	nome varchar(50) not null,
+	periodo int not null,
+	modalidade int not null,
+	situacao int not null default 1,
 	criado_em timestamp NOT NULL,
 	criado_por varchar(200) NOT NULL,
 	alterado_em timestamp NULL,
 	alterado_por varchar(200) NULL,
 	criado_rf varchar(200) NOT NULL,
-	alterado_rf varchar(200) NULL,
-	ano_letivo int4 not  null,
-	nome varchar(50) not null,
-	periodo int not null,
-	modalidade int not null,
-	situacao int not null default 1
+	alterado_rf varchar(200) NULL
 );
