40a4d1d9b656a50f8b0b16513bdaabf4aefdb052
eliton-amcom <eliton.santos@amcom.com.br>
2019-10-09 18:04:45 -0300

    Listar clendarios AB#6289 AB#4164

--- src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendarioEscolar.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendarioEscolar.cs
@@ -1,6 +1,10 @@
 ﻿using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Dados.Contexto;
+using SME.SGP.Dto;
+using System.Collections.Generic;
+using Dapper;
+using System.Text;
 
 namespace SME.SGP.Dados.Repositorios
 {
@@ -8,6 +12,20 @@ namespace SME.SGP.Dados.Repositorios
     {
         public RepositorioTipoCalendarioEscolar(ISgpContext conexao) : base(conexao)
         {
+        }
+
+        public IEnumerable<TipoCalendarioEscolarDto> ObterTiposCalendarioEscolar()
+        {
+            StringBuilder query = new StringBuilder();
+
+            query.AppendLine("select ");
+            query.AppendLine("id, ");
+            query.AppendLine("nome, ");
+            query.AppendLine("ano-letivo, ");
+            query.AppendLine("periodo ");
+            query.AppendLine("from tipo_calendario_escolar");
+
+            return database.Conexao.Query<TipoCalendarioEscolarDto>(query.ToString());
         }
     }
 }
---
+++ src/SME.SGP.Dto/TipoCalendarioEscolarDto.cs
@@ -0,0 +1,17 @@
+﻿using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SME.SGP.Dto
+{
+    public class TipoCalendarioEscolarDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int anoLetivo { get; set; }
+        public Periodo Periodo { get; set; }
+
+    }
+}
