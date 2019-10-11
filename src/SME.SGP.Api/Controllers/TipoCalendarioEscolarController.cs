4ad477cb084c0c22892127f7f33450dd9d8f1d90
Eliton Santos <eliton.santos@amcom.com.br>
2019-10-11 13:34:52 -0300

    correção override AB#6290 AB#6756 AB#4164

--- src/SME.SGP.Api/Controllers/TipoCalendarioEscolarController.cs
+++ src/SME.SGP.Api/Controllers/TipoCalendarioEscolarController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Dominio;
@@ -10,6 +11,7 @@ namespace SME.SGP.Api.Controllers
     [ApiController]
     [Route("api/v1/tipo-calendario-escolar")]
     [ValidaDto]
+    [Authorize("Bearer")]
     public class TipoCalendarioEscolarController : ControllerBase
     {
         private readonly IConsultasTipoCalendarioEscolar consultas;
@@ -24,7 +26,6 @@ namespace SME.SGP.Api.Controllers
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<TipoCalendarioEscolarDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        [Permissao(Permissao.C_C, Policy = "Bearer")]
         public IActionResult BuscarTodos()
         {
             return Ok(consultas.Listar());
@@ -34,7 +35,6 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(IEnumerable<TipoCalendarioEscolarDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [Route("{id}")]
-        [Permissao(Permissao.C_C, Policy = "Bearer")]
         public IActionResult BuscarUm(long id)
         {
             return Ok(consultas.BuscarPorId(id));
@@ -43,7 +43,6 @@ namespace SME.SGP.Api.Controllers
         [HttpPost]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        [Permissao(Permissao.C_I, Permissao.C_A, Policy = "Bearer")]
         public IActionResult Salvar([FromBody]TipoCalendarioEscolarDto dto)
         {
             comandos.Salvar(dto);
@@ -53,7 +52,6 @@ namespace SME.SGP.Api.Controllers
         [HttpDelete]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        [Permissao(Permissao.C_E, Permissao.C_A, Policy = "Bearer")]
         public IActionResult MarcarExcluidos([FromBody]long[] ids)
         {
             comandos.MarcarExcluidos(ids);
