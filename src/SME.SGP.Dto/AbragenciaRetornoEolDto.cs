55b3c969ebda6864d4ebc1eb630b692439ae8fff
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-10-13 12:41:41 -0300

    Inclusão de método no serviço EOL para busca de abrangencia do funcionário. Inclusão de DTOs;

---
+++ src/SME.SGP.Dto/AbragenciaRetornoEolDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SGP.Dto
+{
+    public class AbragenciaRetornoEolDto
+    {
+        public AbragenciaRetornoEolDto()
+        {
+            Dres = new List<AbragenciaDreRetornoEolDto>();
+        }
+
+        public IList<AbragenciaDreRetornoEolDto> Dres { get; set; }
+    }
+}
---
+++ src/SME.SGP.Dto/AbragenciaUeRetornoEolDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SGP.Dto
+{
+    public class AbragenciaUeRetornoEolDto
+    {
+        public AbragenciaUeRetornoEolDto()
+        {
+            Turmas = new List<AbragenciaTurmaRetornoEolDto>();
+        }
+
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public IList<AbragenciaTurmaRetornoEolDto> Turmas { get; set; }
+    }
+}
