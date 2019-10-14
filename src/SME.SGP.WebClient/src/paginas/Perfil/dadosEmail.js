a3d3f69a91fd1f167a26f2a27cacd0d994de3cb4
Eliton Santos <eliton.santos@amcom.com.br>
2019-10-14 13:47:59 -0300

    Alterações AB#5721 AB#5424

--- src/SME.SGP.WebClient/src/paginas/Perfil/dadosEmail.js
+++ src/SME.SGP.WebClient/src/paginas/Perfil/dadosEmail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CampoTexto from '~/componentes/campoTexto';
 import Button from '~/componentes/button';
 import { Colors } from '~/componentes/colors';
@@ -6,16 +6,24 @@ import styled from 'styled-components';
 
 const DadosEmail = () => {
 
+  const [email, setEmail] = useState('teste@teste.com');
+  const [senha, setSenha] = useState('123456');
+
   const Campos = styled.div`
     margin-right: 10px;
+    .campo{
+      margin-top: 50px;
+    }
   `;
 
   return (
     <Campos>
-      <div className="row">
+      <div className="row campo">
         <div className="col-md-10">
           <CampoTexto
+            desabilitado={true}
             label="E-mail"
+            value={email}
             placeholder="Insira um e-mail"
             onChange={() => { }}
             type="email"
@@ -31,13 +39,16 @@ const DadosEmail = () => {
           />
         </div>
       </div>
-      <div className="row">
+      <div className="row campo">
         <div className="col-md-10">
           <CampoTexto
+            desabilitado={true}
             label="Senha"
+            value={senha}
             className="col-11 campo"
             placeholder="Insira uma senha"
             onChange={() => { }}
+            type="password"
           />
         </div>
         <div className="col-md-2">
