9b721d0b30d4f023fb11107e7dc3e04a7daba4f1
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-10-14 17:38:01 -0300

    Inclusão de endpoint de modalidades e abrangencias

--- src/SME.SGP.Api/Controllers/AbrangenciaController.cs
+++ src/SME.SGP.Api/Controllers/AbrangenciaController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Dominio;
 using SME.SGP.Dto;
+using SME.SGP.Infra;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
@@ -28,6 +32,15 @@ namespace SME.SGP.Api.Controllers
 
         public IList<AbrangenciaDreRetorno> ListaDres { get; set; }
 
+        [HttpGet("anos-letivos")]
+        [ProducesResponseType(typeof(int[]), 200)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterAnosLetivos()
+        {
+            return Ok(new int[] { 2019 });
+        }
+
         [HttpGet("dres")]
         [ProducesResponseType(typeof(IEnumerable<AbrangenciaDreRetorno>), 200)]
         [ProducesResponseType(401)]
@@ -37,17 +50,60 @@ namespace SME.SGP.Api.Controllers
             return Ok(ListaDres);
         }
 
-        [HttpGet("dres/ues/{codigoUe}/turmas")]
-        [ProducesResponseType(200)]
+        [HttpGet("modalidades")]
+        [ProducesResponseType(typeof(IEnumerable<EnumeradoRetornoDto>), 200)]
         [ProducesResponseType(401)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        public async Task<IActionResult> ObterTurmas(string codigoTurma)
+        public async Task<IActionResult> ObterModalidades()
         {
-            return Ok();
+            var retorno = Modalidade.GetValues(typeof(Modalidade)).Cast<Modalidade>().Select(v => new EnumeradoRetornoDto
+            {
+                Descricao = v.GetAttribute<DisplayAttribute>().Name,
+                Id = (int)v
+            }).ToList();
+
+            return Ok(retorno);
+        }
+
+        [HttpGet("dres/ues/{codigoUe}/turmas")]
+        [ProducesResponseType(typeof(IEnumerable<AbrangenciaTurmaRetorno>), 200)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterTurmas(string codigoUe)
+        {
+            IList<AbrangenciaTurmaRetorno> listaTurmas = new List<AbrangenciaTurmaRetorno>();
+
+            switch (codigoUe)
+            {
+                case "000191":
+                    listaTurmas.Add(new AbrangenciaTurmaRetorno() { Ano = 1, AnoLetivo = 2019, Codigo = "191", ModalidadeCodigo = 3, Nome = "191-A", Semestre = 1 });
+                    break;
+
+                case "000477":
+                    listaTurmas.Add(new AbrangenciaTurmaRetorno() { Ano = 1, AnoLetivo = 2019, Codigo = "477", ModalidadeCodigo = 3, Nome = "477-A", Semestre = 1 });
+                    break;
+
+                case "000281":
+                    listaTurmas.Add(new AbrangenciaTurmaRetorno() { Ano = 1, AnoLetivo = 2019, Codigo = "281", ModalidadeCodigo = 3, Nome = "281-A", Semestre = 1 });
+                    break;
+
+                case "000311":
+                    listaTurmas.Add(new AbrangenciaTurmaRetorno() { Ano = 1, AnoLetivo = 2019, Codigo = "311", ModalidadeCodigo = 3, Nome = "311-A", Semestre = 1 });
+                    break;
+
+                case "000566":
+                    listaTurmas.Add(new AbrangenciaTurmaRetorno() { Ano = 1, AnoLetivo = 2019, Codigo = "566", ModalidadeCodigo = 3, Nome = "566-A", Semestre = 1 });
+                    break;
+
+                default:
+                    break;
+            }
+
+            return Ok(listaTurmas);
         }
 
         [HttpGet("dres/{codigoDre}/ues")]
-        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(IEnumerable<AbrangenciaUeRetorno>), 200)]
         [ProducesResponseType(401)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         public async Task<IActionResult> ObterUes(string codigoDre)
