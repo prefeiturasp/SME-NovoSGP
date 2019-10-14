711b21adebf417045630f5b204db704bbf6bb50a
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-10-14 16:46:04 -0300

    Inicio da controller de abrangencia

---
+++ src/SME.SGP.Dto/AbrangenciaTurmaRetorno.cs
@@ -0,0 +1,11 @@
+﻿namespace SME.SGP.Dto
+{
+    public class AbrangenciaTurmaRetorno
+    {
+        public int Ano { get; set; }
+        public int AnoLetivo { get; set; }
+        public string Codigo { get; set; }
+        public string ModalidadeCodigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
