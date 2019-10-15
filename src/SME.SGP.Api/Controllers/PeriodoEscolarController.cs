3addc462de5226435a4fc6222438d70bb1971983
Alexsander Camargo <csalexsander@gmail.com>
2019-10-15 18:14:06 -0300

    AB#6311

---
+++ src/SME.SGP.Api/Controllers/PeriodoEscolarController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao.Interfaces.Comandos;
+using SME.SGP.Dominio;
+using SME.SGP.Dto;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/periodo-escolar")]
+    [ValidaDto]
+    public class PeriodoEscolarController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Permissao(Permissao.PA_I, Permissao.PA_A, Policy = "Bearer")]
+        public IActionResult Post([FromBody]PeriodoEscolarListaDto periodos, [FromServices]IComandosPeriodoEscolar comandoPeriodo)
+        {
+            comandoPeriodo.Salvar(periodos);
+            return Ok();
+        }
+    }
+}
