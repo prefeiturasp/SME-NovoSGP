8edded05120766c6d2eaea64db4247b5825458fe
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-10-15 17:01:11 -0300

    Criação do endpoint de consulta e testes integrados AB#6309 AB#6312

--- src/SME.SGP.Dados/Repositorios/RepositorioPeriodoEscolar.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioPeriodoEscolar.cs
@@ -1,4 +1,8 @@
-﻿using SME.SGP.Dados.Contexto;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper;
+using SME.SGP.Dados.Contexto;
 using SME.SGP.Dominio.Entidades;
 using SME.SGP.Dominio.Interfaces.Repositorios;
 
@@ -8,6 +12,17 @@ namespace SME.SGP.Dados.Repositorios
     {
         public RepositorioPeriodoEscolar(ISgpContext conexao) : base(conexao)
         {
-        }        
+        }
+
+        public IList<PeriodoEscolar> ObterPorTipoCalendario(long codigoTipoCalendario)
+        {
+            StringBuilder query = new StringBuilder();
+
+            query.AppendLine("select * from periodo_escolar");
+
+            query.AppendLine("where tipo_calendario_id = @tipoCalendario");
+
+            return database.Conexao.Query<PeriodoEscolar>(query.ToString(), new {tipoCalendario = codigoTipoCalendario }).ToList();
+        }
     }
 }
---
+++ teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasPeriodoEscolarTeste.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using SME.SGP.Aplicacao.Consultas;
+using SME.SGP.Aplicacao.Interfaces.Consultas;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SME.SGP.Aplicacao.Teste.Consultas
+{
+    public class ConsultasPeriodoEscolarTeste
+    {
+        private readonly IConsultasPeriodoEscolar consultas;
+        private readonly Mock<IRepositorioPeriodoEscolar> repositorio;
+
+        public ConsultasPeriodoEscolarTeste()
+        {
+            repositorio = new Mock<IRepositorioPeriodoEscolar>();
+
+            consultas = new ConsultasPeriodoEscolar(repositorio.Object);
+        }
+
+        [Fact(DisplayName = "Deve_Disparar_Excecao_Se_Instanciar_Sem_Dependencia")]
+        public void Deve_Disparar_Excecao_Se_Instanciar_Sem_Dependencia()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ConsultasPeriodoEscolar(null));
+        }
+
+        [Fact(DisplayName ="Deve_Consultar_Periodo_Escolar")]
+        public void Deve_Consultar_Periodo_Escolar()
+        {
+            consultas.ObterPorTipoCalendario(1);
+            repositorio.Verify(r => r.ObterPorTipoCalendario(It.IsAny<long>()));
+        }
+    }
+}
