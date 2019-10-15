60b833201f84bd3d6849cbdd697268df199dc424
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-10-14 21:52:31 -0300

    Endpoint de filtro

---
+++ src/SME.SGP.Dto/AbrangenciaFiltroRetorno.cs
@@ -0,0 +1,18 @@
+﻿namespace SME.SGP.Dto
+{
+    public class AbrangenciaFiltroRetorno
+    {
+        public string AbreviacaoModalidade { get; set; }
+        public int AnoLetivo { get; set; }
+        public string CodigoDre { get; set; }
+        public int CodigoModalidade { get; set; }
+        public string CodigoTurma { get; set; }
+        public string CodigoUe { get; set; }
+        public string DescricaoFiltro { get { return $"{AbreviacaoModalidade} - {NomeTurma} - {NomeUe} "; } }
+        public string NomeDre { get; set; }
+        public string NomeModalidade { get; set; }
+        public string NomeTurma { get; set; }
+        public string NomeUe { get; set; }
+        public int Semestre { get; set; }
+    }
+}
--- src/SME.SGP.Dto/AbrangenciaTurmaRetorno.cs
+++ src/SME.SGP.Dto/AbrangenciaTurmaRetorno.cs
@@ -5,7 +5,7 @@
         public int Ano { get; set; }
         public int AnoLetivo { get; set; }
         public string Codigo { get; set; }
-        public int ModalidadeCodigo { get; set; }
+        public int CodigoModalidade { get; set; }
         public string Nome { get; set; }
         public int Semestre { get; set; }
     }
