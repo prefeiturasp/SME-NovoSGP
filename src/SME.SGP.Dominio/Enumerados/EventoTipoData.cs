3df093f73faf4ed94a33d040a2fef9b0f4cf1bd7
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-10-16 19:13:08 -0300

    Inicando desenvolvimento do backend
    AB#6329

---
+++ src/SME.SGP.Dominio/Enumerados/EventoTipoData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SME.SGP.Dominio.Enumerados
+{
+    public enum EventoTipoData
+    {
+        [Display(Name = "Evento unico")]
+        Unico = 1,
+        [Display(Name = "Evento com período definido")]
+        InicioFim
+    }
+}
