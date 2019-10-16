c91dafd9011063e3d264c012404a669da0781cb0
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-10-16 17:30:12 -0300

    Criando script do banco
    AB#6326

---
+++ scripts/V28__EVENTO_TIPO.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.evento_tipo (
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	descricao varchar(200) NOT NULL,
+	local_ocorrencia int4 NOT NULL,
+	concomitancia bool NOT NULL DEFAULT true,
+	tipo_data int4 NOT NULL,
+	dependencia bool NOT NULL DEFAULT false,
+	letivo int4 NOT NULL,
+	situacao bool NOT NULL DEFAULT true,
+	criado_em timestamp NOT NULL,
+	alterado_em timestamp NOT NULL,
+	alterado_por varchar(200) NOT NULL,
+	criado_por varchar(200) NOT NULL,
+	criado_rf varchar(200) NOT NULL,
+	alterado_rf varchar(200) NOT NULL
+);
