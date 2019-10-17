0c7c03f7542b690a6dc06f71e15144f8ec146f20
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-10-16 21:16:50 -0300

    Correções nos testes de plano anual e tipo calendario AB#4164

--- teste/SME.SGP.Integracao.Teste/TipoCalendarioEscolarTeste.cs
+++ teste/SME.SGP.Integracao.Teste/TipoCalendarioEscolarTeste.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Extensions.Ordering;
 
@@ -50,7 +51,6 @@ namespace SME.SGP.Integracao.Teste
 
             Assert.True(postResult.IsSuccessStatusCode);
 
-
             if (postResult.IsSuccessStatusCode)
             {
                 var getAllResult = _fixture._clientApi.GetAsync($"api/v1/tipo-calendario").Result;
@@ -67,11 +67,10 @@ namespace SME.SGP.Integracao.Teste
                 Assert.Equal(dtoUm.Periodo, tipoCalendarioDto.Periodo);
                 Assert.Equal(dtoUm.Situacao, tipoCalendarioDto.Situacao);
             }
-
         }
 
         [Fact, Order(2)]
-        public void Deve_Incluir_Excluir_Consular_Tipo_Calendario()
+        public async Task Deve_Incluir_Excluir_Consular_Tipo_Calendario()
         {
             _fixture._clientApi.DefaultRequestHeaders.Clear();
 
@@ -93,43 +92,43 @@ namespace SME.SGP.Integracao.Teste
             tipoCalendarioDto2.Situacao = true;
 
             var jsonParaPost = new StringContent(TransformarEmJson(tipoCalendarioDto), UnicodeEncoding.UTF8, "application/json");
-            var postResult = _fixture._clientApi.PostAsync("api/v1/tipo-calendario/", jsonParaPost).Result;
-
-
-            var jsonParaPost2 = new StringContent(TransformarEmJson(tipoCalendarioDto2), UnicodeEncoding.UTF8, "application/json");
-            var postResult2 = _fixture._clientApi.PostAsync("api/v1/tipo-calendario/", jsonParaPost2).Result;
+            var postResult = await _fixture._clientApi.PostAsync("api/v1/tipo-calendario/", jsonParaPost);
 
             Assert.True(postResult.IsSuccessStatusCode);
-            Assert.True(postResult2.IsSuccessStatusCode);
 
-
-            if (postResult.IsSuccessStatusCode && postResult2.IsSuccessStatusCode)
+            if (postResult.IsSuccessStatusCode)
             {
-                var ids = new int[1];
-                ids[0] = 1;
-                var jsonDelete = new StringContent(JsonConvert.SerializeObject(ids), UnicodeEncoding.UTF8, "application/json");
-                HttpRequestMessage request = new HttpRequestMessage
+                var jsonParaPost2 = new StringContent(TransformarEmJson(tipoCalendarioDto2), UnicodeEncoding.UTF8, "application/json");
+                var postResult2 = await _fixture._clientApi.PostAsync("api/v1/tipo-calendario/", jsonParaPost2);
+                Assert.True(postResult2.IsSuccessStatusCode);
+
+                if (postResult2.IsSuccessStatusCode)
                 {
-                    Content = jsonDelete,
-                    Method = HttpMethod.Delete,
-                    RequestUri = new Uri($"{ _fixture._clientApi.BaseAddress}api/v1/tipo-calendario/")
-                };
+                    var ids = new int[1];
+                    ids[0] = 1;
+                    var jsonDelete = new StringContent(JsonConvert.SerializeObject(ids), UnicodeEncoding.UTF8, "application/json");
+                    HttpRequestMessage request = new HttpRequestMessage
+                    {
+                        Content = jsonDelete,
+                        Method = HttpMethod.Delete,
+                        RequestUri = new Uri($"{ _fixture._clientApi.BaseAddress}api/v1/tipo-calendario/")
+                    };
 
-                var deleteResult = _fixture._clientApi.SendAsync(request).Result;
+                    var deleteResult = _fixture._clientApi.SendAsync(request).Result;
 
-                Assert.True(deleteResult.IsSuccessStatusCode);
+                    Assert.True(deleteResult.IsSuccessStatusCode);
 
-                var getAllResult = _fixture._clientApi.GetAsync($"api/v1/tipo-calendario").Result;
-                var dtoTodos = JsonConvert.DeserializeObject<IEnumerable<TipoCalendarioDto>>(getAllResult.Content.ReadAsStringAsync().Result);
+                    var getAllResult = _fixture._clientApi.GetAsync($"api/v1/tipo-calendario").Result;
+                    var dtoTodos = JsonConvert.DeserializeObject<IEnumerable<TipoCalendarioDto>>(getAllResult.Content.ReadAsStringAsync().Result);
 
-                Assert.True(dtoTodos.Count() == 1);
+                    Assert.True(dtoTodos.Count() == 1);
 
-                var getOneResult = _fixture._clientApi.GetAsync($"api/v1/tipo-calendario/1").Result;
-                var dtoUm = JsonConvert.DeserializeObject<TipoCalendarioCompletoDto>(getOneResult.Content.ReadAsStringAsync().Result);
+                    var getOneResult = _fixture._clientApi.GetAsync($"api/v1/tipo-calendario/1").Result;
+                    var dtoUm = JsonConvert.DeserializeObject<TipoCalendarioCompletoDto>(getOneResult.Content.ReadAsStringAsync().Result);
 
-                Assert.Null(dtoUm.Nome);
+                    Assert.Null(dtoUm.Nome);
+                }
             }
-
         }
 
         private string TransformarEmJson(object model)
