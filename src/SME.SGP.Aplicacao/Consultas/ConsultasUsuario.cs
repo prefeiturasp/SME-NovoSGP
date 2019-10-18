931afbe2493445573e1ac9df212665bb44646cb2
Eliton Santos <eliton.santos@amcom.com.br>
2019-10-18 15:59:07 -0300

    Criação do endpoint AB#7149 AB#4928

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ConsultasUsuario : IConsultasUsuario
+    {
+        private readonly IServicoEOL servicoEOL;
+
+        public ConsultasUsuario (IServicoEOL servicoEOL)
+        {
+            this.servicoEOL = servicoEOL ?? throw new System.ArgumentNullException(nameof(servicoEOL));
+        }
+        public async Task<MeusDadosDto> BuscarMeusDados(string login)
+        {
+            var meusDados = await servicoEOL.ObterMeusDados(login);
+            return meusDados;
+        }
+    }
+}
