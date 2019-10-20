9748c8681f92e1ba27efc244bbda46064a81c2c1
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-10-19 23:54:09 -0300

    Iniciando modal de alteração de senha AB#5424

--- src/SME.SGP.WebClient/src/paginas/Perfil/dadosEmail.js
+++ src/SME.SGP.WebClient/src/paginas/Perfil/dadosEmail.js
@@ -1,56 +1,62 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import CampoTexto from '~/componentes/campoTexto';
 import Button from '~/componentes/button';
 import { Colors } from '~/componentes/colors';
-import styled from 'styled-components';
 import ModalConteudoHtml from '~/componentes/modalConteudoHtml';
 import AlertaBalao from '~/componentes/alertaBalao';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Form } from 'antd';
 import api from '~/servicos/api';
 import { sucesso, confirmar } from '~/servicos/alertas';
+import FormularioSenha from './FormularioSenha/formularioSenha';
 
 const DadosEmail = () => {
-
   const [email, setEmail] = useState('teste@teste.com');
   const [emailEdicao, setEmailEdicao] = useState('teste@teste.com');
   const [senha, setSenha] = useState('******');
   const [visualizarFormEmail, setVisualizarFormEmail] = useState(false);
-  const [ocultarModalCancelamento, setOcultarModalCancelamento] = useState(true);
+  const [ocultarModalCancelamento, setOcultarModalCancelamento] = useState(
+    true
+  );
 
   const Campos = styled.div`
     margin-right: 10px;
     margin-left: 40px;
-    .campo{
+    .campo {
       margin-top: 50px;
     }
 
-    .botao{
+    .botao {
       margin-top: 25px;
     }
   `;
 
   const [validacoes] = useState(
     Yup.object({
-      emailUsuario: Yup.string()
-        .email('Digite um e-mail válido.')
+      emailUsuario: Yup.string().email('Digite um e-mail válido.'),
     })
   );
 
   const salvarEmail = novoEmail => {
-      api.put('v1/usuarios/autenticado/email', {novoEmail: novoEmail.emailUsuario}).then(resp =>{
+    api
+      .put('v1/usuarios/autenticado/email', {
+        novoEmail: novoEmail.emailUsuario,
+      })
+      .then(resp => {
         setEmail(novoEmail.emailUsuario);
-        setEmailEdicao(novoEmail.emailEdicao)
+        setEmailEdicao(novoEmail.emailEdicao);
         setVisualizarFormEmail(false);
         setOcultarModalCancelamento(true);
-        sucesso('Solicitação realizada com sucesso. Verifique sua caixa de entrada');
-      })
-  }
+        sucesso(
+          'Solicitação realizada com sucesso. Verifique sua caixa de entrada'
+        );
+      });
+  };
 
   const onClickCancelar = async form => {
     const novoEmail = form.values.emailUsuario;
-    if(email !== novoEmail){
+    if (email !== novoEmail) {
       setVisualizarFormEmail(false);
       const confirmado = await confirmar(
         'Atenção',
@@ -68,21 +74,21 @@ const DadosEmail = () => {
       } else {
         setVisualizarFormEmail(false);
       }
-    }else{
+    } else {
       setVisualizarFormEmail(false);
       setEmailEdicao(email);
     }
-  }
+  };
 
   return (
     <Campos>
       <Formik
         enableReinitialize
         initialValues={{
-          emailUsuario: emailEdicao? emailEdicao: email,
+          emailUsuario: emailEdicao || email,
         }}
         validationSchema={validacoes}
-        onSubmit = {valor => salvarEmail(valor)}
+        onSubmit={valor => salvarEmail(valor)}
         validateOnChange
         validateOnBlur
       >
@@ -92,14 +98,16 @@ const DadosEmail = () => {
               key="reiniciarSenha"
               visivel={visualizarFormEmail}
               onConfirmacaoPrincipal={() => {
-                form.validateForm().then(() => {form.handleSubmit(e => e);});
+                form.validateForm().then(() => {
+                  form.handleSubmit(e => e);
+                });
               }}
               onConfirmacaoSecundaria={() => onClickCancelar(form)}
               onClose={() => onClickCancelar(form)}
               labelBotaoPrincipal="Confirmar"
               labelBotaoSecundario="Cancelar"
               titulo="Editar E-mail"
-              closable={true}
+              closable
             >
               <div>
                 <CampoTexto
@@ -109,9 +117,13 @@ const DadosEmail = () => {
                   maxlength="50"
                 />
                 <div>
-                  <AlertaBalao maxWidth={472} marginTop={14} mostrarAlerta={true}
-                          texto="Você já possui um endereço de e-mail cadastrado. Ao alterá-lo, todas as comunicações
-                          passarão a ser feitas no novo e-mail" />
+                  <AlertaBalao
+                    maxWidth={472}
+                    marginTop={14}
+                    mostrarAlerta
+                    texto="Você já possui um endereço de e-mail cadastrado. Ao alterá-lo, todas as comunicações
+                          passarão a ser feitas no novo e-mail"
+                  />
                 </div>
               </div>
             </ModalConteudoHtml>
@@ -122,11 +134,11 @@ const DadosEmail = () => {
       <div className="row campo w-100">
         <div className="col-md-10">
           <CampoTexto
-            desabilitado={true}
+            desabilitado
             value={email}
             label="E-mail"
             placeholder="Insira um e-mail"
-            onChange={() => { }}
+            onChange={() => {}}
             type="email"
           />
         </div>
@@ -140,30 +152,9 @@ const DadosEmail = () => {
           />
         </div>
       </div>
-      <div className="row campo w-100">
-        <div className="col-md-10">
-          <CampoTexto
-            desabilitado={true}
-            label="Senha"
-            value={senha}
-            className="col-11 campo"
-            placeholder="Insira uma senha"
-            onChange={() => { }}
-            type="password"
-          />
-        </div>
-        <div className="col-md-2 botao">
-          <Button
-            label="Editar"
-            color={Colors.Roxo}
-            border
-            bold
-            onClick={() => { }}
-          />
-        </div>
-      </div>
+      <FormularioSenha />
     </Campos>
   );
-}
+};
 
 export default DadosEmail;
