442d7856dba5128e0eda439071e21a0b7f07ea98
eliton-amcom <eliton.santos@amcom.com.br>
2019-10-21 16:35:11 -0300

    Ajuste ResponseType

--- src/SME.SGP.Api/Controllers/UsuarioController.cs
+++ src/SME.SGP.Api/Controllers/UsuarioController.cs
@@ -51,7 +51,7 @@ namespace SME.SGP.Api.Controllers
 
         [Route("meus-dados")]
         [HttpGet]
-        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(MeusDadosDto), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         public async Task<IActionResult> BuscarMeusDados(string login)
