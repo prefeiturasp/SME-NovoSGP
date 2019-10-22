a8bf521baaa1f938fb77ded3dadea574c5562f16
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-10-22 16:16:59 -0300

    Endpoint para listagem de feriados AB#6344 AB#6341 AB#6342 AB#6343 AB#6348

---
+++ src/SME.SGP.Aplicacao/Comandos/ComandosFeriadoCalendario.cs
@@ -0,0 +1,66 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ComandosFeriadoCalendario : IComandosFeriadoCalendario
+    {
+        private readonly IRepositorioFeriadoCalendario repositorio;
+
+        public ComandosFeriadoCalendario(IRepositorioFeriadoCalendario repositorio)
+        {
+            this.repositorio = repositorio ?? throw new ArgumentNullException(nameof(repositorio));
+        }
+
+        public FeriadoCalendario MapearParaDominio(FeriadoCalendarioDto dto)
+        {
+            FeriadoCalendario entidade = repositorio.ObterPorId(dto.Id);
+            if (entidade == null)
+            {
+                entidade = new FeriadoCalendario();
+            }
+            entidade.Nome = dto.Nome;
+            entidade.Abrangencia = dto.Abrangencia;
+            entidade.Ativo = dto.Ativo;
+            entidade.DataFeriado = dto.DataFeriado;
+            entidade.Tipo = dto.Tipo;
+            return entidade;
+        }
+
+        public void MarcarExcluidos(long[] ids)
+        {
+            var idsInvalidos = "";
+            foreach (long id in ids)
+            {
+                var feriadoCalendario = repositorio.ObterPorId(id);
+                if (feriadoCalendario != null)
+                {
+                    feriadoCalendario.Excluido = true;
+                    repositorio.Salvar(feriadoCalendario);
+                }
+                else
+                {
+                    idsInvalidos += idsInvalidos.Equals("") ? $"{id}" : $", {id}";
+                }
+            }
+            if (!idsInvalidos.Trim().Equals(""))
+            {
+                throw new NegocioException($"Houve um erro ao excluir os feriados ids '{idsInvalidos}'. Um dos feriados não existe");
+            }
+        }
+
+        public void Salvar(FeriadoCalendarioDto dto)
+        {
+            var feriado = MapearParaDominio(dto);
+
+            bool ehRegistroExistente = repositorio.VerificarRegistroExistente(dto.Id, dto.Nome);
+            if (ehRegistroExistente)
+            {
+                throw new NegocioException($"O Feriado '{dto.Nome}' já existe");
+            }
+            repositorio.Salvar(feriado);
+        }
+    }
+}
