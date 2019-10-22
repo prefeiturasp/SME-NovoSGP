3ebf41787d36c6bc21478e331debc5a752d8643d
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-10-22 13:24:10 -0300

    Continuação da lógica de montar o menu

--- src/SME.SGP.Infra/Dtos/MenuRetornoDto.cs
+++ src/SME.SGP.Infra/Dtos/MenuRetornoDto.cs
@@ -4,8 +4,15 @@ namespace SME.SGP.Infra
 {
     public class MenuRetornoDto
     {
+        public MenuRetornoDto()
+        {
+            Menus = new List<MenuPermissaoDto>();
+        }
+
         public int Codigo { get; set; }
         public string Descricao { get; set; }
-        public IList<MenuPermissaoDto> Permissoes { get; set; }
+        public string Icone { get; set; }
+        public IList<MenuPermissaoDto> Menus { get; set; }
+        public int QuantidadeMenus { get; set; }
     }
 }
