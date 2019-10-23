6348a282906ec80910b3c7cad14c609a7dc170a0
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-10-23 16:38:24 -0300

    Inclusão do Perfil autal no JWT AB#7207

--- src/SME.SGP.Aplicacao/Servicos/ServicoTokenJwt.cs
+++ src/SME.SGP.Aplicacao/Servicos/ServicoTokenJwt.cs
@@ -17,12 +17,13 @@ namespace SME.SGP.Aplicacao.Servicos
             this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public string GerarToken(string usuarioLogin, string codigoRf, IEnumerable<Permissao> permissionamentos)
+        public string GerarToken(string usuarioLogin, string codigoRf, IEnumerable<Permissao> permissionamentos, string guidPerfil)
         {
             IList<Claim> claims = new List<Claim>();
 
             claims.Add(new Claim("login", usuarioLogin));
             claims.Add(new Claim("rf", codigoRf));
+            claims.Add(new Claim("perfilAtual", guidPerfil));
 
             foreach (var permissao in permissionamentos)
             {
