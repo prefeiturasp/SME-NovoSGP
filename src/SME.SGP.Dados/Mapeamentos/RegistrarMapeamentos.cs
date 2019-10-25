a6a3eaaaa9f9d9f1a3d730bd95ee440afe41b92c
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-10-25 14:32:24 -0300

    Criação do repositorio de evento AB#4183

--- src/SME.SGP.Dados/Mapeamentos/RegistrarMapeamentos.cs
+++ src/SME.SGP.Dados/Mapeamentos/RegistrarMapeamentos.cs
@@ -32,6 +32,7 @@ namespace SME.SGP.Dados.Mapeamentos
                config.AddMap(new ConfiguracaoEmailMap());
                config.AddMap(new TipoCalendarioMap());
                config.AddMap(new FeriadoCalendarioMap());
+               config.AddMap(new EventoMap());
                config.ForDommel();
            });
         }
