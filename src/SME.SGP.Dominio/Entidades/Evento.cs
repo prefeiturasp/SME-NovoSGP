4502206e2f01f8ecd4d8bdb6e008708bc89ca65f
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-10-25 08:25:37 -0300

    Iniciando modelagem de Evento AB#4183

---
+++ src/SME.SGP.Dominio/Entidades/Evento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SME.SGP.Dominio
+{
+    public class Evento : EntidadeBase
+    {
+        public DateTime? DataFim { get; set; }
+        public DateTime DataInicio { get; set; }
+        public string Descricao { get; set; }
+        public long DreId { get; set; }
+        public bool EhLetivo { get; set; }
+        public FeriadoCalendario FeriadoCalendario { get; set; }
+        public long? FeriadoId { get; set; }
+        public string Nome { get; set; }
+        public TipoCalendario TipoCalendario { get; set; }
+        public long TipoCalendarioId { get; set; }
+        public long TipoEventoId { get; set; }
+        public long UeId { get; set; }
+    }
+}
