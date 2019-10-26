eed17bf88eab030bd48144ca2fe2b1d4f4712681
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-10-26 09:38:18 -0300

    Iniciando servido de evento AB#4183

---
+++ src/SME.SGP.Dominio.Servicos/ServicoEvento.cs
@@ -0,0 +1,11 @@
+﻿using SME.SGP.Dominio.Interfaces;
+
+namespace SME.SGP.Dominio.Servicos
+{
+    public class ServicoEvento : IServicoEvento
+    {
+        public void Salvar(Evento evento)
+        {
+        }
+    }
+}
