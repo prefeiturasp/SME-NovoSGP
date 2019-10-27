56167f8cb5f6954778d25147ea8ff8800122be58
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-10-27 19:00:20 -0300

    Refatoração de tipo de evento para alteração ser separado da inclusão; Endpoint de exclusão de evento; Comando de exclusão de evento; Teste unitário;

--- src/SME.SGP.Api/Controllers/EventoController.cs
+++ src/SME.SGP.Api/Controllers/EventoController.cs
@@ -18,6 +18,17 @@ namespace SME.SGP.Api.Controllers
             this.comandosEvento = comandosEvento ?? throw new System.ArgumentNullException(nameof(comandosEvento));
         }
 
+        [HttpDelete]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        //[Permissao(Permissao.C_I, Policy = "Bearer")]
+        public IActionResult Excluir(long[] eventosId)
+        {
+            comandosEvento.Excluir(eventosId);
+            return Ok();
+        }
+
         [HttpPost]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
