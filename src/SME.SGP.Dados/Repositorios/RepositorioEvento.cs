ef6089d6c7b21c796264514bccfdf2d6678796a5
Everton Nogueira <everton.nogueira@outlook.com.br>
2019-10-29 12:41:48 -0300

    Iniciando listagem de eventos AB#4183

--- src/SME.SGP.Dados/Repositorios/RepositorioEvento.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioEvento.cs
@@ -3,7 +3,9 @@ using SME.SGP.Dados.Contexto;
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace SME.SGP.Dados.Repositorios
 {
@@ -16,6 +18,13 @@ namespace SME.SGP.Dados.Repositorios
         public bool ExisteEventoNaDataEspecificada(DateTime dataInicio)
         {
             return database.Conexao.Query<bool>("select 1 from evento where data_inicio = @dataInicio;", new { dataInicio }).FirstOrDefault();
+        }
+
+        public IEnumerable<Evento> Listar(long? tipoCalendarioId, long? tipoEventoId, string nomeEvento, DateTime? dataInicio, DateTime? dataFim)
+        {
+            var query = new StringBuilder();
+
+            return database.Conexao.Query<Evento>(query.ToString());
         }
     }
 }
