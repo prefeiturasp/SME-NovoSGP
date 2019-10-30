5976e70e3c263e14834eabc461a53a0f9377db7d
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-10-30 17:54:32 -0300

    Script para modificar coluna de em de modalidade para int; endpoints de abrangencia, esperando id modalidade;

--- src/SME.SGP.Aplicacao/Consultas/ConsultasAbrangencia.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasAbrangencia.cs
@@ -1,6 +1,10 @@
-﻿using SME.SGP.Dominio.Interfaces;
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Dto;
+using SME.SGP.Infra;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
@@ -24,28 +28,36 @@ namespace SME.SGP.Aplicacao
             return await repositorioAbrangencia.ObterAbrangenciaPorFiltro(texto, login, perfil);
         }
 
-        public async Task<IEnumerable<AbrangenciaDreRetorno>> ObterDres()
+        public async Task<IEnumerable<AbrangenciaDreRetorno>> ObterDres(Modalidade modalidade)
         {
             var login = servicoUsuario.ObterLoginAtual();
             var perfil = servicoUsuario.ObterPerfilAtual();
 
-            return await repositorioAbrangencia.ObterDres(login, perfil);
+            return await repositorioAbrangencia.ObterDres(login, perfil, modalidade);
         }
 
-        public async Task<IEnumerable<AbrangenciaTurmaRetorno>> ObterTurmas(string codigoUe)
+        public async Task<IEnumerable<EnumeradoRetornoDto>> ObterModalidades()
         {
-            var login = servicoUsuario.ObterLoginAtual();
-            var perfil = servicoUsuario.ObterPerfilAtual();
+            var lista = await repositorioAbrangencia.ObterModalidades();
 
-            return await repositorioAbrangencia.ObterTurmas(codigoUe, login, perfil);
+            return from a in lista
+                   select new EnumeradoRetornoDto() { Id = a, Descricao = ((Modalidade)a).GetAttribute<DisplayAttribute>().Name };
         }
 
-        public async Task<IEnumerable<AbrangenciaUeRetorno>> ObterUes(string codigoDre)
+        public async Task<IEnumerable<AbrangenciaTurmaRetorno>> ObterTurmas(string codigoUe, Modalidade modalidade)
         {
             var login = servicoUsuario.ObterLoginAtual();
             var perfil = servicoUsuario.ObterPerfilAtual();
 
-            return await repositorioAbrangencia.ObterUes(codigoDre, login, perfil);
+            return await repositorioAbrangencia.ObterTurmas(codigoUe, login, perfil, modalidade);
+        }
+
+        public async Task<IEnumerable<AbrangenciaUeRetorno>> ObterUes(string codigoDre, Modalidade modalidade)
+        {
+            var login = servicoUsuario.ObterLoginAtual();
+            var perfil = servicoUsuario.ObterPerfilAtual();
+
+            return await repositorioAbrangencia.ObterUes(codigoDre, login, perfil, modalidade);
         }
     }
 }
