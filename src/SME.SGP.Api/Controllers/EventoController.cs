71664a1fc4c83a58587525c3970438574cf1a3d6
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-10-30 13:32:41 -0300

    Ajustes

--- src/SME.SGP.Api/Controllers/EventoController.cs
+++ src/SME.SGP.Api/Controllers/EventoController.cs
@@ -43,19 +43,19 @@ namespace SME.SGP.Api.Controllers
         }
 
         [HttpGet("meses")]
-        [ProducesResponseType(typeof(IEnumerable<CalendarioEventosMeses>), 200)]
+        [ProducesResponseType(typeof(IEnumerable<CalendarioEventosMesesDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         //[Permissao(Permissao.C_I, Policy = "Bearer")]
-        public IActionResult ObterMeses([FromQuery]CalendarioEventosMesesFiltro calendarioEventoMesesFiltro)
+        public IActionResult ObterMeses([FromQuery]CalendarioEventosMesesFiltroDto calendarioEventoMesesFiltro)
         {
-            var retornoMockado = new List<CalendarioEventosMeses>();
+            var retornoMockado = new List<CalendarioEventosMesesDto>();
 
-            retornoMockado.Add(new CalendarioEventosMeses() { Eventos = 2, Mes = 1 });
-            retornoMockado.Add(new CalendarioEventosMeses() { Eventos = 3, Mes = 2 });
-            retornoMockado.Add(new CalendarioEventosMeses() { Eventos = 5, Mes = 5 });
-            retornoMockado.Add(new CalendarioEventosMeses() { Eventos = 1, Mes = 8 });
-            retornoMockado.Add(new CalendarioEventosMeses() { Eventos = 3, Mes = 12 });
+            retornoMockado.Add(new CalendarioEventosMesesDto() { Eventos = 2, Mes = 1 });
+            retornoMockado.Add(new CalendarioEventosMesesDto() { Eventos = 3, Mes = 2 });
+            retornoMockado.Add(new CalendarioEventosMesesDto() { Eventos = 5, Mes = 5 });
+            retornoMockado.Add(new CalendarioEventosMesesDto() { Eventos = 1, Mes = 8 });
+            retornoMockado.Add(new CalendarioEventosMesesDto() { Eventos = 3, Mes = 12 });
 
             return Ok(retornoMockado);
         }
