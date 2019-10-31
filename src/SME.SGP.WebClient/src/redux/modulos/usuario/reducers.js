4055319024503241be80ae74b70b71bc51b70e2d
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2019-10-31 13:34:23 -0300

    Calendario - validacoes cadastrar eventos - AB#4183 AB#6356 AB#6357

--- src/SME.SGP.WebClient/src/redux/modulos/usuario/reducers.js
+++ src/SME.SGP.WebClient/src/redux/modulos/usuario/reducers.js
@@ -16,7 +16,9 @@ const inicial = {
       'https://graziellanicolai.com.br/wp-content/uploads/2018/03/Graziella-perfil.jpg',
   },
   possuiPerfilSmeOuDre: false,
-  menu: []
+  possuiPerfilDre: false,
+  possuiPerfilSme: false,
+  menu: [],
 };
 
 export default function usuario(state = inicial, action) {
@@ -37,6 +39,8 @@ export default function usuario(state = inicial, action) {
         draft.usuario = action.payload.usuario;
         draft.modificarSenha = action.payload.modificarSenha;
         draft.possuiPerfilSmeOuDre = action.payload.possuiPerfilSmeOuDre;
+        draft.possuiPerfilDre = action.payload.possuiPerfilDre;
+        draft.possuiPerfilSme = action.payload.possuiPerfilSme;
         draft.menu = action.payload.menu;
         break;
       case '@usuario/deslogar':
