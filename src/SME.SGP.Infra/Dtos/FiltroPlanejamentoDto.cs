7bc59bfd836cff3ff2d56bf3244eff97ebdf4912
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-11-01 11:27:22 -0300

    Alterando namespace do DTO

--- src/SME.SGP.Infra/Dtos/FiltroPlanejamentoDto.cs
+++ src/SME.SGP.Infra/Dtos/FiltroPlanejamentoDto.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace SME.SGP.Infra.Dtos
+namespace SME.SGP.Infra
 {
     public class FiltroDisciplinaPlanejamentoDto
     {
         public int CodigoDisciplina { get; set; }
-        public bool Regencia { get; set; }
-        [Range(0,long.MaxValue, ErrorMessage = "É necessario informar o codigo da turma")]
+
+        [Range(0, long.MaxValue, ErrorMessage = "É necessario informar o codigo da turma")]
         public long CodigoTurma { get; set; }
+
+        public bool Regencia { get; set; }
     }
 }
--- teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasDisciplinasTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasDisciplinasTeste.cs
@@ -2,7 +2,7 @@
 using SME.SGP.Aplicacao.Integracoes;
 using SME.SGP.Aplicacao.Integracoes.Respostas;
 using SME.SGP.Dominio.Interfaces;
-using SME.SGP.Infra.Dtos;
+using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
 using System.Linq;
