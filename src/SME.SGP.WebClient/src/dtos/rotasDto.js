9c4d1b2388061b21178e368b8a0c545d5aed007b
Eliton Santos <eliton.santos@amcom.com.br>
2019-11-01 19:11:12 -0300

    alteração permissão rotas filhas

--- src/SME.SGP.WebClient/src/dtos/rotasDto.js
+++ src/SME.SGP.WebClient/src/dtos/rotasDto.js
@@ -2,6 +2,11 @@ const RotasDto = {
   PRINCIPAL:'/',
   PLANO_CICLO:'/planejamento/plano-ciclo',
   PLANO_ANUAL:'/planejamento/plano-anual',
+  PERIODOS_ESCOLARES:'/calendario-escolar/periodos-escolares',
+  TIPO_EVENTOS:'/calendario-escolar/tipo-eventos',
+  ATRIBUICAO_SUPERVISOR_LISTA:'/gestao/atribuicao-supervisor-lista',
+  MEUS_DADOS:'/meus-dados',
+  NOTIFICACOES: '/notificacoes'
 }
 
 export default RotasDto;
