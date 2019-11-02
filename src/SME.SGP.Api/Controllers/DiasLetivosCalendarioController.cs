a05524ac8d5518ad35c08a68453c3f053d6ea3cb
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-11-02 19:31:26 -0300

    Mock dias letivos

---
+++ src/SME.SGP.Api/Controllers/DiasLetivosCalendarioController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dto;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/calendarios/")]
+    [ValidaDto]
+    [Authorize("Bearer")]
+    public class DiasLetivosCalendarioController : ControllerBase
+    {
+        private readonly IComandosDiasLetivos comandos;
+
+        public DiasLetivosCalendarioController(IComandosDiasLetivos comandos)
+        {
+            this.comandos = comandos ?? throw new System.ArgumentNullException(nameof(comandos));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(DiasLetivosDto), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Route("{tipoCalendarioId}/dias-letivos")]
+        //[Permissao(Permissao.C_C, Policy = "Bearer")]
+        public IActionResult CalcularDiasLetivos(long tipoCalendarioId)
+        {
+            return Ok(new DiasLetivosDto { DiasLetivos = 200, EstaAbaixoPermitido = false);
+        }
+    }
+}
