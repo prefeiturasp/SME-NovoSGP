ad556428903460f645322bddc7e84ee5ef84f6b2
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-11-02 19:36:43 -0300

    mock dias letivos

---
+++ src/SME.SGP.Infra/Dtos/DiasLetivosDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Dto
+{
+    public class DiasLetivosDto
+    {
+        public int DiasLetivos { get; set; }
+        public bool EstaAbaixoPermitido { get; set; }
+    }
+}
--- src/SME.SGP.IoC/RegistraDependencias.cs
+++ src/SME.SGP.IoC/RegistraDependencias.cs
@@ -38,6 +38,7 @@ namespace SME.SGP.IoC
             services.TryAddScoped<IComandosPeriodoEscolar, ComandosPeriodoEscolar>();
             services.TryAddScoped<IComandosEventoTipo, ComandosEventoTipo>();
             services.TryAddScoped<IComandosEvento, ComandosEvento>();
+            services.TryAddScoped<IComandosDiasLetivos, ComandosDiasLetivos>();
         }
 
         private static void RegistrarConsultas(IServiceCollection services)
