371f9d8ac033a69e86fe25506aed2f4fd96114ea
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-11-02 18:44:51 -0300

    Mock dias letivos

---
+++ src/SME.SGP.Aplicacao/Comandos/ComandosDiasLetivos.cs
@@ -0,0 +1,36 @@
+﻿using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Dto;
+using System;
+using System.Linq;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ComandosDiasLetivos : IComandosDiasLetivos
+    {
+        private readonly IRepositorioEvento repositorioEvento;
+        private readonly IRepositorioPeriodoEscolar repositorioPeriodoEscolar;
+
+        public ComandosDiasLetivos(IRepositorioPeriodoEscolar repositorioPeriodoEscolar, IRepositorioEvento repositorioEvento)
+        {
+            this.repositorioPeriodoEscolar = repositorioPeriodoEscolar ?? throw new ArgumentNullException(nameof(repositorioPeriodoEscolar));
+            this.repositorioEvento = repositorioEvento ?? throw new ArgumentNullException(nameof(repositorioEvento));
+        }
+
+        public DiasLetivosDto CalcularDiasLetivos(long tipoCalendarioId)
+        {
+            return new DiasLetivosDto { DiasLetivos = 200, EstaAbaixoPermitido = false };
+            //var diasLetivosCalendario = BuscarDiasLetivos(tipoCalendarioId);
+            //var eventos = repositorioEvento.ObterPorTipoDeCalendario(tipoCalendarioId);
+            //return null;
+        }
+
+        private int BuscarDiasLetivos(long tipoCalendarioId)
+        {
+            int diasLetivosPeriodoEscolar = 0;
+            var periodoEscolar = repositorioPeriodoEscolar.ObterPorTipoCalendario(tipoCalendarioId);
+            periodoEscolar.ToList().ForEach(x => diasLetivosPeriodoEscolar += (x.PeriodoFim - x.PeriodoInicio).Days);
+            return diasLetivosPeriodoEscolar;
+        }
+    }
+}
