30b0a96c868dac9b1c55b69437025953c4b1ab3f
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-03 19:55:27 -0300

    Inicio da recorrencia de eventos semana e mensal AB#4622

--- src/SME.SGP.Dominio/Entidades/Evento.cs
+++ src/SME.SGP.Dominio/Entidades/Evento.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace SME.SGP.Dominio
 {
-    public class Evento : EntidadeBase
+    public class Evento : EntidadeBase, ICloneable
     {
         public Evento()
         {
@@ -16,6 +16,7 @@ namespace SME.SGP.Dominio
         public DateTime DataInicio { get; set; }
         public string Descricao { get; set; }
         public string DreId { get; set; }
+        public long? EventoPaiId { get; private set; }
         public bool Excluido { get; set; }
         public FeriadoCalendario FeriadoCalendario { get; set; }
         public long? FeriadoId { get; set; }
@@ -35,6 +36,34 @@ namespace SME.SGP.Dominio
         public void AdicionarTipoEvento(EventoTipo tipoEvento)
         {
             TipoEvento = tipoEvento;
+        }
+
+        public object Clone()
+        {
+            return new Evento
+            {
+                AlteradoEm = AlteradoEm,
+                AlteradoPor = AlteradoPor,
+                AlteradoRF = AlteradoRF,
+                CriadoEm = CriadoEm,
+                CriadoPor = CriadoPor,
+                CriadoRF = CriadoRF,
+                DataFim = DataFim,
+                DataInicio = DataInicio,
+                Descricao = Descricao,
+                DreId = DreId,
+                Excluido = Excluido,
+                FeriadoCalendario = FeriadoCalendario,
+                FeriadoId = FeriadoId,
+                Id = Id,
+                Letivo = Letivo,
+                Nome = Nome,
+                TipoCalendario = TipoCalendario,
+                TipoCalendarioId = TipoCalendarioId,
+                TipoEvento = TipoEvento,
+                TipoEventoId = TipoEventoId,
+                UeId = UeId
+            };
         }
 
         public bool DeveSerEmDiaLetivo()
@@ -82,6 +111,24 @@ namespace SME.SGP.Dominio
             Excluido = true;
         }
 
+        public IEnumerable<Evento> ObterRecorrencia(PadraoRecorrencia padraoRecorrencia, PadraoRecorrenciaMensal padraoRecorrenciaMensal, DateTime dataInicio, DateTime dataFinal, IEnumerable<DayOfWeek> diasDaSemana, int repeteACada, DateTime? diaDeOcorrencia)
+        {
+            if (Id == 0)
+                throw new NegocioException("Só é possível aplicar recorrência em eventos já registrados.");
+
+            if (dataFinal == DateTime.MinValue)
+                throw new NegocioException("A data final é obrigatória para calcular a recorrência de eventos.");
+            if (padraoRecorrencia == PadraoRecorrencia.Semanal)
+                return ObterRecorrenciaSemanal(dataInicio, dataFinal, diasDaSemana, repeteACada);
+            else
+            {
+                if (padraoRecorrenciaMensal == PadraoRecorrenciaMensal.NoDia && !diaDeOcorrencia.HasValue)
+                    throw new NegocioException("O dia de ocorrência é obrigatório para calcular esse tipo recorrência de eventos.");
+
+                return ObterRecorrenciaMensal(padraoRecorrenciaMensal, dataInicio, dataFinal, diaDeOcorrencia, repeteACada, diasDaSemana.FirstOrDefault());
+            }
+        }
+
         public bool PermiteConcomitancia()
         {
             TipoEventoObrigatorio();
@@ -97,11 +144,135 @@ namespace SME.SGP.Dominio
             }
             else
             {
-                if (TipoEvento.TipoData == EventoTipoData.Unico && DataFim.HasValue)
+                if (TipoEvento.TipoData == EventoTipoData.Unico)
                 {
-                    throw new NegocioException("Neste tipo de evento a data final do evento não deve ser informada.");
+                    DataFim = DataInicio;
                 }
             }
+        }
+
+        private void AdicionaEventoPorRecorrenciaMensal(PadraoRecorrenciaMensal padraoRecorrenciaMensal, List<Evento> eventos, DateTime dataAtual, DateTime diaOcorrencia)
+        {
+            if (padraoRecorrenciaMensal == PadraoRecorrenciaMensal.NoDia && dataAtual.Day == diaOcorrencia.Day)
+            {
+                var evento = (Evento)Clone();
+                evento.DataInicio = dataAtual;
+                evento.DataFim = dataAtual;
+                evento.EventoPaiId = Id;
+                eventos.Add(evento);
+            }
+        }
+
+        private void AdicionaEventosMensais(PadraoRecorrenciaMensal padraoRecorrenciaMensal, DateTime dataInicial, DateTime diaOcorrencia, List<Evento> eventos)
+        {
+            var primeiroDiaDoMes = new DateTime(dataInicial.Year, dataInicial.Month, 1);
+            var ultimoDiaDoMes = primeiroDiaDoMes.AddMonths(1).AddDays(-1);
+
+            for (DateTime data = dataInicial; data <= ultimoDiaDoMes; data = data.AddDays(1))
+            {
+                AdicionaEventoPorRecorrenciaMensal(padraoRecorrenciaMensal, eventos, data, diaOcorrencia);
+            }
+        }
+
+        private void AdicionaEventosPorDiasDaSemana(IEnumerable<DayOfWeek> diasDaSemana, List<Evento> eventos, DateTime dataInicial)
+        {
+            var sabado = ObterSabado(dataInicial);
+            for (DateTime data = dataInicial; data <= sabado; data = data.AddDays(1))
+            {
+                if (diasDaSemana.Contains(data.DayOfWeek))
+                {
+                    var evento = (Evento)Clone();
+                    evento.DataInicio = data;
+                    evento.DataFim = data;
+                    evento.EventoPaiId = Id;
+                    eventos.Add(evento);
+                }
+            }
+        }
+
+        private DateTime ObterDomingo(DateTime data)
+        {
+            if (data.DayOfWeek == DayOfWeek.Sunday)
+                return data;
+            int diferenca = (7 + (data.DayOfWeek - DayOfWeek.Sunday)) % 7;
+            return data.AddDays(-1 * diferenca).Date;
+        }
+
+        private DateTime ObterProximaDataRecorrenciaMensal(DateTime dataAtual, PadraoRecorrenciaMensal padraoRecorrenciaMensal, DayOfWeek diaDaSemana)
+        {
+            if (padraoRecorrenciaMensal != PadraoRecorrenciaMensal.NoDia)
+            {
+                dataAtual = ObterProximoDiaDaSemanaDoMes(dataAtual, padraoRecorrenciaMensal, diaDaSemana);
+            }
+            return dataAtual;
+        }
+
+        private DateTime ObterProximoDiaDaSemanaDoMes(DateTime dataAtual, PadraoRecorrenciaMensal padraoRecorrenciaMensal, DayOfWeek diaDaSemana)
+        {
+            int diferenca;
+            if (padraoRecorrenciaMensal == PadraoRecorrenciaMensal.Ultima)
+            {
+                var ultimoDiaDoMes = new DateTime(dataAtual.Year, dataAtual.Month, 1).AddMonths(1).AddDays(-1);
+                diferenca = diaDaSemana - ultimoDiaDoMes.DayOfWeek;
+                return ultimoDiaDoMes.AddDays(diferenca);
+            }
+
+            dataAtual = new DateTime(dataAtual.Year, dataAtual.Month, 1);
+            var multiplicador = 1;
+            dataAtual = dataAtual.AddDays(7 * ((int)padraoRecorrenciaMensal));
+            if (dataAtual.DayOfWeek > diaDaSemana)
+            {
+            }
+            else
+            {
+                multiplicador = ((int)padraoRecorrenciaMensal - 1);
+                dataAtual = dataAtual.AddDays(-(7 * multiplicador));
+            }
+            diferenca = diaDaSemana - dataAtual.DayOfWeek;
+            return dataAtual.AddDays(diferenca * multiplicador);
+        }
+
+        private IEnumerable<Evento> ObterRecorrenciaMensal(PadraoRecorrenciaMensal padraoRecorrenciaMensal, DateTime dataInicio, DateTime dataFinal, DateTime? diaOcorrencia, int repeteACada, DayOfWeek diaDaSemana)
+        {
+            var eventos = new List<Evento>();
+            var dataAtual = dataInicio;
+            while (dataAtual <= dataFinal)
+            {
+                dataAtual = ObterProximaDataRecorrenciaMensal(dataAtual, padraoRecorrenciaMensal, diaDaSemana);
+                var evento = (Evento)Clone();
+                evento.DataInicio = dataAtual;
+                evento.DataFim = dataAtual;
+                evento.EventoPaiId = Id;
+                eventos.Add(evento);
+                dataAtual = dataAtual.AddMonths(repeteACada);
+                //dataAtual = new DateTime(dataAtual.Year, dataAtual.AddMonths(repeteACada).Month, 1);
+            }
+
+            return eventos;
+        }
+
+        private IEnumerable<Evento> ObterRecorrenciaSemanal(DateTime dataInicio, DateTime dataFinal, IEnumerable<DayOfWeek> diasDaSemana, int repeteACada)
+        {
+            var dataInicial = ObterDomingo(dataInicio);
+            if (dataInicial.DayOfWeek < dataInicio.DayOfWeek)
+                dataInicial = dataInicio;
+
+            var eventos = new List<Evento>();
+            while (dataInicial <= dataFinal)
+            {
+                AdicionaEventosPorDiasDaSemana(diasDaSemana, eventos, dataInicial);
+                dataInicial = dataInicial.AddDays(7 * repeteACada);
+                dataInicial = ObterDomingo(dataInicial);
+            }
+            return eventos;
+        }
+
+        private DateTime ObterSabado(DateTime data)
+        {
+            if (data.DayOfWeek == DayOfWeek.Saturday)
+                return data;
+            int diferenca = (((int)DayOfWeek.Saturday - (int)data.DayOfWeek + 7) % 7);
+            return data.AddDays(diferenca);
         }
 
         private void TipoEventoObrigatorio()
--- teste/SME.SGP.Dominio.Teste/EventoTeste.cs
+++ teste/SME.SGP.Dominio.Teste/EventoTeste.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace SME.SGP.Dominio.Teste
@@ -11,6 +13,250 @@ namespace SME.SGP.Dominio.Teste
             Evento evento = ObterEvento();
             Assert.True(evento != null);
             Assert.True(evento.DeveSerEmDiaLetivo());
+        }
+
+        #region RecorrenciaMensal
+
+        [Fact]
+        public void DeveCriarRecorrenciaMensalACada1MesParaSegundaQuartaFeiraDeCadaMes()
+        {
+            DateTime dataInicio = new DateTime(2019, 10, 04);
+            DateTime? dataFinal = new DateTime(2019, 12, 30);
+            IEnumerable<DayOfWeek> diasDaSemana = new List<DayOfWeek> {
+                DayOfWeek.Wednesday
+            };
+            int repeteACada = 1;
+
+            var evento = new Evento()
+            {
+                Nome = "Evento Original",
+                Id = 1
+            };
+
+            var eventos = evento.ObterRecorrencia(PadraoRecorrencia.Mensal, PadraoRecorrenciaMensal.Segunda, dataInicio, dataFinal.Value, diasDaSemana, repeteACada, null);
+
+            Assert.NotNull(eventos);
+            Assert.True(eventos.Count() == 3);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 9 && c.DataInicio.Month == 10);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 13 && c.DataInicio.Month == 11);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 11 && c.DataInicio.Month == 12);
+        }
+
+        [Fact]
+        public void DeveCriarRecorrenciaMensalACada1MesParaUltimaTercaFeiraDeCadaMes()
+        {
+            DateTime dataInicio = new DateTime(2019, 10, 04);
+            DateTime? dataFinal = new DateTime(2019, 12, 30);
+            IEnumerable<DayOfWeek> diasDaSemana = new List<DayOfWeek> {
+                DayOfWeek.Tuesday
+            };
+            int repeteACada = 1;
+
+            var evento = new Evento()
+            {
+                Nome = "Evento Original",
+                Id = 1
+            };
+
+            var eventos = evento.ObterRecorrencia(PadraoRecorrencia.Mensal, PadraoRecorrenciaMensal.Ultima, dataInicio, dataFinal.Value, diasDaSemana, repeteACada, null);
+
+            Assert.NotNull(eventos);
+            Assert.True(eventos.Count() == 3);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 29 && c.DataInicio.Month == 10);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 26 && c.DataInicio.Month == 11);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 31 && c.DataInicio.Month == 12);
+        }
+
+        [Fact]
+        public void DeveCriarRecorrenciaMensalACada2MeseParaTerceiroDomingoDeCadaMes()
+        {
+            DateTime dataInicio = new DateTime(2019, 01, 01);
+            DateTime? dataFinal = new DateTime(2019, 12, 30);
+            IEnumerable<DayOfWeek> diasDaSemana = new List<DayOfWeek> {
+                DayOfWeek.Sunday
+            };
+            int repeteACada = 2;
+
+            var evento = new Evento()
+            {
+                Nome = "Evento Original",
+                Id = 1
+            };
+
+            var eventos = evento.ObterRecorrencia(PadraoRecorrencia.Mensal, PadraoRecorrenciaMensal.Terceira, dataInicio, dataFinal.Value, diasDaSemana, repeteACada, null);
+
+            Assert.NotNull(eventos);
+            Assert.True(eventos.Count() == 6);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 20 && c.DataInicio.Month == 1);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 17 && c.DataInicio.Month == 2);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 17 && c.DataInicio.Month == 3);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 21 && c.DataInicio.Month == 4);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 19 && c.DataInicio.Month == 5);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 16 && c.DataInicio.Month == 6);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 21 && c.DataInicio.Month == 7);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 18 && c.DataInicio.Month == 8);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 15 && c.DataInicio.Month == 9);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 20 && c.DataInicio.Month == 10);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 17 && c.DataInicio.Month == 11);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 15 && c.DataInicio.Month == 12);
+        }
+
+        #endregion RecorrenciaMensal
+
+        [Fact]
+        public void DeveCriarRecorrenciaSemanalACada1Semana()
+        {
+            DateTime dataInicio = new DateTime(2019, 11, 04);
+            DateTime? dataFinal = new DateTime(2019, 11, 30);
+            IEnumerable<DayOfWeek> diasDaSemana = new List<DayOfWeek> {
+                DayOfWeek.Sunday,
+                DayOfWeek.Wednesday,
+                DayOfWeek.Friday
+            };
+            int repeteACada = 1;
+
+            var evento = new Evento()
+            {
+                Nome = "Evento Original",
+                Id = 1
+            };
+            var eventos = evento.ObterRecorrencia(PadraoRecorrencia.Semanal, 0, dataInicio, dataFinal.Value, diasDaSemana, repeteACada, null);
+
+            Assert.NotNull(eventos);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 1);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 2);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 3);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 4);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 5);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 6);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 7);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 8);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 9);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 10);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 11);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 12);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 13);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 14);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 15);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 16);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 17);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 18);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 19);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 20);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 21);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 22);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 23);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 24);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 25);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 26);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 27);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 28);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 29);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 30);
+        }
+
+        [Fact]
+        public void DeveCriarRecorrenciaSemanalACada2Semanas()
+        {
+            DateTime dataInicio = new DateTime(2019, 11, 04);
+            DateTime? dataFinal = new DateTime(2019, 11, 30);
+            IEnumerable<DayOfWeek> diasDaSemana = new List<DayOfWeek> {
+                DayOfWeek.Sunday,
+                DayOfWeek.Wednesday,
+                DayOfWeek.Friday
+            };
+            int repeteACada = 2;
+
+            var evento = new Evento()
+            {
+                Nome = "Evento Original",
+                Id = 1
+            };
+            var eventos = evento.ObterRecorrencia(PadraoRecorrencia.Semanal, 0, dataInicio, dataFinal.Value, diasDaSemana, repeteACada, null);
+
+            Assert.NotNull(eventos);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 1);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 2);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 3);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 4);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 5);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 6);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 7);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 8);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 9);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 10);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 11);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 12);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 13);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 14);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 15);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 16);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 17);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 18);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 19);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 20);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 21);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 22);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 23);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 24);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 25);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 26);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 27);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 28);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 29);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 30);
+        }
+
+        [Fact]
+        public void DeveCriarRecorrenciaSemanalACada3Semanas()
+        {
+            DateTime dataInicio = new DateTime(2019, 11, 04);
+            DateTime? dataFinal = new DateTime(2019, 11, 30);
+            IEnumerable<DayOfWeek> diasDaSemana = new List<DayOfWeek> {
+                DayOfWeek.Sunday,
+                DayOfWeek.Wednesday,
+                DayOfWeek.Friday
+            };
+            int repeteACada = 3;
+
+            var evento = new Evento()
+            {
+                Nome = "Evento Original",
+                Id = 1
+            };
+            var eventos = evento.ObterRecorrencia(PadraoRecorrencia.Semanal, 0, dataInicio, dataFinal.Value, diasDaSemana, repeteACada, null);
+
+            Assert.NotNull(eventos);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 1);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 2);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 3);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 4);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 5);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 6);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 7);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 8);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 9);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 10);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 11);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 12);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 13);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 14);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 15);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 16);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 17);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 18);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 19);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 20);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 21);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 22);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 23);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 24);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 25);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 26);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 27);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 28);
+            Assert.Contains(eventos, c => c.DataInicio.Day == 29);
+            Assert.DoesNotContain(eventos, c => c.DataInicio.Day == 30);
         }
 
         [Fact]
