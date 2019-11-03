265e1ed1dfb24e4f67311fc2da18da151474f633
Flávio Ricardo <flavioricardo@live.com>
2019-11-03 16:17:42 -0300

    Permissionamento AB#7694

--- src/SME.SGP.WebClient/src/dtos/rotasDto.js
+++ src/SME.SGP.WebClient/src/dtos/rotasDto.js
@@ -1,17 +1,17 @@
 const RotasDto = {
   PRINCIPAL: '/',
-  PLANO_CICLO:'/planejamento/plano-ciclo',
-  PLANO_ANUAL:'/planejamento/plano-anual',
-  PERIODOS_ESCOLARES:'/calendario-escolar/periodos-escolares',
-  TIPO_EVENTOS:'/calendario-escolar/tipo-eventos',
-  ATRIBUICAO_SUPERVISOR_LISTA:'/gestao/atribuicao-supervisor-lista',
-  MEUS_DADOS:'/meus-dados',
+  PLANO_CICLO: '/planejamento/plano-ciclo',
+  PLANO_ANUAL: '/planejamento/plano-anual',
+  PERIODOS_ESCOLARES: '/calendario-escolar/periodos-escolares',
+  TIPO_EVENTOS: '/calendario-escolar/tipo-eventos',
+  ATRIBUICAO_SUPERVISOR_LISTA: '/gestao/atribuicao-supervisor-lista',
+  MEUS_DADOS: '/meus-dados',
   NOTIFICACOES: '/notificacoes',
   REINICIAR_SENHA: '/usuarios/reiniciar-senha',
+  CALENDARIO_ESCOLAR: '/calendario-escolar/',
   TIPO_CALENDARIO_ESCOLAR: '/calendario-escolar/tipo-calendario-escolar',
   TIPO_FERIADO: '/calendario-escolar/tipo-feriado',
-  EVENTOS:'/calendario-escolar/eventos'
-}
+  EVENTOS: '/calendario-escolar/eventos',
+};
 
 export default RotasDto;
-
