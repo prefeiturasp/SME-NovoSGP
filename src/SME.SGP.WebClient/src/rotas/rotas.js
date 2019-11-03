e3d1dd68620162cb4615a1034430fae0f8f41fc1
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2019-11-03 17:07:09 -0300

    cadastro aula unica - calendario professor AB#7135 AB#4742

--- src/SME.SGP.WebClient/src/rotas/rotas.js
+++ src/SME.SGP.WebClient/src/rotas/rotas.js
@@ -31,6 +31,7 @@ import TipoEventosForm from '~/paginas/CalendarioEscolar/TipoEventos/tipoEventos
 import { useSelector } from 'react-redux';
 import SemPermissao from '~/paginas/SemPermissao/sem-permissao';
 import RotasDto from '~/dtos/rotasDto';
+import CadastroAula from '~/paginas/CalendarioEscolar/CadastroAula/cadastroAula';
 
 
 export default function Rotas() {
@@ -322,6 +323,35 @@ export default function Rotas() {
     tipo: RotasTipo.EstruturadaAutenticada,
     temPermissionamento: true,
     chavePermissao: RotasDto.EVENTOS
+  });
+
+  // TODO - Alterar quando tiver o calendário do professor
+  rotas.set(RotasDto.CALENDARIO_PROFESSOR, {
+    breadcrumbName: 'Calendário do Professor',
+    menu: ['Calendário Escolar'],
+    parent: '/',
+    component: Principal,
+    exact: true,
+    tipo: RotasTipo.EstruturadaAutenticada,
+    temPermissionamento: false
+  });
+
+  rotas.set(RotasDto.CADASTRO_DE_AULA, {
+      breadcrumbName: 'Cadastro de Aula',
+      parent: '/calendario-escolar/calendario-professor',
+      component: CadastroAula,
+      exact: true,
+      tipo: RotasTipo.EstruturadaAutenticada,
+      temPermissionamento: false
+  });
+
+  rotas.set(`${RotasDto.CADASTRO_DE_AULA}/:id`, {
+      breadcrumbName: 'Cadastro de Aula',
+      parent: RotasDto.CADASTRO_DE_AULA,
+      component: CadastroAula,
+      exact: true,
+      tipo: RotasTipo.EstruturadaAutenticada,
+      temPermissionamento: false
   });
 
   const rotasArray = [];
