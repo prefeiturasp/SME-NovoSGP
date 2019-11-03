11f9c06caa51fc85670a70f67bc7fa61ca412efd
eliton-amcom <eliton.santos@amcom.com.br>
2019-11-03 12:28:04 -0300

    Mapeamento AB#7132

---
+++ src/SME.SGP.Dados/Mapeamentos/AulaMap.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class AulaMap : BaseMap<Aula>
+    {
+        public AulaMap()
+        {
+            ToTable("aula");
+            Map(a => a.TipoAula).ToColumn("tipo_aula");
+            Map(a => a.DisciplinaId).ToColumn("disciplina_id");
+            Map(a => a.Quantidade).ToColumn("quantidade");
+            Map(a => a.Data).ToColumn("data");
+            Map(a => a.RecorrenciaAula).ToColumn("recorrencia_aula");
+            Map(a => a.Excluido).ToColumn("excluido");
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/Aula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SME.SGP.Dominio
+{
+    public class Aula : EntidadeBase
+    {
+        public TipoAula TipoAula { get; set; }
+        public int DisciplinaId { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime Data { get; set; }
+        public RecorrenciaAula RecorrenciaAula { get; set; }
+        public bool Excluido { get; set; }
+    }
+}
