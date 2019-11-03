6ed08da884cd20fb64dc482c7f90060c6f0b5d24
Flávio Ricardo <flavioricardo@live.com>
2019-11-03 17:22:32 -0300

    Permissionamento da tela de tipo de eventos AB#7694

--- src/SME.SGP.WebClient/src/dtos/rotasDto.js
+++ src/SME.SGP.WebClient/src/dtos/rotasDto.js
@@ -2,16 +2,16 @@ const RotasDto = {
   PRINCIPAL: '/',
   PLANO_CICLO: '/planejamento/plano-ciclo',
   PLANO_ANUAL: '/planejamento/plano-anual',
+  CALENDARIO_ESCOLAR: '/calendario-escolar',
+  TIPO_CALENDARIO_ESCOLAR: '/calendario-escolar/tipo-calendario-escolar',
   PERIODOS_ESCOLARES: '/calendario-escolar/periodos-escolares',
   TIPO_EVENTOS: '/calendario-escolar/tipo-eventos',
+  TIPO_FERIADO: '/calendario-escolar/tipo-feriado',
+  EVENTOS: '/calendario-escolar/eventos',
   ATRIBUICAO_SUPERVISOR_LISTA: '/gestao/atribuicao-supervisor-lista',
   MEUS_DADOS: '/meus-dados',
   NOTIFICACOES: '/notificacoes',
   REINICIAR_SENHA: '/usuarios/reiniciar-senha',
-  CALENDARIO_ESCOLAR: '/calendario-escolar/',
-  TIPO_CALENDARIO_ESCOLAR: '/calendario-escolar/tipo-calendario-escolar',
-  TIPO_FERIADO: '/calendario-escolar/tipo-feriado',
-  EVENTOS: '/calendario-escolar/eventos',
 };
 
 export default RotasDto;
