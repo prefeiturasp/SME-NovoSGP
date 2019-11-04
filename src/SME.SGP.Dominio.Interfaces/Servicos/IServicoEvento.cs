4acd5ed130b80d9a7b2f5174ecf2f71fc17d4bd8
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-04 16:20:44 -0300

    Recorrência de eventos AB#4622

--- src/SME.SGP.Dominio.Interfaces/Servicos/IServicoEvento.cs
+++ src/SME.SGP.Dominio.Interfaces/Servicos/IServicoEvento.cs
@@ -1,10 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IServicoEvento
     {
+        Task GravarRecorrencia(Evento evento, DateTime dataInicial, DateTime? dataFinal, int? diaDeOcorrencia, IEnumerable<DayOfWeek> diasDaSemana, PadraoRecorrencia padraoRecorrencia, PadraoRecorrenciaMensal? padraoRecorrenciaMensal, int repeteACada);
+
         Task Salvar(Evento evento);
+
         Task SalvarEventoFeriadosAoCadastrarTipoCalendario(TipoCalendario tipoCalendario);
     }
 }
