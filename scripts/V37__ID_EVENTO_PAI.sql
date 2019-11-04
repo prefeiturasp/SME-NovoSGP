ceb00d9490ab4b9e4a05e894ebf06077097f5ca3
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-04 17:20:18 -0300

    Ajuste no script de eventos AB#4622

--- scripts/V37__ID_EVENTO_PAI.sql
+++ scripts/V37__ID_EVENTO_PAI.sql
@@ -1,2 +1,3 @@
-ALTER TABLE evento ADD evento_pai_id bigint NULL;
-ALTER TABLE evento ADD CONSTRAINT evento_fk FOREIGN KEY (evento_pai_id) REFERENCES evento(id);
+ALTER TABLE if exists evento ADD if not exists evento_pai_id bigint NULL;
+
+select f_cria_fk_se_nao_existir('evento', 'evento_fk', 'FOREIGN KEY (evento_pai_id) REFERENCES evento(id)');
