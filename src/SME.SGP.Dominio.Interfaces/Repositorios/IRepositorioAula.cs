6ded8ebab8a9a864abd61c96d0ecba495820e817
Eliton Santos <eliton.santos@amcom.com.br>
2019-11-04 10:36:27 -0300

    altearções repository

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAula.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAula.cs
@@ -1,10 +1,6 @@
-﻿using SME.SGP.Infra;
-using System.Collections.Generic;
-
-namespace SME.SGP.Dominio.Interfaces
+﻿namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioAula : IRepositorioBase<Aula>
     {
-        IEnumerable<AulaConsultaDto> ObterAulasPorFiltro(FiltroAulaDto dto);
     }
 }
