b0edecb53fe072a5cb9471639267e3e74c2b3179
Flávio Ricardo <flavioricardo@live.com>
2019-11-12 17:46:51 -0300

    Consumo de endpoint de dias letivos e ajustes no calendário AB#8171

--- src/SME.SGP.WebClient/src/componentes-sgp/Calendario/Mes.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/Calendario/Mes.js
@@ -1,6 +1,4 @@
-﻿/* eslint-disable no-param-reassign */
-/* eslint-disable no-return-assign */
-import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -8,6 +6,7 @@ import { store } from '~/redux';
 import {
   selecionaMes,
   atribuiEventosMes,
+  selecionaDia,
 } from '~/redux/modulos/calendarioEscolar/actions';
 import { Base } from '~/componentes/colors';
 import api from '~/servicos/api';
@@ -23,13 +22,20 @@ const Div = styled.div`
 `;
 const Icone = styled.i`
   cursor: pointer;
+  ${props =>
+    props.disabled &&
+    `
+    cursor: not-allowed;
+    pointer-events: none;
+  `}
 `;
 
 const Seta = props => {
-  const { estaAberto } = props;
+  const { estaAberto, disabled } = props;
 
   return (
     <Icone
+      disabled={disabled}
       className={`stretched-link fas ${
         estaAberto ? 'fa-chevron-down' : 'fa-chevron-right text-white'
       } `}
@@ -39,10 +45,12 @@ const Seta = props => {
 
 Seta.propTypes = {
   estaAberto: PropTypes.bool,
+  disabled: PropTypes.bool,
 };
 
 Seta.defaultProps = {
   estaAberto: false,
+  disabled: true,
 };
 
 const Mes = props => {
@@ -104,11 +112,32 @@ const Mes = props => {
   }, [meses]);
 
   const abrirMes = () => {
-    if (filtros && Object.entries(filtros).length > 0) {
+    if (
+      filtros &&
+      Object.entries(filtros).length > 0 &&
+      meses[numeroMes].eventos > 0
+    ) {
       const { tipoCalendarioSelecionado = '' } = filtros;
       if (tipoCalendarioSelecionado) store.dispatch(selecionaMes(numeroMes));
     }
   };
+
+  useEffect(() => {
+    const encontrarMes = setTimeout(() => {
+      const mes = document.querySelector(`.${meses[numeroMes].nome}`);
+      if (mes) {
+        if (meses[numeroMes].estaAberto) {
+          mes.classList.remove('d-none');
+          mes.classList.add('d-block', 'show');
+        } else {
+          mes.classList.remove('d-block', 'show');
+          mes.classList.add('d-none');
+          store.dispatch(selecionaDia(undefined));
+        }
+      }
+    }, 500);
+    return () => clearTimeout(encontrarMes);
+  }, [meses[numeroMes].estaAberto]);
 
   return (
     <Div
@@ -125,7 +154,12 @@ const Mes = props => {
             width: 35,
           }}
         >
-          <Seta estaAberto={mesSelecionado.estaAberto} />
+          <Seta
+            estaAberto={mesSelecionado.estaAberto}
+            disabled={
+              !mesSelecionado.estaAberto && mesSelecionado.eventos === 0
+            }
+          />
         </Div>
         <Div
           className="d-flex align-items-center w-100"
