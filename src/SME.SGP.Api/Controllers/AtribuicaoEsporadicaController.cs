9ceab70cdf6c901d749b9cd55af5a82e4bdf3ff2
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-11-13 20:18:43 -0300

    ...

---
+++ src/SME.SGP.Api/Controllers/AtribuicaoEsporadicaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/atribuicao/esporadica")]
+    [ValidaDto]
+    public class AtribuicaoEsporadicaController : ControllerBase
+    {
+        // public IActionResult
+    }
+}
