40a13bffa50a1fdf84dd986cdf70fb12f19a78c0
Eliton Santos <eliton.santos@amcom.com.br>
2019-11-14 16:07:14 -0300

    Layout

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/PlanoAula/plano-aula.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/PlanoAula/plano-aula.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import CardCollapse from '~/componentes/cardCollapse';
 import styled from 'styled-components';
 import TextEditor from '~/componentes/textEditor/component';
+import Grid from '~/componentes/grid';
+import { Base } from '~/componentes';
 
 const PlanoAula = () => {
   const [mostrarCardPrincipal, setMostrarCardPrincipal] = useState(true);
   const [quantidadeAulas, setQuantidadeAulas] = useState(0);
   const [planoAula, setPlanoAula] = useState({
-    objetivosAprendizagem: null,
+    objetivosEspecificos: 'teste',
     desenvolvimentoAula: 'teste',
     recuperacaoContinua: 'teste',
     licaoCasa: 'teste'
@@ -17,8 +19,41 @@ const PlanoAula = () => {
     corBorda: '#4072d6'
   }
 
+  const [objetivosAprendizagem, setObjetivosAprendizagem] = useState(
+    [
+      {
+        id: 1,
+        selected: false,
+        codigo: 'EF45644',
+        descricao: 'Teste de descrição'
+      },
+      {
+        id: 2,
+        selected: false,
+        codigo: 'EF45645',
+        descricao: 'Teste de descrição, teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste'
+      },
+    ]
+  );
+
   const QuantidadeBotoes = styled.div`
     padding: 0 0 20px 0;
+  `;
+
+  const ObjetivosList = styled.div`
+    max-height: 300px !important;
+  `;
+
+  const ListItem = styled.li`
+    border-color: ${Base.AzulAnakiwa} !important;
+  `;
+
+  const ListItemButton = styled(ListItem)`
+    cursor: pointer;
+
+    &[aria-pressed='true'] {
+      background: ${Base.AzulAnakiwa} !important;
+    }
   `;
 
   return (
@@ -40,6 +75,61 @@ const PlanoAula = () => {
         show={true}
         configCabecalho={configCabecalho}
       >
+        <div className="row">
+          <Grid cols={6}>
+            <h6 className="d-inline-block font-weight-bold my-0 fonte-14">
+              Objetivos de aprendizagem
+            </h6>
+            <ObjetivosList className="mt-4 overflow-auto">
+              {objetivosAprendizagem &&
+                objetivosAprendizagem.length > 0
+                ? objetivosAprendizagem.map((objetivo, index) => {
+                  return (
+                    <ul
+                      key={`${objetivo.id}Bimestre${index}`}
+                      className="list-group list-group-horizontal mt-3"
+                    >
+                      <ListItemButton
+                        className="list-group-item d-flex align-items-center font-weight-bold fonte-14"
+                        role="button"
+                        id={objetivo.id}
+                        aria-pressed={objetivo.selected ? true : false}
+                        data-index={index}
+                        onClick={()=>{}}
+                        onKeyUp={()=>{}}
+                        alt={`Codigo do Objetivo : ${objetivo.codigo} `}
+                      >
+                        {objetivo.codigo}
+                      </ListItemButton>
+                      <ListItem
+                        alt={objetivo.descricao}
+                        className="list-group-item flex-fill p-2 fonte-12"
+                      >
+                        {objetivo.descricao}
+                      </ListItem>
+                    </ul>
+                  );
+                })
+                : null}
+            </ObjetivosList>
+          </Grid>
+          <Grid cols={6}>
+            <h6 className="d-inline-block font-weight-bold my-0 fonte-14">
+              Meus objetivos específicos
+          </h6>
+            <fieldset className="mt-3">
+              <form action="">
+                <TextEditor
+                  className="form-control"
+                  id="textEditor-meus_objetivos"
+                  height="135px"
+                  alt="Meus objetivos específicos"
+                  value={planoAula.objetivosEspecificos}
+                />
+              </form>
+            </fieldset>
+          </Grid>
+        </div>
       </CardCollapse>
 
       <CardCollapse
