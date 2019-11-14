3588aff5efa6b2822b3878f8ac49cf8d29ff5c9e
Rafael Losi <rafael.losi@amcom.com.br>
2019-11-14 10:11:14 -0300

    7915 - Ajuste dos Namespaces

--- src/SME.SGP.Aplicacao/Comandos/ComandosGrade.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosGrade.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SME.SGP.Aplicacao.Comandos
+namespace SME.SGP.Aplicacao
 {
     public class ComandosGrade: IComandosGrade
     {
--- src/SME.SGP.Infra/Dtos/GradeDto.cs
+++ src/SME.SGP.Infra/Dtos/GradeDto.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace SME.SGP.Infra.Dtos
+namespace SME.SGP.Infra
 {
     public class GradeDto
     {
