483c2c64ebc6b2573642237e4e3c18fed3b69716
Eliton Santos <eliton.santos@amcom.com.br>
2019-11-14 13:53:40 -0300

    Criação de text-editors

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/PlanoAula/plano-aula.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/PlanoAula/plano-aula.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react';
 import CardCollapse from '~/componentes/cardCollapse';
+import styled from 'styled-components';
+import TextEditor from '~/componentes/textEditor/component';
 
 const PlanoAula = () => {
   const [mostrarCardPrincipal, setMostrarCardPrincipal] = useState(true);
   const [quantidadeAulas, setQuantidadeAulas] = useState(0);
+  const [planoAula, setPlanoAula] = useState({
+    objetivosAprendizagem: null,
+    desenvolvimentoAula: 'teste',
+    recuperacaoContinua: 'teste',
+    licaoCasa: 'teste'
+  })
+  const configCabecalho = {
+    altura: '44px',
+    corBorda: '#4072d6'
+  }
+
+  const QuantidadeBotoes = styled.div`
+    padding: 0 0 20px 0;
+  `;
+
   return (
     <CardCollapse
       key="plano-aula"
@@ -12,19 +29,80 @@ const PlanoAula = () => {
       indice={'Plano de aula'}
       show={mostrarCardPrincipal}
     >
-      <span>Quantidade de aulas: {quantidadeAulas}</span>
+      <QuantidadeBotoes className="col-md-12">
+        <span>Quantidade de aulas: {quantidadeAulas}</span>
+      </QuantidadeBotoes>
       <CardCollapse
-        key="teste"
+        key="objetivos-aprendizagem"
         onClick={() => { }}
         titulo={'Objetivos de aprendizagem e meus objetivos (Currículo da Cidade)'}
-        indice={'teste'}
+        indice={'objetivos-aprendizagem'}
         show={true}
-        configCabecalho={{
-          altura: '44px',
-          corBorda: '#4072d6'
-        }}
+        configCabecalho={configCabecalho}
       >
+      </CardCollapse>
 
+      <CardCollapse
+        key="desenv-aula"
+        onClick={() => { }}
+        titulo={'Desenvolvimento da aula'}
+        indice={'desenv-aula'}
+        show={true}
+        configCabecalho={configCabecalho}
+      >
+        <fieldset className="mt-3">
+          <form action="">
+            <TextEditor
+              className="form-control"
+              id="textEditor-desenv-aula"
+              height="135px"
+              alt="Desenvolvimento da aula"
+              value={planoAula.desenvolvimentoAula}
+            />
+          </form>
+        </fieldset>
+      </CardCollapse>
+
+      <CardCollapse
+        key="rec-continua"
+        onClick={() => { }}
+        titulo={'Recuperação contínua'}
+        indice={'rec-continua'}
+        show={true}
+        configCabecalho={configCabecalho}
+      >
+        <fieldset className="mt-3">
+          <form action="">
+            <TextEditor
+              className="form-control"
+              id="textEditor-rec-=continua"
+              height="135px"
+              alt="Recuperação contínua"
+              value={planoAula.recuperacaoContinua}
+            />
+          </form>
+        </fieldset>
+      </CardCollapse>
+
+      <CardCollapse
+        key="licao-casa"
+        onClick={() => { }}
+        titulo={'Lição de casa'}
+        indice={'licao-casa'}
+        show={true}
+        configCabecalho={configCabecalho}
+      >
+        <fieldset className="mt-3">
+          <form action="">
+            <TextEditor
+              className="form-control"
+              id="textEditor-licao-casa"
+              height="135px"
+              alt="Lição de casa"
+              value={planoAula.licaoCasa}
+            />
+          </form>
+        </fieldset>
       </CardCollapse>
     </CardCollapse>
   )
