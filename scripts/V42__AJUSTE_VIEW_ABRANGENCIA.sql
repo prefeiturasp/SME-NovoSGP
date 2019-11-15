85d3899b4b856676aab4ed5404b9748daa4b18b5
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-15 10:58:24 -0300

    Ajustes de code review AB#4742

--- scripts/V42__AJUSTE_VIEW_ABRANGENCIA.sql
+++ scripts/V42__AJUSTE_VIEW_ABRANGENCIA.sql
@@ -1,3 +1,4 @@
+DROP VIEW public.v_abrangencia;
 CREATE OR REPLACE VIEW public.v_abrangencia
 AS SELECT ab_dres.dre_id AS dre_codigo,
     ab_dres.abreviacao AS dre_abreviacao,
@@ -9,14 +10,11 @@ AS SELECT ab_dres.dre_id AS dre_codigo,
     ab_turmas.modalidade_codigo,
     ab_turmas.nome AS turma_nome,
     ab_turmas.semestre AS turma_semestre,
+    ab_turmas.qt_duracao_aula,
+    ab_turmas.tipo_turno,
     ab_dres.usuario_id,
     ab_dres.perfil AS usuario_perfil,
     ab_turmas.turma_id AS turma_id
    FROM abrangencia_dres ab_dres
      JOIN abrangencia_ues ab_ues ON ab_ues.abrangencia_dres_id = ab_dres.id
      JOIN abrangencia_turmas ab_turmas ON ab_turmas.abrangencia_ues_id = ab_ues.id;
-
--- Permissions
-
-ALTER TABLE public.v_abrangencia OWNER TO postgres;
-GRANT ALL ON TABLE public.v_abrangencia TO postgres;
