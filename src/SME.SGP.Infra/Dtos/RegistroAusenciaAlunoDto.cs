ed7d65323f168d7860150147814cb49aad635b42
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-15 14:59:24 -0300

    Iniciando serviço de frequencia AB#4151

---
+++ src/SME.SGP.Infra/Dtos/RegistroAusenciaAlunoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SME.SGP.Infra.Dtos
+{
+    public class RegistroAusenciaAlunoDto
+    {
+        public string CodigoAluno { get; set; }
+        public bool Compareceu { get; set; }
+        public DateTime DataAula { get; set; }
+        public string NomeAluno { get; set; }
+        public int NumeroAula { get; set; }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/RegistroFrequenciaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SME.SGP.Infra
+{
+    public class RegistroFrequenciaDto
+    {
+        public long AulaId { get; set; }
+        public string CodigoAluno { get; set; }
+        public DateTime DataAula { get; set; }
+        public string DisciplinaId { get; set; }
+        public bool Migrado { get; set; }
+        public int NumeroAula { get; set; }
+        public long RegistroFrequenciaId { get; set; }
+        public long TipoCalendarioId { get; set; }
+        public string TurmaId { get; set; }
+    }
+}
