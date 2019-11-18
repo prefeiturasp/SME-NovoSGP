badde5e56c01e413577fe87221c9f42179b34ad9
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-18 10:12:58 -0300

    Renomeando metodos AB#4151

--- src/SME.SGP.Api/Controllers/FrequenciaController.cs
+++ src/SME.SGP.Api/Controllers/FrequenciaController.cs
@@ -15,6 +15,7 @@ namespace SME.SGP.Api.Controllers
         [HttpGet]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 200)]
         [Permissao(Permissao.PDA_C, Policy = "Bearer")]
         public async Task<IActionResult> Listar(long aulaId, [FromServices] IConsultasFrequencia consultasFrequencia)
         {
