5b4d9c049ea8693431d8bea5f0281203273efe3d
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-18 00:46:20 -0300

    Iniciando persistencia de frequencia AB#4151

--- src/SME.SGP.Aplicacao/Comandos/ComandoFrequencia.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandoFrequencia.cs
@@ -1,8 +1,22 @@
 ﻿using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
 
 namespace SME.SGP.Aplicacao
 {
     public class ComandoFrequencia : IComandoFrequencia
     {
+        private readonly IRepositorioFrequencia repositorioFrequencia;
+
+        public ComandoFrequencia(IRepositorioFrequencia repositorioFrequencia)
+        {
+            this.repositorioFrequencia = repositorioFrequencia ?? throw new System.ArgumentNullException(nameof(repositorioFrequencia));
+        }
+
+        public void Registrar(FrequenciaDto frequenciaDto)
+        {
+            var registro = repositorioFrequencia.ObterListaFrequenciaPorAula(frequenciaDto.AulaId);
+            registro
+        }
     }
 }
