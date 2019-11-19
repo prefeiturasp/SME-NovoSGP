4e34fbfbc1887327e307d48247fed51e3e76a127
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-11-19 20:38:49 -0300

    Update RepositorioAtribuicaoEsporadica.cs

--- src/SME.SGP.Dados/Repositorios/RepositorioAtribuicaoEsporadica.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioAtribuicaoEsporadica.cs
@@ -1,12 +1,18 @@
 ﻿using SME.SGP.Dados.Contexto;
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
 {
     public class RepositorioAtribuicaoEsporadica : RepositorioBase<AtribuicaoEsporadica>, IRepositorioAtribuicaoEsporadica
     {
         public RepositorioAtribuicaoEsporadica(ISgpContext database) : base(database)
+        {
+        }
+
+        public async Task<PaginacaoResultadoDto<AtribuicaoEsporadica>> ListarAtribuicoes(int anoLetivo, string dreId, string ueId, string codigoRF = "")
         {
         }
     }
