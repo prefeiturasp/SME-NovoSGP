3ea81601779d40fb8db01a9c3df794fd51d0f3f4
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-18 23:24:24 -0300

    Ajustes de campos obrigatórios AB#4151

--- src/SME.SGP.Infra/Dtos/RegistroFrequenciaAlunoDto.cs
+++ src/SME.SGP.Infra/Dtos/RegistroFrequenciaAlunoDto.cs
@@ -1,5 +1,6 @@
 ﻿using SME.SGP.Dominio;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SME.SGP.Infra
 {
@@ -10,12 +11,16 @@ namespace SME.SGP.Infra
             Aulas = new List<FrequenciaAulaDto>();
         }
 
+        [ListaTemElementos(ErrorMessage = "A lista de aulas é obrigatória")]
         public List<FrequenciaAulaDto> Aulas { get; set; }
+
+        [Required(ErrorMessage = "O código do aluno é obrigatório")]
         public string CodigoAluno { get; set; }
+
         public SituacaoMatriculaAluno CodigoSituacaoMatricula { get; set; }
+        public bool Desabilitado { get; set; }
         public string NomeAluno { get; set; }
         public int NumeroAlunoChamada { get; set; }
         public string SituacaoMatricula { get; set; }
-        public bool Desabilitado { get; set; }
     }
 }
