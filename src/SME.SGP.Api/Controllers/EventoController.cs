5e5cb50d5d934ebaf4f3e3506d8dcf68c0ba36d0
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-11-20 20:31:42 -0300

    AB#8493

--- src/SME.SGP.Api/Controllers/EventoController.cs
+++ src/SME.SGP.Api/Controllers/EventoController.cs
@@ -26,8 +26,7 @@ namespace SME.SGP.Api.Controllers
         [HttpPost]
         [ProducesResponseType(typeof(IEnumerable<RetornoCopiarEventoDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        //[Permissao(Permissao.E_I, Policy = "Bearer")]
-        [AllowAnonymous]
+        [Permissao(Permissao.E_I, Policy = "Bearer")]
         public async Task<IActionResult> Criar([FromServices]IComandosEvento comandosEvento, [FromBody]EventoDto eventoDto)
         {
             return Ok(await comandosEvento.Criar(eventoDto));
