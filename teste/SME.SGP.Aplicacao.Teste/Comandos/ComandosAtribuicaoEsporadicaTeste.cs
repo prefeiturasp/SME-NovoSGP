4e953a214faded344aa62b0482704241a63efa3b
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-11-20 16:00:51 -0300

    Endpoint para remover atribuição esporádica; Função no comando para remover; Função no domínio para remover;

---
+++ teste/SME.SGP.Aplicacao.Teste/Comandos/ComandosAtribuicaoEsporadicaTeste.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.Aplicacao.Teste.Comandos
+{
+    public class ComandosAtribuicaoEsporadicaTeste
+    {
+        private readonly ComandosAtribuicaoEsporadica comandosAtribuicaoEsporadica;
+        private readonly Mock<IRepositorioAtribuicaoEsporadica> repositorioAtribuicaoEsporadica;
+        private readonly Mock<IServicoAtribuicaoEsporadica> servicoAtribuicaoEsporadica;
+
+        public ComandosAtribuicaoEsporadicaTeste()
+        {
+            repositorioAtribuicaoEsporadica = new Mock<IRepositorioAtribuicaoEsporadica>();
+            servicoAtribuicaoEsporadica = new Mock<IServicoAtribuicaoEsporadica>();
+            comandosAtribuicaoEsporadica = new ComandosAtribuicaoEsporadica(repositorioAtribuicaoEsporadica.Object, servicoAtribuicaoEsporadica.Object);
+        }
+
+        [Fact(DisplayName = "Deve Excluir Atribuicao")]
+        public async void Deve_Excluir_Atribuicao()
+        {
+            //ARRANGE
+            var atribuicaoEsporadica = new AtribuicaoEsporadica() { Id = 1, Excluido = false };
+            repositorioAtribuicaoEsporadica.Setup(a => a.ObterPorId(atribuicaoEsporadica.Id)).Returns(atribuicaoEsporadica);
+
+            //ACT
+            await comandosAtribuicaoEsporadica.Excluir(1);
+
+            //ASSERT
+            repositorioAtribuicaoEsporadica.Verify(c => c.SalvarAsync(atribuicaoEsporadica), Times.Once);
+        }
+
+        [Fact(DisplayName = "Não Deve Excluir Atribuicao excluida")]
+        public async Task Nao_Deve_Excluir_Atribuicao_Excluida()
+        {
+            //ARRANGE
+            var atribuicaoEsporadica = new AtribuicaoEsporadica() { Id = 1, Excluido = true };
+            repositorioAtribuicaoEsporadica.Setup(a => a.ObterPorId(atribuicaoEsporadica.Id)).Returns(atribuicaoEsporadica);
+
+            //ACT & ASSERT
+            await Assert.ThrowsAsync<NegocioException>(() => comandosAtribuicaoEsporadica.Excluir(1));
+        }
+    }
+}
--- teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasAulasTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasAulasTeste.cs
@@ -4,7 +4,6 @@ using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -23,31 +22,6 @@ namespace SME.SGP.Aplicacao.Teste.Consultas
             Setup();
         }
 
-        private void Setup()
-        {
-            // Mock para testar o metodo ObterPorId
-            var aula = new Aula()
-            {
-                Id = 1,
-                DataAula = new DateTime(2019, 11, 15),
-                ProfessorId = 123,
-                Quantidade = 3
-            };
-
-            repositorio.Setup(c => c.ObterPorId(It.IsAny<long>()))
-                .Returns(aula);
-
-            // Mock das aulas por turma e disciplina
-            IEnumerable<AulasPorTurmaDisciplinaDto> aulas = new List<AulasPorTurmaDisciplinaDto>()
-            {
-                new AulasPorTurmaDisciplinaDto() { ProfessorId = 1, Quantidade = 1, DataAula = new System.DateTime(2019,11,12) },
-                new AulasPorTurmaDisciplinaDto() { ProfessorId = 1, Quantidade = 3, DataAula = new System.DateTime(2019,11,15) },
-            };
-
-            repositorio.Setup(c => c.ObterAulasTurmaDisciplina(It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(Task.FromResult(aulas));
-        }
-
         [Fact]
         public async Task DeveObterAulaPorId()
         {
@@ -64,6 +38,31 @@ namespace SME.SGP.Aplicacao.Teste.Consultas
             var qtd = await consultas.ObterQuantidadeAulasTurma("123", "7");
 
             Assert.True(qtd == 4);
+        }
+
+        private void Setup()
+        {
+            // Mock para testar o metodo ObterPorId
+            var aula = new Aula()
+            {
+                Id = 1,
+                DataAula = new DateTime(2019, 11, 15),
+                //ProfessorId = 123,
+                Quantidade = 3
+            };
+
+            repositorio.Setup(c => c.ObterPorId(It.IsAny<long>()))
+                .Returns(aula);
+
+            // Mock das aulas por turma e disciplina
+            IEnumerable<AulasPorTurmaDisciplinaDto> aulas = new List<AulasPorTurmaDisciplinaDto>()
+            {
+                new AulasPorTurmaDisciplinaDto() { ProfessorId = 1, Quantidade = 1, DataAula = new System.DateTime(2019,11,12) },
+                new AulasPorTurmaDisciplinaDto() { ProfessorId = 1, Quantidade = 3, DataAula = new System.DateTime(2019,11,15) },
+            };
+
+            repositorio.Setup(c => c.ObterAulasTurmaDisciplina(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(aulas));
         }
     }
 }
