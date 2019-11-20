1709b5d7e881fb54695a79c1ec46db3d34685600
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-11-19 21:34:15 -0300

    Ajuste no script

--- scripts/V54__NOTIFICACAO_MENSAGEM.sql
+++ scripts/V54__NOTIFICACAO_MENSAGEM.sql
@@ -1,1 +1,2 @@
-alter table notificacao alter column mensagem type varchar 
+alter table notificacao alter column titulo type varchar(200) ;
+alter table notificacao alter column mensagem type varchar ;
