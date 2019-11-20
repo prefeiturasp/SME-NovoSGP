d0ae0791a6fcc3b540c23334d81a42bc517f0456
Alexsander Camargo <csalexsander@gmail.com>
2019-11-20 18:41:47 -0300

    Endpoint de listagem
    AB#7931

--- src/SME.SGP.Api/Controllers/AtribuicaoEsporadicaController.cs
+++ src/SME.SGP.Api/Controllers/AtribuicaoEsporadicaController.cs
@@ -17,6 +17,7 @@ namespace SME.SGP.Api.Controllers
         [HttpGet("listar")]
         [ProducesResponseType(typeof(PaginacaoResultadoDto<AtribuicaoEsporadicaDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Permissao(Permissao.AE_C, Policy = "Bearer")]
         public async Task<IActionResult> Listar([FromQuery]FiltroAtribuicaoEsporadicaDto filtro, [FromServices]IConsultasAtribuicaoEsporadica consultasAtribuicaoEsporadica)
         {      
             return Ok(await consultasAtribuicaoEsporadica.Listar(filtro));
