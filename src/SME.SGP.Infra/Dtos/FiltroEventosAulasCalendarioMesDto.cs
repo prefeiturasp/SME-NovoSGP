2412becfd12946cc4ebcf189dcd9d45a481f66b3
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-11-20 11:05:58 -0300

    filtro

---
+++ src/SME.SGP.Infra/Dtos/FiltroEventosAulasCalendarioMesDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroEventosAulasCalendarioMesDto : FiltroEventosAulasCalendarioDto
+    {
+        [Required(ErrorMessage = "O campo Mês é obrigatório")]
+        public int Mes { get; set; }
+    }
+}
