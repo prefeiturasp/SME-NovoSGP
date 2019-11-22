6bf4a8d3d6edc700acce7b3dd71f1983e4dca177
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-11-22 15:27:37 -0300

    Endpoint de autocomplete de professor
    AB#8014

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasProfessorTurma.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasProfessorTurma.cs
@@ -8,6 +8,8 @@ namespace SME.SGP.Aplicacao
     {
         IEnumerable<ProfessorTurmaDto> Listar(string codigoRf);
 
+        Task<IEnumerable<ProfessorResumoDto>> ObterResumoAutoComplete(int anoLetivo, string dreId, string nomeProfessor);
+
         Task<ProfessorResumoDto> ObterResumoPorRFAnoLetivo(string codigoRF, int anoLetivo);
 
         Task<IEnumerable<TurmaDto>> ObterTurmasAtribuidasAoProfessorPorEscolaEAnoLetivo(string rfProfessor, string codigoEscola, int anoLetivo);
