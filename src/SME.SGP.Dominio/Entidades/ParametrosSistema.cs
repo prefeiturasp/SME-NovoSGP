6621d69d0e51d7fcb7226ee849aa16723fa132c8
Everton Nogueira <everton.nogueira@amcom.com.br>
2019-11-22 15:43:05 -0300

    Adicionando flag de ativo nos parametros AB#4153

--- src/SME.SGP.Dominio/Entidades/ParametrosSistema.cs
+++ src/SME.SGP.Dominio/Entidades/ParametrosSistema.cs
@@ -3,6 +3,7 @@
     public class ParametrosSistema : EntidadeBase
     {
         public int? Ano { get; set; }
+        public bool Ativo { get; set; }
         public string Descricao { get; set; }
         public string Nome { get; set; }
         public TipoParametroSistema Tipo { get; set; }
