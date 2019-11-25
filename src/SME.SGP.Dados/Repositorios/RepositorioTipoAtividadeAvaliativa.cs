51e72de528d4dfc160390555457682e486266bae
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-11-25 15:21:27 -0300

    ABB#8640

--- src/SME.SGP.Dados/Repositorios/RepositorioTipoAtividadeAvaliativa.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioTipoAtividadeAvaliativa.cs
@@ -1,48 +1,13 @@
 ﻿using SME.SGP.Dados.Contexto;
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
 {
-    public class RepositorioTipoAtividadeAvaliativa : IRepositorioTipoAtividadeAvaliativa
+    public class RepositorioTipoAtividadeAvaliativa : RepositorioBase<TipoAtividadeAvaliativa>, IRepositorioTipoAtividadeAvaliativa
     {
-        protected readonly ISgpContext database;
-
-        public RepositorioTipoAtividadeAvaliativa(ISgpContext database)
+        public RepositorioTipoAtividadeAvaliativa(ISgpContext conexao) : base(conexao)
         {
-            this.database = database;
-        }
-
-        public IEnumerable<TipoAtividadeAvaliativa> Listar()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public TipoAtividadeAvaliativa ObterPorId(long id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Remover(long id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Remover(TipoAtividadeAvaliativa entidade)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public long Salvar(TipoAtividadeAvaliativa entidade)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<long> SalvarAsync(TipoAtividadeAvaliativa entidade)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
