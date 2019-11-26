e82305e48d2cc6c799042c000d5c6ee28917ed56
Marcos Lobo Pereira Costa <marcos.costa@amcom.com.br>
2019-11-26 11:56:42 -0300

    Endpoint mockado de atribuicao de CJ

---
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJListaRetornoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SME.SGP.Infra
+{
+    public class AtribuicaoCJListaRetornoDto
+    {
+        public string[] Disciplinas { get; set; }
+        public long Id { get; set; }
+        public string Modalidade { get; set; }
+        public string Turma { get; set; }
+    }
+}
