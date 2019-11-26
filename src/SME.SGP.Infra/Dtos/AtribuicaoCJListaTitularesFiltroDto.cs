9ca838b0cd30ace03c33d161053057a4cf9829ab
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-11-26 17:12:28 -0300

    Ajustes finais do mock para incluir auditoria

--- src/SME.SGP.Infra/Dtos/AtribuicaoCJListaTitularesFiltroDto.cs
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJListaTitularesFiltroDto.cs
@@ -4,7 +4,9 @@ namespace SME.SGP.Infra
 {
     public class AtribuicaoCJListaTitularesFiltroDto
     {
+        [EnumeradoRequirido(ErrorMessage = "É necessário informar a modalidade.")]
         public Modalidade Modalidade { get; set; }
+
         public string TurmaId { get; set; }
         public string UeId { get; set; }
         public string UsuarioRf { get; set; }
