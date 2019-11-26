73080cf2ecd957ee1abf883457270cf179a2653c
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-11-26 16:07:40 -0300

    Endpoint inicial

---
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJListaTitularesFiltroDto.cs
@@ -0,0 +1,12 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Infra
+{
+    public class AtribuicaoCJListaTitularesFiltroDto
+    {
+        public Modalidade Modalidade { get; set; }
+        public string TurmaId { get; set; }
+        public string UeId { get; set; }
+        public string UsuarioRf { get; set; }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJListaTitularesRetornoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SME.SGP.Infra
+{
+    public class AtribuicaoCJListaTitularesRetornoDto
+    {
+        public string Disciplina { get; set; }
+        public string DisciplinaId { get; set; }
+        public string ProfessorTitular { get; set; }
+        public string ProfessorTitularRf { get; set; }
+        public bool Substituir { get; set; }
+    }
+}
