6579f1c588942d91cf1cfafa184c26659bb8c897
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-11-26 17:12:28 -0300

    Ajustes finais do mock para incluir auditoria

--- src/SME.SGP.Infra/Dtos/AtribuicaoCJTitularesRetornoItemDto.cs
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJTitularesRetornoItemDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace SME.SGP.Infra
 {
-    public class AtribuicaoCJListaTitularesRetornoDto
+    public class AtribuicaoCJTitularesRetornoItemDto
     {
-        public string Disciplina { get; set; }
-        public string DisciplinaId { get; set; }
+        public string ComponenteCurricular { get; set; }
+        public string ComponenteCurricularId { get; set; }
         public string ProfessorTitular { get; set; }
         public string ProfessorTitularRf { get; set; }
         public bool Substituir { get; set; }
