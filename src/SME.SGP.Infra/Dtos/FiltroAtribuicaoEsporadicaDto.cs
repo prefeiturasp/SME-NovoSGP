0b9c23d9655ad774e5525cfdfabf1b4b5e8eac86
Alexsander Camargo <alexsander.camargo@amcom.com.br>
2019-11-28 17:12:20 -0300

    Update FiltroAtribuicaoEsporadicaDto.cs

--- src/SME.SGP.Infra/Dtos/FiltroAtribuicaoEsporadicaDto.cs
+++ src/SME.SGP.Infra/Dtos/FiltroAtribuicaoEsporadicaDto.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SME.SGP.Infra
 {
     public class FiltroAtribuicaoEsporadicaDto
     {
-        [Required(ErrorMessage = "É necessario informar o Id da DRE")]
-        public string DreId { get; set; }
-        [Required(ErrorMessage = "É necessario informar o Id da UE")]
-        public string UeId { get; set; }
-        public string CodigoRF { get; set; }
         [Range(0, double.MaxValue, ErrorMessage = "É necessario informar o ano letivo")]
         public int AnoLetivo { get; set; }
+
+        public string CodigoRF { get; set; }
+
+        [Required(ErrorMessage = "É necessario informar o Id da DRE")]
+        public string DreId { get; set; }
+
+        [Required(ErrorMessage = "É necessario informar o Id da UE")]
+        public string UeId { get; set; }
     }
 }
