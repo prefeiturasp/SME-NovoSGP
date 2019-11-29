0c177e3b3a4ecf61d5cc25c1cbbedcc03bb5def5
ricardo.alves <ricardo.alves@amcom.com.br>
2019-11-28 22:21:48 -0300

    Atualização do retorno da abrangencia do EOL

---
+++ src/SME.SGP.Infra/Enumerados/GrupoSGP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra.Enumerados
+{
+    public enum GruposSGP
+    {
+        ATE,
+        ProfessorReadapto,
+        PAEE,
+        PAP,
+        POA,
+        Professor,
+        ProfessorCJ,
+        ADMUE,
+        Secretário,
+        CP,
+        AD,
+        Diretor,
+        DREBásico,
+        ADMDRE,
+        DIPED,
+        PAAI,
+        CEFAI,
+        NAAPA,
+        DiretorDIPED,
+        Supervisor,
+        SupervisorTecnico,
+        DiretorRegional,
+        COPEDBasico,
+        NTCNAAPA,
+        NTC,
+        NTA,
+        DIEE,
+        DIEJA,
+        DIEI,
+        DIEFM,
+        COPED,
+        ADMSME,
+        ADMCOTIC
+    }
+}
