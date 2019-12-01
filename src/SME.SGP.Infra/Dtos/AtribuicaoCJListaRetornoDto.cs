e84ae28f165010a6bfddfc9a3dab07889e092ee8
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-12-01 20:20:03 -0300

    Melhorias de documentação nos endpoints de atribuição de CJ; Removido campo e classe desnecessárias;

--- src/SME.SGP.Infra/Dtos/AtribuicaoCJListaRetornoDto.cs
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJListaRetornoDto.cs
@@ -1,15 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace SME.SGP.Infra
+﻿namespace SME.SGP.Infra
 {
     public class AtribuicaoCJListaRetornoDto
     {
-        public AtribuicaoCJListaRetornoDto()
-        {
-            Disciplinas = new List<AtribuicaoCJDisciplinaRetornoDto>();
-        }
-
-        public List<AtribuicaoCJDisciplinaRetornoDto> Disciplinas { get; set; }
+        public string[] Disciplinas { get; set; }
         public string Modalidade { get; set; }
         public string Turma { get; set; }
     }
