ec1bc4b358ccfc674335ef32a58f42494482fc17
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-12-02 17:16:28 -0300

    AB#8862

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAtividadeAvaliativa.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAtividadeAvaliativa.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using SME.SGP.Infra;
+using System;
 using System.Threading.Tasks;
-using SME.SGP.Infra;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioAtividadeAvaliativa : IRepositorioBase<AtividadeAvaliativa>
     {
         Task<PaginacaoResultadoDto<AtividadeAvaliativa>> Listar(DateTime? dataAvaliacao, string dreId, string ueID, string nomeAvaliacao, int? tipoAvaliacaoId, string turmaId, Paginacao paginacao);
+
+        Task<bool> VerificarSeJaExisteAvaliacao(DateTime dataAvaliacao, string dreId, string ueId, string turmaId, string professorRf);
+
+        Task<bool> VerificarSeJaExisteAvaliacaoComMesmoNome(string nomeAvaliacao, string dreId, string ueID, string turmaId, string professorRf, DateTime periodoInicio, DateTime periodoFim);
+
+        Task<bool> VerificarSeJaExisteAvaliacaoRegencia(DateTime dataAvaliacao, string dreId, string ueID, string turmaId, string disciplinaId, string codigoRf);
     }
 }
