7358f2a427b843eb093adb0632e845a00a060000
Ítalo Maio <italo.maio@amcom.com.br>
2019-12-02 13:57:55 -0300

    (fix) resolvendo conflitos

--- src/SME.SGP.WebClient/src/componentes/Tag/index.js
+++ src/SME.SGP.WebClient/src/componentes/Tag/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import type from 'prop-types';
-=======
 import t from 'prop-types';
 
 // Ant
 import { Tag as AntTag } from 'antd';
->>>>>>> 1d7bad66a617c18e28055710b061c8afbe92c1ce
 
 // Estilos
 import { TagEstilo } from './styles';
 
-<<<<<<< HEAD
-function Tag({ tipo, children, inverted }) {
-  return (
-    <TagEstilo className={inverted && 'inverted'} tipo={tipo}>
-      {children}
-=======
 function Tag({ tipo, tamanho, fluido, centralizado, children, ...rest }) {
   return (
     <TagEstilo
@@ -25,26 +15,11 @@ function Tag({ tipo, tamanho, fluido, centralizado, children, ...rest }) {
       tipo={tipo}
     >
       <AntTag {...rest}>{children}</AntTag>
->>>>>>> 1d7bad66a617c18e28055710b061c8afbe92c1ce
     </TagEstilo>
   );
 }
 
 Tag.propTypes = {
-<<<<<<< HEAD
-  tipo: type.oneOf([
-    'basico',
-    'informativo1',
-    'informativo2',
-    'erro',
-    'alerta',
-    'atencao',
-    'cancelar',
-    'sucesso',
-  ]),
-  children: type.oneOfType([type.element, type.string]),
-  invertido: type.bool,
-=======
   tipo: t.oneOf([
     'basico',
     'informativo1',
@@ -59,20 +34,14 @@ Tag.propTypes = {
   children: t.oneOfType([t.string, t.element]),
   fluido: t.bool,
   centralizado: t.bool,
->>>>>>> 1d7bad66a617c18e28055710b061c8afbe92c1ce
 };
 
 Tag.defaultProps = {
   tipo: 'basico',
-<<<<<<< HEAD
-  children: () => null,
-  invertido: false,
-=======
   tamanho: 'pequeno',
   children: null,
   fluido: false,
   centralizado: false,
->>>>>>> 1d7bad66a617c18e28055710b061c8afbe92c1ce
 };
 
 export default Tag;
--- src/SME.SGP.WebClient/src/componentes/Tag/styles.js
+++ src/SME.SGP.WebClient/src/componentes/Tag/styles.js
@@ -1,39 +1,6 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
 
-<<<<<<< HEAD
-const temas = {
-  basico: {
-    background: '#dadada',
-    fonte: '#42474A',
-  },
-  informativo1: {
-    background: '#086397',
-    fonte: 'white',
-  },
-  informativo2: {
-    background: '#10A3FB',
-    fonte: 'white',
-  },
-  alerta: {
-    background: '#D06D12',
-    fonte: 'white',
-  },
-  erro: {
-    background: '#B40C02',
-    fonte: 'white',
-  },
-  atencao: {
-    background: '#FFFF30',
-    fonte: '#42474A',
-  },
-  sucesso: {
-    background: '#297805',
-    fonte: 'white',
-  },
-  cancelar: {
-    background: '#040404',
-=======
 const cores = {
   basico: {
     fundo: '#dadada',
@@ -65,27 +32,11 @@ const cores = {
   },
   cancelar: {
     fundo: '#040404',
->>>>>>> 1d7bad66a617c18e28055710b061c8afbe92c1ce
     fonte: 'white',
   },
 };
 
 export const TagEstilo = styled.div`
-<<<<<<< HEAD
-  width: 100%;
-  border-radius: 3px;
-  border: 1px solid ${props => darken(0.05, temas[props.tipo].background)};
-  background-color: ${props => temas[props.tipo].background};
-  color: ${props => temas[props.tipo].fonte};
-  padding: 10px;
-  margin: 4px;
-  text-align: center;
-  font-weight: bold;
-  &.inverted {
-    border-color: ${props => temas[props.tipo].background};
-    color: ${props => temas[props.tipo].background};
-    background-color: transparent;
-=======
   .ant-tag {
     font-weight: bold;
     display: inline-flex;
@@ -127,6 +78,5 @@ export const TagEstilo = styled.div`
       padding: 8px;
       font-size: 14px;
     }
->>>>>>> 1d7bad66a617c18e28055710b061c8afbe92c1ce
   }
 `;
