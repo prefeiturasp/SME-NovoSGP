1f4d8c7124e0f889d986096323a3e501c8d970ef
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-12-02 19:43:19 -0300

    Normalização de disciplina Id

--- src/SME.SGP.Infra/Dtos/AtribuicaoCJPersistenciaItemDto.cs
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJPersistenciaItemDto.cs
@@ -5,7 +5,7 @@ namespace SME.SGP.Infra
     public class AtribuicaoCJPersistenciaItemDto
     {
         [Required(ErrorMessage = "É necessário informar a disciplina.")]
-        public string DisciplinaId { get; set; }
+        public long DisciplinaId { get; set; }
 
         public bool Substituir { get; set; }
     }
