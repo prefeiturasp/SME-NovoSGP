1647e32abe510c9b4faefa2fd573082ca344a20a
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-12-04 19:19:46 -0300

    Ajustes iniciais

---
+++ src/SME.SGP.Api/Controllers/UeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/ues")]
+    [Authorize("Bearer")]
+    public class UeController : ControllerBase
+    {
+        public UeController()
+        {
+        }
+    }
+}
