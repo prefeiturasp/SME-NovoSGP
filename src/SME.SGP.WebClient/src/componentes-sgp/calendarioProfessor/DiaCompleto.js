6bbc4627603dc64f83dcab6f63d4e9ed84b8a112
Flávio Ricardo <flavioricardo@live.com>
2019-12-04 19:51:03 -0300

    Conflito com a dev AB#8630

--- src/SME.SGP.WebClient/src/componentes-sgp/calendarioProfessor/DiaCompleto.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/calendarioProfessor/DiaCompleto.js
@@ -187,68 +187,74 @@ const DiaCompleto = props => {
     estaAberto && (
       <Div className="border-bottom border-top-0 h-100 p-3">
         {eventosDia &&
-          eventosDia.eventosAulas &&
-          eventosDia.eventosAulas.length > 0 ? (
-            <Div className="list-group list-group-flush fade show">
-              {eventosDia.eventosAulas.map(evento => {
-                return (
-                  <Evento
-                    key={shortid.generate()}
-                    className="list-group-item list-group-item-action d-flex rounded"
-                    onClick={() => aoClicarEvento(evento.id, evento.tipoEvento)}
-                    style={{ cursor: 'pointer' }}
+        eventosDia.eventosAulas &&
+        eventosDia.eventosAulas.length > 0 ? (
+          <Div className="list-group list-group-flush fade show">
+            {eventosDia.eventosAulas.map(evento => {
+              return (
+                <Evento
+                  key={shortid.generate()}
+                  className="list-group-item list-group-item-action d-flex rounded"
+                  onClick={() => aoClicarEvento(evento.id, evento.tipoEvento)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <Grid
+                    cols={
+                      (evento.tipoEvento === TiposEventoAulaDTO.Aula && 1) ||
+                      (evento.tipoEvento === TiposEventoAulaDTO.CJ && 1) ||
+                      2
+                    }
+                    className="pl-0"
                   >
-                    <Grid
-                      cols={
-                        (evento.tipoEvento === TiposEventoAulaDTO.Aula && 1) ||
-                        (evento.tipoEvento === TiposEventoAulaDTO.CJ && 1) ||
-                        2
+                    <Botao
+                      label={evento.tipoEvento}
+                      color={
+                        (evento.tipoEvento === TiposEventoAulaDTO.Aula &&
+                          Colors.Roxo) ||
+                        (evento.tipoEvento === TiposEventoAulaDTO.CJ &&
+                          Colors.Laranja) ||
+                        Colors.CinzaBotao
                       }
-                      className="pl-0"
-                    >
-                      <Botao
-                        label={evento.tipoEvento}
-                        color={
-                          (evento.tipoEvento === TiposEventoAulaDTO.Aula &&
-                            Colors.Roxo) ||
-                          (evento.tipoEvento === TiposEventoAulaDTO.CJ &&
-                            Colors.Laranja) ||
-                          Colors.CinzaBotao
-                        }
-                        className="w-100"
-                        height={
-                          evento.tipoEvento === TiposEventoAulaDTO.Aula ||
-                            evento.tipoEvento === TiposEventoAulaDTO.CJ
-                            ? '38px'
-                            : 'auto'
-                        }
-                        border
-                        steady
-                      />
-                    </Grid>
-                    {(evento.tipoEvento === TiposEventoAulaDTO.Aula ||
-                      evento.tipoEvento === TiposEventoAulaDTO.CJ) &&
-                      evento.dadosAula && (
-                        <Grid cols={1} className="px-0">
-                          <Botao
-                            label={window
-                              .moment(evento.dadosAula.horario, 'HH')
-                              .format('HH:mm')}
-                            color={Colors.CinzaBotao}
-                            className="w-100 px-2"
-                            border
-                            steady
-                          />
-                        </Grid>
-                      )}
-                    <Grid
-                      cols={
+                      className="w-100"
+                      height={
                         evento.tipoEvento === TiposEventoAulaDTO.Aula ||
-                          evento.tipoEvento === TiposEventoAulaDTO.CJ
-                          ? 10
-                          : 11
+                        evento.tipoEvento === TiposEventoAulaDTO.CJ
+                          ? '38px'
+                          : 'auto'
                       }
-                      className="align-self-center font-weight-bold pl-0"
+                      border
+                      steady
+                    />
+                  </Grid>
+                  {(evento.tipoEvento === TiposEventoAulaDTO.Aula ||
+                    evento.tipoEvento === TiposEventoAulaDTO.CJ) &&
+                    evento.dadosAula && (
+                      <Grid cols={1} className="px-0">
+                        <Botao
+                          label={window
+                            .moment(evento.dadosAula.horario, 'HH')
+                            .format('HH:mm')}
+                          color={Colors.CinzaBotao}
+                          className="w-100 px-2"
+                          border
+                          steady
+                        />
+                      </Grid>
+                    )}
+                  <Grid
+                    cols={
+                      evento.tipoEvento === TiposEventoAulaDTO.Aula ||
+                      evento.tipoEvento === TiposEventoAulaDTO.CJ
+                        ? 10
+                        : 11
+                    }
+                    className="align-self-center font-weight-bold pl-0"
+                  >
+                    <Div
+                      className={`${(evento.tipoEvento ===
+                        TiposEventoAulaDTO.Aula ||
+                        evento.tipoEvento === TiposEventoAulaDTO.CJ) &&
+                        'pl-3'}`}
                     >
                       {evento.tipoEvento !== TiposEventoAulaDTO.Aula &&
                         evento.tipoEvento !== TiposEventoAulaDTO.CJ &&
