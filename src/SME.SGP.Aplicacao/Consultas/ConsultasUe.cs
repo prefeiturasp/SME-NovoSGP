44bf65371123a18da0db89661fe396c94919630b
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-12-04 19:19:46 -0300

    Ajustes iniciais

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasUe.cs
@@ -0,0 +1,22 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ConsultasUe
+    {
+        private readonly IRepositorioUe repositorioUe;
+
+        public ConsultasUe(IRepositorioUe repositorioUe)
+        {
+            this.repositorioUe = repositorioUe ?? throw new System.ArgumentNullException(nameof(repositorioUe));
+        }
+
+        public async Task<IEnumerable<Modalidade>> ObterModalidadesPorUe(string ueCodigo)
+        {
+            return await repositorioUe.ObterModalidadesPorUe(ueCodigo);
+        }
+    }
+}
