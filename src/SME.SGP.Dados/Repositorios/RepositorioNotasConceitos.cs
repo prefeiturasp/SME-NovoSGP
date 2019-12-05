55e12e04a1e4dd833fc1e6ce391d8a581bf51e21
Alexsander Camargo <csalexsander@gmail.com>
2019-12-05 00:18:48 -0300

    Persistencia de dados

--- src/SME.SGP.Dados/Repositorios/RepositorioNotasConceitos.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioNotasConceitos.cs
@@ -1,6 +1,9 @@
-﻿using SME.SGP.Dados.Contexto;
+﻿using Dapper;
+using SME.SGP.Dados.Contexto;
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SME.SGP.Dados.Repositorios
 {
@@ -8,6 +11,17 @@ namespace SME.SGP.Dados.Repositorios
     {
         public RepositorioNotasConceitos(ISgpContext sgpContext) : base(sgpContext)
         {
+        }
+
+        public IEnumerable<NotaConceito> ObterNotasPorAlunosAtividadesAvaliativas(IEnumerable<long> atividadesAvaliativas, IEnumerable<string> alunosIds)
+        {
+            var sql = @"select id, atividade_avaliativa, aluno_id, nota, conceito, tipo_nota, criado_em, 
+                        criado_por, criado_rf, alterado_em, alterado_por, alterado_rf  
+                        from notas_conceito where atividadeAvaliativa in @atividadesAvaliativas and aluno_id in @alunosIds";
+
+            var parametros = new { atividadesAvaliativas, alunosIds };
+
+            return database.Query<NotaConceito>(sql, parametros);
         }
     }
 }
