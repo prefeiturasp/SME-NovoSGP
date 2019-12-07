7caece91ac23144f43479e756d53374e9ed541e5
Marcos Lobo <marcos.lobo.nit@gmail.com>
2019-12-06 22:20:01 -0300

    Endpoint de listagem

--- src/SME.SGP.Infra/Dtos/ListaNotasConceitosConsultaDto.cs
+++ src/SME.SGP.Infra/Dtos/ListaNotasConceitosConsultaDto.cs
@@ -3,5 +3,7 @@
     public class ListaNotasConceitosConsultaDto
     {
         public int? Bimestre { get; set; }
+        public long DisciplinaCodigo { get; set; }
+        public string ProfessorRf { get; set; }
     }
 }
