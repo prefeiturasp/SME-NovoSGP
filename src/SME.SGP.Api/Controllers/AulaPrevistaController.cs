8f6425c9a82ca0c1a9d88382366c621ec5a8c597
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2019-12-11 18:23:49 -0300

    Endpoint busca por critério

--- src/SME.SGP.Api/Controllers/AulaPrevistaController.cs
+++ src/SME.SGP.Api/Controllers/AulaPrevistaController.cs
@@ -12,6 +12,16 @@ namespace SME.SGP.Api.Controllers
     [ValidaDto]
     public class AulaPrevistaController : ControllerBase
     {
+        [HttpGet("modalidades/{modalidade}/turmas/{turmaId}/disciplinas/{disciplinaId}")]
+        [ProducesResponseType(typeof(AulasPrevistasDadasAuditoriaDto), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        [Permissao(Permissao.ADAP_C, Policy = "Bearer")]
+        public async Task<IActionResult> ObterAulaPrevistaDada(Modalidade modalidade, string turmaId, string disciplinaId, [FromServices]IConsultasAulaPrevista consultas)
+        {
+            return Ok(await consultas.ObterAulaPrevistaDada(modalidade, turmaId, disciplinaId));
+        }
+
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(AulasPrevistasDadasAuditoriaDto), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
