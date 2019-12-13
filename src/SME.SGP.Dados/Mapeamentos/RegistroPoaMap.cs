8cb54615aa75a2d1fc4c5963d6754361e2616deb
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2019-12-13 20:20:25 -0300

    Modelagem, Cadastro e Edicao
    AB#9740 AB#9723 AB#9396

---
+++ src/SME.SGP.Dados/Mapeamentos/RegistroPoaMap.cs
@@ -0,0 +1,15 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class RegistroPoaMap : BaseMap<RegistroPoa>
+    {
+        public RegistroPoaMap()
+        {
+            ToTable("registro_poa");
+            Map(r => r.CodigoRf).ToColumn("codigo_rf");
+            Map(r => r.DreId).ToColumn("dre_id");
+            Map(r => r.UeId).ToColumn("ue_id");
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRegistroPoa.cs
@@ -0,0 +1,6 @@
+﻿namespace SME.SGP.Dominio.Interfaces
+{
+    public interface IRepositorioRegistroPoa : IRepositorioBase<RegistroPoa>
+    {
+    }
+}
