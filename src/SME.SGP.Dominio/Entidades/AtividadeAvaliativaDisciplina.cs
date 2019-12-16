b878d3b31f0e46a67708ca2df6a848bbfd8e4426
Eliton Santos <eliton.santos@amcom.com.br>
2019-12-16 13:05:25 -0300

    Nova entidade atividade disciplina

---
+++ src/SME.SGP.Dominio/Entidades/AtividadeAvaliativaDisciplina.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio
+{
+    public class AtividadeAvaliativaDisciplina : EntidadeBase
+    {
+        public long AtividadeAvaliativaId { get; set; }
+        public string DisciplinaId { get; set; }
+        public bool Excluido { get; set; }
+    }
+}
