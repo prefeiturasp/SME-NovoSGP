b4b2a75785203313dd5f10dda91b54a7e1fc56d0
Alexsander Camargo <csalexsander@gmail.com>
2019-12-16 00:09:06 -0300

    Ajuste listagem, testes, permissionamento lado SGP
    AB#9747 AB#9748 AB#9749 AB#9779

--- src/SME.SGP.Aplicacao/Comandos/ComandosRegistroPoa.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosRegistroPoa.cs
@@ -47,6 +47,7 @@ namespace SME.SGP.Aplicacao
             {
                 Descricao = registroPoaDto.Descricao,
                 DreId = registroPoaDto.DreId,
+                AnoLetivo = registroPoaDto.AnoLetivo,
                 UeId = registroPoaDto.UeId,
                 CodigoRf = registroPoaDto.CodigoRf,
                 Mes = registroPoaDto.Mes,
