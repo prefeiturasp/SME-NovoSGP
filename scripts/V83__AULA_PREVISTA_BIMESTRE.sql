2e8ec148944ab7fd3b5574c4d3e5f2463f739306
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2019-12-16 10:22:18 -0300

    Ajuste script

--- scripts/V83__AULA_PREVISTA_BIMESTRE.sql
+++ scripts/V83__AULA_PREVISTA_BIMESTRE.sql
@@ -2,7 +2,6 @@
 ALTER TABLE public.aula_prevista DROP COLUMN IF EXISTS bimestre;
 
 DROP TABLE if exists public.aula_prevista_bimestre;
-
 CREATE TABLE public.aula_prevista_bimestre (
 	id int8 NOT NULL GENERATED ALWAYS AS identity,
 	aula_prevista_id int8 NOT NULL,
