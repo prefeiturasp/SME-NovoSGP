b6bf6e024a5cfd666801df25809b98d5ffe50bc8
AlexsanderAMcom <53785959+AlexsanderAMcom@users.noreply.github.com>
2019-12-16 00:17:53 -0300

    Update RegistroPoaFiltroDto.cs

--- src/SME.SGP.Infra/Dtos/RegistroPoaFiltroDto.cs
+++ src/SME.SGP.Infra/Dtos/RegistroPoaFiltroDto.cs
@@ -7,14 +7,14 @@ namespace SME.SGP.Infra
 {
     public class RegistroPoaFiltroDto
     {
-        [Required(ErrorMessage = "O codigo rf é obrigatorio")]
+        [Required(ErrorMessage = "O código rf é obrigatório")]
         public string CodigoRf { get; set; }
-        [Required(ErrorMessage = "É obrigatorio informar o id da Dre")]
+        [Required(ErrorMessage = "É obrigatório informar o id da Dre")]
         public string DreId { get; set; }
-        [Required(ErrorMessage = "É obrigatorio informar o Mes")]
+        [Required(ErrorMessage = "É obrigatório informar o Mes")]
         [Range(1,12, ErrorMessage = "Os meses devem ser infomados entre Janeiro e Dezembro")]
         public int Mes { get; set; }
-        [Required(ErrorMessage = "E necessario informar o id da unidade escolar")]
+        [Required(ErrorMessage = "E necessário informar o id da unidade escolar")]
         public string UeId { get; set; }
         public string Titulo { get; set; }
     }
