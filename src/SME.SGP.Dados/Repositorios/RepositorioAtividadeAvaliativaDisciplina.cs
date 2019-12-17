b6fd6b82558d8c113933587fc1f537f7d55e3c47
Eliton Santos <eliton.santos@amcom.com.br>
2019-12-17 14:30:14 -0300

    Modelagem

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioAtividadeAvaliativaDisciplina.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioAtividadeAvaliativaDisciplina : RepositorioBase<AtividadeAvaliativaDisciplina>, IRepositorioAtividadeAvaliativaDisciplina
+    {
+        public RepositorioAtividadeAvaliativaDisciplina(ISgpContext conexao) : base(conexao)
+        {
+        }
+
+        public async Task<IEnumerable<AtividadeAvaliativaDisciplina>> ListarPorIdAtividade(long idAtividadeAvaliativa)
+        {
+            StringBuilder query = new StringBuilder();
+            query.AppendLine("SELECT id,");
+            query.AppendLine("atividade_avaliativa_id,");
+            query.AppendLine("disciplina_id,");
+            query.AppendLine("criado_em,");
+            query.AppendLine("criado_por,");
+            query.AppendLine("alterado_em,");
+            query.AppendLine("alterado_por,");
+            query.AppendLine("criado_rf,");
+            query.AppendLine("alterado_rf,");
+            query.AppendLine("excluido");
+            query.AppendLine("FROM atividade_avaliativa_disciplina");
+            query.AppendLine("WHERE atividade_avaliativa_id = @idAtividadeAvaliativa");
+            query.AppendLine("AND excluido = false");
+
+            return await database.Conexao.QueryAsync<AtividadeAvaliativaDisciplina>(query.ToString(), new
+            {
+                idAtividadeAvaliativa
+            });
+        }
+    }
+}
