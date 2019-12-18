aa7ba042b68d1c1d0def738b4b06e996425dbb68
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2019-12-18 15:12:31 -0300

    Ajuste listagem de eventos

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioEvento.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioEvento.cs
@@ -20,7 +20,7 @@ namespace SME.SGP.Dominio.Interfaces
         bool ExisteEventoPorTipoCalendarioId(long tipoCalendarioId);
 
         Task<PaginacaoResultadoDto<Evento>> Listar(long? tipoCalendarioId, long? tipoEventoId, string nomeEvento, DateTime? dataInicio, DateTime? dataFim, Paginacao paginacao, string dreId, string ueId,
-            Usuario usuario, Guid usuarioPerfil, bool usuarioTemPerfilSupervisorOuDiretor, bool podeVisualizarEventosLocalOcorrenciaDre);
+           bool ehTodasDres, bool ehTodasUes, Usuario usuario, Guid usuarioPerfil, bool usuarioTemPerfilSupervisorOuDiretor, bool podeVisualizarEventosLocalOcorrenciaDre);
 
         Task<IEnumerable<CalendarioEventosNoDiaRetornoDto>> ObterEventosPorDia(CalendarioEventosFiltroDto calendarioEventosMesesFiltro, int mes, int dia, Usuario usuario, Guid usuarioPerfil, bool usuarioTemPerfilSupervisorOuDiretor, bool podeVisualizarEventosLocalOcorrenciaDre);
 
