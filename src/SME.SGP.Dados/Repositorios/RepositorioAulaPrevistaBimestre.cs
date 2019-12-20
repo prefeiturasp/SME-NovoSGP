5b22eedbd828fbddf218cff55bc297240934bc1b
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2019-12-20 05:08:37 -0300

    Analise e correção de busca de aulas previstas
    AB#10112

--- src/SME.SGP.Dados/Repositorios/RepositorioAulaPrevistaBimestre.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioAulaPrevistaBimestre.cs
@@ -19,8 +19,7 @@ namespace SME.SGP.Dados.Repositorios
             StringBuilder query = new StringBuilder();
 
             query.Append(MontarSelect());
-            query.Append(@" where (a.id is null or not a.excluido) and
-                        tp.situacao and not tp.excluido and
+            query.Append(@" where tp.situacao and not tp.excluido and
                         p.tipo_calendario_id = @tipoCalendarioId and
                         ap.turma_id = @turmaId and
                         ap.disciplina_id = @disciplinaId ");
@@ -34,41 +33,39 @@ namespace SME.SGP.Dados.Repositorios
             StringBuilder query = new StringBuilder();
 
             query.Append(MontarSelect());
-            query.Append(@" where (a.id is null or not a.excluido) and
-                        tp.situacao and not tp.excluido and
+            query.Append(@" where tp.situacao and not tp.excluido and
                         ap.id = @aulaPrevistaId ");
             query.Append(MontarGroupOrderBy());
 
             return (await database.Conexao.QueryAsync<AulaPrevistaBimestreQuantidade>(query.ToString(), new { aulaPrevistaId }));
-
         }
 
-        private string MontarSelect()
-        {
-            return @"select apb.id, apb.criado_em as CriadoEm, apb.criado_por as CriadoPor, apb.alterado_em as AlteradoEm, apb.alterado_por as AlteradoPor,
-                         apb.alterado_rf as AlteradoRF, apb.criado_rf as CriadoRF, p.bimestre, p.periodo_inicio as inicio, p.periodo_fim as fim, 
-                         apb.aulas_previstas as Previstas,
-                         COUNT(a.id) filter (where a.tipo_aula = 1 and a.aula_cj = false) as CriadasTitular,
-                         COUNT(a.id) filter (where a.tipo_aula = 1 and a.aula_cj = true) as CriadasCJ,
-                         COUNT(a.id) filter (where a.tipo_aula = 1 and rf.id is not null) as Cumpridas,
-                         COUNT(a.id) filter (where a.tipo_aula = 2 and rf.id is not null) as Reposicoes                         
-                         from periodo_escolar p
-                         inner join tipo_calendario tp on p.tipo_calendario_id = tp.id
-                         left join aula_prevista ap on ap.tipo_calendario_id = p.tipo_calendario_id 
-                         left join aula_prevista_bimestre apb on ap.id = apb.aula_prevista_id and p.bimestre = apb.bimestre
-                         left join aula a on a.turma_id = ap.turma_id and
-                         				a.disciplina_id = ap.disciplina_id and
-			                            a.tipo_calendario_id = p.tipo_calendario_id and 
-				                        a.data_aula BETWEEN p.periodo_inicio AND p.periodo_fim
-                         left join registro_frequencia rf on a.id = rf.aula_id ";
-        }
-
-       
         private string MontarGroupOrderBy()
         {
             return @" group by p.bimestre, p.periodo_inicio, p.periodo_fim, apb.aulas_previstas, apb.Id,
                          	   ap.criado_em, ap.criado_por, ap.alterado_em , ap.alterado_por,
                                ap.alterado_rf, ap.criado_rf; ";
+        }
+
+        private string MontarSelect()
+        {
+            return @"select apb.id, apb.criado_em as CriadoEm, apb.criado_por as CriadoPor, apb.alterado_em as AlteradoEm, apb.alterado_por as AlteradoPor,
+                         apb.alterado_rf as AlteradoRF, apb.criado_rf as CriadoRF, p.bimestre, p.periodo_inicio as inicio, p.periodo_fim as fim,
+                         apb.aulas_previstas as Previstas,
+                         COUNT(a.id) filter (where a.tipo_aula = 1 and a.aula_cj = false) as CriadasTitular,
+                         COUNT(a.id) filter (where a.tipo_aula = 1 and a.aula_cj = true) as CriadasCJ,
+                         COUNT(a.id) filter (where a.tipo_aula = 1 and rf.id is not null) as Cumpridas,
+                         COUNT(a.id) filter (where a.tipo_aula = 2 and rf.id is not null) as Reposicoes
+                         from periodo_escolar p
+                         inner join tipo_calendario tp on p.tipo_calendario_id = tp.id
+                         left join aula_prevista ap on ap.tipo_calendario_id = p.tipo_calendario_id
+                         left join aula_prevista_bimestre apb on ap.id = apb.aula_prevista_id and p.bimestre = apb.bimestre
+                         left join aula a on a.turma_id = ap.turma_id and
+                         				a.disciplina_id = ap.disciplina_id and
+			                            a.tipo_calendario_id = p.tipo_calendario_id and
+				                        a.data_aula BETWEEN p.periodo_inicio AND p.periodo_fim
+                                        and (a.id is null or not a.excluido)
+                         left join registro_frequencia rf on a.id = rf.aula_id ";
         }
     }
 }
