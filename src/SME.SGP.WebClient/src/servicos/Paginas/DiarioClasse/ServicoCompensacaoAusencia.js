a29512a79c971525fd4194ac95fa6695733324ad
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2020-01-13 11:55:23 -0300

    compensacao ausencia - estrutura inicial, layout lista, rotas, menu

---
+++ src/SME.SGP.WebClient/src/servicos/Paginas/DiarioClasse/ServicoCompensacaoAusencia.js
@@ -0,0 +1,26 @@
+import api from '~/servicos/api';
+
+// TODO Alterar quando tiver o back pronto
+const urlPadrao = `v1/compensacao-ausencia`;
+
+class ServicoCompensacaoAusencia {
+  salvar = async (id, compensacao) => {
+    let url = urlPadrao;
+    if (id) {
+      url = `${url}/${id}`;
+    }
+    const metodo = id ? 'put' : 'post';
+    return api[metodo](url, compensacao);
+  };
+
+  obterPorId = async id => {
+    return api.get(`${urlPadrao}/${id}`);
+  };
+
+  deletar = async ids => {
+    const parametros = { data: ids };
+    return api.delete(urlPadrao, parametros);
+  };
+}
+
+export default new ServicoCompensacaoAusencia();
