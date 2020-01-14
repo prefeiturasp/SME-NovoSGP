7c50e194c762ff146a9af73244c3e969afa1d0af
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-01-14 11:24:53 -0300

    Flag docencia compartilhada

--- src/SME.SGP.Aplicacao/Integracoes/Respostas/DisciplinaResposta.cs
+++ src/SME.SGP.Aplicacao/Integracoes/Respostas/DisciplinaResposta.cs
@@ -12,5 +12,8 @@ namespace SME.SGP.Aplicacao.Integracoes.Respostas
 
         [JsonProperty("regencia")]
         public bool Regencia { get; set; }
+
+        [JsonProperty("docenciaCompartilhada")]
+        public bool DocenciaCompartilhada { get; set; }
     }
 }
