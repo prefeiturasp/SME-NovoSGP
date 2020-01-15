90910369cf1a2e1dce0b930ea1281cf3307ad14d
Losi <rafael.losi@amcom.com.br>
2020-01-15 14:07:47 -0300

    10481 - Compensação Ausencia: Obter por id

---
+++ src/SME.SGP.Infra/Dtos/CompensacaoAusenciaAlunoCompletoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class CompensacaoAusenciaAlunoCompletoDto
+    {
+        public string AlunoCodigo { get; set; }
+        public string AlunoNome { get; set; }
+        public double QtdFaltasCompensadas { get; set; }
+        public double QuantidadeFaltasTotais { get; set; }
+    }
+}
