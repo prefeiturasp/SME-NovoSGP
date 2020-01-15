4a77f1b13aa2d4666f8d8d956781bff41f6dc7a8
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2020-01-15 18:32:02 -0300

    compensacao ausencia form - lista de alunos

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/CompensacaoAusencia/styles.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/CompensacaoAusencia/styles.js
@@ -23,3 +23,39 @@ export const Badge = styled.button`
     color: white !important;
   }
 `;
+
+export const BotaoListaAlunos = styled.div`
+  width: 46px;
+  height: 46px;
+  border-radius: 4px;
+  border: solid 1px #a4a4a4;
+  background-color: rgba(0, 0, 0, 0.04);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${Base.Roxo} !important;
+    color: white;
+  }
+
+  i {
+    font-size: 20px;
+  }
+`;
+
+export const ColunaBotaoListaAlunos = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const CardTabelaAlunos = styled.div`
+  padding: 10px;
+  height: 488px;
+  border-radius: 4px;
+  box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.15);
+  background-color: #ffffff;
+`;
