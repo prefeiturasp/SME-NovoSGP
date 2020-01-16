b07e06cd4c3633bfac2d7a0288c2c671f4d6e03c
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-01-15 22:11:01 -0300

    Modelagem inicial de periodo de fechamento AB#4174

--- src/SME.SGP.Api/Controllers/PeriodoFechamentoController.cs
+++ src/SME.SGP.Api/Controllers/PeriodoFechamentoController.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Aplicacao;
 
 namespace SME.SGP.Api.Controllers
 {
     [ApiController]
-    [Route("api/v1/periodos/fechamento/abertura")]
-    [Authorize("Bearer")]
+    [Route("api/v1/periodos/fechamentos/aberturas")]
     public class PeriodoFechamentoController : ControllerBase
     {
         [HttpGet]
         [ProducesResponseType(200)]
-        public IActionResult Get(IConsultasPeriodoFechamento consultasPeriodoFechamento)
+        public IActionResult Get([FromServices] IConsultasPeriodoFechamento consultasPeriodoFechamento)
         {
             return Ok(consultasPeriodoFechamento.GetTeste());
         }
