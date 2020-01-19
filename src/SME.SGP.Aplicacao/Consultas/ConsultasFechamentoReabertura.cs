bc7cbfbb03a5780efe97b8b43138654a4b6787a8
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-01-19 16:01:58 -0300

    endpoint de listagem com filtros e paginação;

--- src/SME.SGP.Aplicacao/Consultas/ConsultasFechamentoReabertura.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasFechamentoReabertura.cs
@@ -1,6 +1,65 @@
-﻿namespace SME.SGP.Aplicacao
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
 {
-    public class ConsultasFechamentoReabertura : IConsultasFechamentoReabertura
+    public class ConsultasFechamentoReabertura : ConsultasBase, IConsultasFechamentoReabertura
     {
+        private readonly IConsultasTipoCalendario consultasTipoCalendario;
+        private readonly IRepositorioFechamentoReabertura repositorioFechamentoReabertura;
+
+        public ConsultasFechamentoReabertura(IRepositorioFechamentoReabertura repositorioFechamentoReabertura,
+            IContextoAplicacao contextoAplicacao, IConsultasTipoCalendario consultasTipoCalendario) : base(contextoAplicacao)
+        {
+            this.repositorioFechamentoReabertura = repositorioFechamentoReabertura ?? throw new System.ArgumentNullException(nameof(repositorioFechamentoReabertura));
+            this.consultasTipoCalendario = consultasTipoCalendario ?? throw new System.ArgumentNullException(nameof(consultasTipoCalendario));
+        }
+
+        public async Task<PaginacaoResultadoDto<FechamentoReaberturaListagemDto>> Listar(long tipoCalendarioId, long? dreId, long? ueId)
+        {
+            var listaEntidades = await repositorioFechamentoReabertura.Listar(tipoCalendarioId, dreId, ueId, Paginacao);
+
+            return MapearListaEntidadeParaDto(listaEntidades);
+        }
+
+        private IEnumerable<FechamentoReaberturaListagemDto> MapearEntidadeParaDto(IEnumerable<FechamentoReabertura> items)
+        {
+            if (items.Any())
+            {
+                var tipoCalendario = consultasTipoCalendario.BuscarPorId(items.FirstOrDefault().TipoCalendarioId);
+                if (tipoCalendario == null)
+                    throw new NegocioException("Não foi possível localizar o tipo de calendário.");
+
+                foreach (var item in items)
+                {
+                    yield return new FechamentoReaberturaListagemDto()
+                    {
+                        Bimestres = item.Bimestres.Select(a => a.Bimestre).ToArray(),
+                        DataInicio = item.Inicio,
+                        DataFim = item.Fim,
+                        Descricao = item.Descricao,
+                        Id = item.Id,
+                        BimestresQuantidadeTotal = tipoCalendario.QuantidadeDeBimestres()
+                    };
+                }
+            }
+        }
+
+        private PaginacaoResultadoDto<FechamentoReaberturaListagemDto> MapearListaEntidadeParaDto(PaginacaoResultadoDto<FechamentoReabertura> listaEntidades)
+        {
+            var retorno = new PaginacaoResultadoDto<FechamentoReaberturaListagemDto>();
+
+            retorno.TotalPaginas = listaEntidades.TotalPaginas;
+            retorno.TotalRegistros = listaEntidades.TotalRegistros;
+
+            retorno.Items = MapearEntidadeParaDto(listaEntidades.Items);
+
+            return retorno;
+        }
     }
 }
---
+++ src/SME.SGP.Infra/Dtos/FechamentoReaberturaFiltroDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Infra
+{
+    public class FechamentoReaberturaFiltroDto
+    {
+        public long? DreId { get; set; }
+        public long TipoCalendarioId { get; set; }
+        public long? UeId { get; set; }
+    }
+}
