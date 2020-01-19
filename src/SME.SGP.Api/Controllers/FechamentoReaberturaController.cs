55f62a452904177575db6c43398e58891d2fa988
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-01-19 16:05:44 -0300

    Permissionamento no endpoint de listagem

--- src/SME.SGP.Api/Controllers/FechamentoReaberturaController.cs
+++ src/SME.SGP.Api/Controllers/FechamentoReaberturaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Infra;
 using System.Threading.Tasks;
@@ -7,13 +9,13 @@ namespace SME.SGP.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/fechamentos/reaberturas")]
-    //[Authorize("Bearer")]
+    [Authorize("Bearer")]
     public class FechamentoReaberturaController : ControllerBase
     {
         [HttpGet]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
-        //[Permissao(Permissao.E_C, Policy = "Bearer")]
+        [Permissao(Permissao.PFR_C, Policy = "Bearer")]
         public async Task<IActionResult> Listar([FromServices] IConsultasFechamentoReabertura consultasFechamentoReabertura, [FromQuery]FechamentoReaberturaFiltroDto fechamentoReaberturaFiltroDto)
         {
             return Ok(await consultasFechamentoReabertura.Listar(fechamentoReaberturaFiltroDto.TipoCalendarioId, fechamentoReaberturaFiltroDto.DreId, fechamentoReaberturaFiltroDto.UeId));
