40473437f1c6828977504687a8cdce114e2280f9
Rafael Losi <rafael.losi@amcom.com.br>
2020-01-21 10:59:56 -0300

    10503 - Copia da compensação de ausencia

---
+++ src/SME.SGP.Infra/Dtos/CompensacaoAusenciaCopiaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class CompensacaoAusenciaCopiaDto
+    {
+        [Required(ErrorMessage = "Deve ser informada a compensação de origem da copia!")]
+        public long CompensacaoOrigemId { get; set; }
+
+        public string TurmaId { get; set; }
+
+        public int Bimestre { get; set; }
+    }
+}
