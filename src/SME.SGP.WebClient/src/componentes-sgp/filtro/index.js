15d13ca2d28f13baa598c9ce99a2df2dd16b48bc
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-01-22 16:18:25 -0300

    Correção disciplinas plano de aula
    AB#11105

--- src/SME.SGP.WebClient/src/componentes-sgp/filtro/index.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/filtro/index.js
@@ -168,7 +168,9 @@ const Filtro = () => {
       );
 
       setTextoAutocomplete(
-        `${modalidadeDesc.desc} - ${turmaDesc.desc} - ${unidadeEscolarDesc.desc}`
+        `${modalidadeDesc ? modalidadeDesc.desc : 'Modalidade'} - ${
+          turmaDesc ? turmaDesc.desc : 'Turma'
+        } - ${unidadeEscolarDesc ? unidadeEscolarDesc.desc : 'Unidade Escolar'}`
       );
 
       setAlternarFocoBusca(false);
@@ -176,6 +178,8 @@ const Filtro = () => {
       const turmaSelecionadaCompleta = turmas.find(
         item => item.valor.toString() === turmaSelecionada
       );
+
+      if (!turmaSelecionadaCompleta) return;
 
       const turma = {
         anoLetivo: anoLetivoSelecionado,
@@ -186,7 +190,7 @@ const Filtro = () => {
         ano: turmaSelecionadaCompleta.ano,
         desc: `${modalidadeDesc.desc} - ${turmaDesc.desc} - ${unidadeEscolarDesc.desc}`,
         periodo: periodoSelecionado || 0,
-        consideraHistorico: consideraHistorico,
+        consideraHistorico,
       };
 
       dispatch(turmasUsuario(turmas));
