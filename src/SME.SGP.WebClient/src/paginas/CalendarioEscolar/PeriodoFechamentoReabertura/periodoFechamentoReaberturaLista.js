d2c444a8a50f32989e64d2ce4c8d8229322707a5
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2020-01-23 18:39:15 -0300

    lista - periodo fechamento REABERTURA

---
+++ src/SME.SGP.WebClient/src/paginas/CalendarioEscolar/PeriodoFechamentoReabertura/periodoFechamentoReaberturaLista.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Cabecalho } from '~/componentes-sgp';
+import Card from '~/componentes/card';
+
+const PeriodoFechamentoReaberturaLista = () => {
+  return (
+    <>
+      <Cabecalho pagina="Período de Fechamento (Reabertura)" />
+      <Card>teste</Card>
+    </>
+  );
+};
+
+export default PeriodoFechamentoReaberturaLista;
