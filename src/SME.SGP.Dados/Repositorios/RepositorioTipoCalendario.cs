312baf5575179493cb7c022a5d5e137136bcf52c
Losi <rafael.losi@amcom.com.br>
2020-01-24 11:13:51 -0300

    10509 - Criado notificação bimestral de alunos com frequencia abaixo do limite;

--- src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendario.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendario.cs
@@ -30,7 +30,7 @@ namespace SME.SGP.Dados.Repositorios
             if (modalidade == ModalidadeTipoCalendario.EJA)
             {
                 var periodoReferencia = semestre == 1 ? "periodo_inicio < @dataReferencia" : "periodo_fim > @dataReferencia";
-                query.AppendLine($"and exists(select 0 from periodo_escolar p tipo_calendario_id = t.id and {periodoReferencia})");
+                query.AppendLine($"and exists(select 0 from periodo_escolar p where tipo_calendario_id = t.id and {periodoReferencia})");
 
                 // 1/6/ano ou 1/7/ano dependendo do semestre
                 dataReferencia = new DateTime(anoLetivo, semestre == 1 ? 6 : 7, 1);
