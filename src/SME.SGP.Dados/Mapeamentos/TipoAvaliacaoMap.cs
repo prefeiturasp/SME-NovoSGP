b64a17d9ba48fd2ab9aa548a50ebd97dcd4e47de
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-01-28 16:54:05 -0300

    Adicionando script e coluna com quantidade de avaliações obrigatórias por bimestre

--- src/SME.SGP.Dados/Mapeamentos/TipoAvaliacaoMap.cs
+++ src/SME.SGP.Dados/Mapeamentos/TipoAvaliacaoMap.cs
@@ -11,6 +11,7 @@ namespace SME.SGP.Dados.Mapeamentos
             Map(t => t.Descricao).ToColumn("descricao");
             Map(t => t.Excluido).ToColumn("excluido");
             Map(t => t.Situacao).ToColumn("situacao");
+            Map(t => t.AvaliacoesNecessariasPorBimestre).ToColumn("avaliacoes_necessarias_bimestre");
         }
     }
 }
