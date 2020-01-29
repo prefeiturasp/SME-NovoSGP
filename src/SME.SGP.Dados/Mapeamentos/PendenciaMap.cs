ea5bf7b64ce17396211063dc6ea8e1e5cf495973
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-01-29 18:02:04 -0300

    Modelagem inicial de fechamento e pendencias

---
+++ src/SME.SGP.Dados/Mapeamentos/PendenciaMap.cs
@@ -0,0 +1,14 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class PendenciaMap : BaseMap<Pendencia>
+    {
+        public PendenciaMap()
+        {
+            ToTable("pendencia");
+            Map(c => c.FechamentoId).ToColumn("fechamento_id");
+            Map(c => c.Fechamento).Ignore();
+        }
+    }
+}
