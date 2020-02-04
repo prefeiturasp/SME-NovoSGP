d5e697f7e6376d10ce7d793b9c078b37e8128dd4
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-02-04 20:00:53 -0300

    script e finalização do map

--- src/SME.SGP.Dados/Mapeamentos/FechamentoFinalMap.cs
+++ src/SME.SGP.Dados/Mapeamentos/FechamentoFinalMap.cs
@@ -9,7 +9,14 @@ namespace SME.SGP.Dados.Mapeamentos
             ToTable("fechamento_final");
             Map(a => a.AusenciasCompensadas).ToColumn("ausencias_compensadas");
             Map(a => a.AlunoCodigo).ToColumn("aluno_codigo");
-            Map(a => a.AlunoCodigo).ToColumn("aluno_codigo");
+            Map(a => a.DisciplinaCodigo).ToColumn("disciplina_codigo");
+            Map(a => a.PercentualFrequencia).ToColumn("percentual_frequencia");
+
+            Map(a => a.ConceitoId).ToColumn("conceito_id");
+            Map(a => a.Conceito).Ignore();
+
+            Map(a => a.TurmaId).ToColumn("turma_id");
+            Map(a => a.Turma).Ignore();
         }
     }
 }
