0a9357566aecda95125cd2d9ba77f92dffe0c750
Flávio Ricardo <flavioricardo@live.com>
2020-02-04 11:31:11 -0300

    AB#11337

--- src/SME.SGP.WebClient/src/paginas/Relatorios/PAP/ResumosGraficos/componentes/Filtro/index.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/PAP/ResumosGraficos/componentes/Filtro/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 // Formulario
@@ -6,43 +6,23 @@ import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
 
 // Componentes
-import { useSelector } from 'react-redux';
-import { Grid, SelectComponent } from '~/componentes';
+// import { useSelector } from 'react-redux';
+import { Grid, SelectComponent, Loader } from '~/componentes';
 
 // Styles
 import { Linha } from '~/componentes/EstilosGlobais';
 
 // Services
-import ServicoFiltro from '~/servicos/Componentes/ServicoFiltro';
+import AbrangenciaServico from '~/servicos/Abrangencia';
+// import ServicoFiltro from '~/servicos/Componentes/ServicoFiltro';
+import FiltroHelper from '~/componentes-sgp/filtro/helper';
 
 function Filtro({ onFiltrar }) {
   const [refForm, setRefForm] = useState({});
 
-  const usuario = useSelector(state => state.usuario);
-  const { turmaSelecionada } = usuario;
-  const filtro = useSelector(state => state.filtro);
-
-  const [listaDres, setListaDres] = useState([]);
-
-  const buscarListaDres = async () => {
-    setListaDres(
-      await ServicoFiltro.listarDres({
-        consideraHistorico:
-          turmaSelecionada && turmaSelecionada.consideraHistorico,
-        modalidadeSelecionada,
-        periodoSelecionado,
-        anoLetivoSelecionado,
-      })
-    );
-  };
-
-  // const []
-
-  useEffect(() => {
-    console.log(usuario);
-    console.log(filtro);
-    // buscarListaDres();
-  }, []);
+  // const usuario = useSelector(state => state.usuario);
+  // const { turmaSelecionada } = usuario;
+  // const filtro = useSelector(state => state.filtro);
 
   const [dreId, setDreId] = useState(undefined);
   const [ueId, setUeId] = useState(undefined);
@@ -51,7 +31,7 @@ function Filtro({ onFiltrar }) {
   const [turmaId, setTurmaId] = useState(undefined);
   const [periodo, setPeriodo] = useState(undefined);
 
-  const [valoresIniciais] = useState({
+  const [valoresIniciais, setValoresIniciais] = useState({
     dreId,
     ueId,
     ciclo,
@@ -59,6 +39,67 @@ function Filtro({ onFiltrar }) {
     turmaId,
     periodo,
   });
+
+  const [carregandoDres, setCarregandoDres] = useState(false);
+  const [listaDres, setListaDres] = useState([]);
+
+  const buscarListaDres = async () => {
+    const dres = [];
+    setCarregandoDres(true);
+
+    AbrangenciaServico.buscarDres()
+      .then(resposta => {
+        if (resposta.data) {
+          resposta.data.forEach(dre => {
+            dres.push({
+              desc: dre.nome,
+              valor: dre.codigo,
+            });
+          });
+        }
+      })
+      .finally(() => {
+        setCarregandoDres(false);
+      });
+
+    dres.sort(FiltroHelper.ordenarLista('desc'));
+    dres.unshift({ valor: 0, desc: 'Todas' });
+    setListaDres(dres);
+  };
+
+  useEffect(() => {
+    buscarListaDres();
+  }, []);
+
+  const [carregandoUes, setCarregandoUes] = useState(false);
+  const [listaUes, setListaUes] = useState([]);
+
+  const buscarListaUes = useCallback(async () => {
+    const ues = [];
+    setCarregandoUes(true);
+
+    AbrangenciaServico.buscarUes(dreId)
+      .then(resposta => {
+        if (resposta.data) {
+          resposta.data.forEach(ue => {
+            ues.push({
+              desc: ue.nome,
+              valor: ue.codigo,
+            });
+          });
+        }
+      })
+      .finally(() => {
+        setCarregandoUes(false);
+      });
+
+    setListaUes(ues.sort(FiltroHelper.ordenarLista('desc')));
+  }, [dreId]);
+
+  useEffect(() => {
+    buscarListaUes();
+    setValoresIniciais({ dreId });
+  }, [buscarListaUes, dreId]);
 
   const validacoes = () => {
     return Yup.object({});
@@ -86,28 +127,34 @@ function Filtro({ onFiltrar }) {
         <Form className="col-md-12 mb-4">
           <Linha className="row mb-2">
             <Grid cols={6}>
-              <SelectComponent
-                className="fonte-14"
-                form={form}
-                name="dreId"
-                lista={listaDres}
-                containerVinculoId="containerFiltro"
-                valueOption="valor"
-                valueText="desc"
-                placeholder="Selecione a DRE"
-              />
+              <Loader loading={carregandoDres} tip="">
+                <SelectComponent
+                  className="fonte-14"
+                  form={form}
+                  name="dreId"
+                  lista={listaDres}
+                  onChange={dre => setDreId(dre)}
+                  containerVinculoId="containerFiltro"
+                  valueOption="valor"
+                  valueText="desc"
+                  placeholder="Selecione a DRE"
+                />
+              </Loader>
             </Grid>
             <Grid cols={6}>
-              <SelectComponent
-                className="fonte-14"
-                form={form}
-                name="dreId"
-                lista={[]}
-                containerVinculoId="containerFiltro"
-                valueOption="valor"
-                valueText="desc"
-                placeholder="Selecione a UE"
-              />
+              <Loader loading={carregandoUes} tip="">
+                <SelectComponent
+                  className="fonte-14"
+                  form={form}
+                  name="ueId"
+                  lista={listaUes}
+                  onChange={ue => setUeId(ue)}
+                  containerVinculoId="containerFiltro"
+                  valueOption="valor"
+                  valueText="desc"
+                  placeholder="Selecione a UE"
+                />
+              </Loader>
             </Grid>
           </Linha>
           <Linha className="row mb-2">
