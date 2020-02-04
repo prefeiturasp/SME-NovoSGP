4acb4d3c6dbb628111b82a654b89aa9075d0ed12
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-04 16:50:07 -0300

    modelagem / refatoração / inicio map

---
+++ src/SME.SGP.Dados/Mapeamentos/FechamentoFinalMap.cs
@@ -0,0 +1,15 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class FechamentoFinalMap : BaseMap<FechamentoFinal>
+    {
+        public FechamentoFinalMap()
+        {
+            ToTable("fechamento_final");
+            Map(a => a.AusenciasCompensadas).ToColumn("ausencias_compensadas");
+            Map(a => a.AlunoCodigo).ToColumn("aluno_codigo");
+            Map(a => a.AlunoCodigo).ToColumn("aluno_codigo");
+        }
+    }
+}
