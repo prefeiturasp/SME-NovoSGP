52b8cbc9cee634b27dc1a8bbce0f9d81d5fbd2cb
Losi <rafael.losi@amcom.com.br>
2020-02-05 20:55:33 -0300

    11423 - Validações na persistencia de Fechamento Turma

--- src/SME.SGP.Dominio.Servicos/ServicoFechamentoTurmaDisciplina.cs
+++ src/SME.SGP.Dominio.Servicos/ServicoFechamentoTurmaDisciplina.cs
@@ -1,4 +1,6 @@
-﻿using SME.SGP.Dominio.Interfaces;
+﻿using SME.SGP.Aplicacao;
+using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,12 @@ namespace SME.SGP.Dominio.Servicos
         private readonly IRepositorioUe repositorioUe;
         private readonly IRepositorioPeriodoFechamento repositorioFechamento;
         private readonly IRepositorioTipoCalendario repositorioTipoCalendario;
+        private readonly IRepositorioTipoAvaliacao repositorioTipoAvaliacao;
+        private readonly IRepositorioAtividadeAvaliativaRegencia repositorioAtividadeAvaliativaRegencia;
+        private readonly IRepositorioAtividadeAvaliativaDisciplina repositorioAtividadeAvaliativaDisciplina;
+        private readonly IConsultasDisciplina consultasDisciplina;
+        private readonly IServicoEOL servicoEOL;
+        private readonly IServicoUsuario servicoUsuario;
         private readonly IUnitOfWork unitOfWork;
 
         public ServicoFechamentoTurmaDisciplina(IRepositorioFechamentoTurmaDisciplina repositorioFechamentoTurmaDisciplina,
@@ -24,6 +32,12 @@ namespace SME.SGP.Dominio.Servicos
                                                 IRepositorioUe repositorioUe,
                                                 IRepositorioPeriodoFechamento repositorioFechamento,
                                                 IRepositorioTipoCalendario repositorioTipoCalendario,
+                                                IRepositorioTipoAvaliacao repositorioTipoAvaliacao,
+                                                IRepositorioAtividadeAvaliativaRegencia repositorioAtividadeAvaliativaRegencia,
+                                                IRepositorioAtividadeAvaliativaDisciplina repositorioAtividadeAvaliativaDisciplina,
+                                                IConsultasDisciplina consultasDisciplina,
+                                                IServicoEOL servicoEOL,
+                                                IServicoUsuario servicoUsuario,
                                                 IUnitOfWork unitOfWork)
         {
             this.repositorioFechamentoTurmaDisciplina = repositorioFechamentoTurmaDisciplina ?? throw new ArgumentNullException(nameof(repositorioFechamentoTurmaDisciplina));
@@ -32,6 +46,12 @@ namespace SME.SGP.Dominio.Servicos
             this.repositorioUe = repositorioUe ?? throw new ArgumentNullException(nameof(repositorioUe));
             this.repositorioFechamento = repositorioFechamento ?? throw new ArgumentNullException(nameof(repositorioFechamento));
             this.repositorioTipoCalendario = repositorioTipoCalendario ?? throw new ArgumentNullException(nameof(repositorioTipoCalendario));
+            this.repositorioTipoAvaliacao = repositorioTipoAvaliacao ?? throw new ArgumentNullException(nameof(repositorioTipoAvaliacao));
+            this.repositorioAtividadeAvaliativaRegencia = repositorioAtividadeAvaliativaRegencia ?? throw new ArgumentNullException(nameof(repositorioAtividadeAvaliativaRegencia));
+            this.repositorioAtividadeAvaliativaDisciplina = repositorioAtividadeAvaliativaDisciplina ?? throw new ArgumentNullException(nameof(repositorioAtividadeAvaliativaDisciplina));
+            this.consultasDisciplina = consultasDisciplina ?? throw new ArgumentNullException(nameof(consultasDisciplina));
+            this.servicoEOL = servicoEOL ?? throw new ArgumentNullException(nameof(servicoEOL));
+            this.servicoUsuario = servicoUsuario ?? throw new ArgumentNullException(nameof(servicoUsuario));
             this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         }
 
@@ -39,6 +59,7 @@ namespace SME.SGP.Dominio.Servicos
         {
             var fechamentoTurma = MapearParaEntidade(id, entidadeDto);
 
+            // Valida periodo de fechamento
             var tipoCalendario = repositorioTipoCalendario.BuscarPorAnoLetivoEModalidade(fechamentoTurma.Turma.AnoLetivo
                                                                 , fechamentoTurma.Turma.ModalidadeCodigo == Modalidade.EJA ? ModalidadeTipoCalendario.EJA : ModalidadeTipoCalendario.FundamentalMedio
                                                                 , DateTime.Now.Month > 6 ? 2 : 1);
@@ -50,7 +71,15 @@ namespace SME.SGP.Dominio.Servicos
             if (fechamento == null || fechamentoBimestre == null)
                 throw new NegocioException("Não localizado período de fechamento em aberto para turma informada");
 
+            // Valida Permissão do Professor na Turma/Disciplina
+            VerificaSeProfessorPodePersistirTurma(servicoUsuario.ObterRf(), entidadeDto.TurmaId, fechamentoBimestre.PeriodoEscolar.PeriodoFim);
+
             fechamentoTurma.PeriodoFechamentoBimestreId = fechamentoBimestre.Id;
+
+            // Valida Avaliações no Bimestre
+            var validacoes = await ValidaMinimoAvaliacoesBimestre(tipoCalendario.Id, entidadeDto.TurmaId, entidadeDto.DisciplinaId, entidadeDto.Bimestre);
+            if (!string.IsNullOrEmpty(validacoes))
+                throw new NegocioException("Não é possível cadastrar nota do bimestre para turma e disciplina em questão: " + validacoes);
 
             // Carrega notas alunos
             var notasConceitosBimestre = await MapearParaEntidade(id, entidadeDto.NotaConceitoAlunos);
@@ -73,6 +102,45 @@ namespace SME.SGP.Dominio.Servicos
                 unitOfWork.Rollback();
                 throw e;
             }
+        }
+        private void VerificaSeProfessorPodePersistirTurma(string codigoRf, string turmaId, DateTime data)
+        {
+            if (!servicoUsuario.PodePersistirTurma(codigoRf, turmaId, data).Result)
+                throw new NegocioException("Você não pode fazer alterações ou inclusões nesta turma e data.");
+        }
+
+        private async Task<string> ValidaMinimoAvaliacoesBimestre(long tipoCalendarioId, string turmaId, long disciplinaId, int bimestre)
+        {
+            var validacoes = new StringBuilder();
+            var tipoAvaliacaoBimestral = await repositorioTipoAvaliacao.ObterTipoAvaliacaoBimestral();
+
+            var disciplinasEOL = servicoEOL.ObterDisciplinasPorIds(new long[] { disciplinaId });
+            if (disciplinasEOL.First().Regencia)
+            {
+                // Disciplinas Regencia de Classe
+                disciplinasEOL = await consultasDisciplina.ObterDisciplinasParaPlanejamento(new FiltroDisciplinaPlanejamentoDto()
+                {
+                    CodigoTurma = long.Parse(turmaId),
+                    CodigoDisciplina = int.Parse(disciplinaId.ToString()),
+                    Regencia = true
+                });
+
+                foreach (var disciplina in disciplinasEOL)
+                {
+                    var avaliacoes = await repositorioAtividadeAvaliativaRegencia.ObterAvaliacoesBimestrais(tipoCalendarioId, turmaId, disciplina.CodigoComponenteCurricular, bimestre);
+                    if ((avaliacoes == null) || (avaliacoes.Count() < tipoAvaliacaoBimestral.AvaliacoesNecessariasPorBimestre))
+                        validacoes.AppendLine($"A disciplina [{disciplina.Nome}] não tem o número mínimo de avaliações bimestral: {tipoAvaliacaoBimestral.AvaliacoesNecessariasPorBimestre}");
+                }
+            }
+            else
+            {
+                var disciplinaEOL = disciplinasEOL.First();
+                var avaliacoes = await repositorioAtividadeAvaliativaDisciplina.ObterAvaliacoesBimestrais(tipoCalendarioId, turmaId, disciplinaEOL.CodigoComponenteCurricular, bimestre);
+                if ((avaliacoes == null) || (avaliacoes.Count() < tipoAvaliacaoBimestral.AvaliacoesNecessariasPorBimestre))
+                    validacoes.AppendLine($"A disciplina [{disciplinaEOL.Nome}] não tem o número mínimo de avaliações bimestral: {tipoAvaliacaoBimestral.AvaliacoesNecessariasPorBimestre}");
+            }
+
+            return validacoes.ToString();
         }
 
         private async Task<IEnumerable<NotaConceitoBimestre>> MapearParaEntidade(long id, IEnumerable<NotaConceitoBimestreDto> notasConceitosAlunosDto)
