2fb5957c5d96f477ef1cb1a733f5b79ec63bcb47
Flávio Ricardo <flavioricardo@live.com>
2020-02-06 16:28:59 -0300

    AB#11338

---
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/PAP/ResumosGraficos/componentes/Resumos/componentes/TabelaTotalEstudantes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+// Ant
+import { Table } from 'antd';
+
+// Componentes
+import { Base } from '~/componentes';
+
+const servico = axios.create({
+  baseURL: 'http://demo7314211.mockable.io/api',
+});
+
+const TabelaTotalEstudantes = () => {
+  return 'olar';
+};
+
+export default TabelaTotalEstudantes;
