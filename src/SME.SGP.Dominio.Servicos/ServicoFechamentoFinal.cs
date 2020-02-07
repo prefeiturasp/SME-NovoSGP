b97bb417843b3bb0998473fa4a84c46781e97225
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-07 09:15:00 -0300

    Ajustes iniciais

---
+++ src/SME.SGP.Dominio.Servicos/ServicoFechamentoFinal.cs
@@ -0,0 +1,20 @@
+﻿using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dominio.Servicos
+{
+    public class ServicoFechamentoFinal : IServicoFechamentoFinal
+    {
+        private readonly IRepositorioFechamentoFinal repositorioFechamentoFinal;
+
+        public ServicoFechamentoFinal(IRepositorioFechamentoFinal repositorioFechamentoFinal)
+        {
+            this.repositorioFechamentoFinal = repositorioFechamentoFinal ?? throw new ArgumentNullException(nameof(repositorioFechamentoFinal));
+        }
+
+        public async Task SalvarAsync(FechamentoFinal fechamentoFinal)
+        {
+        }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/FechamentoFinalSalvarDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SME.SGP.Infra
+{
+    public class FechamentoFinalSalvarDto
+    {
+        public string AlunoRf { get; set; }
+        public string ComponenteCurricularCodigo { get; set; }
+        public string Conceito { get; set; }
+        public double Nota { get; set; }
+        public string TurmaCodigo { get; set; }
+
+        public bool EhNota()
+        {
+            return string.IsNullOrEmpty(Conceito);
+        }
+    }
+}
