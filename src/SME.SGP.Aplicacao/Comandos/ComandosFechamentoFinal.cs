e934334087a83288f01f29f94115d453d2dd8a1d
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-07 09:15:00 -0300

    Ajustes iniciais

--- src/SME.SGP.Aplicacao/Comandos/ComandosFechamentoFinal.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosFechamentoFinal.cs
@@ -1,6 +1,35 @@
-﻿namespace SME.SGP.Aplicacao
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
 {
     public class ComandosFechamentoFinal : IComandosFechamentoFinal
     {
+        private readonly IServicoFechamentoFinal fechamentoFinal;
+
+        public ComandosFechamentoFinal(IServicoFechamentoFinal fechamentoFinal)
+        {
+            this.fechamentoFinal = fechamentoFinal ?? throw new System.ArgumentNullException(nameof(fechamentoFinal));
+        }
+
+        public async Task SalvarAsync(FechamentoFinalSalvarDto fechamentoFinalSalvarDto)
+        {
+            var fechamentoFinal = TransformarDtoSalvarEmEntidade(fechamentoFinalSalvarDto);
+        }
+
+        private static FechamentoFinal TransformarDtoSalvarEmEntidade(FechamentoFinalSalvarDto fechamentoFinalSalvarDto)
+        {
+            var fechamentoFinal = new FechamentoFinal();
+            if (fechamentoFinalSalvarDto.EhNota())
+                fechamentoFinal.Nota = fechamentoFinalSalvarDto.Nota;
+            else
+            {
+                fechamentoFinal.Conceito
+            }
+
+            return fechamentoFinal;
+        }
     }
 }
