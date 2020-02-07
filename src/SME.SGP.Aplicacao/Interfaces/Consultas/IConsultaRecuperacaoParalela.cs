c5b3a374ae061619293007399be4c4c557096a50
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-02-07 17:20:50 -0300

    AB#11332

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultaRecuperacaoParalela.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultaRecuperacaoParalela.cs
@@ -7,5 +7,7 @@ namespace SME.SGP.Aplicacao
     public interface IConsultaRecuperacaoParalela
     {
         Task<RecuperacaoParalelaListagemDto> Listar(FiltroRecuperacaoParalelaDto filtro);
+
+        Task<RecuperacaoParalelaTotalEstudanteDto> TotalEstudantes(long dreId, long ueId, int cicloId, int turmaId, int ano);
     }
 }
