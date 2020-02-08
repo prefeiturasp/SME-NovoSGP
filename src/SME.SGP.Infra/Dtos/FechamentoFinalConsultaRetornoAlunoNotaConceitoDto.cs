9bf9c5fbcc01ce0fb0b059bbe427772e56ae9069
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-02-08 10:06:44 -0300

    adicionado disciplina

--- src/SME.SGP.Infra/Dtos/FechamentoFinalConsultaRetornoAlunoNotaConceitoDto.cs
+++ src/SME.SGP.Infra/Dtos/FechamentoFinalConsultaRetornoAlunoNotaConceitoDto.cs
@@ -3,6 +3,7 @@
     public class FechamentoFinalConsultaRetornoAlunoNotaConceitoDto
     {
         public int Bimestre { get; set; }
+        public string Disciplina { get; set; }
         public string DisciplinaCodigo { get; set; }
         public string NotaConceito { get; set; }
     }
