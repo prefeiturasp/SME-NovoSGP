49cd82a6da3d1b4535a728eba13657b74a27b1b4
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-02-08 09:56:08 -0300

    Ajustes

--- src/SME.SGP.Aplicacao/Comandos/ComandosFechamentoFinal.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosFechamentoFinal.cs
@@ -8,26 +8,47 @@ namespace SME.SGP.Aplicacao
     public class ComandosFechamentoFinal : IComandosFechamentoFinal
     {
         private readonly IServicoFechamentoFinal fechamentoFinal;
+        private readonly IRepositorioConceito repositorioConceito;
+        private readonly IRepositorioFechamentoFinal repositorioFechamentoFinal;
+        private readonly IRepositorioTurma repositorioTurma;
+        private readonly IServicoFechamentoFinal servicoFechamentoFinal;
 
-        public ComandosFechamentoFinal(IServicoFechamentoFinal fechamentoFinal, IRepositorioConceito repositorioConceito)
+        public ComandosFechamentoFinal(IServicoFechamentoFinal fechamentoFinal, IRepositorioConceito repositorioConceito,
+            IServicoFechamentoFinal servicoFechamentoFinal, IRepositorioTurma repositorioTurma, IRepositorioFechamentoFinal repositorioFechamentoFinal)
         {
             this.fechamentoFinal = fechamentoFinal ?? throw new System.ArgumentNullException(nameof(fechamentoFinal));
+            this.repositorioConceito = repositorioConceito ?? throw new System.ArgumentNullException(nameof(repositorioConceito));
+            this.servicoFechamentoFinal = servicoFechamentoFinal ?? throw new System.ArgumentNullException(nameof(servicoFechamentoFinal));
+            this.repositorioTurma = repositorioTurma ?? throw new System.ArgumentNullException(nameof(repositorioTurma));
+            this.repositorioFechamentoFinal = repositorioFechamentoFinal ?? throw new System.ArgumentNullException(nameof(repositorioFechamentoFinal));
         }
 
         public async Task SalvarAsync(FechamentoFinalSalvarDto fechamentoFinalSalvarDto)
         {
             var fechamentoFinal = TransformarDtoSalvarEmEntidade(fechamentoFinalSalvarDto);
+            await servicoFechamentoFinal.SalvarAsync(fechamentoFinal);
         }
 
-        private static FechamentoFinal TransformarDtoSalvarEmEntidade(FechamentoFinalSalvarDto fechamentoFinalSalvarDto)
+        private FechamentoFinal TransformarDtoSalvarEmEntidade(FechamentoFinalSalvarDto fechamentoFinalSalvarDto)
         {
-            var fechamentoFinal = new FechamentoFinal();
+            FechamentoFinal fechamentoFinal;//= new FechamentoFinal();
+
+            var fechamentosDaTurmaEDisciplina = repositorioFechamentoFinal.ObterPorFiltros(fechamentoFinalSalvarDto.TurmaCodigo, fechamentoFinalSalvarDto.ComponenteCurricularCodigo);
+
             if (fechamentoFinalSalvarDto.EhNota())
-                fechamentoFinal.Nota = fechamentoFinalSalvarDto.Nota;
+                fechamentoFinal.Nota = fechamentoFinalSalvarDto.Nota.Value;
             else
             {
-                fechamentoFinal.Conceito =
+                var conceito = repositorioConceito.ObterPorId(fechamentoFinalSalvarDto.ConceitoId.Value);
+                fechamentoFinal.Conceito = conceito ?? throw new NegocioException("Não foi possível localizar o conceito.");
             }
+
+            var turma = repositorioTurma.ObterPorCodigo(fechamentoFinalSalvarDto.TurmaCodigo);
+            if (turma == null)
+                throw new NegocioException("Não foi possível localizar a turma.");
+
+            fechamentoFinal.AtualizarTurma(turma);
+            fechamentoFinal.DisciplinaCodigo = fechamentoFinalSalvarDto.ComponenteCurricularCodigo;
 
             return fechamentoFinal;
         }
--- src/SME.SGP.Infra/Dtos/FechamentoFinalSalvarDto.cs
+++ src/SME.SGP.Infra/Dtos/FechamentoFinalSalvarDto.cs
@@ -4,13 +4,13 @@
     {
         public string AlunoRf { get; set; }
         public string ComponenteCurricularCodigo { get; set; }
-        public string Conceito { get; set; }
-        public double Nota { get; set; }
+        public long? ConceitoId { get; set; }
+        public double? Nota { get; set; }
         public string TurmaCodigo { get; set; }
 
         public bool EhNota()
         {
-            return string.IsNullOrEmpty(Conceito);
+            return ConceitoId.HasValue;
         }
     }
 }
