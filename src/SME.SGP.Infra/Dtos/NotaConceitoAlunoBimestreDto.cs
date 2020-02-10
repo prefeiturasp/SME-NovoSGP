18197d8e39966bacc9f12ee6a811727ecb34a90c
Eliton Santos <eliton.santos@amcom.com.br>
2020-02-10 20:32:28 -0300

    endpoint de persistencia

---
+++ src/SME.SGP.Infra/Dtos/NotaConceitoAlunoBimestreDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class NotaConceitoAlunoBimestreDto
+    {
+        public string AlunoCodigo { get; set; }
+        public string AlunoNome { get; set; }
+        public IEnumerable<NotaConceitoBimestreRetornoDto> Notas { get; set; }
+        public int QuantidadeFaltas { get; set; }
+        public int QuantidadeCompensacoes { get; set; }
+        public double PercentualFrequencia { get; set; }
+    }
+}
