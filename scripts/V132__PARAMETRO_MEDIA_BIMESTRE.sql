e7fd000e353cd15ae4395d163a02e6d58dfab4fa
Rafael Losi <rafael.losi@amcom.com.br>
2020-02-10 14:02:38 -0300

    9269 - Correção do encoding

--- scripts/V132__PARAMETRO_MEDIA_BIMESTRE.sql
+++ scripts/V132__PARAMETRO_MEDIA_BIMESTRE.sql
@@ -1,6 +1,6 @@
-insert into 
+ï»¿insert into 
     public.parametros_sistema (nome,descricao,valor,ano,criado_em,criado_por, criado_rf,tipo)
-select 'MediaBimestre','Média final para aprovação no bimestre','5',null,now(),'Carga Inicial','Carga Inicial', 14
+select 'MediaBimestre','MÃ©dia final para aprovaÃ§Ã£o no bimestre','5',null,now(),'Carga Inicial','Carga Inicial', 14
 where  not exists(
 	select 1
 	from public.parametros_sistema
