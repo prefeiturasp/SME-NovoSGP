e81c029d871044afff457bccaf2e2b541d2c238a
Alexsander Camargo <csalexsander@gmail.com>
2020-02-10 13:37:51 -0300

    Correção deploy Dev-R2
    AB#12041

--- teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasNotasConceitosTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasNotasConceitosTeste.cs
@@ -8,22 +8,31 @@ namespace SME.SGP.Aplicacao.Teste.Consultas
 {
     public class ConsultasNotasConceitosTeste
     {
-        private readonly Mock<IConsultaAtividadeAvaliativa> consultaAtividadeAvaliativa;
         private readonly ConsultasNotasConceitos consultasNotasConceito;
+        private readonly Mock<IConsultaAtividadeAvaliativa> consultasAtividadeAvaliativa;
+        private readonly Mock<IConsultasDisciplina> consultasDisciplina;
+        private readonly Mock<IConsultasFechamentoTurmaDisciplina> consultasFechamentoTurmaDisciplina;
+        private readonly Mock<IRepositorioTipoCalendario> repositorioTipoCalendario;
+        private readonly Mock<IRepositorioPeriodoEscolar> repositorioPeriodoEscolar;
         private readonly Mock<IRepositorioAtividadeAvaliativa> repositorioAtividadeAvaliativa;
+        private readonly Mock<IRepositorioAtividadeAvaliativaDisciplina> repositorioAtividadeAvaliativaDisciplina;
         private readonly Mock<IRepositorioFrequencia> repositorioFrequencia;
+        private readonly Mock<IRepositorioFrequenciaAlunoDisciplinaPeriodo> repositorioFrequenciaAluno;
         private readonly Mock<IRepositorioNotaParametro> repositorioNotaParametro;
         private readonly Mock<IRepositorioNotasConceitos> repositorioNotasConceitos;
+        private readonly Mock<IRepositorioParametrosSistema> repositorioParametrosSistema;
+        private readonly Mock<IRepositorioConceito> repositorioConceito;
+        private readonly Mock<IRepositorioTipoAvaliacao> repositorioTipoAvaliacao;
         private readonly Mock<IServicoAluno> servicoAluno;
         private readonly Mock<IServicoDeNotasConceitos> servicoDeNotasConceitos;
         private readonly Mock<IServicoEOL> servicoEOL;
         private readonly Mock<IServicoUsuario> servicoUsuario;
-        private readonly Mock<IRepositorioAtividadeAvaliativaDisciplina> repositorioAtividadeAvaliativaDisciplina;
+
 
         public ConsultasNotasConceitosTeste()
         {
             servicoEOL = new Mock<IServicoEOL>();
-            consultaAtividadeAvaliativa = new Mock<IConsultaAtividadeAvaliativa>();
+            consultasAtividadeAvaliativa = new Mock<IConsultaAtividadeAvaliativa>();
             servicoDeNotasConceitos = new Mock<IServicoDeNotasConceitos>();
             repositorioNotasConceitos = new Mock<IRepositorioNotasConceitos>();
             repositorioFrequencia = new Mock<IRepositorioFrequencia>();
@@ -32,9 +41,17 @@ namespace SME.SGP.Aplicacao.Teste.Consultas
             repositorioNotaParametro = new Mock<IRepositorioNotaParametro>();
             repositorioAtividadeAvaliativa = new Mock<IRepositorioAtividadeAvaliativa>();
             repositorioAtividadeAvaliativaDisciplina = new Mock<IRepositorioAtividadeAvaliativaDisciplina>();
+            repositorioTipoCalendario = new Mock<IRepositorioTipoCalendario>();
 
-            consultasNotasConceito = new ConsultasNotasConceitos(servicoEOL.Object, consultaAtividadeAvaliativa.Object, servicoDeNotasConceitos.Object, repositorioNotasConceitos.Object,
-                repositorioFrequencia.Object, servicoUsuario.Object, servicoAluno.Object, repositorioNotaParametro.Object, repositorioAtividadeAvaliativa.Object, repositorioAtividadeAvaliativaDisciplina.Object);
+            consultasNotasConceito = new ConsultasNotasConceitos(servicoEOL.Object, consultasAtividadeAvaliativa.Object,
+            consultasFechamentoTurmaDisciplina.Object, consultasDisciplina.Object,
+            servicoDeNotasConceitos.Object, repositorioNotasConceitos.Object,
+            repositorioFrequencia.Object, repositorioFrequenciaAluno.Object,
+            servicoUsuario.Object, servicoAluno.Object, repositorioTipoCalendario.Object,
+            repositorioNotaParametro.Object, repositorioAtividadeAvaliativa.Object,
+            repositorioAtividadeAvaliativaDisciplina.Object, repositorioConceito.Object,
+            repositorioPeriodoEscolar.Object, repositorioParametrosSistema.Object,
+            repositorioTipoAvaliacao.Object);
         }
 
         [Theory]
