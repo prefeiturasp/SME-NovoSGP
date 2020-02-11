e68c343b67fd8e843fd5072a46358f47b71a01aa
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-02-11 16:14:24 -0300

    Correção recuperação paralela
    AB#12119

--- src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
@@ -187,9 +187,9 @@ namespace SME.SGP.Aplicacao.Integracoes
             var alunos = new List<AlunoPorTurmaResposta>();
             var resposta = await httpClient.GetAsync($"turmas/{turmaId}/alunos-ativos");
 
-            if (resposta.StatusCode == HttpStatusCode.NoContent || !resposta.IsSuccessStatusCode)
             if (!resposta.IsSuccessStatusCode)
                 throw new NegocioException($"Não foi encontrado alunos ativos para a turma {turmaId}");
+
             if (resposta.StatusCode == HttpStatusCode.NoContent)
                 return alunos;
 
