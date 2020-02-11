0418a472f16ae28792ce59f706ce41e676f4d61a
eliton-amcom <eliton.santos@amcom.com.br>
2020-02-11 03:04:12 -0300

    criação e consumo de endpoint de busca de lista

--- src/SME.SGP.WebClient/src/servicos/Paginas/Fechamento/ServicoFechamentoBimestre.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Fechamento/ServicoFechamentoBimestre.js
@@ -1,8 +1,10 @@
 import api from '~/servicos/api';
 
 const ServicoFechamentoBimestre = {
-  buscarDados() {
-    return api.get('/v1/fechamento/bimestre');
+  buscarDados(turmaCodigo, disciplinaCodigo, bimestre) {
+    return api.get(
+      `/v1/fechamentos/turmas?turmaCodigo=${turmaCodigo}&disciplinaCodigo=${disciplinaCodigo}&bimestre=${bimestre}`
+    );
   },
 };
 
