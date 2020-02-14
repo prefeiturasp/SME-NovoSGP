f11ca64593cb6d13b3090c205c4d6d70d56c2efb
Rafael Losi <rafael.losi@amcom.com.br>
2020-02-14 13:32:19 -0300

    11469 - Pendencia x Fechamento

--- src/SME.SGP.Dados/Repositorios/RepositorioFechamento.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioFechamento.cs
@@ -1,33 +1,14 @@
-﻿using Dapper;
-using SME.SGP.Dominio;
+﻿using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
 namespace SME.SGP.Dados.Repositorios
 {
-    public class RepositorioFechamento : RepositorioBase<Fechamento>, IRepositorioFechamento
+    public class RepositorioPendenciaFechamento : RepositorioBase<PendenciaFechamento>, IRepositorioPendenciaFechamento
     {
-        public RepositorioFechamento(ISgpContext database) : base(database)
+        public RepositorioPendenciaFechamento(ISgpContext database) : base(database)
         {
         }
 
-        public Fechamento ObterPorTurmaDisciplinaPeriodo(long turmaId, string disciplinaId, long periodoEscolarId)
-        {
-            var query = @"select
-	                            *
-                            from
-	                            fechamento
-                            where
-                                turma_id = @turmaId
-                                and disciplina_id = @disciplinaId
-                                and periodo_escolar_id = @periodoEscolarId";
-
-            return database.Conexao.QueryFirstOrDefault<Fechamento>(query, new
-            {
-                turmaId,
-                disciplinaId,
-                periodoEscolarId
-            });
-        }
     }
 }
