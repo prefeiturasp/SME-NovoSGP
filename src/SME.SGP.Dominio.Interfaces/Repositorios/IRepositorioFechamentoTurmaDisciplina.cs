3422fa05e52f844f34f8924cce99e3da3700d507
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-02-15 15:56:43 -0300

    Refatoração da consulta

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioFechamentoTurmaDisciplina.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioFechamentoTurmaDisciplina.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using SME.SGP.Infra;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using SME.SGP.Infra;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioFechamentoTurmaDisciplina : IRepositorioBase<FechamentoTurmaDisciplina>
     {
-        Task<IEnumerable<NotaConceitoBimestreDto>> ObterNotasBimestre(string codigoAluno, long fechamentoTurmaId);
+        Task<IEnumerable<FechamentoTurmaDisciplina>> ObterFechamentosTurmaDisciplinas(long turmaId, string[] disciplinasId, int bimestre);
+
         Task<FechamentoTurmaDisciplina> ObterFechamentoTurmaDisciplina(string turmaId, long disciplinaId, int bimestre);
+
+        Task<IEnumerable<NotaConceitoBimestreDto>> ObterNotasBimestre(string codigoAluno, long fechamentoTurmaId);
     }
 }
