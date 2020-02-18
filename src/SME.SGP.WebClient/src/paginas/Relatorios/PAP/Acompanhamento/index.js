1f48e63525f04027e32e169a62da019768474f93
Flávio Ricardo <flavioricardo@live.com>
2020-02-18 11:41:50 -0300

    AB#12424

--- src/SME.SGP.WebClient/src/paginas/Relatorios/PAP/Acompanhamento/index.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/PAP/Acompanhamento/index.js
@@ -150,9 +150,17 @@ function RelatorioPAPAcompanhamento() {
     } catch (err) {
       setCarregando(false);
 
-      if (err.response)
-        erro(`Não foi possível completar a requisição: ${JSON.stringify(err)}`);
-      else erro('Ocorreu um erro interno, por favor contate o suporte');
+      if (err.response) {
+        const { data } = err.response;
+        if (data) {
+          const { mensagens } = data;
+          erro(`${mensagens[0]}`);
+        } else {
+          erro('Não foi possível completar a requisição');
+        }
+      } else {
+        erro('Ocorreu um erro interno, por favor contate o suporte');
+      }
     }
   };
 
