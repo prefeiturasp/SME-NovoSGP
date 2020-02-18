90119cdba9bb8f684a07bb0016c7268dfe6c9fbd
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-02-18 13:32:00 -0300

    AB#12427

--- src/SME.SGP.Infra/Enumerados/Permissao.cs
+++ src/SME.SGP.Infra/Enumerados/Permissao.cs
@@ -351,7 +351,7 @@
         [PermissaoMenu(Menu = "Compensação de Ausência", Icone = "", Agrupamento = "Diário de Classe", OrdemAgrupamento = 1, OrdemMenu = 6, EhAlteracao = true, Url = "/diario-classe/compensacao-ausencia")]
         CA_A = 115,
 
-        [PermissaoMenu(Menu = "Acompanhamento PAP", Icone = "fas fa-file-alt", Agrupamento = "Relatórios", OrdemAgrupamento = 7, OrdemMenu = 1, EhConsulta = true, Url = "/relatorios/pap/acompanhamento")]
+        [PermissaoMenu(Menu = "Preenchimento PAP", Icone = "fas fa-file-alt", Agrupamento = "Relatórios", OrdemAgrupamento = 7, OrdemMenu = 1, EhConsulta = true, Url = "/relatorios/pap/acompanhamento")]
         RAP_C = 116,
 
         [PermissaoMenu(Menu = "Resumos e gráficos PAP", Icone = "fas fa-file-alt", Agrupamento = "Relatórios", OrdemAgrupamento = 7, OrdemMenu = 2, EhConsulta = true, Url = "/relatorios/pap/resumos-e-graficos")]
