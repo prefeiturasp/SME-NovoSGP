c774949f972b2017e681d84bba999d01d98e6a0c
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-02-18 16:05:27 -0300

    Ajuste grupos sgp

--- src/SME.SGP.Infra/Enumerados/Abrangencia.cs
+++ src/SME.SGP.Infra/Enumerados/Abrangencia.cs
@@ -6,11 +6,11 @@ namespace SME.SGP.Infra.Enumerados
 {
     public enum Abrangencia
     {
-        UE,
-        Professor,
-        UeTurmasDisciplinas,
-        Dre,
-        DreEscolasAtribuidas,
-        SME
+        UE = 1,
+        Professor = 2,
+        UeTurmasDisciplinas = 3,
+        Dre = 4,
+        DreEscolasAtribuidas = 5,
+        SME = 6
     }
 }
