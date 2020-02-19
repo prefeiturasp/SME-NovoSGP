92f41cad47333c5b1404993700a67b2549fe58b7
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-19 11:19:41 -0300

    Ajustes removendo itens de desenvolvimento

--- src/SME.SGP.Infra/Dtos/AbrangenciaCargoRetornoEolDTO.cs
+++ src/SME.SGP.Infra/Dtos/AbrangenciaCargoRetornoEolDTO.cs
@@ -6,13 +6,10 @@ namespace SME.SGP.Dto
     public class AbrangenciaCargoRetornoEolDTO
     {
         public Abrangencia Abrangencia { get; set; }
-        public long[] CargosId { get; set; }
+        public string CargosId { get; set; }
         public int CdTipoFuncaoAtividade { get; set; }
         public GruposSGP Grupo { get; set; }
         public Guid GrupoID { get; set; }
-
-        //VOLTAR PARA STRING EM PROD!!!!
-        //public string CargosId { get; set; }
         public int? TipoFuncaoAtividade { get; set; }
     }
 }
