2c8ab21113d71ada15b1386d24e121701e935fae
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-20 15:01:34 -0300

    ajustes build

--- src/SME.SGP.Dados/Repositorios/RepositorioComponenteCurricular.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioComponenteCurricular.cs
@@ -1,5 +1,4 @@
-﻿using SME.SGP.Dados.Contexto;
-using SME.SGP.Dominio;
+﻿using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
@@ -9,12 +8,6 @@ namespace SME.SGP.Dados.Repositorios
     {
         public RepositorioComponenteCurricular(ISgpContext conexao) : base(conexao)
         {
-        }
-
-        public IEnumerable<ComponenteCurricular> ObterComponentesJuremaPorCodigoEol(long codigoEol)
-        {
-            var query = "select * from componente_curricular where codigo_eol = @codigoEol";
-            return database.Conexao.Query<ComponenteCurricular>(query, new { codigoEol });
         }
     }
 }
