e93e2e1a2b4f1dc00cdf39944072bf0fa55e485d
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-20 20:37:23 -0300

    refatoração de conceito para conceitoid; abrangencia compacta com list<int> para cargos ids

--- src/SME.SGP.Infra/Dtos/AbrangenciaCargoRetornoEolDTO.cs
+++ src/SME.SGP.Infra/Dtos/AbrangenciaCargoRetornoEolDTO.cs
@@ -1,12 +1,13 @@
 ﻿using SME.SGP.Infra.Enumerados;
 using System;
+using System.Collections.Generic;
 
 namespace SME.SGP.Dto
 {
     public class AbrangenciaCargoRetornoEolDTO
     {
         public Abrangencia Abrangencia { get; set; }
-        public string CargosId { get; set; }
+        public List<int> CargosId { get; set; }
         public int CdTipoFuncaoAtividade { get; set; }
         public GruposSGP Grupo { get; set; }
         public Guid GrupoID { get; set; }
