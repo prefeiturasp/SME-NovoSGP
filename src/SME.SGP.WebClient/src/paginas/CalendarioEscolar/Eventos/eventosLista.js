ce02477bc54b556868e0ce60e3b83cc4ebf914b6
eliton-amcom <eliton.santos@amcom.com.br>
2020-02-25 15:35:42 -0300

    Correção breadcrumb

--- src/SME.SGP.WebClient/src/paginas/CalendarioEscolar/Eventos/eventosLista.js
+++ src/SME.SGP.WebClient/src/paginas/CalendarioEscolar/Eventos/eventosLista.js
@@ -24,6 +24,7 @@ import ServicoEvento from '~/servicos/Paginas/Calendario/ServicoEvento';
 import FiltroHelper from '~/componentes-sgp/filtro/helper';
 import tipoEscolaDTO from '~/dtos/tipoEscolaDto';
 import { Loader } from '~/componentes';
+import { setBreadcrumbManual } from '~/servicos/breadcrumb-services';
 
 const EventosLista = ({ match }) => {
   const usuario = useSelector(store => store.usuario);
@@ -336,7 +337,7 @@ const EventosLista = ({ match }) => {
         'Excluir evento',
         listaNomeExcluir,
         `Deseja realmente excluir ${
-          eventosSelecionados.length > 1 ? 'estes eventos' : 'este evento'
+        eventosSelecionados.length > 1 ? 'estes eventos' : 'este evento'
         }?`,
         'Excluir',
         'Cancelar'
@@ -351,7 +352,7 @@ const EventosLista = ({ match }) => {
             eventosSelecionados.length > 1
               ? 'Eventos excluídos'
               : 'Evento excluído'
-          } com sucesso.`;
+            } com sucesso.`;
           sucesso(mensagemSucesso);
           validarFiltrar();
           setEventosSelecionados([]);
@@ -406,6 +407,11 @@ const EventosLista = ({ match }) => {
     if (tipoCalendarioId) {
       setSelecionouCalendario(true);
       filtrar('tipoCalendarioId', tipoCalendarioId);
+      setBreadcrumbManual(
+        `${match.url}/${tipoCalendarioId}`,
+        '',
+        '/calendario-escolar/eventos'
+      );
     } else {
       setFiltroValido(false);
       setSelecionouCalendario(false);
@@ -593,8 +599,8 @@ const EventosLista = ({ match }) => {
               filtroEhValido={filtroValido.valido}
             />
           ) : (
-            ''
-          )}
+              ''
+            )}
         </div>
       </Card>
     </>
