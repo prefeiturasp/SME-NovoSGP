fcb40eb5c38035682936cd05d78933f71376574e
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-26 18:48:37 -0300

    Ajuste de normalização de datas

--- src/SME.SGP.WebClient/src/index.js
+++ src/SME.SGP.WebClient/src/index.js
@@ -5,5 +5,6 @@ import './bibliotecas/fontawesome';
 import './bibliotecas/moment';
 import 'antd/dist/antd.css';
 import App from './App';
+import './bibliotecas/date';
 
 ReactDOM.render(<App />, document.getElementById('root'));
