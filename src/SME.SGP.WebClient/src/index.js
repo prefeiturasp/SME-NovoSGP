b66f523a945212870f6e35b49e0254c1ab437af0
Marcos Lobo <marcos.costa@amcom.com.br>
2020-02-27 14:09:58 -0300

    Adicionando sobrescrita global para campos dates no stringify.

--- src/SME.SGP.WebClient/src/index.js
+++ src/SME.SGP.WebClient/src/index.js
@@ -5,6 +5,5 @@ import './bibliotecas/fontawesome';
 import './bibliotecas/moment';
 import 'antd/dist/antd.css';
 import App from './App';
-import './bibliotecas/date';
 
 ReactDOM.render(<App />, document.getElementById('root'));
