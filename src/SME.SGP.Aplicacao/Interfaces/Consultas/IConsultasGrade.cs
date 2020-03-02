9f1405a953cb6cb73ad93bc64621913c59dfcdc2
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-03-02 19:32:19 -0300

    Ajuste endpoint grade

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasGrade.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasGrade.cs
@@ -7,7 +7,7 @@ namespace SME.SGP.Aplicacao
 {
     public interface IConsultasGrade
     {
-        Task<GradeComponenteTurmaAulasDto> ObterGradeAulasTurmaProfessor(string turmaCodigo, long disciplina, int semana, DateTime dataAula, string codigoRf = null);
+        Task<GradeComponenteTurmaAulasDto> ObterGradeAulasTurmaProfessor(string turmaCodigo, long disciplina, int semana, DateTime dataAula, string codigoRf = null, bool ehRegencia = false);
 
         Task<GradeDto> ObterGradeTurma(TipoEscola tipoEscola, Modalidade modalidade, int duracao);
 
