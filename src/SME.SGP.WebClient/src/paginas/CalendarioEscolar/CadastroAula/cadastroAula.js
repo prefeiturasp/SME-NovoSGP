94783baf0b8f35fd13cf4fd2ade035b9ce150ccd
Cleiton Dias Dutra <cleiton.dutra@amcom.com.br>
2020-03-04 14:03:03 -0300

    Cadastro de aula - Exibe mensagem de erro depois de clicar no botão Voltar

--- src/SME.SGP.WebClient/src/paginas/CalendarioEscolar/CadastroAula/cadastroAula.js
+++ src/SME.SGP.WebClient/src/paginas/CalendarioEscolar/CadastroAula/cadastroAula.js
@@ -576,7 +576,7 @@ const CadastroAula = ({ match }) => {
     }
   };
 
-  const onClickVoltar = async () => {
+  const onClickVoltar = async form => {
     if (modoEdicao && !somenteLeitura) {
       const confirmado = await confirmar(
         'Atenção',
@@ -587,7 +587,7 @@ const CadastroAula = ({ match }) => {
       );
 
       if (confirmado) {
-        onClickCadastrar(refForm.state.values);
+        validaAntesDoSubmit(form);
       } else {
         history.push('/calendario-escolar/calendario-professor');
       }
@@ -756,7 +756,7 @@ const CadastroAula = ({ match }) => {
                     color={Colors.Azul}
                     border
                     className="mr-2"
-                    onClick={onClickVoltar}
+                    onClick={() => onClickVoltar(form)}
                   />
                   <Button
                     id={shortid.generate()}
