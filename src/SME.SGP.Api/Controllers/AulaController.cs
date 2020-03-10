46f364c6be35833a92fe69f1c2fe3264ba431d57
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-03-10 15:46:07 -0300

    Update AulaController.cs

--- src/SME.SGP.Api/Controllers/AulaController.cs
+++ src/SME.SGP.Api/Controllers/AulaController.cs
@@ -68,16 +68,14 @@ namespace SME.SGP.Api.Controllers
             var quantidadeAulas = recorrencia == (int)RecorrenciaAula.AulaUnica ? 1
                 : await consultas.ObterQuantidadeAulasRecorrentes(aulaId, RecorrenciaAula.RepetirTodosBimestres);
             var existeFrequenciaPlanoAula = await consultas.ChecarFrequenciaPlanoNaRecorrencia(aulaId);
-
-            var retorno = new AulaRecorrenciaDto()
+            
+            return Ok(new AulaRecorrenciaDto()
             {
                 AulaId = aulaId,
                 RecorrenciaAula = recorrencia,
                 QuantidadeAulasRecorrentes = quantidadeAulas,
                 ExisteFrequenciaOuPlanoAula = existeFrequenciaPlanoAula
-            };
-
-            return Ok(retorno);
+            });
         }
     }
 }
