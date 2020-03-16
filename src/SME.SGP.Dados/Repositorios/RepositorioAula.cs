4d20194605b448e83b27d49074fc9dd989ec6fc7
Eliton Santos <eliton.santos@amcom.com.br>
2020-03-16 16:32:03 -0300

    retirar metodos nao utilizados

--- src/SME.SGP.Dados/Repositorios/RepositorioAula.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioAula.cs
@@ -52,26 +52,6 @@ namespace SME.SGP.Dados.Repositorios
             });
         }
 
-
-        public async Task<IEnumerable<DateTime>> ObterDatasAulasExistentes(List<DateTime> datas, string turmaId, string disciplinaId, string professorRf)
-        {
-            var query = @"select DATE(data_aula)
-                 from aula
-                where not excluido
-                  and DATE(data_aula) = ANY(@datas)
-                  and turma_id = @turmaId
-                  and disciplina_id = @disciplinaId
-                  and professor_rf = @professorRf";
-
-            return (await database.Conexao.QueryAsync<DateTime>(query.ToString(), new
-            {
-                datas,
-                turmaId,
-                disciplinaId,
-                professorRf
-            }));
-        }
-
         public async Task<AulaConsultaDto> ObterAulaIntervaloTurmaDisciplina(DateTime dataInicio, DateTime dataFim, string turmaId, long atividadeAvaliativaId)
         {
             var query = @"select *
