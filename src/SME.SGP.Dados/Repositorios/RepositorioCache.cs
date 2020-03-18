0d9941c2f248c04e01b25bb3f87f280bfba24bc6
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-03-18 12:04:18 -0300

    Dispose e refatorações

--- src/SME.SGP.Dados/Repositorios/RepositorioCache.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioCache.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using StackExchange.Redis;
@@ -9,19 +10,13 @@ namespace SME.SGP.Dados.Repositorios
 {
     public class RepositorioCache : IRepositorioCache
     {
-        //private readonly Lazy<ConnectionMultiplexer> lazyConnection;
-
-        //private readonly IDistributedCache distributedCache;
+        private readonly IConfiguration configuration;
         private readonly IServicoLog servicoLog;
 
-        public RepositorioCache(IServicoLog servicoLog)
+        public RepositorioCache(IServicoLog servicoLog, IConfiguration configuration)
         {
-            //this.distributedCache = distributedCache ?? throw new System.ArgumentNullException(nameof(distributedCache));
             this.servicoLog = servicoLog ?? throw new System.ArgumentNullException(nameof(servicoLog));
-            //this.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
-            //    {
-            //        return ConnectionMultiplexer.Connect("localhost");
-            //    });
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         public string Obter(string nomeChave)
@@ -29,14 +24,14 @@ namespace SME.SGP.Dados.Repositorios
             var inicioOperacao = DateTime.UtcNow;
             var timer = System.Diagnostics.Stopwatch.StartNew();
 
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
+                conexao = ObterConexao();
 
                 //var cacheParaRetorno = distributedCache.GetString(nomeChave);
-                var cacheParaRetorno = lazyConnection.Value.GetDatabase().StringGet(nomeChave);
+                var cacheParaRetorno = conexao.Value.GetDatabase().StringGet(ObterChaveInstancia(nomeChave));
                 timer.Stop();
                 servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Obtendo", inicioOperacao, timer.Elapsed, true);
 
@@ -53,26 +48,21 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
         }
 
         public T Obter<T>(string nomeChave)
         {
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
+                conexao = ObterConexao();
 
                 //var stringCache = distributedCache.GetString(nomeChave);
 
-                var stringCache = lazyConnection.Value.GetDatabase().StringGet(nomeChave);
+                var stringCache = conexao.Value.GetDatabase().StringGet(ObterChaveInstancia(nomeChave));
 
                 if (!string.IsNullOrWhiteSpace(stringCache))
                     return JsonConvert.DeserializeObject<T>(stringCache);
@@ -84,36 +74,27 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
             return default(T);
         }
 
         public async Task<T> Obter<T>(string nomeChave, Func<Task<T>> buscarDados, int minutosParaExpirar = 720)
         {
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                //var stringCache = distributedCache.GetString(nomeChave);
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
+                conexao = ObterConexao();
 
-                var stringCache = lazyConnection.Value.GetDatabase().StringGet(nomeChave);
+                var stringCache = conexao.Value.GetDatabase().StringGet(ObterChaveInstancia(nomeChave));
 
                 if (stringCache.HasValue)
                     return JsonConvert.DeserializeObject<T>(stringCache);
 
-                //if (!string.IsNullOrWhiteSpace(stringCache))
-                //    return JsonConvert.DeserializeObject<T>(stringCache);
-
                 var dados = await buscarDados();
 
-                await lazyConnection.Value.GetDatabase().StringSetAsync(nomeChave, JsonConvert.SerializeObject(dados), TimeSpan.FromMinutes(minutosParaExpirar));
+                await conexao.Value.GetDatabase().StringSetAsync(ObterChaveInstancia(nomeChave), JsonConvert.SerializeObject(dados), TimeSpan.FromMinutes(minutosParaExpirar));
 
                 //await distributedCache.SetStringAsync(nomeChave, JsonConvert.SerializeObject(dados), new DistributedCacheEntryOptions()
                 //                                .SetAbsoluteExpiration(TimeSpan.FromMinutes(minutosParaExpirar)));
@@ -127,12 +108,7 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
         }
 
@@ -141,14 +117,13 @@ namespace SME.SGP.Dados.Repositorios
             var inicioOperacao = DateTime.UtcNow;
             var timer = System.Diagnostics.Stopwatch.StartNew();
 
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
-                //var cacheParaRetorno = await distributedCache.GetStringAsync(nomeChave);
+                conexao = ObterConexao();
 
-                var cacheParaRetorno = await lazyConnection.Value.GetDatabase().StringGetAsync(nomeChave);
+                var cacheParaRetorno = await conexao.Value.GetDatabase().StringGetAsync(ObterChaveInstancia(nomeChave));
 
                 timer.Stop();
                 servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Obtendo async", inicioOperacao, timer.Elapsed, true);
@@ -164,12 +139,7 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
         }
 
@@ -178,14 +148,13 @@ namespace SME.SGP.Dados.Repositorios
             var inicioOperacao = DateTime.UtcNow;
             var timer = System.Diagnostics.Stopwatch.StartNew();
 
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
+                conexao = ObterConexao();
 
-                //await distributedCache.RemoveAsync(nomeChave);
-                await lazyConnection.Value.GetDatabase().KeyDeleteAsync(nomeChave);
+                await conexao.Value.GetDatabase().KeyDeleteAsync(ObterChaveInstancia(nomeChave));
 
                 timer.Stop();
                 servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Remover async", inicioOperacao, timer.Elapsed, true);
@@ -199,24 +168,19 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
         }
 
         public void Salvar(string nomeChave, string valor, int minutosParaExpirar = 720)
         {
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
+                conexao = ObterConexao();
 
-                lazyConnection.Value.GetDatabase().StringSet(nomeChave, JsonConvert.SerializeObject(valor), TimeSpan.FromMinutes(minutosParaExpirar));
+                conexao.Value.GetDatabase().StringSet(ObterChaveInstancia(nomeChave), JsonConvert.SerializeObject(valor), TimeSpan.FromMinutes(minutosParaExpirar));
                 //distributedCache.SetString(nomeChave, valor, new DistributedCacheEntryOptions()
                 //                                .SetAbsoluteExpiration(TimeSpan.FromMinutes(minutosParaExpirar)));
             }
@@ -227,12 +191,7 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
         }
 
@@ -241,16 +200,13 @@ namespace SME.SGP.Dados.Repositorios
             var inicioOperacao = DateTime.UtcNow;
             var timer = System.Diagnostics.Stopwatch.StartNew();
 
-            Lazy<ConnectionMultiplexer> lazyConnection = null;
+            Lazy<ConnectionMultiplexer> conexao = null;
 
             try
             {
-                lazyConnection = new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect("localhost"); });
+                conexao = ObterConexao();
 
-                await lazyConnection.Value.GetDatabase().StringSetAsync(nomeChave, valor, TimeSpan.FromMinutes(minutosParaExpirar));
-
-                //await distributedCache.SetStringAsync(nomeChave, valor, new DistributedCacheEntryOptions()
-                //                                .SetAbsoluteExpiration(TimeSpan.FromMinutes(minutosParaExpirar)));
+                await conexao.Value.GetDatabase().StringSetAsync(ObterChaveInstancia(nomeChave), valor, TimeSpan.FromMinutes(minutosParaExpirar));
 
                 timer.Stop();
                 servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Salvar async", inicioOperacao, timer.Elapsed, true);
@@ -264,13 +220,27 @@ namespace SME.SGP.Dados.Repositorios
             }
             finally
             {
-                if (lazyConnection.Value != null)
-                {
-                    lazyConnection.Value.Close();
-                    lazyConnection.Value.Dispose();
-                    lazyConnection = null;
-                }
+                Dispose(conexao);
             }
+        }
+
+        private void Dispose(Lazy<ConnectionMultiplexer> conexao)
+        {
+            if (conexao != null && conexao.IsValueCreated)
+            {
+                conexao.Value.Close();
+                conexao.Value.Dispose();
+            }
+        }
+
+        private string ObterChaveInstancia(string nomeChave)
+        {
+            return configuration.GetValue<string>("Nome-Instancia-Redis") + nomeChave;
+        }
+
+        private Lazy<ConnectionMultiplexer> ObterConexao()
+        {
+            return new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect($"{configuration.GetConnectionString("SGP-Redis")},allowAdmin=true,syncTimeout=1000,connectTimeout=1000"); });
         }
     }
 }
