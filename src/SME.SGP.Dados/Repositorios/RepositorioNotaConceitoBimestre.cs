f39348fdabafeee33e78b3a85eccda55a53c27f8
Losi <rafael.losi@amcom.com.br>
2020-03-18 17:48:44 -0300

    12349 - Endpoint Salvar Anotação Aluno

--- src/SME.SGP.Dados/Repositorios/RepositorioNotaConceitoBimestre.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioNotaConceitoBimestre.cs
@@ -9,15 +9,21 @@ namespace SME.SGP.Dados.Repositorios
 {
     public class RepositorioNotaConceitoBimestre : RepositorioBase<NotaConceitoBimestre>, IRepositorioNotaConceitoBimestre
     {
+        const string queryPorFechamento = "select * from nota_conceito_bimestre where fechamento_turma_disciplina_id = @fechamentoId";
         public RepositorioNotaConceitoBimestre(ISgpContext database) : base(database)
         {
         }
 
+        public async Task<NotaConceitoBimestre> ObterPorAlunoEFechamento(long fechamentoId, string codigoAluno)
+        {
+            var query = queryPorFechamento + " and codigo_aluno = @codigoAluno";
+
+            return await database.Conexao.QueryFirstOrDefaultAsync<NotaConceitoBimestre>(query, new { fechamentoId, codigoAluno });
+        }
+
         public async Task<IEnumerable<NotaConceitoBimestre>> ObterPorFechamentoTurma(long fechamentoId)
         {
-            var query = "select * from nota_conceito_bimestre where fechamento_turma_disciplina_id = @fechamentoId";
-
-            return await database.Conexao.QueryAsync<NotaConceitoBimestre>(query, new { fechamentoId });
+            return await database.Conexao.QueryAsync<NotaConceitoBimestre>(queryPorFechamento, new { fechamentoId });
         }
     }
 }
