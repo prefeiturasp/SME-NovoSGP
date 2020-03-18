dfb7223fc17d49ce7a15d54dcd79589137b395fe
eliton-amcom <eliton.santos@amcom.com.br>
2020-03-18 18:39:18 -0300

    Alterações melhora de performance

--- src/SME.SGP.Dominio.Interfaces/Servicos/IServicoAula.cs
+++ src/SME.SGP.Dominio.Interfaces/Servicos/IServicoAula.cs
@@ -12,6 +12,6 @@ namespace SME.SGP.Dominio.Interfaces
 
         Task ExcluirRecorrencia(Aula aula, RecorrenciaAula recorrencia, Usuario usuario);
 
-        Task<string> Salvar(Aula aula, Usuario usuario, RecorrenciaAula recorrencia, int quantidadeOriginal = 0, bool ehRecorrencia = false, IEnumerable<DateTime> datasComRegistro = null);
+        Task<string> Salvar(Aula aula, Usuario usuario, RecorrenciaAula recorrencia, int quantidadeOriginal = 0, bool ehRecorrencia = false);
     }
 }
