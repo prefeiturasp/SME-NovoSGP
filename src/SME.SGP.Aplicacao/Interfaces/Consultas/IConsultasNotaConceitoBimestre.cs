2591d7fb448022570d2dc3aa670b8e58e3c45cdd
Losi <rafael.losi@amcom.com.br>
2020-03-19 14:29:39 -0300

    12350 - Obter anotação e dados de aluno para o modal no fechamento

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasNotaConceitoBimestre.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasNotaConceitoBimestre.cs
@@ -1,7 +1,10 @@
-﻿namespace SME.SGP.Aplicacao
+﻿using SME.SGP.Dominio;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
 {
     public interface IConsultasNotaConceitoBimestre
     {
-
+        Task<string> ObterAnotacaoPorAlunoEFechamento(long fechamentoId, string codigoAluno);
     }
 }
---
+++ src/SME.SGP.Infra/Dtos/AlunoDadosBasicosDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class AlunoDadosBasicosDto
+    {
+        public string Nome { get; set; }
+        public int NumeroChamada { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CodigoEOL { get; set; }
+        public string Situacao { get; set; }
+        public DateTime DataSituacao { get; set; }
+        public double Frequencia { get; set; }
+
+        public static explicit operator AlunoDadosBasicosDto(AlunoPorTurmaResposta dadosAluno)
+            => dadosAluno == null ? null : new AlunoDadosBasicosDto()
+            {
+                Nome = dadosAluno.NomeAluno,
+                NumeroChamada = dadosAluno.NumeroAlunoChamada,
+                DataNascimento = dadosAluno.DataNascimento,
+                CodigoEOL = dadosAluno.CodigoAluno,
+                Situacao = dadosAluno.SituacaoMatricula,
+                DataSituacao = dadosAluno.DataSituacao
+            };
+    }
+}
