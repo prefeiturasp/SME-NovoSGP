2bd710b12c0400388378fc9eed8be1d5b2270512
Losi <rafael.losi@amcom.com.br>
2020-03-20 10:07:13 -0300

    9276 - Criação de indice na tablea pendencia_fechamento

--- scripts/V138__REFACTOR_FECHAMENTO_TURMA.sql
+++ scripts/V138__REFACTOR_FECHAMENTO_TURMA.sql
@@ -13,6 +13,9 @@ ALTER TABLE public.fechamento_turma_disciplina ADD CONSTRAINT fechamento_turma_d
 ALTER TABLE public.fechamento_turma_disciplina DROP COLUMN if exists situacao;
 ALTER TABLE public.fechamento_turma_disciplina ADD COLUMN situacao int4 not null;
 
+CREATE INDEX if not exists fechamento_turma_disciplina_periodo_id_idx ON public.fechamento_turma_disciplina (periodo_escolar_id);
+CREATE INDEX if not exists fechamento_turma_disciplina_periodo_id_idx ON public.fechamento_turma_disciplina (situacao);
+
 -- PENDENCIA x FECHAMENTO
 alter table pendencia drop column if exists fechamento_id;
 
@@ -34,5 +37,8 @@ ALTER TABLE public.pendencia_fechamento DROP CONSTRAINT if exists pendencia_fech
 ALTER TABLE public.pendencia_fechamento DROP CONSTRAINT if exists pendencia_fechamento_pendencia_fk;
 ALTER TABLE public.pendencia_fechamento ADD CONSTRAINT pendencia_fechamento_fechamento_fk FOREIGN KEY (fechamento_turma_disciplina_id) REFERENCES fechamento_turma_disciplina(id);
 ALTER TABLE public.pendencia_fechamento ADD CONSTRAINT pendencia_fechamento_pendencia_fk FOREIGN KEY (pendencia_id) REFERENCES pendencia(id);
+
+CREATE INDEX if not exists pendencia_fechamento_fechamento_turma_id_idx ON public.pendencia_fechamento (fechamento_turma_disciplina_id);
+CREATE INDEX if not exists pendencia_fechamento_pendencia_id_idx ON public.pendencia_fechamento (pendencia_id);
 
 DROP TABLE if exists fechamento;
