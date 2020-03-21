cb12fd5750c750f1fe4423c7f0a37b12a32b4e41
Cleiton <cleiton.dutra@amcom.com.br>
2020-03-21 18:42:23 -0300

    Fechamento - Análise de pendências - FORM

---
+++ src/SME.SGP.WebClient/src/paginas/Fechamento/PendenciasFechamento/labelSituacaoFechamento.css.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const ResolvidoForm = styled.div`
+  width: 144px;
+  border-radius: 4px;
+  display: inline-grid;
+  padding: 10px;
+  margin-top: 24px;
+  justify-items: center;
+  color: #d06d12;
+  background-color: #fff2e8;
+  border: solid 1px #eec4ad;
+`;
+export const AprovadoForm = styled.div`
+  width: 144px;
+  border-radius: 4px;
+  display: inline-grid;
+  padding: 10px;
+  margin-top: 24px;
+  justify-items: center;
+  color: #198459;
+  border: solid 1px #90ceb5;
+  background-color: #f6ffed;
+`;
+
+export const PendenteForm = styled.div`
+  width: 144px;
+  border-radius: 4px;
+  display: inline-grid;
+  padding: 10px;
+  margin-top: 24px;
+  justify-items: center;
+  color: #490cf5;
+  border: solid 1px #afa2fa;
+  background-color: #f9f0ff;
+`;
