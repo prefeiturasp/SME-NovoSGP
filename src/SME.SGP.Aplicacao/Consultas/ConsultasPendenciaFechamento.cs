49023d6bcb8e9447fdef12493f4da70dcb235533
Losi <rafael.losi@amcom.com.br>
2020-03-23 11:17:48 -0300

    11475 - Listagem de pendencias de fechamento

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasPendenciaFechamento.cs
@@ -0,0 +1,40 @@
+﻿using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ConsultasPendenciaFechamento : ConsultasBase, IConsultasPendenciaFechamento
+    {
+        private readonly IServicoEOL servicoEOL;
+        private readonly IRepositorioPendenciaFechamento repositorioPendenciaFechamento;
+        public ConsultasPendenciaFechamento(IContextoAplicacao contextoAplicacao
+                                , IServicoEOL servicoEOL
+                                , IRepositorioPendenciaFechamento repositorioPendenciaFechamento) : base(contextoAplicacao)
+        {
+            this.servicoEOL = servicoEOL ?? throw new ArgumentNullException(nameof(servicoEOL));
+            this.repositorioPendenciaFechamento = repositorioPendenciaFechamento ?? throw new ArgumentNullException(nameof(repositorioPendenciaFechamento));
+        }
+
+        public async Task<PaginacaoResultadoDto<PendenciaFechamentoResumoDto>> Listar(FiltroPendenciasFechamentosDto filtro)
+        {
+            var retornoConsultaPaginada = await repositorioPendenciaFechamento.ListarPaginada(Paginacao, filtro.TurmaCodigo, filtro.Bimestre, filtro.ComponenteCurricularId);
+
+            // Carrega nomes das disciplinas para o DTO de retorno
+            var disciplinasEOL = await servicoEOL.ObterDisciplinasPorIdsAsync(retornoConsultaPaginada.Items.Select(a => a.DisciplinaId).Distinct().ToArray());
+            foreach(var disciplinaEOL in disciplinasEOL)
+            {
+                retornoConsultaPaginada.Items.Where(c => c.DisciplinaId == disciplinaEOL.CodigoComponenteCurricular).ToList()
+                    .ForEach(d => d.ComponenteCurricular = disciplinaEOL.Nome);
+            }
+
+            return retornoConsultaPaginada;
+        }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/PendenciaFechamentoResumoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class PendenciaFechamentoResumoDto
+    {
+        public long DisciplinaId { get; set; }
+        public string ComponenteCurricular { get; set; }
+        public string Descricao { get; set; }
+        public string Situacao { get; set; }
+    }
+}
