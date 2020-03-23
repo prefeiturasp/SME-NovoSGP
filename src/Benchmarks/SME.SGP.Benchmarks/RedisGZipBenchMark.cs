42697496caf0b65d1f9d3cf3ff3108d98fae41e0
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-03-23 15:32:57 -0300

    inciando configurações do benchmark

---
+++ src/Benchmarks/SME.SGP.Benchmarks/RedisGZipBenchMark.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SME.SGP.Benchmarks
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    public class RedisGZipBenchMark
+    {
+        //private readonly IRepositorioCache repositorioCache;
+
+        //public RedisGZipBenchMark()
+        //{
+        //    //IServiceCollection serviceCollection = new ServiceCollection();
+        //    //ConfigureServices(serviceCollection);
+
+        //    //var services = serviceCollection.BuildServiceProvider();
+
+        //    //repositorioCache = new RepositorioCache(services.GetService<IDistributedCache>(), services.GetService<IServicoLog>());
+        //}
+
+        [Benchmark]
+        public void CacheComGZip()
+        {
+            // repositorioCache.Obter("teste", true);
+        }
+
+        //private static void ConfigureServices(IServiceCollection services)
+        //{
+        //    //services.AddDistributedRedisCache(options =>
+        //    //{
+        //    //    options.Configuration = "localhost";
+        //    //    options.InstanceName = "SGP-";
+        //    //});
+
+        //}
+    }
+}
