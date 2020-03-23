5f62843e4186cba42d8e63ba187b4a1fcaa1cd0c
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-03-23 17:11:18 -0300

    Benchmark de compactação dos caches no redis

--- src/Benchmarks/SME.SGP.Benchmarks/RedisGZipBenchMark.cs
+++ src/Benchmarks/SME.SGP.Benchmarks/RedisGZipBenchMark.cs
@@ -1,4 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
+using SME.SGP.Infra.Utilitarios;
+using StackExchange.Redis;
+using System;
 
 namespace SME.SGP.Benchmarks
 {
@@ -7,6 +10,13 @@ namespace SME.SGP.Benchmarks
     [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
     public class RedisGZipBenchMark
     {
+        private readonly Lazy<ConnectionMultiplexer> conexao;
+
+        public RedisGZipBenchMark()
+        {
+            conexao = ObterConexao();
+        }
+
         //private readonly IRepositorioCache repositorioCache;
 
         //public RedisGZipBenchMark()
@@ -22,7 +32,61 @@ namespace SME.SGP.Benchmarks
         [Benchmark]
         public void CacheComGZip()
         {
-            // repositorioCache.Obter("teste", true);
+            //var conexao = ObterConexao();
+            try
+            {
+                var valorParaGravar = Convert.ToBase64String(UtilGZip.Comprimir("12345678910"));
+
+                conexao.Value.GetDatabase().StringSet("teste-benchmark", valorParaGravar, TimeSpan.FromMinutes(10));
+                var valorReortono = UtilGZip.Descomprimir(Convert.FromBase64String(conexao.Value.GetDatabase().StringGet("teste-benchmark")));
+
+                conexao.Value.GetDatabase().KeyDelete("teste-benchmark");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                //   Dispose(conexao);
+            }
+        }
+
+        [Benchmark]
+        public void CacheSemGZip()
+        {
+            //var conexao = ObterConexao();
+            try
+            {
+                var valorParaGravar = "12345678910";
+
+                conexao.Value.GetDatabase().StringSet("teste-benchmark", valorParaGravar, TimeSpan.FromMinutes(10));
+                conexao.Value.GetDatabase().StringGet("teste-benchmark");
+
+                conexao.Value.GetDatabase().KeyDelete("teste-benchmark");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                //   Dispose(conexao);
+            }
+        }
+
+        private void Dispose(Lazy<ConnectionMultiplexer> conexao)
+        {
+            if (conexao != null && conexao.IsValueCreated)
+            {
+                conexao.Value.Close();
+                conexao.Value.Dispose();
+            }
+        }
+
+        private Lazy<ConnectionMultiplexer> ObterConexao()
+        {
+            return new Lazy<ConnectionMultiplexer>(() => { return ConnectionMultiplexer.Connect($"localhost,allowAdmin=true,syncTimeout=1000,connectTimeout=1000"); });
         }
 
         //private static void ConfigureServices(IServiceCollection services)
