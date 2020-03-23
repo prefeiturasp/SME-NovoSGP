8c7ba661f8c6017245567e3a19dc452767efebec
Losi <rafael.losi@amcom.com.br>
2020-03-23 11:17:48 -0300

    11475 - Listagem de pendencias de fechamento

--- src/SME.SGP.Dados/Repositorios/RepositorioPendenciaFechamento.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioPendenciaFechamento.cs
@@ -1,4 +1,9 @@
-﻿using SME.SGP.Dominio;
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
@@ -10,5 +15,30 @@ namespace SME.SGP.Dados.Repositorios
         {
         }
 
+        public async Task<PaginacaoResultadoDto<PendenciaFechamentoResumoDto>> ListarPaginada(Paginacao paginacao, string turmaCodigo, int bimestre, long componenteCurricularId)
+        {
+            var retorno = new PaginacaoResultadoDto<PendenciaFechamentoResumoDto>();
+
+            var query = new StringBuilder(@"select p.descricao, p.situacao, ftd.disciplina_id as DisciplinaId
+                                  from pendencia_fechamento pf 
+                                 inner join fechamento_turma_disciplina ftd on ftd.id = pf.fechamento_turma_disciplina_id 
+                                 inner join turma t on t.id = ftd.turma_id 
+                                 inner join periodo_escolar pe on pe.id = ftd.periodo_escolar_id 
+                                 inner join pendencia p on p.id = pf.pendencia_id 
+                                  where t.turma_id = @turmaCodigo ");
+            if (bimestre > 0)
+                query.AppendLine(" and pe.bimestre = @bimestre");
+            if (componenteCurricularId > 0)
+                query.AppendLine(" and ftd.disciplina_id = @componenteCurricularId");
+
+            using (var multi = await database.Conexao.QueryMultipleAsync(query.ToString(), new {turmaCodigo, bimestre, componenteCurricularId }))
+            {
+                retorno.Items = multi.Read<PendenciaFechamentoResumoDto>().ToList();
+                retorno.TotalRegistros = multi.ReadFirst<int>();
+            }
+            retorno.TotalPaginas = (int)Math.Ceiling((double)retorno.TotalRegistros / paginacao.QuantidadeRegistros);
+
+            return retorno;
+        }
     }
 }
--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioPendenciaFechamento.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioPendenciaFechamento.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SME.SGP.Infra;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioPendenciaFechamento : IRepositorioBase<PendenciaFechamento>
     {
+        Task<PaginacaoResultadoDto<PendenciaFechamentoResumoDto>> ListarPaginada(Paginacao paginacao, string turmaCodigo, int bimestre, long componenteCurricularId);
     }
 }
