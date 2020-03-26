ac9db2fbe0f99cd701da267fa5e65be3fdf7eaec
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-03-25 22:36:35 -0300

    Correção campo nota final
    Correção campo nota fina
    
    AB#14034

--- src/SME.SGP.Dados/Repositorios/RepositorioEventoFechamento.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioEventoFechamento.cs
@@ -23,18 +23,17 @@ namespace SME.SGP.Dados.Repositorios
 
         public async Task<bool> UeEmFechamento(DateTime dataReferencia, string dreCodigo, string ueCodigo, int bimestre, long tipoCalendarioId)
         {
-            var query = @"select count(ef.id)
-                         from evento_fechamento ef
-                        inner join evento e on e.id = ef.evento_id
-                        inner join periodo_fechamento_bimestre pfb on pfb.id = ef.fechamento_id
+            var query = @"select count(pf.id) from periodo_fechamento pf 
+                        inner join periodo_fechamento_bimestre pfb on pf.id = pfb.periodo_fechamento_id 
                         inner join periodo_escolar pe on pe.id = pfb.periodo_escolar_id
-                        where not e.excluido 
-                          and e.data_inicio <= @dataReferencia
-                          and e.data_fim >= @dataReferencia
-                          and e.dre_id = @dreCodigo
-                          and e.ue_id = @ueCodigo
-                          and pe.bimestre = @bimestre
-                          and pe.tipo_calendario_id = @tipoCalendarioId";
+                        inner join dre dre on dre.id = pf.dre_id 
+                        inner join ue ue on ue.id = pf.ue_id 
+                        where pe.tipo_calendario_id = @tipoCalendarioId
+                        and pe.bimestre =@bimestre
+                        and ue.ue_id = @ueCodigo
+                        and dre.dre_id = @dreCodigo
+                        and pfb.inicio_fechamento <= @dataReferencia
+                        and pfb.final_fechamento >= @dataReferencia";
 
             return await database.Conexao.QueryFirstAsync<int>(query, new
             {
