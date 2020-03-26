0753436a7dbbb607f80124a5839ad9f4732fdc85
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-03-26 19:15:20 -0300

    Ajuste método get fechamento turma sintese

---
+++ src/SME.SGP.Dominio/Enumerados/SinteseEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SME.SGP.Dominio
+{
+    public enum SinteseEnum
+    {
+        [Display(Name = "Frequente")]
+        Frequente = 1,
+
+        [Display(Name = "Não frequente")]
+        NaoFrequente = 2
+    }
+}
