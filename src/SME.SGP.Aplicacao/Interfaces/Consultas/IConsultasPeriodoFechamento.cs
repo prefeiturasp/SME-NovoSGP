91e44bb019c4a9c060ac9d91986e47f879a9ed4e
Alexsander Camargo <csalexsander@gmail.com>
2020-03-27 21:53:06 -0300

    Aula dentro do periodo
    Aula dentro do periodo
    
    AB#14086

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasPeriodoFechamento.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasPeriodoFechamento.cs
@@ -12,5 +12,6 @@ namespace SME.SGP.Aplicacao
         Task<bool> TurmaEmPeriodoDeFechamento(string turmaCodigo, DateTime dataReferencia, int bimestre = 0);
         Task<bool> TurmaEmPeriodoDeFechamento(Turma turma, TipoCalendario tipoCalendario, DateTime dataReferencia, int bimestre = 0);
         Task<IEnumerable<PeriodoEscolarDto>> ObterPeriodosEmAberto(long ueId);
+        Task<bool> TurmaEmPeriodoDeFechamento(Turma turma, DateTime dataReferencia, int bimestre = 0);
     }
 }
