04e1cfb6e04ed872e5243afb3c21db5aae481a80
Losi <rafael.losi@amcom.com.br>
2020-03-30 15:29:55 -0300

    merge dev

--- src/SME.SGP.Dominio.Servicos/ServicoFechamentoTurmaDisciplina.cs
+++ src/SME.SGP.Dominio.Servicos/ServicoFechamentoTurmaDisciplina.cs
@@ -183,7 +183,6 @@ namespace SME.SGP.Dominio.Servicos
 
             if (!componenteSemNota)
             {
-
                 servicoPendenciaFechamento.ValidarAvaliacoesSemNotasParaNenhumAluno(fechamento.Id, turma.CodigoTurma, disciplinaId, periodoEscolar.PeriodoInicio, periodoEscolar.PeriodoFim);
                 servicoPendenciaFechamento.ValidarPercentualAlunosAbaixoDaMedia(fechamento);
             }
