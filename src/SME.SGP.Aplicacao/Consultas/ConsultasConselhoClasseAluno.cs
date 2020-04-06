bb41cd690c1cd7c95b998eef4b269a6d76263823
Losi <rafael.losi@amcom.com.br>
2020-04-06 10:16:38 -0300

    14459 - Modelagem Conselho de Classe

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasConselhoClasseAluno.cs
@@ -0,0 +1,14 @@
+﻿using SME.SGP.Dominio.Interfaces;
+using System;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ConsultasConselhoClasseAluno : IConsultasConselhoClasseAluno
+    {
+        private readonly IRepositorioConselhoClasseAluno repositorioConselhoClasseAluno;
+        public ConsultasConselhoClasseAluno(IRepositorioConselhoClasseAluno repositorioConselhoClasseAluno)
+        {
+            this.repositorioConselhoClasseAluno = repositorioConselhoClasseAluno ?? throw new ArgumentNullException(nameof(repositorioConselhoClasseAluno));
+        }
+    }
+}
