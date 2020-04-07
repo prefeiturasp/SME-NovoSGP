766d51a479a4db0a3a79ece6302a2876562f8305
Cleiton <cleiton.dutra@amcom.com.br>
2020-04-07 10:14:25 -0300

    Conselho de classe - Estrutura da tela do conselho

--- src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/conselho-classe.js
+++ src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/conselho-classe.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Loader } from '~/componentes';
 import { Ordenacao } from '~/componentes-sgp';
@@ -10,116 +10,56 @@ import Card from '~/componentes/card';
 import { Colors } from '~/componentes/colors';
 import TabelaRetratil from '~/componentes/TabelaRetratil';
 import { Container } from './conselho-classe.css';
+import ServicoConselhoClasse from '~/servicos/Paginas/ConselhoClasse/ServicoConselhoClasse';
+import { erros } from '~/servicos/alertas';
 
 const ConselhoClasse = () => {
   const usuario = useSelector(store => store.usuario);
   const { turmaSelecionada } = usuario;
 
   const [carregandoGeral, setCarregandoGeral] = useState(false);
+  const [alunos, setAlunos] = useState([]);
+  const [dadosAluno, setDadosAluno] = useState({});
 
-  const mockAlunos = [
-    {
-      id: 1,
-      idEol: 1,
-      numeroChamada: 1,
-      nome: 'Italo Gustavo Pereira de Maio',
-      ativo: true,
-      situacao: 'Transferido em 11/11/2011',
-    },
-    {
-      id: 2,
-      idEol: 2,
-      numeroChamada: 2,
-      nome: 'Thiago de Oliveira Ramos',
-      ativo: true,
-      situacao: 'Novo aluno',
-    },
-    {
-      id: 3,
-      idEol: 3,
-      numeroChamada: 3,
-      nome: 'Alana Ferreira de Oliveira',
-      ativo: true,
-      situacao: null,
-    },
-    {
-      id: 4,
-      idEol: 4,
-      numeroChamada: 4,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: null,
-    },
-    {
-      id: 5,
-      idEol: 5,
-      numeroChamada: 5,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: false,
-      situacao: null,
-    },
-    {
-      id: 6,
-      idEol: 6,
-      numeroChamada: 6,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: 'Transferido em 11/11/2011',
-    },
-    {
-      id: 7,
-      idEol: 7,
-      numeroChamada: 7,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: null,
-    },
-    {
-      id: 8,
-      idEol: 8,
-      numeroChamada: 8,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: null,
-    },
-    {
-      id: 9,
-      idEol: 9,
-      numeroChamada: 9,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: null,
-    },
-    {
-      id: 10,
-      idEol: 10,
-      numeroChamada: 10,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: false,
-      situacao: null,
-    },
-    {
-      id: 11,
-      idEol: 11,
-      numeroChamada: 11,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: null,
-    },
-    {
-      id: 12,
-      idEol: 12,
-      numeroChamada: 12,
-      nome: 'Alany Santos da Silva Sauro',
-      ativo: true,
-      situacao: null,
-    },
-  ];
+  const resetarTela = () => {
+    setDadosAluno({});
+    setAlunos([]);
+  };
 
-  const [alunos, setAlunos] = useState(mockAlunos);
+  const obterListaAlunos = async () => {
+    const turmaCodigo = turmaSelecionada.turma;
+    const anoLetivo = turmaSelecionada.anoLetivo;
+    const retorno = await ServicoConselhoClasse.obterListaAlunos(
+      turmaCodigo,
+      anoLetivo
+    ).catch(e => erros(e));
+    if (retorno && retorno.data) {
+      setAlunos(retorno.data);
+    }
+  };
 
-  const onChangeAlunoSelecionado = aluno => {
-    console.log(aluno);
+  useEffect(() => {
+    if (turmaSelecionada.turma) {
+      obterListaAlunos();
+    } else {
+      // TODO - Reseta tela
+    }
+  }, []);
+
+  const obterFrequenciaAluno = async codigoAluno => {
+    const retorno = await ServicoConselhoClasse.obterFrequenciaAluno(
+      codigoAluno
+    ).catch(e => erros(e));
+    if (retorno && retorno.data) {
+      return retorno.data;
+    }
+    return 0;
+  };
+
+  const onChangeAlunoSelecionado = async aluno => {
+    const frequenciaGeralAluno = await obterFrequenciaAluno(aluno.codigoEOL);
+    aluno.frequencia = frequenciaGeralAluno;
+    setDadosAluno(aluno);
   };
 
   const onClickVoltar = () => {
@@ -173,6 +113,7 @@ const ConselhoClasse = () => {
                       border
                       className="mr-2"
                       onClick={onClickCancelar}
+                      disabled={!alunos || alunos.length < 1}
                     />
                     <Button
                       label="Salvar"
@@ -181,44 +122,41 @@ const ConselhoClasse = () => {
                       bold
                       className="mr-2"
                       onClick={onClickSalvar}
+                      disabled={!alunos || alunos.length < 1}
                     />
                   </div>
                 </div>
               </div>
-              <div className="col-md-12 mb-2 d-flex">
-                <Ordenacao
-                  conteudoParaOrdenar={alunos}
-                  ordenarColunaNumero="id"
-                  ordenarColunaTexto="nome"
-                  retornoOrdenado={retorno => setAlunos(retorno)}
-                />
-                <Button
-                  className="btn-imprimir"
-                  icon="print"
-                  color={Colors.Azul}
-                  border
-                  onClick={() => {}}
-                  disabled
-                />
-              </div>
-              <div className="col-md-12 mb-2">
-                <TabelaRetratil
-                  onChangeAlunoSelecionado={onChangeAlunoSelecionado}
-                  alunos={alunos}
-                >
-                  <DetalhesAluno
-                    dados={{
-                      nome: 'ALANA FERREIRA DE OLIVEIRA',
-                      numero: 1,
-                      dataNascimento: '02/02/2020',
-                      codigoEOL: 4241513,
-                      situacao: 'Matriculado',
-                      dataSituacao: '04/02/2019',
-                      frequencia: 96,
-                    }}
-                  />
-                </TabelaRetratil>
-              </div>
+              {alunos && alunos.length ? (
+                <>
+                  <div className="col-md-12 mb-2 d-flex">
+                    <Ordenacao
+                      conteudoParaOrdenar={alunos}
+                      ordenarColunaNumero="id"
+                      ordenarColunaTexto="nome"
+                      retornoOrdenado={retorno => setAlunos(retorno)}
+                    />
+                    <Button
+                      className="btn-imprimir"
+                      icon="print"
+                      color={Colors.Azul}
+                      border
+                      onClick={() => {}}
+                      disabled
+                    />
+                  </div>
+                  <div className="col-md-12 mb-2">
+                    <TabelaRetratil
+                      onChangeAlunoSelecionado={onChangeAlunoSelecionado}
+                      alunos={alunos}
+                    >
+                      <DetalhesAluno dados={dadosAluno} />
+                    </TabelaRetratil>
+                  </div>
+                </>
+              ) : (
+                ''
+              )}
             </>
           ) : (
             ''
