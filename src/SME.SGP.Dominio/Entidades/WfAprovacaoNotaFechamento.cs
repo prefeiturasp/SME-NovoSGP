3b2220802299b0399c69dbef6d245d2642a18553
Losi <rafael.losi@amcom.com.br>
2020-04-06 23:05:37 -0300

    13664 - Criacao de WF Aprovação para Alteração de Nota Fechamento Extenporanea

---
+++ src/SME.SGP.Dominio/Entidades/WfAprovacaoNotaFechamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio
+{
+    public class WfAprovacaoNotaFechamento
+    {
+        public long Id { get; set; }
+        public long WfAprovacaoId { get; set; }
+        public WorkflowAprovacao WfAprovacao { get; set; }
+        public long FechamentoNotaId { get; set; }
+        public FechamentoNota FechamentoNota { get; set; }
+
+        public double? Nota { get; set; }
+        public long? ConceitoId { get; set; }
+        public Conceito Conceito { get; set; }
+    }
+}
