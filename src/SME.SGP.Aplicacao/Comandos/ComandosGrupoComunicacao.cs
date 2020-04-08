2d66f757739d248db12613ed81eae1502cae3cc8
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-04-08 16:04:59 -0300

    AB#14701

---
+++ src/SME.SGP.Aplicacao/Comandos/ComandosGrupoComunicacao.cs
@@ -0,0 +1,34 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ComandosGrupoComunicacao : IComandosGrupoComunicacao
+    {
+        private readonly IRepositorioAulaPrevista repositorio;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ComandosGrupoComunicacao(IRepositorioAulaPrevista repositorio)
+        {
+            this.repositorio = repositorio ?? throw new ArgumentNullException(nameof(repositorio));
+        }
+
+        public Task Alterar(GrupoComunicacaoDto dto, long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Excluir(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Inserir(GrupoComunicacaoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
