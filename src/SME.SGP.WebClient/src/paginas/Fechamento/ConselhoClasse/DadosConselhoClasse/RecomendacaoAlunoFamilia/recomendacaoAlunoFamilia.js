cf84cbca38399427464d5cb0d292106edcb351c9
Cleiton <cleiton.dutra@amcom.com.br>
2020-04-08 12:51:37 -0300

    conselho de classe - recomendacoes aluno e familia

--- src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/RecomendacaoAlunoFamilia/recomendacaoAlunoFamilia.js
+++ src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/RecomendacaoAlunoFamilia/recomendacaoAlunoFamilia.js
@@ -7,18 +7,17 @@ import ServicoConselhoClasse from '~/servicos/Paginas/ConselhoClasse/ServicoCons
 
 const RecomendacaoAlunoFamilia = props => {
   const {
-    onChangeRecomentacaoAluno,
-    onChangeRecomentacaoFamilia,
+    onChangeRecomendacaoAluno,
+    onChangeRecomendacaoFamilia,
     codigoTurma,
     codigoAluno,
     numeroBimestre,
   } = props;
 
   const [exibirCardRecomendacao, setExibirCardRecomendacao] = useState(false);
-  const [modoEdicao, setModoEdicao] = useState(false);
 
-  const [recomentacaoAluno, setRecomentacaoAluno] = useState('teste 1');
-  const [recomentacaoFamilia, setRecomentacaoFamilia] = useState('teste 2');
+  const [recomendacaoAluno, setRecomendacaoAluno] = useState('teste 1');
+  const [recomendacaoFamilia, setRecomendacaoFamilia] = useState('teste 2');
 
   const onClickExpandirRecomendacao = async () => {
     // TODO
@@ -30,8 +29,8 @@ const RecomendacaoAlunoFamilia = props => {
     //   ).catch(e => erros(e));
 
     //   if (retorno && retorno.data) {
-    //     setRecomentacaoAluno(retorno.data.recomendacaoAluno);
-    //     setRecomentacaoFamilia(retorno.data.recomentacaoFamilia);
+    //     setRecomendacaoAluno(retorno.data.recomendacaoAluno);
+    //     setRecomendacaoFamilia(retorno.data.recomendacaoFamilia);
     //   }
     // }
 
@@ -39,39 +38,37 @@ const RecomendacaoAlunoFamilia = props => {
   };
 
   const onChangeAluno = textoAluno => {
-    setModoEdicao(true);
-    onChangeRecomentacaoAluno(textoAluno);
+    onChangeRecomendacaoAluno(textoAluno);
   };
 
   const onChangeFamilia = textoFamilia => {
-    setModoEdicao(true);
-    onChangeRecomentacaoFamilia(textoFamilia);
+    onChangeRecomendacaoFamilia(textoFamilia);
   };
 
   return (
     <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-2">
       <CardCollapse
-        key="recomentacao-aluno-familia-collapse"
+        key="recomendacao-aluno-familia-collapse"
         onClick={onClickExpandirRecomendacao}
         titulo="Recomendações ao aluno e a família"
-        indice="recomentacao-aluno-familia-collapse"
+        indice="recomendacao-aluno-familia-collapse"
         show={exibirCardRecomendacao}
-        alt="recomentacao-aluno-familia"
+        alt="recomendacao-aluno-familia"
       >
         <div className="row">
           <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <Editor
               label="Recomendações ao aluno"
-              id="recomentacao-aluno"
-              inicial={recomentacaoAluno}
+              id="recomendacao-aluno"
+              inicial={recomendacaoAluno}
               onChange={onChangeAluno}
             />
           </div>
           <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <Editor
               label="Recomendações a família "
-              id="recomentacao-familia"
-              inicial={recomentacaoFamilia}
+              id="recomendacao-familia"
+              inicial={recomendacaoFamilia}
               onChange={onChangeFamilia}
             />
           </div>
@@ -82,16 +79,16 @@ const RecomendacaoAlunoFamilia = props => {
 };
 
 RecomendacaoAlunoFamilia.propTypes = {
-  onChangeRecomentacaoAluno: PropTypes.func,
-  onChangeRecomentacaoFamilia: PropTypes.func,
+  onChangeRecomendacaoAluno: PropTypes.func,
+  onChangeRecomendacaoFamilia: PropTypes.func,
   codigoTurma: PropTypes.string,
   codigoAluno: PropTypes.string,
   numeroBimestre: PropTypes.string,
 };
 
 RecomendacaoAlunoFamilia.defaultProps = {
-  onChangeRecomentacaoAluno: () => {},
-  onChangeRecomentacaoFamilia: () => {},
+  onChangeRecomendacaoAluno: () => {},
+  onChangeRecomendacaoFamilia: () => {},
   codigoTurma: '',
   codigoAluno: '',
   numeroBimestre: '0',
