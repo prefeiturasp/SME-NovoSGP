3980ae1575cc33d3e9011f082bb0a6f81ad8c44e
Flávio Ricardo <flavioricardo@live.com>
2020-04-08 17:59:02 -0300

    AB#14245

--- src/SME.SGP.WebClient/src/paginas/AcompanhamentoEscolar/Comunicados/Cadastro/index.js
+++ src/SME.SGP.WebClient/src/paginas/AcompanhamentoEscolar/Comunicados/Cadastro/index.js
@@ -1,7 +1,8 @@
-import React, { useState, useCallback, useEffect } from 'react';
+/* eslint-disable react/no-this-in-sfc */
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 // import shortid from 'shortid';
 
 import { Form, Formik } from 'formik';
@@ -17,6 +18,9 @@ import {
   CampoTexto,
   CampoData,
   Label,
+  TextEditor,
+  // momentSchema,
+  Base,
 } from '~/componentes';
 
 import { Linha } from '~/componentes/EstilosGlobais';
@@ -26,6 +30,21 @@ import RotasDto from '~/dtos/rotasDto';
 import { verificaSomenteConsulta } from '~/servicos/servico-navegacao';
 
 const ComunicadosCadastro = ({ match }) => {
+  const ErroValidacao = styled.span`
+    color: ${Base.Vermelho};
+  `;
+
+  const InseridoAlterado = styled.div`
+    color: ${Base.CinzaMako};
+    font-weight: bold;
+    font-style: normal;
+    font-size: 10px;
+    object-fit: contain;
+    p {
+      margin: 0;
+    }
+  `;
+
   const [loaderSecao] = useState(false);
 
   const [somenteConsulta, setSomenteConsulta] = useState(false);
@@ -35,6 +54,9 @@ const ComunicadosCadastro = ({ match }) => {
     setSomenteConsulta(verificaSomenteConsulta(permissoesTela));
   }, [permissoesTela]);
 
+  const [novoRegistro, setNovoRegistro] = useState(true);
+  const [modoEdicao] = useState(false);
+
   const [idComunicado, setIdComunicado] = useState();
 
   useEffect(() => {
@@ -42,6 +64,24 @@ const ComunicadosCadastro = ({ match }) => {
       setIdComunicado(match.params.id);
   }, [match]);
 
+  const [inseridoAlterado, setInseridoAlterado] = useState({
+    alteradoEm: '',
+    alteradoPor: '',
+    criadoEm: '',
+    criadoPor: '',
+  });
+
+  const [descricaoComunicado, setDescricaoComunicado] = useState('');
+
+  useEffect(() => {
+    if (idComunicado) {
+      setInseridoAlterado({});
+      setDescricaoComunicado('');
+      setNovoRegistro(false);
+    }
+  }, [idComunicado]);
+
+  const textEditorRef = useRef();
   const [refForm, setRefForm] = useState({});
 
   const gruposLista = [
@@ -55,20 +95,48 @@ const ComunicadosCadastro = ({ match }) => {
 
   const [valoresIniciais] = useState({
     grupoId: [],
-    titulo: '',
+    dataEnvioInicio: '',
+    dataEnvioFim: '',
     dataExpiracaoInicio: '',
     dataExpiracaoFim: '',
+    titulo: '',
   });
 
-  const validacoes = () => {
-    return Yup.object({});
-  };
+  const [validacoes] = useState(
+    Yup.object({
+      grupoId: Yup.string().required('Campo obrigatório'),
+      dataEnvioInicio: Yup.string().required('Campo obrigatório'),
+      dataEnvioFim: Yup.string().required('Campo obrigatório'),
+      dataExpiracaoInicio: Yup.string().required('Campo obrigatório'),
+      dataExpiracaoFim: Yup.string().required('Campo obrigatório'),
+      titulo: Yup.string()
+        .required('Campo obrigatório')
+        .min(10, 'Deve conter no mínimo 10 caracteres'),
+    })
+  );
 
-  const validarAntesDeSalvar = valores => {
-    const formContext = refForm && refForm.getFormikContext();
-    if (formContext.isValid && Object.keys(formContext.errors).length === 0) {
-      console.log(valores);
-    }
+  const [descricaoValida, setDescricaoValida] = useState(true);
+
+  const validarAntesDeSalvar = form => {
+    const arrayCampos = Object.keys(valoresIniciais);
+
+    arrayCampos.forEach(campo => {
+      form.setFieldTouched(campo, true, true);
+    });
+
+    const descricao = textEditorRef.current.state.value.replace(
+      '<p><br></p>',
+      ''
+    );
+
+    form.validateForm().then(() => {
+      setDescricaoValida(descricao.length);
+
+      if (refForm && form.isValid && descricao.length) {
+        setDescricaoComunicado(descricao);
+        form.handleSubmit(form);
+      }
+    });
   };
 
   const onClickExcluir = async () => {};
@@ -77,33 +145,46 @@ const ComunicadosCadastro = ({ match }) => {
     history.push(RotasDto.ACOMPANHAMENTO_COMUNICADOS);
   };
 
-  const onClickBotaoPrincipal = () => {};
+  const onClickBotaoPrincipal = form => {
+    validarAntesDeSalvar(form);
+  };
+
+  const onClickSalvar = valores => {
+    const dadosSalvar = {
+      ...valores,
+      descricao: textEditorRef.current.state.value,
+    };
+    console.log(dadosSalvar);
+  };
 
   return (
     <>
       <Cabecalho pagina="Comunicação com pais ou responsáveis" />
       <Loader loading={loaderSecao}>
         <Card mx="mx-0">
-          <ButtonGroup
-            somenteConsulta={somenteConsulta}
-            permissoesTela={permissoesTela[RotasDto.ACOMPANHAMENTO_COMUNICADOS]}
-            onClickExcluir={onClickExcluir}
-            onClickVoltar={onClickVoltar}
-            onClickBotaoPrincipal={onClickBotaoPrincipal}
-            labelBotaoPrincipal={idComunicado ? 'Alterar' : 'Cadastrar'}
-          />
           <Formik
             enableReinitialize
             initialValues={valoresIniciais}
-            validationSchema={validacoes()}
-            onSubmit={valores => validarAntesDeSalvar(valores)}
+            validationSchema={validacoes}
             ref={refFormik => setRefForm(refFormik)}
-            validate={valores => validarAntesDeSalvar(valores)}
-            validateOnChange
+            onSubmit={valores => onClickSalvar(valores)}
             validateOnBlur
+            validateOnChange
           >
             {form => (
               <Form className="col-md-12 mb-4">
+                <ButtonGroup
+                  form={form}
+                  novoRegistro={novoRegistro}
+                  modoEdicao={modoEdicao}
+                  somenteConsulta={somenteConsulta}
+                  // permissoesTela={permissoesTela[RotasDto.ACOMPANHAMENTO_COMUNICADOS]}
+                  permissoesTela={{ podeIncluir: true, podeAlterar: true }}
+                  onClickExcluir={onClickExcluir}
+                  onClickVoltar={onClickVoltar}
+                  onClickBotaoPrincipal={() => onClickBotaoPrincipal(form)}
+                  labelBotaoPrincipal={idComunicado ? 'Salvar' : 'Cadastrar'}
+                />
                 <Linha className="row mb-2">
                   <Grid cols={4}>
                     <Label control="grupoId" text="Grupo" />
@@ -179,6 +260,44 @@ const ComunicadosCadastro = ({ match }) => {
                       placeholder="Procure pelo título do comunicado"
                       value={form.values.titulo}
                     />
+                  </Grid>
+                </Linha>
+                <Linha className="row">
+                  <Grid cols={12}>
+                    <Label control="textEditor" text="Descrição" />
+                    <TextEditor
+                      ref={textEditorRef}
+                      id="textEditor"
+                      height="120px"
+                      maxHeight="calc(100vh)"
+                      className={`${!descricaoValida && 'is-invalid'}`}
+                      value={descricaoComunicado}
+                      disabled={somenteConsulta}
+                    />
+                    {!descricaoValida && (
+                      <ErroValidacao>Campo obrigatório</ErroValidacao>
+                    )}
+                    <InseridoAlterado>
+                      {inseridoAlterado.criadoPor &&
+                      inseridoAlterado.criadoEm ? (
+                        <p className="pt-2">
+                          INSERIDO por {inseridoAlterado.criadoPor} em{' '}
+                          {inseridoAlterado.criadoEm}
+                        </p>
+                      ) : (
+                        ''
+                      )}
+
+                      {inseridoAlterado.alteradoPor &&
+                      inseridoAlterado.alteradoEm ? (
+                        <p>
+                          ALTERADO por {inseridoAlterado.alteradoPor} em{' '}
+                          {inseridoAlterado.alteradoEm}
+                        </p>
+                      ) : (
+                        ''
+                      )}
+                    </InseridoAlterado>
                   </Grid>
                 </Linha>
               </Form>
