f661064705fdd5005c655e7e68d89620daff717b
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-04-10 17:37:43 -0300

    AB#14249

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultaComunicado.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultaComunicado.cs
@@ -1,4 +1,6 @@
 ﻿using SME.SGP.Dto;
+using SME.SGP.Infra;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
@@ -6,5 +8,7 @@ namespace SME.SGP.Aplicacao
     public interface IConsultaComunicado
     {
         Task<ComunicadoCompletoDto> BuscarPorIdAsync(long id);
+
+        Task<IEnumerable<ComunicadoDto>> Listar(FiltroComunicadoDto filtro);
     }
 }
--- src/SME.SGP.Infra/Dtos/FiltroComunicadoDto.cs
+++ src/SME.SGP.Infra/Dtos/FiltroComunicadoDto.cs
@@ -4,9 +4,8 @@ namespace SME.SGP.Infra
 {
     public class FiltroComunicadoDto
     {
-        public DateTime DataEnvio { get; set; }
-        public DateTime DataExpiracao { get; set; }
-        public string Descricao { get; set; }
+        public DateTime? DataEnvio { get; set; }
+        public DateTime? DataExpiracao { get; set; }
         public string Titulo { get; set; }
     }
 }
