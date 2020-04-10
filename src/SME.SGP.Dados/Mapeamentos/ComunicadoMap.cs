3a689d7af481df9fd685bedcbeb98f260e9fec07
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-04-10 11:21:53 -0300

    AB#14252

---
+++ src/SME.SGP.Dados/Mapeamentos/ComunicadoMap.cs
@@ -0,0 +1,23 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class ComunicadoMap : DommelEntityMap<Comunicado>
+    {
+        public ComunicadoMap()
+        {
+            ToTable("comunicado");
+            Map(c => c.Id).ToColumn("Id");
+            Map(c => c.ComunicadoGrupoId).ToColumn("comunicado_grupo_id");
+            Map(c => c.AlteradoEm).ToColumn("alterado_em");
+            Map(c => c.AlteradoPor).ToColumn("alterado_por");
+            Map(c => c.AlteradoRF).ToColumn("alterado_rf");
+            Map(c => c.CriadoEm).ToColumn("criado_em");
+            Map(c => c.CriadoPor).ToColumn("criado_por");
+            Map(c => c.CriadoRF).ToColumn("criado_rF");
+            Map(c => c.Descricao).ToColumn("descricao");
+            Map(c => c.Titulo).ToColumn("Titulo");
+        }
+    }
+}
---
+++ src/SME.SGP.Dados/Repositorios/RepositorioComunicado.cs
@@ -0,0 +1,13 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioComunicado : RepositorioBase<Comunicado>, IRepositorioComunicado
+    {
+        public RepositorioComunicado(ISgpContext conexao) : base(conexao)
+        {
+        }
+    }
+}
