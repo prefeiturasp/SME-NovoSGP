7007cef2f5843e365242f3be7fcded97adafbbe4
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-04-10 11:21:53 -0300

    AB#14252

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultaComunicado.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra.Interfaces;
+using System;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ConsultaComunicado : ConsultasBase, IConsultaComunicado
+    {
+        private readonly IRepositorioComunicado repositorio;
+
+        public ConsultaComunicado(
+            IRepositorioComunicado repositorio,
+            IContextoAplicacao contextoAplicacao) : base(contextoAplicacao)
+        {
+            this.repositorio = repositorio ?? throw new ArgumentNullException(nameof(repositorio));
+        }
+    }
+}
