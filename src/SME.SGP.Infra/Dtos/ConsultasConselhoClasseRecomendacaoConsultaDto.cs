be07f54d95d692a2acd6b0b9ed37cdb7119362da
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-04-10 11:12:17 -0300

    Incluindo campo de anotações do aluno; Refatorando antiga anotações do aluno para anotações pedagogicas

--- src/SME.SGP.Infra/Dtos/ConsultasConselhoClasseRecomendacaoConsultaDto.cs
+++ src/SME.SGP.Infra/Dtos/ConsultasConselhoClasseRecomendacaoConsultaDto.cs
@@ -4,7 +4,8 @@ namespace SME.SGP.Infra
 {
     public class ConsultasConselhoClasseRecomendacaoConsultaDto
     {
-        public IEnumerable<FechamentoAlunoAnotacaoConselhoDto> AnotacoesPedagogicas { get; set; }
+        public IEnumerable<FechamentoAlunoAnotacaoConselhoDto> AnotacoesAluno { get; set; }
+        public string AnotacoesPedagocias { get; set; }
         public string RecomendacaoAluno { get; set; }
         public string RecomendacaoFamilia { get; set; }
     }
