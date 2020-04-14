a6b9c971d89a102fb50b25d30a44c06450616a51
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-04-14 11:53:45 -0300

    ajustes merge

--- src/SME.SGP.Dominio.Interfaces/Servicos/IServicoConselhoClasse.cs
+++ src/SME.SGP.Dominio.Interfaces/Servicos/IServicoConselhoClasse.cs
@@ -1,14 +1,10 @@
 ﻿using SME.SGP.Infra;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IServicoConselhoClasse
     {
-        Task<AuditoriaDto> GerarConselhoClasse(ConselhoClasse conselhoClasse);
         Task<AuditoriaConselhoClasseAlunoDto> SalvarConselhoClasseAluno(ConselhoClasseAluno conselhoClasseAluno);
     }
 }
