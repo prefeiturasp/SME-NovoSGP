d12dea6a3a58c8a44c1a0fd1ec2981d4bf41b337
Losi <rafael.losi@amcom.com.br>
2020-04-14 17:42:50 -0300

    12011 - Includo Auditoria no get das Recomendações;

--- src/SME.SGP.Infra/Dtos/ConsultasConselhoClasseRecomendacaoConsultaDto.cs
+++ src/SME.SGP.Infra/Dtos/ConsultasConselhoClasseRecomendacaoConsultaDto.cs
@@ -8,9 +8,10 @@ namespace SME.SGP.Infra
         public long ConselhoClasseId { get; set; }
         public IEnumerable<FechamentoAlunoAnotacaoConselhoDto> AnotacoesAluno { get; set; }
 
-        public string AnotacoesPedagocias { get; set; }
+        public string AnotacoesPedagogicas { get; set; }
         public int Bimestre { get; set; }
         public string RecomendacaoAluno { get; set; }
         public string RecomendacaoFamilia { get; set; }
+        public AuditoriaDto Auditoria { get; set; }
     }
 }
