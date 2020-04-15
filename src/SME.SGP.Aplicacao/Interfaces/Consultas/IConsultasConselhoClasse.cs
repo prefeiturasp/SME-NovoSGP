ba6b4f2f6d5ed8a365346de96e82dbd079a042f0
Losi <rafael.losi@amcom.com.br>
2020-04-15 16:36:27 -0300

    12011 - Não acessar aba final sem o fechamento do ultimo bimestre

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasConselhoClasse.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasConselhoClasse.cs
@@ -2,11 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
     public interface IConsultasConselhoClasse
     {
         ConselhoClasse ObterPorId(long conselhoClasseId);
+        Task<bool> ValidaConselhoClasseUltimoBimestre(Turma turma);
     }
 }
