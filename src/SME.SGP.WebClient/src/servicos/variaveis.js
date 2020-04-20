371eca221c2d7911d5a5896148deeae01d6baf1d
Cleiton <cleiton.dutra@amcom.com.br>
2020-04-20 14:48:04 -0300

    cache e tela branca - ajuste em rotas

--- src/SME.SGP.WebClient/src/servicos/variaveis.js
+++ src/SME.SGP.WebClient/src/servicos/variaveis.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const urlBase = () =>
   axios
-    .get('../../../configuracoes/variaveis.json')
+    .get('/../../../configuracoes/variaveis.json')
     .then(response => {
       return response.data.API_URL;
     })
