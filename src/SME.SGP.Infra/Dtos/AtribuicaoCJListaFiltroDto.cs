eddce0c97dc087f14ec9ac480ea99ca18f10d9c9
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-04-21 14:56:06 -0300

    Adicionado verificação de ano letivo da turma na listagem de atribuição CJ

--- src/SME.SGP.Infra/Dtos/AtribuicaoCJListaFiltroDto.cs
+++ src/SME.SGP.Infra/Dtos/AtribuicaoCJListaFiltroDto.cs
@@ -4,6 +4,7 @@
     {
         public string DreId { get; set; }
         public string UeId { get; set; }
+        public int AnoLetivo { get; set; }
         public string UsuarioNome { get; set; }
         public string UsuarioRf { get; set; }
     }
