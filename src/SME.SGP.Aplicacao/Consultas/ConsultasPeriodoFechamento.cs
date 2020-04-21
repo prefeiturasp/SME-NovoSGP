c13c87088696906578486b78b82611f9521d722b
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-04-21 10:34:58 -0300

    Correção conselho de classe final
    Correção conselho de classe final
    
    AB#15267

--- src/SME.SGP.Aplicacao/Consultas/ConsultasPeriodoFechamento.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasPeriodoFechamento.cs
@@ -78,7 +78,10 @@ namespace SME.SGP.Aplicacao
         public async Task<bool> TurmaEmPeriodoDeFechamento(Turma turma, TipoCalendario tipoCalendario, DateTime dataReferencia, int bimestre = 0)
         {
             var ueEmFechamento = await UeEmFechamento(turma, tipoCalendario, turma.Ue.CodigoUe, turma.Ue.Dre.CodigoDre, bimestre, dataReferencia);
-            return ueEmFechamento || await UeEmReaberturaDeFechamento(tipoCalendario, turma.Ue.CodigoUe, turma.Ue.Dre.CodigoDre, bimestre, dataReferencia);
+
+            var ueEmReabertura = await UeEmReaberturaDeFechamento(tipoCalendario, turma.Ue.CodigoUe, turma.Ue.Dre.CodigoDre, bimestre, dataReferencia);
+
+            return ueEmFechamento || ueEmReabertura;
         }
 
         private async Task<bool> UeEmFechamento(Turma turma, TipoCalendario tipoCalendario, string ueCodigo, string dreCodigo, int bimestre, DateTime dataReferencia)
