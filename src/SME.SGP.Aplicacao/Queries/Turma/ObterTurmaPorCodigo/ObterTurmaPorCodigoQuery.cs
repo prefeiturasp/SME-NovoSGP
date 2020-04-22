2430ae0a64200416119fd2effd05eea22b1bcb77
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-04-22 17:27:31 -0300

    Adicionando mediatr na aplicação; Endpoint de consulta de tipo de calendário por turma

---
+++ src/SME.SGP.Aplicacao/Queries/Turma/ObterTurmaPorCodigo/ObterTurmaPorCodigoQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterTurmaPorCodigoQuery : IRequest<Turma>
+    {
+        public string TurmaCodigo { get; set; }
+    }
+}
