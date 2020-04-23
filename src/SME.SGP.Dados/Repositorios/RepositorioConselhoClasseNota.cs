a56953ef25f1f251721ebee6f4369510b0f3dd41
Losi <rafael.losi@amcom.com.br>
2020-04-23 17:48:32 -0300

    12206 - Criado endpoint de consulta de notas e frequencia do conselho de classe

--- src/SME.SGP.Dados/Repositorios/RepositorioConselhoClasseNota.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioConselhoClasseNota.cs
@@ -1,4 +1,7 @@
-﻿using SME.SGP.Dominio;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
@@ -10,5 +13,15 @@ namespace SME.SGP.Dados.Repositorios
         {
         }
 
+        public async Task<IEnumerable<NotaConceitoBimestreComponenteDto>> ObterNotasAluno(long conselhoClasseId, string alunoCodigo)
+        {
+            var query = @"select ccn.componente_curricular_codigo as ComponenteCurricularCodigo, ccn.conceito_id as ConceitoId, ccn.nota
+                          from conselho_classe_aluno cca 
+                         inner join conselho_classe_nota ccn on ccn.conselho_classe_aluno_id = cca.id
+                          where cca.aluno_codigo = @alunoCodigo
+                            and cca.conselho_classe_id = @conselhoClasseId ";
+
+            return await database.Conexao.QueryAsync<NotaConceitoBimestreComponenteDto>(query, new { conselhoClasseId, alunoCodigo });
+        }
     }
 }
