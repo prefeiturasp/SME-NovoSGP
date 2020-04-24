64a525f789b933cf7ec2092d4ee7d44ecdb9570e
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-04-24 17:10:14 -0300

    Mock de endpoint de retorno de eventos aulas por dia

---
+++ src/SME.SGP.Infra/Dtos/EventosAulasNoDiaCalendarioDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class EventosAulasNoDiaCalendarioDto
+    {
+        public EventosAulasNoDiaCalendarioDto()
+        {
+            EventosAulas = new List<EventoAulaDto>();
+        }
+        public bool PodeCadastrarAula { get; set; }
+        public IList<EventoAulaDto> EventosAulas { get; set; }        
+        public string MensagemPeriodoEncerrado { get; set; }
+    }
+}
