e708b556abe32df66b7cff1e8bd87d7aea43dc9f
eliton-amcom <eliton.santos@amcom.com.br>
2020-04-24 16:38:51 -0300

    Layout justificativa

--- src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/Justificativa/Justificativa.js
+++ src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/Justificativa/Justificativa.js
@@ -1,68 +1,79 @@
 import React, { useState } from 'react';
-import Editor from '~/componentes/editor/editor';
-import { Formik } from 'formik';
+import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Historico } from './Justificativa.css';
-import { Colors } from '~/componentes';
+import Editor from '~/componentes/editor/editor';
+import { Colors, Auditoria } from '~/componentes';
 import Button from '~/componentes/button';
 
 const Justificativa = () => {
-  const validacoes = {
+  const validacoes = Yup.object({
     justificativa: Yup.string().required('Campo justificativa é obrigatório'),
-  };
+  });
+
+  const valoresIniciais = { justificativa: '' };
+  const [auditoria, setAuditoria] = useState([]);
   const onChangeJustificativa = () => {};
   const salvarJustificativa = valor => {};
 
-  const clicouBotaoSalvar = (form, e) => {
-    e.persist();
-    form.validateForm().then(() => form.handleSubmit(e));
+  const clicouBotaoSalvar = form => {
+    const arrayCampos = Object.keys(valoresIniciais);
+    arrayCampos.forEach(campo => {
+      form.setFieldTouched(campo, true, true);
+    });
+    form.validateForm().then(() => {
+      if (form.isValid || Object.keys(form.errors).length === 0) {
+        form.handleSubmit(e => e);
+      }
+    });
   };
+
   return (
     <>
       <div className="row">
         <div className="col-md-12">
           <span>Justificativa de nota pós-conselho</span>
         </div>
-        <div className="col-sm-12 col-md-8">
+        <div className="col-md-12">
           <Formik
             enableReinitialize
             onSubmit={valor => salvarJustificativa(valor)}
             validationSchema={validacoes}
+            initialValues={valoresIniciais}
             validateOnBlur={false}
             validateOnChange={false}
           >
             {form => (
-              <fieldset className="mt-3">
-                <Editor
-                  form={form}
-                  name="justificativa"
-                  id="justificativa"
-                  onChange={onChangeJustificativa}
-                />
-                <div className="d-flex justify-content-end pt-2">
-                  <Button
-                    label="Salvar"
-                    color={Colors.Roxo}
-                    onClick={e => {
-                      clicouBotaoSalvar(form, e);
-                    }}
-                    border
+              <Form>
+                <fieldset className="mt-3">
+                  <Editor
+                    form={form}
+                    name="justificativa"
+                    id="justificativa"
+                    onChange={onChangeJustificativa}
                   />
-                </div>
-              </fieldset>
+                  <div className="d-flex justify-content-end pt-2">
+                    <Button
+                      label="Salvar"
+                      color={Colors.Roxo}
+                      onClick={e => {
+                        clicouBotaoSalvar(form, e);
+                      }}
+                      border
+                    />
+                  </div>
+                </fieldset>
+              </Form>
             )}
           </Formik>
         </div>
-        <Historico className="col-sm-12 col-md-4">
-          <span className="label">Histórico:</span>
-          <br />
-          <span>
-            Notas (ou conceitos) da avaliação XYZ inseridos por por Nome
-            Usuário(9999999) em 10/01/2019, às 15:00. Notas (ou conceitos) da
-            avaliação ABC alterados por Nome Usuário(9999999) em 11/01/2019, às
-            16:00.{' '}
-          </span>
-        </Historico>
+        <Auditoria
+          criadoEm={auditoria.criadoEm}
+          criadoPor={auditoria.criadoPor}
+          criadoRf={auditoria.criadoRf}
+          alteradoPor={auditoria.alteradoPor}
+          alteradoEm={auditoria.alteradoEm}
+          alteradoRf={auditoria.alteradoRf}
+        />
       </div>
     </>
   );
