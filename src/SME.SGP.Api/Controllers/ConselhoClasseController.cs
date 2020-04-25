4e902a7a0a2d9fda5944d10a54077071443532f9
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-04-24 21:07:38 -0300

    Mock conselho de classe nota Dto
    Mock conselho de classe nota Dto
    
    AB#15559

--- src/SME.SGP.Api/Controllers/ConselhoClasseController.cs
+++ src/SME.SGP.Api/Controllers/ConselhoClasseController.cs
@@ -4,6 +4,7 @@ using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
+using System;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
@@ -27,6 +28,7 @@ namespace SME.SGP.Api.Controllers
 
         [HttpPost("recomendacoes")]
         [ProducesResponseType(401)]
+        [ProducesResponseType(204)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(AuditoriaConselhoClasseAlunoDto), 200)]
         [Permissao(Permissao.CC_I, Policy = "Bearer")]
@@ -34,5 +36,43 @@ namespace SME.SGP.Api.Controllers
         {
             return Ok(await comandosConselhoClasseAluno.SalvarAsync(conselhoClasseAlunoDto));
         }
+
+        [HttpGet("detalhamento/{id}")]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(204)] 
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> DetalhamentoNotaJustificativa(long id)
+        {
+            var retorno = new ConselhoDeClasseNotaDto
+            {
+                Nota = 5,
+                Migrado = false,
+                Justificativa = "Teste sodfsdfasduhfasudfhuasdfuhasdfhu",
+                Id = 1,
+                ComponenteCurricularCodigo = 1,
+                Conceito = new ConceitoDto
+                {
+                    Id = 1,
+                    Aprovado = false,
+                    Descricao = "teste",
+                    Valor = "NS"
+                },
+                ConceitoId = 1,
+                ConselhoClasseAlunoId = 1,
+                Auditoria = new AuditoriaDto
+                {
+                    Id = 1,
+                    AlteradoEm = DateTime.Now,
+                    AlteradoPor = "Professor",
+                    AlteradoRF = "1234567",
+                    CriadoEm = DateTime.Now.AddMinutes(-5),
+                    CriadoPor = "Professor",
+                    CriadoRF = "1234567"
+                }
+            };
+
+            return Ok(retorno);
+        }
+
     }
 }
