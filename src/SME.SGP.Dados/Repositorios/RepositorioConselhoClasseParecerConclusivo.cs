7cb503e7d387dc4c2ced4f316e37adfa256c050c
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-04-27 18:14:58 -0300

    Modelagem
    Modelagem
    
    AB#12232

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioConselhoClasseParecerConclusivo.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using SME.SGP.Dados.Repositorios;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioConselhoClasseParecerConclusivo : RepositorioBase<ConselhoClasseParecerConclusivo>, IRepositorioConselhoClasseParecerConclusivo
+    {
+        public RepositorioConselhoClasseParecerConclusivo(ISgpContext database) : base(database)
+        {
+        }
+
+        public async Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterListaPorTurmaId(long turmaId, DateTime dataConsulta)
+        {
+            var where = "t.id = @parametro";
+
+            return await ObterListaPorTurma(where, turmaId, dataConsulta);
+        }
+
+        public async Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterListaPorTurmaCodigo(long turmaCodigo, DateTime dataConsulta)
+        {
+            var where = "t.turma_id = @parametro";
+
+            return await ObterListaPorTurma(where, turmaCodigo, dataConsulta);
+        }
+
+        private async Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterListaPorTurma(string where, object parametro, DateTime dataConsulta)
+        {
+            var sql = string.Format(ObterSqlParecerConclusivoTurma(), where);
+
+            var param = new { parametro, dataConsulta };
+
+            return await database.Conexao.QueryAsync<ConselhoClasseParecerConclusivo>(sql, param);
+        }
+
+        private string ObterSqlParecerConclusivoTurma()
+        {
+            return @"select ccp.* from conselho_classe_parecer ccp 
+                        inner join conselho_classe_parecer_ano ccpa on ccp.id = ccpa.parecer_id 
+                        inner join turma t on cast(ccpa.ano_turma as varchar) = t.ano and ccpa.modalidade = t.modalidade_codigo
+                        where {0} and ccpa.inicio_vigencia <= @dataConsulta and (ccpa.fim_vigencia >= @dataConsulta or ccpa.fim_vigencia is null)";
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConselhoClasseParecerConclusivo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorioConselhoClasseParecerConclusivo : IRepositorioBase<ConselhoClasseParecerConclusivo>
+    {
+        Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterListaPorTurmaCodigo(long turmaCodigo, DateTime dataConsulta);
+        Task<IEnumerable<ConselhoClasseParecerConclusivo>> ObterListaPorTurmaId(long turmaId, DateTime dataConsulta);
+    }
+}
