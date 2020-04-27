8db1b3290b0bfd95b25d0149daeed79fc7652f25
Losi <rafael.losi@amcom.com.br>
2020-04-27 19:02:47 -0300

    12340 - Criado Entidades, Consultas, Repositorios e Comandos do Relatrorio Semestral

---
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasRelatorioSemestralAluno.cs
@@ -0,0 +1,22 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ConsultasRelatorioSemestralAluno : ConsultasBase, IConsultasRelatorioSemestralAluno
+    {
+        public ConsultasRelatorioSemestralAluno(IContextoAplicacao contextoAplicacao): base(contextoAplicacao)
+        {
+
+        }
+
+        public Task<RelatorioSemestralAluno> ObterPorTurmaAlunoAsync(long relatorioSemestralId, string alunoCodigo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
