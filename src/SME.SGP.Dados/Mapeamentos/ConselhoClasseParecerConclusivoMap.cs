6b91005bd90ece0deeacc55b7f6f9716206a5e75
AlexsanderAMcom <alexsander.camargo@amcom.com.br>
2020-04-27 18:14:58 -0300

    Modelagem
    Modelagem
    
    AB#12232

---
+++ src/SME.SGP.Dados/Mapeamentos/ConselhoClasseParecerConclusivoMap.cs
@@ -0,0 +1,21 @@
+﻿using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class ConselhoClasseParecerConclusivoMap : BaseMap<ConselhoClasseParecerConclusivo>
+    {
+        public ConselhoClasseParecerConclusivoMap()
+        {
+            ToTable("conselho_classe_parecer");
+            Map(x => x.Nome).ToColumn("nome");
+            Map(x => x.Aprovado).ToColumn("aprovado");
+            Map(x => x.Frequencia).ToColumn("frequencia");
+            Map(x => x.Conselho).ToColumn("conselho");
+            Map(x => x.InicioVigencia).ToColumn("inicio_vigencia");
+            Map(x => x.FimVigencia).ToColumn("fim_vigencia");
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/ConselhoClasseParecerConclusivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio
+{
+    public class ConselhoClasseParecerConclusivo : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public bool Aprovado { get; set; }
+        public bool Frequencia { get; set; }
+        public bool Conselho { get; set; }
+        public DateTime InicioVigencia { get; set; }
+        public DateTime? FimVigencia { get; set; }
+    }
+}
