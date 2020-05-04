f1b6d8c44d87e3f8b445fb6966180206c4a309e6
eliton-amcom <eliton.santos@amcom.com.br>
2020-05-04 19:20:21 -0300

    validação de inserir todas as notas

--- src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/servicoSalvarConselhoClasse.js
+++ src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/servicoSalvarConselhoClasse.js
@@ -9,6 +9,7 @@ import {
   setIdCamposNotasPosConselho,
   setNotaConceitoPosConselhoAtual,
   setGerandoParecerConclusivo,
+  addDisciplinasComNotaPosConselho,
 } from '~/redux/modulos/conselhoClasse/actions';
 import notasConceitos from '~/dtos/notasConceitos';
 
@@ -203,6 +204,7 @@ class ServicoSalvarConselhoClasse {
     ).catch(e => erro(e));
 
     if (retorno && retorno.status === 200) {
+      dispatch(addDisciplinasComNotaPosConselho(codigoComponenteCurricular));
       if (!dadosPrincipaisConselhoClasse.conselhoClasseId) {
         dadosPrincipaisConselhoClasse.conselhoClasseId =
           retorno.data.conselhoClasseId;
@@ -232,12 +234,19 @@ class ServicoSalvarConselhoClasse {
     return false;
   };
 
-  validarNotaPosConselho = async () => {
+  validarNotaPosConselho = async clickouEmSalvar => {
     const { dispatch } = store;
 
     const limparDadosNotaPosConselhoJustificativa = () => {
       dispatch(setExpandirLinha([]));
       dispatch(setNotaConceitoPosConselhoAtual({}));
+    };
+
+    const erroAoValidarNotasSalvas = () => {
+      erro(
+        'O estudante precisa ter todas as notas pós-conselho informadas para que seja possível salvar o seu conselho de classe'
+      );
+      return false;
     };
 
     const state = store.getState();
@@ -248,7 +257,29 @@ class ServicoSalvarConselhoClasse {
       notaConceitoPosConselhoAtual,
       dadosPrincipaisConselhoClasse,
       desabilitarCampos,
+      dadosListasNotasConceitos,
+      disciplinasComNotaPosConselho,
     } = conselhoClasse;
+
+    const validarNotasConceitosSalvos = componentes => {
+      if (!componentes || componentes.length === 0) return true;
+      for (let index = 0; index < componentes.length; index++) {
+        const componente = componentes[index];
+        if (!componente.notaPosConselho.nota) {
+          if (
+            !disciplinasComNotaPosConselho.find(
+              d => String(d) === String(componente.codigoComponenteCurricular)
+            )
+          ) {
+            const retorno = erroAoValidarNotasSalvas();
+            if (!retorno) {
+              return retorno;
+            }
+          }
+        }
+      }
+      return true;
+    };
 
     if (desabilitarCampos) {
       return true;
@@ -281,6 +312,27 @@ class ServicoSalvarConselhoClasse {
 
       // Voltar para a tela e não executa a ação!
       return false;
+    }
+
+    if (clickouEmSalvar) {
+      for (let index = 0; index < dadosListasNotasConceitos.length; index++) {
+        const dados = dadosListasNotasConceitos[index];
+        const validacaoCompCurricular = validarNotasConceitosSalvos(
+          dados.componentesCurriculares
+        );
+        if (!validacaoCompCurricular) {
+          return false;
+        }
+        const validacaoCompRegencia = dados.componenteRegencia
+          ? validarNotasConceitosSalvos(
+              dados.componenteRegencia.componentesCurriculares
+            )
+          : true;
+        if (!validacaoCompRegencia) {
+          return false;
+        }
+      }
+      return true;
     }
 
     return true;
