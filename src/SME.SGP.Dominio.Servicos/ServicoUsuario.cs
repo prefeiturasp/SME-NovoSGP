112564d7016a2682dd2dfce87e64adf43fbbb297
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-05-06 15:27:05 -0300

    Ajustes busca login

--- src/SME.SGP.Dominio.Servicos/ServicoUsuario.cs
+++ src/SME.SGP.Dominio.Servicos/ServicoUsuario.cs
@@ -152,12 +152,9 @@ namespace SME.SGP.Dominio
             return usuario;
         }
 
-        public Usuario ObterUsuarioPorCodigoRfLoginOuAdiciona(string codigoRf, string login = "", string nome = "", string email = "")
+        public Usuario ObterUsuarioPorCodigoRfLoginOuAdiciona(string codigoRf, string login = "", string nome = "", string email = "", bool buscaLogin = false)
         {
-            var usuario = repositorioUsuario.ObterPorCodigoRfLogin(codigoRf, login);
-
-            if (usuario == null)
-                usuario = repositorioUsuario.ObterPorCodigoRfLogin(null, login);
+            var usuario = repositorioUsuario.ObterPorCodigoRfLogin(buscaLogin ? null : codigoRf, login);
 
             if (usuario != null)
             {
