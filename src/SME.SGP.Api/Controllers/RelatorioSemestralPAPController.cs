5c6c66836513991facfed42cc45a01d897c1a001
Losi <rafael.losi@amcom.com.br>
2020-05-07 16:14:44 -0300

    11895 - Criados Endpoints e Mocks para o front

---
+++ src/SME.SGP.Api/Controllers/RelatorioSemestralPAPController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/relatorios/pap/semestral")]
+    [Authorize("Bearer")]
+    public class RelatorioSemestralPAPController : ControllerBase
+    {
+        [HttpGet("semestres")]
+        [ProducesResponseType(typeof(IEnumerable<SemestreAcompanhamentoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        //[Permissao(Permissao.NC_C, Permissao.NC_I, Policy = "Bearer")]
+        public async Task<IActionResult> ObterSemestres()
+        {
+            return Ok(new List<SemestreAcompanhamentoDto>()
+            {
+                new SemestreAcompanhamentoDto(1, "Acompanhamento 1º Semestre"),
+                new SemestreAcompanhamentoDto(2, "Acompanhamento 2º Semestre")
+            });
+        }
+
+        [HttpGet("turmas/{turmaCodigo}/semestres/{semestre}/alunos/{alunoCodigo}")]
+        [ProducesResponseType(typeof(RelatorioSemestralAlunoDto), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        //[Permissao(Permissao.NC_C, Permissao.NC_I, Policy = "Bearer")]
+        public async Task<IActionResult> ObterRelatorioAluno(string alunoCodigo, string turmaCodigo, int semestre, [FromServices]IConsultasRelatorioSemestralAluno consultasRelatorioSemestralAluno)
+        {
+            return Ok(new RelatorioSemestralAlunoDto()
+            {
+                PodeEditar = true,
+                RelatorioSemestralId = 1,
+                RelatorioSemestralAlunoId = 1,
+                Secoes = new List<RelatorioSemestralAlunoSecaoDto>()
+                {
+                    new RelatorioSemestralAlunoSecaoDto(1, "Histórico do Estudante", "Trajetória do estudante, reprovações, histórico de faltas, acompanhamento das aprendizagens", true, "Teste valor histórico"),
+                    new RelatorioSemestralAlunoSecaoDto(2, "Dificuldades", "Dificuldades apresentadas inicialmente ", true, "Teste valor dificuldades"),
+                    new RelatorioSemestralAlunoSecaoDto(3, "Encaminhamentos", "Encaminhamentos realizados", true, "Teste valor Encaminhamentos"),
+                    new RelatorioSemestralAlunoSecaoDto(4, "Avanços", "Avanços observados", true, "Teste valor Avanços"),
+                    new RelatorioSemestralAlunoSecaoDto(5, "Outros", "Outras observações", false, "Teste valor outros"),
+                },
+                Auditoria = new AuditoriaDto()
+                {
+                    Id = 1,
+                    CriadoPor = "Fulano",
+                    CriadoEm = DateTime.Today,
+                    CriadoRF = "789456123",
+                    AlteradoPor = "Fulano",
+                    AlteradoEm = DateTime.Now,
+                    AlteradoRF = "789456123",
+                }
+            });
+        }
+
+        [HttpPost("turmas/{turmaCodigo}/semestres/{semestre}/alunos/{alunoCodigo}")]
+        [ProducesResponseType(typeof(AuditoriaRelatorioSemestralAlunoDto), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        //[Permissao(Permissao.NC_C, Permissao.NC_I, Policy = "Bearer")]
+        public async Task<IActionResult> SalvarRelatorioAluno(string alunoCodigo, string turmaCodigo, int semestre
+            , long relatorioSemestralId, long relatorioSemestralAlunoId, [FromBody] RelatorioSemestralAlunoPersistenciaDto relatorioSemestralAlunoDto
+            , [FromServices]IComandosRelatorioSemestralAluno comandosRelatorioSemestralAluno)
+            => await comandosRelatorioSemestralAluno.Salvar(alunoCodigo, turmaCodigo, semestre, relatorioSemestralId, relatorioSemestralAlunoId, relatorioSemestralAlunoDto);
+
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/RelatorioSemestralAlunoPersistenciaDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class RelatorioSemestralAlunoPersistenciaDto
+    {
+        public IEnumerable<RelatorioSemestralAlunoSecaoDto> Secoes { get; set; }
+    }
+}
