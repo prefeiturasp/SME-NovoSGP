b6b4cd9c336dcd41e288952dd49845c3a5594f04
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-05-08 09:59:35 -0300

    Ajuste regra de criaçãod e aula

--- src/SME.SGP.Aplicacao/Queries/Aula/ObterPodeCadastrarAulaPorData/ObterPodeCadastrarAulaPorDataQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Aula/ObterPodeCadastrarAulaPorData/ObterPodeCadastrarAulaPorDataQueryHandler.cs
@@ -29,7 +29,7 @@ namespace SME.SGP.Aplicacao
 
             var hoje = DateTime.Today;
 
-            var temEventoLetivoNoDia = repositorioEvento.EhEventoLetivoPorTipoDeCalendarioDataDreUe(request.TipoCalendarioId, request.DataAula, request.DreCodigo, request.UeCodigo);
+            var temEventoLetivoNoDia = !repositorioEvento.EhEventoLetivoPorTipoDeCalendarioDataDreUe(request.TipoCalendarioId, request.DataAula, request.DreCodigo, request.UeCodigo);
 
             if (temEventoLetivoNoDia)
                 return new PodeCadastrarAulaPorDataRetornoDto(true, string.Empty);
