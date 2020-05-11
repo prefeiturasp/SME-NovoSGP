9fc4c211f73c9aaf23b1868030873ad862260768
Losi <rafael.losi@amcom.com.br>
2020-05-11 20:50:02 -0300

    12470 - Incluso filtro de authorização por autenticação básica no HangFire

---
+++ src/Bibliotecas/SME.Background.Hangfire/Filters/SgpAuthAuthorizationFilterOptions.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire.Dashboard.BasicAuthorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.Background.Hangfire
+{
+    public class SgpAuthAuthorizationFilterOptions : BasicAuthAuthorizationFilterOptions
+    {
+        public static string AdminUser = "admin";
+        public SgpAuthAuthorizationFilterOptions()
+        {
+            RequireSsl = false;
+            SslRedirect = false;
+            LoginCaseSensitive = true;
+            Users = new[]
+            {
+                new BasicAuthAuthorizationUser
+                {
+                    Login = AdminUser,
+                    PasswordClear =  "Sgp@amcom"
+                },
+                new BasicAuthAuthorizationUser
+                {
+                    Login = "user",
+                    PasswordClear =  "Sgp@1234"
+                },
+            };
+        }
+    }
+}
