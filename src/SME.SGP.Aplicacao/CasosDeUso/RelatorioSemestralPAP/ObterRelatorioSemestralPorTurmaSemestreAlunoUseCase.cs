dd66aeb87e2d2cdcd5c8479b35ebd8d2f890472e
eliton-amcom <eliton.santos@amcom.com.br>
2020-05-12 09:14:12 -0300

    correções sugeridas

--- src/SME.SGP.Aplicacao/CasosDeUso/RelatorioSemestralPAP/ObterRelatorioSemestralPorTurmaSemestreAlunoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/RelatorioSemestralPAP/ObterRelatorioSemestralPorTurmaSemestreAlunoUseCase.cs
@@ -9,23 +9,17 @@ namespace SME.SGP.Aplicacao
     public class ObterRelatorioSemestralPorTurmaSemestreAlunoUseCase
     {
         public static async Task<RelatorioSemestralAlunoDto> Executar(IMediator mediator, string alunoCodigo, string turmaCodigo, int semestre, IConsultasPeriodoEscolar consultasPeriodoEscolar)
-        {
+        {            
+            var relatorio = await mediator.Send(new ObterRelatorioSemestralPorTurmaSemestreAlunoQuery(alunoCodigo, turmaCodigo, semestre));
+
             var turma = await mediator.Send(new ObterTurmaPorCodigoQuery()
             {
                 TurmaCodigo = turmaCodigo
             });
-            var relatorio = await mediator.Send(new ObterRelatorioSemestralPorTurmaSemestreAlunoQuery(alunoCodigo, turmaCodigo, semestre));
 
             var bimestreAtual = consultasPeriodoEscolar.ObterBimestre(DateTime.Today, turma.ModalidadeCodigo, turma.Semestre);
 
-            if (turma.ModalidadeCodigo != Modalidade.EJA)
-            {
-                relatorio.PodeEditar = bimestreAtual == 2 || bimestreAtual == 4;
-            }
-            else
-            {
-                relatorio.PodeEditar = true;
-            }
+            relatorio.PodeEditar = await mediator.Send(new PodeEditarRelatorioQuery(turma, bimestreAtual));
 
             return relatorio;
         }
