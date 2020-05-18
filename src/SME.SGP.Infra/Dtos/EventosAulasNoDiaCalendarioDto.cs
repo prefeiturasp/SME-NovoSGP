861ef5d7d762dd85441ad8326ef98389f1a0311e
Robson <robson.silva@amcom.com.br>
2020-05-18 10:48:01 -0300

    Inclusão da funcionalidade de atualizar o mês ao selecionar o dia no calendário de professor.

--- src/SME.SGP.Infra/Dtos/EventosAulasNoDiaCalendarioDto.cs
+++ src/SME.SGP.Infra/Dtos/EventosAulasNoDiaCalendarioDto.cs
@@ -13,5 +13,6 @@ namespace SME.SGP.Infra
         public bool PodeCadastrarAula { get; set; }
         public IEnumerable<EventoAulaDto> EventosAulas { get; set; }        
         public string MensagemPeriodoEncerrado { get; set; }
+        public IEnumerable<EventoAulaDiaDto> EventosAulasMes { get; set; }
     }
 }
