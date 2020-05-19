09146be0c135cbb347792dc50dd60d8f515665e7
eliton-amcom <eliton.santos@amcom.com.br>
2020-05-19 19:14:12 -0300

    retirar import mediator

--- src/SME.SGP.Api/Controllers/ObjetivoAprendizagemController.cs
+++ src/SME.SGP.Api/Controllers/ObjetivoAprendizagemController.cs
@@ -1,5 +1,4 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
