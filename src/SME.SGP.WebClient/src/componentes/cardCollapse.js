c5f33e5bcdccf052d2d9a640fdd0fba59c86b445
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-05-26 18:59:11 -0300

    Correção no evento de click dos collapses

--- src/SME.SGP.WebClient/src/componentes/cardCollapse.js
+++ src/SME.SGP.WebClient/src/componentes/cardCollapse.js
@@ -19,7 +19,14 @@ const CardCollapse = React.forwardRef((props, ref) => {
 
   return (
     <Card ref={ref} className="card shadow-sm mb-3">
-      <CardHeader indice={indice} border icon show={show} onclick={onClick} configuracao={configCabecalho}>
+      <CardHeader
+        indice={indice}
+        border
+        icon={icon}
+        show={show}
+        onClick={onClick}
+        configuracao={configCabecalho}
+      >
         {titulo}
       </CardHeader>
       <div className={`collapse fade ${show && 'show'}`} id={`${indice}`}>
