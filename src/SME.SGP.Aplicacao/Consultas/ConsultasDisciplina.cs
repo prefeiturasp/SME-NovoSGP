7938563df7864c480f6adc14f034db455a9db02c
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-05-27 18:18:54 -0300

    Ajuste na consulta de disciplinas

--- src/SME.SGP.Aplicacao/Consultas/ConsultasDisciplina.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasDisciplina.cs
@@ -365,7 +365,7 @@ namespace SME.SGP.Aplicacao
             return disciplinasDto;
         }
 
-        private IEnumerable<DisciplinaResposta> MapearComponentes(IEnumerable<ComponenteCurricularEol> componentesCurriculares)
+        public IEnumerable<DisciplinaResposta> MapearComponentes(IEnumerable<ComponenteCurricularEol> componentesCurriculares)
         {
             foreach (var componenteCurricular in componentesCurriculares)
                 yield return new DisciplinaResposta()
