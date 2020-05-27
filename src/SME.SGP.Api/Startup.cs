196e99816cd0c2d4b744146de762778c0ab986be
Robson <robson.silva@amcom.com.br>
2020-05-27 16:47:23 -0300

    #15257 - Ajuste na criação de evento para que não considere dia posterior após as 21hrs.

--- src/SME.SGP.Api/Startup.cs
+++ src/SME.SGP.Api/Startup.cs
@@ -44,6 +44,7 @@ namespace SME.SGP.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseRequestLocalization();
 
             app.UseHttpsRedirection();
             app.UseSwagger();
