24b92f46764fadba7c66f99275db18d1c50e622c
Cleiton <cleiton.dutra@amcom.com.br>
2020-05-28 11:03:56 -0300

    add menu relatorio semestral

--- src/SME.SGP.Infra/Enumerados/Permissao.cs
+++ src/SME.SGP.Infra/Enumerados/Permissao.cs
@@ -363,6 +363,9 @@
         [PermissaoMenu(Menu = "Compensação de Ausência", Icone = "", Agrupamento = "Diário de Classe", OrdemAgrupamento = 1, OrdemMenu = 6, EhAlteracao = true, Url = "/diario-classe/compensacao-ausencia")]
         CA_A = 115,
 
+        [PermissaoMenu(Menu = "Relatório Semestral", Icone = "fas fa-file-alt", Agrupamento = "Relatórios", OrdemAgrupamento = 7, OrdemMenu = 3, EhConsulta = true, Url = "/relatorios/pap/relatorio-semestral")]
+        RSP_C = 144,
+
         [PermissaoMenu(Menu = "Resumos e gráficos PAP", Icone = "fas fa-file-alt", Agrupamento = "Relatórios", OrdemAgrupamento = 7, OrdemMenu = 2, EhConsulta = true, Url = "/relatorios/pap/resumos-e-graficos")]
         RGP_C = 120,
 
@@ -404,17 +407,18 @@
 
         [PermissaoMenu(Menu = "Conselho de Classe", Icone = "", Agrupamento = "Fechamento", OrdemAgrupamento = 2, OrdemMenu = 3, EhAlteracao = true, Url = "/fechamento/conselho-classe")]
         CC_A = 139,
+         
+        // TODO - Débto técnico para adicionar submenu!
+        //[PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhConsulta = true, Url = "/relatorios/pap/relatorio-semestral")]
+        //RSP_C = 144,
 
-        [PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhConsulta = true, Url = "/relatorios/pap/relatorio-semestral")]
-        RSP_C = 144,
+        //[PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhInclusao = true, Url = "/relatorios/pap/relatorio-semestral")]
+        //RSP_I = 145,
 
-        [PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhInclusao = true, Url = "/relatorios/pap/relatorio-semestral")]
-        RSP_I = 145,
+        //[PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhExclusao = true, Url = "/relatorios/pap/relatorio-semestral")]
+        //RSP_E = 146,
 
-        [PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhExclusao = true, Url = "/relatorios/pap/relatorio-semestral")]
-        RSP_E = 146,
-
-        [PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhAlteracao = true, Url = "/relatorios/pap/relatorio-semestral")]
-        RSP_A = 147,
+        //[PermissaoMenu(Menu = "Relatório Semestral ", Icone = "", Agrupamento = "Relatórios ", OrdemAgrupamento = 2, OrdemMenu = 4, EhAlteracao = true, Url = "/relatorios/pap/relatorio-semestral")]
+        //RSP_A = 147,
     }
 }
