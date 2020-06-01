26f5e048dff9a56d637f0d0d7d45600900da5178
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-01 16:12:28 -0300

    Início da refatoração do enpoint de cadastro de aula

---
+++ src/SME.SGP.Aplicacao/Commands/Aula/InserirAula/InserirAulaUnicaCommand.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+
+namespace SME.SGP.Aplicacao.Commands.Aula.InserirAula
+{
+    public class InserirAulaUnicaCommand : IRequest<RetornoBaseDto>
+    {
+        public InserirAulaUnicaCommand(Usuario usuario, DateTime dataAula, Turma turma, string componenteCurricularId, string codigoRfProfessor, TipoCalendario tipoCalendario)
+        {
+            //TODO VALIDAR CAMPOS
+
+            DataAula = dataAula;
+            Turma = turma;
+            ComponenteCurricularId = componenteCurricularId;
+            CodigoRfProfessor = codigoRfProfessor;
+            TipoCalendario = tipoCalendario;
+        }
+
+        public DateTime DataAula { get; private set; }
+
+        public Turma Turma { get; private set; }
+
+        public string ComponenteCurricularId { get; set; }
+
+        public string CodigoRfProfessor { get; set; }
+
+        public string UeId { get; set; }
+
+        public TipoCalendario TipoCalendario { get; set; }
+
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Commands/Aula/InserirAula/InserirAulaUnicaCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Commands.Aula.InserirAula
+{
+    public class InserirAulaUnicaCommandHandler : IRequestHandler<InserirAulaUnicaCommand, RetornoBaseDto>
+    {
+        private readonly IRepositorioAula repositorioAula;
+        private readonly IServicoDiaLetivo servicoDiaLetivo;
+        private readonly IRepositorioAtribuicaoCJ repositorioAtribuicaoCJ;
+
+        public InserirAulaUnicaCommandHandler(IRepositorioAula repositorioAula,
+                                              IServicoDiaLetivo servicoDiaLetivo,
+                                              IRepositorioAtribuicaoCJ repositorioAtribuicaoCJ)
+        {
+            this.repositorioAula = repositorioAula ?? throw new System.ArgumentNullException(nameof(repositorioAula));
+            this.servicoDiaLetivo = servicoDiaLetivo ?? throw new System.ArgumentNullException(nameof(servicoDiaLetivo));
+            this.repositorioAtribuicaoCJ = repositorioAtribuicaoCJ ?? throw new System.ArgumentNullException(nameof(repositorioAtribuicaoCJ));
+        }
+        public async Task<RetornoBaseDto> Handle(InserirAulaUnicaCommand request, CancellationToken cancellationToken)
+        {
+            var retorno = new RetornoBaseDto();
+
+            var aulasExistentes = await repositorioAula.ObterAulasPorDataTurmaDisciplinaProfessorRf(request.DataAula, request.Turma.CodigoTurma, request.ComponenteCurricularId, request.CodigoRfProfessor);
+            if (aulasExistentes != null)
+                throw new NegocioException("Já existe uma aula criada neste dia para este componente curricular");
+
+            var temLiberacaoExcepcionalNessaData = servicoDiaLetivo.ValidaSeEhLiberacaoExcepcional(request.DataAula, request.TipoCalendario.Id, request.UeId);
+            
+            var diaLetivo = servicoDiaLetivo.ValidarSeEhDiaLetivo(request.DataAula, request.TipoCalendario.Id, null, request.UeId);
+
+            if (!temLiberacaoExcepcionalNessaData && !diaLetivo)
+                throw new NegocioException("Não é possível cadastrar essa aula pois a data informada está fora do período letivo.");
+
+            return retorno;
+        }
+    }
+}
