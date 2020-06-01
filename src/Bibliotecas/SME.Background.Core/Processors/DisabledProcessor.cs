7dfb24f9c90ec11d7fff60d78965ae4437748b1c
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-06-01 19:31:16 -0300

    Ajustes sonar

--- src/Bibliotecas/SME.Background.Core/Processors/DisabledProcessor.cs
+++ src/Bibliotecas/SME.Background.Core/Processors/DisabledProcessor.cs
@@ -28,7 +28,7 @@ namespace SME.Background.Core.Processors
 
         public void ExecutarPeriodicamente(Expression<Action> metodo, string cron)
         {
-            throw new ExcecaoServicoDesativado("Não é possível realizar novos processamentos periódicos pois o serviço de processamento em segundo plano está desativado");
+            throw new ExcecaoServicoDesativadoException("Não é possível realizar novos processamentos periódicos pois o serviço de processamento em segundo plano está desativado");
         }
 
         public void ExecutarPeriodicamente<T>(Expression<Action<T>> metodo, string cron)
