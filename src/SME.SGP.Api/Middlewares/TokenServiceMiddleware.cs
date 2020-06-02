bd2f341a7bf8cb5e24d6a1ac7fad41242d0538af
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-01 22:30:34 -0300

    Adicionando fluent validation ao pipeline do aspnetcore mvc para validação do modelstate no início do request

--- src/SME.SGP.Api/Middlewares/TokenServiceMiddleware.cs
+++ src/SME.SGP.Api/Middlewares/TokenServiceMiddleware.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using SME.SGP.Aplicacao;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Middlewares
