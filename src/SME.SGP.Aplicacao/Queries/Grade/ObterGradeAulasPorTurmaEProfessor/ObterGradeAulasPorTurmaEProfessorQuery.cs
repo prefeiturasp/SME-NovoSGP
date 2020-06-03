f2933c2ef96828a2fe4c2a918c5126bed70f776d
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-03 17:15:44 -0300

    17610 - Alterado rotina da grade para Query

---
+++ src/SME.SGP.Aplicacao/Queries/Grade/ObterGradeAulasPorTurmaEProfessor/ObterGradeAulasPorTurmaEProfessorQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterGradeAulasPorTurmaEProfessorQuery: IRequest<GradeComponenteTurmaAulasDto>
+    {
+        public ObterGradeAulasPorTurmaEProfessorQuery(string turmaCodigo, long componenteCurricular, DateTime dataAula, string codigoRf = null, bool ehRegencia = false, bool ehExperienciaPegagogica = false)
+        {
+            TurmaCodigo = turmaCodigo;
+            ComponenteCurricular = componenteCurricular;
+            DataAula = dataAula;
+            CodigoRf = codigoRf;
+            EhRegencia = ehRegencia;
+            EhExperienciaPedagogica = ehExperienciaPegagogica;
+        }
+
+        public string TurmaCodigo { get; set; }
+        public long ComponenteCurricular { get; set; }
+        public DateTime DataAula { get; set; }
+        public string CodigoRf { get; set; }
+        public bool EhRegencia { get; set; }
+        public bool EhExperienciaPedagogica { get; set; }
+    }
+}
