c1d8e4ecf77af88395de9b327bca272fbc5086ec
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-04 15:44:40 -0300

    inclusao de teste de integração do versionamento inclusao de teste de integração de ciclos parcial

---
+++ teste/SME.SGP.Integracao.Teste/VersaoTeste.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SME.SGP.Integracao.Teste
+{
+    [Collection("Testserver collection")]
+    public class VersaoTeste
+    {
+        private readonly TestServerFixture fixture;
+
+        public VersaoTeste(TestServerFixture fixture)
+        {
+            this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        }
+
+        [Fact(DisplayName = "Retornar a última versão da release")]
+        [Trait("Versionamento", "Github api")]
+        public async void Versionamento_Retornar_Ultima_Versao()
+        {
+            // Arrange & Act
+            fixture._clientApi.DefaultRequestHeaders.Clear();
+            var result = await fixture._clientApi.GetAsync("api/v1/versoes/");
+            var versao = await result.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.True(result.IsSuccessStatusCode);
+            //Assert.StartsWith("Versão: ", versao);
+        }
+    }
+}
