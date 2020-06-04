d54f9866e268dc68af64974c9020e9a29937962e
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-04 17:55:24 -0300

    Inclusão de classe de teste integrado para Conselho de Classe para testar o endpoint que retorna o detalhamento de notas

---
+++ teste/SME.SGP.Integracao.Teste/ConselhoClasseTeste.cs
@@ -0,0 +1,32 @@
+﻿using SME.SGP.Infra;
+using System;
+using System.Net.Http.Headers;
+using Xunit;
+
+namespace SME.SGP.Integracao.Teste
+{
+    [Collection("Testserver collection")]
+    public class ConselhoClasseTeste
+    {
+        private readonly TestServerFixture fixture;
+        private readonly long _id = 1;
+
+        public ConselhoClasseTeste(TestServerFixture fixture)
+        {
+            this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        }
+
+        [Fact(DisplayName = "Retornar detalhamento de nota")]
+        [Trait("Conselho de Classe", "Detalhamento de Nota")]
+        public async void Retornar_Detalhamento_De_Nota()
+        {
+            // Arrange & Act
+            fixture._clientApi.DefaultRequestHeaders.Clear();
+            fixture._clientApi.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", fixture.GerarToken(new Permissao[] { }));
+            var result = await fixture._clientApi.GetAsync($"api/v1/conselhos-classe/detalhamento/{_id}");
+
+            // Assert
+            Assert.True(result.IsSuccessStatusCode);
+        }
+    }
+}
