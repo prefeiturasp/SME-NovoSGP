373e923d1e490d49d1cef7fc12370efca46b03fd
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-04 16:55:51 -0300

    Use Case Abstract e Interface base

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/AbstractUseCase.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class AbstractUseCase
+    {
+        protected readonly IMediator mediator;
+
+        public AbstractUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+    }
+}
