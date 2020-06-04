7bd814dddc23712e4ce4143dbbca023a247248f4
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-04 15:44:40 -0300

    inclusao de teste de integração do versionamento inclusao de teste de integração de ciclos parcial

---
+++ teste/SME.SGP.Integracao.Teste/CicloTeste.cs
@@ -0,0 +1,38 @@
+﻿using SME.SGP.Infra;
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+using Xunit;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace SME.SGP.Integracao.Teste
+{
+    [Collection("Testserver collection")]
+    public class CicloTeste
+    {
+        private readonly TestServerFixture fixture;
+
+        public CicloTeste(TestServerFixture fixture)
+        {
+            this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        }
+
+        [Fact(DisplayName = "Retornar os ciclos")]
+        [Trait("Ciclo", "Ciclos")]
+        public async void Ciclos_Retornar_Ciclos()
+        {
+            // Arrange            
+            FiltroCicloDto filtroCiclo = new FiltroCicloDto();
+
+            // Act
+            fixture._clientApi.DefaultRequestHeaders.Clear();
+            var jsonParaPost = new StringContent(JsonConvert.SerializeObject(filtroCiclo), Encoding.UTF8, "application/json");
+            fixture._clientApi.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", fixture.GerarToken(new Permissao[] { }));
+            var result = await fixture._clientApi.PostAsync("api/v1/ciclos/", jsonParaPost);
+
+            // Assert
+            Assert.True(true); // TODO: rever chamada
+        }
+    }
+}
