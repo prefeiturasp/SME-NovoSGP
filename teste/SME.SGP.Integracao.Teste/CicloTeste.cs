8f9d6e9a4766daba3498c0778db384df4bbbd225
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-05 15:37:57 -0300

    Corrigido teste de integração que realiza o filtro dos ciclos.

--- teste/SME.SGP.Integracao.Teste/CicloTeste.cs
+++ teste/SME.SGP.Integracao.Teste/CicloTeste.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Xunit;
 using Newtonsoft.Json;
 using System.Net.Http;
+using SME.SGP.Dominio;
+using System.Collections.Generic;
 
 namespace SME.SGP.Integracao.Teste
 {
@@ -23,16 +25,18 @@ namespace SME.SGP.Integracao.Teste
         public async void Ciclos_Retornar_Ciclos()
         {
             // Arrange            
-            FiltroCicloDto filtroCiclo = new FiltroCicloDto();
+            List<string> anos = new List<string>();
+            for (int ano = 1; ano <= 9; ano++) anos.Add(ano.ToString());
+            FiltroCicloDto filtroCiclo = new FiltroCicloDto { Anos = anos, AnoSelecionado = "1", Modalidade = 5 };
 
             // Act
             fixture._clientApi.DefaultRequestHeaders.Clear();
             var jsonParaPost = new StringContent(JsonConvert.SerializeObject(filtroCiclo), Encoding.UTF8, "application/json");
             fixture._clientApi.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", fixture.GerarToken(new Permissao[] { }));
-            var result = await fixture._clientApi.PostAsync("api/v1/ciclos/", jsonParaPost);
+            var result = await fixture._clientApi.PostAsync("api/v1/ciclos/filtro", jsonParaPost);
 
             // Assert
-            Assert.True(true); // TODO: rever chamada
+            Assert.True(fixture.ValidarStatusCodeComSucesso(result));
         }
     }
 }
