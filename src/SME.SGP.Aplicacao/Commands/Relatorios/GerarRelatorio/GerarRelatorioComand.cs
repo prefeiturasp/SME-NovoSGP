fdba0050ee44a0f78a9920e9fe0bb8e31cd23479
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-06-09 12:36:15 -0300

    Adicionado Handler de gerar relatorio

---
+++ src/SME.SGP.Aplicacao/Commands/Relatorios/GerarRelatorio/GerarRelatorioComand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.SGP.Infra.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao.Commands
+{
+    public class GerarRelatorioComand : IRequest<bool>
+    {
+        public GerarRelatorioComand(TipoRelatorio tipoRelatorio, object filtros)
+        {
+            TipoRelatorio = tipoRelatorio;
+            Filtros = filtros;
+        }
+
+        public object Filtros { get; set; }
+        public TipoRelatorio TipoRelatorio { get; set; }
+    }
+}
