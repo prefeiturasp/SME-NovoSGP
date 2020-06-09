7cba620080b2ba3750b5e0f0aa3cbd64ab12a214
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-09 19:39:55 -0300

    Implementação de Relatorio de Correlacao(parcial) Repositório Entidade (Dominio) Comando

---
+++ scripts/V196__RELATORIO_CORRELACAO_CREATE.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.relatorio_correlacao
+(
+	id bigint not null
+	generated always as identity,
+
+);
+
+CREATE TABLE public.relatorio_correlacao_jasper
+(
+	id bigint not null
+	generated always as identity,
+
+);
---
+++ src/SME.SGP.Dados/Repositorios/RepositorioCorrelacaoRelatorio.cs
@@ -0,0 +1,53 @@
+﻿using SME.SGP.Dominio.Entidades;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioRelatorioCorrelacao : IRepositorioRelatorioCorrelacao
+    {
+        private readonly ISgpContext contexto;
+
+        public RepositorioRelatorioCorrelacao(ISgpContext contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public IEnumerable<RelatorioCorrelacao> Listar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RelatorioCorrelacao ObterPorId(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RelatorioCorrelacao> ObterPorIdAsync(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remover(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remover(RelatorioCorrelacao entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public long Salvar(RelatorioCorrelacao entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<long> SalvarAsync(RelatorioCorrelacao entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
