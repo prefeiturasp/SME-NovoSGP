0070f7c742b48da1d345e99f6faad5529d71d617
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-06-09 14:37:39 -0300

    Adicionado fila Rabbit

---
+++ src/SME.SGP.Infra/Dtos/AdicionaFilaDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra.Dtos
+{
+  public  class AdicionaFilaDto
+    {
+        public AdicionaFilaDto(string fila, object filtros, string endpoint)
+        {
+            Fila = fila;
+            Filtros = filtros;
+            Endpoint = endpoint;
+        }
+
+        public string Fila { get; set; }
+
+        public object Filtros { get; set; }
+
+        public string Endpoint{ get; set; }
+    }
+}
---
+++ src/SME.SGP.Infra/Interfaces/IServicoFila.cs
@@ -0,0 +1,13 @@
+﻿using SME.SGP.Infra.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Infra.Interfaces
+{
+   public interface IServicoFila
+    {
+        Task AdicionarFila(AdicionaFilaDto adicionaFilaDto);
+    }
+}
