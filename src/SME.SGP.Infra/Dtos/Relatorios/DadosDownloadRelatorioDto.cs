1f3de0162fc3d2e4ee1510bc83473c72e06d96d6
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-11 20:28:53 -0300

    Endpoint generico para download do relatorio

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/DadosDownloadRelatorioDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Infra
+{
+    public class DadosDownloadRelatorioDto : DadosRelatorioDto
+    {
+        public string ContentType { get; set; }
+        public string NomeArquivo { get; set; }
+    }
+}
