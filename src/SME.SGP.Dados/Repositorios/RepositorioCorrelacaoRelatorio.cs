fccb01acef0ac2ad20e5b7e537867fe720c20adf
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-11 09:06:34 -0300

    Correção do teste e adicionando repositorio de correlação

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioCorrelacaoRelatorio.cs
@@ -0,0 +1,16 @@
+﻿using SME.SGP.Dominio.Entidades;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioCorrelacaoRelatorio : RepositorioBase<RelatorioCorrelacao>, IRepositorioCorrelacaoRelatorio
+    {
+        private readonly ISgpContext contexto;
+
+        public RepositorioCorrelacaoRelatorio(ISgpContext contexto) : base(contexto)
+        {
+            this.contexto = contexto;
+        }
+    }
+}
