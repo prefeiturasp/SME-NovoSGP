685383725d3f8194bd26cdbe214751a92205721c
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-06-11 18:42:44 -0300

    Ajuste boletim usecase

--- src/SME.SGP.Aplicacao/CasosDeUso/Boletim/BoletimUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Boletim/BoletimUseCase.cs
@@ -1,13 +1,9 @@
 ﻿using MediatR;
-using SME.SGP.Aplicacao.Commands.Relatorios.GerarRelatorio;
 using SME.SGP.Aplicacao.Interfaces.CasosDeUso;
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
-using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos.Relatorios;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao.CasosDeUso
@@ -15,16 +11,19 @@ namespace SME.SGP.Aplicacao.CasosDeUso
     public class BoletimUseCase : IBoletimUseCase
     {
         private readonly IMediator mediator;
+        private readonly IUnitOfWork unitOfWork;
         private readonly IRepositorioUe repositorioUe;
         private readonly IRepositorioCicloEnsino repositorioCicloEnsino;
         private readonly IRepositorioTurma repositorioTurma;
 
         public BoletimUseCase(IMediator mediator,
+                              IUnitOfWork unitOfWork,
                               IRepositorioUe repositorioUe,
                               IRepositorioCicloEnsino repositorioCicloEnsino,
                               IRepositorioTurma repositorioTurma)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
             this.repositorioUe = repositorioUe ?? throw new ArgumentNullException(nameof(repositorioUe));
             this.repositorioCicloEnsino = repositorioCicloEnsino ?? throw new ArgumentNullException(nameof(repositorioCicloEnsino));
             this.repositorioTurma = repositorioTurma ?? throw new ArgumentNullException(nameof(repositorioTurma));
@@ -43,7 +42,11 @@ namespace SME.SGP.Aplicacao.CasosDeUso
                 repositorioTurma.ObterPorCodigo(filtroRelatorioBoletimDto.CodigoTurma) == null)
                 throw new NegocioException("Não foi possível encontrar a turma");
 
-            return await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.Boletim, filtroRelatorioBoletimDto));
+            unitOfWork.IniciarTransacao();
+            var usuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+            var retorno = await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.Boletim, filtroRelatorioBoletimDto, usuarioLogado.Id));
+            unitOfWork.PersistirTransacao();
+            return retorno;
         }
     }
 }
