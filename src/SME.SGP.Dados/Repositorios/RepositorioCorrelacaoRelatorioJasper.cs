811623f2ccdefd64a0f9569587a25677fdc4028c
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-10 21:58:01 -0300

    Ajustando script, repositorio e entidades

--- src/SME.SGP.Dados/Repositorios/RepositorioCorrelacaoRelatorioJasper.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioCorrelacaoRelatorioJasper.cs
@@ -1,53 +1,16 @@
 ﻿using SME.SGP.Dominio.Entidades;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
 {
-    public class RepositorioRelatorioCorrelacaoJasper : IRepositorioRelatorioCorrelacaoJasper
+    public class RepositorioRelatorioCorrelacaoJasper : RepositorioBase<RelatorioCorrelacaoJasper>, IRepositorioRelatorioCorrelacaoJasper
     {
         private readonly ISgpContext contexto;
 
-        public RepositorioRelatorioCorrelacaoJasper(ISgpContext contexto)
+        public RepositorioRelatorioCorrelacaoJasper(ISgpContext contexto) : base(contexto)
         {
             this.contexto = contexto;
-        }
-
-        public IEnumerable<RelatorioCorrelacaoJasper> Listar()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public RelatorioCorrelacaoJasper ObterPorId(long id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<RelatorioCorrelacaoJasper> ObterPorIdAsync(long id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Remover(long id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Remover(RelatorioCorrelacaoJasper entidade)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public long Salvar(RelatorioCorrelacaoJasper entidade)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<long> SalvarAsync(RelatorioCorrelacaoJasper entidade)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
