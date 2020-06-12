412132941794bb9fe339b9d4548890ddd5a09c74
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-12 11:27:05 -0300

    17313 - Mock endpoint listagem alunos

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Turma/ObterListaAlunosDaTurmaUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso.Turma
+{
+    public class ObterListaAlunosDaTurmaUseCase : AbstractUseCase, IObterListaAlunosDaTurmaUseCase
+    {
+        public ObterListaAlunosDaTurmaUseCase(IMediator mediator): base(mediator)
+        {
+        }
+
+        public Task<IEnumerable<AlunoSimplesDto>> Executar(FiltroListagemAlunosDto param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/AlunoSimplesDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class AlunoSimplesDto
+    {
+        public int NumeroChamada { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
