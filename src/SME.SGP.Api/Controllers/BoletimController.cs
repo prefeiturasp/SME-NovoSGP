e5b1bcbfb0fb8522870fb58cff8f257e9d5ff5c7
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-12 11:27:05 -0300

    17313 - Mock endpoint listagem alunos

--- src/SME.SGP.Api/Controllers/BoletimController.cs
+++ src/SME.SGP.Api/Controllers/BoletimController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Aplicacao.Interfaces.CasosDeUso;
+using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos.Relatorios;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
@@ -13,6 +17,25 @@ namespace SME.SGP.Api.Controllers
         public async Task<IActionResult> Imprimir([FromQuery] FiltroRelatorioBoletimDto filtroRelatorioBoletimDto, [FromServices] IBoletimUseCase boletimUseCase)
         {
             return Ok(await boletimUseCase.Executar(filtroRelatorioBoletimDto));
+        }
+
+        [HttpGet("alunos")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(IEnumerable<AlunoSimplesDto>), 500)]
+        public async Task<IActionResult> ListarAlunos([FromQuery] FiltroListagemAlunosDto filtroListagemAlunosDto, [FromServices] IObterListaAlunosDaTurmaUseCase obterListaAlunosDaTurmaUseCase)
+        {
+
+            return Ok(new List<AlunoSimplesDto>()
+            {
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Alvaro Ramos Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Aline Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Bianca Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "José Ramos Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Valentina Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Laura Ramos Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Angela Ramos Grassi"},
+                new AlunoSimplesDto() { NumeroChamada = 1, Nome = "Marcos Ramos Grassi"},
+            });
         }
     }
 }
