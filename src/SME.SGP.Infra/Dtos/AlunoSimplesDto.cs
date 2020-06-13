99215fd743cb41a5fc4847e71069628fda9bbb9f
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-13 18:07:34 -0300

    4518 - Incluso Codigo do Aluno na lista da turma

--- src/SME.SGP.Infra/Dtos/AlunoSimplesDto.cs
+++ src/SME.SGP.Infra/Dtos/AlunoSimplesDto.cs
@@ -6,6 +6,8 @@ namespace SME.SGP.Infra
 {
     public class AlunoSimplesDto
     {
+        public string Codigo { get; set; }
+
         public int NumeroChamada { get; set; }
 
         public string Nome { get; set; }
