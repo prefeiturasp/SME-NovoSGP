ab12ab91267dac9fd64daf7fb357bd26490206bd
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-13 17:40:40 -0300

    17344 - Criado endpoint de listagem de pareceres conclusivos

---
+++ src/SME.SGP.Aplicacao/Queries/ParecerConclusivo/ObterPareceresConclusivosPorTurma/ObterPareceresConclusivosPorTurmaQuery.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterPareceresConclusivosPorTurmaQuery: IRequest<IEnumerable<ParecerConclusivoDto>>
+    {
+        public ObterPareceresConclusivosPorTurmaQuery(string turmaCodigo, DateTime dataConsulta)
+        {
+            TurmaCodigo = turmaCodigo;
+            DataConsulta = dataConsulta;
+        }
+
+        public string TurmaCodigo { get; set; }
+        public DateTime DataConsulta { get; set; }
+    }
+
+    public class ObterPareceresConclusivosPorTurmaQueryValidator: AbstractValidator<ObterPareceresConclusivosPorTurmaQuery>
+    {
+        public ObterPareceresConclusivosPorTurmaQueryValidator()
+        {
+            RuleFor(c => c.TurmaCodigo)
+                .NotEmpty()
+                .WithMessage("O código da turma deve ser informado.");
+
+            RuleFor(c => c.DataConsulta)
+                .NotEmpty()
+                .WithMessage("Necessário informar uma data para vigência dos pareceres.");
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/ParecerConclusivo/ObterPareceresConclusivosPorTurma/ObterPareceresConclusivosPorTurmaQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Queries.ParecerConclusivo.ObterPareceresConclusivosPorTurma
+{
+    public class ObterPareceresConclusivosPorTurmaQueryHandler : IRequestHandler<ObterPareceresConclusivosPorTurmaQuery, IEnumerable<ParecerConclusivoDto>>
+    {
+        private readonly IRepositorioConselhoClasseParecerConclusivo repositorioConselhoClasseParecerConclusivo;
+
+        public ObterPareceresConclusivosPorTurmaQueryHandler(IRepositorioConselhoClasseParecerConclusivo repositorioConselhoClasseParecerConclusivo)
+        {
+            this.repositorioConselhoClasseParecerConclusivo = repositorioConselhoClasseParecerConclusivo ?? throw new ArgumentNullException(nameof(repositorioConselhoClasseParecerConclusivo));
+        }
+
+        public async Task<IEnumerable<ParecerConclusivoDto>> Handle(ObterPareceresConclusivosPorTurmaQuery request, CancellationToken cancellationToken)
+            => await repositorioConselhoClasseParecerConclusivo.ObterListaResumidaPorTurmaCodigoAsync(request.TurmaCodigo, request.DataConsulta);
+    }
+}
