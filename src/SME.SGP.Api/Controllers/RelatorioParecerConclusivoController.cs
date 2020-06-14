4ee78fed4ac3215906aabcc5647970d30181d2a5
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-14 17:06:53 -0300

    17346 - EndPoint geração relatorio Parecer Conclusivo

---
+++ src/SME.SGP.Api/Controllers/RelatorioParecerConclusivoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/relatorios/parecer-conclusivo")]
+    [Authorize("Bearer")]
+    public class RelatorioParecerConclusivoController : ControllerBase
+    {
+        [HttpGet("imprimir")]
+        public async Task<IActionResult> Imprimir([FromQuery] FiltroRelatorioParecerConclusivoDto filtroRelatorioParecerConclusivoDto, [FromServices]IImpressaoRelatorioParecerConclusivoUseCase impressaoUseCase)
+        {
+            return Ok(await impressaoUseCase.Executar(filtroRelatorioParecerConclusivoDto));
+        }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/Relatorios/FiltroRelatorioParecerConclusivoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroRelatorioParecerConclusivoDto
+    {
+        public string TurmaCodigo { get; set; }
+
+        public string[] AlunosCodigo { get; set; }
+    }
+}
