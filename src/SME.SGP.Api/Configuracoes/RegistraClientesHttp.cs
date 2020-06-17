b02d02cb83df0e6172991a71690a207ff9a6dc4d
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-06-16 21:29:57 -0300

    alterando algumas variáveis de ambiente

--- src/SME.SGP.Api/Configuracoes/RegistraClientesHttp.cs
+++ src/SME.SGP.Api/Configuracoes/RegistraClientesHttp.cs
@@ -39,12 +39,8 @@ namespace SME.SGP.Api
 
             services.AddSingleton(jasperCookieHandler);
 
-            //var basicAuth = $"{configuration.GetValue<string>("ConfiguracaoJasper:Username")}:{configuration.GetValue<string>("ConfiguracaoJasper:Password")}".EncodeTo64();
-            var basicAuth = $"ebufaino:AMcom20anos".EncodeTo64();
-
+            var basicAuth = $"{configuration.GetValue<string>("ConfiguracaoJasper:Username")}:{configuration.GetValue<string>("ConfiguracaoJasper:Password")}".EncodeTo64();            
             var jasperUrl = configuration.GetValue<string>("ConfiguracaoJasper:Hostname");
-            //var jasperUrl = "http://dev-jasper.sme.prefeitura.sp.gov.br";
-
 
             services.AddHttpClient<ISevicoJasper, SevicoJasper>(c =>
             {
