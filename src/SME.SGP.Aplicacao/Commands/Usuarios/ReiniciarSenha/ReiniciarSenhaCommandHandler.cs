c8a241bcdcb52024d5f2608ecd4b43c1d0ab970d
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-18 19:51:52 -0300

    A funcionalidade de reiniciar senha foi refatorada para atender ao padrão mediatr Foi implementado teste de integração para o endpoint Alteração no DTO de retorno  para apresentar a mensagem

---
+++ src/SME.SGP.Aplicacao/Commands/Usuarios/ReiniciarSenha/ReiniciarSenhaCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ReiniciarSenhaCommandHandler : IRequestHandler<ReiniciarSenhaCommand, UsuarioReinicioSenhaDto>
+    {
+        public readonly IComandosUsuario comandosUsuario;
+        private readonly IServicoEol servicoEOL;
+
+        public ReiniciarSenhaCommandHandler(IServicoFila servicoFila)
+        {
+            this.comandosUsuario = comandosUsuario ?? throw new System.ArgumentNullException(nameof(comandosUsuario));
+            this.servicoEOL = servicoEOL ?? throw new System.ArgumentNullException(nameof(servicoEOL));
+        }
+
+        public async Task<UsuarioReinicioSenhaDto> Handle(ReiniciarSenhaCommand request, CancellationToken cancellationToken)
+        {
+            var usuario = await servicoEOL.ObterMeusDados(request.CodigoRf);
+
+            var retorno = new UsuarioReinicioSenhaDto();
+
+            if (String.IsNullOrEmpty(usuario.Email))
+                retorno.DeveAtualizarEmail = true;
+            else
+            {
+                await servicoEOL.ReiniciarSenha(request.CodigoRf);
+                retorno.Mensagem = $"Senha do usuário {request.CodigoRf} - {usuario.Nome} reiniciada com sucesso.";
+                retorno.DeveAtualizarEmail = false;
+            }
+
+            return retorno;
+        }
+    }
+}
