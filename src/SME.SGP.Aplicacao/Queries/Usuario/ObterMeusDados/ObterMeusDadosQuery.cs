96e678763b747d7d2e513a24e0cb68a103b9e0d4
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-06-20 16:29:06 -0300

    - teste de intergação foi comentado para definirmos um usuário de teste - ajustes na mensagem do command reiniciar senha - foi retirado injeção de comando usuário que não estava sendo utilizada - implementação de Query para obter dados do EOL parcial (a validar)

---
+++ src/SME.SGP.Aplicacao/Queries/Usuario/ObterMeusDados/ObterMeusDadosQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterMeusDadosQuery : IRequest<MeusDadosDto>
+    {
+        public string Login { get; set; }
+
+        public ObterMeusDadosQuery(string login)
+        {
+            Login = login;
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/Usuario/ObterMeusDados/ObterMeusDadosQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterMeusDadosQueryValidator : AbstractValidator<ObterMeusDadosQuery>
+    {
+        public ObterMeusDadosQueryValidator()
+        {
+            RuleFor(c => c.Login)
+                .NotEmpty()
+                .WithMessage("Login é obrigatório.");
+        }
+    }
+}
