8c8c0ce32dd6138b4fe9e5773cd273b26971777a
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-23 18:08:33 -0300

    Ajustes de codesmell

--- src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/ConselhoClasse/RelatorioConselhoClasseAtaFinalUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/ConselhoClasse/RelatorioConselhoClasseAtaFinalUseCase.cs
@@ -1,8 +1,6 @@
 ﻿using MediatR;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
-using SME.SGP.Infra.Dtos;
-using SME.SGP.Infra.Interfaces;
 using System;
 using System.Threading.Tasks;
 
@@ -11,14 +9,12 @@ namespace SME.SGP.Aplicacao
     public class RelatorioConselhoClasseAtaFinalUseCase : IRelatorioConselhoClasseAtaFinalUseCase
     {
         private readonly IMediator mediator;
-        private readonly IServicoFila servicoFila;
 
-        public RelatorioConselhoClasseAtaFinalUseCase(IMediator mediator, IServicoFila servicoFila)
+        public RelatorioConselhoClasseAtaFinalUseCase(IMediator mediator)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            this.servicoFila = servicoFila;
         }
-        public async Task<bool> Executar(FiltroRelatorioConselhoClasseAtaFinalDto filtroRelatorioConselhoClasseDto)
+        public async Task<bool> Executar(FiltroRelatorioConselhoClasseAtaFinalDto filtroRelatorioConselhoClasseAtaFinalDto)
         {
             var usuarioId = await mediator.Send(new ObterUsuarioLogadoIdQuery());
 
@@ -26,7 +22,7 @@ namespace SME.SGP.Aplicacao
                 throw new NegocioException("Não foi possível localizar o usuário.");
 
 
-            return await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.ConselhoClasseAtaFinal, filtroRelatorioConselhoClasseDto, usuarioId));
+            return await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.ConselhoClasseAtaFinal, filtroRelatorioConselhoClasseAtaFinalDto, usuarioId));
         }
     }
 }
