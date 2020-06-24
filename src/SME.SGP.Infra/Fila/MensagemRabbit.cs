3efbd73eba1c5e3c8ecbcc9fd3506ae879ec68f7
Rafael Losi <rafael.losi@amcom.com.br>
2020-06-24 17:12:50 -0300

    18698 - Notificar Usuario ao ocorrer erro na execução do worker

--- src/SME.SGP.Infra/Fila/MensagemRabbit.cs
+++ src/SME.SGP.Infra/Fila/MensagemRabbit.cs
@@ -5,11 +5,12 @@ namespace SME.SGP.Infra
 {
     public class MensagemRabbit
     {
-        public MensagemRabbit(string action, object filtros, Guid codigoCorrelacao)
+        public MensagemRabbit(string action, object filtros, Guid codigoCorrelacao, bool notificarErroUsuario = false)
         {
             Action = action;
             Filtros = filtros;
             CodigoCorrelacao = codigoCorrelacao;
+            NotificarErroUsuario = notificarErroUsuario;
         }
 
         public string Action { get; set; }
@@ -17,6 +18,7 @@ namespace SME.SGP.Infra
         public Guid CodigoCorrelacao { get; set; }
         public string UsuarioLogadoNomeCompleto { get; set; }
         public string UsuarioLogadoRF { get; set; }
+        public bool NotificarErroUsuario { get; set; }
         public T ObterObjetoFiltro<T>() where T : class
         {
             return JsonConvert.DeserializeObject<T>(Filtros.ToString());
