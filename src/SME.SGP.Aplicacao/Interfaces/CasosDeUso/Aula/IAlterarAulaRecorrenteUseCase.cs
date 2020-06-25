493bbecadea3f411bebbd0554fd3b7332148af88
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-06-25 16:34:56 -0300

    Removendo metodo herdado

--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/Aula/IAlterarAulaRecorrenteUseCase.cs
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/Aula/IAlterarAulaRecorrenteUseCase.cs
@@ -1,10 +1,8 @@
 ﻿using SME.SGP.Infra;
-using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
     public interface IAlterarAulaRecorrenteUseCase : IUseCase<MensagemRabbit, bool>
     {
-        Task<bool> Executar(MensagemRabbit mensagemRabbit);
     }
 }
