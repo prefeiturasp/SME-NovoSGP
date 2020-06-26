01e2b82dff6b927b3b45fae013660e5867d2e453
Robson <robson.silva@amcom.com.br>
2020-06-26 09:30:48 -0300

    AB#18597 Reegatado um ajuste para criação do usuário ao utilizar o recurso de recuperar senha. Fizamos um ajuste pontual referente a obtenção do usuário por código rf ou login.

--- src/SME.SGP.Aplicacao/Comandos/ComandosUsuario.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosUsuario.cs
@@ -279,11 +279,13 @@ namespace SME.SGP.Aplicacao
         public async Task<string> SolicitarRecuperacaoSenha(string login)
         {
             var usuario = repositorioUsuario.ObterPorCodigoRfLogin(null, login);
+            var usuarioCore = await servicoEOL.ObterMeusDados(login);
+
+            if (usuarioCore == null)
+                throw new NegocioException("Usuário não encontrado.");
 
             if (usuario == null)
-            {
-                throw new NegocioException("Usuário não encontrado.");
-            }
+                usuario = servicoUsuario.ObterUsuarioPorCodigoRfLoginOuAdiciona(usuarioCore.CodigoRf, login, usuarioCore.Nome, usuarioCore.Email);
 
             if (usuario.Perfis == null || !usuario.Perfis.Any())
             {
