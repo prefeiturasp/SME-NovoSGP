e3ade63ea6422445840edabc24583a8294462b97
Cleiton <cleiton.dutra@amcom.com.br>
2020-06-29 17:25:04 -0300

    ajustes bugs sonar - front

--- src/SME.SGP.WebClient/src/componentes-sgp/filtro/helper.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/filtro/helper.js
@@ -21,8 +21,9 @@ class FiltroHelper {
   obterModalidades = async ({ consideraHistorico, anoLetivoSelecionado }) => {
     const modalidadesLista = [];
 
-    if (!anoLetivoSelecionado || typeof anoLetivoSelecionado === 'undefined')
+    if (!anoLetivoSelecionado) {
       return null;
+    }
 
     return ServicoFiltro.listarModalidades({
       consideraHistorico,
