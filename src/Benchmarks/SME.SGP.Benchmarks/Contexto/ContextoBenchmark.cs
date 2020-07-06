30ff51284c2b85c9efb1d37e06dcfc511fba990c
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-07-06 12:47:10 -0300

    Interceptor dapper para log das queries no insights Benchmark do mesmo

---
+++ src/Benchmarks/SME.SGP.Benchmarks/Contexto/ContextoBenchmark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using SME.SGP.Infra.Contexto;
+using SME.SGP.Infra.Interfaces;
+
+namespace SME.SGP.Benchmarks
+{
+    public class ContextoBenchmark : ContextoBase
+    {
+        public override void AdicionarVariaveis(IDictionary<string, object> variaveis)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IContextoAplicacao AtribuirContexto(IContextoAplicacao contexto)
+        {
+            throw new Exception("Este tipo de conexto não permite atribuição");
+        }
+    }
+}
