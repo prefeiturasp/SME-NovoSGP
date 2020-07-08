e4e7911dde5cc1b7b3e7e074fa96faaa59804d54
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-07-08 10:57:59 -0300

    endpoint do relatório de faltas e frequencia

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/FaltasFrequencia/GerarRelatorioFaltasFrequenciaUseCase.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class GerarRelatorioFaltasFrequenciaUseCase : IGerarRelatorioFaltasFrequenciaUseCase
+    {
+        private readonly IMediator mediator;
+
+        public GerarRelatorioFaltasFrequenciaUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+        }
+        public async Task<bool> Executar(FiltroRelatorioFaltasFrequenciaDto filtro)
+        {
+            var usuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+            await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.FaltasFrequencia, filtro, usuarioLogado));
+            return await Task.FromResult(true);
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Enumerados/CondicoesRelatorioFaltasFrequencia.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio
+{
+    public enum CondicoesRelatorioFaltasFrequencia
+    {
+        Igual = 1,
+        Maior = 2,
+        Menor = 3
+    }
+}
