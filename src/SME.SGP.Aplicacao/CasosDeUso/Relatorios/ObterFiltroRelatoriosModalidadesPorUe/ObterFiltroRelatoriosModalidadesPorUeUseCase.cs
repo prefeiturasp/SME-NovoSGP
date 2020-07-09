b906abd389f5a6fd9757545d30d6c8ce822c59b3
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-07-09 15:05:29 -0300

    iniciando endpoint de modalidades por ue

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/ObterFiltroRelatoriosModalidadesPorUe/ObterFiltroRelatoriosModalidadesPorUeUseCase.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Dto;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterFiltroRelatoriosModalidadesPorUeUseCase
+    {
+        private readonly IMediator mediator;
+        private readonly IRepositorioAbrangencia repositorioAbrangencia;
+
+        public ObterFiltroRelatoriosModalidadesPorUeUseCase(IMediator mediator,
+                                              IRepositorioAbrangencia repositorioAbrangencia)
+        {
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+            this.repositorioAbrangencia = repositorioAbrangencia ?? throw new System.ArgumentNullException(nameof(repositorioAbrangencia));
+        }
+        public async Task<IEnumerable<Modalidade>> Executar(string codigoUe)
+        {
+            return await repositorioAbrangencia.ObterModalidadesPorUe(codigoUe);
+        }
+    }
+}
