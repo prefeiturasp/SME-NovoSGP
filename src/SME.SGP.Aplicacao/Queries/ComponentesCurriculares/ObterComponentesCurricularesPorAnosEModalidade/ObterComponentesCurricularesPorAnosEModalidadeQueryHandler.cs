be7c752900c53123ae5f65d2a77928361dda316f
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-07-12 12:31:05 -0300

    Endpoint de componentes curriculares por anos e modalidade

---
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorAnosEModalidade/ObterComponentesCurricularesPorAnosEModalidadeQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Dominio;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Queries.ComponentesCurriculares.ObterComponentesCurricularesPorAnosEModalidade
+{
+    public class ObterComponentesCurricularesPorAnosEModalidadeQueryHandler : IRequestHandler<ObterComponentesCurricularesPorAnosEModalidadeQuery, IEnumerable<ComponenteCurricularEol>>
+    {
+        private readonly IServicoEol servicoEol;
+
+        public ObterComponentesCurricularesPorAnosEModalidadeQueryHandler(IServicoEol servicoEol)
+        {
+            this.servicoEol = servicoEol ?? throw new System.ArgumentNullException(nameof(servicoEol));
+        }
+
+        public async Task<IEnumerable<ComponenteCurricularEol>> Handle(ObterComponentesCurricularesPorAnosEModalidadeQuery request, CancellationToken cancellationToken)
+        {
+            return await servicoEol.ObterComponentesCurricularesPorAnosEModalidade(request.Modalidade, request.AnosEscolares, request.AnoLetivo);
+        }
+    }
+}
