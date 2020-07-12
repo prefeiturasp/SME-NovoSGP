3d77fc9e2586749985435d2791c6feaaf7ad0ee5
Cleiton <cleiton.dutra@amcom.com.br>
2020-07-12 18:41:14 -0300

    Relatório de faltas frequencia - endpoint tela de filtro

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/FaltasFrequencia/ServicoFaltasFrequencia.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/FaltasFrequencia/ServicoFaltasFrequencia.js
@@ -1,13 +1,8 @@
-// import api from '~/servicos/api';
+import api from '~/servicos/api';
 
 class ServicoFaltasFrequencia {
   gerar = dados => {
-    console.log(dados);
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ data: { sucesso: true }, status: 200 });
-      }, 3000);
-    });
+    return api.post(`v1/relatorios/faltas-frequencia`, dados);
   };
 }
 
