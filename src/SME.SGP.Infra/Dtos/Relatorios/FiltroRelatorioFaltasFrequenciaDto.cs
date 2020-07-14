0df8530413e2dfe35669447b9435bac2c5929ee0
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-07-14 19:19:37 -0300

    Ajustes no filtro backend

--- src/SME.SGP.Infra/Dtos/Relatorios/FiltroRelatorioFaltasFrequenciaDto.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/FiltroRelatorioFaltasFrequenciaDto.cs
@@ -13,11 +13,13 @@ namespace SME.SGP.Infra
         public int? Semestre { get; set; }
         public IEnumerable<string> AnosEscolares { get; set; }
         public IEnumerable<long> ComponentesCurriculares { get; set; }
-        public IEnumerable<int> Bimestres { get; set; }
+        public List<int> Bimestres { get; set; }
         public TipoRelatorioFaltasFrequencia TipoRelatorio { get; set; }
         public CondicoesRelatorioFaltasFrequencia Condicao { get; set; }
         public int ValorCondicao { get; set; }
         public TipoFormatoRelatorio TipoFormatoRelatorio { get; set; }
+        public string NomeUsuario { get; set; }
+        public string CodigoRf { get; set; }
     }
 
 
@@ -25,32 +27,36 @@ namespace SME.SGP.Infra
     {
         public FiltroRelatorioFaltasFrequenciaDtoValidator()
         {
-
             RuleFor(c => c.TipoRelatorio)
-            .IsInEnum()
-            .WithMessage("O tipo de relatório de faltas ou frequência deve ser informado.");
+                .IsInEnum()
+                .WithMessage("O tipo de relatório de faltas ou frequência deve ser informado.");
 
             RuleFor(c => c.Modalidade)
-            .IsInEnum()
-            .WithMessage("A modalidade deve ser informada.");
+                .IsInEnum()
+                .WithMessage("A modalidade deve ser informada.");
 
             RuleFor(c => c.Semestre)
-            .NotEmpty()
-            .WithMessage("Quando a modalidade é EJA o Semestre deve ser informado.")
-            .When(c => c.Modalidade == Modalidade.EJA);
+                .NotEmpty()
+                .WithMessage("Quando a modalidade é EJA o Semestre deve ser informado.")
+                .When(c => c.Modalidade == Modalidade.EJA);
+
+            //RuleFor(c => c.Bimestres)
+            //    .NotEmpty()
+            //    .WithMessage("Os bimestres devem ser informados.")
+            //    .When(c => c.Modalidade != Modalidade.EJA);
 
 
             RuleFor(c => c.Condicao)
-            .IsInEnum()
-            .WithMessage("A condição deve ser informada.");
+                .IsInEnum()
+                .WithMessage("A condição deve ser informada.");
 
             RuleFor(c => c.ValorCondicao)
-            .NotEmpty()
-            .WithMessage("O valor para a condição deve ser informado.");
+                .NotEmpty()
+                .WithMessage("O valor para a condição deve ser informado.");
 
             RuleFor(c => c.TipoFormatoRelatorio)
-           .NotEmpty()
-           .WithMessage("O formato deve ser informado.");
+                .NotEmpty()
+                .WithMessage("O formato deve ser informado.");
         }
     }
 
