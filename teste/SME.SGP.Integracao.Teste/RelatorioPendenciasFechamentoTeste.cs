313dfc45b5bf9a68dc2fc94d0b50715743ffe1d1
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-15 01:04:10 -0300

    Implementação para controller de geração de relatório de pendências do fechamento ajuste no docker-compose-database

---
+++ teste/SME.SGP.Integracao.Teste/RelatorioPendenciasFechamentoTeste.cs
@@ -0,0 +1,31 @@
+﻿using SME.SGP.Infra;
+using System;
+using System.Net.Http.Headers;
+using Xunit;
+
+namespace SME.SGP.Integracao.Teste
+{
+    [Collection("Testserver collection")]
+    public class RelatorioPendenciasFechamentoTeste
+    {
+        private readonly TestServerFixture fixture;
+
+        public RelatorioPendenciasFechamentoTeste(TestServerFixture fixture)
+        {
+            this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        }
+
+        [Fact(DisplayName = "Relatório de Pendências no Fechamento")]
+        [Trait("Pendências no Fechamento", "gerar")]
+        public async void Deve_Realizar_Chamada_Para_Gerar_Relatorio()
+        {
+            // Arrange & Act
+            fixture._clientApi.DefaultRequestHeaders.Clear();
+            fixture._clientApi.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", fixture.GerarToken(new Permissao[] { }));
+            var result = await fixture._clientApi.GetAsync($"api/v1/relatorio/pendencias-fechamento/gerar");
+
+            // Assert
+            Assert.True(fixture.ValidarStatusCodeComSucesso(result));
+        }
+    }
+}
