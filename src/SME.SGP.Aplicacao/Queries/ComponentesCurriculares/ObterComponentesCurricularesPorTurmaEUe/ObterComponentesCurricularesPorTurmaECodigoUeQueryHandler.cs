bcd7ba9c7b2a5bd0c505afdb5274695bf4b53ed4
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-17 15:11:36 -0300

    Implementação parcial de endpoint para obter componentes curriculares por turma e código da UE

---
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorTurmaEUe/ObterComponentesCurricularesPorTurmaECodigoUeQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Integracoes;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterComponentesCurricularesPorTurmaECodigoUeQueryHandler : IRequestHandler<ObterComponentesCurricularesPorTurmaECodigoUeQuery, IEnumerable<ComponenteCurricularDto>>
+    {
+        private readonly IServicoEol servicoEOL;
+        public ObterComponentesCurricularesPorTurmaECodigoUeQueryHandler(IServicoEol servicoEOL)
+        {
+            this.servicoEOL = servicoEOL ?? throw new ArgumentNullException(nameof(servicoEOL));
+        }
+
+        public async Task<IEnumerable<ComponenteCurricularDto>> Handle(ObterComponentesCurricularesPorTurmaECodigoUeQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
