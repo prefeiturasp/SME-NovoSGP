7ac481ee924762868bd33814ee2ccf7c3c726f06
Rafael Losi <rafael.losi@amcom.com.br>
2020-07-17 18:09:14 -0300

    19926 - Incluso mensagem ao usuario na notificação de relatório gerado

---
+++ src/SME.SGP.Infra/Fila/MensagemRelatorioProntoDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class MensagemRelatorioProntoDto
+    {
+        public string MensagemUsuario { get; set; }
+    }
+}
