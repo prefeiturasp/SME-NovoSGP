8f365a18ce074f7c436b70f1f1f45a684299b69c
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-07-17 15:11:36 -0300

    Implementação parcial de endpoint para obter componentes curriculares por turma e código da UE

---
+++ src/SME.SGP.Api/Controllers/ComponentesCurricularesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos.Relatorios;
+using System;
+using System.Threading.Tasks;
+using SME.SGP.Aplicacao;
+
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/componentes-curriculares")]
+    public class ComponentesCurricularesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("obter")]
+        [ProducesResponseType(typeof(Boolean), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public async Task<IActionResult> Obter(FiltroComponentesCurricularesPorTurmaECodigoUeDto filtroComponentesCurricularesPorTurmaECodigoUeDto, [FromServices] IObterComponentesCurricularesPorTurmaECodigoUeUseCase obterComponentesCurricularesPorTurmaECodigoUeUseCase)
+        {
+            return Ok(await obterComponentesCurricularesPorTurmaECodigoUeUseCase.Executar(filtroComponentesCurricularesPorTurmaECodigoUeDto));
+        }
+
+
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/Relatorios/FiltroComponentesCurricularesPorTurmaECodigoUeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Infra.Dtos.Relatorios
+{
+    public class FiltroComponentesCurricularesPorTurmaECodigoUeDto
+    {
+        public string[] CodigoTurma { get; set; }
+        public string CodigoUe { get; set; }
+    }
+}
