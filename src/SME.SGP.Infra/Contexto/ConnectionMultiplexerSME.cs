6e4354e1540fd4c8f9061a3e32c767be9752c092
Robson <robson.silva@amcom.com.br>
2020-07-21 09:18:52 -0300

    AB#19488 Ajustes no modo de conexão do Redis utilizando o multiplex. Ajustes no repositório de cache. Ajustes nas configurações dos testes de integração. Habilitando novamente os testes no build.

--- src/SME.SGP.Infra/Contexto/ConnectionMultiplexerSME.cs
+++ src/SME.SGP.Infra/Contexto/ConnectionMultiplexerSME.cs
@@ -1,25 +1,22 @@
 ﻿using SME.SGP.Infra.Interfaces;
 using StackExchange.Redis;
 using System;
-using System.Diagnostics;
 
 namespace SME.SGP.Infra.Contexto
 {
     public class ConnectionMultiplexerSME : IConnectionMultiplexerSME
     {
-        private readonly string host;
         private readonly IConnectionMultiplexer connectionMultiplexer;
 
         public ConnectionMultiplexerSME(string host, IServicoLog servicoLog)
         {
             try
             {
-                this.host = host;
                 this.connectionMultiplexer = ConnectionMultiplexer.Connect(host);
             }
             catch (RedisConnectionException rcex)
             {
-                servicoLog.Registrar(rcex);
+                servicoLog.Registrar($"Erro de conexão com o servidor Redis. {rcex}");
             }
             catch (Exception ex)
             {
