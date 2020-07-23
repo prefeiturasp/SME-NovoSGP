2290782cee5f9610507a06c4c2352e8ff361db14
eliton-amcom <eliton.santos@amcom.com.br>
2020-07-22 23:48:23 -0300

    ajustes consumo de endpoints componentes

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/Pendencias/ServicoRelatorioPendencias.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/Pendencias/ServicoRelatorioPendencias.js
@@ -2,7 +2,7 @@ import api from '~/servicos/api';
 
 class ServicoRelatorioPendencias {
   gerar = async params => {
-    const url = '/v1/relatorio/pendencias-fechamento/gerar';
+    const url = '/v1/relatorios/fechamentos/pendencias';
     return api.post(url, params);
   };
 }
