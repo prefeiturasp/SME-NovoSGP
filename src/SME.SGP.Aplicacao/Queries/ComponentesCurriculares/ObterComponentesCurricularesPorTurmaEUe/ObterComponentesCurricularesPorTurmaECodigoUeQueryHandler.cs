fd183c535944e3f0bf5cd1b9b460b3377ed12724
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-07-23 00:31:24 -0300

    Ajuste de using

--- src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorTurmaEUe/ObterComponentesCurricularesPorTurmaECodigoUeQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorTurmaEUe/ObterComponentesCurricularesPorTurmaECodigoUeQueryHandler.cs
@@ -1,13 +1,13 @@
 ﻿using MediatR;
+using Newtonsoft.Json;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using SME.SGP.Infra;
-using System.Net.Http;
-using Newtonsoft.Json;
-using SME.SGP.Dominio;
 
 namespace SME.SGP.Aplicacao
 {
@@ -23,16 +23,18 @@ namespace SME.SGP.Aplicacao
         public async Task<IEnumerable<ComponenteCurricularDto>> Handle(ObterComponentesCurricularesPorTurmaECodigoUeQuery request, CancellationToken cancellationToken)
         {
             using var httpClient = httpClientFactory.CreateClient("servicoEOL");
-            var turmas = String.Join("&turmas=", request.CodigosDeTurmas);
-
-            var resposta = await httpClient.GetAsync($"/api/v1/componentes-curriculares/ues/{request.CodigoUe}/turmas?turmas={turmas}");
-
-            if (resposta.IsSuccessStatusCode && resposta.StatusCode != HttpStatusCode.NoContent)
             {
-                var json = await resposta.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<IEnumerable<ComponenteCurricularDto>>(json);
+                var turmas = String.Join("&turmas=", request.CodigosDeTurmas);
+
+                var resposta = await httpClient.GetAsync($"/api/v1/componentes-curriculares/ues/{request.CodigoUe}/turmas?turmas={turmas}");
+
+                if (resposta.IsSuccessStatusCode && resposta.StatusCode != HttpStatusCode.NoContent)
+                {
+                    var json = await resposta.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<IEnumerable<ComponenteCurricularDto>>(json);
+                }
+                else throw new NegocioException("Não foi possível obter Componentes Curriculares.");
             }
-            else throw new NegocioException("Não foi possível obter Componentes Curriculares.");
         }
     }
 }
