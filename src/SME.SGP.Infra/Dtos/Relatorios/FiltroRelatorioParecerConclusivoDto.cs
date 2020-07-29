8e1a00bb6fa5a1fa17d506098fc71e2e29809ac0
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-07-29 14:17:44 -0300

    merge cherry-pick

--- src/SME.SGP.Infra/Dtos/Relatorios/FiltroRelatorioParecerConclusivoDto.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/FiltroRelatorioParecerConclusivoDto.cs
@@ -7,10 +7,11 @@ namespace SME.SGP.Infra.Dtos.Relatorios
         public int AnoLetivo { get; set; }
         public string DreCodigo { get; set; }
         public string UeCodigo { get; set; }
-        public Modalidade Modalidade { get; set; }
+        public Modalidade? Modalidade { get; set; }
         public int? Semestre { get; set; }
-        public string Ciclo { get; set; }
+        public long CicloId { get; set; }
         public string AnoEscolar { get; set; }
         public long ParecerConclusivoId { get; set; }
+        public string[] Anos { get; set; }        
     }
 }
