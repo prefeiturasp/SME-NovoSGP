d8759bc83b041d18ddb389c8c7f803321b1e9a5e
Daniel Gomes <daniel.gomes@amcom.com.br>
2020-08-05 17:12:08 -0300

    Corrigindo merge

--- src/SME.SGP.Api/Controllers/DiarioBordoController.cs
+++ src/SME.SGP.Api/Controllers/DiarioBordoController.cs
@@ -3,13 +3,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
+using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos;
 using System.Threading.Tasks;
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using SME.SGP.Aplicacao.Interfaces;
 
 namespace SME.SGP.Api.Controllers
 {
@@ -27,12 +24,12 @@ namespace SME.SGP.Api.Controllers
         {
             return Ok(await ObterDiarioBordoPorAulaIdUseCase.Executar(mediator, aulaId));
         }
-        
+
         [HttpPost]
         [ProducesResponseType(typeof(AuditoriaDto), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        //[Permissao(Permissao., Policy = "Bearer")]
-        public async Task<IActionResult> Salvar([FromServices]IInserirDiarioBordoUseCase useCase, [FromBody]InserirDiarioBordoDto diarioBordoDto)
+        [Permissao(Permissao.DDB_I, Policy = "Bearer")]
+        public async Task<IActionResult> Salvar([FromServices] IInserirDiarioBordoUseCase useCase, [FromBody] InserirDiarioBordoDto diarioBordoDto)
         {
             return Ok(await useCase.Executar(diarioBordoDto));
         }
--- teste/SME.SGP.Integracao.Teste/DiarioBordoTeste.cs
+++ teste/SME.SGP.Integracao.Teste/DiarioBordoTeste.cs
@@ -1,12 +1,10 @@
-﻿using SME.SGP.Infra;
+﻿using Newtonsoft.Json;
+using SME.SGP.Infra;
 using System;
-using System.Collections.Generic;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using Xunit;
-﻿using Newtonsoft.Json
-using System.Net.Http;
-using System.Text;
 using Xunit.Extensions.Ordering;
 
 namespace SME.SGP.Integracao.Teste
@@ -20,7 +18,7 @@ namespace SME.SGP.Integracao.Teste
         {
             this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
         }
-      
+
         [Fact(DisplayName = "Obter Diário de Bordo")]
         [Trait("DiarioBordo", "Obter Diário de Bordo")]
         public async void Obter()
@@ -30,11 +28,12 @@ namespace SME.SGP.Integracao.Teste
             fixture._clientApi.DefaultRequestHeaders.Clear();
             fixture._clientApi.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", fixture.GerarToken(new Permissao[] { Permissao.DDB_C }));
 
-            var id = "31886";
-            var result = await fixture._clientApi.GetAsync($"api/v1/diario-bordo/{id}");
+            string id = "31886";
+            HttpResponseMessage result = await fixture._clientApi.GetAsync($"api/v1/diario-bordo/{id}");
 
             // Assert
             Assert.True(fixture.ValidarStatusCodeComSucesso(result));
+        }
 
         [Fact, Order(1)]
         public void Deve_Inserir_Diario_De_Bordo()
@@ -44,12 +43,12 @@ namespace SME.SGP.Integracao.Teste
             fixture._clientApi.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", fixture.GerarToken(new Permissao[] { Permissao.ACJ_A })); //TODO Colocar o permissionamento correto quando for criado
 
-            var diarioBordoDto = new InserirDiarioBordoDto();
+            InserirDiarioBordoDto diarioBordoDto = new InserirDiarioBordoDto();
 
-            var jsonParaPost = new StringContent(TransformarEmJson(diarioBordoDto), UnicodeEncoding.UTF8, "application/json");
+            StringContent jsonParaPost = new StringContent(TransformarEmJson(diarioBordoDto), UnicodeEncoding.UTF8, "application/json");
 
-            var postResult = fixture._clientApi.PostAsync("api/v1/diario-bordo/", jsonParaPost).Result;
-            var result = postResult.Content.ReadAsStringAsync().Result;
+            HttpResponseMessage postResult = fixture._clientApi.PostAsync("api/v1/diario-bordo/", jsonParaPost).Result;
+            string result = postResult.Content.ReadAsStringAsync().Result;
 
             Assert.True(postResult.IsSuccessStatusCode, result);
         }
