dc90c41b68d8c73e46f775fe955376d86f8c7cf8
Daniel Gomes <daniel.gomes@amcom.com.br>
2020-08-05 09:28:42 -0300

    Camadas iniciadas

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/DiarioBordo/ObterDiarioBordoPorIdUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Queries.DiarioBordo.ObterDiarioBordoPorId;
+using SME.SGP.Dominio;
+using SME.SGP.Infra.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso.DiarioBordo
+{
+    public static class ObterDiarioBordoPorIdUseCase
+    {
+        public static async Task<DiarioBordoDto> Executar(IMediator mediator, long diarioBordoId)
+        {
+            var diarioBordo = await mediator.Send(new ObterDiarioBordoPorIdQuery(diarioBordoId));
+            if (diarioBordo == null || diarioBordo.Excluido)
+                throw new NegocioException($"Diário de Bordo de id {diarioBordoId} não encontrado");
+
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/DiarioBordo/ObterDiarioBordoPorId/ObterDiarioBordoPorIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao.Queries.DiarioBordo.ObterDiarioBordoPorId
+{
+    public class ObterDiarioBordoPorIdQuery : IRequest<DiarioBordo>
+    {
+
+        public ObterDiarioBordoPorIdQuery(long diarioBordoId)
+        {
+            DiarioBordoId = diarioBordoId;
+        }
+
+        public long DiarioBordoId { get; set; }
+    }
+}
