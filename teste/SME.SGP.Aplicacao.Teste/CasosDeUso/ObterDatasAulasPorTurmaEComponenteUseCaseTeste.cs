fb0b1c81264bcfd6ff6caf472c6d8b08fa6eff7a
Rafael Losi <rafael.losi@amcom.com.br>
2020-08-06 09:50:33 -0300

    19442 -

---
+++ teste/SME.SGP.Aplicacao.Teste/CasosDeUso/ObterDatasAulasPorTurmaEComponenteUseCaseTeste.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Moq;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.Aplicacao.Teste
+{
+    public class ObterDatasAulasPorTurmaEComponenteUseCaseTeste
+    {
+        private readonly ObterDatasAulasPorTurmaEComponenteUseCase useCase;
+        private readonly Mock<IMediator> mediator;
+        private readonly Mock<IServicoUsuario> servicoUsuario;
+
+        public ObterDatasAulasPorTurmaEComponenteUseCaseTeste()
+        {
+            mediator = new Mock<IMediator>();
+            servicoUsuario = new Mock<IServicoUsuario>();
+            useCase = new ObterDatasAulasPorTurmaEComponenteUseCase(mediator.Object, servicoUsuario.Object);
+        }
+
+        [Fact]
+        public async Task Deve_Consultar_Datas_Aulas_Por_Turma_E_Componente()
+        {
+            // Arrange
+            servicoUsuario.Setup(a => a.ObterUsuarioLogado())
+                .ReturnsAsync(new Dominio.Usuario());
+
+            mediator.Setup(a => a.Send(It.IsAny<ObterDatasAulasPorProfessorEComponenteQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<DatasAulasDto>());
+
+            // Act
+            var datasAulas = await useCase.Executar(new ConsultaDatasAulasDto("123", "1105"));
+
+            // Assert
+            Assert.NotNull(datasAulas);
+        }
+    }
+}
