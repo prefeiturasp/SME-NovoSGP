36612be2b226c6db435c7cdd31b8749cb28df871
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-08-05 23:01:09 -0300

    Criado metodo para salvar as aulas, necessario realizar testes com bulk insert

---
+++ src/SME.SGP.Aplicacao/Queries/DiasLetivos/ObterDiasLetivosPorPeriodosEscolares/ObterDiasLetivosPorPeriodosEscolaresQuery.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterDiasLetivosPorPeriodosEscolaresQuery : IRequest<List<DiaLetivoDto>>
+    {
+        public ObterDiasLetivosPorPeriodosEscolaresQuery(IEnumerable<Dominio.PeriodoEscolar> periodosEscolares, long tipoCalendarioId)
+        {
+            PeriodosEscolares = periodosEscolares;
+            TipoCalendarioId = tipoCalendarioId;
+        }
+
+        public ObterDiasLetivosPorPeriodosEscolaresQuery(Dominio.PeriodoEscolar periodoEscolar, long tipoCalendarioId)
+        {
+            PeriodosEscolares = new List<Dominio.PeriodoEscolar> { periodoEscolar };
+            TipoCalendarioId = tipoCalendarioId;
+        }
+
+        public long TipoCalendarioId { get; set; }
+        public IEnumerable<Dominio.PeriodoEscolar> PeriodosEscolares { get; set; }
+    }
+}
