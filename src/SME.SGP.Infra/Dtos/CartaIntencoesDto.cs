1d16196ebce7b1c80c5fdd71f870a4d1fea73a05
Daniel Gomes <daniel.gomes@amcom.com.br>
2020-08-07 17:27:56 -0300

    AB#21042 - Modelagem de Carta de Intenções para atender AB#19478

---
+++ src/SME.SGP.Infra/Dtos/CartaIntencoesDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra.Dtos
+{
+    public class CartaIntencoesDto
+    {
+        public long TurmaId { get; set; }
+
+        public TurmaDto Turma { get; set; }
+
+        public long PeriodoEscolarId { get; set; }
+
+        public int Bimestre { get; set; }
+
+        public PeriodoEscolarDto PeriodoEscolar { get; set; }
+
+        public long ComponenteCurricularId { get; set; }
+
+        public string Planejamento { get; set; }
+
+        public bool Excluido { get; set; }
+    }
+}
