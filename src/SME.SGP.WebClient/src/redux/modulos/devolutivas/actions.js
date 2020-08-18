afe96975fd89d52df1b0781872d3bbfc8158a5fe
Cleiton <cleiton.dutra@amcom.com.br>
2020-08-18 16:04:57 -0300

    devolutivas tela de cadastro ajustes -  consulta por id, performace, layout

--- src/SME.SGP.WebClient/src/redux/modulos/devolutivas/actions.js
+++ src/SME.SGP.WebClient/src/redux/modulos/devolutivas/actions.js
@@ -2,3 +2,13 @@ export const setDadosPlanejamentos = payload => ({
   type: '@devolutivas/setDadosPlanejamentos',
   payload,
 });
+
+export const setPaginaAtiva = payload => ({
+  type: '@devolutivas/setPaginaAtiva',
+  payload,
+});
+
+export const limparDadosPlanejamento = payload => ({
+  type: '@devolutivas/limparDadosPlanejamento',
+  payload,
+});
