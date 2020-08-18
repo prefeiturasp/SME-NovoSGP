b555411df7ba0bdbd401e54b3bbc1093488b37ca
Cleiton <cleiton.dutra@amcom.com.br>
2020-08-18 16:04:57 -0300

    devolutivas tela de cadastro ajustes -  consulta por id, performace, layout

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/Devolutivas/DadosPlanejamentoDiarioBordo/dadosPlanejamentoDiarioBordo.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/Devolutivas/DadosPlanejamentoDiarioBordo/dadosPlanejamentoDiarioBordo.js
@@ -1,51 +1,21 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import CardPlanejamento from '~/paginas/DiarioClasse/Devolutivas/DadosPlanejamentoDiarioBordo/CardPlanejamento/cardPlanejamento';
-import { BarraNavegacao } from '~/componentes';
+import BarraNavegacaoPlanejamento from '~/paginas/DiarioClasse/Devolutivas/DadosPlanejamentoDiarioBordo/BarraNavegacaoPlanejamento/barraNavegacaoPlanejamento';
 
-const DadosPlanejamentoDiarioBordo = props => {
+const DadosPlanejamentoDiarioBordo = React.memo(props => {
   const { onChangePage } = props;
 
   const dadosPlanejamentos = useSelector(
     store => store.devolutivas.dadosPlanejamentos
   );
 
-  const [paginaAtiva, setPaginaAtiva] = useState();
-  const [paginas, setPaginas] = useState([]);
-
-  const montarPaginas = useCallback(() => {
-    const itens = [];
-    for (let index = 0; index < dadosPlanejamentos.totalPaginas; index += 1) {
-      itens.push({ id: index + 1 });
-    }
-    setPaginas(itens);
-  }, [dadosPlanejamentos]);
-
-  useEffect(() => {
-    if (dadosPlanejamentos && dadosPlanejamentos.totalPaginas) {
-      montarPaginas();
-    }
-  }, [dadosPlanejamentos, montarPaginas]);
-
-  const onChangeItem = pagina => {
-    setPaginaAtiva(pagina);
-    onChangePage(pagina.id);
-  };
-
   return (
     <>
-      {dadosPlanejamentos && paginas && paginas.length ? (
+      {dadosPlanejamentos && dadosPlanejamentos.totalPaginas ? (
         <>
-          <BarraNavegacao
-            itens={paginas}
-            itemAtivo={
-              paginaAtiva
-                ? paginas.find(pagina => pagina.id === paginaAtiva.id)
-                : paginas[0]
-            }
-            onChangeItem={onChangeItem}
-          />
-
+          <BarraNavegacaoPlanejamento onChangePage={onChangePage} />
           <CardPlanejamento />
         </>
       ) : (
@@ -53,6 +23,14 @@ const DadosPlanejamentoDiarioBordo = props => {
       )}
     </>
   );
+});
+
+DadosPlanejamentoDiarioBordo.propTypes = {
+  onChangePage: PropTypes.func,
+};
+
+DadosPlanejamentoDiarioBordo.defaultProps = {
+  onChangePage: () => {},
 };
 
 export default DadosPlanejamentoDiarioBordo;
