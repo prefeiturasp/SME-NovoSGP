935dbca9e8e558029a5be40c41db5f61a02b57f1
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-08-21 19:37:56 -0300

    Ajuste regencia

--- src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorAnosEModalidade/ObterComponentesCurricularesPorAnosEModalidadeQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorAnosEModalidade/ObterComponentesCurricularesPorAnosEModalidadeQueryHandler.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Aplicacao.Integracoes.Respostas;
 using SME.SGP.Dominio;
+using SME.SGP.Dto;
+using SME.SGP.Infra;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -11,10 +14,16 @@ namespace SME.SGP.Aplicacao.Queries.ComponentesCurriculares.ObterComponentesCurr
     public class ObterComponentesCurricularesPorAnosEModalidadeQueryHandler : IRequestHandler<ObterComponentesCurricularesPorAnosEModalidadeQuery, IEnumerable<ComponenteCurricularEol>>
     {
         private readonly IServicoEol servicoEol;
+        private readonly IConsultasAbrangencia consultasAbrangencia;
+        private readonly IConsultasDisciplina consultasDisciplina;
 
-        public ObterComponentesCurricularesPorAnosEModalidadeQueryHandler(IServicoEol servicoEol)
+        public ObterComponentesCurricularesPorAnosEModalidadeQueryHandler(IServicoEol servicoEol, 
+                                                                          IConsultasAbrangencia consultasAbrangencia,
+                                                                          IConsultasDisciplina consultasDisciplina)
         {
             this.servicoEol = servicoEol ?? throw new System.ArgumentNullException(nameof(servicoEol));
+            this.consultasAbrangencia = consultasAbrangencia ?? throw new System.ArgumentNullException(nameof(consultasAbrangencia));
+            this.consultasDisciplina = consultasDisciplina ?? throw new System.ArgumentNullException(nameof(consultasDisciplina));
         }
 
         public async Task<IEnumerable<ComponenteCurricularEol>> Handle(ObterComponentesCurricularesPorAnosEModalidadeQuery request, CancellationToken cancellationToken)
@@ -24,6 +33,29 @@ namespace SME.SGP.Aplicacao.Queries.ComponentesCurriculares.ObterComponentesCurr
             {
                 throw new NegocioException("Nenhum componente localizado para a modalidade e anos informados.");
             }
+
+            if(componentes.Any(componentes  => componentes.Regencia))
+            {
+                var turmasAbrangencia = await consultasAbrangencia.ObterTurmas(request.CodigoUe, request.Modalidade);
+
+                var turmasNosAnos = turmasAbrangencia.Where(t => request.AnosEscolares.Contains(t.Ano) && t.AnoLetivo == request.AnoLetivo).DistinctBy(c => c.Ano);
+
+                var lstComponentesRegencia = new List<DisciplinaResposta>();
+
+                foreach (var regencia in componentes.Where(componentes => componentes.Regencia))
+                {
+                    var turmaNoAno = turmasNosAnos.FirstOrDefault(t => t.Ano == "0");
+
+                    var componentesRegencia = await consultasDisciplina.ObterComponentesRegencia(ConverterParaTurma(turmaNoAno, request.CodigoUe), regencia.Codigo);
+
+                    if (componentesRegencia != null || componentesRegencia.Any())
+                        lstComponentesRegencia.AddRange(componentesRegencia);
+                }
+
+                if (lstComponentesRegencia != null && lstComponentesRegencia.Any())
+                    componentes.AddRange(MapearParaComponenteCurricularDto(lstComponentesRegencia));
+            }
+
             componentes = componentes.OrderBy(c => c.Descricao).ToList();
             componentes.Insert(0, new ComponenteCurricularEol
             {
@@ -31,6 +63,27 @@ namespace SME.SGP.Aplicacao.Queries.ComponentesCurriculares.ObterComponentesCurr
                 Descricao = "Todos"
             });
             return componentes;
+        }
+
+        private IEnumerable<ComponenteCurricularEol> MapearParaComponenteCurricularDto(IEnumerable<DisciplinaResposta> respostaEol)
+        {
+            foreach (var item in respostaEol)
+            {
+                yield return new ComponenteCurricularEol()
+                {
+                  
+                };
+            }
+        }
+
+        private Turma ConverterParaTurma(AbrangenciaTurmaRetorno turmaAbrangencia, string codigoUe)
+        {
+            return new Turma()
+            {
+                CodigoTurma = turmaAbrangencia.Codigo,
+                ModalidadeCodigo = (Modalidade)turmaAbrangencia.CodigoModalidade,
+                Ue = new Ue() { CodigoUe = codigoUe }
+            };
         }
     }
 }
---
+++ src/SME.SGP.Infra/Extensoes/Colecoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SME.SGP.Infra
+{
+    public static class Colecoes
+    {
+        /// <summary>
+        /// Retorna a mesma lista removendo os itens duplicados de acordo com o filtro
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lista">Enumerable para remover duplicados</param>
+        /// <param name="filtro">Function com o campo que deve comparar</param>
+        /// <returns></returns>
+        public static IEnumerable<T> DistinctBy<T>(this IEnumerable<T> lista, Func<T, object> filtro)
+        {
+            return lista.GroupBy(filtro).Select(x => x.First());
+        }
+
+        public static T[] VazioSeNull<T>(this T[] array)
+        {
+            return array == null ? Array.Empty<T>() : array;
+        }
+    }
+}
