2e7a7d27289561ab0cf87cdfdde7311e855e362f
eliton-amcom <eliton.santos@amcom.com.br>
2020-08-22 16:24:19 -0300

    ajsute front

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/CompensacaoAusencia/ServicoRelatorioCompensacaoAusencia.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/CompensacaoAusencia/ServicoRelatorioCompensacaoAusencia.js
@@ -2,7 +2,7 @@ import api from '~/servicos/api';
 
 class ServicoRelatorioCompensacaoAusencia {
   gerar = async params => {
-    const url = 'v1/relatorios/frequencia/compensacoes-ausencias';
+    const url = 'v1/relatorios/frequencias/compensacoes-ausencias';
     return api.post(url, params);
   };
 }
