b616dd17a9b1565acc9d5fd8031e1ca9904a9750
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-08-24 13:30:02 -0300

    Salvar anotações frequência aluno

---
+++ src/SME.SGP.Aplicacao/Queries/Aluno/ObterAlunoPorCodigoEol/ObterAlunoPorCodigoEolQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Integracoes;
+using SME.SGP.Infra;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterAlunoPorCodigoEolQueryHandler : IRequestHandler<ObterAlunoPorCodigoEolQuery, AlunoPorTurmaResposta>
+    {
+        private readonly IServicoEol servicoEol;
+
+        public ObterAlunoPorCodigoEolQueryHandler(IServicoEol servicoEol)
+        {
+            this.servicoEol = servicoEol ?? throw new System.ArgumentNullException(nameof(servicoEol));
+        }
+
+        public async Task<AlunoPorTurmaResposta> Handle(ObterAlunoPorCodigoEolQuery request, CancellationToken cancellationToken)
+        {
+            return (await servicoEol.ObterDadosAluno(request.CodigoAluno, request.AnoLetivo))?.FirstOrDefault();
+        }
+    }
+}
