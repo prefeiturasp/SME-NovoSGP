3ab98c948dbff96f5b783c75c033f9daea6ed00a
Rafael Losi <rafael.losi@amcom.com.br>
2020-08-24 17:29:35 -0300

    19845 - corrigido query de obterdiarios para a devolutiva

--- src/SME.SGP.Aplicacao/Queries/DiarioBordo/ObterDatasEfetivasDiarios/ObterDatasEfetivasDiariosQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/DiarioBordo/ObterDatasEfetivasDiarios/ObterDatasEfetivasDiariosQuery.cs
@@ -8,11 +8,15 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterDatasEfetivasDiariosQuery : IRequest<IEnumerable<Tuple<long, DateTime>>>
     {
+        public string TurmaCodigo { get; set; }
+        public long ComponenteCurricularCodigo { get; set; }
         public DateTime PeriodoInicio { get; }
         public DateTime PeriodoFim { get; }
 
-        public ObterDatasEfetivasDiariosQuery(DateTime periodoInicio, DateTime periodoFim)
+        public ObterDatasEfetivasDiariosQuery(string turmaCodigo, long componenteCurricularCodigo, DateTime periodoInicio, DateTime periodoFim)
         {
+            TurmaCodigo = turmaCodigo;
+            ComponenteCurricularCodigo = componenteCurricularCodigo;
             PeriodoInicio = periodoInicio;
             PeriodoFim = periodoFim;
         }
