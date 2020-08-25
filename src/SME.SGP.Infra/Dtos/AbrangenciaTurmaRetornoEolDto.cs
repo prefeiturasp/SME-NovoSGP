3aca3b52ce6fedb5d204473585215519f4dfec90
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-08-25 01:09:18 -0300

    Removendo codigo obsoleto

--- src/SME.SGP.Infra/Dtos/AbrangenciaTurmaRetornoEolDto.cs
+++ src/SME.SGP.Infra/Dtos/AbrangenciaTurmaRetornoEolDto.cs
@@ -17,6 +17,5 @@ namespace SME.SGP.Dto
         public bool EHistorico { get; set; }
         public bool EnsinoEspecial { get; set; }
         public DateTime? DataInicioTurma { get; set; }
-        public string SituacaoTurma { get; set; }
     }
 }
