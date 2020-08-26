beb6ccc37b977666ea5ec011133a3f1fefceada5
Rafael Losi <rafael.losi@amcom.com.br>
2020-08-25 21:01:29 -0300

    ajustes branch

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioMotivoAusencia.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioMotivoAusencia.cs
@@ -6,5 +6,6 @@ namespace SME.SGP.Dominio.Interfaces
     public interface IRepositorioMotivoAusencia
     {
         Task<IEnumerable<MotivoAusencia>> ListarAsync();
+        Task<MotivoAusencia> ObterPorIdAsync(long id);
     }
 }
