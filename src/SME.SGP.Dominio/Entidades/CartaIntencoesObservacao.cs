794ad15d76f9a5615432701c048501481d16dc9d
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-08-28 13:15:44 -0300

    Criada entidade CartaIntecoesObservacao

---
+++ src/SME.SGP.Dominio/Entidades/CartaIntencoesObservacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SME.SGP.Dominio
+{
+    public class CartaIntencoesObservacao : EntidadeBase
+    {
+        public CartaIntencoesObservacao(string observacao, long cartaIntencoesId, long usuarioId)
+        {
+            Observacao = observacao;
+            CartaIntencoesId = cartaIntencoesId;
+            UsuarioId = usuarioId;
+        }
+        protected CartaIntencoesObservacao()
+        {
+        }
+
+        public string Observacao { get; set; }
+        public long CartaIntencoesId { get; set; }
+        public long UsuarioId { get; set; }
+        public bool Excluido { get; set; }
+
+        public void ValidarUsuarioAlteracao(long usuarioId)
+        {
+            if (usuarioId != UsuarioId)
+            {
+                throw new NegocioException("Você não pode alterar essa observação.");
+            }
+        }
+
+        public void Remover()
+        {
+            Excluido = true;
+        }
+    }
+}
