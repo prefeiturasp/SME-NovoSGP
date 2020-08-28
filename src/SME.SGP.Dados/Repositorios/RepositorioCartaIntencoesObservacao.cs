92947c5555f01538b6bdbf9e48e332d4e73b041e
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-08-28 15:55:10 -0300

    Correção repositório

--- src/SME.SGP.Dados/Repositorios/RepositorioCartaIntencoesObservacao.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioCartaIntencoesObservacao.cs
@@ -32,26 +32,5 @@ namespace SME.SGP.Dados.Repositorios
 			return await database.Conexao.QueryAsync<ListarObservacaoCartaIntencoesDto>(sql, new { cartaIntencoesId, usuarioLogadoId });
 		}
 
-      //  public async Task<IEnumerable<ListarObservacaoDiarioBordoDto>> ListarPorDiarioBordoAsync(long diarioBordoId, long usuarioLogadoId)
-      //  {
-      //      var sql = @"select
-						//	id,
-						//	observacao,
-						//	(usuario_id = @usuarioLogadoId) as Proprietario,
-						//	criado_em as CriadoEm,
-						//	criado_por as CriadoPor,
-						//	criado_rf as CriadoRf,
-						//	alterado_em as AlteradoEm,
-						//	alterado_por as AlteradoPor,
-						//	alterado_rf as AlteradoRf
-						//from
-						//	diario_bordo_observacao
-						//where
-						//	diario_bordo_id = @diarioBordoId
-						//	and not excluido 
-      //                  order by criado_em desc";
-
-      //      return await database.Conexao.QueryAsync<ListarObservacaoDiarioBordoDto>(sql, new { diarioBordoId, usuarioLogadoId });
-      //  }
-    }
+	}
 }
