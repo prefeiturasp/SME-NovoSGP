3a16be037a3da42f0d592178e72de2403e73517a
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-08-28 14:47:02 -0300

    Branch inicial da listagem

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/CartaIntencoes/ListarObservacaoCartaIntencoesUseCase.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ListarObservacaoCartaIntencoesUseCase : IListarObservacaoCartaIntencoesUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ListarObservacaoCartaIntencoesUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<IEnumerable<ListarObservacaoCartaIntencoesDto>> Executar(long cartaIntencoesId)
+        {
+            var usuarioId = await mediator.Send(new ObterUsuarioLogadoIdQuery());
+            return await mediator.Send(new ListarObservacaoCartaIntencoesQuery(cartaIntencoesId, usuarioId));
+        }
+    }
+}
