3e41bb15c3a7edaee1b9ec819264c16af4ea5804
Luiz Henrique Araújo <70153714+luizaraujoamcom@users.noreply.github.com>
2020-08-31 16:45:31 -0300

    Update SalvarCartaIntencoesObservacaoDto.cs

--- src/SME.SGP.Infra/Dtos/CartaIntencoesObservacao/SalvarCartaIntencoesObservacaoDto.cs
+++ src/SME.SGP.Infra/Dtos/CartaIntencoesObservacao/SalvarCartaIntencoesObservacaoDto.cs
@@ -7,16 +7,4 @@ namespace SME.SGP.Infra.Dtos
     {
         public string Observacao { get; set; }
     }
-
-
-    //public class SalvarCartaIntencoesObservacaoDtoValidator : AbstractValidator<SalvarCartaIntencoesObservacaoDto>
-    //{
-    //    public SalvarCartaIntencoesObservacaoDtoValidator()
-    //    {
-
-    //        RuleFor(c => c.Observacao)
-    //            .NotEmpty()
-    //            .WithMessage("O campo observação deve ser informado.");
-    //    }
-    //}
 }
