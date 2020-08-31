f6adde59a2ffbd34ca3a314a01093a4c7aeb46b5
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-08-31 16:13:35 -0300

    Refatorado salvar observação

---
+++ src/SME.SGP.Infra/Dtos/CartaIntencoesObservacao/SalvarCartaIntencoesObservacaoDto.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace SME.SGP.Infra.Dtos
+{
+    public class SalvarCartaIntencoesObservacaoDto
+    {
+        public string Observacao { get; set; }
+    }
+
+
+    //public class SalvarCartaIntencoesObservacaoDtoValidator : AbstractValidator<SalvarCartaIntencoesObservacaoDto>
+    //{
+    //    public SalvarCartaIntencoesObservacaoDtoValidator()
+    //    {
+
+    //        RuleFor(c => c.Observacao)
+    //            .NotEmpty()
+    //            .WithMessage("O campo observação deve ser informado.");
+    //    }
+    //}
+}
