e2e1ff03253b60abf1f7f9c81f32b9971a5aff47
Cleiton <cleiton.dutra@amcom.com.br>
2020-09-01 16:39:59 -0300

    dashboard - layout cards + mock de dados

--- src/SME.SGP.WebClient/src/paginas/Principal/principal.js
+++ src/SME.SGP.WebClient/src/paginas/Principal/principal.js
@@ -1,59 +1,56 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CardsDashboard from '~/componentes-sgp/cardsDashboard/cardsDashboard';
+import {
+  setCarregandoDadosCardsDashboard,
+  setDadosCardsDashboard,
+} from '~/redux/modulos/dashboard/actions';
+import ServicoDashboard from '~/servicos/Paginas/Dashboard/ServicoDashboard';
+import Alert from '../../componentes/alert';
 import Card from '../../componentes/card';
 import Grid from '../../componentes/grid';
-import CardLink from '../../componentes/cardlink';
 import Row from '../../componentes/row';
-import Alert from '../../componentes/alert';
-import {
-  URL_PLANO_ANUAL,
-  URL_PLANO_CICLO,
-  URL_FREQ_PLANO_AULA,
-} from '../../constantes/url';
 import ListaNotificacoes from './listaNotificacoes';
-import modalidade from '~/dtos/modalidade';
-import { Container, Label, Dashboard } from './principal.css';
-import { obterDescricaoNomeMenu } from '~/servicos/servico-navegacao';
-import RotasDto from '~/dtos/rotasDto';
+import { Container, Label } from './principal.css';
 
 const Principal = () => {
-  const FREQUENCIA_TYPE = 'frequencia';
-  const CICLOS_TYPE = 'ciclos';
-  const ANUAL_TYPE = 'anual';
-  const [escolaSelecionada, setEscolaSelecionada] = useState(false);
   const [turmaSelecionada, setTurmaSelecionada] = useState(false);
 
   const usuario = useSelector(state => state.usuario);
   const perfil = useSelector(state => state.perfil.perfilSelecionado);
   const modalidades = useSelector(state => state.filtro.modalidades);
 
+  const dispatch = useDispatch();
+
   const validarFiltro = useCallback(() => {
     if (!usuario.turmaSelecionada) {
       setTurmaSelecionada(false);
-      setEscolaSelecionada(false);
       return;
     }
 
     const temTurma = !!usuario.turmaSelecionada.turma;
-    const temEscola = !!usuario.turmaSelecionada.unidadeEscolar;
 
     setTurmaSelecionada(temTurma);
-    setEscolaSelecionada(temEscola);
   }, [usuario.turmaSelecionada]);
 
   useEffect(() => {
     validarFiltro();
   }, [usuario, validarFiltro]);
 
-  const cicloLiberado = () => {
-    return escolaSelecionada;
-  };
+  const obterDadosDashboard = useCallback(async () => {
+    dispatch(setDadosCardsDashboard([]));
+    dispatch(setCarregandoDadosCardsDashboard(true));
+    const retorno = await ServicoDashboard.obterDadosDashboard();
 
-  const isDesabilitado = tipo => {
-    if (!escolaSelecionada) return true;
-    if (tipo === CICLOS_TYPE) return !cicloLiberado();
-    return !turmaSelecionada;
-  };
+    if (retorno && retorno.data && retorno.data.length) {
+      dispatch(setDadosCardsDashboard(retorno.data));
+    }
+    dispatch(setCarregandoDadosCardsDashboard(false));
+  }, [dispatch]);
+
+  useEffect(() => {
+    obterDadosDashboard();
+  }, []);
 
   return (
     <div className="col-md-12">
@@ -110,56 +107,7 @@ const Principal = () => {
           </div>
         </div>
       </Card>
-      <Dashboard>
-        <Row>
-          <Grid cols={12} className="form-inline alinhar-itens-topo">
-            <CardLink
-              cols={[4, 4, 4, 12]}
-              iconSize="90px"
-              url={URL_FREQ_PLANO_AULA}
-              disabled={isDesabilitado(FREQUENCIA_TYPE)}
-              icone="fa-columns"
-              pack="fas"
-              label={obterDescricaoNomeMenu(
-                RotasDto.FREQUENCIA_PLANO_AULA,
-                modalidades,
-                usuario.turmaSelecionada
-              )}
-              minHeight="177px"
-            />
-            <CardLink
-              cols={[4, 4, 4, 12]}
-              classHidden="hidden-xs-down"
-              iconSize="90px"
-              url={URL_PLANO_CICLO}
-              disabled={isDesabilitado(CICLOS_TYPE)}
-              icone="fa-calendar-minus"
-              pack="far"
-              label={obterDescricaoNomeMenu(
-                RotasDto.PLANO_CICLO,
-                modalidades,
-                usuario.turmaSelecionada
-              )}
-              minHeight="177px"
-            />
-            <CardLink
-              cols={[4, 4, 4, 12]}
-              classHidden="hidden-xs-down"
-              iconSize="90px"
-              url={URL_PLANO_ANUAL}
-              disabled={isDesabilitado(ANUAL_TYPE)}
-              icone="fa-calendar-alt"
-              pack="far"
-              label={obterDescricaoNomeMenu(
-                RotasDto.PLANO_ANUAL,
-                modalidades,
-                usuario.turmaSelecionada
-              )}
-              minHeight="177px"
-            />
-          </Grid>
-        </Row>
-      </Dashboard>
+      <CardsDashboard />
     </div>
   );
 };
