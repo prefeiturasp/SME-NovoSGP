ec2b35abbac4af7ad8b395070cb5c980b44ff3ea
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-09-01 12:42:12 -0300

    Endpoint SGP relatório calendário

--- src/SME.SGP.Api/Controllers/RelatorioController.cs
+++ src/SME.SGP.Api/Controllers/RelatorioController.cs
@@ -33,5 +33,11 @@ namespace SME.SGP.Api.Controllers
         {
             return Ok(await gerarRelatorioFaltasFrequenciaUseCase.Executar(filtroRelatorioFaltasFrequenciaDto));
         }
+
+        [HttpPost("calendario")]
+        public async Task<IActionResult> Calendario([FromBody] FiltroRelatorioCalendarioDto filtroRelatorioCalendarioDto, [FromServices] IRelatorioCalendarioUseCase relatorioCalendarioUseCase)
+        {
+            return Ok(await relatorioCalendarioUseCase.Executar(filtroRelatorioCalendarioDto));
+        }
     }
 }
