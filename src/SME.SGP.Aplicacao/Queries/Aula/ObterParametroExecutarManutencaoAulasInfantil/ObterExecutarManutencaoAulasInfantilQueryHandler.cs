f34b7b76f61b2144f9e160ae3e1cec028f9a2695
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-01 12:42:50 -0300

    Implementado parâmetro para executar ou não a rotina

---
+++ src/SME.SGP.Aplicacao/Queries/Aula/ObterParametroExecutarManutencaoAulasInfantil/ObterExecutarManutencaoAulasInfantilQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    class ObterExecutarManutencaoAulasInfantilQueryHandler : IRequestHandler<ObterExecutarManutencaoAulasInfantilQuery, bool>
+    {
+        private readonly IRepositorioParametrosSistema repositorioParametrosSistema;
+
+        public ObterExecutarManutencaoAulasInfantilQueryHandler(IRepositorioParametrosSistema repositorioParametrosSistema)
+        {
+            this.repositorioParametrosSistema = repositorioParametrosSistema ?? throw new ArgumentNullException(nameof(repositorioParametrosSistema));
+        }
+        public async Task<bool> Handle(ObterExecutarManutencaoAulasInfantilQuery request, CancellationToken cancellationToken)
+        {
+            var valor = await this.repositorioParametrosSistema.ObterValorUnicoPorTipo(Dominio.TipoParametroSistema.ExecutarManutencaoAulasInfantil);
+
+            if (valor.Equals("1"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
