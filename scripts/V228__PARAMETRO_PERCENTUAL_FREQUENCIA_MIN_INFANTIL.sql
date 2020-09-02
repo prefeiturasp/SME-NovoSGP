5d48d5d50582c1c7f0ada9c604478015978308c3
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-02 12:34:51 -0300

    Alterado 2019 para 2021

--- scripts/V228__PARAMETRO_PERCENTUAL_FREQUENCIA_MIN_INFANTIL.sql
+++ scripts/V228__PARAMETRO_PERCENTUAL_FREQUENCIA_MIN_INFANTIL.sql
@@ -2,21 +2,6 @@ insert
     into 
     public.parametros_sistema (nome,descricao,valor,ano,criado_em,criado_por, criado_rf,tipo)
 select
-	'PercentualFrequenciaMinimaInfantil','Percentual de frequência mínima da educação infantil para envio da notificação bimestral','60','2019',now(),'Carga Inicial','Carga Inicial',27
-where
-	not exists(
-	select
-		1
-	from
-		public.parametros_sistema
-	where
-		tipo = 27 and ano = '2019' );
-
-
-insert 
-    into 
-    public.parametros_sistema (nome,descricao,valor,ano,criado_em,criado_por, criado_rf,tipo)
-select
 	'PercentualFrequenciaMinimaInfantil','Percentual de frequência mínima da educação infantil para envio da notificação bimestral','60','2020',now(),'Carga Inicial','Carga Inicial',27
 where
 	not exists(
@@ -26,3 +11,18 @@ where
 		public.parametros_sistema
 	where
 		tipo = 27 and ano = '2020' );
+		
+
+insert 
+    into 
+    public.parametros_sistema (nome,descricao,valor,ano,criado_em,criado_por, criado_rf,tipo)
+select
+	'PercentualFrequenciaMinimaInfantil','Percentual de frequência mínima da educação infantil para envio da notificação bimestral','60','2021',now(),'Carga Inicial','Carga Inicial',27
+where
+	not exists(
+	select
+		1
+	from
+		public.parametros_sistema
+	where
+		tipo = 27 and ano = '2021' );
