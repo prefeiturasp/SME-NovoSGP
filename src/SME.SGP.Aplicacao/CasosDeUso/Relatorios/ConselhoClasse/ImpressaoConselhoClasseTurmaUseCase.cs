c387544a54f74faa39b5314330fe8ccc4773499b
eliton-amcom <eliton.santos@amcom.com.br>
2020-09-02 17:14:46 -0300

    validação backend, se valores foram informados

--- src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/ConselhoClasse/ImpressaoConselhoClasseTurmaUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/ConselhoClasse/ImpressaoConselhoClasseTurmaUseCase.cs
@@ -21,6 +21,12 @@ namespace SME.SGP.Aplicacao
         {
             var usuario = await mediator.Send(new ObterUsuarioLogadoQuery());
 
+            if(filtroRelatorioConselhoClasseDto.FechamentoTurmaId <= 0)
+                throw new NegocioException("O ID do fechamento da turma não pode ser nulo");
+
+            if (filtroRelatorioConselhoClasseDto.ConselhoClasseId <= 0)
+                throw new NegocioException("O ID do conselho de classe não pode ser nulo");
+
             if (usuario == null)
                 throw new NegocioException("Não foi possível localizar o usuário.");
 
