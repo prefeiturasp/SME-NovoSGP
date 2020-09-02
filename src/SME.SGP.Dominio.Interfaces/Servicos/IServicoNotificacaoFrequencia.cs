2747585617179baedbe35b9199b1d732964de3d3
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-02 18:23:12 -0300

    Criada alteração nas classes de notificação

--- src/SME.SGP.Dominio.Interfaces/Servicos/IServicoNotificacaoFrequencia.cs
+++ src/SME.SGP.Dominio.Interfaces/Servicos/IServicoNotificacaoFrequencia.cs
@@ -5,7 +5,7 @@ namespace SME.SGP.Dominio.Interfaces
 {
     public interface IServicoNotificacaoFrequencia
     {
-        void ExecutaNotificacaoRegistroFrequencia();
+        Task ExecutaNotificacaoRegistroFrequencia();
         void VerificaRegraAlteracaoFrequencia(long registroFrequenciaId, DateTime criadoEm, DateTime alteradoEm, long usuarioAlteracaoId);
         Task NotificarCompensacaoAusencia(long compensacaoId);
         Task NotificarAlunosFaltosos();
