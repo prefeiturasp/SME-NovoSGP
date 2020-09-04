955abe547e5fae82ffe2df2a1ff841b4c58dd7bc
Rodrigo Galante <rodrigo.galante@globo.com>
2020-09-04 01:39:44 -0300

    Reiniciar Senha - Escola Aqui

---
+++ src/SME.SGP.Aplicacao/Commands/Usuarios/EscolaAqui/SolicitarReiniciarSenhaCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SME.SGP.Dominio;
+using SME.SGP.Infra.Dtos;
+using SME.SGP.Infra.Dtos.EscolaAqui;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SolicitarReiniciarSenhaCommandHandler : IRequestHandler<SolicitarReiniciarSenhaCommand, RespostaSolicitarReiniciarSenhaDto>
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public SolicitarReiniciarSenhaCommandHandler(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
+        public async Task<RespostaSolicitarReiniciarSenhaDto> Handle(SolicitarReiniciarSenhaCommand request, CancellationToken cancellationToken)
+        {
+            var httpClient = httpClientFactory.CreateClient("servicoAcompanhamentoEscolar");
+
+            var solicitarReiniciarSenhaDto = new SolicitarReiniciarSenhaDto(request.Cpf);
+            var parametros = JsonConvert.SerializeObject(solicitarReiniciarSenhaDto);
+            var resposta = await httpClient.PutAsync($"/api/v1/Autenticacao/Senha/ReiniciarSenha", new StringContent(parametros, Encoding.UTF8, "application/json-patch+json"));
+
+            if (resposta.IsSuccessStatusCode && resposta.StatusCode != HttpStatusCode.NoContent)
+            {
+                var json = await resposta.Content.ReadAsStringAsync();
+                var respostaApi = JsonConvert.DeserializeObject<RespostaApi>(json);
+
+                return new RespostaSolicitarReiniciarSenhaDto(respostaApi.Data.ToString());
+            }
+            else throw new NegocioException("Não foi possível reinciar a senha");
+        }
+    }
+
+}
---
+++ src/SME.SGP.Aplicacao/Commands/Usuarios/EscolaAqui/SolicitarReiniciarSenhaCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SolicitarReiniciarSenhaCommandValidator : AbstractValidator<SolicitarReiniciarSenhaCommand>
+    {
+        public SolicitarReiniciarSenhaCommandValidator()
+        {
+            RuleFor(x => x.Cpf).NotEmpty().WithMessage("O CPF é Obrigátorio"); ;
+        }
+    }
+}
