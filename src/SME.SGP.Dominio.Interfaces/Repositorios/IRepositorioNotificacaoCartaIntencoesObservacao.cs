6c3b5f3ae5959b302b7e095e3b02c6ed6e7979cb
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-07 15:08:16 -0300

    adicionado repositorio e mapeamento

---
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioNotificacaoCartaIntencoesObservacao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio.Interfaces
+{
+    public interface IRepositorioNotificacaoCartaIntencoesObservacao
+    {
+        void Inserir(long notificacaoId, long cartaIntencoesObservacaoId);
+        void Excluir(long cartaIntencoesObservacaoId);
+    }
+}
