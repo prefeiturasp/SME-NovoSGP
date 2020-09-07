6222778d44b6b169441002a9ac14c3912876938f
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-09-07 20:18:30 -0300

    Modelagem Devolutiva

---
+++ src/SME.SGP.Dominio/Entidades/DevolutivaDiarioBordoNotificacao.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio
+{
+    public class DevolutivaDiarioBordoNotificacao : EntidadeBase
+    {
+        public long Id { get; set; }
+        public long DevolutivaId { get; set; }
+        public long NotificacaoId { get; set; }
+    }
+}
