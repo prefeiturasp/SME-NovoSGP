846b260455fe6184de0fbc921b41fd82c3ad477d
wagnersantos-amcom <wagner.santos@amcom.com.br>
2020-09-08 20:22:32 -0300

    organizando imports da tela de eventos

--- src/SME.SGP.WebClient/src/servicos/index.js
+++ src/SME.SGP.WebClient/src/servicos/index.js
@@ -4,3 +4,4 @@ export * from './Paginas';
 export { default as api } from './api';
 export { default as history } from './history';
 export { default as AbrangenciaServico } from './Abrangencia';
+export * from './breadcrumb-services';
