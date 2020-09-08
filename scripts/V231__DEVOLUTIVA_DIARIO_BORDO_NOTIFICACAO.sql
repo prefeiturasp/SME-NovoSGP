1fd1caede151a6a45388d77011d8be72287519c7
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-09-08 15:48:06 -0300

    Ajuste da modelagem

--- scripts/V231__DEVOLUTIVA_DIARIO_BORDO_NOTIFICACAO.sql
+++ scripts/V231__DEVOLUTIVA_DIARIO_BORDO_NOTIFICACAO.sql
@@ -1,11 +1,9 @@
 
---V229__DIARIO_BORDO_OBSERVACAO_NOTIFICACAO
-
 DROP TABLE IF EXISTS public.devolutiva_diario_bordo_notificacao;
 
 CREATE TABLE public.devolutiva_diario_bordo_notificacao (
 	id int8 NOT NULL GENERATED ALWAYS AS identity,
-	devolutiva_id int8 NOT NULL,
+	observacao_id int8 NOT NULL,
 	notificacao_id int8 NOT null
 );
 
@@ -16,4 +14,12 @@ select
 		'FOREIGN KEY (notificacao_id) REFERENCES notificacao (id)'
 	);
 
+select
+	f_cria_fk_se_nao_existir(
+		'devolutiva_diario_bordo_notificacao',
+		'devolutiva_diario_bordo_notificacao_diario_bordo_observacao_fk',
+		'FOREIGN KEY (observacao_id) REFERENCES diario_bordo_observacao (id)'
+	);
+
 CREATE INDEX devolutiva_diario_bordo_notificacao_notificacao__idx ON public.devolutiva_diario_bordo_notificacao USING btree (notificacao_id);
+CREATE INDEX devolutiva_diario_bordo_notificacao_diario_bordo_observacao_idx ON public.diario_bordo_observacao_notificacao USING btree (observacao_id);
