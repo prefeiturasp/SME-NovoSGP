5627b88889d251436394beb548e5655e27946703
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-14 15:28:00 -0300

    Repositorio

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioPendenciaAula.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioPendenciaAula: IRepositorioPendenciaAula
+    {
+        private readonly ISgpContext database;
+
+        public RepositorioPendenciaAula(ISgpContext database)
+        {
+            this.database = database;
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/PendenciaAula.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Dominio
+{
+    public class PendenciaAula
+    {
+        public long AulaId { get; set; }
+        public TipoPendenciaAula TipoPendenciaAula { get; set; }
+    }
+}
