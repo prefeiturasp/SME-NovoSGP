71495270d72a5a20be55d79a43b058470cef1df5
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-09-14 14:12:20 -0300

    Alteração de endpoint recuperação paralela
    Adicionando ano letivo nos filtros dos endpoints de recuperação paralela
    
    AB#23375

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRecuperacaoParalela.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRecuperacaoParalela.cs
@@ -8,12 +8,12 @@ namespace SME.SGP.Dominio.Interfaces
     {
         Task<IEnumerable<RetornoRecuperacaoParalela>> Listar(long turmaId, long periodoId);
 
-        Task<IEnumerable<RetornoRecuperacaoParalelaTotalAlunosAnoDto>> ListarTotalAlunosSeries(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano);
+        Task<IEnumerable<RetornoRecuperacaoParalelaTotalAlunosAnoDto>> ListarTotalAlunosSeries(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano, int anoLetivo);
 
-        Task<IEnumerable<RetornoRecuperacaoParalelaTotalAlunosAnoFrequenciaDto>> ListarTotalEstudantesPorFrequencia(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano);
+        Task<IEnumerable<RetornoRecuperacaoParalelaTotalAlunosAnoFrequenciaDto>> ListarTotalEstudantesPorFrequencia(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano, int anoLetivo);
 
-        Task<PaginacaoResultadoDto<RetornoRecuperacaoParalelaTotalResultadoDto>> ListarTotalResultado(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano, int? pagina);
+        Task<PaginacaoResultadoDto<RetornoRecuperacaoParalelaTotalResultadoDto>> ListarTotalResultado(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano, int anoLetivo, int? pagina);
 
-        Task<IEnumerable<RetornoRecuperacaoParalelaTotalResultadoDto>> ListarTotalResultadoEncaminhamento(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano, int? pagina);
+        Task<IEnumerable<RetornoRecuperacaoParalelaTotalResultadoDto>> ListarTotalResultadoEncaminhamento(int? periodo, string dreId, string ueId, int? cicloId, string turmaId, string ano, int anoLetivo, int? pagina);
     }
 }
