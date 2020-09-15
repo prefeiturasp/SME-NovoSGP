6e73899ab3bba2b1be3c4bbca530af920c38fb7c
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-09-15 16:30:59 -0300

    Ajuste

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioDiarioBordo.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioDiarioBordo.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Dominio.Interfaces
-//{
+{
     public interface IRepositorioDiarioBordo : IRepositorioBase<DiarioBordo>
     {
         Task<DiarioBordo> ObterPorAulaId(long aulaId);
