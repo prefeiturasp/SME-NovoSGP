c945e3b5f703c3cd842127c5b8f225dee7bcda9d
Rafael Losi <rafael.losi@amcom.com.br>
2020-09-16 17:17:31 -0300

    23142 - BranchMark das ferramentas para comparação de performance

---
+++ src/SME.SGP.Infra/Json/Converters/LongToStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SME.SGP.Infra.Json
+{
+    public class LongToStringConverter : JsonConverter<long>
+    {
+        public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if ((reader.TokenType == JsonTokenType.Number) && (typeToConvert == typeof(string)))
+            {
+                ReadOnlySpan<byte> span = reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan;
+                if (Utf8Parser.TryParse(span, out long number, out int bytesConsumed) && span.Length == bytesConsumed)
+                    return number;
+
+                if (Int64.TryParse(reader.GetString(), out number))
+                    return number;
+            }
+
+            return reader.GetInt64();
+        }
+
+        public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
