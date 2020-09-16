103aad6883a8c56876037176276a27ad5aff6147
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-09-16 09:29:15 -0300

    23769

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/ConselhoClasse/ObterBimestresComConselhoClasseTurmaUsecase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterBimestresComConselhoClasseTurmaUsecase : AbstractUseCase, IObterBimestresComConselhoClasseTurmaUseCase
+    {
+        public ObterBimestresComConselhoClasseTurmaUsecase(IMediator mediator) : base(mediator)
+        {
+        }
+        public async Task<IEnumerable<BimestreComConselhoClasseTurmaDto>> Executar(long turmaId)
+        {
+            return null;
+        }
+
+    }
+}
