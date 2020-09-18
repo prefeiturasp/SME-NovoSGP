d5ad9b32105f9ddcd4828d1079248161f820ff06
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-18 10:17:46 -0300

    Adicionado useCase

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/FiltroRelatorioPlanoAulaDto.cs
@@ -0,0 +1,11 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Infra.Dtos.Relatorios
+{
+    public class FiltroRelatorioPlanoAulaDto
+    {
+        public long PlanoAulaId { get; set; }
+        public string UsuarioNome { get; set; }
+        public string UsuarioRf { get; set; }
+    }
+}
