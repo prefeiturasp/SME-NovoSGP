22d7d562d82e8fcc64c2d73b655f4ba32d94abe7
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-21 10:53:54 -0300

    Correção da Abrangencia

--- src/SME.SGP.Dominio/Entidades/ObjetivoAprendizagem.cs
+++ src/SME.SGP.Dominio/Entidades/ObjetivoAprendizagem.cs
@@ -7,7 +7,7 @@ namespace SME.SGP.Dominio
     {
         private readonly Dictionary<string, int> Anos = new Dictionary<string, int>
         {
-           {"first",1},{"second",2},{"third",3},{"fourth",4},{"fifth",5},{"sixth",6},{"seventh",7},{"eighth",8},{"nineth",9}
+           {"first",1},{"second",2},{"third",3},{"fourth",4},{"fifth",5},{"sixth",6},{"seventh",7},{"eighth",8},{"nineth",9},{"tenth",10 },{"eleventh",11 },{"twelfth",12}, {"thirteenth", 13}
         };
 
         public int Ano => Anos[AnoTurma];
