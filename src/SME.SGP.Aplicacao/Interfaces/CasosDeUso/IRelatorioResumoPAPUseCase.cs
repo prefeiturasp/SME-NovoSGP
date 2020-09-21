32ea5a86599ee376b51d5a490b30961712f1a80a
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-09-21 16:13:01 -0300

    Endpoint relatorio resumo use case

---
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/IRelatorioResumoPAPUseCase.cs
@@ -0,0 +1,10 @@
+﻿using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public interface IRelatorioResumoPAPUseCase
+    {
+        Task<bool> Executar(FiltroRelatorioResumoPAPDto filtroRelatorioResumoPAPDto);
+    }
+}
