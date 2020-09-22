7aaa101d77958e295d1154e3880ae435366b078d
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-09-22 19:16:09 -0300

    Ajustes no salvar

--- src/SME.SGP.Infra/Dtos/PlanejamentoAnual/SalvarPlanejamentoAnualDto.cs
+++ src/SME.SGP.Infra/Dtos/PlanejamentoAnual/SalvarPlanejamentoAnualDto.cs
@@ -4,8 +4,9 @@ namespace SME.SGP.Infra
 {
     public class SalvarPlanejamentoAnualDto
     {
+        public IEnumerable<PlanejamentoAnualPeriodoEscolarDto> PeriodosEscolares { get; set; }
         public long Id { get; set; }
-        public long PeriodoEscolarId { get; set; }
-        public List<PlanejamentoAnualComponenteDto> Componentes { get; set; }
+        //public long PeriodoEscolarId { get; set; }
+        //public List<PlanejamentoAnualComponenteDto> Componentes { get; set; }
     }
 }
