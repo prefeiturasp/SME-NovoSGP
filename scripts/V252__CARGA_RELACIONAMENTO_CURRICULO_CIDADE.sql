dbb2ac71df6a733de26eaa347b3ac150fa7822e9
William Manoel dos Santos <william.santos@amcom.com.br>
2020-09-28 17:57:47 -0300

    Retirada da criação da FK

--- scripts/V252__CARGA_RELACIONAMENTO_CURRICULO_CIDADE.sql
+++ scripts/V252__CARGA_RELACIONAMENTO_CURRICULO_CIDADE.sql
@@ -5,9 +5,3 @@ insert into componente_curriculo_cidade(codigo, componente_curricular_id)
       from componente_curricular_jurema ccj 
      where codigo_eol not in (1073,1074,1072) 
 
-select
-	f_cria_fk_se_nao_existir(
-		'componente_curriculo_cidade',
-		'componente_curriculo_cidade_objetivo_aprendizagem_fk',
-		'FOREIGN KEY (componente_curricular_id) REFERENCES objetivo_aprendizagem (componente_curricular_id)'
-	);
