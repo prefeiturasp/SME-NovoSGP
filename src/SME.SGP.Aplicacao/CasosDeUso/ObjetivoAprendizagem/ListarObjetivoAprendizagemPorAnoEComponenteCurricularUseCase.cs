ad46b51e375252856c8e445eff49e8031dc97361
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-09-29 16:47:36 -0300

    24973 - Testes alterados para o novo formato

--- src/SME.SGP.Aplicacao/CasosDeUso/ObjetivoAprendizagem/ListarObjetivoAprendizagemPorAnoEComponenteCurricularUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/ObjetivoAprendizagem/ListarObjetivoAprendizagemPorAnoEComponenteCurricularUseCase.cs
@@ -2,8 +2,10 @@
 using SME.SGP.Aplicacao.Interfaces.CasosDeUso;
 using SME.SGP.Aplicacao.Queries.ComponentesCurriculares.ObterComponentesCurricularesPorAnosEModalidade;
 using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
@@ -19,7 +21,34 @@ namespace SME.SGP.Aplicacao
 
         public async Task<IEnumerable<ObjetivoAprendizagemDto>> Executar(long ano, long componenteCurricularId, bool ensinoEspecial)
         {
-            return await mediator.Send(new ListarObjetivoAprendizagemPorAnoEComponenteCurricularQuery(ano, componenteCurricularId, ensinoEspecial));
+            long[] ids;
+
+            if (componenteCurricularId == 138)
+            {
+                ids = new long[] { (ensinoEspecial ? 11 : 6) };
+            }
+            else
+            {
+                ids = await mediator.Send(new ObterJuremaIdsPorComponentesCurricularIdQuery(componenteCurricularId));
+            }
+
+            var objetivos = await mediator.Send(new ListarObjetivoAprendizagemPorAnoEComponenteCurricularQuery(ano, ids));
+
+            
+
+            foreach (var item in objetivos)
+            {
+                item.ComponenteCurricularEolId = componenteCurricularId;
+            }
+
+            IEnumerable<int> anos = Enumerable.Range(1, 9);
+            if (ensinoEspecial && !anos.Select(a => a.ToString()).Contains(ano.ToString()))
+            {
+                return objetivos.OrderBy(o => o.Ano).ThenBy(x => x.Codigo);
+            }
+
+            objetivos = objetivos.Where(x => x.Ano == ((AnoTurma)ano).Name()).ToList();
+            return objetivos.OrderBy(o => o.Codigo);
         }
     }
 }
