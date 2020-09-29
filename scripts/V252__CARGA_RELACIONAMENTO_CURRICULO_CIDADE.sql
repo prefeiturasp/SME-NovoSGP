4a349e8cae456b1b203d6d5a6c21034e2076436a
William Manoel dos Santos <william.santos@amcom.com.br>
2020-09-29 16:29:13 -0300

    ajuste no script (exclusão 1072, 1073, 1074)

--- scripts/V252__CARGA_RELACIONAMENTO_CURRICULO_CIDADE.sql
+++ scripts/V252__CARGA_RELACIONAMENTO_CURRICULO_CIDADE.sql
@@ -1,7 +1,9 @@
+
+delete from componente_curricular_jurema where codigo_eol in (1072, 1073, 1074)
+  
 delete from componente_curriculo_cidade 
 
 insert into componente_curriculo_cidade(codigo, componente_curricular_id)
     select ccj.codigo_jurema, ccj.codigo_eol 
       from componente_curricular_jurema ccj 
-     where codigo_eol not in (1073,1074,1072) 
 
