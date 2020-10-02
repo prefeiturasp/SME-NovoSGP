741609b8e971982c40df6bcfdbc6d77f381b0381
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-10-02 12:24:40 -0300

    25148 - Refatorado e adicionada validação do William

--- src/SME.SGP.Aplicacao/Commands/PlanejamentoAnual/Salvar/SalvarPlanejamentoAnualCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/PlanejamentoAnual/Salvar/SalvarPlanejamentoAnualCommandHandler.cs
@@ -2,13 +2,15 @@
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
-    public class SalvarPlanejamentoAnualCommandHandler : IRequestHandler<SalvarPlanejamentoAnualCommand, PlanejamentoAnualAuditoriaDto>
+    public class SalvarPlanejamentoAnualCommandHandler : AbstractUseCase, IRequestHandler<SalvarPlanejamentoAnualCommand, PlanejamentoAnualAuditoriaDto>, 
     {
         private readonly IRepositorioPlanejamentoAnual repositorioPlanejamentoAnual;
         private readonly IRepositorioPlanejamentoAnualPeriodoEscolar repositorioPlanejamentoAnualPeriodoEscolar;
@@ -18,7 +20,7 @@ namespace SME.SGP.Aplicacao
         public SalvarPlanejamentoAnualCommandHandler(IRepositorioPlanejamentoAnual repositorioPlanejamentoAnual,
                                                      IRepositorioPlanejamentoAnualPeriodoEscolar repositorioPlanejamentoAnualPeriodoEscolar,
                                                      IRepositorioPlanejamentoAnualComponente repositorioPlanejamentoAnualComponente,
-                                                     IRepositorioPlanejamentoAnualObjetivosAprendizagem repositorioPlanejamentoAnualObjetivosAprendizagem)
+                                                     IRepositorioPlanejamentoAnualObjetivosAprendizagem repositorioPlanejamentoAnualObjetivosAprendizagem, IMediator mediator) : base(mediator)
         {
             this.repositorioPlanejamentoAnual = repositorioPlanejamentoAnual ?? throw new System.ArgumentNullException(nameof(repositorioPlanejamentoAnual));
             this.repositorioPlanejamentoAnualPeriodoEscolar = repositorioPlanejamentoAnualPeriodoEscolar ?? throw new System.ArgumentNullException(nameof(repositorioPlanejamentoAnualPeriodoEscolar));
@@ -36,8 +38,34 @@ namespace SME.SGP.Aplicacao
                 await repositorioPlanejamentoAnual.SalvarAsync(planejamentoAnual);
             }
 
+            List<PeriodoEscolar> excecoesAtribuicao = new List<PeriodoEscolar>();
+            List<PeriodoEscolar> excecoesEmAberto = new List<PeriodoEscolar>();
+            var usuario = await mediator.Send(new ObterUsuarioLogadoQuery());
+            var turma = await mediator.Send(new ObterTurmaPorIdQuery(comando.TurmaId));
+            if (turma == null)
+                throw new NegocioException($"Turma de id [{turma.Id}] não localizada!");
+
+            foreach (var periodoEscolar in comando.PeriodosEscolares)
+            {
+                var periodo = await mediator.Send(new ObterPeriodoEscolarePorIdQuery(periodoEscolar.PeriodoEscolarId));
+                var temAtribuicao = await mediator.Send(new ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaNoPeriodoQuery(comando.ComponenteCurricularId, turma.CodigoTurma, usuario.CodigoRf, periodo.PeriodoInicio.Date, periodo.PeriodoFim.Date));
+                if (!temAtribuicao)
+                    excecoesAtribuicao.Add(periodo);
+
+                var periodoEmAberto = mediator.Send(new TurmaEmPeriodoAbertoQuery(turma, DateTime.Today, periodo.Bimestre, turma.AnoLetivo == DateTime.Today.Year)).Result;
+                if (!periodoEmAberto)
+                    excecoesEmAberto.Add(periodo);
+            }
+
+            if (excecoesAtribuicao.Any())
+                throw new NegocioException($"Você não possui atribuição.");
+
+            if (excecoesEmAberto.Any())
+                throw new NegocioException($"Algum bimestre não está com o período aberto.");
+
             foreach (var periodo in comando.PeriodosEscolares)
             {
+
                 var planejamentoAnualPeriodoEscolar = await repositorioPlanejamentoAnualPeriodoEscolar.ObterPorPlanejamentoAnualId(planejamentoAnual.Id, periodo.PeriodoEscolarId);
                 if (planejamentoAnualPeriodoEscolar == null)
                 {
