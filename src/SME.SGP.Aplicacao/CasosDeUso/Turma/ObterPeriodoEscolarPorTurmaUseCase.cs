67bd7005c18b1a95a63bb3bab654d5b21d88217e
William Manoel dos Santos <william.santos@amcom.com.br>
2020-10-02 09:59:56 -0300

    Endpoint Obter Periodo escolar por turma

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Turma/ObterPeriodoEscolarPorTurmaUseCase.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterPeriodoEscolarPorTurmaUseCase : IObterPeriodoEscolarPorTurmaUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ObterPeriodoEscolarPorTurmaUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<IEnumerable<PeriodoEscolarPorTurmaDto>> Executar(string turmaId)
+        {
+
+            var turma = await mediator.Send(new ObterTurmaComUeEDrePorCodigoQuery(turmaId));
+            if (turma == null)
+                throw new NegocioException($"Turma de codigo [{turmaId}] não localizada!");
+
+            var periodos = await mediator.Send(new ObterPeriodosEscolaresPorAnoEModalidadeTurmaQuery(turma.ModalidadeCodigo, turma.AnoLetivo, turma.Semestre));       
+            
+            return periodos?.Select(c => new PeriodoEscolarPorTurmaDto
+            {
+                Bimestre = c.Bimestre,
+                Id = c.Id,
+                Migrado = c.Migrado,
+                PeriodoAberto = mediator.Send(new TurmaEmPeriodoAbertoQuery(turma, DateTime.Today, c.Bimestre, turma.AnoLetivo == DateTime.Today.Year)).Result,
+        });
+        }
+    }
+}
