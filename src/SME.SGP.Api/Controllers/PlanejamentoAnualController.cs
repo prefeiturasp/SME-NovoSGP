601a4175797c71c8046e7d6630d728d75a394186
Rafael Losi <rafael.losi@amcom.com.br>
2020-10-05 17:39:03 -0300

    24347 - criação de novos endpoint mockados

--- src/SME.SGP.Api/Controllers/PlanejamentoAnualController.cs
+++ src/SME.SGP.Api/Controllers/PlanejamentoAnualController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Infra;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
@@ -31,5 +32,88 @@ namespace SME.SGP.Api.Controllers
         {
             return Ok(await useCase.Executar(turmaId, componenteCurricularId, periodoEscolarId));
         }
+
+        [HttpGet("turmas/{turmaId}/componentes-curriculares/{componenteCurricularId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(typeof(long), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Permissao(Permissao.PA_C, Policy = "Bearer")]
+        public async Task<IActionResult> ObterPlanejamentoId(long turmaId, long componenteCurricularId)
+        {
+            // TODO incluir UseCase para busca do planejamento anual id por turma e componente
+            return Ok(1000);
+        }
+
+
+        [HttpPost("migrar")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Permissao(Permissao.PA_I, Permissao.PA_A, Policy = "Bearer")]
+        public IActionResult Migrar(MigrarPlanejamentoAnualDto migrarPlanoAnualDto)
+        {
+            // TODO Incluir UseCase para copia do plano
+            return Ok();
+        }
+
+        [HttpGet("turmas/copia")]
+        [ProducesResponseType(typeof(IEnumerable<TurmaParaCopiaPlanoAnualDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterTurmasParaCopia([FromQuery] int turmaId, [FromQuery] long componenteCurricular)
+        {
+            // TODO Incluir UseCase de consulta de Turmas para copia por componente e turma de origem
+
+            return Ok(new List<TurmaParaCopiaPlanoAnualDto>()
+            {
+                new TurmaParaCopiaPlanoAnualDto()
+                {
+                    Id = 11,
+                    Nome = "1A",
+                    PossuiPlano = true,
+                    TurmaId = 123123
+                },
+                new TurmaParaCopiaPlanoAnualDto()
+                {
+                    Id = 22,
+                    Nome = "1B",
+                    PossuiPlano = false,
+                    TurmaId = 321321
+                },
+                new TurmaParaCopiaPlanoAnualDto()
+                {
+                    Id = 33,
+                    Nome = "1C",
+                    PossuiPlano = false,
+                    TurmaId = 123321
+                },
+            });
+        }
+
+        [HttpGet("{planejamentoAnualId}/preiodos-escolares/copia")]
+        [ProducesResponseType(typeof(IEnumerable<PlanejamentoAnualPeriodoEscolarResumoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterPeriodosEscolaresParaCopia(long planejamentoAnualId)
+        {
+            // TODO Incluir UseCase de consulta de Periodos Escolares com conteúdo para cópia
+
+            return Ok(new List<PlanejamentoAnualPeriodoEscolarResumoDto>()
+            {
+                new PlanejamentoAnualPeriodoEscolarResumoDto()
+                {
+                    Id = 11,
+                    Bimestre = 1
+                },
+                new PlanejamentoAnualPeriodoEscolarResumoDto()
+                {
+                    Id = 22,
+                    Bimestre = 2
+                },
+                new PlanejamentoAnualPeriodoEscolarResumoDto()
+                {
+                    Id = 33,
+                    Bimestre = 3
+                },
+            });
+        }
+
     }
 }
