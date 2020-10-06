2e6dc8ae9c7496fc7ffe2e0ffe20be9913115fe2
krisamcom <krishnamuti.barros@amcom.com.br>
2020-10-06 15:42:17 -0300

    Revert "Bug: 25031"
    This reverts commit 23c334bf3a6323008bc87da15ae7518bab498720.

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAbrangencia.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAbrangencia.cs
@@ -1,7 +1,6 @@
 ﻿using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Dto;
 using SME.SGP.Infra;
-using SME.SGP.Infra.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,8 +13,6 @@ namespace SME.SGP.Dominio.Interfaces
 
         void ExcluirAbrangencias(IEnumerable<long> ids);
 
-        void ExcluirAbrangenciasHistoricas(IEnumerable<long> ids);
-
         void InserirAbrangencias(IEnumerable<Abrangencia> abrangencias, string login);
 
         Task<bool> JaExisteAbrangencia(string login, Guid perfil);
@@ -23,8 +20,6 @@ namespace SME.SGP.Dominio.Interfaces
         Task<IEnumerable<AbrangenciaFiltroRetorno>> ObterAbrangenciaPorFiltro(string texto, string login, Guid perfil, bool consideraHistorico);
 
         Task<IEnumerable<AbrangenciaSinteticaDto>> ObterAbrangenciaSintetica(string login, Guid perfil, string turmaId = "", bool consideraHistorico = false);
-
-        Task<IEnumerable<AbrangenciaHistoricaDto>> ObterAbrangenciaHistoricaPorLogin(string login);
 
         Task<AbrangenciaFiltroRetorno> ObterAbrangenciaTurma(string turma, string login, Guid perfil, bool consideraHistorico = false, bool abrangenciaPermitida = false);
 
