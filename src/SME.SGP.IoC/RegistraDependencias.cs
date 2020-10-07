88945d3cac01216c555e06874a97010a8023d1b6
William Manoel dos Santos <william.santos@amcom.com.br>
2020-10-07 13:45:58 -0300

    Migrar planejamento anual

--- src/SME.SGP.IoC/RegistraDependencias.cs
+++ src/SME.SGP.IoC/RegistraDependencias.cs
@@ -468,6 +468,7 @@ namespace SME.SGP.IoC
             services.TryAddScoped<IObterComunicadoEscolaAquiUseCase, ObterComunicadoEscolaAquiUseCase>();
             services.TryAddScoped<IObterAlunosPorTurmaEAnoLetivoEscolaAquiUseCase, ObterAlunosPorTurmaEAnoLetivoEscolaAquiUseCase>();
             services.TryAddScoped<IObterComunicadosPaginadosEscolaAquiUseCase, ObterComunicadosPaginadosEscolaAquiUseCase>();
+            services.TryAddScoped<IMigrarPlanejamentoAnualUseCase, MigrarPlanejamentoAnualUseCase>();
 
         }
     }
