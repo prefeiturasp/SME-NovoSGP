aa099a612843c56063da8bb6ef9bb3ddb3cd7330
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-10-14 05:49:05 -0300

    Endpoint lista tipos de calendario por ano-letivo e modalidade

---
+++ src/SME.SGP.Aplicacao/Queries/TipoCalendario/ObterTiposCalendarioPorAnoLetivoModalidadeo/ObterTiposCalendarioPorAnoLetivoModalidadeoQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterTiposCalendarioPorAnoLetivoModalidadeoQuery : IRequest<IEnumerable<TipoCalendario>>
+    {
+        public ObterTiposCalendarioPorAnoLetivoModalidadeoQuery(int anoLetivo, Modalidade modalidade)
+        {
+            AnoLetivo = anoLetivo;
+            Modalidade = modalidade;
+        }
+
+        public int AnoLetivo { get; }
+        public Modalidade Modalidade { get; }
+    }
+}
