e02149cfc1166e6279c097e17f69e995dd3f674a
Cleiton <cleiton.dutra@amcom.com.br>
2020-10-14 16:58:27 -0300

    Refact Plano Aula - tabs com lista de componentes curriculares

--- src/SME.SGP.WebClient/src/servicos/Paginas/DiarioClasse/ServicoPlanoAula.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/DiarioClasse/ServicoPlanoAula.js
@@ -2,10 +2,13 @@ import { store } from '~/redux';
 import {
   setDadosParaSalvarPlanoAula,
   setDadosPlanoAula,
+  setExibirCardCollapsePlanoAula,
   setExibirLoaderFrequenciaPlanoAula,
+  setListaComponentesCurricularesPlanejamento,
 } from '~/redux/modulos/frequenciaPlanoAula/actions';
 import { erros } from '~/servicos/alertas';
 import api from '~/servicos/api';
+import ServicoComponentesCurriculares from '~/servicos/Paginas/ComponentesCurriculares/ServicoComponentesCurriculares';
 
 class ServicoPlanoAula {
   gerarRelatorioPlanoAula = planoAulaId => {
@@ -15,10 +18,40 @@ class ServicoPlanoAula {
 
   obterPlanoAula = async () => {
     const { dispatch } = store;
-    const state = store.getState();
 
-    const { frequenciaPlanoAula } = state;
-    const { aulaId, dadosPlanoAula } = frequenciaPlanoAula;
+    const state = store.getState();
+    const { frequenciaPlanoAula, usuario } = state;
+    const { turmaSelecionada } = usuario;
+    const {
+      aulaId,
+      dadosPlanoAula,
+      componenteCurricular,
+    } = frequenciaPlanoAula;
+
+    // Seta o componente curricular selecionado no SelectComponent quando não é REGENCIA!
+    const montarListaComponenteCurricularesPlanejamento = () => {
+      dispatch(
+        setListaComponentesCurricularesPlanejamento([componenteCurricular])
+      );
+    };
+
+    // Carrega lista de componentes para montar as TABS!
+    const obterListaComponentesCurricularesPlanejamento = () => {
+      dispatch(setExibirLoaderFrequenciaPlanoAula(true));
+      ServicoComponentesCurriculares.obterComponetensCuricularesRegencia(
+        turmaSelecionada.id
+      )
+        .then(resposta => {
+          dispatch(setListaComponentesCurricularesPlanejamento(resposta.data));
+        })
+        .catch(e => {
+          dispatch(setListaComponentesCurricularesPlanejamento([]));
+          erros(e);
+        })
+        .finally(() => {
+          dispatch(setExibirLoaderFrequenciaPlanoAula(false));
+        });
+    };
 
     if (!dadosPlanoAula) {
       dispatch(setExibirLoaderFrequenciaPlanoAula(true));
@@ -31,8 +64,23 @@ class ServicoPlanoAula {
       if (plano && plano.data) {
         dispatch(setDadosPlanoAula(plano.data));
         dispatch(setDadosParaSalvarPlanoAula(plano.data));
+
+        const ehMigrado = plano.data.migrado;
+        // TODO Validar se é necessário usar essa prop no redux!
+        // dispatch(setEhRegistroMigrado(!!ehMigrado));
+
+        // Quando for MIGRADO mostrar somente um tab com o componente curricular já selecionado!
+        if (ehMigrado) {
+          montarListaComponenteCurricularesPlanejamento();
+        } else if (componenteCurricular.regencia) {
+          // Quando for REGENCIA carregar a lista de componentes curriculares!
+          obterListaComponentesCurricularesPlanejamento();
+        } else {
+          montarListaComponenteCurricularesPlanejamento();
+        }
       } else {
-        dispatch(setDadosPlanoAula({}));
+        dispatch(setDadosPlanoAula());
+        dispatch(setExibirCardCollapsePlanoAula({ exibir: false }));
       }
     }
   };
