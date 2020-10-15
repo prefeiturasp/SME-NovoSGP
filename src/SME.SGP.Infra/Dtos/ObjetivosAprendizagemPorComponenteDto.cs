1381b99258a9564c0489c4280af1aac09dde327d
Rafael Losi <rafael.losi@amcom.com.br>
2020-10-15 19:38:20 -0300

    26131 - Inclusão de referencia componente curricular no objetivo plano aula

---
+++ src/SME.SGP.Infra/Dtos/ObjetivosAprendizagemPorComponenteDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class ObjetivosAprendizagemPorComponenteDto
+    {
+        public long ComponenteCurricularId { get; set; }
+        public List<ObjetivoAprendizagemDto> ObjetivosAprendizagem { get; set; }
+
+        public ObjetivosAprendizagemPorComponenteDto()
+        {
+            ObjetivosAprendizagem = new List<ObjetivoAprendizagemDto>();
+        }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/PlanoAula/ObjetivoAprendizagemComponenteDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class ObjetivoAprendizagemComponenteDto
+    {
+        public long Id { get; set; }
+        public long ComponenteCurricularId { get; set; }
+    }
+}
