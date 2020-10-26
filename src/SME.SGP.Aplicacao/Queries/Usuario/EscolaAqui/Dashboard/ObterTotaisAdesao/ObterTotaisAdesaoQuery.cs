6a0707a00173b10d27a25cff071ec2d73054fb08
Rodrigo Galante <rodrigo.galante@globo.com>
2020-10-26 10:03:01 -0300

    Implementação parcial de endpoint de totais de adesão do app

---
+++ src/SME.SGP.Aplicacao/Queries/Usuario/EscolaAqui/Dashboard/ObterTotaisAdesao/ObterTotaisAdesaoQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SGP.Infra.Dtos.EscolaAqui.DashboardAdesao;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterTotaisAdesaoQuery : IRequest<IEnumerable<TotaisAdesaoResultado>>
+    {
+        public string CodigoDre { get; set; }
+        public long CodigoUe { get; set; }
+
+        public ObterTotaisAdesaoQuery(string codigoDre, long codigoUe)
+        {
+            CodigoDre = codigoDre;
+            CodigoUe = codigoUe;
+        }
+    }
+}
