0043f0410e4ae13e283a1ae8f8b4b03fe086dbce
Rafael Losi <rafael.losi@amcom.com.br>
2020-10-29 20:18:09 -0300

    26955 - Criado endpoint de componentes por usuario e lista de turmas

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/ComponentesCurriculares/ObterComponentesCurricularesPorProfessorETurmasCodigosUseCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterComponentesCurricularesPorProfessorETurmasCodigosUseCase : AbstractUseCase, IObterComponentesCurricularesPorProfessorETurmasCodigosUseCase
+    {
+        public ObterComponentesCurricularesPorProfessorETurmasCodigosUseCase(IMediator mediator) : base (mediator)
+        {
+        }
+
+        public async Task<IEnumerable<DisciplinaNomeDto>> Executar(IEnumerable<string> turmasCodigos)
+        {
+            var usuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+
+            var componetesCurriculares = new List<DisciplinaNomeDto>();
+
+            foreach (var turmaCodigo in turmasCodigos)
+            {
+                var componentesNaTurma = await mediator.Send(new ObterComponentesCurricularesPorUsuarioECodigoTurmaQuery(usuarioLogado, turmaCodigo));
+                if ((componentesNaTurma != null) && componentesNaTurma.Any())
+                    componetesCurriculares.AddRange(componentesNaTurma);
+            }
+
+            return componetesCurriculares.DistinctBy(x => x.Codigo);
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterComponentesCurricularesPorUsuarioECodigoTurma/ObterComponentesCurricularesPorUsuarioECodigoTurmaQuery.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterComponentesCurricularesPorUsuarioECodigoTurmaQuery : IRequest<IEnumerable<DisciplinaNomeDto>>
+    {
+        public ObterComponentesCurricularesPorUsuarioECodigoTurmaQuery(Usuario usuarioLogado, string turmaCodigo)
+        {
+            UsuarioLogado = usuarioLogado;
+            TurmaCodigo = turmaCodigo;
+        }
+
+        public Usuario UsuarioLogado { get; set; }
+        public string TurmaCodigo { get; set; }
+    }
+
+    public class ObterComponentesCurricularesPorUsuarioECodigoTurmaQueryValidator : AbstractValidator<ObterComponentesCurricularesPorUsuarioECodigoTurmaQuery>
+    {
+        public ObterComponentesCurricularesPorUsuarioECodigoTurmaQueryValidator()
+        {
+            RuleFor(c => c.UsuarioLogado)
+            .NotEmpty()
+            .WithMessage("O usuario logado deve ser informado para consulta de componentes curriculares.");
+
+            RuleFor(c => c.TurmaCodigo)
+            .NotEmpty()
+            .WithMessage("O código da turma deve ser informado para consulta de componentes curriculares.");
+
+        }
+    }
+}
