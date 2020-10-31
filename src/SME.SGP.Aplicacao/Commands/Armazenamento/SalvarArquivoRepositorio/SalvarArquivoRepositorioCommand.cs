9b864a14f292dd0d61708b9924a0585e1a06beee
Rafael Losi <rafael.losi@amcom.com.br>
2020-10-31 16:47:46 -0300

    26539 - Criado endpoint de upload de arquivo

---
+++ src/SME.SGP.Aplicacao/Commands/Armazenamento/SalvarArquivoRepositorio/SalvarArquivoRepositorioCommand.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SalvarArquivoRepositorioCommand : IRequest<ArquivoArmazenadoDto>
+    {
+        public SalvarArquivoRepositorioCommand(string nomeArquivo, TipoArquivo tipo)
+        {
+            NomeArquivo = nomeArquivo;
+            Tipo = tipo;
+        }
+
+        public string NomeArquivo { get; set; }
+        public TipoArquivo Tipo { get; set; }
+    }
+
+    public class SalvarArquivoRepositorioCommandValidator : AbstractValidator<SalvarArquivoRepositorioCommand>
+    {
+        public SalvarArquivoRepositorioCommandValidator()
+        {
+            RuleFor(c => c.NomeArquivo)
+            .NotEmpty()
+            .WithMessage("O Nome do Arquivo deve ser informado para registrar no repositório.");
+
+            RuleFor(c => c.Tipo)
+            .NotEmpty()
+            .WithMessage("O Tipo do Arquivo deve ser informado para registrar no repositório.");
+        }
+    }
+}
