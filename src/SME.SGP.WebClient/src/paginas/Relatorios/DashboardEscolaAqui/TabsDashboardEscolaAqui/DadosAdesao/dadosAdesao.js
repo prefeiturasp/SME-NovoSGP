81f323ee3722edd21586ab6d53cce203a645ead1
Cleiton <cleiton.dutra@amcom.com.br>
2020-11-01 19:08:31 -0300

    Dashboard - Adesão - consumir endpoints

--- src/SME.SGP.WebClient/src/paginas/Relatorios/DashboardEscolaAqui/TabsDashboardEscolaAqui/DadosAdesao/dadosAdesao.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/DashboardEscolaAqui/TabsDashboardEscolaAqui/DadosAdesao/dadosAdesao.js
@@ -9,27 +9,68 @@ const DadosAdesao = props => {
   const { codigoDre, codigoUe } = props;
 
   const [dadosGraficoAdesao, setDadosGraficoAdesao] = useState([]);
-  const [exibirLoader, setExibirLoader] = useState([]);
+  const [exibirLoader, setExibirLoader] = useState(false);
   const [dataUltimaAtualizacao, setDataUltimaAtualizacao] = useState();
 
   const obterDataUltimaAtualizacao = async () => {
-    const retorno = await ServicoDashboardEscolaAqui.obterUltimaAtualizacaoPorProcesso();
-    if (retorno && retorno.data) {
-      setDataUltimaAtualizacao(moment(retorno.data).format('DD/MM/YYYY HH:mm'));
+    const retorno = await ServicoDashboardEscolaAqui.obterUltimaAtualizacaoPorProcesso(
+      'ConsolidarAdesaoEOL'
+    );
+    if (retorno && retorno.data && retorno.data.dataUltimaAtualizacao) {
+      setDataUltimaAtualizacao(
+        moment(retorno.data.dataUltimaAtualizacao).format('DD/MM/YYYY HH:mm')
+      );
     }
+  };
+
+  const mapearParaDtoGraficoPizza = dados => {
+    const totalUsuariosComCpfInvalidos = {
+      label: 'Responsáveis sem CPF ou com CPF inválido no EOL',
+      value: dados.totalUsuariosComCpfInvalidos || 0,
+      color: '#F98F84',
+    };
+
+    const totalUsuariosSemAppInstalado = {
+      label: 'Usuários que não realizaram a instalação',
+      value: dados.totalUsuariosSemAppInstalado || 0,
+      color: '#57CDBC',
+    };
+
+    const totalUsuariosPrimeiroAcesso = {
+      label: 'Usuários com primeiro acesso incompleto',
+      value: dados.totalUsuariosPrimeiroAcesso || 0,
+      color: '#EFB971',
+    };
+
+    const totalUsuariosValidos = {
+      label: 'Usuários válidos',
+      value: dados.totalUsuariosValidos || 0,
+      color: '#3982AC',
+    };
+
+    const dadosMapeados = [
+      totalUsuariosComCpfInvalidos,
+      totalUsuariosSemAppInstalado,
+      totalUsuariosPrimeiroAcesso,
+      totalUsuariosValidos,
+    ];
+
+    setDadosGraficoAdesao(dadosMapeados);
   };
 
   const obterDadosGraficoAdesao = useCallback(async () => {
     setExibirLoader(true);
     const retorno = await ServicoDashboardEscolaAqui.obterDadosGraficoAdesao(
-      codigoDre,
-      codigoUe
+      codigoDre === '-99' ? '' : codigoDre,
+      codigoUe === '-99' ? '' : codigoUe
     )
       .catch(e => erros(e))
       .finally(() => setExibirLoader(false));
 
     if (retorno && retorno.data && retorno.data.length) {
-      setDadosGraficoAdesao(retorno.data);
+      mapearParaDtoGraficoPizza(retorno.data[0]);
+    } else {
+      setDadosGraficoAdesao([]);
     }
   }, [codigoDre, codigoUe]);
 
