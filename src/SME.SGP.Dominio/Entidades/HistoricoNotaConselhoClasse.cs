fd6771745a398e7776883e65c950d96d19c6671c
williamamcom <69849971+williamamcom@users.noreply.github.com>
2020-11-03 15:47:00 -0300

    Update src/SME.SGP.Dominio/Entidades/HistoricoNotaConselhoClasse.cs
    Co-authored-by: Luiz Henrique Araújo <70153714+luizaraujoamcom@users.noreply.github.com>

--- src/SME.SGP.Dominio/Entidades/HistoricoNotaConselhoClasse.cs
+++ src/SME.SGP.Dominio/Entidades/HistoricoNotaConselhoClasse.cs
@@ -2,6 +2,7 @@
 {
     public class HistoricoNotaConselhoClasse
     {
+        public long Id {get; set;}
         public long HistoricoNotaId { get; set; }
         public long ConselhoClasseNotaId { get; set; }
     }
