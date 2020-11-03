317fdc96e0d8e90f78eedb730cfd48bda1253f52
williamamcom <69849971+williamamcom@users.noreply.github.com>
2020-11-03 15:46:39 -0300

    Update src/SME.SGP.Dominio/Entidades/HistoricoNota.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Dominio/Entidades/HistoricoNota.cs
+++ src/SME.SGP.Dominio/Entidades/HistoricoNota.cs
@@ -2,7 +2,7 @@
 {
     public class HistoricoNota : EntidadeBase
     {
-        public long HistoricoNotaId { get; set; }
+        public long Id { get; set; }
         public string NotaAnterior { get; set; }
         public string NotaNova { get; set; }
     }
