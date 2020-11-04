4c00c1c9ecabda775f72617af7fcc98e036f2819
Bruno Alevato <bruno.alevato@amcom.com.br>
2020-11-04 17:18:11 -0300

    Remocao validacao campos evento e tipo calendario

--- src/SME.SGP.WebClient/src/paginas/AcompanhamentoEscolar/Comunicados/Cadastro/index.js
+++ src/SME.SGP.WebClient/src/paginas/AcompanhamentoEscolar/Comunicados/Cadastro/index.js
@@ -379,8 +379,6 @@ const ComunicadosCadastro = ({ match }) => {
       dataEnvio: momentSchema.required('Campo obrigatório'),
       CodigoDre: Yup.string().required('Campo obrigatório'),
       CodigoUe: Yup.string().required('Campo obrigatório'),
-      eventoId: Yup.string().required('Campo obrigatório'),
-      tipoCalendarioId: Yup.string().required('Campo obrigatório'),
       dataExpiracao: momentSchema
         .required('Campo obrigatório')
         .test(
