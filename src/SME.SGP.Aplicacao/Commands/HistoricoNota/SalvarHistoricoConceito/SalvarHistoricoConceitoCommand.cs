b4d14624f26b20eb00aa95f6cafd66038eb701c0
Rafael Losi <rafael.losi@amcom.com.br>
2020-11-04 10:14:30 -0300

    18870 - Separados commands de salvar hitorico nota x conceito

---
+++ src/SME.SGP.Aplicacao/Commands/HistoricoNota/SalvarHistoricoConceito/SalvarHistoricoConceitoCommand.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SalvarHistoricoConceitoCommand : IRequest<long>
+    {
+        public SalvarHistoricoConceitoCommand(long conceitoAnteriorId, long conceitoNovoId)
+        {
+            ConceitoAnteriorId = conceitoAnteriorId;
+            ConceitoNovoId = conceitoNovoId;
+        }
+
+        public long ConceitoAnteriorId { get; set; }
+        public long ConceitoNovoId { get; set; }
+    }
+
+    public class SalvarHIstoricoConceitoCommandValidator : AbstractValidator<SalvarHistoricoConceitoCommand>
+    {
+        public SalvarHIstoricoConceitoCommandValidator()
+        {
+            RuleFor(c => c.ConceitoAnteriorId)
+            .NotEmpty()
+            .WithMessage("O conceito anterior deve ser informado para geração do histórico.");
+
+            RuleFor(c => c.ConceitoNovoId)
+            .NotEmpty()
+            .WithMessage("O conceito novo deve ser informado para geração do histórico.");
+        }
+    }
+}
--- src/SME.SGP.Aplicacao/Commands/HistoricoNota/SalvarHistoricoNota/SalvarHistoricoNotaCommand.cs
+++ src/SME.SGP.Aplicacao/Commands/HistoricoNota/SalvarHistoricoNota/SalvarHistoricoNotaCommand.cs
@@ -5,24 +5,27 @@ namespace SME.SGP.Aplicacao
 {
     public class SalvarHistoricoNotaCommand : IRequest<long>
     {
-        public SalvarHistoricoNotaCommand(double? notaAnterior, double? notaNova, long? conceitoAnteriorId, long? conceitoNovoId)
+        public SalvarHistoricoNotaCommand(double notaAnterior, double notaNova)
         {
             NotaAnterior = notaAnterior;
             NotaNova = notaNova;
-            ConceitoAnteriorId = conceitoAnteriorId;
-            ConceitoNovoId = conceitoNovoId;
         }
 
-        public double? NotaAnterior { get; set; }
-        public double? NotaNova { get; set; }
-        public long? ConceitoAnteriorId { get; set; }
-        public long? ConceitoNovoId { get; set; }
+        public double NotaAnterior { get; set; }
+        public double NotaNova { get; set; }
     }
 
     public class SalvarHistoricoNotaCommandValidator : AbstractValidator<SalvarHistoricoNotaCommand>
     {
         public SalvarHistoricoNotaCommandValidator()
         {
+            RuleFor(c => c.NotaAnterior)
+            .NotEmpty()
+            .WithMessage("A nota anteior deve ser informada para geração do histórico");
+
+            RuleFor(c => c.NotaNova)
+            .NotEmpty()
+            .WithMessage("A nota nova deve ser informada para geração do histórico");
         }
     }
 }
