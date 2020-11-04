8503d005189e80acdbfd4dcfe10e18d8ea18e074
Rodrigo Galante <rodrigo.galante@globo.com>
2020-11-04 15:40:21 -0300

    Cherry pick 6a0707a00173b10d27a25cff071ec2d73054fb08​​​​

---
+++ src/SME.SGP.Infra/Dtos/EscolaAqui/DashboardAdesao/TotaisAdesaoResultado.cs
@@ -0,0 +1,13 @@
+﻿namespace SME.SGP.Infra.Dtos.EscolaAqui.DashboardAdesao
+{
+    public class TotaisAdesaoResultado
+    {
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public int Codigoturma { get; set; }
+        public long TotalUsuariosComCpfInvalidos { get; set; }
+        public long TotalUsuariosPrimeiroAcesso { get; set; }
+        public long TotalUsuariosSemAppInstalado { get; set; }
+        public long TotalUsuariosValidos { get; set; }
+    }
+}
