574d7dd0a4e7c4b4d69a4e451740e5c7481978b3
William Manoel dos Santos <william.santos@amcom.com.br>
2020-11-04 22:36:18 -0300

    18870 - Ajustes geração de histórico

--- src/SME.SGP.Dados/Mapeamentos/HistoricoNotaMap.cs
+++ src/SME.SGP.Dados/Mapeamentos/HistoricoNotaMap.cs
@@ -7,6 +7,8 @@ namespace SME.SGP.Dados.Mapeamentos
         public HistoricoNotaMap()
         {
             ToTable("historico_nota");
+            Map(e => e.ConceitoAnteriorId).ToColumn("conceito_anterior_id");
+            Map(e => e.ConceitoNovoId).ToColumn("conceito_novo_id");
             Map(e => e.NotaAnterior).ToColumn("nota_anterior");
             Map(e => e.NotaNova).ToColumn("nota_nova");
         }
