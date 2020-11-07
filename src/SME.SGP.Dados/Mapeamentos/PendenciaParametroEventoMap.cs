45ee4e2702112167d3af800029579370728966eb
Rafael Losi <rafael.losi@amcom.com.br>
2020-11-07 18:09:21 -0300

    27528 - Modelagem Pendencias gerais

---
+++ src/SME.SGP.Dados/Mapeamentos/PendenciaParametroEventoMap.cs
@@ -0,0 +1,17 @@
+﻿using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dados
+{
+    public class PendenciaParametroEventoMap : BaseMap<PendenciaParametroEvento>
+    {
+        public PendenciaParametroEventoMap()
+        {
+            ToTable("pendencia_parametro_evento");
+            Map(c => c.PendenciaId).ToColumn("pendencia_id");
+            Map(c => c.ParametroSistemaId).ToColumn("parametro_sistema_id");
+        }
+    }
+}
