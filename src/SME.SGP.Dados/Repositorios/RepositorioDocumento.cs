44eb1cf6bcd6b78a54b515a7bde708d41d6db3d6
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-09 11:52:12 -0300

    27477 - Criada estrutura com entidades, mapeamentos, repositórios e script de inserção

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioDocumento.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioDocumento : RepositorioBase<Documento>, IRepositorioDocumento
+    {
+        public RepositorioDocumento(ISgpContext conexao) : base(conexao)
+        {
+        }
+
+}
---
+++ src/SME.SGP.Dominio/Entidades/Documento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SME.SGP.Dominio
+{
+    public class Documento : EntidadeBase
+    {
+        public long ClassificacaoDocumentoId { get; set; }
+        public ClassificacaoDocumento ClassificacaoDocumento { get; set; }
+        public long ArquivoId { get; set; }
+        public Arquivo Arquivo { get; set; }
+
+    }
+}
