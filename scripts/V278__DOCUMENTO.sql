eda40de269d31ed712975d3ea89a775b716acbc1
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-09 11:52:12 -0300

    27477 - Criada estrutura com entidades, mapeamentos, repositórios e script de inserção

---
+++ scripts/V278__DOCUMENTO.sql
@@ -0,0 +1,63 @@
+CREATE TABLE IF NOT EXISTS public.tipo_documento(
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	descricao varchar(60) NULL,
+	CONSTRAINT tipo_documento_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.classificacao_documento(
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	descricao varchar(10) NULL,
+	tipo_documento_id int8 NOT NULL,
+	CONSTRAINT classificacao_documento_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.documento(
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	usuario_rf varchar(200) NULL,
+	alterado_por varchar(200) NULL,
+	alterado_rf varchar(200) NULL,
+	alterado_em timestamp NULL,
+	criado_por varchar(200) NOT NULL,
+	criado_rf varchar(200) NOT NULL,
+	criado_em timestamp NOT null,
+	arquivo_id int8 NOT NULL,
+	classificacao_documento_id int8 NOT NULL,
+	CONSTRAINT documento_pk PRIMARY KEY (id)
+);
+
+select
+	f_cria_fk_se_nao_existir(
+		'classificacao_documento',
+		'classificacao_documento_tipo_documento',
+		'FOREIGN KEY (tipo_documento_id) REFERENCES tipo_documento (id)'
+	);
+
+select
+	f_cria_fk_se_nao_existir(
+		'documento',
+		'documento_classificacao_documento',
+		'FOREIGN KEY (classificacao_documento_id) REFERENCES classificacao_documento (id)'
+	);
+
+select
+	f_cria_fk_se_nao_existir(
+		'documento',
+		'documento_arquivo',
+		'FOREIGN KEY (arquivo_id) REFERENCES arquivo (id)'
+	);
+
+CREATE INDEX classificacao_documento_tipo_documento_idx ON public.classificacao_documento USING btree (tipo_documento_id);
+CREATE INDEX documento_classificacao_documento_idx ON public.documento USING btree (classificacao_documento_id);
+CREATE INDEX documento_arquivo_idx ON public.documento USING btree (arquivo_id);
+
+insert into public.tipo_documento (descricao) values ('Plano de Trabalho');
+insert into public.tipo_documento (descricao) values ('Documentos');
+
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('PAEE', 1);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('PAP', 1);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('POA', 1);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('POED', 1);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('POEI', 1);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('POSL', 1);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('PEA', 2);
+insert into public.classificacao_documento (descricao, tipo_documento_id) values ('PPP', 2);
---
+++ src/SME.SGP.Dados/Mapeamentos/DocumentoMap.cs
@@ -0,0 +1,14 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class DocumentoMap : BaseMap<Documento>
+    {
+        public DocumentoMap()
+        {
+            ToTable("documento");
+            Map(a => a.ClassificacaoDocumentoId).ToColumn("classificacao_documento_id");
+            Map(a => a.ArquivoId).ToColumn("arquivo_id");
+        }
+    }
+}
