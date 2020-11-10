a3342c5b0f0b40561f3525bda5571f284671e856
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-10 11:48:04 -0300

    27486 - Adicionado UeId para verificar o documento

--- src/SME.SGP.Infra/Dtos/Armazenamento/VerificarUsuarioDocumentoDto.cs
+++ src/SME.SGP.Infra/Dtos/Armazenamento/VerificarUsuarioDocumentoDto.cs
@@ -11,14 +11,16 @@ namespace SME.SGP.Infra
 
         }
 
-        public VerificarUsuarioDocumentoDto(long tipoDocumentoId, long classificacaoId, long usuarioId)
+        public VerificarUsuarioDocumentoDto(long tipoDocumentoId, long classificacaoId, long usuarioId, long ueId)
         {
             TipoDocumentoId = tipoDocumentoId;
             ClassificacaoId = classificacaoId;
             UsuarioId = usuarioId;
+            UeId = ueId;
         }
         public long TipoDocumentoId { get; set; }
         public long ClassificacaoId { get; set; }
         public long UsuarioId { get; set; }
+        public long UeId { get; set; }
     }
 }
