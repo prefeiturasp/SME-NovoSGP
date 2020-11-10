f113f25f8ede09e0b4246af99d45ba6b2447d26a
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-10 11:47:09 -0300

    27486 - Alterado script para adicionar o campo ueId

--- scripts/V278__DOCUMENTO.sql
+++ scripts/V278__DOCUMENTO.sql
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS public.documento(
 	criado_rf varchar(200) NOT NULL,
 	criado_em timestamp NOT null,
 	arquivo_id int8 NOT NULL,
+	ue_id int8 NOT NULL,
 	classificacao_documento_id int8 NOT NULL,
 	CONSTRAINT documento_pk PRIMARY KEY (id)
 );
@@ -53,10 +54,18 @@ select
 		'FOREIGN KEY (usuario_id) REFERENCES usuario (id)'
 	);
 
+select
+	f_cria_fk_se_nao_existir(
+		'documento',
+		'documento_ue',
+		'FOREIGN KEY (ue_id) REFERENCES ue (id)'
+	);
+
 CREATE INDEX classificacao_documento_tipo_documento_idx ON public.classificacao_documento USING btree (tipo_documento_id);
 CREATE INDEX documento_classificacao_documento_idx ON public.documento USING btree (classificacao_documento_id);
 CREATE INDEX documento_arquivo_idx ON public.documento USING btree (arquivo_id);
 CREATE INDEX documento_usuario_idx ON public.documento USING btree (usuario_id);
+CREATE INDEX documento_ue_idx ON public.documento USING btree (ue_id);
 
 insert into public.tipo_documento (descricao) values ('Plano de Trabalho');
 insert into public.tipo_documento (descricao) values ('Documentos');
