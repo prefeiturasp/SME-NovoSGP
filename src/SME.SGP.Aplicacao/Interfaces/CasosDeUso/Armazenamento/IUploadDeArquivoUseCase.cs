4326734e2b9072612623b22c3f6e06e4f57e8da2
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-11 11:32:32 -0300

    27506 - Criado endpoint para realizar o upload de documentos, juntamente com o filtro de tipo de arquivo

--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/Armazenamento/IUploadDeArquivoUseCase.cs
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/Armazenamento/IUploadDeArquivoUseCase.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
-    public interface IUploadDeArquivoUseCase : IUseCase<IFormFile, Guid>
+    public interface IUploadDeArquivoUseCase
     {
+        Task<Guid> Executar(IFormFile file, string tipoConteudo = "");
     }
 }
