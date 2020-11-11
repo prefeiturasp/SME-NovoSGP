0520ec8445897ab1873e758f1c9c9b99cbbedb58
Rafael Losi <rafael.losi@amcom.com.br>
2020-11-11 09:39:00 -0300

    27542 - Geração de pendência de calendario ue

---
+++ src/SME.SGP.Aplicacao/Commands/PendenciaCalendarioUe/SalvarPendenciaCalendarioUe/SalvarPendenciaCalendarioUeCommand.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SalvarPendenciaCalendarioUeCommand : IRequest<bool>
+    {
+        public SalvarPendenciaCalendarioUeCommand(long tipoCalendarioId, long ueId, string descricao)
+        {
+            TipoCalendarioId = tipoCalendarioId;
+            UeId = ueId;
+            Descricao = descricao;
+        }
+
+        public long TipoCalendarioId { get; set; }
+        public long UeId { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class SalvarPendenciaCalendarioUeCommandValidator : AbstractValidator<SalvarPendenciaCalendarioUeCommand>
+    {
+        public SalvarPendenciaCalendarioUeCommandValidator()
+        {
+            RuleFor(c => c.TipoCalendarioId)
+            .NotEmpty()
+            .WithMessage("O id do tipo de calendário deve ser informado para geração de pendência.");
+
+            RuleFor(c => c.UeId)
+            .NotEmpty()
+            .WithMessage("O id da UE deve ser informado para geração de pendência.");
+
+            RuleFor(c => c.Descricao)
+            .NotEmpty()
+            .WithMessage("A descrição deve ser informada para geração de pendência.");
+        }
+
+    }
+}
