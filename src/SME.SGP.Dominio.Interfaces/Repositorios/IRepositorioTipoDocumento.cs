0954aef72762eb4d68d06ac94e74efe1987bb086
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-11-12 01:27:07 -0300

    27489 - Adicionada novo filtro de listagem de tipos de arquivos

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioTipoDocumento.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioTipoDocumento.cs
@@ -7,5 +7,6 @@ namespace SME.SGP.Dominio.Interfaces
     public interface IRepositorioTipoDocumento
     {
         Task<IEnumerable<TipoDocumentoDto>> ListarDocumentosPorPerfil(string[] perfil);
+        Task<IEnumerable<TipoDocumentoDto>> ListarTipoDocumentoClassificacao();
     }
 }
