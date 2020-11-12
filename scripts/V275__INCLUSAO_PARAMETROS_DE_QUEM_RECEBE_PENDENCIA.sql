0da557245ea3ddc9a6645d025cc937b2535126a0
William Manoel dos Santos <william.santos@amcom.com.br>
2020-11-12 14:46:18 -0300

    Correção de script para gerar mais de um registro por tipo

--- scripts/V275__INCLUSAO_PARAMETROS_DE_QUEM_RECEBE_PENDENCIA.sql
+++ scripts/V275__INCLUSAO_PARAMETROS_DE_QUEM_RECEBE_PENDENCIA.sql
@@ -1,60 +1,34 @@
 insert  into  
 	public.parametros_sistema (nome,descricao,valor,ano,ativo,criado_em,criado_por, criado_rf,tipo)
 select
-	'GerarPendenciaAulasDiasNaoLetivos','Gerar pendencia de aulas em dias não letivos para o professor','Professor', 2020, true, now(),'Carga Inicial','Carga Inicial',35
-where
-	not exists(
-	select 	1
-	from public.parametros_sistema 
-	where tipo = 35);
+	'GerarPendenciaAulasDiasNaoLetivos','Gerar pendencia de aulas em dias não letivos para o professor','Professor', 2020, true, now(),'Carga Inicial','Carga Inicial',35;
 
 	
 insert  into  
 	public.parametros_sistema (nome,descricao,valor,ano,ativo,criado_em,criado_por, criado_rf,tipo)
 select
-	'GerarPendenciaAulasDiasNaoLetivos','Gerar pendencia de aulas em dias não letivos para CP','CP', 2020, true, now(),'Carga Inicial','Carga Inicial',35
-where
-	not exists(
-	select 	1
-	from public.parametros_sistema 
-	where tipo = 35);
-	
+	'GerarPendenciaAulasDiasNaoLetivos','Gerar pendencia de aulas em dias não letivos para CP','CP', 2020, true, now(),'Carga Inicial','Carga Inicial',35;
+
+
 insert  into  
 	public.parametros_sistema (nome,descricao,valor,ano,ativo,criado_em,criado_por, criado_rf,tipo)
 select
-	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o CP','CP', 2020, true, now(),'Carga Inicial','Carga Inicial',36
-where
-	not exists(
-	select 	1
-	from public.parametros_sistema 
-	where tipo = 36);
-	
+	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o CP','CP', 2020, true, now(),'Carga Inicial','Carga Inicial',36;
+
+
 insert  into  
 	public.parametros_sistema (nome,descricao,valor,ano,ativo,criado_em,criado_por, criado_rf,tipo)
 select
-	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o AD','AD', 2020, true, now(),'Carga Inicial','Carga Inicial',36
-where
-	not exists(
-	select 	1
-	from public.parametros_sistema 
-	where tipo = 36);
-	
+	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o AD','AD', 2020, true, now(),'Carga Inicial','Carga Inicial',36;
+
+
 insert  into  
 	public.parametros_sistema (nome,descricao,valor,ano,ativo,criado_em,criado_por, criado_rf,tipo)
 select
-	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o Diretor','Diretor', 2020, true, now(),'Carga Inicial','Carga Inicial',36
-where
-	not exists(
-	select 	1
-	from public.parametros_sistema 
-	where tipo = 36);
-	
+	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o Diretor','Diretor', 2020, true, now(),'Carga Inicial','Carga Inicial',36;
+
+
 insert  into  
 	public.parametros_sistema (nome,descricao,valor,ano,ativo,criado_em,criado_por, criado_rf,tipo)
 select
-	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o ADM UE','ADM UE', 2020, true, now(),'Carga Inicial','Carga Inicial',36
-where
-	not exists(
-	select 	1
-	from public.parametros_sistema 
-	where tipo = 36);
+	'GerarPendenciaDiasLetivosInsuficientes','Gerar pendências de dias letivos insuficientes para o ADM UE','ADM UE', 2020, true, now(),'Carga Inicial','Carga Inicial',36;
