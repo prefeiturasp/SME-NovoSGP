1b91efdd17f9859b721d9b1b73d44112aabf54b0
Rafael Losi <rafael.losi@amcom.com.br>
2020-11-13 11:50:54 -0300

    11658 - Alterado validação de documento existente para o usuario descartando o Documento atual

--- src/SME.SGP.Aplicacao/Queries/Armazenamento/VerificaUsuarioPossuiArquivo/VerificaUsuarioPossuiArquivoQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/Armazenamento/VerificaUsuarioPossuiArquivo/VerificaUsuarioPossuiArquivoQuery.cs
@@ -9,13 +9,15 @@ namespace SME.SGP.Aplicacao
 {
     public class VerificaUsuarioPossuiArquivoQuery : IRequest<bool>
     {
-        public VerificaUsuarioPossuiArquivoQuery(long tipoDocumentoId, long classificacaoId, long usuarioId, long ueId)
+        public VerificaUsuarioPossuiArquivoQuery(long tipoDocumentoId, long classificacaoId, long usuarioId, long ueId, long documentoId)
         {
             TipoDocumentoId = tipoDocumentoId;
             ClassificacaoId = classificacaoId;
             UsuarioId = usuarioId;
             UeId = ueId;
+            DocumentoId = documentoId;
         }
+        public long DocumentoId { get; set; }
         public long TipoDocumentoId { get; set; }
         public long ClassificacaoId { get; set; }
         public long UsuarioId { get; set; }
@@ -26,6 +28,10 @@ namespace SME.SGP.Aplicacao
     {
         public VerificaUsuarioPossuiArquivoQueryValidator()
         {
+            RuleFor(c => c.DocumentoId)
+            .NotEmpty()
+            .WithMessage("O id do documento deve ser informado.");
+
             RuleFor(c => c.TipoDocumentoId)
             .NotEmpty()
             .WithMessage("O id do tipo de documento deve ser informado.");
--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioDocumento.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioDocumento.cs
@@ -7,7 +7,7 @@ namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioDocumento : IRepositorioBase<Documento>
     {
-        Task<bool> ValidarUsuarioPossuiDocumento(long tipoDocumentoId, long classificacaoId, long usuarioId, long ueId);
+        Task<bool> ValidarUsuarioPossuiDocumento(long tipoDocumentoId, long classificacaoId, long usuarioId, long ueId, long documentoId);
         Task<PaginacaoResultadoDto<DocumentoDto>> ObterPorUeTipoEClassificacaoPaginada(long ueId, long tipoDocumentoId, long classificacaoId, Paginacao paginacao);
         Task<bool> RemoverReferenciaArquivo(long documentoId, long arquivoId);
         Task<bool> ExcluirDocumentoPorId(long id);
