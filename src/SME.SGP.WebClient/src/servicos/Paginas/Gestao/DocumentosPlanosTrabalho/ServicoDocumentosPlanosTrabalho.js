686d390fda67cd4c8c99cdaadd716f96f7445edd
Cleiton <cleiton.dutra@amcom.com.br>
2020-11-13 00:52:13 -0300

    Plano de trabalho - consumir endpoints - add rota editando registro - validacoes

--- src/SME.SGP.WebClient/src/servicos/Paginas/Gestao/DocumentosPlanosTrabalho/ServicoDocumentosPlanosTrabalho.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Gestao/DocumentosPlanosTrabalho/ServicoDocumentosPlanosTrabalho.js
@@ -3,23 +3,26 @@ import api from '~/servicos/api';
 const urlPadrao = 'v1/armazenamento/documentos';
 
 class ServicoDocumentosPlanosTrabalho {
-  obterArquivoParaDownload = codigoArquivo => {
-    return api.get(`${urlPadrao}/${codigoArquivo}`, {
-      responseType: 'arraybuffer',
-    });
-  };
-
   obterTiposDeDocumentos = () => {
     return api.get(`${urlPadrao}/tipos`);
   };
 
-  salvarDocumento = params => {
-    const url = `${urlPadrao}`;
-    return api.post(url, params);
+  salvarDocumento = (params, documentoId) => {
+    if (documentoId) {
+      return api.put(`${urlPadrao}/${documentoId}`, {
+        codigoArquivo: params.arquivoCodigo,
+      });
+    }
+
+    return api.post(`${urlPadrao}`, params);
   };
 
   obterDocumento = documentoId => {
     return api.get(`${urlPadrao}/${documentoId}`);
+  };
+
+  excluirDocumento = documentoId => {
+    return api.delete(`${urlPadrao}/${documentoId}`);
   };
 }
 
