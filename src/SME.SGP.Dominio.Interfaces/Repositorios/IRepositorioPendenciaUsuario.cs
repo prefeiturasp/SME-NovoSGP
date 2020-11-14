55c0542ed13c010d0afcc32c18f1ed9771cdff21
William Manoel dos Santos <william.santos@amcom.com.br>
2020-11-13 22:58:15 -0300

    Gerar a relação de usuário com a pendência

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioPendenciaUsuario.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioPendenciaUsuario.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SME.SGP.Dominio.Interfaces
+﻿namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioPendenciaUsuario : IRepositorioBase<PendenciaUsuario>
     {
