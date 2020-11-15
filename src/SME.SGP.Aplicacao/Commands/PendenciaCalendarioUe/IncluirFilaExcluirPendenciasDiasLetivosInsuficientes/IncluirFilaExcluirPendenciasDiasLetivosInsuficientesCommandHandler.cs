7f9991a7023fa4d5e5a52f2b0edfdecf408a50da
Rafael Losi <rafael.losi@amcom.com.br>
2020-11-15 15:16:54 -0300

    removido injecao do ServicoFila dos servicos Workflow e Evento

---
+++ src/SME.SGP.Aplicacao/Commands/PendenciaCalendarioUe/IncluirFilaExcluirPendenciasDiasLetivosInsuficientes/IncluirFilaExcluirPendenciasDiasLetivosInsuficientesCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class IncluirFilaExcluirPendenciasDiasLetivosInsuficientesCommandHandler : IRequestHandler<IncluirFilaExcluirPendenciasDiasLetivosInsuficientesCommand, bool>
+    {
+        private readonly IServicoFila servicoFila;
+
+        public IncluirFilaExcluirPendenciasDiasLetivosInsuficientesCommandHandler(IServicoFila servicoFila)
+        {
+            this.servicoFila = servicoFila ?? throw new ArgumentNullException(nameof(servicoFila));
+        }
+
+        public Task<bool> Handle(IncluirFilaExcluirPendenciasDiasLetivosInsuficientesCommand request, CancellationToken cancellationToken)
+        {
+            servicoFila.PublicaFilaWorkerSgp(new PublicaFilaSgpDto(RotasRabbit.RotaExecutaExclusaoPendenciaParametroEvento,
+                                                       new ExcluirPendenciasDiasLetivosInsuficientesCommand(request.TipoCalendarioId, request.DreCodigo, request.UeCodigo),
+                                                       Guid.NewGuid(),
+                                                       request.Usuario));
+
+            return Task.FromResult(true);
+        }
+    }
+}
