691d7646613a4e89001a1169597645f9eca476a2
Cleiton <cleiton.dutra@amcom.com.br>
2020-11-14 21:21:56 -0300

    [Escola aqui] Dashboard - Total de comunicados

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/EscolaAqui/DashboardEscolaAqui/ServicoDashboardEscolaAqui.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/EscolaAqui/DashboardEscolaAqui/ServicoDashboardEscolaAqui.js
@@ -24,105 +24,14 @@ class ServicoDashboardEscolaAqui {
     return api.get(url);
   };
 
-  obterDadosGraficoTotalComunicadosEnviados = (
-    codigoDre,
-    codigoUe,
-    anoLetivo
-  ) => {
-    console.log(`DRE: ${codigoDre}`);
-    console.log(`UE: ${codigoUe}`);
-    console.log(`ANO LETIVO: ${anoLetivo}`);
-
-    const mock = { totalExpirados: 200, totalVigentes: 500 };
-
-    return new Promise(resolve => {
-      return setTimeout(() => {
-        return resolve({ status: 200, data: mock });
-      }, 2000);
-    });
+  obterComunicadosTotaisSme = (codigoDre, codigoUe, anoLetivo) => {
+    const url = `${urlPadrao}/comunicados/totais?anoLetivo=${anoLetivo}&codigoDre=${codigoDre}&codigoUe=${codigoUe}`;
+    return api.get(url);
   };
 
-  obterDadosGraficoTotalComunicadosPorDRE = (
-    codigoDre,
-    codigoUe,
-    anoLetivo
-  ) => {
-    console.log(`DRE: ${codigoDre}`);
-    console.log(`UE: ${codigoUe}`);
-    console.log(`ANO LETIVO: ${anoLetivo}`);
-
-    const mock = [
-      {
-        nomeDre: 'DRE 01',
-        comunicadosvigentes: 20,
-        comunicadosExpirados: 30,
-      },
-      {
-        nomeDre: 'DRE 02',
-        comunicadosvigentes: 10,
-        comunicadosExpirados: 50,
-      },
-      {
-        nomeDre: 'DRE 03',
-        comunicadosvigentes: 60,
-        comunicadosExpirados: 70,
-      },
-      {
-        nomeDre: 'DRE 04',
-        comunicadosvigentes: 50,
-        comunicadosExpirados: 70,
-      },
-      {
-        nomeDre: 'DRE 05',
-        comunicadosvigentes: 80,
-        comunicadosExpirados: 90,
-      },
-      {
-        nomeDre: 'DRE 06',
-        comunicadosvigentes: 80,
-        comunicadosExpirados: 100,
-      },
-      {
-        nomeDre: 'DRE 07',
-        comunicadosvigentes: 500,
-        comunicadosExpirados: 300,
-      },
-      {
-        nomeDre: 'DRE 08',
-        comunicadosvigentes: 60,
-        comunicadosExpirados: 70,
-      },
-      {
-        nomeDre: 'DRE 09',
-        comunicadosvigentes: 80,
-        comunicadosExpirados: 400,
-      },
-      {
-        nomeDre: 'DRE 10',
-        comunicadosvigentes: 500,
-        comunicadosExpirados: 700,
-      },
-      {
-        nomeDre: 'DRE 11',
-        comunicadosvigentes: 600,
-        comunicadosExpirados: 400,
-      },
-      {
-        nomeDre: 'DRE 12',
-        comunicadosvigentes: 120,
-        comunicadosExpirados: 158,
-      },
-      {
-        nomeDre: 'DRE 13',
-        comunicadosvigentes: 167,
-        comunicadosExpirados: 70,
-      },
-    ];
-    return new Promise(resolve => {
-      return setTimeout(() => {
-        return resolve({ status: 200, data: mock });
-      }, 2000);
-    });
+  obterComunicadosTotaisAgrupadosPorDre = anoLetivo => {
+    const url = `${urlPadrao}/comunicados/totais/agrupados?anoLetivo=${anoLetivo}`;
+    return api.get(url);
   };
 }
 
