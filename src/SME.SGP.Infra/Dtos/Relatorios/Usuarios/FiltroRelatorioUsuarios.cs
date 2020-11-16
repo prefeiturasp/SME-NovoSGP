a3ee9ce374f264ede43e632bca945fe802e2a391
Rafael Losi <rafael.losi@amcom.com.br>
2020-11-16 00:30:46 -0300

    27563 - Criado estrutura para chamar o relatório de usuários

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/Usuarios/FiltroRelatorioUsuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroRelatorioUsuarios
+    {
+        public int DreId { get; set; }
+        public int UeId { get; set; }
+        public string UsuarioRf { get; set; }
+        public string[] Perfis { get; set; }
+        public int[] Situacoes { get; set; }
+        public int DiasSemAcesso { get; set; }
+        public bool ExibirHistorico { get; set; }
+    }
+}
