be48e0be22e98b99a0871a96d4299e2a87df88f6
wagnersantos-amcom <wagner.santos@amcom.com.br>
2020-11-18 14:53:28 -0300

    adicionando url dinamica em sondagem

--- src/SME.SGP.WebClient/src/paginas/Sondagem/sondagem.js
+++ src/SME.SGP.WebClient/src/paginas/Sondagem/sondagem.js
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { obterUrlSondagem } from '~/servicos/variaveis';
 
 const Sondagem = ({ history }) => {
   useEffect(() => {
-    window.open(`http://localhost:61540/sgp?redirect=/Relatorios/Sondagem`);
-    history.push('/');
+    (async () => {
+      const url = await obterUrlSondagem();
+      window.open(`${url}/sgp?redirect=/Relatorios/Sondagem`);
+      history.push('/');
+    })();
   }, [history]);
 
   return <div>sondagem</div>;
+};
+
+Sondagem.propTypes = {
+  history: PropTypes.func.isRequired,
 };
 
 export default Sondagem;
