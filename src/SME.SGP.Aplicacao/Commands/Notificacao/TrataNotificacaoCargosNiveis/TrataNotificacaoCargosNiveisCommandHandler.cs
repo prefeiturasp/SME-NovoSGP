f4d839563f5c940ca3cdfd4639cfbb11c714022a
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-11-19 17:22:17 -0300

    Inicio do tratamento das mensagens

--- src/SME.SGP.Aplicacao/Commands/Notificacao/TrataNotificacaoCargosNiveis/TrataNotificacaoCargosNiveisCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Notificacao/TrataNotificacaoCargosNiveis/TrataNotificacaoCargosNiveisCommandHandler.cs
@@ -7,12 +7,21 @@ namespace SME.SGP.Aplicacao
 {
     public class TrataNotificacaoCargosNiveisCommandHandler : IRequestHandler<TrataNotificacaoCargosNiveisCommand, bool>
     {
+        private readonly IMediator mediator;
+
+        public TrataNotificacaoCargosNiveisCommandHandler(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+        }
         public async Task<bool> Handle(TrataNotificacaoCargosNiveisCommand request, CancellationToken cancellationToken)
         {
 
-            foreach (var notificacaoParaTratar in request.Notificacoes.OrderBy(a => a.UEId))
+            foreach (var notificacaoParaTratar in request.Notificacoes.GroupBy(a => a.UEId))
             {
-
+                var cargosIdsDaUe = notificacaoParaTratar.Select(a => a.Cargo).Distinct();
+                var funcionariosCargosDaUe = await mediator.Send(new ObterFuncionariosCargosPorUeCargosQuery(notificacaoParaTratar.Key, cargosIdsDaUe));
+                
+                //Verificar no EOL rfs e cargos na UE
             }
 
             return true;
