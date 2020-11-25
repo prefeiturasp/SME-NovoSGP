6ecd01b01e5557581d3867c7025cd0a372d2ac24
eliton-amcom <eliton.santos@amcom.com.br>
2020-11-25 16:52:31 -0300

    alterar controller

--- src/SME.SGP.Api/Controllers/RelatorioController.cs
+++ src/SME.SGP.Api/Controllers/RelatorioController.cs
@@ -74,5 +74,14 @@ namespace SME.SGP.Api.Controllers
         {
             return Ok(await relatorioUseCase.Executar(filtro));
         }
+
+        [HttpPost("atribuicoes/cjs")]
+        [ProducesResponseType(typeof(Boolean), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public async Task<IActionResult> Gerar(FiltroRelatorioAtribuicaoCJDto filtros, [FromServices] IRelatorioAtribuicaoCJUseCase relatorioAtribuicaoCJUseCase)
+        {
+            return Ok(await relatorioAtribuicaoCJUseCase.Executar(filtros));
+        }
     }
 }
