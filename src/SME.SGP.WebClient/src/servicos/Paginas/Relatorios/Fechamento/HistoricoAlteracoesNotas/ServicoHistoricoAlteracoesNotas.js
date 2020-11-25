71cf6a3ef32896222c826fbec72a63793a1c1c31
Cleiton <cleiton.dutra@amcom.com.br>
2020-11-25 00:10:30 -0300

    Nota do bimestre - Relatório de alterações - validacoes

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/Fechamento/HistoricoAlteracoesNotas/ServicoHistoricoAlteracoesNotas.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/Fechamento/HistoricoAlteracoesNotas/ServicoHistoricoAlteracoesNotas.js
@@ -2,7 +2,7 @@ import api from '~/servicos/api';
 
 class ServicoHistoricoAlteracoesNotas {
   gerar = dados => {
-    return api.post(`v1/relatorios/historico-alteracao-notasa`, dados);
+    return api.post(`v1/relatorios/historico-alteracao-notas`, dados);
   };
 }
 
