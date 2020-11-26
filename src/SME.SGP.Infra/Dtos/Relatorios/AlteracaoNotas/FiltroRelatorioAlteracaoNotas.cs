5c8192853128b840077b6af9244bdcd2f649a18c
William Manoel dos Santos <william.santos@amcom.com.br>
2020-11-26 00:17:08 -0300

    Ajuste na rota e no dto

--- src/SME.SGP.Infra/Dtos/Relatorios/AlteracaoNotas/FiltroRelatorioAlteracaoNotas.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/AlteracaoNotas/FiltroRelatorioAlteracaoNotas.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace SME.SGP.Infra
 {
-    public class FiltroRelatorioAlteracaoNotasBimestre
+    public class FiltroRelatorioAlteracaoNotas
     {
         public string CodigoDre { get; set; }
         public string CodigoUe { get; set; }        
         public long AnoLetivo { get; set; }
         public Modalidade ModalidadeTurma { get; set; }
         public int Semestre { get; set; }
-        public long Turma { get; set; }
+        public List<long> Turma { get; set; }
         public IEnumerable<long> ComponentesCurriculares { get; set; }
         public List<int> Bimestres { get; set; }
         public TipoAlteracaoNota TipoAlteracaoNota { get; set; }
