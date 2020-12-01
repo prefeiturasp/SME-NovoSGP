5fc4d61889f1f7a5c6bafa80f575949776376a8c
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-12-01 19:48:54 -0300

    18043 - Criada uma versão que lê pelo cargo e ue direto do EOL

---
+++ src/SME.SGP.Aplicacao/Queries/Funcionario/ObterFuncionariosPorCargo/ObterFuncionariosPorCargoQuery.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterFuncionariosPorCargoQuery : IRequest<IEnumerable<FuncionarioDto>>
+    {
+        public ObterFuncionariosPorCargoQuery(Cargo cargo)
+        {
+            Cargo = cargo;
+        }
+
+        public Cargo Cargo { get; set; }
+    }
+
+    public class ObterFuncionariosPorCargoQueryValidator : AbstractValidator<ObterFuncionariosPorCargoQuery>
+    {
+        public ObterFuncionariosPorCargoQueryValidator()
+        {
+            RuleFor(c => c.Cargo)
+               .NotEmpty()
+               .WithMessage("O cargo deve ser informado.");
+        }
+    }
+}
