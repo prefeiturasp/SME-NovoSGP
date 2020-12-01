075fb7f94050153268f9eb5e9e2a659d47d2efde
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-12-01 16:07:29 -0300

    28676 - Correções nos commands de notificação

--- src/SME.SGP.Aplicacao/Commands/Notificacao/ExecutaNotificacaoPeriodoFechamentoIniciando/ExecutaNotificacaoPeriodoFechamentoIniciandoCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Notificacao/ExecutaNotificacaoPeriodoFechamentoIniciando/ExecutaNotificacaoPeriodoFechamentoIniciandoCommandHandler.cs
@@ -22,20 +22,39 @@ namespace SME.SGP.Aplicacao
 
         public async Task<bool> Handle(ExecutaNotificacaoPeriodoFechamentoIniciandoCommand request, CancellationToken cancellationToken)
         {
-
-            var funcionarios = await mediator.Send(new ObterFuncionariosQuery(request.PeriodoIniciandoBimestre.PeriodoFechamento.Dre.CodigoDre, request.PeriodoIniciandoBimestre.PeriodoFechamento.Ue.CodigoUe, "", ""));
-            await EnviarNotificacoes(funcionarios, request.PeriodoIniciandoBimestre.PeriodoEscolar, request.PeriodoIniciandoBimestre, request.PeriodoIniciandoBimestre.PeriodoFechamento.Ue);
+            await NotificarUe(request.PeriodoFechamentoBimestre.PeriodoFechamento.Ue, request.PeriodoFechamentoBimestre.PeriodoEscolar, request.PeriodoFechamentoBimestre);
             return true;
         }
 
-        private async Task EnviarNotificacoes(IEnumerable<UsuarioEolRetornoDto> funcionarios, PeriodoEscolar periodoEscolar, PeriodoFechamentoBimestre periodoFechamentoBimestre, Ue ue)
+        private async Task NotificarUe(Ue ue, PeriodoEscolar periodoEscolar, PeriodoFechamentoBimestre periodoFechamentoBimestre)
         {
-            var titulo = $"Início do período de fechamento do {periodoEscolar.Bimestre}º bimestre - {ue.TipoEscola.ShortName()} {ue.Nome} ({ue.Dre.Abreviacao})";
-            var mensagem = @$"O fechamento do <b>{periodoEscolar.Bimestre}º bimestre</b> na <b>{ue.TipoEscola.ShortName()} {ue.Nome} ({ue.Dre.Abreviacao})</b> irá iniciar no dia <b>{periodoFechamentoBimestre.InicioDoFechamento.Date}</b>.";
+            var descricaoUe = $"{ue.TipoEscola.ShortName()} {ue.Nome} ({ue.Dre.Abreviacao})";
+            var titulo = $"Início do período de fechamento do {periodoEscolar.Bimestre}º bimestre - {descricaoUe}";
+            var mensagem = @$"O fechamento do <b>{periodoEscolar.Bimestre}º bimestre</b> na <b>{descricaoUe}</b> irá iniciar no dia <b>{periodoFechamentoBimestre.InicioDoFechamento.Date}</b>.";
 
-            foreach (var funcionario in funcionarios)
-                await mediator.Send(new NotificarUsuarioCommand(titulo, mensagem, funcionario.CodigoRf, NotificacaoCategoria.Aviso, NotificacaoTipo.Calendario));
+            await mediator.Send(new EnviarNotificacaoCommand(titulo, mensagem, NotificacaoCategoria.Aviso, NotificacaoTipo.Calendario, ObterCargosGestaoEscola(), ue.Dre.CodigoDre, ue.CodigoUe));
 
+            var usuarios = await ObterUsuariosProfessoresEAdms(ue);
+            if (usuarios != null && usuarios.Any())
+                await mediator.Send(new EnviarNotificacaoUsuariosCommand(titulo, mensagem, NotificacaoCategoria.Aviso, NotificacaoTipo.Calendario, usuarios, ue.Dre.CodigoDre, ue.CodigoUe));
+        }
+        private Cargo[] ObterCargosGestaoEscola()
+          => new[] { Cargo.CP, Cargo.AD, Cargo.Diretor };
+
+        private async Task<IEnumerable<long>> ObterUsuariosProfessoresEAdms(Ue ue)
+        {
+            var usuarios = await mediator.Send(new ObterFuncionariosDreOuUePorPerfisQuery(ue.CodigoUe, ObterPerfis()));
+
+            var listaUsuarios = new List<long>();
+            foreach (var usuario in usuarios)
+                listaUsuarios.Add(await mediator.Send(new ObterUsuarioIdPorRfOuCriaQuery(usuario)));
+
+            return listaUsuarios;
+        }
+
+        private IEnumerable<Guid> ObterPerfis()
+        {
+            return new List<Guid>() { Perfis.PERFIL_PROFESSOR, Perfis.PERFIL_CJ, Perfis.PERFIL_PROFESSOR_INFANTIL, Perfis.PERFIL_CJ_INFANTIL, Perfis.PERFIL_ADMUE };
         }
     }
 }
