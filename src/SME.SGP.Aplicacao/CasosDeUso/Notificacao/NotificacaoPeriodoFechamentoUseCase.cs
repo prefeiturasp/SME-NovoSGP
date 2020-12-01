e2615f8e1b8e20c1df43a5fc15bd159f971d6e5d
Rafael Losi <rafael.losi@amcom.com.br>
2020-12-01 00:53:47 -0300

    28680 - Notificação Periodo Fechamento não cadastrado

--- src/SME.SGP.Aplicacao/CasosDeUso/Notificacao/NotificacaoPeriodoFechamentoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Notificacao/NotificacaoPeriodoFechamentoUseCase.cs
@@ -18,10 +18,32 @@ namespace SME.SGP.Aplicacao
             var ano = DateTime.Now.Year;
 
             await VerificaNotificacaoPeriodoFechamentoUe(ano);
+            await VerificaNotificacaoPeriodoFechamentoDre(ano);
 
             return true;
         }
 
+        private async Task VerificaNotificacaoPeriodoFechamentoDre(int ano)
+        {
+            var parametros = await mediator.Send(new ObterParametrosSistemaPorTipoEAnoQuery(Dominio.TipoParametroSistema.DiasNotificacaoPeriodoFechamentoDre, ano));
+            foreach (var parametro in parametros)
+            {
+                var diasParaFechamento = int.Parse(parametro.Valor);
+
+                var periodoIniciando = await ObterPeriodosFechamento(ModalidadeTipoCalendario.FundamentalMedio, DateTime.Now.Date.AddDays(diasParaFechamento));
+                if (periodoIniciando != null)
+                    await NotificarDre(ModalidadeTipoCalendario.FundamentalMedio, periodoIniciando);
+
+                periodoIniciando = await ObterPeriodosFechamento(ModalidadeTipoCalendario.EJA, DateTime.Now.Date.AddDays(diasParaFechamento));
+                if (periodoIniciando != null)
+                    await NotificarDre(ModalidadeTipoCalendario.EJA, periodoIniciando);
+            }
+        }
+
+        private async Task NotificarDre(ModalidadeTipoCalendario modalidade, PeriodoFechamentoBimestre periodoIniciando)
+        {
+            await mediator.Send(new NotificarPeriodoFechamentoDreCommand(modalidade, periodoIniciando));
+        }
         private async Task VerificaNotificacaoPeriodoFechamentoUe(int ano)
         {
             var parametros = await mediator.Send(new ObterParametrosSistemaPorTipoEAnoQuery(Dominio.TipoParametroSistema.DiasNotificacaoPeriodoFechamentoUe, ano));
@@ -32,6 +54,10 @@ namespace SME.SGP.Aplicacao
                 var periodoIniciando = await ObterPeriodosFechamento(ModalidadeTipoCalendario.FundamentalMedio, DateTime.Now.Date.AddDays(diasParaFechamento));
                 if (periodoIniciando != null)
                     await NotificarUe(ModalidadeTipoCalendario.FundamentalMedio, periodoIniciando);
+
+                periodoIniciando = await ObterPeriodosFechamento(ModalidadeTipoCalendario.EJA, DateTime.Now.Date.AddDays(diasParaFechamento));
+                if (periodoIniciando != null)
+                    await NotificarUe(ModalidadeTipoCalendario.EJA, periodoIniciando);
             }
         }
 
---
+++ src/SME.SGP.Aplicacao/Queries/Funcionario/ObterFuncionariosDreOuUePorPerfis/ObterFuncionariosDreOuUePorPerfisQuery.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterFuncionariosDreOuUePorPerfisQuery : IRequest<IEnumerable<string>>
+    {
+
+        public ObterFuncionariosDreOuUePorPerfisQuery(string codigoDreUe, IEnumerable<Guid> perfis)
+        {
+            CodigoDreUe = codigoDreUe;
+            Perfis = perfis;
+        }
+
+        public string CodigoDreUe { get; set; }
+        public IEnumerable<Guid> Perfis { get; set; }
+    }
+
+    public class ObterFuncionariosDreOuUePorPerfisQueryValidator : AbstractValidator<ObterFuncionariosDreOuUePorPerfisQuery>
+    {
+        public ObterFuncionariosDreOuUePorPerfisQueryValidator()
+        {
+            RuleFor(c => c.CodigoDreUe)
+               .NotEmpty()
+               .WithMessage("O código da Dre ou Ue deve ser informado para consulta de funcionários.");
+
+            RuleFor(c => c.Perfis)
+               .Must(a => a.Any())
+               .WithMessage("Os perfis devem ser informados para consulta de funcionários.");
+        }
+    }
+}
