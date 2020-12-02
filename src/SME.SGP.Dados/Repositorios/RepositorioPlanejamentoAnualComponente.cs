5e560e95155734887fdfd3f09a2ceedc40b4fe85
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-12-02 10:23:16 -0300

    Modificando exclusão física para  lógica nos planejamentos anuais

--- src/SME.SGP.Dados/Repositorios/RepositorioPlanejamentoAnualComponente.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioPlanejamentoAnualComponente.cs
@@ -14,7 +14,7 @@ namespace SME.SGP.Dados.Repositorios
 
         public async Task<PlanejamentoAnualComponente> ObterPorPlanejamentoAnualPeriodoEscolarId(long componenteCurricularId, long id)
         {
-            var sql = @"select * from planejamento_anual_componente where planejamento_anual_periodo_escolar_id = @id and componente_curricular_id = @componenteCurricularId";
+            var sql = @"select * from planejamento_anual_componente where planejamento_anual_periodo_escolar_id = @id and componente_curricular_id = @componenteCurricularId and excluido = false";
             var planejamento = await database.Conexao.QueryFirstOrDefaultAsync<PlanejamentoAnualComponente>(sql, new { id, componenteCurricularId });
             return planejamento;
         }
@@ -25,11 +25,5 @@ namespace SME.SGP.Dados.Repositorios
             await database.Conexao.ExecuteAsync(sql, new { id });
         }
 
-        public async Task RemoverPorPlanejamentoAnual(long id)
-        {
-            var sql = @"delete from planejamento_anual_componente where planejamento_anual_id = @id;
-                        delete from planejamento_anual_componente where planejamento_anual_id = @id;";
-            await database.Conexao.ExecuteAsync(sql, new { id });
-        }
     }
 }
