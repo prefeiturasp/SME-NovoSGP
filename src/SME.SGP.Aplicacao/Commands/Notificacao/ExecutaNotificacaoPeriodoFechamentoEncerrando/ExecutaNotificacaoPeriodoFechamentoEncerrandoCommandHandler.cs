0d38be7512c9744c41f774e0b99c1367bd911a40
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2020-12-03 14:53:18 -0300

    29811 - Correção para workflow

--- src/SME.SGP.Aplicacao/Commands/Notificacao/ExecutaNotificacaoPeriodoFechamentoEncerrando/ExecutaNotificacaoPeriodoFechamentoEncerrandoCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Notificacao/ExecutaNotificacaoPeriodoFechamentoEncerrando/ExecutaNotificacaoPeriodoFechamentoEncerrandoCommandHandler.cs
@@ -34,18 +34,23 @@ namespace SME.SGP.Aplicacao
 
         private async Task EnviarNotificacaoProfessores(IEnumerable<Turma> turmas, PeriodoEscolar periodoEscolar, PeriodoFechamentoBimestre periodoFechamentoBimestre, Ue ue)
         {
-            
             var descricaoUe = $"{ue.TipoEscola.ShortName()} {ue.Nome} ({ue.Dre.Abreviacao})";
             var titulo = $"Término do período de fechamento do  {periodoEscolar.Bimestre}º bimestre - {descricaoUe}";
             var mensagem = $@"O fechamento do <b>{periodoEscolar.Bimestre}º bimestre</b> na <b>{descricaoUe}</b> irá encerrar no dia <b>{periodoFechamentoBimestre.FinalDoFechamento.Date:dd/MM/yyyy}</b>.
                 <br/><br/>Após esta data o sistema será bloqueado para edições neste bimestre.";
 
-
             var professores = await ObterProfessores(turmas);
             if (professores != null && professores.Any())
                 await mediator.Send(new EnviarNotificacaoUsuariosCommand(titulo, mensagem, NotificacaoCategoria.Aviso, NotificacaoTipo.Calendario, professores, ue.Dre.CodigoDre, ue.CodigoUe));
 
+            await mediator.Send(new EnviarNotificacaoCommand(titulo, mensagem.ToString(), NotificacaoCategoria.Aviso, NotificacaoTipo.Calendario, ObterCargosGestaoEscola(),
+             periodoFechamentoBimestre.PeriodoFechamento.Ue.Dre.CodigoDre,
+             periodoFechamentoBimestre.PeriodoFechamento.Ue.CodigoUe));
         }
+
+
+        private Cargo[] ObterCargosGestaoEscola()
+        => new[] { Cargo.CP, Cargo.AD, Cargo.Diretor };
 
 
         private async Task<IEnumerable<long>> ObterProfessores(IEnumerable<Turma> turmas)
@@ -58,7 +63,7 @@ namespace SME.SGP.Aplicacao
 
                 foreach (var professor in professores)
                 {
-                    if(professor != "")
+                    if (professor != "")
                         listaUsuarios.Add(await mediator.Send(new ObterUsuarioIdPorRfOuCriaQuery(professor)));
                 }
             }
