dcfbdcb87a63d02f927286198e9b9f49d98db829
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-03 12:08:14 -0300

    ajuste na consulta

--- src/SME.SGP.Dados/Repositorios/RepositorioComunicado.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioComunicado.cs
@@ -446,7 +446,7 @@ namespace SME.SGP.Dados.Repositorios
 
         public async Task<bool> VerificaExistenciaComunicadoParaEvento(long eventoId)
         {
-            var sql = $@"select count(id) from comunicado where not excluido and data_expiracao >= current_date+1 and evento_id = @eventoId";
+            var sql = $@"select count(id) from comunicado where not excluido and data_expiracao >= current_date and evento_id = @eventoId";
             var parametros = new { eventoId };
             var quantidadeComunicadosComEvento = await database.QuerySingleAsync<int>(sql, parametros);
             return (quantidadeComunicadosComEvento > 0 ? true : false);
