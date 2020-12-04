358ec6c784e1dabd2f02212e6e64d23397ea31a8
Robson <robson.silva@amcom.com.br>
2020-12-04 15:28:35 -0300

    Bug: 29765 Ajuste nas validações ao selecionar ou salvar plano anual de componentes curriculares do tipo território do saber.

--- src/SME.SGP.Aplicacao/Consultas/ConsultasFrequencia.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasFrequencia.cs
@@ -132,7 +132,7 @@ namespace SME.SGP.Aplicacao
             return alunosAusentesDto;
         }
 
-        public async Task<FrequenciaDto> ObterListaFrequenciaPorAula(long aulaId)
+        public async Task<FrequenciaDto> ObterListaFrequenciaPorAula(long aulaId, long? disciplinaId = null)
         {
             var aula = repositorioAula.ObterPorId(aulaId);
             if (aula == null)
@@ -170,7 +170,7 @@ namespace SME.SGP.Aplicacao
                                                     TipoParametroSistema.PercentualFrequenciaAlerta,
                                                     bimestre.PeriodoInicio.Year));
 
-            var disciplinaAula = await repositorioComponenteCurricular.ObterDisciplinasPorIds(new long[] { Convert.ToInt64(aula.DisciplinaId) });
+            var disciplinaAula = await repositorioComponenteCurricular.ObterDisciplinasPorIds(new long[] { disciplinaId.HasValue ? disciplinaId.Value : Convert.ToInt64(aula.DisciplinaId) });
 
             if (disciplinaAula == null || disciplinaAula.ToList().Count <= 0)
                 throw new NegocioException("Disciplina da aula não encontrada");
