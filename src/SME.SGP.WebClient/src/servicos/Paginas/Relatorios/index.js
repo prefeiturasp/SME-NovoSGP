52e3cbdfee0bccc4e75e2a78653e19813bc1c0e6
wagnersantos-amcom <wagner.santos@amcom.com.br>
2020-12-04 11:28:04 -0300

    criação da tela de relatório atribuição cj

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/index.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/index.js
@@ -1,1 +1,2 @@
 export { default as ResumosGraficosPAPServico } from './PAP/ResumosGraficos';
+export { default as ServicoRelatorioAtribuicoes } from './Gestao/Atribuicoes/ServicoRelatorioAtribuicoes';
