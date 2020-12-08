d35c0c08e8421147e23db0f3aa477b0dfdedc1bb
William Manoel dos Santos <william.santos@amcom.com.br>
2020-12-08 09:51:08 -0300

    Criação de endpoint, permissão no menu e use case e chamada ao rabbit

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/LeituraComunicados/FiltroRelatorioLeituraComunicados.cs
@@ -0,0 +1,24 @@
+﻿using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroRelatorioLeituraComunicados
+    {
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public long AnoLetivo { get; set; }
+        public Modalidade ModalidadeTurma { get; set; }
+        public int Semestre { get; set; }
+        public int Ano { get; set; }
+        public List<long> Turma { get; set; }
+        public List<long> Grupo { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public string Comunicado { get; set; }
+        public bool ListarResponsavelEstudante { get; set; }
+        public bool ListarComunicadosExpirados { get; set; }
+        public string NomeUsuario { get; set; }
+    }
+}
