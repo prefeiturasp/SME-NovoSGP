7b23e1c5a66d3e86d2bb58a6e25a42851a71f2d4
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-10 19:28:24 -0300

    Inclusão de novos campos e correção de filtro por GruposIds

--- src/SME.SGP.Infra/Dtos/EscolaAqui/ComunicadosFiltro/ComunicadoParaFiltroDaDashboardDto.cs
+++ src/SME.SGP.Infra/Dtos/EscolaAqui/ComunicadosFiltro/ComunicadoParaFiltroDaDashboardDto.cs
@@ -7,5 +7,9 @@ namespace SME.SGP.Infra.Dtos.EscolaAqui.ComunicadosFiltro
         public long Id { get; set; }
         public string Titulo { get; set; }
         public DateTime DataEnvio { get; set; }
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public string Modalidade { get; set; }
+
     }
 }
