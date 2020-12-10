cd9703bb9a5c83f51aba478c3c5dff5a299dc47a
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-10 17:44:36 -0300

    inclusão de novos campos DRE, UE e Modalidade

--- src/SME.SGP.Infra/Dtos/EscolaAqui/ComunicadosFiltro/ComunicadoParaFiltroDaDashboardDto.cs
+++ src/SME.SGP.Infra/Dtos/EscolaAqui/ComunicadosFiltro/ComunicadoParaFiltroDaDashboardDto.cs
@@ -6,6 +6,9 @@ namespace SME.SGP.Infra.Dtos.EscolaAqui.ComunicadosFiltro
     {
         public long Id { get; set; }
         public string Titulo { get; set; }
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public string Modalidade { get; set; }
         public DateTime DataEnvio { get; set; }
     }
 }
