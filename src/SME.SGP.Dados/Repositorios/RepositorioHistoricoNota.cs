914237f519a455dcc9074075dadad74d60386d49
Rafael Losi <rafael.losi@amcom.com.br>
2020-12-13 02:45:38 -0300

    30384 - Correção do usuario de criação da alteração de nota quando esta passa por aprovação WF

--- src/SME.SGP.Dados/Repositorios/RepositorioHistoricoNota.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioHistoricoNota.cs
@@ -1,4 +1,5 @@
-﻿using SME.SGP.Dominio;
+﻿using System.Threading.Tasks;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
@@ -8,7 +9,19 @@ namespace SME.SGP.Dados.Repositorios
     {
         public RepositorioHistoricoNota(ISgpContext conexao) : base(conexao)
         {
+        }
 
+        public override async Task<long> SalvarAsync(HistoricoNota entidade)
+        {
+            if (string.IsNullOrEmpty(entidade.CriadoRF))
+                entidade.CriadoRF = database.UsuarioLogadoRF;
+            if (string.IsNullOrEmpty(entidade.CriadoPor))
+                entidade.CriadoPor = database.UsuarioLogadoNomeCompleto;
+
+            entidade.Id = (long)(await database.Conexao.InsertAsync(entidade));
+            await AuditarAsync(entidade.Id, "I");
+
+            return entidade.Id;
         }
     }
 }
