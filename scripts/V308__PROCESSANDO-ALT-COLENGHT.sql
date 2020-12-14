0d28c30e54292fb46bc248f1036d6d92d07d315e
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-12-14 14:20:35 -0300

    Alterando tabela de processamento para suportar disciplina_id maiores

---
+++ scripts/V308__PROCESSANDO-ALT-COLENGHT.sql
@@ -0,0 +1,5 @@
+DROP INDEX processo_executando_disciplina_idx ;
+
+ALTER TABLE public.processo_executando ALTER COLUMN disciplina_id TYPE varchar(20);
+
+CREATE INDEX processo_executando_disciplina_idx ON public.processo_executando USING btree (disciplina_id);
