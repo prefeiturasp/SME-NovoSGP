20b0ab84136e2676ba869521f5b93d848fb65d6b
Robson <robson.silva@amcom.com.br>
2020-12-16 12:05:39 -0300

    Bug: 30721 Ajuste no teste unitário relacionado a obtenção de aula.

--- teste/SME.SGP.Aplicacao.Teste/Queries/ObterDatasAulasPorProfessorEComponenteQueryHandlerTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Queries/ObterDatasAulasPorProfessorEComponenteQueryHandlerTeste.cs
@@ -34,39 +34,56 @@ namespace SME.SGP.Aplicacao.Teste.Queries
         [Fact]
         public async Task Deve_Obter_Datas_Aulas()
         {
-            // Arrange
-            //repositorioTurma.Setup(x => x.ObterPorCodigo(It.IsAny<string>()))
-            //    .ReturnsAsync(new Dominio.Turma()
-            //    {
-            //        AnoLetivo = 2020,
-            //        CodigoTurma = "123",
-            //    });
+            //Arrange
+            repositorioTurma.Setup(x => x.ObterPorCodigo(It.IsAny<string>()))
+                .ReturnsAsync(new Dominio.Turma()
+                {
+                    AnoLetivo = 2020,
+                    CodigoTurma = "123",
+                });
 
-            //repositorio.Setup(x => x.ObterDatasDeAulasPorAnoTurmaEDisciplina(It.IsAny<long>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
-            //    .Returns(new List<Aula>()
-            //    {
-            //        new Aula() { DataAula = new DateTime(2020, 08, 05), Id = 1 },
-            //        new Aula() { DataAula = new DateTime(2020, 08, 05), Id = 2 },
-            //        new Aula() { DataAula = new DateTime(2020, 08, 06), Id = 3 },
-            //    });
+            var aula1 = new Aula() { DataAula = new DateTime(2020, 08, 05), Id = 1 };
+            var aula2 = new Aula() { DataAula = new DateTime(2020, 08, 05), Id = 2 };
+            var aula3 = new Aula() { DataAula = new DateTime(2020, 08, 06), Id = 3 };
 
-            //mediator.Setup(x => x.Send(It.IsAny<ObterTipoCalendarioIdPorTurmaQuery>(), It.IsAny<CancellationToken>()))
-            //    .ReturnsAsync(1);
+            repositorio.Setup(x => x.ObterDatasDeAulasPorAnoTurmaEDisciplina(It.IsAny<long>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .Returns(new List<Aula>()
+                {
+                    aula1, aula2, aula3
+                });
 
-            //mediator.Setup(x => x.Send(It.IsAny<ObterPeriodosEscolaresPorTipoCalendarioQuery>(), It.IsAny<CancellationToken>()))
-            //    .ReturnsAsync(new List<PeriodoEscolar>()
-            //    {
-            //        new PeriodoEscolar() { Id = 1, Bimestre = 1},
-            //    });
+            repositorioAula.Setup(x => x.ObterPorId(1))
+                .Returns(aula1);
 
-            //// Act
-            //var datasAulas = await query.Handle(new ObterDatasAulasPorProfessorEComponenteQuery("123", "123", "1105", false, false), new CancellationToken());
+            repositorioAula.Setup(x => x.ObterPorId(2))
+                .Returns(aula2);
 
-            //// Assert
-            //Assert.NotNull(datasAulas);
+            repositorioAula.Setup(x => x.ObterPorId(3))
+                .Returns(aula3);
 
-            //Assert.True(datasAulas.Count() == 2, "O retorno deve conter duas datas");
-            //Assert.True(datasAulas.First().Aulas.Count() == 2, "O primeiro dia deve conter duas aulas");
+            mediator.Setup(x => x.Send(It.IsAny<ObterTipoCalendarioIdPorTurmaQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(1);
+
+            mediator.Setup(x => x.Send(It.IsAny<ObterPeriodosEscolaresPorTipoCalendarioQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<PeriodoEscolar>()
+                {
+                    new PeriodoEscolar() { Id = 1, Bimestre = 1},
+                });
+
+            var usuario = new Usuario();
+            usuario.DefinirPerfis(new List<PrioridadePerfil>());
+            usuario.DefinirPerfilAtual(Perfis.PERFIL_DIRETOR);
+            mediator.Setup(x => x.Send(It.IsAny<ObterUsuarioLogadoQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(usuario);
+
+            // Act
+            var datasAulas = await query.Handle(new ObterDatasAulasPorProfessorEComponenteQuery("123", "123", "1105", false, false), new CancellationToken());
+
+            // Assert
+            Assert.NotNull(datasAulas);
+
+            Assert.True(datasAulas.Count() == 2, "O retorno deve conter duas datas");
+            Assert.True(datasAulas.First().Aulas.Count() == 2, "O primeiro dia deve conter duas aulas");
         }
     }
 }
