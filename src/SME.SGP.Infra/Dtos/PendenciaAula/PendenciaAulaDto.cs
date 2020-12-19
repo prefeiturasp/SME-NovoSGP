78924a497cc80f8b22e61ffd231e0640ad2a8f74
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-12-19 18:13:32 -0300

    Ajuste verificar pendências da aula

--- src/SME.SGP.Infra/Dtos/PendenciaAula/PendenciaAulaDto.cs
+++ src/SME.SGP.Infra/Dtos/PendenciaAula/PendenciaAulaDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SME.SGP.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,30 @@ namespace SME.SGP.Infra
 {
     public class PendenciaAulaDto
     {
+        public long AulaId { get; set; }
+        public bool PossuiPendenciaDiarioBordo { get; set; }
+        public bool PossuiPendenciaAtividadeAvaliativa { get; set; }
+        public bool PossuiPendenciaFrequencia { get; set; }
+        public bool PossuiPendenciaPlanoAula { get; set; }
+
+        public long[] RetornarTipoPendecias()
+        {
+            var tipoPendencias = new List<TipoPendenciaAula>();
+
+            if (PossuiPendenciaAtividadeAvaliativa)
+                tipoPendencias.Add(TipoPendenciaAula.Avaliacao);
+
+            if (PossuiPendenciaDiarioBordo)
+                tipoPendencias.Add(TipoPendenciaAula.DiarioBordo);
+
+            if (PossuiPendenciaFrequencia)
+                tipoPendencias.Add(TipoPendenciaAula.Frequencia);
+
+            if (PossuiPendenciaPlanoAula)
+                tipoPendencias.Add(TipoPendenciaAula.PlanoAula);
+
+            return Array.ConvertAll(tipoPendencias.ToArray(), value => (long)value);
+        }
         public DateTime DataAula { get; set; }
         public string Motivo { get; set; }
     }
