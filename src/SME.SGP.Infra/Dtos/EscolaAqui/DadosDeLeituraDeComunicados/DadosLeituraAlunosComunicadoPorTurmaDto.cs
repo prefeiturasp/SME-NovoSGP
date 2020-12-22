197e3308aac417329db69382ada1346d47e5e877
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-22 17:07:35 -0300

    Implementação de enpoint de dados de leitura para alunos de uma turma

---
+++ src/SME.SGP.Infra/Dtos/EscolaAqui/DadosDeLeituraDeComunicados/DadosLeituraAlunosComunicadoPorTurmaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SME.SGP.Infra.Dtos.EscolaAqui.DadosDeLeituraDeComunicados
+{
+    public class DadosLeituraAlunosComunicadoPorTurmaDto
+    {
+        public short NumeroChamada { get; set; }
+        public string NomeAluno { get; set; }
+        public string NomeResponsavel { get; set; }
+        public string TelefoneResponsavel { get; set; }
+        public short PossueApp { get; set; }
+        public bool LeuComunicado { get; set; }
+        public DateTime? DataLeitura { get; set; }
+    }
+}
