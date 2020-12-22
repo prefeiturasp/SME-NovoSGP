56680a0486ee833a4b9a24f1288aae5a71592372
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-22 20:18:21 -0300

    inclusao de propriedades de situação do aluno

--- src/SME.SGP.Infra/Dtos/EscolaAqui/DadosDeLeituraDeComunicados/DadosLeituraAlunosComunicadoPorTurmaDto.cs
+++ src/SME.SGP.Infra/Dtos/EscolaAqui/DadosDeLeituraDeComunicados/DadosLeituraAlunosComunicadoPorTurmaDto.cs
@@ -8,8 +8,10 @@ namespace SME.SGP.Infra.Dtos.EscolaAqui.DadosDeLeituraDeComunicados
         public string NomeAluno { get; set; }
         public string NomeResponsavel { get; set; }
         public string TelefoneResponsavel { get; set; }
-        public short PossueApp { get; set; }
+        public bool PossueApp { get; set; }
         public bool LeuComunicado { get; set; }
         public DateTime? DataLeitura { get; set; }
+        public DateTime? DataSituacaoAluno { get; set; }
+        public string SituacaoAluno { get; set; }
     }
 }
