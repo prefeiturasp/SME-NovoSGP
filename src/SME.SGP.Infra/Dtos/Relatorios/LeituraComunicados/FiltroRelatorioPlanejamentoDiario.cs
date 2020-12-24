f114c4d6b539f10b087cc49ffab79d56ffacae52
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-24 11:07:22 -0300

    Implementação parcial do relatório de planejamento diario

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/LeituraComunicados/FiltroRelatorioPlanejamentoDiario.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroRelatorioPlanejamentoDiario 
+    {
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public long AnoLetivo { get; set; }
+        public Modalidade ModalidadeTurma { get; set; }
+        public int Semestre { get; set; }
+        public string CodigoTurma { get; set; }
+        public int ComponenteCurricular { get; set; }
+        public int Bimestre { get; set; }
+        public bool ListarDataFutura { get; set; }
+        public bool ExibirDetalhamento { get; set; }
+    }
+}
