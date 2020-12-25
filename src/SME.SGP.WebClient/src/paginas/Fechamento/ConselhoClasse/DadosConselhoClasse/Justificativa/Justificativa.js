39e7d0895007674b03c2c39da875c719bb456320
Cleiton <cleiton.dutra@amcom.com.br>
2020-12-25 18:22:10 -0300

    add joditEditor - conselho de classe

--- src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/Justificativa/Justificativa.js
+++ src/SME.SGP.WebClient/src/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/Justificativa/Justificativa.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 import { Auditoria, Colors, Loader } from '~/componentes';
 import Button from '~/componentes/button';
-import Editor from '~/componentes/editor/editor';
+import JoditEditor from '~/componentes/jodit-editor/joditEditor';
 import {
   setNotaConceitoPosConselhoAtual,
   setSalvouJustificativa,
@@ -56,9 +56,10 @@ const Justificativa = props => {
 
   const [carregandoSessao, setCarregandoSessao] = useState(false);
 
-  const salvarJustificativa = () => {
+  const salvarJustificativa = async () => {
     setCarregandoSessao(true);
-    servicoSalvarConselhoClasse.salvarNotaPosConselho(turma);
+    await servicoSalvarConselhoClasse.salvarNotaPosConselho(turma);
+    setCarregandoSessao(false);
     dispatch(setSalvouJustificativa(true));
   };
 
@@ -111,8 +112,9 @@ const Justificativa = props => {
                 {form => (
                   <Form>
                     <fieldset className="mt-3">
-                      <Editor
+                      <JoditEditor
                         form={form}
+                        value={form.values.justificativa}
                         name="justificativa"
                         id="justificativa"
                         desabilitar={
