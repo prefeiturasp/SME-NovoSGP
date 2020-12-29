8c5e7764b5d687725ecef61af692a8d034e57afc
Carlos Dias <carlos.dias@amcom.com.br>
2020-12-29 10:25:02 -0300

    CM - Ajuste da máscara AB#31449

--- src/SME.SGP.Api/Controllers/OcorrenciaController.cs
+++ src/SME.SGP.Api/Controllers/OcorrenciaController.cs
@@ -22,9 +22,9 @@ namespace SME.SGP.Api.Controllers
         {
             var resultado = new List<OcorrenciaListagemDto>
             {
-                new OcorrenciaListagemDto { AlunoOcorrencia = "Carlos Augusto Ferreira Dias (1234567)", DataOcorrencia = DateTime.Today.ToString("dd/MM/yyy"), Id = 1, Titulo = "Briga na escola"},
-                new OcorrenciaListagemDto { AlunoOcorrencia = "Marcos Lobo (1234567)", DataOcorrencia = DateTime.Today.ToString("dd/MM/yyy"), Id = 2, Titulo = "Acidente em sala de aula"},
-                new OcorrenciaListagemDto { AlunoOcorrencia = "3 alunos envolvidos", DataOcorrencia = DateTime.Today.ToString("dd/MM/yyy"), Id = 3, Titulo = "Vandalismo no pátio"},
+                new OcorrenciaListagemDto { AlunoOcorrencia = "Carlos Augusto Ferreira Dias (1234567)", DataOcorrencia = DateTime.Today.ToString("dd/MM/yyyy"), Id = 1, Titulo = "Briga na escola"},
+                new OcorrenciaListagemDto { AlunoOcorrencia = "Marcos Lobo (1234567)", DataOcorrencia = DateTime.Today.ToString("dd/MM/yyyy"), Id = 2, Titulo = "Acidente em sala de aula"},
+                new OcorrenciaListagemDto { AlunoOcorrencia = "3 alunos envolvidos", DataOcorrencia = DateTime.Today.ToString("dd/MM/yyyy"), Id = 3, Titulo = "Vandalismo no pátio"},
             };
 
             return await Task.FromResult(Ok(resultado));
