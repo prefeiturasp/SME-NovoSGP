228622ee294d486bac4bb4d34733bcd060ff2a2d
Carlos Dias <carlos.dias@amcom.com.br>
2020-12-29 16:42:34 -0300

    CM - Adição do controller e endpoint para consulta de tipos de ocorrência.

---
+++ src/SME.SGP.Api/Controllers/OcorrenciaTipoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos.Ocorrencias.Tipos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/ocorrencias/tipos")]
+    //[Authorize("Bearer")]
+    public class OcorrenciaTipoController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OcorrenciaTipoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        // O permissionamento será adicionado em uma task separada
+        public async Task<IActionResult> Get()
+        {
+            var resultado = new List<OcorrenciaTipoDto>
+            {
+                new OcorrenciaTipoDto { Descricao = "Briga", Id = 1},
+                new OcorrenciaTipoDto { Descricao = "Vandalismo", Id = 2},
+                new OcorrenciaTipoDto { Descricao = "Acidente", Id = 3}
+            };
+
+            return await Task.FromResult(Ok(resultado));
+        }
+    }
+}
