f3ddf86e2b3d13433c9f32ea81d68ae1169d11fb
Carlos Dias <carlos.dias@amcom.com.br>
2020-12-29 09:56:03 -0300

    CM - Alteração do retorno de data para string AB#31449

--- src/SME.SGP.Infra/Dtos/Ocorrencias/Listagens/OcorrenciaListagemDto.cs
+++ src/SME.SGP.Infra/Dtos/Ocorrencias/Listagens/OcorrenciaListagemDto.cs
@@ -5,7 +5,7 @@ namespace SME.SGP.Infra.Dtos.Ocorrencias.Listagens
     public class OcorrenciaListagemDto
     {
         public long Id { get; set; }
-        public DateTime DataOcorrencia { get; set; }
+        public string DataOcorrencia { get; set; }
         public string Titulo { get; set; }
         public string AlunoOcorrencia { get; set; }
     }
