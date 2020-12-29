0a4b6476722ac21c160cccec990a669e7ccbaa31
Carlos Dias <carlos.dias@amcom.com.br>
2020-12-29 09:43:44 -0300

    CM - Criação do endpoint de listagem de ocorrências (mock) AB#31449

---
+++ src/SME.SGP.Infra/Dtos/Ocorrencias/Listagens/FiltroOcorrenciaListagemDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SME.SGP.Infra.Dtos.Ocorrencias.Listagens
+{
+    public class FiltroOcorrenciaListagemDto
+    {
+        public DateTime? DataOcorrenciaInicio { get; set; }
+        public DateTime? DataOcorrenciaFim { get; set; }
+        public string AlunoNome { get; set; }
+        public string Titulo { get; set; }
+    }
+}
