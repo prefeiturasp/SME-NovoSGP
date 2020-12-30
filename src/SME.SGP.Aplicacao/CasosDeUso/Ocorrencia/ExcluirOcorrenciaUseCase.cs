eaab616a5e900697f59957c74068f7ff91021b52
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2020-12-30 16:34:49 -0300

    Estrutura use cases

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Ocorrencia/ExcluirOcorrenciaUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExcluirOcorrenciaUseCase : AbstractUseCase, IExcluirOcorrenciaUseCase
+    {
+        public ExcluirOcorrenciaUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public Task<bool> Executar(IEnumerable<long> param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Ocorrencia/ObterOcorrenciaUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterOcorrenciaUseCase : AbstractUseCase, IObterOcorrenciaUseCase
+    {
+        public ObterOcorrenciaUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public Task<OcorrenciaDto> Executar(long param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
