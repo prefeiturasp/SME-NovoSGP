1c7dceaa735479970399503e1c5bf8b7463a8df2
eliton-amcom <eliton.santos@amcom.com.br>
2020-12-30 18:46:25 -0300

    31304 - Ajustes finais listagem

--- src/SME.SGP.WebClient/src/paginas/Gestao/Ocorrencia/ListaOcorrencias.js
+++ src/SME.SGP.WebClient/src/paginas/Gestao/Ocorrencia/ListaOcorrencias.js
@@ -1,25 +1,93 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import shortid from 'shortid';
-import { Cabecalho } from '~/componentes-sgp';
 import {
-  Card,
   Button,
-  Colors,
   CampoData,
-  LocalizadorCrianca,
+  Card,
+  Colors,
+  InputBusca,
+  ListaPaginada,
 } from '~/componentes';
+import { Cabecalho } from '~/componentes-sgp';
+import { erros, ServicoOcorrencias, sucesso } from '~/servicos';
+import history from '~/servicos/history';
 
 const ListaOcorrencias = () => {
   const [dataInicial, setDataInicial] = useState();
   const [dataFinal, setDataFinal] = useState();
+  const [nomeCrianca, setNomeCrianca] = useState();
+  const [tituloOcorrencia, setTituloOcorrencia] = useState();
+  const [filtro, setFiltro] = useState();
+  const [itenSelecionados, setItensSelecionados] = useState([]);
 
-  const onClickVoltar = () => {};
-  const onClickExcluir = () => {};
+  const colunas = [
+    {
+      title: 'Data da ocorrência',
+      dataIndex: 'dataOcorrencia',
+      width: '20%',
+    },
+    {
+      title: 'Criança',
+      dataIndex: 'alunoOcorrencia',
+      width: '40%',
+    },
+    {
+      title: 'Título da ocorrência',
+      dataIndex: 'titulo',
+      width: '40%',
+    },
+  ];
+
+  const onSetFiltro = async () => {
+    setFiltro({
+      DataOcorrenciaInicio: dataInicial?.format('DD/MM/YYYY') || '',
+      DataOcorrenciaFim: dataFinal?.format('DD/MM/YYYY') || '',
+      AlunoNome: nomeCrianca || '',
+      titulo: tituloOcorrencia || '',
+    });
+  };
+
+  const onClickVoltar = () => {
+    history.push('/');
+  };
+
+  const onClickExcluir = async () => {
+    if (itenSelecionados?.length) {
+      const parametros = { data: itenSelecionados };
+      const excluir = await ServicoOcorrencias.excluir(parametros).catch(e =>
+        erros(e)
+      );
+      if (excluir && excluir.status === 200) {
+        const mensagemSucesso = `${
+          itenSelecionados.length > 1
+            ? 'Ocorrências excluídas'
+            : 'Ocorrência excluída'
+        } com sucesso.`;
+        sucesso(mensagemSucesso);
+        setItensSelecionados([]);
+        onSetFiltro();
+      }
+    }
+  };
+
   const onClickNovo = () => {};
 
-  const onChangeDataInicial = () => {};
-  const onChangeDataFinal = () => {};
-  const onChangeLocalizadorEstudante = () => {};
+  const onSelecionarItems = items => {
+    setItensSelecionados([...items.map(item => String(item.id))]);
+  };
+
+  const onChangeDataInicial = valor => {
+    setDataInicial(valor);
+  };
+  const onChangeDataFinal = valor => {
+    setDataFinal(valor);
+  };
+
+  useEffect(() => {
+    if (dataInicial && dataFinal) {
+      onSetFiltro();
+    }
+  }, [dataInicial, dataFinal]);
 
   return (
     <>
@@ -42,6 +110,7 @@ const ListaOcorrencias = () => {
             border
             className="mr-2"
             onClick={onClickExcluir}
+            disabled={!itenSelecionados?.length}
           />
           <Button
             id={shortid.generate()}
@@ -64,16 +133,42 @@ const ListaOcorrencias = () => {
         </div>
         <div className="col-sm-12 col-md-3" style={{ marginTop: '25px' }}>
           <CampoData
-            valor={dataInicial}
+            valor={dataFinal}
             onChange={onChangeDataFinal}
             placeholder="Data final"
             formatoData="DD/MM/YYYY"
           />
         </div>
         <div className="col-sm-12 col-md-6">
-          <LocalizadorCrianca
+          <InputBusca
+            placeholder="Procure pelo nome da criança"
             label="Criança"
-            placeHolder="Procure pelo nome da criança"
+            onPressEnter={onSetFiltro}
+            onClick={onSetFiltro}
+            valor={nomeCrianca}
+            onChange={valor => setNomeCrianca(valor.currentTarget.value)}
+          />
+        </div>
+        <div className="col-sm-12 col-md-6 mt-2">
+          <InputBusca
+            placeholder="Procure pelo título da ocorrência"
+            label="Título da ocorrência"
+            onPressEnter={onSetFiltro}
+            onClick={onSetFiltro}
+            valor={tituloOcorrencia}
+            onChange={valor => setTituloOcorrencia(valor.currentTarget.value)}
+          />
+        </div>
+        <div className="col-md-12 pt-4">
+          <ListaPaginada
+            url="v1/ocorrencias"
+            id="lista-eventos"
+            colunaChave="id"
+            colunas={colunas}
+            filtro={filtro}
+            onClick={() => {}}
+            multiSelecao
+            selecionarItems={onSelecionarItems}
           />
         </div>
       </Card>
