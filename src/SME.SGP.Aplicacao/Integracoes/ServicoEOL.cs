971580c4c96f6b1c7daff797bbe75b6bf7b64f56
Robson <robson.silva@amcom.com.br>
2020-12-30 12:55:56 -0300

    Bug: 30794 Ajuste de código.

--- src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
@@ -773,7 +773,7 @@ namespace SME.SGP.Aplicacao.Integracoes
             
             var datasParaEnvio = JsonConvert.SerializeObject(datas);
 
-            var resposta = await httpClient.PostAsync($"professores/{professorRf}/turmas/{codigoTurma}/disciplinas/{codigoDisciplina}/atribuicao/recorrencia/verificar/datas", new StringContent(datasParaEnvio, Encoding.ut, "application/json"));
+            var resposta = await httpClient.PostAsync($"professores/{professorRf}/turmas/{codigoTurma}/disciplinas/{codigoDisciplina}/atribuicao/recorrencia/verificar/datas", new StringContent(datasParaEnvio, Encoding.UTF8, "application/json"));
 
             if (resposta.IsSuccessStatusCode)
             {
