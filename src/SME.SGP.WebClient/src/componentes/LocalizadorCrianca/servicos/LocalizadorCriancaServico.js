2b8e4ecf8314553bd02e67f49a53ec812f3827e4
eliton-amcom <eliton.santos@amcom.com.br>
2020-12-30 18:46:25 -0300

    31304 - Ajustes finais listagem

--- src/SME.SGP.WebClient/src/componentes/LocalizadorCrianca/servicos/LocalizadorCriancaServico.js
+++
@@ -1,9 +0,0 @@
-import api from '~/servicos/api';
-
-class LocalizadorGenericoServico {
-  buscarDados = url => {
-    return api.get(url);
-  };
-}
-
-export default new LocalizadorGenericoServico();
