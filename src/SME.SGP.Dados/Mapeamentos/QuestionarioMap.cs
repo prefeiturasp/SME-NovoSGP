9eeabfa50008c8100a6bfe6025bee6476c216100
Rafael Losi <rafael.losi@amcom.com.br>
2020-12-31 14:21:04 -0300

    31581 - Criario modelagem para Questionario dinâmico

---
+++ src/SME.SGP.Dados/Mapeamentos/QuestionarioMap.cs
@@ -0,0 +1,15 @@
+﻿using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dados
+{
+    public class QuestionarioMap : BaseMap<Questionario>
+    {
+        public QuestionarioMap()
+        {
+            ToTable("questionario");
+        }
+    }
+}
