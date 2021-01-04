0d691f0db9dcdeaa7f7a7534bd0a084b67a7655f
Rodrigo Galante <rodrigo.galante@globo.com>
2021-01-04 17:31:01 -0300

    Alteração de rota do endpoint para api/v1/estudante/pesquisa e ajustes nas consultas

---
+++ src/SME.SGP.Aplicacao/Queries/Aluno/ObterAlunosPorCodigoEolNome/ObterAlunosPorCodigoEolNomeQueryValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterAlunosPorCodigoEolNomeQueryValidator : AbstractValidator<ObterAlunosPorCodigoEolNomeQuery>
+    {
+        public ObterAlunosPorCodigoEolNomeQueryValidator()
+        {
+            RuleFor(c => c.AnoLetivo)
+                .NotEmpty()
+                .WithMessage("O ano letivo deve ser informado.");
+            RuleFor(c => c.CodigoUe)
+                .NotEmpty()
+                .WithMessage("O código da ue deve ser informado.");
+            RuleFor(c => c.Nome)
+                .NotEmpty()
+                .WithMessage("O Nome do estudante deve ser informado.");
+            RuleFor(c => c.CodigoEOL)
+                .NotEmpty()
+                .WithMessage("O Código do estudante deve ser informado.");
+        }
+    }
+}
