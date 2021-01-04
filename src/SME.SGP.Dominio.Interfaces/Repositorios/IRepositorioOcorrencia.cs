cc4836771766d62c2f9dd19d5433197431c089c4
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-01-04 15:51:57 -0300

    Ajuste listagem ocorrência

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioOcorrencia.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioOcorrencia.cs
@@ -1,4 +1,5 @@
 ﻿using SME.SGP.Dominio.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,6 +7,6 @@ namespace SME.SGP.Dominio
 {
     public interface IRepositorioOcorrencia : IRepositorioBase<Ocorrencia>
     {
-        Task<IEnumerable<Ocorrencia>> Listar(long diarioBordoId, long usuarioLogadoId);
+        Task<IEnumerable<Ocorrencia>> Listar(string titulo, string alunoNome, DateTime? dataOcorrenciaInicio, DateTime? dataOcorrenciaFim);
     }
 }
