6d2091e65a869104097542dd3c1f6242060f1fd3
Carlos Dias <carlos.dias@amcom.com.br>
2021-01-04 15:01:40 -0300

    CM - Adição do log de auditoria.

--- src/SME.SGP.Dados/Repositorios/RepositorioOcorrenciaAluno.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioOcorrenciaAluno.cs
@@ -1,6 +1,7 @@
 ﻿using SME.SGP.Dados.Repositorios;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
+using System;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Dados
@@ -19,13 +20,28 @@ namespace SME.SGP.Dados
             if (entidade.Id > 0)
             {
                 await database.Conexao.UpdateAsync(entidade);
+                await AuditarAsync(entidade.Id, "A");
             }
             else
             {
                 entidade.Id = (long)(await database.Conexao.InsertAsync(entidade));
+                await AuditarAsync(entidade.Id, "I");
             }
 
             return entidade.Id;
+        }
+
+        protected async Task AuditarAsync(long identificador, string acao)
+        {
+            await database.Conexao.InsertAsync(new Auditoria()
+            {
+                Data = DateTime.Now,
+                Entidade = nameof(OcorrenciaAluno).ToLower(),
+                Chave = identificador,
+                Usuario = database.UsuarioLogadoNomeCompleto,
+                RF = database.UsuarioLogadoRF,
+                Acao = acao
+            });
         }
     }
 }
