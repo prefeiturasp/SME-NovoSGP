dc6e3cadaeb37347457a80c25a6a9ecf614a5c6f
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-01-05 20:20:51 -0300

    Ajuste listagem ocorrencia

--- src/SME.SGP.Aplicacao/CasosDeUso/Ocorrencia/ListarOcorrenciasUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Ocorrencia/ListarOcorrenciasUseCase.cs
@@ -11,7 +11,7 @@ namespace SME.SGP.Aplicacao
         {
         }
 
-        public async Task<IEnumerable<OcorrenciaListagemDto>> Executar(FiltroOcorrenciaListagemDto dto)
+        public async Task<PaginacaoResultadoDto<OcorrenciaListagemDto>> Executar(FiltroOcorrenciaListagemDto dto)
         {
             var retorno = await mediator.Send(new ListarOcorrenciasQuery(dto.DataOcorrenciaInicio, dto.DataOcorrenciaFim, dto.AlunoNome, dto.Titulo, dto.TurmaId));
             return retorno;
