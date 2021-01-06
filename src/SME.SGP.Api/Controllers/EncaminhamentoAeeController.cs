2c9bb224c9f4540749341ab677638c8cd5482aaa
Ivano Mucci <ivano.mucci@amcom.com.br>
2021-01-06 15:55:29 -0300

    endpoint de questionario e encaminhamentos

---
+++ src/SME.SGP.Api/Controllers/EncaminhamentoAeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/encaminhamento-aee")]
+    public class EncaminhamentoAeeController : ControllerBase
+    {
+        [HttpGet("questionario")]
+        [ProducesResponseType(typeof(IEnumerable<QuestaoAeeDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterQuestionario([FromQuery] long questionarioId, [FromQuery] long? encaminhamentoId, [FromServices] IObterQuestionarioEncaminhamentoAeeUseCase useCase)
+        {
+            return Ok(await useCase.Executar(questionarioId, encaminhamentoId));
+        }
+    }
+}
