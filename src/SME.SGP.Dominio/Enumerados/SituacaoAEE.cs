13c65b92c5624dfd0b615269660e205f64533c19
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2021-01-07 18:28:56 -0300

    32004 - Criado endpoint para listagem de situacoes

---
+++ src/SME.SGP.Dominio/Enumerados/SituacaoAEE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SME.SGP.Dominio.Enumerados
+{
+    public enum SituacaoAEE
+    {
+        [Display(Name = "Rascunho")]
+        rascunho = 1,
+        [Display(Name = "Encaminhado")]
+        encaminhado = 2,
+        [Display(Name = "Finalizado")]
+        finalizado = 3,
+    }
+}
