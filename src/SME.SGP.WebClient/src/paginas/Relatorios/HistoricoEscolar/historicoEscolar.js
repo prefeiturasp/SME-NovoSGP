07f58ed524029f22ab1ecfdd2c5e037b49d8f1fb
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2021-01-07 11:49:20 -0300

    31576 - Removido exibirPesquisaCodigoAluno

--- src/SME.SGP.WebClient/src/paginas/Relatorios/HistoricoEscolar/historicoEscolar.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/HistoricoEscolar/historicoEscolar.js
@@ -564,7 +564,6 @@ const HistoricoEscolar = () => {
                   onChange={onChangeLocalizadorEstudante}
                   anoLetivo={anoLetivo}
                   desabilitado={!dreId || !ueId}
-                  exibirPesquisaCodigoAluno
                 />
               </div>
             </div>
