3c20aeb8327412b24907c6d03fa0a33ccf3a8bfa
Ivano Mucci <ivano.mucci@amcom.com.br>
2021-01-08 16:00:16 -0300

    Endpoint de remover arquivo da resposta aee

--- src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoAEE.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoAEE.cs
@@ -4,6 +4,7 @@ using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
 {
@@ -12,5 +13,23 @@ namespace SME.SGP.Dados.Repositorios
         public RepositorioRespostaEncaminhamentoAEE(ISgpContext database) : base(database)
         {
         }
-    }
+
+        public async Task<bool> RemoverPorArquivoId(long arquivoId)
+        {
+			await Task.CompletedTask;
+
+			var sql =
+				$@"
+					update resposta_encaminhamento_aee 
+					set excluido = true 
+					where arquivo_id = @arquivoId 
+                ";
+
+			return (
+				database
+				.Conexao
+				.Execute(sql, new { arquivoId })
+				) > 0;
+		}
+	}
 }
--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRespostaEncaminhamentoAEE.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRespostaEncaminhamentoAEE.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioRespostaEncaminhamentoAEE : IRepositorioBase<RespostaEncaminhamentoAEE>
     {
+        Task<bool> RemoverPorArquivoId(long arquivoId);
     }
 }
