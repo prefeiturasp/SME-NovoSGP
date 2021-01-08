117068b10e1dbf08498a1ca412daa823c8d69b91
williamamcom <69849971+williamamcom@users.noreply.github.com>
2021-01-08 11:41:03 -0300

    Update src/SME.SGP.Aplicacao/Queries/EncaminhamentoAEE/ObterInstrucoesModal/ObterInstrucoesModalQueryHandler.cs
    Co-authored-by: Luiz Henrique Araújo <70153714+luizaraujoamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Queries/EncaminhamentoAEE/ObterInstrucoesModal/ObterInstrucoesModalQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/EncaminhamentoAEE/ObterInstrucoesModal/ObterInstrucoesModalQueryHandler.cs
@@ -11,7 +11,7 @@ namespace SME.SGP.Aplicacao
             var instruoesModal = @"<b>O preenchimento deste encaminhamento é exclusivo para o público da Educação Especial. Segundo as normativas do município, o Atendimento Educação Especializado (AEE) é um serviço a ser ofertado exclusivamente para Estudantes/Crianças nos seguintes casos:</b> <br/><p>
                                       1 - Com Deficiência <br/>
                                       2 - Com Transtornos Globais do Desenvolvimento (TGD) <br/>
-                                      3 - Com Altas Habilidades / Superdotação";
+                                      3 - Com Altas Habilidades / Superdotação</p>";
 
             return instruoesModal;
         }
