2fc7e76cdc1f4dfb47bfe985fb9a8de22110b771
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2021-01-08 10:20:09 -0300

    26936 - Normalização do controller

---
+++ src/SME.SGP.Api/Controllers/EncaminhamentoAEEController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Aplicacao.Interfaces.CasosDeUso;
+using SME.SGP.Dominio.Enumerados;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlunoDto = SME.SGP.Infra.Dtos.Relatorios.HistoricoEscolar.AlunoDto;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/encaminhamento-aee")]
+    public class EncaminhamentoAEEController : ControllerBase
+    {
+        [HttpGet("secoes")]
+        [ProducesResponseType(typeof(IEnumerable<SecaoQuestionarioDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterSecoesPorEtapaDeEncaminhamentoAEE([FromQuery] long etapa, [FromServices] IObterSecoesPorEtapaDeEncaminhamentoAEEUseCase obterSecoesPorEtapaDeEncaminhamentoAEEUseCase)
+        {
+            return Ok(await obterSecoesPorEtapaDeEncaminhamentoAEEUseCase.Executar(etapa));
+        }
+
+        [HttpGet("questionario")]
+        [ProducesResponseType(typeof(IEnumerable<QuestaoAeeDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterQuestionario([FromQuery] long questionarioId, [FromQuery] long? encaminhamentoId, [FromServices] IObterQuestionarioEncaminhamentoAeeUseCase useCase)
+        {
+            return Ok(await useCase.Executar(questionarioId, encaminhamentoId));
+        }
+
+        [HttpGet]
+        [Route("situacoes")]
+        [ProducesResponseType(typeof(IEnumerable<AlunoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public IActionResult ObterSituacoes()
+        {
+            var situacoes = Enum.GetValues(typeof(SituacaoAEE))
+                        .Cast<SituacaoAEE>()
+                        .Select(d => new { codigo = (int)d, descricao = d.Name() })
+                        .ToList();
+            return Ok(situacoes);
+        }
+    }
+}
