2dd1d7a93fc8cfa6266ba8d218ed55d135672665
Rafael Losi <rafael.losi@amcom.com.br>
2021-01-10 09:59:57 -0300

    31596 - Exclusão de Encaminhamento AEE

--- src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoAEE.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoAEE.cs
@@ -4,6 +4,7 @@ using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
 {
@@ -11,6 +12,13 @@ namespace SME.SGP.Dados.Repositorios
     {
         public RepositorioRespostaEncaminhamentoAEE(ISgpContext database) : base(database)
         {
+        }
+
+        public async Task<IEnumerable<long>> ObterArquivosPorQuestaoId(long questaoEncaminhamentoAEEId)
+        {
+            var query = "select arquivo_id from resposta_encaminhamento_aee where questao_encaminhamento_id = @questaoEncaminhamentoAEEId and arquivo_id is not null";
+
+            return await database.Conexao.QueryAsync<long>(query, new { questaoEncaminhamentoAEEId });
         }
     }
 }
