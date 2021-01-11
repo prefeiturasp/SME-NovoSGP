40662d9dfd766662502f0a480157b4b0109504a2
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-01-11 17:31:54 -0300

    Mock endpoint listagem

---
+++ src/SME.SGP.Infra/Dtos/DiarioBordo/DiarioBordoTituloDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SME.SGP.Infra
+{
+    public class DiarioBordoTituloDto
+    {
+        public DiarioBordoTituloDto(long id, string titulo)
+        {
+            Id = id;
+            Titulo = titulo;
+        }
+
+        public long Id { get; set; }
+        public string Titulo { get; set; }
+      
+    }
+}
