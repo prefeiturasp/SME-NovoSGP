8341990640aaed1d881e1ea4b445dde6dbd2e5f3
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-01-12 17:57:55 -0300

    Mock get detalhes

---
+++ src/SME.SGP.Infra/Dtos/DiarioBordo/ObservacaoNotificacoesDiarioBordoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SME.SGP.Infra
+{
+    public class ObservacaoNotificacoesDiarioBordoDto
+    {
+        public string Observacao { get; set; }
+
+        public int QtdUsuariosNotificacao { get; set; }
+
+        public AuditoriaDto Auditoria { get; set; }
+    }
+}
