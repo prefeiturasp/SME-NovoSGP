2bfa1b7eec9ac138a0db0d49147a491ed128bcd2
William Manoel dos Santos <william.santos@amcom.com.br>
2021-01-13 09:01:05 -0300

    Ajuste na estrutura do endpoint

--- src/SME.SGP.Aplicacao/Queries/Estudante/ObterNecessidadesEspeciaisAluno/ObterNecessidadesEspeciaisAlunoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Estudante/ObterNecessidadesEspeciaisAluno/ObterNecessidadesEspeciaisAlunoQueryHandler.cs
@@ -1,10 +1,6 @@
 using MediatR;
-using Newtonsoft.Json;
-using SME.SGP.Dominio;
+using SME.SGP.Aplicacao.Integracoes;
 using SME.SGP.Infra;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,26 +8,15 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterNecessidadesEspeciaisAlunoQueryHandler : IRequestHandler<ObterNecessidadesEspeciaisAlunoQuery, InformacoesEscolaresAlunoDto>
     {
-        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IServicoEol servicoEOL;
 
-        public ObterNecessidadesEspeciaisAlunoQueryHandler(IHttpClientFactory httpClientFactory)
+        public ObterNecessidadesEspeciaisAlunoQueryHandler(IServicoEol servicoEOL)
         {
-            this.httpClientFactory = httpClientFactory;
+            this.servicoEOL = servicoEOL ?? throw new System.ArgumentNullException(nameof(servicoEOL));
         }
 
         public async Task<InformacoesEscolaresAlunoDto> Handle(ObterNecessidadesEspeciaisAlunoQuery request, CancellationToken cancellationToken)
-        {
-            var httpClient = httpClientFactory.CreateClient("servicoEOL");
+                    => await servicoEOL.ObterNecessidadesEspeciaisAluno(request.CodigoAluno);
 
-            var url = new StringBuilder("alunos/" + request.CodigoAluno + "/necessidades-especiais");
-
-            var resposta = await httpClient.GetAsync($"{url}", cancellationToken);
-
-            if (!resposta.IsSuccessStatusCode || resposta.StatusCode == HttpStatusCode.NoContent)
-                throw new NegocioException("Não foram encontrados dados de necessidades especiais para o aluno.", HttpStatusCode.InternalServerError);
-
-            var json = await resposta.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<InformacoesEscolaresAlunoDto>(json);
-        }
     }
 }
