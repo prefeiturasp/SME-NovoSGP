158fe3f3f84bd3a8ed637dc8a299025485e13caf
Luiz Henrique Araújo <luiz.araujo@amcom.com.br>
2021-01-13 10:27:01 -0300

    31595 - Mudança na dto de Encaminhamento

--- src/SME.SGP.Infra/Dtos/EncaminhamentoAee/EncaminhamentoAeeDto.cs
+++ src/SME.SGP.Infra/Dtos/EncaminhamentoAee/EncaminhamentoAeeDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SME.SGP.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,14 @@ namespace SME.SGP.Infra
 {
     public class EncaminhamentoAEEDto
     {
+        public EncaminhamentoAEEDto()
+        {
+            Secoes = new List<EncaminhamentoAEESecaoDto>();
+        }
         public long Id { get; set; }
         public long TurmaId { get; set; }
         public string AlunoCodigo { get; set; }
         public AuditoriaDto Auditoria { get; set; }
+        public List<EncaminhamentoAEESecaoDto> Secoes { get; set; }
     }
 }
