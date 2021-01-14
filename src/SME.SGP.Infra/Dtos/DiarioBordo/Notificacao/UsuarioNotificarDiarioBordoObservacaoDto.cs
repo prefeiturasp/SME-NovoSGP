89e714085274a524efaca99503e4807007e7311c
Carlos Dias <carlos.dias@amcom.com.br>
2021-01-14 10:28:41 -0300

    CM - Mock do endpoit de busca de usuários para serem notificados.

---
+++ src/SME.SGP.Infra/Dtos/DiarioBordo/Notificacao/UsuarioNotificarDiarioBordoObservacaoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Infra.Dtos
+{
+    public class UsuarioNotificarDiarioBordoObservacaoDto
+    {
+        public long UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public bool PodeRemover { get; set; }
+    }
+}
