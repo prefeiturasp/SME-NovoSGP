1184fc6c8e4684090fcbf95ef68597fe90688984
Carlos Dias <carlos.dias@amcom.com.br>
2021-01-14 14:53:43 -0300

    CM - Endpoint de Notificar Usuário

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/DiarioBordo/ObterUsuarioNotificarDiarioBordoObservacaoUseCase.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterUsuarioNotificarDiarioBordoObservacaoUseCase : AbstractUseCase, IObterUsuarioNotificarDiarioBordoObservacaoUseCase
+    {
+        public ObterUsuarioNotificarDiarioBordoObservacaoUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<IEnumerable<UsuarioNotificarDiarioBordoObservacaoDto>> Executar(ObterUsuarioNotificarDiarioBordoObservacaoDto dto) 
+            => await mediator.Send(new ObterUsuarioNotificarDiarioBordoObservacaoQuery(dto.TurmaId, dto.ObservacaoId));
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/DiarioBordo/ObterUsuariosNotificarDiarioBordoObservacao/ObterUsuarioNotificarDiarioBordoObservacaoQuery.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Infra.Dtos;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterUsuarioNotificarDiarioBordoObservacaoQuery : IRequest<IEnumerable<UsuarioNotificarDiarioBordoObservacaoDto>>
+    {
+        public long TurmaId { get; set; }
+        public long? ObservacaoId { get; set; }
+
+        public ObterUsuarioNotificarDiarioBordoObservacaoQuery(long turmaId)
+        {
+            TurmaId = turmaId;
+        }
+
+        public ObterUsuarioNotificarDiarioBordoObservacaoQuery(long turmaId, long? observacaoId)
+            : this(turmaId)
+        {
+            ObservacaoId = observacaoId;
+        }
+    }
+
+    public class ObterUsuarioNotificarDiarioBordoObservacaoQueryValidator : AbstractValidator<ObterUsuarioNotificarDiarioBordoObservacaoQuery>
+    {
+        public ObterUsuarioNotificarDiarioBordoObservacaoQueryValidator()
+        {
+            RuleFor(x => x.TurmaId)
+                .NotEmpty()
+                .WithMessage("A turma deve ser informada.");
+
+            RuleFor(x => x.ObservacaoId)
+                .NotEmpty()
+                .When(x => x.ObservacaoId != null)
+                .WithMessage("A observação informada é inválida.");
+        }
+    }
+}
