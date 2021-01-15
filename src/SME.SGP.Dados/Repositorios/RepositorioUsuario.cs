0047dd1c6c528e20148847916fb3693654aa9df3
Carlos Dias <carlos.dias@amcom.com.br>
2021-01-15 11:23:12 -0300

    CM - Ajustes levantados em review.

--- src/SME.SGP.Dados/Repositorios/RepositorioUsuario.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioUsuario.cs
@@ -77,18 +77,15 @@ namespace SME.SGP.Dados.Repositorios
             return await database.Conexao.QueryAsync<long>(query.ToString());
         }
 
-        public async Task<IEnumerable<Usuario>> ObterUsuariosPorCodigoRf(IList<string> codigoRf)
+        public async Task<IEnumerable<Usuario>> ObterUsuariosPorCodigoRf(IList<string> codigosRf)
         {
-            var query = new StringBuilder();
-            query.Append("select * from usuario ");
-            query.Append("where rf_codigo in ");
-            query.Append("(");
-            foreach (var rf in codigoRf)
-            {
-                query.Append($"'{rf}',");
-            }
-            query.Append("'0')");
-            return await database.Conexao.QueryAsync<Usuario>(query.ToString());
+            var query = @"select 
+                            * 
+                        from 
+                            usuario u
+                        where
+                            u.rf_codigo = any(@codigosRf)";
+            return await database.Conexao.QueryAsync<Usuario>(query.ToString(), new { codigosRf });
         }
 
         public async Task<long> ObterUsuarioIdPorCodigoRfAsync(string codigoRf)
