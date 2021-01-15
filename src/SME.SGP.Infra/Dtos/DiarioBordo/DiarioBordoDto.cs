88f3b668b087b0001ef64bdafba2555af8fccd16
Jeferson Cassiano <jeferson.cassiano@amcom.com.br>
2021-01-15 12:21:04 -0300

    Mock endpoint busca diario por id

--- src/SME.SGP.Infra/Dtos/DiarioBordo/DiarioBordoDto.cs
+++ src/SME.SGP.Infra/Dtos/DiarioBordo/DiarioBordoDto.cs
@@ -1,12 +1,12 @@
-﻿using SME.SGP.Dominio;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SME.SGP.Infra.Dtos
 {
     public class DiarioBordoDto
     {
+
+        public DateTime Data { get; set; }
         public long AulaId { get; set; }
         public AulaDto Aula { get; set; }
         public long? DevolutivaId { get; set; }
@@ -15,10 +15,7 @@ namespace SME.SGP.Infra.Dtos
         public string Planejamento { get; set; }
         public string ReflexoesReplanejamento { get; set; }
 
-        public bool Excluido { get; set; }
-        public bool Migrado { get; set; }
-
-        public bool TemPeriodoAberto { get; set; }
+        public IEnumerable<ObservacaoNotificacoesDiarioBordoDto> Observacoes { get; set; }
 
         public AuditoriaDto Auditoria { get; set; }
     }
