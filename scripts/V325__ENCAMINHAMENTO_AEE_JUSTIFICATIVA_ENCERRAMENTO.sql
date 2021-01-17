89c0cfe1bad2fed2948acc7d9f1aa948871d1aa0
William Manoel dos Santos <william.santos@amcom.com.br>
2021-01-17 19:24:16 -0300

    Troca de justificativa por motivo encerramento

--- scripts/V325__ENCAMINHAMENTO_AEE_JUSTIFICATIVA_ENCERRAMENTO.sql
+++ scripts/V325__ENCAMINHAMENTO_AEE_JUSTIFICATIVA_ENCERRAMENTO.sql
@@ -1,3 +1,3 @@
-alter table encaminhamento_aee drop if exists justificativa_encerramento;
+alter table encaminhamento_aee drop if exists motivo_encerramento;
 
-alter table encaminhamento_aee add justificativa_encerramento varchar;
+alter table encaminhamento_aee add motivo_encerramento varchar;
