125adb667f3a9379aed38b89238651a8cc516260
William Manoel dos Santos <william.santos@amcom.com.br>
2021-01-19 18:06:05 -0300

    Endpoint Enviar para Analise

---
+++ src/SME.SGP.Aplicacao/Commands/EncaminhamentoAEE/EnviarParaAnaliseEncaminhamentoAEE/EnviarParaAnaliseEncaminhamentoAEECommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class EnviarParaAnaliseEncaminhamentoAEECommandHandler : IRequestHandler<EnviarParaAnaliseEncaminhamentoAEECommand, bool>
+    {
+        public IMediator mediator { get; }
+        public IRepositorioEncaminhamentoAEE repositorioEncaminhamentoAEE { get; }
+
+        public EnviarParaAnaliseEncaminhamentoAEECommandHandler(IMediator mediator, IRepositorioEncaminhamentoAEE repositorioEncaminhamentoAEE)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.repositorioEncaminhamentoAEE = repositorioEncaminhamentoAEE ?? throw new ArgumentNullException(nameof(repositorioEncaminhamentoAEE));
+        }
+
+        public async Task<bool> Handle(EnviarParaAnaliseEncaminhamentoAEECommand request, CancellationToken cancellationToken)
+        {
+            var encaminhamentoAEE = await mediator.Send(new ObterEncaminhamentoAEEComTurmaPorIdQuery(request.EncaminhamentoId));
+
+            if (encaminhamentoAEE == null)
+                throw new NegocioException("O encaminhamento informado não foi encontrado");
+
+            encaminhamentoAEE.Situacao = Dominio.Enumerados.SituacaoAEE.Encaminhado;
+
+            var idEntidadeEncaminhamento = await repositorioEncaminhamentoAEE.SalvarAsync(encaminhamentoAEE);
+
+            return idEntidadeEncaminhamento != 0;
+        }
+    }
+}
