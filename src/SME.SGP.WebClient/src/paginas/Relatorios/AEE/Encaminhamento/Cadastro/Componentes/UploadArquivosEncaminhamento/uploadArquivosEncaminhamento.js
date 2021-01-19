98c71d281b11951f1838d734e3182250f098def2
Cleiton <cleiton.dutra@amcom.com.br>
2021-01-19 19:14:35 -0300

    [SGP AEE] Encaminhamento - UploadArquivosEncaminhamento

---
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/AEE/Encaminhamento/Cadastro/Componentes/UploadArquivosEncaminhamento/uploadArquivosEncaminhamento.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import UploadArquivos from '~/componentes-sgp/UploadArquivos/uploadArquivos';
+import { setEncaminhamentoAEEEmEdicao } from '~/redux/modulos/encaminhamentoAEE/actions';
+import { erros, sucesso } from '~/servicos';
+import ServicoEncaminhamentoAEE from '~/servicos/Paginas/Relatorios/AEE/ServicoEncaminhamentoAEE';
+
+const UploadArquivosEncaminhamento = props => {
+  const { dados } = props;
+  const { form, questaoAtual } = dados;
+
+  const dispatch = useDispatch();
+
+  const onRemoveFile = async arquivo => {
+    const codigoArquivo = arquivo.xhr;
+
+    // TODO TESTAR!
+    if (arquivo.arquivoId) {
+      if (
+        form &&
+        form?.setFieldValue &&
+        form?.values?.[questaoAtual?.id]?.length
+      ) {
+        const novoMap = form?.values?.[questaoAtual?.id];
+        const indice = novoMap.findIndex(item => arquivo.arquivoId === item);
+
+        novoMap.splice(indice, 1);
+
+        form.setFieldValue(String(questaoAtual?.id), novoMap);
+        form.setFieldTouched(String(questaoAtual?.id), true);
+      }
+      sucesso(`Arquivo ${arquivo.name} removido com sucesso`);
+      return true;
+    }
+
+    const resposta = await ServicoEncaminhamentoAEE.removerArquivo(
+      codigoArquivo
+    ).catch(e => erros(e));
+
+    if (resposta && resposta.status === 200) {
+      sucesso(`Arquivo ${arquivo.name} removido com sucesso`);
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <div className="col-md-12 mt-2">
+      <UploadArquivos
+        form={form}
+        name={String(questaoAtual?.id)}
+        id={String(questaoAtual?.id)}
+        // desabilitarGeral={desabilitarCampos}
+        // tiposArquivosPermitidos=".pdf"
+        desabilitarUpload={form?.values?.[questaoAtual?.id]?.length > 9}
+        onRemove={onRemoveFile}
+        urlUpload="v1/encaminhamento-aee/upload"
+        defaultFileList={
+          form?.values?.[questaoAtual?.id]?.length
+            ? form?.values?.[questaoAtual?.id]
+            : []
+        }
+        onChangeListaArquivos={() => {
+          dispatch(setEncaminhamentoAEEEmEdicao(true));
+        }}
+      />
+    </div>
+  );
+};
+
+UploadArquivosEncaminhamento.propTypes = {
+  dados: PropTypes.oneOfType([PropTypes.object]),
+};
+
+UploadArquivosEncaminhamento.defaultProps = {
+  dados: {},
+};
+
+export default UploadArquivosEncaminhamento;
