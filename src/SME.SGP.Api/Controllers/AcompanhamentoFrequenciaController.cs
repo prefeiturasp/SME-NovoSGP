8ee45944c69f9a9d160bf4b98e360074e0fb6bc8
Carlos Dias <carlos.dias@amcom.com.br>
2021-01-19 15:09:40 -0300

    CM - Permissionamento

--- src/SME.SGP.Api/Controllers/AcompanhamentoFrequenciaController.cs
+++ src/SME.SGP.Api/Controllers/AcompanhamentoFrequenciaController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Infra;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
@@ -15,6 +17,7 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(FrequenciaAlunosPorBimestreDto), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 602)]
+        [Permissao(Permissao.AFQ_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterInformacoesDeFrequenciaAlunosPorBimestre([FromQuery] ObterFrequenciaAlunosPorBimestreDto dto, [FromServices] IObterInformacoesDeFrequenciaAlunosPorBimestreUseCase useCase)
         {
             return Ok(await useCase.Executar(dto));
@@ -24,6 +27,7 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(IEnumerable<JustificativaAlunoDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 602)]
+        [Permissao(Permissao.AFQ_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterJustificativasAlunoPorComponenteCurricular(long turmaId, long alunoCodigo, long componenteCurricularId, [FromServices] IObterJustificativasAlunoPorComponenteCurricularUseCase useCase)
         {
             return Ok(await useCase.Executar(new FiltroJustificativasAlunoPorComponenteCurricular(turmaId, componenteCurricularId, alunoCodigo)));
