7100fb05449423cef007d177e12f0525afd5eda4
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-01-20 13:23:14 -0300

    32197 - Criada integração com backend no modal de anotações

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/AcompanhamentoFrequencia/Componentes/ListaAlunos/listaAlunos.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/AcompanhamentoFrequencia/Componentes/ListaAlunos/listaAlunos.js
@@ -15,6 +15,7 @@ import ModalAnotacoesAcompanhamentoFrequencia from './modalAnotacoesAcompanhamen
 
 import ServicoAcompanhamentoFrequencia from '~/servicos/Paginas/DiarioClasse/ServicoAcompanhamentoFrequencia';
 import { erros } from '~/servicos';
+import { Loader } from '~/componentes';
 
 const ListaAlunos = props => {
   const usuario = useSelector(store => store.usuario);
@@ -23,10 +24,12 @@ const ListaAlunos = props => {
   const { bimestreSelecionado, componenteCurricularId } = props;
   const dispatch = useDispatch();
 
-  const [dadosBimestre, setDadosBimestre] = useState([]);
+  const [carregandoListaAlunos, setCarregandoListaAlunos] = useState(false);
+  const [dadosBimestre, setDadosBimestre] = useState(null);
 
   useEffect(() => {
     const obterAlunos = async () => {
+      setCarregandoListaAlunos(true);
       const retorno = await ServicoAcompanhamentoFrequencia.obterAcompanhamentoFrequenciaPorBimestre(
         turmaSelecionada?.id,
         componenteCurricularId,
@@ -36,6 +39,7 @@ const ListaAlunos = props => {
       if (retorno?.data) {
         setDadosBimestre(retorno?.data);
       }
+      setCarregandoListaAlunos(false);
     };
 
     obterAlunos();
@@ -50,95 +54,106 @@ const ListaAlunos = props => {
 
   return (
     <>
-      <ModalAnotacoesAcompanhamentoFrequencia />
-      <TabelaColunasFixas>
-        <div className="row">
-          <div className="col-md-6 col-sm-12">
-            <Ordenacao
-              className="mb-2"
-              conteudoParaOrdenar={dadosBimestre?.frequenciaAlunos}
-              ordenarColunaNumero="numeroChamada"
-              ordenarColunaTexto="nome"
-              retornoOrdenado={retorno => {
-                onChangeOrdenacao(retorno);
-              }}
-            />
-          </div>
+      <Loader loading={carregandoListaAlunos} />
+      {dadosBimestre ? (
+        <>
+          <ModalAnotacoesAcompanhamentoFrequencia />
+          <TabelaColunasFixas>
+            <div className="row">
+              <div className="col-md-6 col-sm-12">
+                <Ordenacao
+                  className="mb-2"
+                  conteudoParaOrdenar={dadosBimestre?.frequenciaAlunos}
+                  ordenarColunaNumero="numeroChamada"
+                  ordenarColunaTexto="nome"
+                  retornoOrdenado={retorno => {
+                    onChangeOrdenacao(retorno);
+                  }}
+                />
+              </div>
 
-          <Marcadores className="col-md-6 col-sm-12 d-flex justify-content-end">
-            <MarcadorAulas className="ml-2">
-              <span>Aulas previstas </span>
-              <span className="numero">
-                {dadosBimestre && dadosBimestre.totalAulasPrevistas
-                  ? dadosBimestre.totalAulasPrevistas
-                  : 0}
-              </span>
-            </MarcadorAulas>
-            <MarcadorAulas className="ml-2">
-              <span>Aulas dadas </span>
-              <span className="numero">
-                {dadosBimestre && dadosBimestre.totalAulasDadas
-                  ? dadosBimestre.totalAulasDadas
-                  : 0}
-              </span>
-            </MarcadorAulas>
-          </Marcadores>
-        </div>
-        <div className="wrapper">
-          <div className="header-fixo">
-            <table className="table">
-              <thead className="tabela-dois-thead">
-                <tr>
-                  <th className="col-linha-quatro" colSpan="2">
-                    Nome
-                  </th>
-                  <th className="col-linha-dois">Ausências no Bimestre</th>
-                  <th className="col-linha-dois">Compensações de ausência</th>
-                  <th className="col-linha-dois">Frequência</th>
-                </tr>
-              </thead>
-              <tbody className="tabela-um-tbody">
-                {dadosBimestre?.frequenciaAlunos?.map((data, index) => {
-                  return (
-                    <>
-                      <tr id={index}>
-                        <td className="col-valor-linha-tres">
-                          <strong>{data.numeroChamada}</strong>
-                        </td>
-                        <td className="col-valor-linha-quatro">{data.nome}</td>
-                        <td className="col-valor-linha-dois">
-                          {data.ausencias}
-                        </td>
-                        <td className="col-valor-linha-dois">
-                          {data.compensacoes}
-                        </td>
-                        <td className="col-valor-linha-dois">
-                          {data.frequencia}%
-                          {data.ausencias > 0 ? (
-                            <BtnExpandirFrequenciaAluno
-                              indexLinha={index}
-                              codigoAluno={data.alunoRf}
-                            />
-                          ) : (
-                            <></>
-                          )}
-                        </td>
-                      </tr>
-                      <AusenciasAluno
-                        indexLinha={index}
-                        dados={data.ausencias}
-                        turmaId={turmaSelecionada?.id}
-                        componenteCurricularId={componenteCurricularId}
-                        codigoAluno={data.alunoRf}
-                      />
-                    </>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </TabelaColunasFixas>
+              <Marcadores className="col-md-6 col-sm-12 d-flex justify-content-end">
+                <MarcadorAulas className="ml-2">
+                  <span>Aulas previstas </span>
+                  <span className="numero">
+                    {dadosBimestre && dadosBimestre.totalAulasPrevistas
+                      ? dadosBimestre.totalAulasPrevistas
+                      : 0}
+                  </span>
+                </MarcadorAulas>
+                <MarcadorAulas className="ml-2">
+                  <span>Aulas dadas </span>
+                  <span className="numero">
+                    {dadosBimestre && dadosBimestre.totalAulasDadas
+                      ? dadosBimestre.totalAulasDadas
+                      : 0}
+                  </span>
+                </MarcadorAulas>
+              </Marcadores>
+            </div>
+            <div className="wrapper">
+              <div className="header-fixo">
+                <table className="table">
+                  <thead className="tabela-dois-thead">
+                    <tr>
+                      <th className="col-linha-quatro" colSpan="2">
+                        Nome
+                      </th>
+                      <th className="col-linha-dois">Ausências no Bimestre</th>
+                      <th className="col-linha-dois">
+                        Compensações de ausência
+                      </th>
+                      <th className="col-linha-dois">Frequência</th>
+                    </tr>
+                  </thead>
+                  <tbody className="tabela-um-tbody">
+                    {dadosBimestre?.frequenciaAlunos?.map((data, index) => {
+                      return (
+                        <>
+                          <tr id={index}>
+                            <td className="col-valor-linha-tres">
+                              <strong>{data.numeroChamada}</strong>
+                            </td>
+                            <td className="col-valor-linha-quatro">
+                              {data.nome}
+                            </td>
+                            <td className="col-valor-linha-dois">
+                              {data.ausencias}
+                            </td>
+                            <td className="col-valor-linha-dois">
+                              {data.compensacoes}
+                            </td>
+                            <td className="col-valor-linha-dois">
+                              {data.frequencia}%
+                              {data.ausencias > 0 ? (
+                                <BtnExpandirFrequenciaAluno
+                                  indexLinha={index}
+                                  codigoAluno={data.alunoRf}
+                                />
+                              ) : (
+                                <></>
+                              )}
+                            </td>
+                          </tr>
+                          <AusenciasAluno
+                            indexLinha={index}
+                            dados={data.ausencias}
+                            turmaId={turmaSelecionada?.id}
+                            componenteCurricularId={componenteCurricularId}
+                            codigoAluno={data.alunoRf}
+                          />
+                        </>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </TabelaColunasFixas>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
