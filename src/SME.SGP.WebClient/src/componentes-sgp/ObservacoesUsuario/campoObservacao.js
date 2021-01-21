fce6f0cfa239c00b07f45365321f7312e5ae171e
eliton-amcom <eliton.santos@amcom.com.br>
2021-01-20 21:42:55 -0300

    32839 - Ajustes após testes

--- src/SME.SGP.WebClient/src/componentes-sgp/ObservacoesUsuario/campoObservacao.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/ObservacoesUsuario/campoObservacao.js
@@ -105,6 +105,7 @@ const CampoObservacao = props => {
             color={Colors.Azul}
             border
             onClick={() => setModalVisivel(true)}
+            disabled={!podeIncluir}
           />
         </div>
         <div className="p-0 col-md-6 d-flex justify-content-end">
