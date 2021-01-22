19763820d8322ec0c2d3241e798c2f772c28720e
Rafael Losi <rafael.losi@amcom.com.br>
2021-01-22 14:50:51 -0300

    26936 Concluido na seção encaminhamento AEE

--- src/SME.SGP.Dados/Repositorios/RepositorioSecaoEncaminhamentoAEE.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioSecaoEncaminhamentoAEE.cs
@@ -12,12 +12,14 @@ namespace SME.SGP.Dados.Repositorios
         {
         }
 
-        public async Task<IEnumerable<SecaoQuestionarioDto>> ObterSecaoEncaminhamentoPorEtapa(long etapa)
+        public async Task<IEnumerable<SecaoQuestionarioDto>> ObterSecaoEncaminhamentoPorEtapa(long etapa, long encaminhamentoAeeId = 0)
         {
             var query = @"SELECT sea.id
 	                            , sea.nome
 	                            , sea.questionario_id
+	                            , eas.concluido
                          FROM secao_encaminhamento_aee sea
+                        left join encaminhamento_aee_secao eas on eas.encaminhamento_aee_id = @encaminhamentoAeeId and eas.secao_encaminhamento_id = sea.id
                          WHERE not sea.excluido 
                            AND sea.etapa = @etapa
                          ORDER BY sea.ordem ";
