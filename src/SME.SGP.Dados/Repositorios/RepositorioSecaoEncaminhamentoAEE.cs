275df6b6e92fa86ccb069d548979e64b07409945
Cleiton <cleiton.dutra@amcom.com.br>
2021-01-22 14:19:35 -0300

    [SGP AEE] Encaminhamento - salvar e editar encaminhamentoAEEDto.Situacao

--- src/SME.SGP.Dados/Repositorios/RepositorioSecaoEncaminhamentoAEE.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioSecaoEncaminhamentoAEE.cs
@@ -17,7 +17,6 @@ namespace SME.SGP.Dados.Repositorios
             var query = @"SELECT sea.id
 	                            , sea.nome
 	                            , sea.questionario_id
-	                            , sea.concluido
                          FROM secao_encaminhamento_aee sea
                          WHERE not sea.excluido 
                            AND sea.etapa = @etapa
