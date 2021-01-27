34b721cf570561546b4e8851d0348252a05b349f
William Manoel dos Santos <william.santos@amcom.com.br>
2021-01-27 09:37:24 -0300

    Script para Remover Interrogação dupla na pergunta 6

---
+++ scripts/V328__AJUSTE_QUESTAO_ENCAMINHAMENTO_AEE_ETAPA_1.sql
@@ -0,0 +1,3 @@
+update questao 
+   set nome = 'Diante das dificuldades apresentadas acima, quais estratégias pedagógicas foram feitas em sala de aula antes do encaminhamento ao AEE?'
+ where id = 14;
