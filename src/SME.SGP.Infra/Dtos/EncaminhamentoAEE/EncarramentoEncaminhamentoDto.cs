2651d4d3e886c055c44afdf767b5315205c634f0
Rafael Losi <rafael.losi@amcom.com.br>
2021-01-27 20:48:31 -0300

    33138 Corrigido passagem de parametro motivo para aceitar um texto maior

---
+++ src/SME.SGP.Infra/Dtos/EncaminhamentoAEE/EncarramentoEncaminhamentoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra.Dtos
+{
+    public class EncarramentoEncaminhamentoDto
+    {
+        public long EncaminhamentoId { get; set; }
+        public string MotivoEncerramento { get; set; }
+    }
+}
