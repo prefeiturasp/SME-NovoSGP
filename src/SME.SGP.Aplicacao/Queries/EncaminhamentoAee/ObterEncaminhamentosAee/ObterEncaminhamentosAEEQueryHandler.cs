2b0b483a02279c094224c741310ad9e2f8bb902c
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-01-27 18:22:55 -0300

    33126 - Alterado o modo do retorno caso n tenha uma situação

--- src/SME.SGP.Aplicacao/Queries/EncaminhamentoAee/ObterEncaminhamentosAee/ObterEncaminhamentosAEEQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/EncaminhamentoAee/ObterEncaminhamentosAee/ObterEncaminhamentosAEEQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using SME.SGP.Infra.Interfaces;
@@ -52,7 +53,7 @@ namespace SME.SGP.Aplicacao
                 listaEncaminhamentos.Add(new EncaminhamentoAEEResumoDto()
                 {
                     Id = encaminhamento.Id,
-                    Situacao = encaminhamento.Situacao != 0 ? encaminhamento.Situacao.Name() : "Erro ao cadastrar situação",
+                    Situacao = encaminhamento.Situacao != 0 ? encaminhamento.Situacao.Name() : SituacaoAEE.Rascunho.Name(),
                     Turma = $"{encaminhamento.TurmaModalidade.ShortName()} - {encaminhamento.TurmaNome}",
                     Numero = aluno?.NumeroAlunoChamada ?? 0,
                     Nome = aluno?.NomeAluno
