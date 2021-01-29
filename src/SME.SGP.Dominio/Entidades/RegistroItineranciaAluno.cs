341873bdb0dbaecc5270c3a8515ab386833569aa
eliton-amcom <eliton.santos@amcom.com.br>
2021-01-29 17:03:04 -0300

    32922 - Modelagem

---
+++ src/SME.SGP.Dominio/Entidades/RegistroItineranciaAluno.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SME.SGP.Dominio
+{
+    public class RegistroItineranciaAluno
+    {
+        public string CodigoAluno { get; set; }
+        public long RegistroItineranciaId { get; set; }
+        public IEnumerable<RegistroItineranciaAlunoQuestao>  AlunosQuestoes { get; set; }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/RegistroItineranciaObjetivo.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Dominio
+{
+    public class RegistroItineranciaObjetivo : EntidadeBase
+    {
+        public long RegistroItineranciaObjetivosBaseId { get; set; }
+        public long RegistroItineranciaId { get; set; }
+    }
+}
