5d0c50fd2238a90ef192030796e8b728fa274ec2
William Manoel dos Santos <william.santos@amcom.com.br>
2021-02-01 11:44:07 -0300

    Troca de usuario_id para responsavel_id

--- src/SME.SGP.Dominio/Entidades/EncaminhamentoAEE.cs
+++ src/SME.SGP.Dominio/Entidades/EncaminhamentoAEE.cs
@@ -17,7 +17,7 @@ namespace SME.SGP.Dominio
         public SituacaoAEE Situacao { get; set; }
         public bool Excluido { get; set; }
         public string MotivoEncerramento { get; set; }
-        public long UsuarioId { get; set; }
+        public long ResponsavelId { get; set; }
         public List<EncaminhamentoAEESecao> Secoes { get; set; }
 
     }
