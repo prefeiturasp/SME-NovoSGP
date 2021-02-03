dabd772f3cb3b39c24b395f06a69f9839a448e5f
Cleiton <cleiton.dutra@amcom.com.br>
2021-02-03 10:23:45 -0300

    localizador de estudante - digitar somente numero

--- src/SME.SGP.WebClient/src/componentes/LocalizadorEstudante/index.js
+++ src/SME.SGP.WebClient/src/componentes/LocalizadorEstudante/index.js
@@ -98,7 +98,7 @@ const LocalizadorEstudante = props => {
   };
 
   const onBuscarPorCodigo = async codigo => {
-    if (!codigo) {
+    if (!codigo.codigo) {
       limparDados();
       return;
     }
