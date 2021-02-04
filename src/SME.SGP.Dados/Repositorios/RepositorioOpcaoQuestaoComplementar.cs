9c4e448ed451998e20bdaebe9ad6b045f41b9193
William Manoel dos Santos <william.santos@amcom.com.br>
2021-02-04 15:33:03 -0300

    Alteração de Relacionamento Opção Resposta x Questão complementar

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioOpcaoQuestaoComplementar.cs
@@ -0,0 +1,12 @@
+﻿using SME.SGP.Dominio.Entidades;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioOpcaoQuestaoComplementar : RepositorioBase<OpcaoQuestaoComplementar>, IRepositorioOpcaoQuestaoComplementar
+    {
+        public RepositorioOpcaoQuestaoComplementar(ISgpContext database) : base(database)
+        {
+        }
+    }
+}
