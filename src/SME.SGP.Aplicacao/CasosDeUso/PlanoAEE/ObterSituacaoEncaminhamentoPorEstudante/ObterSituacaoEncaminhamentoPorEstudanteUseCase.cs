64603c99bf3fe4f0998de800456c59fc0579fe8a
Rodrigo Galante <rodrigo.galante@globo.com>
2021-02-07 23:48:51 -0300

    Serviço que retorna a situação de um encaminhamento por aluno

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/PlanoAEE/ObterSituacaoEncaminhamentoPorEstudante/ObterSituacaoEncaminhamentoPorEstudanteUseCase.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso
+{
+    public class ObterSituacaoEncaminhamentoPorEstudanteUseCase : AbstractUseCase, IObterSituacaoEncaminhamentoPorEstudanteUseCase
+    {
+        public ObterSituacaoEncaminhamentoPorEstudanteUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<SituacaoEncaminhamentoPorEstudanteDto> Executar(string codigoAluno)
+        {
+            return await mediator.Send(new ObterSituacaoEncaminhamentoAEEPorEstudanteQuery(codigoAluno));
+        }
+    }
+}
