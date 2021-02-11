2bec1e21850d3a2ee2638148d08f06dc2c0f98c6
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-02-10 22:51:37 -0300

    33315 - Alterado para exibir corretamente a situação do encaminhamento

--- src/SME.SGP.WebClient/src/paginas/Relatorios/AEE/Plano/Cadastro/Componentes/SituacaoEncaminhamentoAEE/situacaoEncaminhamentoAEE.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/AEE/Plano/Cadastro/Componentes/SituacaoEncaminhamentoAEE/situacaoEncaminhamentoAEE.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPlanoAEESituacaoEncaminhamentoAEE } from '~/redux/modulos/planoAEE/actions';
 import ServicoEncaminhamentoAEE from '~/servicos/Paginas/Relatorios/AEE/ServicoEncaminhamentoAEE';
 
 const SituacaoEncaminhamentoAEE = () => {
+  const dispatch = useDispatch();
   const dadosCollapseLocalizarEstudante = useSelector(
     store => store.collapseLocalizarEstudante.dadosCollapseLocalizarEstudante
   );
@@ -11,16 +13,18 @@ const SituacaoEncaminhamentoAEE = () => {
 
   useEffect(() => {
     const obtemSituacaoEncaminhamento = async () => {
+      dispatch(setPlanoAEESituacaoEncaminhamentoAEE());
       const retorno = await ServicoEncaminhamentoAEE.obterAlunoSituacaoEncaminhamentoAEE(
-        dadosCollapseLocalizarEstudante?.codigoEstudante
+        dadosCollapseLocalizarEstudante?.codigoAluno
       );
 
-      if (retorno.data) {
-        setSituacao(retorno.data);
+      if (retorno?.data) {
+        setSituacao(retorno?.data);
+        dispatch(setPlanoAEESituacaoEncaminhamentoAEE(retorno?.data));
       }
     };
     obtemSituacaoEncaminhamento();
-  });
+  }, [dadosCollapseLocalizarEstudante]);
 
   return situacao?.situacao ? (
     <>
