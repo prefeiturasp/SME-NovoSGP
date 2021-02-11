b1275d210e0c4a4205693b38f7a54afea5d315ac
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-02-10 22:51:17 -0300

    33315 - Alterado o link para click ao abrir a situação do encaminhamento

--- src/SME.SGP.WebClient/src/paginas/Relatorios/AEE/Plano/Cadastro/Componentes/BotaoVerSituacaoEncaminhamentoAEE/botaoVerSituacaoEncaminhamentoAEE.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/AEE/Plano/Cadastro/Componentes/BotaoVerSituacaoEncaminhamentoAEE/botaoVerSituacaoEncaminhamentoAEE.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { Colors } from '~/componentes';
 import Button from '~/componentes/button';
 import { RotasDto } from '~/dtos';
@@ -10,20 +9,24 @@ const BotaoVerSituacaoEncaminhamentoAEE = () => {
     store => store.planoAEE.planoAEESituacaoEncaminhamentoAEE
   );
 
+  const onClick = () => {
+    const win = window.open(
+      `${RotasDto.RELATORIO_AEE_ENCAMINHAMENTO}/editar/${planoAEESituacaoEncaminhamentoAEE?.id}`,
+      '_blank'
+    );
+    win.focus();
+  };
+
   return planoAEESituacaoEncaminhamentoAEE?.id ? (
-    <Link
-      to={`${RotasDto.RELATORIO_AEE_ENCAMINHAMENTO}/editar/${planoAEESituacaoEncaminhamentoAEE?.id}`}
-      target="_blank"
-    >
-      <Button
-        id="btn-voltar"
-        label="Ver detalhes do encaminhamento"
-        icon="eye"
-        color={Colors.Azul}
-        border
-        className="mr-2"
-      />
-    </Link>
+    <Button
+      id="btn-voltar"
+      label="Ver detalhes do encaminhamento"
+      icon="eye"
+      color={Colors.Azul}
+      border
+      className="mr-2"
+      onClick={onClick}
+    />
   ) : (
     ''
   );
