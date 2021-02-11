5d7471aa377e95f9941bcc161a18b2d76094a2e2
Rafael Losi <rafael.losi@amcom.com.br>
2021-02-11 13:11:07 -0300

    removido arquivos para recriar

--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/EncaminhamentoAEE/IEncerrarEncaminhamentoAEEUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SME.SGP.Aplicacao
-{
-    public interface IEncerrarEncaminhamentoAEEUseCase
-    {
-        Task<bool> Executar(long encaminhamentoId, string motivoEncerramento);
-    }
-}
--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/EncaminhamentoAEE/IRemoverResponsavelEncaminhamentoAEEUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SME.SGP.Aplicacao
-{
-    public interface IRemoverResponsavelEncaminhamentoAEEUseCase
-    {
-        Task<bool> Executar(long encaminhamentoId);
-    }
-}
