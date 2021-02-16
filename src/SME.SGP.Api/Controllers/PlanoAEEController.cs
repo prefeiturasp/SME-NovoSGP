9ecbd0a988a70392472ab13b660d18ac74178a7b
Rafael Losi <rafael.losi@amcom.com.br>
2021-02-16 10:37:26 -0300

    AB#34286 Criados mocks dos endpoints para o consumo do front

--- src/SME.SGP.Api/Controllers/PlanoAEEController.cs
+++ src/SME.SGP.Api/Controllers/PlanoAEEController.cs
@@ -81,5 +81,43 @@ namespace SME.SGP.Api.Controllers
         {
             return Ok(await useCase.Executar(codigoEstudante));
         }
+
+
+        [HttpGet("{planoAEEId}/reestruturacoes")]
+        [ProducesResponseType(typeof(IEnumerable<PlanoAEEReestruturacaoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterReestruturacoes(long planoAEEId)
+        {
+            return Ok(new List<PlanoAEEReestruturacaoDto>()
+            {
+                new PlanoAEEReestruturacaoDto() { Id = 1, Data = new DateTime(2021, 1, 10), Descricao = "Alteradas as atividades que o aluno fazia", Versao = "v1 - 05/01/2021", VersaoId = 10},
+                new PlanoAEEReestruturacaoDto() { Id = 2, Data = new DateTime(2021, 1, 31), Descricao = "Alterado o periodo de vigência do plano", Versao = "v2 - 30/01/2021", VersaoId = 11},
+                new PlanoAEEReestruturacaoDto() { Id = 3, Data = new DateTime(2021, 2, 05), Descricao = "Alterado dias e horarios do aluno no AEE", Versao = "v3 - 04/01/2021", VersaoId = 12},
+                new PlanoAEEReestruturacaoDto() { Id = 4, Data = new DateTime(2021, 2, 15), Descricao = "Alterado encaminhamento do aluno", Versao = "v4 - 13/01/2021", VersaoId = 13},
+            });
+        }
+
+        [HttpGet("{planoAEEId}/versoes")]
+        [ProducesResponseType(typeof(IEnumerable<PlanoAEEDescricaoVersaoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ObterVersoes(long planoAEEId)
+        {
+            return Ok(new List<PlanoAEEDescricaoVersaoDto>()
+            {
+                new PlanoAEEDescricaoVersaoDto() { Id = 10, Descricao = "v1 - 05/01/2021" },
+                new PlanoAEEDescricaoVersaoDto() { Id = 11, Descricao = "v2 - 30/01/2021" },
+                new PlanoAEEDescricaoVersaoDto() { Id = 12, Descricao = "v3 - 04/01/2021" },
+                new PlanoAEEDescricaoVersaoDto() { Id = 13, Descricao = "v4 - 13/01/2021" },
+            });
+        }
+
+        [HttpPost("{planoAEEId}/reestruturacoes")]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 601)]
+        public async Task<IActionResult> SalvarReestruturacao([FromBody] PlanoAEEReestrutucacaoPersistenciaDto planoAeeReestruturacaoDto)
+        {
+            return Ok();
+        }
     }
 }
---
+++ src/SME.SGP.Infra/Dtos/PlanoAEE/PlanoAEEReestruturacaoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class PlanoAEEReestruturacaoDto
+    {
+        public long Id { get; set; }
+        public DateTime Data { get; set; }
+        public long VersaoId { get; set; }
+        public string Versao { get; set; }
+        public string Descricao { get; set; }
+    }
+}
