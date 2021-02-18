0df7d35f6b8b16d92bc39b64c0e28180c7789a8e
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-02-18 18:51:27 -0300

    34170 - Correção da NotificacaoEncaminhamentoAEEDto para utilizar em outro command

---
+++ src/SME.SGP.Infra/Dtos/Notificacao/NotificacaoEncaminhamentoAEEDto.cs
@@ -0,0 +1,13 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
+
+namespace SME.SGP.Infra.Dtos
+{
+    public class NotificacaoEncaminhamentoAEEDto
+    {
+        public string UsuarioRF { get; set; }
+        public string UsuarioNome { get; set; }
+        public long EncaminhamentoAEEId { get; set; }
+
+    }
+}
