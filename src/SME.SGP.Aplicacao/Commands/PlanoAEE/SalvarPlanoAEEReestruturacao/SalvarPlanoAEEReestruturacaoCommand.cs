16690b7bda33719846db8f179a1b4bcda7325305
Rafael Losi <rafael.losi@amcom.com.br>
2021-02-19 12:00:17 -0300

    34046 validação de versão e descrição para salvar a reestruturação do plano AEE

--- src/SME.SGP.Aplicacao/Commands/PlanoAEE/SalvarPlanoAEEReestruturacao/SalvarPlanoAEEReestruturacaoCommand.cs
+++ src/SME.SGP.Aplicacao/Commands/PlanoAEE/SalvarPlanoAEEReestruturacao/SalvarPlanoAEEReestruturacaoCommand.cs
@@ -24,6 +24,14 @@ namespace SME.SGP.Aplicacao
             RuleFor(a => a.Reestruturacao)
                 .NotEmpty()
                 .WithMessage("A reestruturação do plano deve ser informada para o salvamento");
+
+            RuleFor(a => a.Reestruturacao.Descricao)
+                .NotEmpty()
+                .WithMessage("É necessário informar a descrição da reestruturação para o salvamento");
+
+            RuleFor(a => a.Reestruturacao.PlanoAEEVersaoId)
+                .NotEmpty()
+                .WithMessage("É necessário informar a versão do plano para o salvamento da reestruturação");
         }
     }
 }
