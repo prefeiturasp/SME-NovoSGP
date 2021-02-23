65c916de46a4493dbd9e3bed9ef5f96d35188668
Rodrigo Galante <rodrigo.galante@globo.com>
2021-02-23 18:08:08 -0300

    Implementação de Gerar pendência encaminhamentoAEECP

---
+++ src/SME.SGP.Aplicacao/Commands/EncaminhamentoAEE/Pendencias/GerarPendenciaCPEncaminhamentoAEE/GerarPendenciaCPEncaminhamentoAEECommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace SME.SGP.Aplicacao
+{
+    public class GerarPendenciaCPEncaminhamentoAEECommandValidator : AbstractValidator<GerarPendenciaCPEncaminhamentoAEECommand>
+    {
+        public GerarPendenciaCPEncaminhamentoAEECommandValidator()
+        {
+            RuleFor(c => c.EncaminhamentoAEEId)
+                   .NotEmpty()
+                   .WithMessage("O encaminhamento ID precisa ser informado.");
+        }
+    }
+}
--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/PendenciaEncaminhamentoAEE/IGerarPendenciaEncaminhamentoAEEParaCPUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SME.SGP.Aplicacao.Interfaces.CasosDeUso
-{
-    public interface IGerarPendenciaEncaminhamentoAEEParaCPUseCase
-    {
-        Task Executar();
-    }
-}
