3b106902ca0286d51e163395d5f9f42179fc41c8
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-03-02 15:47:33 -0300

    35224 - Refatorado o modo como estava obtendo a turma, pois estava enviando tanto a entidade turma como o turma código. Foi alterado para enviar somente turma e adicionado um validator

--- src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterBimestreAtualPorTurmaQuery/ObterBimestreAtualQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterBimestreAtualPorTurmaQuery/ObterBimestreAtualQuery.cs
@@ -6,22 +6,13 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterBimestreAtualQuery : IRequest<int>
     {
-        public ObterBimestreAtualQuery() { }
-
-        public ObterBimestreAtualQuery(string turmaCodigo, DateTime dataReferencia, Turma turma = null)
+        public ObterBimestreAtualQuery(DateTime dataReferencia, Turma turma = null)
         {
-            if (string.IsNullOrEmpty(turmaCodigo))
-                throw new NegocioException("Para obter o bimestre atual é necessário informar o código da turma");
-            if (dataReferencia == DateTime.MinValue)
-                throw new NegocioException("Para obter o bimestre atual é necessário informar a data de referência");
-
-            TurmaCodigo = turmaCodigo;
             DataReferencia = dataReferencia;
             Turma = turma;
         }
 
         public Turma Turma { get; set; }
-        public string TurmaCodigo { get; set; }
         public DateTime DataReferencia { get; set; }
     }
 }
