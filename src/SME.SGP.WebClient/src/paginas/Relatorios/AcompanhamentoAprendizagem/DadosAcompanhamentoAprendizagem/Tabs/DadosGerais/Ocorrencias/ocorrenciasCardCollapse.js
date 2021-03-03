ae3f49aaa24abe881ab4a9612dafe00da86d78f5
Cleiton <cleiton.dutra@amcom.com.br>
2021-03-03 13:52:49 -0300

    Relatório do Acompanhamento da Aprendizagem - Ocorrencias

---
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/AcompanhamentoAprendizagem/DadosAcompanhamentoAprendizagem/Tabs/DadosGerais/Ocorrencias/ocorrenciasCardCollapse.js
@@ -0,0 +1,58 @@
+import * as moment from 'moment';
+import React, { useState } from 'react';
+import { ListaPaginada } from '~/componentes';
+import CardCollapse from '~/componentes/cardCollapse';
+
+const OcorrenciasCardCollapse = () => {
+  const [exibir, setExibir] = useState(true);
+
+  const [filtro, setFiltro] = useState({});
+
+  const colunas = [
+    {
+      title: 'Data',
+      dataIndex: 'data',
+      render: data => {
+        let dataFormatada = '';
+        if (data) {
+          dataFormatada = moment(data).format('DD/MM/YYYY');
+        }
+        return <span> {dataFormatada}</span>;
+      },
+    },
+    {
+      title: 'Registrado por',
+      dataIndex: 'registradoPor',
+    },
+    {
+      title: 'Título da ocorrência',
+      dataIndex: 'tituloOcorrência',
+    },
+  ];
+
+  const onClickExpandir = () => setExibir(!exibir);
+
+  return (
+    <div className="col-md-12 mb-2">
+      <CardCollapse
+        key="ocorrencias-acompanhamento-aprendizagem-collapse"
+        onClick={onClickExpandir}
+        titulo="Ocorrências"
+        indice="ocorrencias-acompanhamento-aprendizagem"
+        show={exibir}
+        alt="ocorrencias-acompanhamento-aprendizagem"
+      >
+        <div className="col-md-12 mb-2">
+          <ListaPaginada
+            url=""
+            id="lista-ocorrencias-acompanhamento-aprendizagem"
+            colunas={colunas}
+            filtro={filtro}
+          />
+        </div>
+      </CardCollapse>
+    </div>
+  );
+};
+
+export default OcorrenciasCardCollapse;
