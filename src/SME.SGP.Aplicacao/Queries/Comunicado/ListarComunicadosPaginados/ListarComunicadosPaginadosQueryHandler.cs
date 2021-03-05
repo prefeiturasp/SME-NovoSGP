148e01eac4ceaace7aded9a247527f0d3a0dd3d6
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-03-05 20:29:49 -0300

    35086 - Criado endpoint que obtem os comunicados enviados para o aluno

---
+++ src/SME.SGP.Aplicacao/Queries/Comunicado/ListarComunicadosPaginados/ListarComunicadosPaginadosQueryHandler.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Dto;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Queries.Evento.ObterDataPossuiEventoLiberacaoExcepcional
+{
+    public class ListarComunicadosPaginadosQueryHandler : ConsultasBase, IRequestHandler<ListarComunicadosPaginadosQuery, PaginacaoResultadoDto<ComunicadoAlunoReduzidoDto>>
+    {
+        private readonly IRepositorioComunicado repositorioComunicado;
+
+        public ListarComunicadosPaginadosQueryHandler(IContextoAplicacao contextoAplicacao, IRepositorioComunicado repositorioComunicado) : base(contextoAplicacao)
+        {
+            this.repositorioComunicado = repositorioComunicado ?? throw new System.ArgumentNullException(nameof(repositorioComunicado));
+        }
+        public async Task<PaginacaoResultadoDto<ComunicadoAlunoReduzidoDto>> Handle(ListarComunicadosPaginadosQuery request, CancellationToken cancellationToken)
+        {
+            return MapearParaDto(await repositorioComunicado.ObterComunicadosReduzidos(request.DRECodigo, request.UECodigo, request.TurmaCodigo, request.AlunoCodigo, Paginacao));
+        }
+
+        private PaginacaoResultadoDto<ComunicadoAlunoReduzidoDto> MapearParaDto(PaginacaoResultadoDto<ComunicadoAlunoReduzidoDto> resultadoDto)
+        {
+            return new PaginacaoResultadoDto<ComunicadoAlunoReduzidoDto>()
+            {
+                TotalPaginas = resultadoDto.TotalPaginas,
+                TotalRegistros = resultadoDto.TotalRegistros,
+                Items = MapearComunicados(resultadoDto.Items)
+            };
+        }
+
+        private IEnumerable<ComunicadoAlunoReduzidoDto> MapearComunicados(IEnumerable<ComunicadoAlunoReduzidoDto> comunicados)
+        {
+            var listaComunicados = new List<ComunicadoAlunoReduzidoDto>();
+
+            foreach (var comunicado in comunicados)
+            {
+                listaComunicados.Add(new ComunicadoAlunoReduzidoDto()
+                {
+                    DataEnvio = comunicado.DataEnvio.Date,
+                    Titulo = comunicado.Titulo,
+                    Categoria = comunicado.Categoria,
+                    CategoriaNome = comunicado.Categoria.Name(),
+                    StatusLeitura = "Não Lida"
+                }); ;
+            }
+
+            return listaComunicados;
+        }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/ComunicadoAlunoReduzidoDto.cs
@@ -0,0 +1,19 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SME.SGP.Dto
+{
+    public class ComunicadoAlunoReduzidoDto
+    {
+        public DateTime DataEnvio { get; set; }
+        public string Titulo { get; set; }
+        public TipoComunicado Categoria { get; set; }
+        public string CategoriaNome { get; set; }
+        public string StatusLeitura { get; set; }
+    }
+}
