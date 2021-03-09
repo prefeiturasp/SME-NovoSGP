031581f1c238f325309337b82fadf70f090224c7
wagnersantos-amcom <wagner.santos@amcom.com.br>
2021-03-09 09:24:29 -0300

    Consumo de endpoint

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/Planejamento/servicoRelatorioDevolutivas.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/Planejamento/servicoRelatorioDevolutivas.js
@@ -2,7 +2,7 @@ import api from '~/servicos/api';
 
 class ServicoRelatorioDevolutivas {
   gerar = params => {
-    return api.post('', params);
+    return api.post('/v1/relatorios/devolutivas', params);
   };
 }
 
