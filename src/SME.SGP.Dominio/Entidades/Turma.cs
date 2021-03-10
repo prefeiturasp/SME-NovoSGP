02eba8dc28dad45f061be3bb70889a172de8afdb
elitonds <elitonds@gmail.com>
2021-03-10 12:02:24 -0300

    ajustes

--- src/SME.SGP.Dominio/Entidades/Turma.cs
+++ src/SME.SGP.Dominio/Entidades/Turma.cs
@@ -87,8 +87,7 @@ namespace SME.SGP.Dominio
         public bool EhTurmaEdFisicaOuItinerario()
         {
             var tipoTurma = (SME.SGP.Dominio.Enumerados.TipoTurma)TipoTurma;
-            return tipoTurma.EhUmDosValores(SME.SGP.Dominio.Enumerados.TipoTurma.EdFisica, SME.SGP.Dominio.Enumerados.TipoTurma.Itinerarios2AAno) &&
-                !tipoTurma.EhUmDosValores(SME.SGP.Dominio.Enumerados.TipoTurma.Regular);
+            return tipoTurma.EhUmDosValores(SME.SGP.Dominio.Enumerados.TipoTurma.EdFisica, SME.SGP.Dominio.Enumerados.TipoTurma.Itinerarios2AAno);
         }
     }
 }
