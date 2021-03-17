85c0c1d12308eaf5c89cd76b76a2ff92b4844073
Cleiton <cleiton.dutra@amcom.com.br>
2021-03-17 14:14:26 -0300

    Mover Registro Individual - Agora é um componente para usar em outras telas

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/RegistroIndividual/componentes/index.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/RegistroIndividual/componentes/index.js
@@ -3,4 +3,4 @@ export { default as BotoesAcoes } from './BotoesAcoes/botoesAcoes';
 export { default as BotaoOrdenarListaAlunos } from './BotaoOrdenarListaAlunos/botaoOrdenarListaAlunos';
 export { default as TabelaRetratilRegistroIndividual } from './TabelaRetratilRegistroIndividual/tabelaRetratilRegistroIndividual';
 export { default as ObjectCardRegistroIndividual } from './ObjectCardRegistroIndividual/objectCardRegistroIndividual';
-export { default as DadosRegistroIndividual } from './DadosRegistroIndividual/dadosRegistroIndividual';
+export { default as DadosRegistroIndividual } from '~/componentes-sgp/RegistroIndividual/dadosRegistroIndividual';
