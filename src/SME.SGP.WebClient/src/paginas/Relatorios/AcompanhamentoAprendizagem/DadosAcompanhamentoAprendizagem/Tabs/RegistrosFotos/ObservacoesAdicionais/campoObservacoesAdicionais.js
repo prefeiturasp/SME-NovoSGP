f602fc1a8fe82fd5a82975032210739f2f54c7f1
Cleiton <cleiton.dutra@amcom.com.br>
2021-03-22 07:12:57 -0300

    Acompanhamento Aprendizagem - Observacoes Adicionais

---
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/AcompanhamentoAprendizagem/DadosAcompanhamentoAprendizagem/Tabs/RegistrosFotos/ObservacoesAdicionais/campoObservacoesAdicionais.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { JoditEditor } from '~/componentes';
+import { setAcompanhamentoAprendizagemEmEdicao } from '~/redux/modulos/acompanhamentoAprendizagem/actions';
+import ServicoAcompanhamentoAprendizagem from '~/servicos/Paginas/Relatorios/AcompanhamentoAprendizagem/ServicoAcompanhamentoAprendizagem';
+
+const CampoObservacoesAdicionais = () => {
+  const dispatch = useDispatch();
+
+  const dadosAcompanhamentoAprendizagem = useSelector(
+    store => store.acompanhamentoAprendizagem.dadosAcompanhamentoAprendizagem
+  );
+
+  const [observacao, setObservacao] = useState();
+
+  useEffect(() => {
+    if (dadosAcompanhamentoAprendizagem.observacoes) {
+      setObservacao(dadosAcompanhamentoAprendizagem.observacoes);
+    }
+  }, [dadosAcompanhamentoAprendizagem]);
+
+  const onChange = valorNovo => {
+    ServicoAcompanhamentoAprendizagem.atualizarObservacoes(valorNovo);
+    dispatch(setAcompanhamentoAprendizagemEmEdicao(true));
+  };
+
+  return (
+    <JoditEditor
+      id="observacoes-adicionais-editor"
+      value={observacao}
+      onChange={onChange}
+    />
+  );
+};
+
+export default CampoObservacoesAdicionais;
