9312b5a1b1172d7faf3c7a6ed4aae414266170d9
Cleiton <cleiton.dutra@amcom.com.br>
2021-03-23 15:28:01 -0300

    Quando o Semestre ainda não tiver iniciado - Não exibir o filtro de datas e exibir um feedback

--- src/SME.SGP.WebClient/src/paginas/Relatorios/AcompanhamentoAprendizagem/DadosAcompanhamentoAprendizagem/Tabs/RegistrosFotos/RegistrosIndividuais/registrosIndividuais.js
+++ src/SME.SGP.WebClient/src/paginas/Relatorios/AcompanhamentoAprendizagem/DadosAcompanhamentoAprendizagem/Tabs/RegistrosFotos/RegistrosIndividuais/registrosIndividuais.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import * as moment from 'moment';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Loader } from '~/componentes';
 import { RegistrosAnterioresConteudo } from '~/componentes-sgp/RegistroIndividual/registrosAnteriores/registrosAnterioresConteudo';
@@ -6,7 +8,9 @@ import CardCollapse from '~/componentes/cardCollapse';
 import { RotasDto } from '~/dtos';
 import { limparDadosRegistroIndividual } from '~/redux/modulos/registroIndividual/actions';
 
-const RegistrosIndividuais = () => {
+const RegistrosIndividuais = props => {
+  const { semestreSelecionado } = props;
+
   const exibirLoaderGeralRegistroAnteriores = useSelector(
     store => store.registroIndividual.exibirLoaderGeralRegistroAnteriores
   );
@@ -27,6 +31,23 @@ const RegistrosIndividuais = () => {
   const permissoesTela =
     usuario.permissoes[RotasDto.ACOMPANHAMENTO_APRENDIZAGEM];
 
+  const [exibirRegistroIndividual, setExibirRegistroIndividual] = useState(
+    false
+  );
+
+  useEffect(() => {
+    if (dadosAcompanhamentoAprendizagem?.periodoInicio) {
+      const hoje = moment();
+      const periodoInicio = moment(
+        dadosAcompanhamentoAprendizagem.periodoInicio
+      );
+      const exibir = hoje.isSameOrAfter(periodoInicio);
+      setExibirRegistroIndividual(exibir);
+    } else {
+      setExibirRegistroIndividual(false);
+    }
+  }, [dadosAcompanhamentoAprendizagem]);
+
   useEffect(() => {
     return () => {
       dispatch(limparDadosRegistroIndividual());
@@ -46,16 +67,30 @@ const RegistrosIndividuais = () => {
           indice="registros-individuais"
           alt="registros-individuais"
         >
-          <RegistrosAnterioresConteudo
-            permissoesTela={permissoesTela}
-            periodoInicio={dadosAcompanhamentoAprendizagem?.periodoInicio}
-            periodoFim={dadosAcompanhamentoAprendizagem?.periodoFim}
-            podeEditar={!desabilitarCamposAcompanhamentoAprendizagem}
-          />
+          {exibirRegistroIndividual ? (
+            <RegistrosAnterioresConteudo
+              permissoesTela={permissoesTela}
+              periodoInicio={dadosAcompanhamentoAprendizagem?.periodoInicio}
+              periodoFim={dadosAcompanhamentoAprendizagem?.periodoFim}
+              podeEditar={!desabilitarCamposAcompanhamentoAprendizagem}
+            />
+          ) : (
+            <div className="text-center">
+              {`O período do ${semestreSelecionado}º semestre não foi iniciado`}
+            </div>
+          )}
         </CardCollapse>
       </div>
     </Loader>
   );
+};
+
+RegistrosIndividuais.propTypes = {
+  semestreSelecionado: PropTypes.string,
+};
+
+RegistrosIndividuais.defaultProps = {
+  semestreSelecionado: '',
 };
 
 export default RegistrosIndividuais;
