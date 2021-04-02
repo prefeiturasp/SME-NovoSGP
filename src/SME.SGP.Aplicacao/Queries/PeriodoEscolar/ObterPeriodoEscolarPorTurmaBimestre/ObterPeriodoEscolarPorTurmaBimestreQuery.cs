e094d6ea047e472d7ee77e963daadc499af4b25b
William Manoel dos Santos <william.santos@amcom.com.br>
2021-04-02 12:11:57 -0300

    Ajuste na validação dos bimestres

--- src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterPeriodoEscolarPorTurmaBimestre/ObterPeriodoEscolarPorTurmaBimestreQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterPeriodoEscolarPorTurmaBimestre/ObterPeriodoEscolarPorTurmaBimestreQuery.cs
@@ -25,7 +25,7 @@ namespace SME.SGP.Aplicacao
                .WithMessage("A turma deve ser informada para consulta do periodo escolar.");
 
             RuleFor(c => c.Bimestre)
-               .NotNull()
+               .InclusiveBetween(0,4)
                .WithMessage("O bimestre deve ser informado para consulta do periodo escolar.");
         }
     }
