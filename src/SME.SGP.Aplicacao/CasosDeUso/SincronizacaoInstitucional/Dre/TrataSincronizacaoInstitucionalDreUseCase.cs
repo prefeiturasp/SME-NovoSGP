7578e40e2909ece1e2c158229361cb1aa6dbfc05
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-04-07 16:49:29 -0300

    Ajustes de nomeclatura

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/SincronizacaoInstitucional/Dre/TrataSincronizacaoInstitucionalDreUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class TrataSincronizacaoInstitucionalDreUseCase : AbstractUseCase, ITrataSincronizacaoInstitucionalDreUseCase
+    {
+        public TrataSincronizacaoInstitucionalDreUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public Task<bool> Executar(MensagemRabbit param)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+} 
