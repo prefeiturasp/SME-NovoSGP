ede25bc677588366644e0690e1ce2c999fa953e2
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-04-10 18:11:24 -0300

    37421 - Criado fluxo para enviar notificação para CP e AD sobre itinerância registrada

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Itinerancia/Notificacao/NotificacaoSalvarItineranciaUseCase.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class NotificacaoSalvarItineranciaUseCase : AbstractUseCase, INotificacaoSalvarItineranciaUseCase
+    {
+        public NotificacaoSalvarItineranciaUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(MensagemRabbit param)
+        {
+            var mensagemRabbit = JsonConvert.DeserializeObject<NotificacaoSalvarItineranciaDto>(param.Mensagem.ToString());
+
+            if (mensagemRabbit.Estudantes.Any())
+            {
+                await mediator.Send(new NotificacaoSalvarItineranciaAlunosCommand(mensagemRabbit.Ues.FirstOrDefault().CodigoUe, mensagemRabbit.CriadoRF, mensagemRabbit.CriadoPor,  mensagemRabbit.DataVisita, mensagemRabbit.Estudantes));
+            }
+
+            return true;
+        }
+    }
+}
