5115fdc377efbec2a75295754e153de98b3f2772
Carlos Dias <carlosafdias@outlook.com>
2021-04-11 16:58:48 -0300

    CM - Ajuste do Endpoint de consulta modalidades por UE (UeController)

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/UE/ObterModalidadesPorUe/ObterModalidadesPorUeUseCase.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterModalidadesPorUeUseCase : IObterModalidadesPorUeUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ObterModalidadesPorUeUseCase(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<IEnumerable<ModalidadeRetornoDto>> Executar(string ueCodigo, int anoLetivo, bool consideraNovasModalidades)
+        {
+            var modadlidadesQueSeraoIgnoradas = await ObterModalidadesQueSeraoIgnoradas(anoLetivo, consideraNovasModalidades);
+            return await mediator.Send(new ObterModalidadesPorUeEAnoLetivoQuery(ueCodigo, anoLetivo, modadlidadesQueSeraoIgnoradas));
+        }
+
+        private async Task<IEnumerable<Modalidade>> ObterModalidadesQueSeraoIgnoradas(int anoLetivo, bool consideraNovasModalidades)
+        {
+            if (consideraNovasModalidades) return null;
+            return await mediator.Send(new ObterNovasModalidadesPorAnoQuery(anoLetivo));
+        }
+    }
+}
--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasUe.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultasUe.cs
@@ -7,8 +7,6 @@ namespace SME.SGP.Aplicacao
 {
     public interface IConsultasUe
     {
-        Task<IEnumerable<ModalidadeRetornoDto>> ObterModalidadesPorUe(string ueCodigo, int ano);
-
         Task<IEnumerable<TurmaRetornoDto>> ObterTurmas(string ueCodigo, int modalidadeId, int ano);
 
         Ue ObterPorId(long id);
