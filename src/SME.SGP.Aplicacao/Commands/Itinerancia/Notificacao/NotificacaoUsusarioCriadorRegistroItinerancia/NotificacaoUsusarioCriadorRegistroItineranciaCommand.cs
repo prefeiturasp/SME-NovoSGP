de33560e0e83f682097f434cfdb4a393552b9b69
Rodrigo Galante <rodrigo.galante@globo.com>
2021-04-12 15:49:34 -0300

    Criação da notificação

---
+++ src/SME.SGP.Aplicacao/Commands/Itinerancia/Notificacao/NotificacaoUsusarioCriadorRegistroItinerancia/NotificacaoUsusarioCriadorRegistroItineranciaCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class NotificacaoUsusarioCriadorRegistroItineranciaCommand : IRequest<bool>
+    {
+        public NotificacaoUsusarioCriadorRegistroItineranciaCommand(string ueCodigo, string criadoRF, string criadoPor, DateTime dataVisita, IEnumerable<ItineranciaAlunoDto> estudantes)
+        {
+            UeCodigo = ueCodigo;
+            CriadoRF = criadoRF;
+            CriadoPor = criadoPor;
+            DataVisita = dataVisita;
+            Estudantes = estudantes;
+        }
+
+        public string UeCodigo { get; set; }
+        public string CriadoRF { get; set; }
+        public string CriadoPor { get; set; }
+        public DateTime DataVisita { get; set; }
+
+        public IEnumerable<ItineranciaAlunoDto> Estudantes { get; set; }
+
+    }
+}
