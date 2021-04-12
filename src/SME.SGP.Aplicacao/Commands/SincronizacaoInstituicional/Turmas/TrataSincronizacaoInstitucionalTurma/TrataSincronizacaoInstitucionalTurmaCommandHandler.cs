c9cce4fd0e3b16d7e4f9f7cea67fa62fdacc67e7
William Manoel dos Santos <william.santos@amcom.com.br>
2021-04-12 18:56:06 -0300

    Inicio Dev - Fila de turma

---
+++ src/SME.SGP.Aplicacao/Commands/SincronizacaoInstituicional/Turmas/TrataSincronizacaoInstitucionalTurma/TrataSincronizacaoInstitucionalTurmaCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Enumerados;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class TrataSincronizacaoInstitucionalTurmaCommandHandler : IRequestHandler<TrataSincronizacaoInstitucionalTurmaCommand, bool>
+    {
+        private readonly IRepositorioTurma repositorioTurma;
+
+        public TrataSincronizacaoInstitucionalTurmaCommandHandler(IRepositorioTurma repositorioTurma)
+        {
+            this.repositorioTurma = repositorioTurma ?? throw new ArgumentNullException(nameof(repositorioTurma));
+        }
+
+        public Task<bool> Handle(TrataSincronizacaoInstitucionalTurmaCommand request, CancellationToken cancellationToken)
+        {
+            // TODO : Verificar se a turma está concluída para marcar como histórica
+            var turma = request.Turma;
+
+            if(turma.Situacao == "C")
+            {
+                // MArcar como histórica
+            }
+
+            // TODO : Verificar se não tiver calendário cadastrado e a turma for extinta deve excluir direto 
+
+            // TODO : Obter o tipo de calendário pela modalidade da turma 
+
+            // - Obter os periodos do tipo de calendário
+            // - Obter o primeiro periodo e sua data inicial 
+            // - Se a Data da extinção da turma for menor que a Inicial do periodo, deve-se deletar a turma do SGP 
+            //   caso contrário, deve-se marcar a turma como histórica
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Enumerados/SituacaoTurma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio.Enumerados
+{
+    public enum SituacaoTurma
+    {
+        Ativa = 1,
+        Extinta = 2,
+        Concluida = 3
+
+    }
+}
