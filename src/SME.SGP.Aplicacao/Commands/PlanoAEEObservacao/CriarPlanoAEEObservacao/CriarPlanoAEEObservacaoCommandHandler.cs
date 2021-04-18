418e7e36861e5e2f78a0981bfe00f86c3bcd6a36
Rafael Losi <rafael.losi@amcom.com.br>
2021-04-18 15:35:50 -0300

    37585 enpoint Criar Observação Plano AEE

---
+++ src/SME.SGP.Aplicacao/Commands/PlanoAEEObservacao/CriarPlanoAEEObservacao/CriarPlanoAEEObservacaoCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class CriarPlanoAEEObservacaoCommandHandler : IRequestHandler<CriarPlanoAEEObservacaoCommand, AuditoriaDto>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IRepositorioPlanoAEEObservacao repositorioPlanoAEEObservacao;
+
+        public CriarPlanoAEEObservacaoCommandHandler(IUnitOfWork unitOfWork, IRepositorioPlanoAEEObservacao repositorioPlanoAEEObservacao)
+        {
+            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            this.repositorioPlanoAEEObservacao = repositorioPlanoAEEObservacao ?? throw new ArgumentNullException(nameof(repositorioPlanoAEEObservacao));
+        }
+
+        public async Task<AuditoriaDto> Handle(CriarPlanoAEEObservacaoCommand request, CancellationToken cancellationToken)
+        {
+            var observacao = new PlanoAEEObservacao(request.PlanoAEEId, request.Observacao);
+
+            using (var transacao = unitOfWork.IniciarTransacao())
+            {
+                try
+                {
+                    await repositorioPlanoAEEObservacao.SalvarAsync(observacao);
+                    // Gerar notificação 
+
+                    unitOfWork.PersistirTransacao();
+                }
+                catch (Exception e)
+                {
+                    unitOfWork.Rollback();
+                    throw;
+                }                
+            }
+
+            return (AuditoriaDto)observacao;
+        }
+    }
+}
--- src/SME.SGP.Dominio/Entidades/PlanoAEEObservacao.cs
+++ src/SME.SGP.Dominio/Entidades/PlanoAEEObservacao.cs
@@ -6,6 +6,13 @@ namespace SME.SGP.Dominio
 {
     public class PlanoAEEObservacao : EntidadeBase
     {
+        public PlanoAEEObservacao() { }
+        public PlanoAEEObservacao(long planoAEEId, string observacao)
+        {
+            PlanoAEEId = planoAEEId;
+            Observacao = observacao;
+        }
+
         public PlanoAEE PlanoAEE { get; set; }
         public long PlanoAEEId { get; set; }
         public string Observacao { get; set; }
