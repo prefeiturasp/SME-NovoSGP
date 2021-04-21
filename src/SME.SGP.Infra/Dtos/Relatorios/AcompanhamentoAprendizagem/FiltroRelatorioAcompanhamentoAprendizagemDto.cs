ee9969f833b383daed55f25482e4ec32884ccf46
William Manoel dos Santos <william.santos@amcom.com.br>
2021-04-21 16:21:05 -0300

    Estrutura do relatório

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/AcompanhamentoAprendizagem/FiltroRelatorioAcompanhamentoAprendizagemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Infra
+{
+    public class FiltroRelatorioAcompanhamentoAprendizagemDto
+    {
+        public string TurmaCodigo { get; set; }
+        public long? AlunoCodigo { get; set; }
+    }
+}
