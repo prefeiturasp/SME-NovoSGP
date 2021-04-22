cda493882414edbdf64088e46bcb53bbecfe50fb
Robson <robson.silva@amcom.com.br>
2021-04-22 07:41:05 -0300

    Bug: 36763 Inclusão da funcionalidade que verifica a validade do registro de processo executando para regulação da execução de cálculo de frequência.

--- src/SME.SGP.Dominio/Enumerados/TipoParametroSistema.cs
+++ src/SME.SGP.Dominio/Enumerados/TipoParametroSistema.cs
@@ -95,6 +95,9 @@ namespace SME.SGP.Dominio
         [Display(Name = "Pendência por ausência de registro individual")]        
         PendenciaPorAusenciaDeRegistroIndividual = 48,
 
+        // Processo Executando
+        TempoValidadeProcessoExecutandoEmSegundos = 49,
+
         //Sistema
         HabilitarServicosEmBackground = 100
     }
