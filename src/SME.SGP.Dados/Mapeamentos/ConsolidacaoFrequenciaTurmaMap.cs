4e14628ce4170f6ee19fcd1a87eb34ea5f7c991e
Rodrigo Galante <rodrigo.galante@globo.com>
2021-04-30 09:15:05 -0300

    Modelagem base para o dashboard

---
+++ src/SME.SGP.Dados/Mapeamentos/ConsolidacaoFrequenciaTurmaMap.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class ConsolidacaoFrequenciaTurmaMap : DommelEntityMap<ConsolidacaoFrequenciaTurma>
+    {
+        public ConsolidacaoFrequenciaTurmaMap()
+        {
+            ToTable("consolidacao_frequencia_turma");
+            
+            Map(c => c.Id).ToColumn("id");
+            Map(c => c.Modalidade).ToColumn("modalidade");
+            Map(c => c.Semestre).ToColumn("semestre");
+        }
+    }
+
+    
+}
