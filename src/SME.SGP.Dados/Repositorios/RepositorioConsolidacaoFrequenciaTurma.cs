71fed6284328ca398b2af0ceac0a0e020526b391
elitonds <elitonds@gmail.com>
2021-05-03 19:00:28 -0300

    38527 - Permissionamento e ajuste de endpoint

--- src/SME.SGP.Dados/Repositorios/RepositorioConsolidacaoFrequenciaTurma.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioConsolidacaoFrequenciaTurma.cs
@@ -10,7 +10,7 @@ namespace SME.SGP.Dados.Repositorios
     {
         protected readonly ISgpContext database;
 
-        protected RepositorioConsolidacaoFrequenciaTurma(ISgpContext database)
+        public RepositorioConsolidacaoFrequenciaTurma(ISgpContext database)
         {
             this.database = database;
         }
@@ -18,31 +18,35 @@ namespace SME.SGP.Dados.Repositorios
         private string ObterWhereFrequenciaGlobalPorAno(long dreId, long ueId, Modalidade? modalidade)
         {
             var subQuery = "";
-            if (dreId > 0) subQuery += " and cft.dre_id = @dreId";
-            if (ueId > 0) subQuery += " and cft.ue_id = @ueId";
-            if (modalidade > 0) subQuery += " and cft.modalidade  = @modalidade";
+            if (dreId > 0) subQuery += " and dre.id = @dreId";
+            if (ueId > 0) subQuery += " and ue.id = @ueId";
+            if (modalidade > 0) subQuery += " and t.modalidade_codigo  = @modalidade";
             return subQuery;
         }
 
         public async Task<IEnumerable<FrequenciaGlobalPorAnoDto>> ObterFrequenciaGlobalPorAnoAsync(int anoLetivo, long dreId, long ueId, Modalidade? modalidade)
         {
             var sql = $@"select * from (
-	                        select cft.modalidade, 
+	                        select t.modalidade_codigo as modalidade, 
 		                           t.ano, 
 		                           cft.quantidade_acima_minimo_frequencia as quantidade , 
 		                           'Qtd. acima do mínimo de frequencia' as descricao
 	                          from consolidacao_frequencia_turma cft
 	                         inner join turma t on t.id = cft.turma_id
+                             inner join ue on ue.id = t.ue_id 
+                             inner join dre on dre.id = ue.dre_id 
 	                         where quantidade_acima_minimo_frequencia > 0
 	                           and t.ano_letivo = @anoLetivo
                               {ObterWhereFrequenciaGlobalPorAno(dreId, ueId, modalidade)}
                              union 
-                     	    select cft.modalidade, 
+                     	    select t.modalidade_codigo as modalidade, 
 		                           t.ano, 
 		                           cft.quantidade_abaixo_minimo_frequencia as quantidade, 
 		                           'Qtd. abaixo do mínimo de frequencia' as descricao
 	                          from consolidacao_frequencia_turma cft
 	                         inner join turma t on t.id = cft.turma_id
+                             inner join ue on ue.id = t.ue_id 
+                             inner join dre on dre.id = ue.dre_id 
 	                         where quantidade_abaixo_minimo_frequencia > 0
 	                           and t.ano_letivo = @anoLetivo
                                 {ObterWhereFrequenciaGlobalPorAno(dreId, ueId, modalidade)}
