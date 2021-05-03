41a882be2729cb9afc5aa98ae9b632ec18a40251
Rafael Losi <rafael.losi@amcom.com.br>
2021-05-03 17:09:51 -0300

    39012 Ajuste na modelagem da consolidação de Frequencia fazendo referencia a Turma

--- src/SME.SGP.Dados/Mapeamentos/ConsolidacaoFrequenciaTurmaMap.cs
+++ src/SME.SGP.Dados/Mapeamentos/ConsolidacaoFrequenciaTurmaMap.cs
@@ -8,16 +8,7 @@ namespace SME.SGP.Dados.Mapeamentos
         public ConsolidacaoFrequenciaTurmaMap()
         {
             ToTable("consolidacao_frequencia_turma");
-            
-            Map(c => c.Id).ToColumn("id");
-            Map(c => c.Modalidade).ToColumn("modalidade");
-            Map(c => c.AnoLetivo).ToColumn("ano_letivo");
-            Map(c => c.Semestre).ToColumn("semestre");
-            Map(c => c.CodigoDre).ToColumn("codigo_dre");
-            Map(c => c.SiglaDre).ToColumn("sigla_dre"); 
-            Map(c => c.CodigoUe).ToColumn("codigo_ue");
-            Map(c => c.SiglaUe).ToColumn("sigla_ue");
-            Map(c => c.CodigoTurma).ToColumn("codigo_turma");
+            Map(c => c.TurmaId).ToColumn("turma_id");
             Map(c => c.QuantidadeAcimaMinimoFrequencia).ToColumn("quantidade_acima_minimo_frequencia");
             Map(c => c.QuantidadeAbaixoMinimoFrequencia).ToColumn("quantidade_abaixo_minimo_frequencia");
 
