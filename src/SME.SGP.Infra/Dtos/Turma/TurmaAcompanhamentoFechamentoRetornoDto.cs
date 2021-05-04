f23f3bb33f32c2164cba181013c091235f4cad33
William Manoel dos Santos <william.santos@amcom.com.br>
2021-05-04 16:54:35 -0300

    Criar mock fechamentos/acompanhamentos/turmas

---
+++ src/SME.SGP.Infra/Dtos/Turma/TurmaAcompanhamentoFechamentoRetornoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Infra
+{
+    public class TurmaAcompanhamentoFechamentoRetornoDto
+    {
+        public long TurmaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
