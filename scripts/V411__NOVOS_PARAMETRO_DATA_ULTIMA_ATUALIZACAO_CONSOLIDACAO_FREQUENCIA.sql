868808bc60636ffe764df42ba4847cc7bdf95083
Rodrigo Galante <rodrigo.galante@globo.com>
2021-05-05 10:35:45 -0300

    inclusão do ano de 2014

--- scripts/V411__NOVOS_PARAMETRO_DATA_ULTIMA_ATUALIZACAO_CONSOLIDACAO_FREQUENCIA.sql
+++ scripts/V411__NOVOS_PARAMETRO_DATA_ULTIMA_ATUALIZACAO_CONSOLIDACAO_FREQUENCIA.sql
@@ -15,3 +15,6 @@ values('ExecucaoConsolidacaoFrequenciaTurma', 58, 'Data da última execução da
 
 insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
 values('ExecucaoConsolidacaoFrequenciaTurma', 58, 'Data da última execução da rotina de consolidação de frequência', '', 2015, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('ExecucaoConsolidacaoFrequenciaTurma', 58, 'Data da última execução da rotina de consolidação de frequência', '', 2014, true, now(), 'SISTEMA', '0');
