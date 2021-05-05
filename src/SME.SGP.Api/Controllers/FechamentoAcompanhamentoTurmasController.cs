50c2c6c01e61558e411709f3578fe569b87a5656
William Manoel dos Santos <william.santos@amcom.com.br>
2021-05-05 15:40:57 -0300

    Ajuste de conflito

--- src/SME.SGP.Api/Controllers/FechamentoAcompanhamentoTurmasController.cs
+++ src/SME.SGP.Api/Controllers/FechamentoAcompanhamentoTurmasController.cs
@@ -1,14 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using SME.SGP.Api.Filtros;
+﻿using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Aplicacao;
-using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Dominio;
-using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos.FechamentoAcompanhamentoTurmas;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api
@@ -24,86 +19,7 @@ namespace SME.SGP.Api
         //[Permissao(Permissao.RI_C, Policy = "Bearer")]
         public async Task<IActionResult> ListaTurmas([FromQuery] FiltroAcompanhamentoFechamentoTurmasDto filtro, [FromServices] IObterTurmasFechamentoAcompanhamentoUseCase useCase)
         {
-            return Ok(await useCase.Executar(filtro));            
-            var listaPaginada = new PaginacaoResultadoDto<TurmaAcompanhamentoFechamentoRetornoDto>()
-            {
-                TotalPaginas = 10,
-                TotalRegistros = 10,
-                Items = new List<TurmaAcompanhamentoFechamentoRetornoDto>()
-                {
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 123456,
-                        Nome = "Turma 1A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 345689,
-                        Nome = "Turma 1B",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 453567,
-                        Nome = "Turma 1C",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 3457435,
-                        Nome = "Turma 2A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 765432,
-                        Nome = "Turma 2A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 123456,
-                        Nome = "Turma 2B",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 098789,
-                        Nome = "Turma 2C",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 434564,
-                        Nome = "Turma 3A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 941267,
-                        Nome = "Turma 4A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                       TurmaId = 342367,
-                        Nome = "Turma 4B",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 675456,
-                        Nome = "Turma 5A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 543234,
-                        Nome = "Turma 5B",
-                    },
-                     new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 675456,
-                        Nome = "Turma 6A",
-                    },
-                    new TurmaAcompanhamentoFechamentoRetornoDto()
-                    {
-                        TurmaId = 543234,
-                        Nome = "Turma 6B",
-                    }
-                }
-            };
-            return Ok(listaPaginada);
+            return Ok(await useCase.Executar(filtro));
         }
 
         [HttpGet("turmas/{turmaId}/fechamentos/bimestres/{bimestre}")]
