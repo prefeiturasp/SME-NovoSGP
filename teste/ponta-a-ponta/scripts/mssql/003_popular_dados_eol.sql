5fa1a69ab186322f0ccdb4201a9905badf5458ce
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-05-12 19:04:29 -0300

    38882 - Adicionado novos scripts

--- teste/ponta-a-ponta/scripts/mssql/003_popular_dados_eol.sql
+++ teste/ponta-a-ponta/scripts/mssql/003_popular_dados_eol.sql
@@ -1,112 +1,131 @@
 use se1426;
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(1,1180827,'Usuário 1180827','F','1962-07-22','',null,null);
+values(1,1180827,'Usuário 1180827','F','1962-07-22','1180827',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(2,5888085,'Usuário 5888085','F','1962-07-22','',null,null);
+values(2,5888085,'Usuário 5888085','F','1962-07-22','5888085',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(3,5996236,'Usuário 5996236','F','1962-07-22','',null,null);
+values(3,5996236,'Usuário 5996236','F','1962-07-22','5996236',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(4,6291732,'Usuário 6291732','F','1962-07-22','',null,null);
+values(4,6291732,'Usuário 6291732','F','1962-07-22','6291732',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(5,6347380,'Usuário 6347380','F','1962-07-22','',null,null);
+values(5,6347380,'Usuário 6347380','F','1962-07-22','6347380',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(6,6517579,'Usuário 6517579','F','1962-07-22','',null,null);
+values(6,6517579,'Usuário 6517579','F','1962-07-22','6517579',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(7,6769195,'Usuário 6769195','F','1962-07-22','',null,null);
+values(7,6769195,'Usuário 6769195','F','1962-07-22','6769195',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
 values(8,6775861,'Usuário 6775861','F','1962-07-22','',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(9,6801811,'Usuário 6801811','F','1962-07-22','',null,null);
+values(9,6801811,'Usuário 6801811','F','1962-07-22','6801811',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(10,6904955,'Usuário 6904955','F','1962-07-22','',null,null);
+values(10,6904955,'Usuário 6904955','F','1962-07-22','6904955',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(11,6917461,'Usuário 6917461','F','1962-07-22','',null,null);
+values(11,6917461,'Usuário 6917461','F','1962-07-22','6917461',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(12,6955207,'Usuário 6955207','F','1962-07-22','',null,null);
+values(12,6955207,'Usuário 6955207','F','1962-07-22','6955207',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(13,6997201,'Usuário 6997201','F','1962-07-22','',null,null);
+values(13,6997201,'Usuário 6997201','F','1962-07-22','6997201',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(14,7217684,'Usuário 7217684','F','1962-07-22','',null,null);
+values(14,7217684,'Usuário 7217684','F','1962-07-22','7217684',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(15,7220502,'Usuário 7220502','F','1962-07-22','',null,null);
+values(15,7220502,'Usuário 7220502','F','1962-07-22','7220502',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(16,7237871,'Usuário 7237871','F','1962-07-22','',null,null);
+values(16,7237871,'Usuário 7237871','F','1962-07-22','7237871',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(17,7247206,'Usuário 7247206','F','1962-07-22','',null,null);
+values(17,7247206,'Usuário 7247206','F','1962-07-22','7247206',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(18,7250461,'Usuário 7250461','F','1962-07-22','',null,null);
+values(18,7250461,'Usuário 7250461','F','1962-07-22','7250461',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(19,7252684,'Usuário 7252684','F','1962-07-22','',null,null);
+values(19,7252684,'Usuário 7252684','F','1962-07-22','7252684',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(20,7268149,'Usuário 7268149','F','1962-07-22','',null,null);
+values(20,7268149,'Usuário 7268149','F','1962-07-22','7268149',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(21,7288417,'Usuário 7288417','F','1962-07-22','',null,null);
+values(21,7288417,'Usuário 7288417','F','1962-07-22','7288417',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(22,7551487,'Usuário 7551487','F','1962-07-22','',null,null);
+values(22,7551487,'Usuário 7551487','F','1962-07-22','7551487',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(23,7766980,'Usuário 7766980','F','1962-07-22','',null,null);
+values(23,7766980,'Usuário 7766980','F','1962-07-22','7766980',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(24,7806809,'Usuário 7806809','F','1962-07-22','',null,null);
+values(24,7806809,'Usuário 7806809','F','1962-07-22','7806809',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(25,7902964,'Usuário 7902964','F','1962-07-22','',null,null);
+values(25,7902964,'Usuário 7902964','F','1962-07-22','7902964',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(26,7909179,'Usuário 7909179','F','1962-07-22','',null,null);
+values(26,7909179,'Usuário 7909179','F','1962-07-22','7909179',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(27,7913648,'Usuário 7913648','F','1962-07-22','',null,null);
+values(27,7913648,'Usuário 7913648','F','1962-07-22','7913648',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(28,7916477,'Usuário 7916477','F','1962-07-22','',null,null);
+values(28,7916477,'Usuário 7916477','F','1962-07-22','7916477',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(29,7924488,'Usuário 7924488','F','1962-07-22','',null,null);
+values(29,7924488,'Usuário 7924488','F','1962-07-22','7924488',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(30,7941706,'Usuário 7941706','F','1962-07-22','',null,null);
+values(30,7941706,'Usuário 7941706','F','1962-07-22','7941706',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(31,7942630,'Usuário 7942630','F','1962-07-22','',null,null);
+values(31,7942630,'Usuário 7942630','F','1962-07-22','7942630',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(32,8003530,'Usuário 8003530','F','1962-07-22','',null,null);
+values(32,8003530,'Usuário 8003530','F','1962-07-22','8003530',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values (33,8026637,'Usuário 8026637','F','1962-07-22','',null,null);
+values (33,8026637,'Usuário 8026637','F','1962-07-22','8026637',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(34,8129142,'Usuário 8129142','F','1962-07-22','',null,null);
+values(34,8129142,'Usuário 8129142','F','1962-07-22','8129142',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(35,8207607,'Usuário 8207607','F','1962-07-22','',null,null);
+values(35,8207607,'Usuário 8207607','F','1962-07-22','8207607',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(36,8279250,'Usuário 8279250','F','1962-07-22','',null,null);
+values(36,8279250,'Usuário 8279250','F','1962-07-22','8279250',null,null);
 
 insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
-values(37,8417211,'Usuário 8417211','F','1962-07-22','',null,null);
+values(37,8417211,'Usuário 8417211','F','1962-07-22','8417211',null,null);
+
+
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], 
+[tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], 
+[tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], 
+[dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], 
+[cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], 
+[cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) 
+VALUES (N'017442', N'ANTONIO SAMPAIO, VER.', N'ANTONIO SAMPAIO, VER', 3, 1, 35207676, N'108800', N'162800000490000', 
+4320, 57, 1, N'000101151454', N'00033477', N'108800', 81, NULL, NULL, N'VOLUNTÁRIOS DA PÁTRIA', N'733   ', NULL, 
+N'SANTANA', 2011000, 198846, 315, NULL, N'U', CAST(N'2016-05-17T10:44:22.860' AS DateTime), NULL, 70, 3550308, 45, 
+CAST(-46.626685 AS Numeric(9, 6)), CAST(-23.514191 AS Numeric(9, 6)))
+
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], [tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], [tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], [dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], [cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], [cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) VALUES (N'019262', N'BUTANTA', N'BUTANTA', 3, 1, 35056996, N'108100', N'162100000790200', 416, NULL, 1, NULL, N'00053637', N'108100', 8, NULL, NULL, N'ENGENHEIRO HEITOR ANTONIO EIRAS GARCIA', N'1870  ', NULL, N'JARDIM ESMERALDA', 5588001, 86037, 308, NULL, N'U', CAST(N'2012-08-06T09:13:15.453' AS DateTime), NULL, 67, 3550308, 50, CAST(-46.750786 AS Numeric(9, 6)), CAST(-23.582405 AS Numeric(9, 6)))
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], [tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], [tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], [dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], [cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], [cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) VALUES (N'019375', N'PARAISOPOLIS', N'PARAISOPOLIS', 3, 1, 35413008, N'108200', N'162200001810300', 371754, 32, 1, N'000000811492', N'00161941', N'108200', 81, NULL, NULL, N'DOUTOR JOSÉ AUGUSTO DE SOUZA E SILVA', N'S/N   ', NULL, N'JARDIM PARQUE MORUMBI', 5712040, NULL, 384, NULL, N'U', CAST(N'2020-08-07T14:00:55.140' AS DateTime), NULL, 83, 3550308, 57, CAST(-46.727379 AS Numeric(9, 6)), CAST(-23.622254 AS Numeric(9, 6)))
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], [tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], [tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], [dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], [cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], [cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) VALUES (N'019493', N'CAPAO REDONDO ', N'CAPAO REDONDO', 3, 1, 35574211, N'108200', N'162200000020000', 27965985, 80, 1, N'000012049623', N'00296826', N'108200', 8, NULL, NULL, N'COMENDADOR SANTANNA', N'745   ', NULL, N'CAPÃO REDONDO', 5866000, NULL, 71, NULL, N'U', CAST(N'2019-05-10T10:24:35.997' AS DateTime), NULL, 19, 3550308, 57, CAST(-46.773489 AS Numeric(9, 6)), CAST(-23.673572 AS Numeric(9, 6)))
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], [tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], [tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], [dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], [cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], [cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) VALUES (N'019506', N'MILTON PEREIRA COSTA', N'VILA JACUI', 3, 1, 35560455, N'109300', N'163300001280000', 28624557, 91, 1, N'000725318007', N'00242827', N'109300', 81, NULL, NULL, N'HENRIQUE XAVIER DE BRITO', N'221   ', NULL, N'JARDIM PEDRO JOSÉ NUNES', 8061015, NULL, 403, NULL, N'U', CAST(N'2019-05-14T07:28:34.120' AS DateTime), NULL, 87, 3550308, 63, CAST(-46.466948 AS Numeric(9, 6)), CAST(-23.500383 AS Numeric(9, 6)))
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], [tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], [tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], [dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], [cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], [cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) VALUES (N'094668', N'DERVILLE ALLEGRETTI, PROF.', N'PROF. DERVILLE ALLEGRETTI', 3, 1, 35070178, N'108800', N'162800000530000', 4321, 175, 1, N'000101151373', N'00001763', N'108800', 81, NULL, NULL, N'VOLUNTARIOS DA PATRIA', N'777   ', NULL, N'SANTANA', 2011000, 198846, 315, NULL, N'U', CAST(N'2018-05-10T15:47:59.553' AS DateTime), NULL, 70, 3550308, 45, CAST(-46.626707 AS Numeric(9, 6)), CAST(-23.513611 AS Numeric(9, 6)))
+INSERT [dbo].[v_cadastro_unidade_educacao] ([cd_unidade_educacao], [nm_unidade_educacao], [nm_exibicao_unidade], [cd_autorizacao], [tp_unidade_educacao], [cd_cie_unidade_educacao], [cd_unidade_administrativa_referencia], [cd_endereco_grh], [ci_endereco], [qt_funcionario], [tp_situacao_unidade], [nr_rgi_sabesp], [cd_unidade_educacao_prodesp], [cd_unidade_administrativa_portal], [tp_logradouro], [dc_ponto_referencia], [sg_titulo_logradouro], [nm_logradouro], [cd_nr_endereco], [dc_complemento_endereco], [nm_bairro], [cd_cep], [cd_logradouro], [cd_micro_regiao], [cd_sql_endereco], [tp_localizacao_endereco], [dt_atualizacao_endereco], [cd_caixa_postal], [cd_distrito_mec], [cd_municipio], [cd_sub_prefeitura], [cd_coordenada_geo_x], [cd_coordenada_geo_y]) VALUES (N'094765', N'MAXIMO DE MOURA SANTOS, PROF.', N'PROF. MAXIMO DE MOURA SANTOS', 3, 1, 35054525, N'108800', N'162800000510000', 3784, 71, 1, N'000097851671', N'00001462', N'108800', 81, NULL, NULL, N'JULIA LOPES DE ALMEIDA', N'275   ', NULL, N'VILA PAULICEIA', 2301000, 6823, 320, NULL, N'U', CAST(N'2017-05-25T16:54:35.633' AS DateTime), NULL, 70, 3550308, 45, CAST(-46.613710 AS Numeric(9, 6)), CAST(-23.489517 AS Numeric(9, 6)))
