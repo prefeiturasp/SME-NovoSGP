aa52a56f862581ff88ef9c8da8e881d059feda63
Carlos Dias <carlosafdias@outlook.com>
2021-05-12 18:01:16 -0300

    CM - Endpoint de quantidade de diarios de bordo com devolutiva e com devolutivas pendentes por turma/ano

---
+++ src/SME.SGP.Infra/Dtos/Grafico/Devolutivas/FiltroGraficoDiariosDeBordoComDevolutivaEDevolutivaPendenteDto.cs
@@ -0,0 +1,17 @@
+﻿using SME.SGP.Dominio;
+using System.ComponentModel.DataAnnotations;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroGraficoDiariosDeBordoComDevolutivaEDevolutivaPendenteDto
+    {
+        [Required]
+        public int AnoLetivo { get; set; }
+
+        [Required]
+        public Modalidade Modalidade { get; set; }
+
+        public long? DreId { get; set; }
+        public long? UeId { get; set; }
+    }
+}
