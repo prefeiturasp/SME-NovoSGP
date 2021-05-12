5ed68f7736b97c82cf736f51d4e359223421b349
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-05-12 11:02:19 -0300

    38882 - Adicionado script para popular tabela do eol

---
+++ teste/ponta-a-ponta/scripts/mssql/003_popular_dados_eol.sql
@@ -0,0 +1,112 @@
+use se1426;
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(1,1180827,'Usuário 1180827','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(2,5888085,'Usuário 5888085','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(3,5996236,'Usuário 5996236','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(4,6291732,'Usuário 6291732','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(5,6347380,'Usuário 6347380','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(6,6517579,'Usuário 6517579','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(7,6769195,'Usuário 6769195','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(8,6775861,'Usuário 6775861','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(9,6801811,'Usuário 6801811','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(10,6904955,'Usuário 6904955','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(11,6917461,'Usuário 6917461','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(12,6955207,'Usuário 6955207','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(13,6997201,'Usuário 6997201','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(14,7217684,'Usuário 7217684','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(15,7220502,'Usuário 7220502','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(16,7237871,'Usuário 7237871','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(17,7247206,'Usuário 7247206','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(18,7250461,'Usuário 7250461','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(19,7252684,'Usuário 7252684','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(20,7268149,'Usuário 7268149','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(21,7288417,'Usuário 7288417','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(22,7551487,'Usuário 7551487','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(23,7766980,'Usuário 7766980','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(24,7806809,'Usuário 7806809','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(25,7902964,'Usuário 7902964','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(26,7909179,'Usuário 7909179','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(27,7913648,'Usuário 7913648','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(28,7916477,'Usuário 7916477','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(29,7924488,'Usuário 7924488','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(30,7941706,'Usuário 7941706','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(31,7942630,'Usuário 7942630','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(32,8003530,'Usuário 8003530','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values (33,8026637,'Usuário 8026637','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(34,8129142,'Usuário 8129142','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(35,8207607,'Usuário 8207607','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(36,8279250,'Usuário 8279250','F','1962-07-22','',null,null);
+
+insert into v_servidor_cotic (cd_servidor, cd_registro_funcional, nm_pessoa, cd_sexo_pessoa, dt_nascimento_pessoa, cd_cpf_pessoa, nm_social, cd_inep_pessoa)
+values(37,8417211,'Usuário 8417211','F','1962-07-22','',null,null);
