2d7b84a65ccee71dffa05f6c41c4d4e254211d00
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-05-11 23:25:53 -0300

    38882 - Refatorado scrip´ts do EOL e CoreSSO

---
+++ teste/ponta-a-ponta/scripts/postgres/sgp-populate/V999002__insere_usuarios.sql
@@ -0,0 +1,64 @@
+/*PAEE*/
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('8003530',now(), '0','Sistema','8003530','Usuario 8003530');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7247206',now(), '0','Sistema','7247206','Usuario 7247206');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7902964',now(), '0','Sistema','7902964','Usuario 7902964');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7942630',now(), '0','Sistema','7942630','Usuario 7942630');
+
+/*CP*/
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7806809',now(), '0','Sistema','7806809','Usuario 7806809');
+
+/*CP (BASE)*/
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7913648',now(), '0','Sistema','7913648','Usuario 7913648');
+
+/*CP SOBREPOSTO*/
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7250461',now(), '0','Sistema','7250461','Usuario 7250461');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('8129142',now(), '0','Sistema','8129142','Usuario 8129142');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('8207607',now(), '0','Sistema','8207607','Usuario 8207607');
+
+/*AD*/
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7268149',now(), '0','Sistema','7268149','Usuario 7268149');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6997201',now(), '0','Sistema','6997201','Usuario 6997201');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('8279250',now(), '0','Sistema','8279250','Usuario 8279250');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6904955',now(), '0','Sistema','6904955','Usuario 6904955');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6291732',now(), '0','Sistema','6291732','Usuario 6291732');
+
+/*Diretor*/
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6955207',now(), '0','Sistema','6955207','Usuario 6955207');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6347380',now(), '0','Sistema','6347380','Usuario 6347380');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7766980',now(), '0','Sistema','7766980','Usuario 7766980');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7217684',now(), '0','Sistema','7217684','Usuario 7217684');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7252684',now(), '0','Sistema','7252684','Usuario 7252684');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7220502',now(), '0','Sistema','7220502','Usuario 7220502');
+
+/* ADM UE */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6917461',now(), '0','Sistema','6917461','Usuario 6917461');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7551487',now(), '0','Sistema','7551487','Usuario 7551487');
+
+/* ADM DRE */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('5996236',now(), '0','Sistema','5996236','Usuario 5996236');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6775861',now(), '0','Sistema','6775861','Usuario 6775861');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('8026637',now(), '0','Sistema','8026637','Usuario 8026637');
+
+/* Professor */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7941706',now(), '0','Sistema','7941706','Usuario 7941706');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6801811',now(), '0','Sistema','6801811','Usuario 6801811');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6769195',now(), '0','Sistema','6769195','Usuario 6769195');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('8417211',now(), '0','Sistema','8417211','Usuario 8417211');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7916477',now(), '0','Sistema','7916477','Usuario 7916477');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7288417',now(), '0','Sistema','7288417','Usuario 7288417');
+
+/* Supervisor */ 
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('5888085',now(), '0','Sistema','5888085','Usuario 5888085');
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('1180827',now(), '0','Sistema','1180827','Usuario 1180827');
+
+/* Comunicados UE */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7237871',now(), '0','Sistema','7237871','Usuario 7237871');
+
+/* Laudo Médico (Temporário) */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('6517579 ',now(), '0','Sistema','6517579','Usuario 6517579');
+
+/* Perfil de ADM SME COPED */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7909179',now(), '0','Sistema','7909179','Usuario 7909179');
+
+/* Perfil de ADM COTIC */
+insert into usuario (rf_codigo, criado_em, criado_rf, criado_por, login, nome) values ('7924488',now(), '0','Sistema','7924488','Usuario 7924488');
