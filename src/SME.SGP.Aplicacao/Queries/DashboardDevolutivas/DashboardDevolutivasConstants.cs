f80a07b7c511f9bb7d0ad37e29dbe0937c208a14
Carlos Dias <carlosafdias@outlook.com>
2021-05-13 15:13:06 -0300

    CM - Endpoint de consolidação de devolutivas estimadas e registradas

--- src/SME.SGP.Aplicacao/Queries/DashboardDevolutivas/DashboardDevolutivasConstants.cs
+++ src/SME.SGP.Aplicacao/Queries/DashboardDevolutivas/DashboardDevolutivasConstants.cs
@@ -6,5 +6,7 @@
         internal const string QuantidadeComDevolutivaPendente = "Qtd. com devolutiva pendente";
         internal const string QuantidadeComReflexoesEReplanejamento = "Qtd. com o campo reflexões e replanejamento preenchido";
         internal const string QuantidadeSemReflexoesEReplanejamento = "Qtd. com o campo reflexões e replanejamento não preenchido";
+        internal const string QuantidadeDeDevolutivasEstimadas = "Qtd. estimativa de devolutivas";
+        internal const string QuantidadeDeDevolutivasRegistradas = "Qtd. de devolutivas registradas";
     }
 }
