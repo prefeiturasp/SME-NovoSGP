f71bf303ff308575604c0945b1d118aca1e15a18
Rafael Losi <rafael.losi@amcom.com.br>
2021-05-25 18:22:28 -0300

    40478 Ajuste exclusão de calculos de frequencia e inclusão de Polly e transação na persistencia

--- src/SME.SGP.Infra/Politicas/PoliticaPolly.cs
+++ src/SME.SGP.Infra/Politicas/PoliticaPolly.cs
@@ -6,6 +6,7 @@ namespace SME.GoogleClassroom.Infra
 {
     public abstract class PoliticaPolly
     {
+        public static string SGP => "RetryPolicySGP";
         public static string PublicaFila => "RetryPolicyFilasRabbit";
     }
 }
