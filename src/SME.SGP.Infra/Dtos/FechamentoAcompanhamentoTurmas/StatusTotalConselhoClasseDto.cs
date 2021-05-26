f3526a99d5bd6b802c0b5f5d380937d671dbf482
Luiz Araújo <luiz.araujo@amcom.com.br>
2021-05-26 02:34:56 -0300

    corrigido conselhoclasse

--- src/SME.SGP.Infra/Dtos/FechamentoAcompanhamentoTurmas/StatusTotalConselhoClasseDto.cs
+++ src/SME.SGP.Infra/Dtos/FechamentoAcompanhamentoTurmas/StatusTotalConselhoClasseDto.cs
@@ -4,7 +4,7 @@ namespace SME.SGP.Infra
 {
     public class StatusTotalConselhoClasseDto
     {
-        public StatusConselhoClasse Status { get; set; }
+        public int Status { get; set; }
         public string Descricao { get; set; }
         public int Quantidade { get; set; }
     }
