ec89d3ca5f3a1e5dd7ca7b7e74e28f9ff43a5845
Rafael Losi <rafael.losi@amcom.com.br>
2021-05-26 15:41:06 -0300

    Reintroduzido endpoint de conciliação de turma

--- src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ConciliacaoFrequenciaTurmasUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ConciliacaoFrequenciaTurmasUseCase.cs
@@ -13,11 +13,10 @@ namespace SME.SGP.Aplicacao
 
         public async Task Executar()
         {
-            await mediator.Send(new ConciliacaoFrequenciaTurmasCommand(DateTime.Now));
-            //await Executar(DateTime.Now);
+            await ProcessarNaData(DateTime.Now);
         }
 
-        public async Task Executar(DateTime dataPeriodo)
+        public async Task ProcessarNaData(DateTime dataPeriodo)
         {
             await mediator.Send(new ConciliacaoFrequenciaTurmasCommand(dataPeriodo));
         }
