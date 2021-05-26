c2aa4830ca0ed489443733cfe5e105dc3e9d0915
Rodrigo Galante <rodrigo.galante@globo.com>
2021-05-26 15:56:40 -0300

    ajuste na consulta por modalidade

--- src/SME.SGP.Dados/Repositorios/RepositorioTurma.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioTurma.cs
@@ -1101,14 +1101,14 @@ namespace SME.SGP.Dados.Repositorios
                     else anosCondicao.Add(ano.ShortName());
                 }
             }
-            var sql = dreId > 0 ? QueryInformacoesEscolaresTurmasPorAno(dreId, ueId, anosCondicao, tiposTurma, semestre, modalidade) : 
+            var sql = dreId > 0 ? QueryInformacoesEscolaresTurmasPorAno(dreId, ueId, anosCondicao, tiposTurma, semestre) : 
                 QueryInformacoesEscolaresTurmasPorDre(dreId, ueId, anosCondicao, tiposTurma, semestre);
             return await contexto
                 .Conexao
                 .QueryAsync<GraficoBaseDto>(sql, new { modalidade, dreId, ueId, anoLetivo, semestre, tiposTurma, anosCondicao });
         }
 
-        private string CondicoesInformacoesEscolares(long dreId, long ueId, IEnumerable<string> anosCondicao, IEnumerable<int> tiposTurma, int? semestre, Modalidade modalidade)
+        private string CondicoesInformacoesEscolares(long dreId, long ueId, IEnumerable<string> anosCondicao, IEnumerable<int> tiposTurma, int? semestre)
         {
             var query = new StringBuilder("");
             if (semestre > 0) query.AppendLine(@"  and t.semestre = @semestre");
@@ -1116,7 +1116,6 @@ namespace SME.SGP.Dados.Repositorios
             if (ueId > 0) query.AppendLine(@"  and ue.id = @ueId");
             if (anosCondicao != null && anosCondicao.Any()) query.AppendLine(@"  and t.ano = ANY(@anosCondicao)");
             if (tiposTurma != null && tiposTurma.Any()) query.AppendLine(@"  and t.tipo_turma = ANY(@tiposTurma)");
-            if (modalidade > 0) query.AppendLine(@" and t.modalidade_codigo = @modalidade");
             
             return query.ToString();
         }
@@ -1129,7 +1128,8 @@ namespace SME.SGP.Dados.Repositorios
                                            inner join ue on ue.id = t.ue_id 
                                            inner join dre on dre.id = ue.dre_id 
                                            where t.ano is not null 
-                                             and t.ano_letivo = @anoLetivo"); 
+                                             and t.ano_letivo = @anoLetivo  
+                                             and t.modalidade_codigo = @modalidade"); 
             if (semestre > 0) query.AppendLine(@"  and t.semestre = @semestre");
             if (dreId > 0) query.AppendLine(@" and dre.id = @dreId");
             if (ueId > 0) query.AppendLine(@"  and ue.id = @ueId");
@@ -1147,7 +1147,7 @@ namespace SME.SGP.Dados.Repositorios
             return query.ToString();
         }
 
-        private string QueryInformacoesEscolaresTurmasPorAno(long dreId, long ueId, IEnumerable<string> anosCondicao, IEnumerable<int> tiposTurma, int? semestre, Modalidade modalidade)
+        private string QueryInformacoesEscolaresTurmasPorAno(long dreId, long ueId, IEnumerable<string> anosCondicao, IEnumerable<int> tiposTurma, int? semestre)
         {
             var query = new StringBuilder(@"select * from (	
 	                                            (select t.ano as descricao,
@@ -1157,12 +1157,13 @@ namespace SME.SGP.Dados.Repositorios
 	                                             inner join dre on dre.id = ue.dre_id 
 	                                             where  t.ano is not null
 	                                               and t.tipo_turma not in (2,3,7)
-                                                   and t.ano_letivo = @anoLetivo ");
+                                                   and t.ano_letivo = @anoLetivo
+                                                   and t.modalidade_codigo = @modalidade ");
 
             if (tiposTurma.Any() && !anosCondicao.Any()) query.AppendLine("and t.ano = '0'");
             else query.AppendLine("and t.ano <> '0' ");
 
-            query.AppendLine(CondicoesInformacoesEscolares(dreId, ueId, anosCondicao, null, semestre, modalidade));
+            query.AppendLine(CondicoesInformacoesEscolares(dreId, ueId, anosCondicao, null, semestre));
             query.AppendLine(@" group by t.ano
 	                                             order by t.ano)
 	                                             union
@@ -1176,8 +1177,9 @@ namespace SME.SGP.Dados.Repositorios
 	                                             inner join dre on dre.id = ue.dre_id 
 	                                             where t.ano is not null
 	                                               and t.tipo_turma in (2,3,7) 
-                                                   and t.ano_letivo = @anoLetivo ");
-            query.AppendLine(CondicoesInformacoesEscolares(dreId, ueId, anosCondicao, tiposTurma, semestre, modalidade));
+                                                   and t.ano_letivo = @anoLetivo
+                                                   and t.modalidade_codigo = @modalidade ");
+            query.AppendLine(CondicoesInformacoesEscolares(dreId, ueId, anosCondicao, tiposTurma, semestre));
             query.AppendLine(@"group by t.tipo_turma) x
                                             order by x.descricao");
             return query.ToString();
