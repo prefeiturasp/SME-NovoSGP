ac4aab9e24854399d30309f1b1e495197d595b84
William Manoel dos Santos <william.santos@amcom.com.br>
2021-06-01 10:24:11 -0300

    feature/40805

--- src/SME.SGP.Dados/Repositorios/RepositorioPendenciaFechamento.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioPendenciaFechamento.cs
@@ -48,7 +48,7 @@ namespace SME.SGP.Dados.Repositorios
 
         public async Task<PendenciaFechamentoCompletoDto> ObterPorPendenciaId(long pendenciaId)
         {
-            var query = @"select p.id as PendenciaId, p.titulo as descricao, p.descricao as detalhamento
+            var query = @"select p.id as PendenciaId, p.titulo as descricao, p.descricao as detalhamento, p.descricao_html as descricaohtml
                                 , p.situacao, ftd.disciplina_id as DisciplinaId, pe.bimestre, pf.fechamento_turma_disciplina_id as FechamentoId
                                 , p.criado_em as CriadoEm, p.criado_por as CriadoPor, p.criado_rf as CriadoRf, p.alterado_em as AlteradoEm, p.alterado_por as AlteradoPor, p.alterado_rf as AlteradoRf
                           from pendencia_fechamento pf
