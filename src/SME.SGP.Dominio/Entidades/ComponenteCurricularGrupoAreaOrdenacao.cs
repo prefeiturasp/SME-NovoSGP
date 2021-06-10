91331dc9541c50e9f48630ed88bdc1a490420202
William Manoel dos Santos <william.santos@amcom.com.br>
2021-06-10 08:21:12 -0300

    Correção da ordenação dos componentes curriculares

---
+++ src/SME.SGP.Dominio/Entidades/ComponenteCurricularGrupoAreaOrdenacao.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio
+{
+    public class ComponenteCurricularGrupoAreaOrdenacao
+    {
+        public long GrupoMatrizId { get; set; }
+        public long AreaConhecimentoId { get; set; }
+        public int Ordem { get; set; }
+    }
+}
