1223ca4eaa13a5a5496572710dcc0f7ef297f978
Rodrigo Galante <rodrigo.galante@globo.com>
2021-06-16 09:44:51 -0300

    Update DashboardDiarioBordoController.cs
    permissionamento endpoints

--- src/SME.SGP.Api/Controllers/DashboardDiarioBordoController.cs
+++ src/SME.SGP.Api/Controllers/DashboardDiarioBordoController.cs
@@ -16,7 +16,7 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         [ProducesResponseType(typeof(IEnumerable<GraficoTotalDiariosPendentesDTO>), 200)]
-        //[Permissao(Permissao.DB_C, Policy = "Bearer")]
+        [Permissao(Permissao.DB_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterQuantidadeTotalDeDiariosEDevolutivasPorAnoTurma([FromQuery] FiltroDasboardDiarioBordoDto filtro, [FromServices] IObterQuantidadeTotalDeDiariosEDevolutivasPorAnoTurmaUseCase useCase)
         {
             return Ok(await useCase.Executar(filtro));
@@ -26,7 +26,7 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         [ProducesResponseType(typeof(IEnumerable<GraficoTotalDiariosEDevolutivasDTO>), 200)]
-        //[Permissao(Permissao.DB_C, Policy = "Bearer")]
+        [Permissao(Permissao.DB_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterQuantidadeTotalDeDiariosPendentesPorAnoTurma([FromQuery] FiltroDasboardDiarioBordoDto filtro, [FromServices] IObterQuantidadeTotalDeDiariosPendentesPorAnoTurmaUseCase useCase)
         {
             return Ok(await useCase.Executar(filtro));
