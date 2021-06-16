04272d212274c3c001f0685207395822f15e44b4
Rafael Losi <rafael.losi@amcom.com.br>
2021-06-16 17:50:58 -0300

    42048 Melhoria enviado apenas uma mensagem para fila de persistencia por turma

---
+++ src/SME.SGP.Infra/Dtos/MigracaoFrequencia/ParametroFrequenciasPersistirDto.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Infra
+{
+    public class ParametroFrequenciasPersistirDto
+    {
+        public ParametroFrequenciasPersistirDto() { }
+        public ParametroFrequenciasPersistirDto(List<RegistroFrequenciaAluno> frequenciasPersistir)
+        {
+            FrequenciasPersistir = frequenciasPersistir;
+        }
+
+        public List<RegistroFrequenciaAluno> FrequenciasPersistir { get; }
+    }
+}
