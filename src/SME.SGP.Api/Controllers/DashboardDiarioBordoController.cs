7edf6ad213869e0b6b38f14b6fc30571159036d7
Rodrigo Galante <rodrigo.galante@globo.com>
2021-06-16 09:53:38 -0300

    ajuste using permissao

--- src/SME.SGP.Api/Controllers/DashboardDiarioBordoController.cs
+++ src/SME.SGP.Api/Controllers/DashboardDiarioBordoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Infra;
 using System.Collections.Generic;
