bef59ba78d996748675d703b78b07bba9f67dbea
elitonds <elitonds@gmail.com>
2021-06-17 10:53:03 -0300

    incluir modalidade na mensagem de notificação

--- src/SME.SGP.Aplicacao/CasosDeUso/Notificacao/NotificarExclusaoAulaComFrequenciaUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Notificacao/NotificarExclusaoAulaComFrequenciaUseCase.cs
@@ -25,7 +25,7 @@ namespace SME.SGP.Aplicacao
             var titulares = await mediator.Send(new ObterProfessoresTitularesDaTurmaQuery(turma.CodigoTurma));
             if (titulares != null)
             {
-                var mensagem = new StringBuilder($"As seguintes aulas da turma {turma.Nome} da {turma.Ue.TipoEscola.ShortName()} {turma.Ue.Nome} ({turma.Ue.Dre.Abreviacao}) não foram excluídas pela rotina automática pois já possuem registros de frequência.<br>");
+                var mensagem = new StringBuilder($"As seguintes aulas da turma {turma.ModalidadeCodigo.ShortName()} - {turma.Nome} da {turma.Ue.TipoEscola.ShortName()} {turma.Ue.Nome} ({turma.Ue.Dre.Abreviacao}) não foram excluídas pela rotina automática pois já possuem registros de frequência.<br>");
                 foreach (var data in datasAulas)
                 {
                     mensagem.AppendLine($"<br>{data:dd/MM/yyyy}");
