4e80bdcc842ca64175a188226b13c5ac1a884049
Rafael Losi <rafael.losi@amcom.com.br>
2021-06-22 14:14:04 -0300

    Ajuste numero parametro para nao conflitar com outra história que esta em desenvolvimento

--- scripts/V453__CRIACAO_PARAMETRO_IMAGENS_PERCURSO_TURMA.sql
+++ scripts/V453__CRIACAO_PARAMETRO_IMAGENS_PERCURSO_TURMA.sql
@@ -1,5 +1,5 @@
 insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
-values('QuantidadeImagensPercursoTurma', 71, 'Quantidade de Imagens Permitidas na Seção Percurso Coletivo da Turma', '1', 2021, true, now(), 'SISTEMA', '0');
+values('QuantidadeImagensPercursoTurma', 72, 'Quantidade de Imagens Permitidas na Seção Percurso Coletivo da Turma', '1', 2021, true, now(), 'SISTEMA', '0');
 
 insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
-values('QuantidadeImagensPercursoTurma', 71, 'Quantidade de Imagens Permitidas na Seção Percurso Coletivo da Turma', '1', 2020, true, now(), 'SISTEMA', '0');
+values('QuantidadeImagensPercursoTurma', 72, 'Quantidade de Imagens Permitidas na Seção Percurso Coletivo da Turma', '1', 2020, true, now(), 'SISTEMA', '0');
