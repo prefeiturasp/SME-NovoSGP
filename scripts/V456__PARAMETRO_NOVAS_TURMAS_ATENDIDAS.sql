8031b17ed32402238c02f943ccc716673b131ab4
Rodrigo Galante <rodrigo.galante@globo.com>
2021-06-24 15:45:12 -0300

    Criação de parâmetro novas turmas atendidas

---
+++ scripts/V456__PARAMETRO_NOVAS_TURMAS_ATENDIDAS.sql
@@ -0,0 +1,23 @@
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2021, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2020, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2019, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2018, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2017, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2016, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2015, true, now(), 'SISTEMA', '0');
+
+insert into parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
+values('AgrupamentoTurmasFiltro', 74, 'Agrupamento de turmas - filtro principal', '5,6,7', 2014, true, now(), 'SISTEMA', '0');
