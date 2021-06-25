eb3c21da0b9ebc063bc5f381b2faf8c82d52ba87
Rafael Losi <rafael.losi@amcom.com.br>
2021-06-25 10:59:17 -0300

    Ajuste descrição simples dos diarios de bordo

--- src/SME.SGP.Infra/Dtos/Devolutiva/DiarioBordoDevolutivaDto.cs
+++ src/SME.SGP.Infra/Dtos/Devolutiva/DiarioBordoDevolutivaDto.cs
@@ -9,10 +9,45 @@ namespace SME.SGP.Infra
         public DateTime Data { get; set; }
         public string DescricaoPlanejamento { get; set; }
         public string DescricaoReflexoes { get; set; }
-        public string Planejamento { get => $@"<b>Planejamento</b><br/>
-                                    {DescricaoPlanejamento}<br/>
-                                    <br/><b>Reflexões e replanejamento</b><br/>
-                                    {DescricaoReflexoes}"; }
-        public string PlanejamentoSimples { get => UtilRegex.RemoverTagsHtml(UtilRegex.RemoverTagsHtmlMidia(Planejamento)); }
+        public string Planejamento
+        {
+            get
+            {
+                var descricao = ObterPlanejamento();
+                if (!string.IsNullOrEmpty(DescricaoReflexoes))
+                    descricao += ObterReflexoes();
+
+                return descricao;
+            }
+        }
+
+        public string PlanejamentoSimples
+        {
+            get
+            {
+                var descricao = ObterPlanejamento(false);
+                if (!string.IsNullOrEmpty(DescricaoReflexoes))
+                    descricao += ObterReflexoes(false);
+
+                return descricao;
+            }
+        }
+
+
+        private string ObterPlanejamento(bool textoFormatado = true)
+        {
+            var descricao = textoFormatado ?
+                DescricaoPlanejamento :
+                UtilRegex.RemoverTagsHtml(UtilRegex.RemoverTagsHtmlMidia(DescricaoPlanejamento));
+            return $"<b>Planejamento</b><br/>{descricao}<br/>";
+        }
+
+        private string ObterReflexoes(bool textoFormatado = true)
+        {
+            var descricao = textoFormatado ?
+                DescricaoReflexoes :
+                UtilRegex.RemoverTagsHtml(UtilRegex.RemoverTagsHtmlMidia(DescricaoReflexoes));
+            return $"<br/><b>Reflexões e replanejamento</b><br/>{descricao}";
+        }
     }
 }
