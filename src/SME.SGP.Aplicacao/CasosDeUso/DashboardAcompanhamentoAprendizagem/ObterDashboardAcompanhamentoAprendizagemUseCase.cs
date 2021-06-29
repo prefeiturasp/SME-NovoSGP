25e32b25320417c4b5df28a8e8bb1423b5fce99d
Rafael Losi <rafael.losi@amcom.com.br>
2021-06-29 11:00:06 -0300

    42079 Endpoint Dash de Acompanhamento de Aprendizagem

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/DashboardAcompanhamentoAprendizagem/ObterDashboardAcompanhamentoAprendizagemUseCase.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterDashboardAcompanhamentoAprendizagemUseCase : AbstractUseCase, IObterDashboardAcompanhamentoAprendizagemUseCase
+    {
+        public ObterDashboardAcompanhamentoAprendizagemUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<IEnumerable<DashboardAcompanhamentoAprendizagemDto>> Executar(FiltroDashboardAcompanhamentoAprendizagemDto filtro)
+        {
+            await mediator.Send(new ObterDashBoardEncaminhamentoAprendizagemQuery());
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/DashboardAcomanhamentoAprendizagem/ObterDashBoardEncaminhamentoAprendizagem/ObterDashBoardEncaminhamentoAprendizagemQuery.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterDashBoardEncaminhamentoAprendizagemQuery : IRequest<IEnumerable<DashboardAcompanhamentoAprendizagemDto>>
+    {
+        public ObterDashBoardEncaminhamentoAprendizagemQuery(int anoLetivo, long dreId, long ueId, int semestre)
+        {
+            AnoLetivo = anoLetivo;
+            DreId = dreId;
+            UeId = ueId;
+            Semestre = semestre;
+        }
+
+        public int AnoLetivo { get; }
+        public long DreId { get; }
+        public long UeId { get; }
+        public int Semestre { get; }
+    }
+
+    public class ObterDashBoardEncaminhamentoAprendizagemQueryValidator : AbstractValidator<ObterDashBoardEncaminhamentoAprendizagemQuery>
+    {
+        public ObterDashBoardEncaminhamentoAprendizagemQueryValidator()
+        {
+            RuleFor(a => a.AnoLetivo)
+                .NotEmpty()
+                .WithMessage("O ano letivo deve ser informado para consulta do Dashboard de Acompanhamento de Aprendizagem");
+            RuleFor(a => a.Semestre)
+                .NotEmpty()
+                .WithMessage("O semestre deve ser informado para consulta do Dashboard de Acompanhamento de Aprendizagem");
+        }
+    }
+}
