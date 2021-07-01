688d8c180f2e377062fcc10937acbd2a2b8d0f37
Rafael Losi <rafael.losi@amcom.com.br>
2021-07-01 00:01:46 -0300

    corrigido set;

--- src/SME.SGP.Infra/Dtos/ConsolidacaoAcompanhamentoAprendizagemAluno/FiltroAcompanhamentoAprendizagemAlunoTurmaDTO.cs
+++ src/SME.SGP.Infra/Dtos/ConsolidacaoAcompanhamentoAprendizagemAluno/FiltroAcompanhamentoAprendizagemAlunoTurmaDTO.cs
@@ -5,7 +5,7 @@
         public long TurmaId { get; set; }
         public int AnoLetivo { get; set; }
         public int Semestre { get; set; }
-        public int QuantidadeAlunosTurma { get; }
+        public int QuantidadeAlunosTurma { get; set; }
 
         public FiltroAcompanhamentoAprendizagemAlunoTurmaDTO(long turmaId, int anoLetivo, int semestre, int quantidadeAlunosTurma)
         {
