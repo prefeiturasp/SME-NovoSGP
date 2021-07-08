debb250f1c0fcecae27997679e0adc5758285ee8
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-07-08 10:24:29 -0300

    Atualizada classe de comunicados para retornar -99 quando for todos

--- src/SME.SGP.Aplicacao/Queries/Usuario/EscolaAqui/Comunicados/ObterComunicadosPorId/ObterComunicadoPorIdQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Usuario/EscolaAqui/Comunicados/ObterComunicadosPorId/ObterComunicadoPorIdQueryHandler.cs
@@ -46,62 +46,11 @@ namespace SME.SGP.Aplicacao
 
             comunicado.Modalidades = (await repositorioComunicadoModalidade.ObterModalidadesPorComunicadoId(comunicado.Id)).ToArray();
 
+            comunicado.Modalidades = comunicado.Modalidades.Length == Enum.GetValues(typeof(Modalidade)).Length ? new int[] { -99 } : comunicado.Modalidades;
+
             var dto = (ComunicadoCompletoDto)comunicado;            
 
-            await ValidarAbrangenciaUsuario(dto);
-
             return dto;
-        }
-
-        private async Task ValidarAbrangenciaUsuario(ComunicadoDto filtroDto)
-        {
-            //var usuarioLogado = await servicoUsuario.ObterUsuarioLogado();
-
-            //if ((filtroDto.CodigoDre?.Equals(TODAS) ?? true) && !usuarioLogado.EhPerfilSME())
-            //    throw new NegocioException("Apenas usuários SME podem visualizar comunicados de todas as DREs");
-
-            //if ((filtroDto.CodigoUe?.Equals(TODAS) ?? true) && !(usuarioLogado.EhPerfilDRE() || usuarioLogado.EhPerfilSME()))
-            //    throw new NegocioException("Apenas usuários SME e DRE podem visualizar comunicados de todas as Escolas");
-
-            //if (usuarioLogado.EhPerfilDRE() && (!filtroDto.CodigoDre?.Equals(TODAS) ?? false))
-            //    await ValidarAbrangenciaDre(filtroDto);
-
-            //if (usuarioLogado.EhPerfilUE() && (!filtroDto.CodigoUe?.Equals(TodTODASas) ?? false))
-            //    await ValidarAbrangenciaUE(filtroDto);
-        }
-
-        private async Task ValidarAbrangenciaUE(ComunicadoCompletoDto comunicadoCompletoDto)
-        {
-            var abrangenciaUes = await _consultasAbrangencia.ObterUes(comunicadoCompletoDto.CodigoDre, null);
-
-            var ue = abrangenciaUes.FirstOrDefault(x => x.Codigo.Equals(comunicadoCompletoDto.CodigoUe));
-
-            if (ue == null)
-                throw new NegocioException($"Usuário não possui permissão para enviar comunicados para a UE com codigo {comunicadoCompletoDto.CodigoUe}");
-
-            if (comunicadoCompletoDto.Turmas != null && comunicadoCompletoDto.Turmas.Any())
-                await ValidarAbrangenciaTurma(comunicadoCompletoDto);
-        }
-
-        private async Task ValidarAbrangenciaDre(ComunicadoCompletoDto comunicadoCompletoDto)
-        {
-            var abrangenciaDres = await _consultasAbrangencia.ObterDres(null);
-
-            var dre = abrangenciaDres.FirstOrDefault(x => x.Codigo.Equals(comunicadoCompletoDto.CodigoDre));
-
-            if (dre == null)
-                throw new NegocioException($"Usuário não possui permissão para enviar comunicados para a DRE com codigo {comunicadoCompletoDto.CodigoDre}");
-        }
-
-        private async Task ValidarAbrangenciaTurma(ComunicadoCompletoDto comunicadoCompletoDto)
-        {
-            foreach (var turma in comunicadoCompletoDto.Turmas)
-            {
-                var abrangenciaTurmas = await _consultasAbrangencia.ObterAbrangenciaTurma(turma.CodigoTurma);
-
-                if (abrangenciaTurmas == null)
-                    throw new NegocioException($"Usuário não possui permissão para enviar comunicados para a Turma com codigo {turma}");
-            }
         }
     }
 }
