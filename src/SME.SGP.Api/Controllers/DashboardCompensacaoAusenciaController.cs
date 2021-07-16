943754ce0a4225d1fe73312034d6c7fb4e23db7d
William Manoel dos Santos <william.santos@amcom.com.br>
2021-07-16 16:20:30 -0300

    correção no controller

--- src/SME.SGP.Api/Controllers/DashboardCompensacaoAusenciaController.cs
+++ src/SME.SGP.Api/Controllers/DashboardCompensacaoAusenciaController.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Infra;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Api.Controllers
 {
     [ApiController]
-    // [Authorize("Bearer")]
+    [Authorize("Bearer")]
     [Route("api/v1/dashboard/compensacoes/ausencia")]
     public class DashboardCompensacaoAusenciaController : Controller
     {
@@ -15,16 +16,17 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         [ProducesResponseType(typeof(GraficoCompensacaoAusenciaDto), 200)]
+        [Permissao(Permissao.DF_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterTotalAusenciasCompensadas(int anoLetivo, long dreId, long ueId, int modalidade, int bimestre, [FromQuery] int semestre, [FromServices] IObterDadosDashboardTotalAusenciasCompensadasUseCase useCase)
         {
-
-            return Ok(await useCase.Executar(anoLetivo, dreId, ueId, modalidade, bimestre, semestre));            
+            return Ok(await useCase.Executar(anoLetivo, dreId, ueId, modalidade, bimestre, semestre));
         }
 
         [HttpGet("anos/{anoLetivo}/dres/{dreId}/ues/{ueId}/modalidades/{modalidade}/consolidado/compensacoes-consideradas")]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         [ProducesResponseType(typeof(GraficoCompensacaoAusenciaDto), 200)]
+        [Permissao(Permissao.DF_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterTotalCompensacoesConsideradas(int anoLetivo, long dreId, long ueId, int modalidade, int bimestre, [FromQuery] int semestre, [FromServices] IObterDadosDashboardTotalAtividadesCompensacaoUseCase useCase)
         {
             return Ok(await useCase.Executar(anoLetivo, dreId, ueId, modalidade, bimestre, semestre));
