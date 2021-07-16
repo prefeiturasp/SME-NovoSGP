4218c565e14ef5666581d18f562a9b1bdd1612be
elitonds <elitonds@gmail.com>
2021-07-16 11:19:34 -0300

    ajuste considerar data final

--- src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendario.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendario.cs
@@ -254,14 +254,16 @@ namespace SME.SGP.Dados.Repositorios
 	                        tc.ano_letivo = @anoLetivo
 	                        and tc.modalidade = @modalidadeTipoCalendarioId
 	                        and not tc.excluido ";
-            if(modalidadeTipoCalendarioId == (int)Modalidade.EJA.ObterModalidadeTipoCalendario() && semestre == 1)
+            var dataReferencia = new DateTime(anoLetivo, semestre == 1 ? 6 : 7, 1);
+
+            if (modalidadeTipoCalendarioId == (int)Modalidade.EJA.ObterModalidadeTipoCalendario() && semestre > 0)
             {
-                query += $"and pe.periodo_inicio < @periodoInicialReferencia";
+                if (semestre == 1)
+                    query += $"and pe.periodo_inicio < @dataReferencia";
+                else query += $"and pe.periodo_fim > @dataReferencia";
             }
 
-            var periodoInicialReferencia = new DateTime(anoLetivo, 6, 30);
-
-            return await database.Conexao.QueryAsync<PeriodoCalendarioBimestrePorAnoLetivoModalidadeDto>(query.ToString(), new { anoLetivo, modalidadeTipoCalendarioId, periodoInicialReferencia });
+            return await database.Conexao.QueryAsync<PeriodoCalendarioBimestrePorAnoLetivoModalidadeDto>(query.ToString(), new { anoLetivo, modalidadeTipoCalendarioId, dataReferencia });
         }
     }
 }
