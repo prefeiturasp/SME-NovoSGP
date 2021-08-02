3487fe82c83514db60ba72298dcaa8d9224db4ac
elitonds <elitonds@gmail.com>
2021-08-02 10:51:24 -0300

    42148 - ajuste no script para excluir vinculo com usuarios

--- scripts/V470__AJUSTE_REMOVER_PENDENCIAS_FECHAMENTO_INDEVIDAS_EJA.sql
+++ scripts/V470__AJUSTE_REMOVER_PENDENCIAS_FECHAMENTO_INDEVIDAS_EJA.sql
@@ -1,7 +1,6 @@
 do $$
 declare
 	idsPendencias bigint[];
-	idsPendenciasFechamento bigint[];
 
 begin	  
 	select array_agg(distinct (p.id)) into idsPendencias
@@ -13,22 +12,10 @@ begin
 	 where p.tipo = 4
 	   and t.modalidade_codigo = 3
 	   and ftd.disciplina_id in (1060,1061);
-	
-	select array_agg(distinct(pf.id)) into idsPendenciasFechamento
-  	  from pendencia_fechamento pf  
-	 inner join pendencia p on p.id = pf.pendencia_id 
-	 inner join fechamento_turma_disciplina ftd on ftd.id = pf.fechamento_turma_disciplina_id 
-	 inner join fechamento_turma ft on ft.id = ftd.fechamento_turma_id 
-	 inner join turma t on t.id = ft.turma_id 
-	 where p.tipo = 4
-	   and t.modalidade_codigo = 3
-	   and ftd.disciplina_id in (1060,1061);
-   
-	if idsPendencias is not null then
-		delete from pendencia_fechamento where id = ANY(idsPendenciasFechamento);
-	end if;
 
-	if idsPendenciasFechamento is not null then 
+	if idsPendencias is not null then 
+		delete from pendencia_fechamento where pendencia_id = ANY(idsPendencias);
+		delete from pendencia_usuario where pendencia_id = ANY(idsPendencias);
  		delete from pendencia where id = ANY(idsPendencias);
  	end if;
 end $$;
