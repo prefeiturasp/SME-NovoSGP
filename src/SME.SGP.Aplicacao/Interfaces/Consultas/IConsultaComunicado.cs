45761ee1023b4e96bf18fb3aa20c6a1e2ec336a3
William Manoel dos Santos <william.santos@amcom.com.br>
2021-08-03 10:44:26 -0300

    ajuste de comentario

--- src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultaComunicado.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Consultas/IConsultaComunicado.cs
@@ -8,9 +8,6 @@ namespace SME.SGP.Aplicacao
     public interface IConsultaComunicado
     {
         Task<ComunicadoCompletoDto> BuscarPorIdAsync(long id);
-
-        //Task<PaginacaoResultadoDto<ComunicadoDto>> ListarPaginado(FiltroComunicadoDto filtro);
-
         Task<IEnumerable<AlunoPorTurmaResposta>> ObterAlunosPorTurma(string codigoTurma, int anoLetivo);
     }
 }
