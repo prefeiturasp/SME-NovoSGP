0489b0032a6fce88d64d0ffce295494f48422b1b
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-08-04 09:16:52 -0300

    Adicionada verificação se é EJA

--- src/SME.SGP.Dados/Repositorios/RepositorioUe.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioUe.cs
@@ -146,8 +146,10 @@ namespace SME.SGP.Dados.Repositorios
                             on t.ue_id = u.id
                             where u.ue_id = @ueCodigo
                             and t.modalidade_codigo = @modalidade
-                            and t.ano_letivo = @ano
-                            and t.historica = @ehHistorico");
+                            and t.ano_letivo = @ano ");
+
+            if(modalidade == Modalidade.EJA)
+                query.AppendLine(" and t.historica = @ehHistorico ");
 
             return await contexto.QueryAsync<Turma>(query.ToString(), new { ueCodigo, modalidade, ano, ehHistorico });
         }
