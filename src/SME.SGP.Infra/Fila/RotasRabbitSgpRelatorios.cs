352b83fdff1c56a5199dac5526cb27e7b2f60f38
thiiagofernando <thiago.fernando@msn.com>
2021-08-04 10:49:39 -0300

    removido codigo sem uso

--- src/SME.SGP.Infra/Fila/RotasRabbitSgpRelatorios.cs
+++ src/SME.SGP.Infra/Fila/RotasRabbitSgpRelatorios.cs
@@ -2,8 +2,6 @@
 {
     public static class RotasRabbitSgpRelatorios
     {
-        //public const string WorkerRelatoriosSgp = "sme.sr.workers.sgp";
-
         public const string RotaRelatoriosSolicitados = "relatorios.solicitados";
         public const string RotaRelatoriosSolicitadosBoletim = "relatorios.solicitados.boletim";
         public const string RotaRelatoriosSolicitadosBoletimDetalhado = "relatorios.solicitados.boletimdetalhado";
