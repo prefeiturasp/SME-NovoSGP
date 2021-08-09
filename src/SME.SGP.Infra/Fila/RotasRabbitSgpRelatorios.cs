54ae85da915d6d9f4cf8371907c44b819ed50702
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-08-09 17:48:19 -0300

    Ajuste nas rotas dos relatórios

--- src/SME.SGP.Infra/Fila/RotasRabbitSgpRelatorios.cs
+++ src/SME.SGP.Infra/Fila/RotasRabbitSgpRelatorios.cs
@@ -2,37 +2,34 @@
 {
     public static class RotasRabbitSgpRelatorios
     {
-        public const string RotaRelatoriosSolicitados = "relatorios.solicitados";
-        public const string RotaRelatoriosSolicitadosBoletim = "relatorios.solicitados.boletim";
-        public const string RotaRelatoriosSolicitadosBoletimDetalhado = "relatorios.solicitados.boletimdetalhado";
-        public const string RotaRelatoriosSolicitadosPlanoDeAula = "relatorios.solicitados.planodeaula";
-        public const string RotaRelatoriosSolicitadosRegistroIndividual = "relatorios.solicitados.registroindividual";
-        public const string RotaRelatoriosSolicitadosConselhoDeClasse = "relatorios.solicitados.conselhodeclasse";
-        public const string RotaRelatoriosSolicitadosRelatorioAcompanhamentoAprendizagem = "relatorios.solicitados.relatorioacompanhamentoaprendizagem";
-        public const string RotaRelatoriosSolicitadosCalendarioEscolar = "relatorios.solicitados.calendárioescolar";
-        public const string RotaRelatoriosSolicitadosRegistroItinerancia = "relatorios.solicitados.registroitinerancia";
-        public const string RotaRelatoriosSolicitadosPendencias = "relatorios.solicitados.pendencias";
-        public const string RotaRelatoriosSolicitadosAcompanhamentoFechamento = "relatorios.solicitados.acompanhamentofechamento";
-        public const string RotaRelatoriosSolicitadosParecerConclusivo = "relatorios.solicitados.parecerconclusivo";
-        public const string RotaRelatoriosSolicitadosNotasConceitosFinais = "relatorios.solicitados.notasconceitosfinais";
-        public const string RotaRelatoriosSolicitadosRelatorioAlteraçãoNotas = "relatorios.solicitados.relatorioalteracaonotas";
-        public const string RotaRelatoriosSolicitadosPapResumos = "relatorios.solicitados.papresumos";
-        public const string RotaRelatoriosSolicitadosPapGraficos = "relatorios.solicitados.papgraficos";
-        public const string RotaRelatoriosSolicitadosPapRelatorioSemestral = "relatorios.solicitados.paprelatoriosemestral";
-        public const string RotaRelatoriosSolicitadosFrequencia = "relatorios.solicitados.frequencia";
-        public const string RotaRelatoriosSolicitadosCompensacaoAusencia = "relatorios.solicitados.compensacaoausencia";
-        public const string RotaRelatoriosSolicitadosControleGrade = "relatorios.solicitados.controlegrade";
-        public const string RotaRelatoriosSolicitadosControlePlanejamentoDiario = "relatorios.solicitados.controleplanejamentodiario";
-        public const string RotaRelatoriosSolicitadosDevolutivas = "relatorios.solicitados.devolutivas";
-        public const string RotaRelatoriosSolicitadosUsuarios = "relatorios.solicitados.usuarios";
-        public const string RotaRelatoriosSolicitadosAtribuicoes = "relatorios.solicitados.atribuicoes";
-        public const string RotaRelatoriosSolicitadosNotificacoes = "relatorios.solicitados.notificacoes";
-        public const string RotaRelatoriosSolicitadosEscolaAquiLeitura = "relatorios.solicitados.escolaaquileitura";
-        public const string RotaRelatoriosSolicitadosEscolaAquiAdesao = "relatorios.solicitados.escolaaquiadesao";
-        public const string RotaRelatoriosSolicitadosAtaFinalResultados = "relatorios.solicitados.atafinalresultados";
-        public const string RotaRelatoriosSolicitadosHistoricoEscolar = "relatorios.solicitados.historicoescolar";
-
-        public const string RotaRabbitDeadletterTratar = "sgp.sr.rabbit.deadletter.tratar";
-        public const string RotaRabbitDeadletterSync = "sgp.sr.rabbit.deadletter.sync";
+        public const string RotaRelatoriosSolicitados = "sr.relatorios.solicitados";
+        public const string RotaRelatoriosSolicitadosBoletim = "sr.relatorios.solicitados.boletim";
+        public const string RotaRelatoriosSolicitadosBoletimDetalhado = "sr.relatorios.solicitados.boletimdetalhado";
+        public const string RotaRelatoriosSolicitadosPlanoDeAula = "sr.relatorios.solicitados.planodeaula";
+        public const string RotaRelatoriosSolicitadosRegistroIndividual = "sr.relatorios.solicitados.registroindividual";
+        public const string RotaRelatoriosSolicitadosConselhoDeClasse = "sr.relatorios.solicitados.conselhodeclasse";
+        public const string RotaRelatoriosSolicitadosRelatorioAcompanhamentoAprendizagem = "sr.relatorios.solicitados.relatorioacompanhamentoaprendizagem";
+        public const string RotaRelatoriosSolicitadosCalendarioEscolar = "sr.relatorios.solicitados.calendárioescolar";
+        public const string RotaRelatoriosSolicitadosRegistroItinerancia = "sr.relatorios.solicitados.registroitinerancia";
+        public const string RotaRelatoriosSolicitadosPendencias = "sr.relatorios.solicitados.pendencias";
+        public const string RotaRelatoriosSolicitadosAcompanhamentoFechamento = "sr.relatorios.solicitados.acompanhamentofechamento";
+        public const string RotaRelatoriosSolicitadosParecerConclusivo = "sr.relatorios.solicitados.parecerconclusivo";
+        public const string RotaRelatoriosSolicitadosNotasConceitosFinais = "sr.relatorios.solicitados.notasconceitosfinais";
+        public const string RotaRelatoriosSolicitadosRelatorioAlteraçãoNotas = "sr.relatorios.solicitados.relatorioalteracaonotas";
+        public const string RotaRelatoriosSolicitadosPapResumos = "sr.relatorios.solicitados.papresumos";
+        public const string RotaRelatoriosSolicitadosPapGraficos = "sr.relatorios.solicitados.papgraficos";
+        public const string RotaRelatoriosSolicitadosPapRelatorioSemestral = "sr.relatorios.solicitados.paprelatoriosemestral";
+        public const string RotaRelatoriosSolicitadosFrequencia = "sr.relatorios.solicitados.frequencia";
+        public const string RotaRelatoriosSolicitadosCompensacaoAusencia = "sr.relatorios.solicitados.compensacaoausencia";
+        public const string RotaRelatoriosSolicitadosControleGrade = "sr.relatorios.solicitados.controlegrade";
+        public const string RotaRelatoriosSolicitadosControlePlanejamentoDiario = "sr.relatorios.solicitados.controleplanejamentodiario";
+        public const string RotaRelatoriosSolicitadosDevolutivas = "sr.relatorios.solicitados.devolutivas";
+        public const string RotaRelatoriosSolicitadosUsuarios = "sr.relatorios.solicitados.usuarios";
+        public const string RotaRelatoriosSolicitadosAtribuicoes = "sr.relatorios.solicitados.atribuicoes";
+        public const string RotaRelatoriosSolicitadosNotificacoes = "sr.relatorios.solicitados.notificacoes";
+        public const string RotaRelatoriosSolicitadosEscolaAquiLeitura = "sr.relatorios.solicitados.escolaaquileitura";
+        public const string RotaRelatoriosSolicitadosEscolaAquiAdesao = "sr.relatorios.solicitados.escolaaquiadesao";
+        public const string RotaRelatoriosSolicitadosAtaFinalResultados = "sr.relatorios.solicitados.atafinalresultados";
+        public const string RotaRelatoriosSolicitadosHistoricoEscolar = "sr.relatorios.solicitados.historicoescolar";
     }
 }
