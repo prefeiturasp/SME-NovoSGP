9d3b7791cb04e94d16a5dd18a0ca4ae5f1a85eda
William Manoel dos Santos <william.santos@amcom.com.br>
2021-08-11 16:30:27 -0300

    Correção de data referência

--- src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendario.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioTipoCalendario.cs
@@ -182,11 +182,11 @@ namespace SME.SGP.Dados.Repositorios
             DateTime dataReferencia = DateTime.MinValue;
             if (modalidade == ModalidadeTipoCalendario.EJA)
             {
-                var periodoReferencia = semestre == 1 ? "periodo_inicio < @dataReferencia" : "periodo_inicio > @dataReferencia";
+                var periodoReferencia = semestre == 1 ? "periodo_inicio < @dataReferencia" : "periodo_fim > @dataReferencia";
                 query.AppendLine($"and exists(select 0 from periodo_escolar p where tipo_calendario_id = t.id and {periodoReferencia})");
 
                 // 1/6/ano ou 1/7/ano dependendo do semestre
-                dataReferencia = new DateTime(anoLetivo, semestre == 1 ? 6 : 7, 1);
+                dataReferencia = new DateTime(anoLetivo, semestre == 1 ? 6 : 8, 1);
             }
 
             return await database.Conexao.QueryFirstOrDefaultAsync<long>(query.ToString(), new { anoLetivo, modalidade = (int)modalidade, dataReferencia });
