72868c5e76f307d0d56efaa1e4a893eb9cfcfb6f
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-08-12 19:50:23 -0300

    ajuste para retornar alunos específicos

--- src/SME.SGP.Aplicacao/CasosDeUso/EscolaAqui/Comunicados/ObterAlunosPorTurma/ObterAlunosPorTurmaEAnoLetivoEscolaAquiUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/EscolaAqui/Comunicados/ObterAlunosPorTurma/ObterAlunosPorTurmaEAnoLetivoEscolaAquiUseCase.cs
@@ -2,6 +2,7 @@
 using SME.SGP.Aplicacao.Interfaces.CasosDeUso.EscolaAqui;
 using SME.SGP.Infra;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
@@ -17,7 +18,17 @@ namespace SME.SGP.Aplicacao
 
         public async Task<IEnumerable<AlunoPorTurmaResposta>> Executar(string codigoTurma, int anoLetivo)
         {
-            return await mediator.Send(new ObterAlunosAtivosPorTurmaCodigoQuery(codigoTurma));
+            var alunos = await mediator.Send(new ObterAlunosPorTurmaEAnoLetivoQuery(codigoTurma));
+
+            if (alunos.Any())
+                return alunos.Where(a => a.CodigoSituacaoMatricula == Dominio.SituacaoMatriculaAluno.Ativo || 
+                a.CodigoSituacaoMatricula == Dominio.SituacaoMatriculaAluno.Concluido ||
+                a.CodigoSituacaoMatricula == Dominio.SituacaoMatriculaAluno.PendenteRematricula ||
+                a.CodigoSituacaoMatricula == Dominio.SituacaoMatriculaAluno.Rematriculado ||
+                a.CodigoSituacaoMatricula == Dominio.SituacaoMatriculaAluno.SemContinuidade ||
+                a.CodigoSituacaoMatricula == Dominio.SituacaoMatriculaAluno.ReclassificadoSaida);
+
+            return alunos;
         }
     }
 }
