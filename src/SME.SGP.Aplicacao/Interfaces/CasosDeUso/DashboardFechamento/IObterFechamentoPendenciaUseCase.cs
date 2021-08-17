c7a685439e4c2336157b4e4a854b8f2b5410c3a4
Diego Delmiro <diego.delmiro@amcom.com.br>
2021-08-17 17:28:49 -0300

    46953 criando endpoint para listar pendencia do fechamento

---
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/DashboardFechamento/IObterFechamentoPendenciaUseCase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using SME.SGP.Infra.Dtos;
+
+namespace SME.SGP.Aplicacao
+{
+    public interface IObterFechamentoPendenciasUseCase : IUseCase<FiltroDashboardFechamentoDto, IEnumerable<FechamentoPendeciaDto>>
+    {
+    }
+}
