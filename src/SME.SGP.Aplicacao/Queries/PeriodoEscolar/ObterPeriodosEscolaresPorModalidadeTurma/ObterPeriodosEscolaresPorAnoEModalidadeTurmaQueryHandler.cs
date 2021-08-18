da80bcdd63a774a515cb604b9f761c5f659a64f6
Rafael Losi <rafael.losi@amcom.com.br>
2021-08-18 18:09:51 -0300

    47463 ajustes parametros

--- src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterPeriodosEscolaresPorModalidadeTurma/ObterPeriodosEscolaresPorAnoEModalidadeTurmaQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterPeriodosEscolaresPorModalidadeTurma/ObterPeriodosEscolaresPorAnoEModalidadeTurmaQueryHandler.cs
@@ -17,8 +17,7 @@ namespace SME.SGP.Aplicacao
         }
         public async Task<IEnumerable<PeriodoEscolar>> Handle(ObterPeriodosEscolaresPorAnoEModalidadeTurmaQuery request, CancellationToken cancellationToken)
         {
-            var semestre = request.Semestre.HasValue ? request.Semestre.Value : 1;
-            return await repositorioPeriodoEscolar.ObterPorAnoLetivoEModalidadeTurma(request.AnoLetivo, request.ModalidadeTurma.ObterModalidadeTipoCalendario(), semestre);
+            return await repositorioPeriodoEscolar.ObterPorAnoLetivoEModalidadeTurma(request.AnoLetivo, request.ModalidadeTurma.ObterModalidadeTipoCalendario(), request.Semestre);
         }
     }
 }
