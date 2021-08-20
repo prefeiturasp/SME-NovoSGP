78e693f3bb0df1b7266957b5bf9421fb862bbd33
thiiagofernando <thiago.fernando@msn.com>
2021-08-20 12:12:01 -0300

    Ajustes em endpoint de consulta no EOL AB#47723

--- src/SME.SGP.Aplicacao/Interfaces/Integracoes/IServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Interfaces/Integracoes/IServicoEOL.cs
@@ -99,7 +99,8 @@ namespace SME.SGP.Aplicacao.Integracoes
 
         Task<UsuarioResumoCoreDto> ObterResumoCore(string login);
 
-        Task<ProfessorResumoDto> ObterResumoProfessorPorRFAnoLetivo(string codigoRF, int anoLetivo, string dreId = "", string ueId="", bool buscarOutrosCargos = false);
+        Task<ProfessorResumoDto> ObterResumoProfessorPorRFAnoLetivo(string codigoRF, int anoLetivo, bool buscarOutrosCargos = false);
+        Task<ProfessorResumoDto> ObterProfessorPorRFUeDreAnoLetivo(string codigoRF, int anoLetivo, string dreId,string ueId);
 
         IEnumerable<SupervisoresRetornoDto> ObterSupervisoresPorCodigo(string[] codigoSupervisores);
 
