bf610907145968693c6aa2f6a532b3d06fb425c6
Rafael Losi <rafael.losi@amcom.com.br>
2021-08-27 22:42:04 -0300

    42778 Criado endpoint para execução manual da Consolidação de Frequência da Turma

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ExecutaConsolidacaoFrequenciaPorAnoUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExecutaConsolidacaoFrequenciaPorAnoUseCase : AbstractUseCase, IExecutaConsolidacaoFrequenciaPorAnoUseCase
+    {
+        public ExecutaConsolidacaoFrequenciaPorAnoUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task Executar(int ano)
+        {
+            await mediator.Send(new LimparConsolidacaoFrequenciaTurmasPorAnoCommand(ano));
+            await mediator.Send(new ExecutarConsolidacaoFrequenciaNoAnoCommand(ano));
+        }
+    }
+}
