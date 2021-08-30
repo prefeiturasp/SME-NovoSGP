de785e90c2132acf23c9f9b9b9efbf8db6c4d0d1
Thiago Fernando <thiago.fernando@msn.com>
2021-08-30 16:29:44 -0300

    Ajustes

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/DashboardFechamento/ObterFechamentoConselhoClasseSituacaoUseCase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterFechamentoConselhoClasseSituacaoUseCase : AbstractUseCase, IObterFechamentoConselhoClasseSituacaoUseCase
+    {
+        public ObterFechamentoConselhoClasseSituacaoUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<IEnumerable<GraficoBaseDto>> Executar(FiltroDashboardFechamentoDto param)
+        {
+            var fechamentosRetorno = await mediator.Send(new ObterFechamentoSituacaoQuery(param.UeId, param.AnoLetivo,
+                param.DreId,
+                param.Modalidade,
+                param.Semestre,
+                param.Bimestre));
+
+            List<GraficoBaseDto> fechamentos = new List<GraficoBaseDto>();
+
+            foreach (var fechamento in fechamentosRetorno)
+            {
+                var grupo = $"{fechamento.AnoTurma}";
+                if(fechamento.Quantidade > 0)
+                    fechamentos.Add(new GraficoBaseDto(grupo, fechamento.Quantidade, fechamento.Situacao.Name()));
+            }
+
+            return fechamentos.OrderBy(a => a.Grupo).ToList();
+        }
+    }
+}
