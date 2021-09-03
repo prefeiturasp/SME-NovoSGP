215c31f272ff0f7ecce0ec4c8ad5ce7f5f3d0018
BernardRibeiro <bernard.ribeiro@gmail.com>
2021-09-03 11:48:50 -0300

    Melhora solução do problema

--- src/SME.SGP.Dados/Repositorios/RepositorioAulaPrevistaBimestre.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioAulaPrevistaBimestre.cs
@@ -9,28 +9,41 @@ namespace SME.SGP.Dados.Repositorios
 {
     public class RepositorioAulaPrevistaBimestre : RepositorioBase<AulaPrevistaBimestre>, IRepositorioAulaPrevistaBimestre
     {
-        const string Select = @"select apb.id, apb.criado_em as CriadoEm, apb.criado_por as CriadoPor, apb.alterado_em as AlteradoEm, apb.alterado_por as AlteradoPor,
-                         apb.alterado_rf as AlteradoRF, apb.criado_rf as CriadoRF, p.bimestre, p.periodo_inicio as inicio, p.periodo_fim as fim,
-                         apb.aulas_previstas as Previstas,
-                         SUM(a.quantidade) filter (where a.tipo_aula = 1 and a.aula_cj = false) as CriadasTitular,
-                         SUM(a.quantidade) filter (where a.tipo_aula = 1 and a.aula_cj = true) as CriadasCJ,
-                         SUM(a.quantidade) filter (where a.tipo_aula = 1 and rf.id is not null) as Cumpridas,
-                         SUM(a.quantidade) filter (where a.tipo_aula = 1 and rf.id is null and a.data_aula <= now()) as CumpridasSemFrequencia,
-                         SUM(a.quantidade) filter (where a.tipo_aula = 2 and rf.id is not null) as Reposicoes
-                         from periodo_escolar p
-                         inner join tipo_calendario tp on p.tipo_calendario_id = tp.id
-                         left join aula_prevista ap on ap.tipo_calendario_id = p.tipo_calendario_id
-                         left join aula_prevista_bimestre apb on ap.id = apb.aula_prevista_id and p.bimestre = apb.bimestre and not apb.excluido
-                         left join aula a on a.turma_id = ap.turma_id and
-                         				a.disciplina_id = ap.disciplina_id and
-			                            a.tipo_calendario_id = p.tipo_calendario_id and
-				                        a.data_aula BETWEEN p.periodo_inicio AND p.periodo_fim
+        const string Select = @"
+                        select
+                            apb.id, 
+	                        apb.criado_em as CriadoEm,
+	                        apb.criado_por as CriadoPor, 
+	                        apb.alterado_em as AlteradoEm, 
+	                        apb.alterado_por as AlteradoPor,
+	                        apb.alterado_rf as AlteradoRF, 
+	                        apb.criado_rf as CriadoRF, 
+	                        p.bimestre,
+	                        p.periodo_inicio as inicio, 
+	                        p.periodo_fim as fim,
+	                        apb.aulas_previstas as Previstas,
+	                        cc.permite_registro_frequencia as LancaFrequencia,
+	                        SUM(a.quantidade) filter (where a.tipo_aula = 1 and a.aula_cj = false) as CriadasTitular,
+	                        SUM(a.quantidade) filter (where a.tipo_aula = 1 and a.aula_cj = true) as CriadasCJ,
+	                        SUM(a.quantidade) filter (where a.tipo_aula = 1 and rf.id is not null and cc.permite_registro_frequencia) as Cumpridas,
+                            SUM(a.quantidade) filter (where a.tipo_aula = 1 and a.data_aula <= now() and not cc.permite_registro_frequencia) as CumpridasSemFrequencia,
+	                        SUM(a.quantidade) filter (where a.tipo_aula = 2 and rf.id is not null and cc.permite_registro_frequencia) as Reposicoes,
+	                        SUM(a.quantidade) filter (where a.tipo_aula = 2 and a.data_aula <= now() and not cc.permite_registro_frequencia) as ReposicoesSemFrequencia   
+                        from periodo_escolar p
+	                        inner join tipo_calendario tp on p.tipo_calendario_id = tp.id
+	                        inner join aula_prevista ap on ap.tipo_calendario_id = p.tipo_calendario_id
+	                        left join aula_prevista_bimestre apb on ap.id = apb.aula_prevista_id and p.bimestre = apb.bimestre and not apb.excluido 
+	                        left join aula a on a.turma_id = ap.turma_id and		
+ 				                        a.disciplina_id = ap.disciplina_id and
+                                        a.tipo_calendario_id = p.tipo_calendario_id and
+                                        a.data_aula BETWEEN p.periodo_inicio AND p.periodo_fim
                                         and (a.id is null or not a.excluido)
-                         left join registro_frequencia rf on a.id = rf.aula_id ";
+	                        left join registro_frequencia rf on a.id = rf.aula_id
+	                        left join componente_curricular cc on a.disciplina_id::int8 = cc.id ";
 
         const string GroupOrderBy = @" group by p.bimestre, p.periodo_inicio, p.periodo_fim, apb.aulas_previstas, apb.Id,
                          	   ap.criado_em, ap.criado_por, ap.alterado_em , ap.alterado_por,
-                               ap.alterado_rf, ap.criado_rf; ";
+                               ap.alterado_rf, ap.criado_rf, cc.permite_registro_frequencia; ";
 
         public RepositorioAulaPrevistaBimestre(ISgpContext conexao) : base(conexao)
         {
--- src/SME.SGP.Dominio/Entidades/AulaPrevistaBimestreQuantidade.cs
+++ src/SME.SGP.Dominio/Entidades/AulaPrevistaBimestreQuantidade.cs
@@ -9,6 +9,7 @@ namespace SME.SGP.Dominio
         public int CriadasCJ { get; set; }
 
         public int Cumpridas { get; set; }
+
         public int CumpridasSemFrequencia { get; set; }
 
         public DateTime Inicio { get; set; }
@@ -16,5 +17,9 @@ namespace SME.SGP.Dominio
         public DateTime Fim { get; set; }
 
         public int Reposicoes { get; set; }
+
+        public int ReposicoesSemFrequencia { get; set; }
+
+        public bool LancaFrequencia { get; set; }
     }
 }
