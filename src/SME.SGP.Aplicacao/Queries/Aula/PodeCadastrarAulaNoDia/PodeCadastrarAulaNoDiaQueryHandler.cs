c7e01117cd8d35edf91298f6909ac312efb2416e
Rodrigo Galante <rodrigo.galante@globo.com>
2021-09-03 09:23:49 -0300

    Utilizando apenas uma query para retornar dre e ue

--- src/SME.SGP.Aplicacao/Queries/Aula/PodeCadastrarAulaNoDia/PodeCadastrarAulaNoDiaQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Aula/PodeCadastrarAulaNoDia/PodeCadastrarAulaNoDiaQueryHandler.cs
@@ -53,15 +53,13 @@ namespace SME.SGP.Aplicacao
 
         private async Task<bool> PodeCadastrarAulaNoDia(PodeCadastrarAulaNoDiaQuery request, Turma turma, long tipoCalendarioId)
         {
-            var ue = await mediator.Send(new ObterUEPorTurmaIdQuery(turma.Id));
-
-            var dre = await mediator.Send(new ObterDREPorIdQuery(ue.DreId));
+            var dreUe = await mediator.Send(new ObterCodigosDreUePorTurmaQuery(turma.CodigoTurma));
 
             return await mediator.Send(new ValidarSeEhDiaLetivoQuery(
                 request.DataAula,
-                tipoCalendarioId, 
-                ue.CodigoUe, 
-                dre.CodigoDre));
+                tipoCalendarioId,
+                dreUe.DreCodigo,
+                dreUe.UeCodigo));
         }
 
         private async Task<bool> ExisteAula(PodeCadastrarAulaNoDiaQuery request)
