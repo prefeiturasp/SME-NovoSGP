6aa731696d571b2ff4893ebba098503e8d593dac
robsonamcom <53609688+robsonamcom@users.noreply.github.com>
2021-09-06 14:29:06 -0300

    Update src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterOrdenacaoAreasConhecimento/ObterOrdenacaoAreasConhecimentoQuery.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterOrdenacaoAreasConhecimento/ObterOrdenacaoAreasConhecimentoQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterOrdenacaoAreasConhecimento/ObterOrdenacaoAreasConhecimentoQuery.cs
@@ -3,7 +3,7 @@ using SME.SGP.Infra;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SME.SGP.Aplicacao.Queries.AreaDoConhecimento.ObterOrdenacaoAreasConhecimento
+namespace SME.SGP.Aplicacao
 {
     public class ObterOrdenacaoAreasConhecimentoQuery : IRequest<IEnumerable<ComponenteCurricularGrupoAreaOrdenacaoDto>>
     {
