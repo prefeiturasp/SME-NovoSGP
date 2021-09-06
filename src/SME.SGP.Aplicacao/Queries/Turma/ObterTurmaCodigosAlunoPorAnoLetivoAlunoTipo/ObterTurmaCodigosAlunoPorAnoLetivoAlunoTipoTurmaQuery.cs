ab676fb1b2cd2d5e2d407b890fc43910ded48e64
Rafael Losi <rafael.losi@amcom.com.br>
2021-09-06 16:56:25 -0300

    48253 Consulta turmas que o aluno faz parte no bimestre

--- src/SME.SGP.Aplicacao/Queries/Turma/ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipo/ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/Turma/ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipo/ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery.cs
@@ -8,16 +8,18 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery : IRequest<string[]>
     {
-        public ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery(int anoLetivo, string codigoAluno, IEnumerable<TipoTurma> tiposTurmas, bool? consideraHistorico = null)
+        public ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery(int anoLetivo, string codigoAluno, IEnumerable<TipoTurma> tiposTurmas, bool? consideraHistorico = null, DateTime? dataReferencia = null)
         {
             AnoLetivo = anoLetivo;
             CodigoAluno = codigoAluno;
             TiposTurmas = tiposTurmas;
+            DataReferencia = dataReferencia;
             ConsideraHistorico = VerificaConsideraHistorico(consideraHistorico);
         }
         public int AnoLetivo { get; set; }
         public string CodigoAluno { get; set; }
         public IEnumerable<TipoTurma> TiposTurmas { get; set; }
+        public DateTime? DataReferencia { get; }
         public bool ConsideraHistorico { get; set; }
 
         private bool VerificaConsideraHistorico(bool? consideraHistorico)
