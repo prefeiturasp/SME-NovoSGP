ba6c6ac160e67ff45705dc658ecf0af9ba4e34b1
robsonamcom <53609688+robsonamcom@users.noreply.github.com>
2021-09-06 14:27:26 -0300

    Update src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AreaDoConhecimento/IObterComponentesDasAreasDeConhecimentoUseCase.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AreaDoConhecimento/IObterComponentesDasAreasDeConhecimentoUseCase.cs
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AreaDoConhecimento/IObterComponentesDasAreasDeConhecimentoUseCase.cs
@@ -1,7 +1,7 @@
 ﻿using SME.SGP.Infra;
 using System.Collections.Generic;
 
-namespace SME.SGP.Aplicacao.Interfaces.CasosDeUso.AreaDoConhecimento
+namespace SME.SGP.Aplicacao.Interfaces
 {
     public interface IObterComponentesDasAreasDeConhecimentoUseCase : IUseCase<(IEnumerable<DisciplinaDto>, IEnumerable<AreaDoConhecimentoDto>), IEnumerable<DisciplinaDto>>
     {
