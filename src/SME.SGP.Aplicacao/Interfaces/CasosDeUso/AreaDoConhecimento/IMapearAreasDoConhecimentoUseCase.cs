fd49b577d4c7fedf1b178cb49bbd39f945b071e5
robsonamcom <53609688+robsonamcom@users.noreply.github.com>
2021-09-06 14:27:05 -0300

    Update src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AreaDoConhecimento/IMapearAreasDoConhecimentoUseCase.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AreaDoConhecimento/IMapearAreasDoConhecimentoUseCase.cs
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AreaDoConhecimento/IMapearAreasDoConhecimentoUseCase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SME.SGP.Aplicacao.Interfaces.CasosDeUso.AreaDoConhecimento
+namespace SME.SGP.Aplicacao.Interfaces
 {
     public interface IMapearAreasDoConhecimentoUseCase : IUseCase<(IEnumerable<DisciplinaDto>, IEnumerable<AreaDoConhecimentoDto>, IEnumerable<ComponenteCurricularGrupoAreaOrdenacaoDto>, long), IEnumerable<IGrouping<(string Nome, int? Ordem, long Id), AreaDoConhecimentoDto>>>
     {
