59237e7203b557097b1f22711845a3bb6ea91540
robsonamcom <53609688+robsonamcom@users.noreply.github.com>
2021-09-06 14:29:01 -0300

    Update src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterComponentesAreasConhecimento/ObterComponentesAreasConhecimentoQueryHandler.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterComponentesAreasConhecimento/ObterComponentesAreasConhecimentoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterComponentesAreasConhecimento/ObterComponentesAreasConhecimentoQueryHandler.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SME.SGP.Aplicacao.Queries.AreaDoConhecimento.ObterComponentesAreasConhecimento
+namespace SME.SGP.Aplicacao
 {
     public class ObterComponentesAreasConhecimentoQueryHandler : IRequestHandler<ObterComponentesAreasConhecimentoQuery, IEnumerable<DisciplinaDto>>
     {
