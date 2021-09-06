a445086c3c4546c7ddad61e752c2ebd31a9039e6
robsonamcom <53609688+robsonamcom@users.noreply.github.com>
2021-09-06 14:29:11 -0300

    Update src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterOrdenacaoAreasConhecimento/ObterOrdenacaoAreasConhecimentoQueryHandler.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterOrdenacaoAreasConhecimento/ObterOrdenacaoAreasConhecimentoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/AreaDoConhecimento/ObterOrdenacaoAreasConhecimento/ObterOrdenacaoAreasConhecimentoQueryHandler.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SME.SGP.Aplicacao.Queries.AreaDoConhecimento.ObterOrdenacaoAreasConhecimento
+namespace SME.SGP.Aplicacao
 {
     public class ObterOrdenacaoAreasConhecimentoQueryHandler : IRequestHandler<ObterOrdenacaoAreasConhecimentoQuery, IEnumerable<ComponenteCurricularGrupoAreaOrdenacaoDto>>
     {
