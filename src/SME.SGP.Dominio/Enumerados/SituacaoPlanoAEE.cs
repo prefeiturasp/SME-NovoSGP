e2fa6b74d3bdd05882688deb3ccf9cce3fdc2226
William Manoel dos Santos <william.santos@amcom.com.br>
2021-09-10 19:52:28 -0300

    Alterar/Remover situações não utilizadas ou renomeadas e remover o salvar reetruturação de planos

--- src/SME.SGP.Dominio/Enumerados/SituacaoPlanoAEE.cs
+++ src/SME.SGP.Dominio/Enumerados/SituacaoPlanoAEE.cs
@@ -4,10 +4,8 @@ namespace SME.SGP.Dominio.Enumerados
 {
     public enum SituacaoPlanoAEE
     {
-        [Display(Name = "Em andamento")]
-        EmAndamento = 1,
-        [Display(Name = "Em andamento/Reestruturação")]
-        Reestruturado = 2,
+        [Display(Name = "validado")]
+        EmAndamento = 1,        
         [Display(Name = "Encerrado")]
         Encerrado = 3,
         [Display(Name = "Aguardando parecer da coordenação")]
--- src/SME.SGP.IoC/RegistraDependencias.cs
+++ src/SME.SGP.IoC/RegistraDependencias.cs
@@ -837,8 +837,7 @@ namespace SME.SGP.IoC
             services.TryAddScoped<IObterSituacaoEncaminhamentoPorEstudanteUseCase, ObterSituacaoEncaminhamentoPorEstudanteUseCase>();
             services.TryAddScoped<ISalvarPlanoAEEUseCase, SalvarPlanoAEEUseCase>();
             services.TryAddScoped<IObterVersoesPlanoAEEUseCase, ObterVersoesPlanoAEEUseCase>();
-            services.TryAddScoped<IObterRestruturacoesPlanoAEEPorIdUseCase, ObterRestruturacoesPlanoAEEPorIdUseCase>();
-            services.TryAddScoped<ISalvarReestruturacaoPlanoAEEUseCase, SalvarReestruturacaoPlanoAEEUseCase>();
+            services.TryAddScoped<IObterRestruturacoesPlanoAEEPorIdUseCase, ObterRestruturacoesPlanoAEEPorIdUseCase>();            
 
             services.TryAddScoped<IExecutaEncerramentoPlanoAEEEstudantesInativosUseCase, ExecutaEncerramentoPlanoAEEEstudantesInativosUseCase>();
             services.TryAddScoped<IEncerrarPlanosAEEEstudantesInativosUseCase, EncerrarPlanosAEEEstudantesInativosUseCase>();
