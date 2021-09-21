004d76745d7316708350d9809de91e442adde9c5
William Manoel dos Santos <william.santos@amcom.com.br>
2021-09-21 15:21:39 -0300

    Inicio teste unitarios do cadastro de aula

---
+++ teste/SME.SGP.Aplicacao.Teste/Handlers/InserirAulaUnicaCommandHandlerTeste.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Moq;
+using SME.SGP.Aplicacao.Commands.Aulas.InserirAula;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Aplicacao.Teste.Handlers
+{
+    public class InserirAulaUnicaCommandHandlerTeste
+    {
+        private readonly Mock<IMediator> mediator;
+        private readonly Mock<IRepositorioAula> repositorioAula;
+        private readonly InserirAulaUnicaCommandHandler inserirAulaUnicaCommandHandler;
+
+        public InserirAulaUnicaCommandHandlerTeste(Mock<IMediator> mediator, Mock<IRepositorioAula> repositorioAula, InserirAulaUnicaCommandHandler inserirAulaUnicaCommandHandler)
+        {
+            this.mediator = new Mock<IMediator>(); 
+            this.repositorioAula = new Mock<IRepositorioAula>(); 
+            this.inserirAulaUnicaCommandHandler = new InserirAulaUnicaCommandHandler(repositorioAula.Object ,mediator.Object); ;
+        }
+    }
+}
