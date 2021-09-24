71e44b5a41cc1dc2043871c85e29f8a6a7c83154
Robson <robson.silva@amcom.com.br>
2021-09-24 14:53:02 -0300

    Bug: 50060 Ajuste na funcionalidade de salvar plano acertando o fluxo de aprovação quando utilizado o recurso de devolução.

---
+++ src/SME.SGP.Aplicacao/Queries/PendenciaPlanoAEE/ExistePendenciaPlanoAEE/ExistePendenciaPlanoAEEQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExistePendenciaPlanoAEEQueryHandler : IRequestHandler<ExistePendenciaPlanoAEEQuery, bool>
+    {
+        private readonly IRepositorioPendenciaPlanoAEE repositorioPendenciaPlanoAEE;
+
+        public ExistePendenciaPlanoAEEQueryHandler(IRepositorioPendenciaPlanoAEE repositorioPendenciaPlanoAEE)
+        {
+            this.repositorioPendenciaPlanoAEE = repositorioPendenciaPlanoAEE ?? throw new ArgumentNullException(nameof(repositorioPendenciaPlanoAEE));
+        }
+
+        public async Task<bool> Handle(ExistePendenciaPlanoAEEQuery request, CancellationToken cancellationToken)
+        {
+            return await repositorioPendenciaPlanoAEE
+                .ExistePendenciaPorPlano(request.PlanoAeeId);
+
+        }
+    }
+}
