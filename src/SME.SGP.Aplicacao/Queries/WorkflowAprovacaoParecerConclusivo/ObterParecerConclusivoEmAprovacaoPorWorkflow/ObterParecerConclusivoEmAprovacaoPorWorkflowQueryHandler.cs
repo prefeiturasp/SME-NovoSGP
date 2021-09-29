ae8a6f120017495595c5d21c0d40ecf81a402431
Rafael Losi <rafael.losi@amcom.com.br>
2021-09-29 02:15:35 -0300

    50201 Aprovação do WF de Alteração de Parecer Conclusivo

---
+++ src/SME.SGP.Aplicacao/Queries/WorkflowAprovacaoParecerConclusivo/ObterParecerConclusivoEmAprovacaoPorWorkflow/ObterParecerConclusivoEmAprovacaoPorWorkflowQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterParecerConclusivoEmAprovacaoPorWorkflowQueryHandler : IRequestHandler<ObterParecerConclusivoEmAprovacaoPorWorkflowQuery, WFAprovacaoParecerConclusivo>
+    {
+        private readonly IRepositorioWFAprovacaoParecerConclusivo repositorioWFAprovacaoParecerConclusivo;
+
+        public ObterParecerConclusivoEmAprovacaoPorWorkflowQueryHandler(IRepositorioWFAprovacaoParecerConclusivo repositorioWFAprovacaoParecerConclusivo)
+        {
+            this.repositorioWFAprovacaoParecerConclusivo = repositorioWFAprovacaoParecerConclusivo ?? throw new ArgumentNullException(nameof(repositorioWFAprovacaoParecerConclusivo));
+        }
+
+        public async Task<WFAprovacaoParecerConclusivo> Handle(ObterParecerConclusivoEmAprovacaoPorWorkflowQuery request, CancellationToken cancellationToken)
+        {
+            return await repositorioWFAprovacaoParecerConclusivo.ObterPorWorkflowId(request.WorkflowId);
+        }
+    }
+}
