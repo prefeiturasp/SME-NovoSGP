173c198b598812fe35981ca4f1fcdfcf3a7cfd6d
marcoslobo <marcos.lobo.nit@gmail.com>
2021-09-30 16:31:21 -0300

    Modificação do Log para rabbit;

---
+++ src/SME.SGP.Aplicacao/Commands/Log/SalvarLogViaRabbit/SalvarLogViaRabbitCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Enumerados;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SalvarLogViaRabbitCommand : IRequest<bool>
+    {
+        public SalvarLogViaRabbitCommand(string mensagem, LogNivel nivel, string contexto, string observacao)
+        {
+            Mensagem = mensagem;
+            Nivel = nivel;
+            Contexto = contexto;
+            Observacao = observacao;
+        }
+
+        public string Mensagem { get; set; }
+        public LogNivel Nivel { get; set; }
+        public string Contexto { get; set; }
+        public string Observacao { get; set; }
+    }
+
+}
