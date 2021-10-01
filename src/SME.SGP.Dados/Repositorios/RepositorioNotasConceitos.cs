09aad3969ef902372c9d51256125407737228812
Rafael Losi <rafael.losi@amcom.com.br>
2021-10-01 08:56:45 -0300

    AB#50622 Correção erro ao carregar nota em aprovação e erro ao salvar Histórico

--- src/SME.SGP.Dados/Repositorios/RepositorioNotasConceitos.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioNotasConceitos.cs
@@ -93,14 +93,13 @@ namespace SME.SGP.Dados.Repositorios
 
         public async Task<double> ObterNotaEmAprovacao(string codigoAluno, long disciplinaId, long turmaFechamentoId)
         {
-            var sql = $@"select coalesce(w.nota,-1)
+            var sql = $@"select coalesce(coalesce(w.nota,w.conceito_id),-1)
                             from fechamento_turma ft 
                             inner join fechamento_turma_disciplina ftd on ftd.fechamento_turma_id = ft.id
                             inner join fechamento_aluno fa on fa.fechamento_turma_disciplina_id = ftd.id
                             inner join fechamento_nota fn on fn.fechamento_aluno_id = fa.id
                             left join wf_aprovacao_nota_fechamento w on w.fechamento_nota_id = fn.id
-                            where ft.id = @turmaFechamentoId and ftd.disciplina_id = @disciplinaId and fa.aluno_codigo = @codigoAluno";
-
+                            where ft.id = @turmaFechamentoId and fn.disciplina_id = @disciplinaId and fa.aluno_codigo = @codigoAluno";
 
             return await database.QuerySingleOrDefaultAsync<double>(sql, new { turmaFechamentoId,disciplinaId, codigoAluno });
         }
