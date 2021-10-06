a2b55845a847a712bee7f4c9d0a9e4b0e9796990
Helio Barros <helio.barros@amcom.com.br>
2021-10-06 15:07:58 -0300

    50826 - Verificar recusa de nota pós-conselho para quando não existe nota

---
+++ src/SME.SGP.Aplicacao/Commands/ConselhoClasseNota/ExcluirConselhoClasseNota/ExcluirConselhoClasseNotaCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExcluirConselhoClasseNotaCommandHandler : AsyncRequestHandler<ExcluirConselhoClasseNotaCommand>
+    {
+        private readonly IRepositorioConselhoClasseNota conselhoClasseNota;
+
+        public ExcluirConselhoClasseNotaCommandHandler(IRepositorioConselhoClasseNota conselhoClasseNota)
+        {
+            this.conselhoClasseNota = conselhoClasseNota ?? throw new ArgumentNullException(nameof(conselhoClasseNota));
+        }
+
+        protected override async Task Handle(ExcluirConselhoClasseNotaCommand request, CancellationToken cancellationToken)
+        {
+            await conselhoClasseNota.Excluir(request.ConselhoClasseNotaId);
+        }
+
+    }
+}
