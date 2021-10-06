33b4db6c7e9f865654edf3697c5d9bf971dc5691
Rafael Losi <rafael.losi@amcom.com.br>
2021-10-06 14:20:21 -0300

    49610 Corrigido script de numero de imagens

--- scripts/V486__ATUALIZACAO_PARAMETROS_QUANTIDADE_IMAGENS.sql
+++ scripts/V486__ATUALIZACAO_PARAMETROS_QUANTIDADE_IMAGENS.sql
@@ -1,6 +1,9 @@
 update parametros_sistema 
 set nome = 'QuantidadeImagensPercursoIndividualCrianca',
-descricao = 'Quantidade de Imagens permitiras no percurso individual da criança'
-where id = 336;
+	descricao = 'Quantidade de Imagens permitiras no percurso individual da criança',
+	valor = 3
+where tipo = 54;
 
-update parametros_sistema set valor = 2 where id in (400, 401);
+update parametros_sistema 
+set valor = 2 
+where tipo = 72;
