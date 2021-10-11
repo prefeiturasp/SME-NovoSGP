a14feb23fef3f20d10bfa4ff376f86907b50e32b
Thiago Fernando <thiago.fernando@msn.com>
2021-10-11 17:36:34 -0300

    Criado commands

--- src/SME.SGP.Aplicacao/Comandos/ComandosConselhoClasseAluno.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosConselhoClasseAluno.cs
@@ -3,6 +3,9 @@ using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using System;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using MediatR;
 
 namespace SME.SGP.Aplicacao
 {
@@ -11,14 +14,16 @@ namespace SME.SGP.Aplicacao
         private readonly IConsultasConselhoClasseAluno consultasConselhoClasseAluno;
         private readonly IConsultasConselhoClasse consultasConselhoClasse;
         private readonly IServicoConselhoClasse servicoConselhoClasse;
+        private readonly IMediator  mediator;
 
         public ComandosConselhoClasseAluno(IConsultasConselhoClasseAluno consultasConselhoClasseAluno,
                                            IConsultasConselhoClasse consultasConselhoClasse,
-                                           IServicoConselhoClasse servicoConselhoClasse)
+                                           IServicoConselhoClasse servicoConselhoClasse, IMediator mediator)
         {
             this.consultasConselhoClasseAluno = consultasConselhoClasseAluno ?? throw new ArgumentNullException(nameof(consultasConselhoClasseAluno));
             this.consultasConselhoClasse = consultasConselhoClasse ?? throw new ArgumentNullException(nameof(consultasConselhoClasse));
             this.servicoConselhoClasse = servicoConselhoClasse ?? throw new ArgumentNullException(nameof(servicoConselhoClasse));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         public async Task<ParecerConclusivoDto> GerarParecerConclusivoAsync(long conselhoClasseId, long fechamentoTurmaId, string alunoCodigo)
@@ -44,6 +49,8 @@ namespace SME.SGP.Aplicacao
                 };
             }
 
+            var moverArquivo = mediator.Send(new MoverArquivoPastaDestinoCommand(TipoArquivo.Editor, conselhoClasseAlunoDto.AnotacoesPedagogicas));
+            var deletar = mediator.Send(new DeletarArquivoPastaTempCommand(conselhoClasseAluno.AnotacoesPedagogicas, conselhoClasseAlunoDto.AnotacoesPedagogicas));
             conselhoClasseAluno.AnotacoesPedagogicas = conselhoClasseAlunoDto.AnotacoesPedagogicas;
             conselhoClasseAluno.RecomendacoesAluno = conselhoClasseAlunoDto.RecomendacaoAluno;
             conselhoClasseAluno.RecomendacoesFamilia = conselhoClasseAlunoDto.RecomendacaoFamilia;
---
+++ src/SME.SGP.Aplicacao/Commands/DeletarArquivo/DeletarArquivoPastaTempCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class DeletarArquivoPastaTempCommandHandler : IRequestHandler<DeletarArquivoPastaTempCommand, bool>
+    {
+        public async Task<bool> Handle(DeletarArquivoPastaTempCommand request, CancellationToken cancellationToken)
+        {
+            var expressao = @"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
+            var regex = new Regex(expressao);
+            var atual = regex.Matches(request.ArquivoAtual).Cast<Match>().Select(c => c.Value).ToList();
+            var novo = regex.Matches(request.ArquivoNovo).Cast<Match>().Select(c => c.Value).ToList();
+            var diferente = atual.Except(novo);
+            return true;
+        }
+
+        private void DeletarArquivo(IEnumerable diferente)
+        {
+
+        }
+    }
+}
