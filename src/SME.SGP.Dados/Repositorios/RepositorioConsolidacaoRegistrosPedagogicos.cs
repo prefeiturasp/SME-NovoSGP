e1d5d0bcfd5603ee3051e1cae3dbf70022c27f2c
Helio Barros <helio.barros@amcom.com.br>
2021-10-13 09:56:50 -0300

    Modelagem consolidação - relatório de registros pedagógicos

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioConsolidacaoRegistrosPedagogicos.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dados
+{
+    public class RepositorioConsolidacaoRegistrosPedagogicos : IRepositorioConsolidacaoRegistrosPedagogicos
+    {
+        private readonly ISgpContext database;
+
+        public RepositorioConsolidacaoRegistrosPedagogicos(ISgpContext database)
+        {
+            this.database = database ?? throw new System.ArgumentNullException(nameof(database));
+        }
+    }
+}
