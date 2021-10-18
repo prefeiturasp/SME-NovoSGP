e7aa32dd901e053a0f57fa52af8aaaf31cb96d4b
Thiago Fernando <thiago.fernando@msn.com>
2021-10-18 16:15:04 -0300

    51646 - Correção ao Deletar o Arquivo

--- src/SME.SGP.Aplicacao/Commands/DeletarArquivo/RemoverArquivosExcluidosCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/DeletarArquivo/RemoverArquivosExcluidosCommandHandler.cs
@@ -13,9 +13,11 @@ namespace SME.SGP.Aplicacao
     {
         public async Task<bool> Handle(RemoverArquivosExcluidosCommand request, CancellationToken cancellationToken)
         {
-            var expressao = @"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
+            var expressao = @"[0-9]{4}\\[0-9]{2}\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
             var regex = new Regex(expressao);
+            var regexCaminhoAtual = new Regex(expressao);
             var atual = regex.Matches(request.ArquivoAtual).Cast<Match>().Select(c => c.Value).ToList();
+            var caminhoAtual = regexCaminhoAtual.Matches(request.ArquivoAtual).Cast<Match>().Select(c => c.Value).ToList();
             var novo = regex.Matches(request.ArquivoNovo).Cast<Match>().Select(c => c.Value).ToList();
             var diferente = atual.Except(novo);
             DeletarArquivo(diferente, request.Caminho);
@@ -26,7 +28,7 @@ namespace SME.SGP.Aplicacao
         {
             foreach (var item in diferente)
             {
-                var path = Path.Combine(UtilArquivo.ObterDiretorioBase(), caminho, item.ToString());
+                var path = Path.Combine(UtilArquivo.ObterDiretorioBase(), caminho,item.ToString());
                 if (File.Exists(path))
                     File.Delete(path);
             }
