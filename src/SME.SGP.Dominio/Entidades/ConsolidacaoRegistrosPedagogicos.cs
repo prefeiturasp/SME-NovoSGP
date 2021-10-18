8f225f4a059621a7a8b0e51914bf390df40ab287
Helio Barros <helio.barros@amcom.com.br>
2021-10-18 17:39:58 -0300

    ajustes data

--- src/SME.SGP.Dominio/Entidades/ConsolidacaoRegistrosPedagogicos.cs
+++ src/SME.SGP.Dominio/Entidades/ConsolidacaoRegistrosPedagogicos.cs
@@ -16,9 +16,9 @@ namespace SME.SGP.Dominio
         public string RFProfessor { get; set; }
         public int QuantidadeAulas { get; set; }
         public int FrequenciasPendentes { get; set; }
-        public DateTime DataUltimaFrequencia { get; set; }
-        public DateTime DataUltimoDiarioBordo { get; set; }
-        public DateTime DataUltimoPlanoAula { get; set; }
+        public DateTime? DataUltimaFrequencia { get; set; }
+        public DateTime? DataUltimoDiarioBordo { get; set; }
+        public DateTime? DataUltimoPlanoAula { get; set; }
         public int DiarioBordoPendentes { get; set; }
         public int PlanoAulaPendentes { get; set; }
     }
