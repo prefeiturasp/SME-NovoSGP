8d4dfbbae537c71e30031d2fe0c330b0def78abe
Diego Delmiro <diego.delmiro@amcom.com.br>
2021-10-18 11:41:56 -0300

    51487 adicionando logica pra validar parametro ProcessarDeadletter

--- scripts/V497__ADICIONANDO_PARAMETRO_SISTEMA_PROCESSAR_DEADLETTER.sql
+++ scripts/V497__ADICIONANDO_PARAMETRO_SISTEMA_PROCESSAR_DEADLETTER.sql
@@ -1,7 +1,7 @@
 ﻿insert  into
     public.parametros_sistema (nome,descricao,valor,ano,criado_em,criado_por, criado_rf,tipo)
 select
-    'ProcessarDeadletter','parametro para ativar ou desativar o processamento das deadletter', '1', null, now(),'Sistema','Sistema',101
+    'ProcessarDeadletter','parametro para ativar ou desativar o processamento das deadletter', '1', 2021, now(),'Sistema','Sistema',101
 where
     not exists(
             select 	1
