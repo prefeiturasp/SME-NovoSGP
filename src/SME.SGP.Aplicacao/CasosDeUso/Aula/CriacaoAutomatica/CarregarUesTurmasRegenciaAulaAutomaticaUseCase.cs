15863c6715bddb9d8731d78d25579c02051b3584
Robson <robson.silva@amcom.com.br>
2021-10-18 14:47:57 -0300

    Bug: 50991 Ajuste na rotina de criação de aulas automáticas para regência, alterando para remover duplicidade de aulas e aulas já criadas no mesmo dia que já criadas pelo usuário na disciplina de regência. Tratamento da rotina para receber somente uma turma para o processamento.

--- src/SME.SGP.Aplicacao/CasosDeUso/Aula/CriacaoAutomatica/CarregarUesTurmasRegenciaAulaAutomaticaUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Aula/CriacaoAutomatica/CarregarUesTurmasRegenciaAulaAutomaticaUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Sentry;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
 using System;
@@ -16,19 +17,34 @@ namespace SME.SGP.Aplicacao
 
         public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
         {
-            foreach (var modalidade in new Modalidade[] { Modalidade.Fundamental, Modalidade.EJA })
+            Modalidade[] modalidades;
+            Turma turma = null;
+
+            if (mensagemRabbit?.Mensagem != null)
+            {
+                turma = await mediator.Send(new ObterTurmaComUeEDrePorCodigoQuery(mensagemRabbit.Mensagem.ToString()));
+
+                if (turma == null || (turma.ModalidadeCodigo != Modalidade.Fundamental && turma.ModalidadeCodigo != Modalidade.EJA))
+                    return false;
+                else
+                    modalidades = new Modalidade[] { turma.ModalidadeCodigo };
+            }
+            else
+                modalidades = new Modalidade[] { Modalidade.Fundamental, Modalidade.EJA };
+
+            foreach (var modalidade in modalidades)
             {
                 if (modalidade == Modalidade.EJA)
                 {
-                    await ObterDados(modalidade, 1);
-                    await ObterDados(modalidade, 2);
+                    await ObterDados(modalidade, 1, turma);
+                    await ObterDados(modalidade, 2, turma);
                 }
-                else await ObterDados(modalidade);
+                else await ObterDados(modalidade, turma: turma);
             }
             return true;
         }
 
-        private async Task<bool> ObterDados(Modalidade modalidade, int? semestre = null)
+        private async Task<bool> ObterDados(Modalidade modalidade, int? semestre = null, Turma turma = null)
         {
             var anoAtual = DateTime.Now.Year;
             var tipoCalendarioId = await mediator.Send(new ObterIdTipoCalendarioPorAnoLetivoEModalidadeQuery(modalidade, anoAtual, semestre));
@@ -39,11 +55,29 @@ namespace SME.SGP.Aplicacao
             {
                 var diasForaDoPeriodoEscolar = (List<DateTime>)await mediator.Send(new ObterDiasForaDoPeriodoEscolarQuery(periodosEscolares));
                 var diasLetivosENaoLetivos = await mediator.Send(new ObterDiasPorPeriodosEscolaresComEventosLetivosENaoLetivosQuery(periodosEscolares, tipoCalendarioId, true));
-                var uesCodigos = await mediator.Send(new ObterUesCodigosPorModalidadeEAnoLetivoQuery(modalidade, anoAtual));
+                var uesCodigos = turma != null ? new string[] { turma.Ue.CodigoUe } : await mediator.Send(new ObterUesCodigosPorModalidadeEAnoLetivoQuery(modalidade, anoAtual));
                 foreach (var ueCodigo in uesCodigos)
                 {
                     var componentesCurriculares = modalidade == Modalidade.Fundamental ? componentesCurricularesFundamental : componentesCurricularesEja;
-                    var dadosTurmaComponente = await mediator.Send(new ObterDadosComponenteCurricularTurmaPorUeEAnoLetivoQuery(anoAtual, ueCodigo, componentesCurriculares));
+
+                    var dadosTurmaComponente = new List<DadosTurmaAulasAutomaticaDto>();
+                    if (turma != null)
+                    {
+                        var componentesTurmaEol = await mediator.Send(new ObterComponentesCurricularesEOLPorTurmasCodigoQuery(new string[] { turma.CodigoTurma }, false));
+                        var componentesConsiderados = componentesTurmaEol.Where(ct => ct.Regencia && componentesCurriculares.Contains(ct.Codigo));
+                        var dadosTurmaEol = await mediator.Send(new ObterDadosTurmaEolQuery(turma.CodigoTurma));
+                        componentesConsiderados.ToList().ForEach(cc => dadosTurmaComponente.Add(new DadosTurmaAulasAutomaticaDto()
+                        {
+                            ComponenteCurricularCodigo = cc.Codigo,
+                            ComponenteCurricularDescricao = cc.Descricao,
+                            TurmaCodigo = turma.CodigoTurma,
+                            DataInicioTurma = dadosTurmaEol.DataInicioTurma
+                        }));
+
+                    }
+                    else
+                        dadosTurmaComponente.AddRange(await mediator.Send(new ObterDadosComponenteCurricularTurmaPorUeEAnoLetivoQuery(anoAtual, ueCodigo, componentesCurriculares)));
+
                     if (dadosTurmaComponente.Any())
                     {
                         var dados = new DadosCriacaoAulasAutomaticasDto(ueCodigo, tipoCalendarioId, diasLetivosENaoLetivos, diasForaDoPeriodoEscolar, modalidade, dadosTurmaComponente);
