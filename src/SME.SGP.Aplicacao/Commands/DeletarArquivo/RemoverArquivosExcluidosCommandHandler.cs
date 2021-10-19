59ba97bb341de98a08169b899aba59a469e18515
Thiago Fernando <thiago.fernando@msn.com>
2021-10-19 09:25:12 -0300

    Adicionado command de delete ao excluir registro na anotações do estudante e devolutiva

--- src/SME.SGP.Aplicacao/Commands/DeletarArquivo/RemoverArquivosExcluidosCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/DeletarArquivo/RemoverArquivosExcluidosCommandHandler.cs
@@ -13,7 +13,7 @@ namespace SME.SGP.Aplicacao
     {
         public async Task<bool> Handle(RemoverArquivosExcluidosCommand request, CancellationToken cancellationToken)
         {
-            var expressao = @"[0-9]{4}\\[0-9]{2}\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
+            var expressao = @"\\[0-9]{4}\\[0-9]{2}\\[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
             var regex = new Regex(expressao);
             var atual = regex.Matches(request.ArquivoAtual).Cast<Match>().Select(c => c.Value).ToList();
             var novo = regex.Matches(request.ArquivoNovo).Cast<Match>().Select(c => c.Value).ToList();
@@ -26,9 +26,9 @@ namespace SME.SGP.Aplicacao
         {
             foreach (var item in diferente)
             {
-                var path = Path.Combine(UtilArquivo.ObterDiretorioBase(),caminho,item.ToString());
-                if (File.Exists(path))
-                    File.Delete(path);
+                var arquivo = $@"{UtilArquivo.ObterDiretorioBase()}\{caminho}{item.ToString()}";
+                if (File.Exists(arquivo))
+                    File.Delete(arquivo);
             }
 
         }
