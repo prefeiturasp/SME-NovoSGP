cbef65d835a98c9f4586e660ae45856fa109a6ca
Thiago Fernando <thiago.fernando@msn.com>
2021-10-20 09:54:20 -0300

    Excluir documentos do plano de aula e frequencia quando excluir aula

--- src/SME.SGP.Aplicacao/Commands/PlanoAula/ExcluirPlanoAulaDaAula/ExcluirPlanoAulaDaAulaCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/PlanoAula/ExcluirPlanoAulaDaAula/ExcluirPlanoAulaDaAulaCommandHandler.cs
@@ -1,8 +1,8 @@
 ﻿using MediatR;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,16 +11,32 @@ namespace SME.SGP.Aplicacao
     public class ExcluirPlanoAulaDaAulaCommandHandler : IRequestHandler<ExcluirPlanoAulaDaAulaCommand, bool>
     {
         private readonly IRepositorioPlanoAula repositorioPlanoAula;
+        private readonly IMediator mediator;
 
         public ExcluirPlanoAulaDaAulaCommandHandler(IRepositorioPlanoAula repositorioPlanoAula)
         {
             this.repositorioPlanoAula = repositorioPlanoAula ?? throw new ArgumentNullException(nameof(repositorioPlanoAula));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         public async Task<bool> Handle(ExcluirPlanoAulaDaAulaCommand request, CancellationToken cancellationToken)
         {
+            var plano = await mediator.Send(new ObterPlanoAulaPorAulaIdQuery(request.AulaId));
+
+            MoverRemoverExcluidos(plano.Descricao, TipoArquivo.PlanoAula);
+            MoverRemoverExcluidos(plano.DesenvolvimentoAula,TipoArquivo.PlanoAulaDesenvolvimento);
+            MoverRemoverExcluidos(plano.RecuperacaoAula, TipoArquivo.PlanoAulaRecuperacao);
+            MoverRemoverExcluidos(plano.LicaoCasa, TipoArquivo.PlanoAulaLicaoCasa);
+
             await repositorioPlanoAula.ExcluirPlanoDaAula(request.AulaId);
             return true;
+        }
+        private void MoverRemoverExcluidos(string mensagem,TipoArquivo tipo)
+        {
+            if (!string.IsNullOrEmpty(mensagem))
+            {
+                var deletarArquivosNaoUtilziados = mediator.Send(new RemoverArquivosExcluidosCommand(mensagem, string.Empty, tipo.Name()));
+            }
         }
     }
 }
