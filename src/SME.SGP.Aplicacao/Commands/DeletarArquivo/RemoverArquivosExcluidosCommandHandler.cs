57e1d804143603af76f8e2b581b5ba0b0297f8e8
Rafael Losi <rafael.losi@amcom.com.br>
2021-10-21 11:43:06 -0300

    removido console51752 Corrigido Regex

--- src/SME.SGP.Aplicacao/Commands/DeletarArquivo/RemoverArquivosExcluidosCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/DeletarArquivo/RemoverArquivosExcluidosCommandHandler.cs
@@ -13,7 +13,7 @@ namespace SME.SGP.Aplicacao
     {
         public async Task<bool> Handle(RemoverArquivosExcluidosCommand request, CancellationToken cancellationToken)
         {
-            var expressao = @"\\[0-9]{4}\\[0-9]{2}\\[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
+            var expressao = @"\/[0-9]{4}\/[0-9]{2}\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[A-Za-z0-4]+";
             var regex = new Regex(expressao);
             var atual = regex.Matches(request.ArquivoAtual).Cast<Match>().Select(c => c.Value).ToList();
             var novo = regex.Matches(request.ArquivoNovo).Cast<Match>().Select(c => c.Value).ToList();
