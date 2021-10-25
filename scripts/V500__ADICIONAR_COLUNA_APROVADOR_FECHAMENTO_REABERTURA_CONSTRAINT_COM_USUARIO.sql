0630897f54c489157d1b96614663d16be44d81de
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2021-10-25 13:51:42 -0300

    Usando a função existente

--- scripts/V500__ADICIONAR_COLUNA_APROVADOR_FECHAMENTO_REABERTURA_CONSTRAINT_COM_USUARIO.sql
+++ scripts/V500__ADICIONAR_COLUNA_APROVADOR_FECHAMENTO_REABERTURA_CONSTRAINT_COM_USUARIO.sql
@@ -2,16 +2,4 @@
 ALTER TABLE fechamento_reabertura ADD COLUMN IF NOT EXISTS aprovador_id int8 null;
  
 --> Adicionando a referência (constraint) com tabela de usuarios
-DO 
-$$ BEGIN
-IF NOT EXISTS (select constraint_name 
-               from information_schema.table_constraints 
-               where upper(table_name) =  upper('fechamento_reabertura')  
-               and upper(constraint_name) = upper('fechamento_reabertura_usuario_fk'))
-THEN
-   ALTER TABLE fechamento_reabertura ADD CONSTRAINT fechamento_reabertura_usuario_fk FOREIGN KEY (aprovador_id) REFERENCES public.usuario(id);
-ELSE raise NOTICE 'Constraint fechamento_reabertura_usuario_fk já existe';   
-
-END IF;
-END
-$$;
+select f_cria_constraint_se_nao_existir('fechamento_reabertura','fechamento_reabertura_usuario_fk','FOREIGN KEY (aprovador_id) REFERENCES public.usuario(id)')
