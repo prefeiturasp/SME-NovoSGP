e371f1e1dc88fc39b14a2cd2ae8161fad3c7cd21
Thiago Fernando <thiago.fernando@msn.com>
2021-11-01 10:52:57 -0300

    Ajustes não finalizados

---
+++ src/SME.SGP.Infra/Dtos/Relatorios/AcompanhamentoFrequencia/FiltroAcompanhamentoFrequenciaJustificativaDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SME.SGP.Infra
+{
+    public class FiltroAcompanhamentoFrequenciaJustificativaDto
+    {
+        public IEnumerable<int> AlunosCodigo { get; set; }
+        public string DreCodigo { get; set; }
+        public string UeCodigo { get; set; }
+        public string TurmaCodigo { get; set; }
+        public string Bimestre { get; set; }
+        public string UsuarioRF { get; set; }
+        public string UsuarioNome { get; set; }
+    }
+}
