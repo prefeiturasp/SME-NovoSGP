e146b3f8f5c2f325d32fa167fff0eb4ce7a4940e
William Manoel dos Santos <william.santos@amcom.com.br>
2021-11-02 13:20:40 -0300

    ajuste na consolidação

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConsolidacaoRegistrosPedagogicos.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConsolidacaoRegistrosPedagogicos.cs
@@ -1,7 +1,5 @@
 ﻿using SME.SGP.Infra;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Dominio
@@ -9,6 +7,7 @@ namespace SME.SGP.Dominio
     public interface IRepositorioConsolidacaoRegistrosPedagogicos
     {
         Task<bool> ExisteConsolidacaoRegistroPedagogicoPorAno(int ano);
+        Task ExcluirPorAno(int anoLetivo);
         Task Excluir(ConsolidacaoRegistrosPedagogicos consolidacao);
         Task<IEnumerable<ConsolidacaoRegistrosPedagogicosDto>> GerarRegistrosPedagogicos(long ueId, int anoLetivo);
         Task<long> Inserir(ConsolidacaoRegistrosPedagogicos consolidacao);
