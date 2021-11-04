ab33b49a39e53f6653cc83df62fc9734b6f62060
Helio Barros <helio.barros@amcom.com.br>
2021-11-04 16:06:16 -0300

    modelagem + entidades + mapeamentos + repositorios + dependências de pendencia_perfil e pendencia_perfil_usuario

---
+++ src/SME.SGP.Dominio/Entidades/PendenciaPerfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SGP.Dominio
+{
+    public class PendenciaPerfil : EntidadeBase
+    {
+        public int Cargo { get; set; }
+        public int Nivel { get; set; }
+        public long PendenciaId { get; set; }
+        public Pendencia Pendencia { get; set; }       
+    }
+}
