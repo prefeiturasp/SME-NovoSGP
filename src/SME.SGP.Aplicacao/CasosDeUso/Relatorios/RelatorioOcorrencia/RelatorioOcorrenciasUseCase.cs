be42c8e201aca40caa2a2ae3cf46e09b8ea0bced
Thiago Fernando <thiago.fernando@msn.com>
2021-11-18 09:51:49 -0300

    SGP - Endpoint chamada do relatório

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/RelatorioOcorrencia/RelatorioOcorrenciasUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class RelatorioOcorrenciasUseCase : AbstractUseCase, IRelatorioOcorrenciasUseCase
+    {
+        public RelatorioOcorrenciasUseCase(IMediator mediator) : base(mediator){}
+        public async Task<bool> Executar(FiltroImpressaoOcorrenciaDto filtro)
+        {
+            var usuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+            filtro.UsuarioNome = usuarioLogado.Nome;
+            filtro.UsuarioRf = usuarioLogado.CodigoRf;
+            return await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.RelatorioOcorrencias, filtro, usuarioLogado, rotaRelatorio: RotasRabbitSgpRelatorios.RotaRelatoriosSolicitadosImpressaoOcorrencias));
+        }
+    }
+}
