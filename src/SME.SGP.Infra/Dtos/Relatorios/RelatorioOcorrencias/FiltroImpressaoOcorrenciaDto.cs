2a14d8c0fd600347ad200f87b9cff4876cf248e9
Rafael Losi <rafael.losi@amcom.com.br>
2021-11-19 15:16:56 -0300

    26880 Alterado tipo TurmaId

--- src/SME.SGP.Infra/Dtos/Relatorios/RelatorioOcorrencias/FiltroImpressaoOcorrenciaDto.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/RelatorioOcorrencias/FiltroImpressaoOcorrenciaDto.cs
@@ -6,7 +6,7 @@ namespace SME.SGP.Infra
     {
         public string DreCodigo { get; set; }
         public string UeCodigo { get; set; }
-        public string TurmaCodigo { get; set; }
+        public long TurmaId { get; set; }
         public string UsuarioNome { get; set; }
         public string UsuarioRf { get; set; }
         public IEnumerable<long> OcorrenciasIds { get; set; }
