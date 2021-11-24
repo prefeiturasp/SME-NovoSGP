b6b2ddb8364cdcef97ef1ba7906a8804459f6c95
William Manoel dos Santos <william.santos@amcom.com.br>
2021-11-24 17:08:59 -0300

    locando informações da requisição sincrona do relatório de itinerancia

--- src/SME.SGP.Aplicacao/Commands/Relatorios/SolicitaRelatorioItinerancia/SolicitaRelatorioItineranciaCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Relatorios/SolicitaRelatorioItinerancia/SolicitaRelatorioItineranciaCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Newtonsoft.Json;
+using SME.SGP.Dominio.Interfaces.Repositorios;
 using System;
 using System.Net;
 using System.Net.Http;
@@ -12,24 +13,38 @@ namespace SME.SGP.Aplicacao
     public class SolicitaRelatorioItineranciaCommandHandler : IRequestHandler<SolicitaRelatorioItineranciaCommand, Guid>
     {
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly IRepositorioTesteLog repositorioTesteLog;
 
-        public SolicitaRelatorioItineranciaCommandHandler(IHttpClientFactory httpClientFactory)
+        public SolicitaRelatorioItineranciaCommandHandler(IHttpClientFactory httpClientFactory, IRepositorioTesteLog repositorioTesteLog)
         {
             this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            this.repositorioTesteLog = repositorioTesteLog ?? throw new ArgumentNullException(nameof(repositorioTesteLog));
         }
 
         public async Task<Guid> Handle(SolicitaRelatorioItineranciaCommand request, CancellationToken cancellationToken)
-        {            
-            var httpClient = httpClientFactory.CreateClient("servicoServidorRelatorios");            
-            var filtro = JsonConvert.SerializeObject(request.Filtro);
-            var resposta = await httpClient.PostAsync($"api/v1/relatorios/sincronos/itinerancias", new StringContent(filtro, Encoding.UTF8, "application/json-patch+json"));
-            
-            if (resposta.IsSuccessStatusCode && resposta.StatusCode != HttpStatusCode.NoContent)
+        {   
+            try
             {
-                var json = await resposta.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<Guid>(json);
+                var httpClient = httpClientFactory.CreateClient("servicoServidorRelatorios");
+                var filtro = JsonConvert.SerializeObject(request.Filtro);
+                var resposta = await httpClient.PostAsync($"api/v1/relatorios/sincronos/itinerancias", new StringContent(filtro, Encoding.UTF8, "application/json-patch+json"));
+
+                if (resposta.IsSuccessStatusCode && resposta.StatusCode != HttpStatusCode.NoContent)
+                {
+                    var json = await resposta.Content.ReadAsStringAsync();
+
+                    await repositorioTesteLog.Gravar($"DefaultRequestHeaders - {httpClient.DefaultRequestHeaders.ToString()} - StatusCode de Resposta - {resposta.StatusCode.ToString()} - Código correlação : {json}");
+                    return JsonConvert.DeserializeObject<Guid>(json);
+                }
+                var mensagem = $"DefaultRequestHeaders - {httpClient.DefaultRequestHeaders.ToString()} - StatusCode de Resposta - {resposta.StatusCode.ToString()}";
+                await repositorioTesteLog.Gravar(mensagem);
+                return Guid.Empty;
             }
-            return Guid.Empty;
+            catch (Exception ex)
+            {
+                await repositorioTesteLog.Gravar($"SolicitaRelatorioItineranciaCommand Error - {ex.ToString()}");
+                throw;
+            }
         }
     }
 }
