35588b7500a94664d225ce72aa8dea9c530dd371
Luiz Cezário <lkcezario@gmail.com>
2021-11-24 17:26:45 -0300

    AB#53773 - Refatora caso de uso

--- src/SME.SGP.Aplicacao/CasosDeUso/CompensacaoAusencia/PeriodoDeCompensacaoAbertoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/CompensacaoAusencia/PeriodoDeCompensacaoAbertoUseCase.cs
@@ -14,20 +14,25 @@ namespace SME.SGP.Aplicacao
         public PeriodoDeCompensacaoAbertoUseCase(IMediator mediator)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-        }       
+        }
 
         public async Task<bool> VerificarPeriodoAberto(string turmaCodigo, int bimestre)
         {
             var turma = await mediator.Send(new ObterTurmaComUeEDrePorCodigoQuery(turmaCodigo));
 
-            var parametroSistema = await mediator.Send(new ObterParametroSistemaPorTipoEAnoQuery(TipoParametroSistema.PermiteCompensacaoForaPeriodo, turma.AnoLetivo));
+            var parametroSistema = await mediator
+                                         .Send(new ObterParametroSistemaPorTipoEAnoQuery(TipoParametroSistema.PermiteCompensacaoForaPeriodo,
+                                                                                         turma.AnoLetivo));
 
-            if (parametroSistema != null && parametroSistema.Ativo && turma.AnoLetivo == DateTimeExtension.HorarioBrasilia().Year)
-                return true;
+            var periodoEstahAberto = parametroSistema != null
+                                    ? parametroSistema.Ativo && turma.AnoLetivo == DateTimeExtension.HorarioBrasilia().Year
+                                    : await mediator
+                                            .Send(new TurmaEmPeriodoAbertoQuery(turma,
+                                                                                DateTimeExtension.HorarioBrasilia(),
+                                                                                bimestre,
+                                                                                true));
 
-            var turmaEmPeriodoAberto = await mediator.Send(new TurmaEmPeriodoAbertoQuery(turma, DateTimeExtension.HorarioBrasilia(), bimestre, true));
-
-            return turmaEmPeriodoAberto;
+            return periodoEstahAberto;
         }
     }
 }
