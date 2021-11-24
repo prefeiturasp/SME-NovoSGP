38f7cb3b6ce183f58b89b529a717f23d09e83bae
Robson <robson.silva@amcom.com.br>
2021-11-24 08:13:25 -0300

    Bug: 53043 Implementação para tentar detectar o problema da mensagem cair no deadletter ao executar a notificação de salvar itinerancia.

--- src/SME.SGP.Aplicacao/CasosDeUso/Itinerancia/Notificacao/NotificacaoSalvarItineranciaUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Itinerancia/Notificacao/NotificacaoSalvarItineranciaUseCase.cs
@@ -2,28 +2,41 @@
 using Newtonsoft.Json;
 using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos;
+using System;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
     public class NotificacaoSalvarItineranciaUseCase : AbstractUseCase, INotificacaoSalvarItineranciaUseCase
     {
-        public NotificacaoSalvarItineranciaUseCase(IMediator mediator) : base(mediator)
+        private readonly IRepositorioTesteLog repositorioTesteLog;
+
+        public NotificacaoSalvarItineranciaUseCase(IMediator mediator, IRepositorioTesteLog repositorioTesteLog) : base(mediator)
         {
+            this.repositorioTesteLog = repositorioTesteLog ?? throw new ArgumentNullException(nameof(repositorioTesteLog));
         }
 
         public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
         {
-            var notificacao = JsonConvert.DeserializeObject<NotificacaoSalvarItineranciaDto>(mensagemRabbit.Mensagem.ToString());
+            try
+            {
+                var notificacao = JsonConvert.DeserializeObject<NotificacaoSalvarItineranciaDto>(mensagemRabbit.Mensagem.ToString());
 
-            if (notificacao == null)
-                throw new NegocioException("Não foi possível obter os dados do registro de itinerância, para criar a notificação.");
+                if (notificacao == null)
+                    throw new NegocioException("Não foi possível obter os dados do registro de itinerância, para criar a notificação.");
 
-            await mediator.Send(new NotificacaoSalvarItineranciaAlunosCommand(notificacao.UeId, notificacao.CriadoRF, notificacao.CriadoPor, notificacao.DataVisita, notificacao.Estudantes, notificacao.ItineranciaId));
+                await mediator.Send(new NotificacaoSalvarItineranciaAlunosCommand(notificacao.UeId, notificacao.CriadoRF, notificacao.CriadoPor, notificacao.DataVisita, notificacao.Estudantes, notificacao.ItineranciaId));
 
-            return true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                await repositorioTesteLog.Gravar(ex.ToString());
+                throw;
+            }
         }
     }
 }
