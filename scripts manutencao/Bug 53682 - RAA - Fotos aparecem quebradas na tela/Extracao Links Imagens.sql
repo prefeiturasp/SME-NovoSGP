230a2afafa979395b7403f6e778153c02d7f98eb
Rafael Losi <rafael.losi@amcom.com.br>
2021-11-28 16:56:15 -0300

    AB#54128 Alterado coluna do Link original da imagem

--- scripts manutencao/Bug 53682 - RAA - Fotos aparecem quebradas na tela/Extracao Links Imagens.sql
+++ scripts manutencao/Bug 53682 - RAA - Fotos aparecem quebradas na tela/Extracao Links Imagens.sql
@@ -1,10 +1,6 @@
 drop table if exists tmp_acompanhamento_aluno;
 
-select aas.* 
-	, CONCAT('https://novosgp.sme.prefeitura.sp.gov.br/Arquivos/aluno/acompanhamento/',
-        EXTRACT(YEAR FROM aas.CRIADO_EM),'/',
-        EXTRACT(MONTH FROM aas.CRIADO_EM),'/',
-	 	aas.Nome) as NomeCompleto
+select aas.id, aas.Codigo, aas.link as NomeCompleto
 	, CONCAT('https://novosgp.sme.prefeitura.sp.gov.br/Arquivos/aluno/acompanhamento/',
         EXTRACT(YEAR FROM a.CRIADO_EM),'/',
         EXTRACT(MONTH FROM a.CRIADO_EM),'/',
@@ -18,11 +14,16 @@ select aas.*
         EXTRACT(MONTH FROM a.CRIADO_EM),'/',
 	 	aas.Nome) as NomeCompletoAlternativo3
 into tmp_acompanhamento_aluno
+
   from arquivo a
  inner join (
 select uuid(cast(regexp_matches(PERCURSO_INDIVIDUAL, '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}', 'gi')  as varchar)) as Codigo
-	, CONCAT(uuid(cast(regexp_matches(PERCURSO_INDIVIDUAL, '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}', 'gi')  as varchar))
-	, SUBSTRING(cast(regexp_matches(PERCURSO_INDIVIDUAL, '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[0-9a-zA-Z]+', 'gi') as varchar), '\.[0-9a-zA-Z]+')) as Nome
+	, replace(replace(cast(
+		regexp_matches(PERCURSO_INDIVIDUAL, '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[0-9a-zA-Z]+', 'gi') 
+		as varchar),'{',''),'}','') as Nome
+	, replace(replace(cast(
+		regexp_matches(PERCURSO_INDIVIDUAL, 'https:\/\/novosgp.sme.prefeitura.sp.gov.br\/Arquivos\/aluno\/acompanhamento\/2021\/(?:[0-9]{1}|[0-9]{2})\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}.[0-9a-zA-Z]+', 'gi') 
+		as varchar),'{',''),'}','') as Link
 	, aas.ID, aas.PERCURSO_INDIVIDUAL, aas.CRIADO_EM
 from ACOMPANHAMENTO_ALUNO_SEMESTRE aas
 where PERCURSO_INDIVIDUAL like '%https://novosgp.sme.prefeitura.sp.gov.br/Arquivos/aluno/acompanhamento/%'
