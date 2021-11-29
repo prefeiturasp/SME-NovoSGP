226ed87c7e9f300cd369d7d2d15471c802cc0cf4
William Manoel dos Santos <william.santos@amcom.com.br>
2021-11-29 18:36:17 -0300

    Ajuste nas informações da listagem

---
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterDescricaoComponentesCurricularesPorIds/ObterDescricaoComponentesCurricularesPorIdsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterDescricaoComponentesCurricularesPorIdsQueryHandler : IRequestHandler<ObterDescricaoComponentesCurricularesPorIdsQuery, IEnumerable<ComponenteCurricularSimplesDto>>
+    {
+        private readonly IRepositorioComponenteCurricular repositorioComponenteCurricular;
+
+        public ObterDescricaoComponentesCurricularesPorIdsQueryHandler(IRepositorioComponenteCurricular repositorioComponenteCurricular)
+        {
+            this.repositorioComponenteCurricular = repositorioComponenteCurricular ?? throw new ArgumentNullException(nameof(repositorioComponenteCurricular));
+        }
+
+        public async Task<IEnumerable<ComponenteCurricularSimplesDto>> Handle(ObterDescricaoComponentesCurricularesPorIdsQuery request, CancellationToken cancellationToken)
+            => await repositorioComponenteCurricular.ObterDescricaoPorIds(request.Ids);
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/ComponenteCurricular/ComponenteCurricularSimplesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.SGP.Infra
+{
+    public class ComponenteCurricularSimplesDto
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
