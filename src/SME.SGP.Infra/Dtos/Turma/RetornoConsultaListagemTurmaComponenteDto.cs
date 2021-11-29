41eb907f7ece22a598efd9b9f7ac5b420888b369
William Manoel dos Santos <william.santos@amcom.com.br>
2021-11-29 14:29:05 -0300

    Ajuste na listagem paginada do listão

--- src/SME.SGP.Infra/Dtos/Turma/RetornoConsultaListagemTurmaComponenteDto.cs
+++ src/SME.SGP.Infra/Dtos/Turma/RetornoConsultaListagemTurmaComponenteDto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SME.SGP.Dominio;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SME.SGP.Infra
@@ -8,11 +10,17 @@ namespace SME.SGP.Infra
     {
         public int Id { get; set; }
         public long TurmaCodigo { get; set; }
-        public string Modalidade { get; set; }
+        public Modalidade Modalidade { get; set; }
         public string NomeTurma { get; set; }
         public string Ano { get; set; }
         public string NomeComponenteCurricular { get; set; }
         public long ComponenteCurricularCodigo { get; set; }
-        public int Turno { get; set; }
+        public int Turno { get; set; }       
+
+        public string NomeTurmaFormatado()
+        {
+            return $"{Modalidade.ShortName()} - {NomeTurma} - {Ano} - {NomeComponenteCurricular}";
+        }
+
     }
 }
