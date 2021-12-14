5c25af76f5742e33b3ef3bfb9581c5707a04464c
William Manoel dos Santos <william.santos@amcom.com.br>
2021-12-14 20:46:55 -0300

    Ajuste na regex

--- src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoAluno/SalvarAcompanhamentoAlunoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoAluno/SalvarAcompanhamentoAlunoUseCase.cs
@@ -36,7 +36,7 @@ namespace SME.SGP.Aplicacao
                     {
                         var caminhoBase = UtilArquivo.ObterDiretorioBase();
                         var caminhoArquivoDestino = Path.Combine(caminhoBase, novoCaminho, nomeArquivo.ToString());                        
-                        var str = Regex.Replace(acompanhamentoAluno.PercursoIndividual, @$"https.+?{nomeArquivo.ToString()}", caminhoArquivoDestino);
+                        var str = Regex.Replace(acompanhamentoAluno.PercursoIndividual, $@"https.+?{nomeArquivo.ToString()}", caminhoArquivoDestino);
                         acompanhamentoAluno.PercursoIndividual = str;
                     }
                 }
