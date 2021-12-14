ed7cef81b1a5440227ec806478f49ff9ce2f7e2f
Luiz Cezário <lkcezario@gmail.com>
2021-12-14 20:01:00 -0300

    AB#55253 - cria lógica para bloquear edição de atividade avaliativa de outro professor.

--- src/SME.SGP.Infra/Dtos/AtividadeAvaliativaCompletaDto.cs
+++ src/SME.SGP.Infra/Dtos/AtividadeAvaliativaCompletaDto.cs
@@ -6,15 +6,27 @@ namespace SME.SGP.Infra
     public class AtividadeAvaliativaCompletaDto : AtividadeAvaliativaDto
     {
         public DateTime? AlteradoEm { get; set; }
+        
         public string AlteradoPor { get; set; }
+        
         public string AlteradoRF { get; set; }
+        
         public List<AtividadeAvaliativaRegenciaDto> AtividadesRegencia { get; set; }
+        
         public string Categoria { get; set; }
+        
         public DateTime CriadoEm { get; set; }
+        
         public bool DentroPeriodo { get; set; }
+        
         public string CriadoPor { get; set; }
+        
         public string CriadoRF { get; set; }
+        
         public long Id { get; set; }
+        
         public bool Importado { get; set; }
+
+        public bool PodeEditarAvaliacao { get; set; }
     }
 }
