54bf5cf5f82ed97be57051ffb718f6e718838e76
Helio Barros <helio.barros@amcom.com.br>
2021-12-16 17:08:09 -0300

    ajuste endpoint de listagem de frequencias

--- src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ObterFrequenciasPorPeriodoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ObterFrequenciasPorPeriodoUseCase.cs
@@ -218,6 +218,29 @@ namespace SME.SGP.Aplicacao
             if (alunosDaTurma == null || !alunosDaTurma.Any())
                 throw new NegocioException("Não foram encontrados alunos para a aula/turma informada.");
 
+            var turma = await mediator.Send(new ObterTurmaComUeEDrePorCodigoQuery(codigoTurma));
+            if (turma == null)
+                throw new NegocioException("Não foi encontrada uma turma com o id informado. Verifique se você possui abrangência para essa turma.");
+
+            var tipoCalendarioId = await mediator.Send(new ObterTipoCalendarioIdPorTurmaQuery(turma));
+
+            var periodoEscolar = await mediator.Send(new ObterPeriodosEscolaresPorTipoCalendarioIdEDataQuery(tipoCalendarioId, dataInicio));
+            if (periodoEscolar == null)
+                throw new NegocioException("Ocorreu um erro, esta aula está fora do período escolar.");
+
+            var parametroPercentualCritico = await mediator.Send(new ObterParametroSistemaPorTipoEAnoQuery(TipoParametroSistema.PercentualFrequenciaCritico, periodoEscolar.PeriodoInicio.Year));
+            if (parametroPercentualCritico == null)
+                throw new NegocioException("Parâmetro de percentual de frequência em nível crítico não encontrado contate a SME.");
+            var percentualCritico = int.Parse(parametroPercentualCritico.Valor);
+
+            var parametroPercentualAlerta = await mediator.Send(new ObterParametroSistemaPorTipoEAnoQuery(TipoParametroSistema.PercentualFrequenciaAlerta, periodoEscolar.PeriodoInicio.Year));
+            if (parametroPercentualAlerta == null)
+                throw new NegocioException("Parâmetro de percentual de frequência em alerta não encontrado contate a SME.");
+            var percentualAlerta = int.Parse(parametroPercentualAlerta.Valor);
+
+            var frequenciaTurmaRegistrada = await mediator.Send(new ObterFrequenciaAlunosPorTurmaDisciplinaEPeriodoEscolarQuery(turma, long.Parse(disciplinaId), periodoEscolar.Id));
+            var turmaPossuiFrequenciaRegistrada = await mediator.Send(new ExisteFrequenciaRegistradaPorTurmaComponenteCurricularQuery(turma.CodigoTurma, disciplinaId, periodoEscolar.Id));
+
             foreach (var aluno in alunosDaTurma)
             {
                 var frequenciaAluno = new FrequeciaPorDataPeriodoDto
@@ -248,6 +271,9 @@ namespace SME.SGP.Aplicacao
                     }
                     frequenciaAluno.Aulas.Add(frequenciaAlunoAula);
                 }
+                var frequenciaAlunoRegistrada = frequenciaTurmaRegistrada.FirstOrDefault(a => a.CodigoAluno == aluno.CodigoAluno && a.DisciplinaId == disciplinaId && a.PeriodoEscolarId == periodoEscolar.Id);
+
+                frequenciaAluno.IndicativoFrequencia = ObterIndicativoFrequencia(frequenciaAlunoRegistrada, percentualAlerta, percentualCritico, turmaPossuiFrequenciaRegistrada, turma);
 
                 var detalhesAulaAluno = await ObterDetalhesFrequencia(aluno.CodigoAluno, dataInicio, dataFim,aulas);
                 foreach (var aulaDetalhe in detalhesAulaAluno)
@@ -294,33 +320,6 @@ namespace SME.SGP.Aplicacao
                         });
                     }
                 }
-
-                var turma = await mediator.Send(new ObterTurmaComUeEDrePorCodigoQuery(aula.TurmaId));
-                if (turma == null)
-                    throw new NegocioException("Não foi encontrada uma turma com o id informado. Verifique se você possui abrangência para essa turma.");
-
-                var periodoEscolar = await mediator.Send(new ObterPeriodosEscolaresPorTipoCalendarioIdEDataQuery(frequenciaAluno.TipoCalendario, frequenciaAluno.DataAula));
-                if (periodoEscolar == null)
-                    throw new NegocioException("Ocorreu um erro, esta aula está fora do período escolar.");
-
-                var parametroPercentualCritico = await mediator.Send(new ObterParametroSistemaPorTipoEAnoQuery(TipoParametroSistema.PercentualFrequenciaCritico, periodoEscolar.PeriodoInicio.Year));
-                if (parametroPercentualCritico == null)
-                    throw new NegocioException("Parâmetro de percentual de frequência em nível crítico não encontrado contate a SME.");
-                var percentualCritico = int.Parse(parametroPercentualCritico.Valor);
-
-                var parametroPercentualAlerta = await mediator.Send(new ObterParametroSistemaPorTipoEAnoQuery(TipoParametroSistema.PercentualFrequenciaAlerta, periodoEscolar.PeriodoInicio.Year));
-                if (parametroPercentualAlerta == null)
-                    throw new NegocioException("Parâmetro de percentual de frequência em alerta não encontrado contate a SME.");
-                var percentualAlerta = int.Parse(parametroPercentualAlerta.Valor);
-
-                var frequenciaTurmaRegistrada = await mediator.Send(new ObterFrequenciaAlunosPorTurmaDisciplinaEPeriodoEscolarQuery(turma, long.Parse(aula.DisciplinaId), periodoEscolar.Id));
-
-                var frequenciaAlunoRegistrada = frequenciaTurmaRegistrada.FirstOrDefault(a => a.CodigoAluno == codigoAluno && a.DisciplinaId == aula.DisciplinaId && a.PeriodoEscolarId == periodoEscolar.Id);
-
-                var turmaPossuiFrequenciaRegistrada = await mediator.Send(new ExisteFrequenciaRegistradaPorTurmaComponenteCurricularQuery(turma.CodigoTurma, aula.DisciplinaId, periodoEscolar.Id));
-
-                frequenciaDetalhadaALuno.IndicativoFrequencia = ObterIndicativoFrequencia(frequenciaAlunoRegistrada, percentualAlerta, percentualCritico, turmaPossuiFrequenciaRegistrada, turma);
-
                 frequenciaRetorno.Add(frequenciaDetalhadaALuno);
             }
             return frequenciaRetorno;
--- src/SME.SGP.Infra/Dtos/Frequencia/FrequeciaPorDataPeriodoDto.cs
+++ src/SME.SGP.Infra/Dtos/Frequencia/FrequeciaPorDataPeriodoDto.cs
@@ -12,6 +12,7 @@ namespace SME.SGP.Infra
         public string CodigoAluno { get; set; }
         public string NomeAluno { get; set; }
         public int NumeroAlunoChamada { get; set; }
+        public IndicativoFrequenciaDto IndicativoFrequencia { get; set; }
         public IList<FrequenciaAulaPorDataPeriodoDto> Aulas { get; set; }
         public IList<FrequenciaDetalhadaDto> AulasDetalhes { get; set; }
     }
