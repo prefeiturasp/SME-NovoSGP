ae41ba9244a1b8f69123f9db9312413cdabd4f66
Ricardo P. Silva <ricardo.silva@amcom.com.br>
2021-12-22 13:22:37 -0300

    Query para Endpoint Diario de Bordo Listão

---
+++ src/SME.SGP.Aplicacao/Queries/DiarioBordo/ObterDiarioDeBordoSemObservacoesPorId/ObterDiarioDeBordoSemObservacoesPorIdQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterDiarioDeBordoSemObservacoesPorIdQueryHandler : IRequestHandler<ObterDiarioDeBordoSemObservacoesPorIdQuery, DiarioBordoListaoDto>
+    {
+        private readonly IRepositorioDiarioBordo repositorioDiarioBordo;
+        private readonly IMediator mediator;
+
+        public ObterDiarioDeBordoSemObservacoesPorIdQueryHandler(IRepositorioDiarioBordo repositorioDiarioBordo, IMediator mediator)
+        {
+            this.repositorioDiarioBordo = repositorioDiarioBordo ?? throw new ArgumentNullException(nameof(repositorioDiarioBordo));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<DiarioBordoListaoDto> Handle(ObterDiarioDeBordoSemObservacoesPorIdQuery request, CancellationToken cancellationToken)
+        {
+            var diarioBordo = await repositorioDiarioBordo.ObterPorIdAsync(request.Id);
+
+            return MapearParaDto(diarioBordo);
+        }
+
+        private DiarioBordoListaoDto MapearParaDto(Dominio.DiarioBordo diarioBordo)
+        {
+            return new DiarioBordoListaoDto()
+            {
+                Auditoria = (AuditoriaDto)diarioBordo,
+                Planejamento = diarioBordo.Planejamento,
+                ReflexoesReplanejamento = diarioBordo.ReflexoesReplanejamento,
+            };
+        }
+    }
+}
