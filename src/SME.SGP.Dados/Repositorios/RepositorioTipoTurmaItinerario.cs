1bf3e6671d5d90536fd8dd396678955fd83fcfe3
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2022-01-17 13:48:07 -0300

    Criar entidade db tipos_turma_itinerario, Entidade Back, Repositorio e registro de dependencias

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioTipoTurmaItinerario.cs
@@ -0,0 +1,28 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioTipoTurmaItinerario : IRepositorioTipoTurmaItinerario
+    {
+        private readonly ISgpContext contexto;
+
+        public RepositorioTipoTurmaItinerario(ISgpContext contexto)
+        {
+            this.contexto = contexto;
+        }
+        
+        public async Task<IEnumerable<TipoTurmaItinerario>> ObterPorSerie(int serie)
+        {
+            return await contexto.Conexao.QueryAsync<TipoTurmaItinerario>("select * from tipo_turma_itinerario where serie = @serie", new { serie });
+        }
+
+        public async Task<TipoTurmaItinerario> ObterPorId(int id)
+        {
+            return await contexto.QueryFirstOrDefaultAsync<TipoTurmaItinerario>("select * from tipo_turma_itinerario where id = @id", new { id });
+        }
+    }
+}
