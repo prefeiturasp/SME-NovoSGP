ee2cb56827b444955379920e743b955e4f5187fa
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2022-01-26 13:14:53 -0300

    Removendo classes que foram migradas para a aPI EOL

--- src/SME.SGP.Dominio/Entidades/TipoTurmaItinerario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SME.SGP.Dominio
-{
-    public class TipoTurmaItinerario
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public int Serie { get; set; }
-    }
-}
