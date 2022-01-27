3819ee9acb50aaeb966fadbf8bf2181b5dbd6c52
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2022-01-27 18:41:15 -0300

    Inclusão dos tipos itinerários do ensino médio

--- src/SME.SGP.Aplicacao/Consultas/ConsultasConselhoClasseAluno.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasConselhoClasseAluno.cs
@@ -155,8 +155,9 @@ namespace SME.SGP.Aplicacao
 
             if (turma.DeveVerificarRegraRegulares() || turmasitinerarioEnsinoMedio.Any(a=> a.Id == (int)turma.TipoTurma))
             {
-                List<TipoTurma> turmasCodigosParaConsulta = new List<TipoTurma>() { turma.TipoTurma };
+                var turmasCodigosParaConsulta = new List<int>() { (int)turma.TipoTurma };
                 turmasCodigosParaConsulta.AddRange(turma.ObterTiposRegularesDiferentes());
+                turmasCodigosParaConsulta.AddRange(turmasitinerarioEnsinoMedio.Select(s=> s.Id));
                 turmasCodigos = await mediator.Send(new ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery(turma.AnoLetivo, alunoCodigo, turmasCodigosParaConsulta, consideraHistorico, periodoEscolar?.PeriodoFim));
                 if (!turmasCodigos.Any())
                     turmasCodigos = new string[1] { turma.CodigoTurma };
