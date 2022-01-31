a98e64e631f3cac3dceaa0d7b48da1b8dbee8fda
Ricardo P. Silva <ricardo.silva@amcom.com.br>
2022-01-31 17:36:02 -0300

    Correção de script

--- scripts manutencao/57489 - Remover o campo reflexões e replanejamento/V519__MOVER_REFLEXOES_REPLANEJAMENTO_TO_PLANEJAMENTO.sql
+++ scripts manutencao/57489 - Remover o campo reflexões e replanejamento/V519__MOVER_REFLEXOES_REPLANEJAMENTO_TO_PLANEJAMENTO.sql
@@ -1,95 +1,95 @@
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202008'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202009'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202010'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202011'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202012'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202101'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202102'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202103'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202104'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202105'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202106'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202107'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202108'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202109'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202110'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202111'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202112'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202201'
 and reflexoes_replanejamento <> '';
 
 update diario_bordo 
-set planejamento = planejamento || '<br/>' || reflexoes_replanejamento
+set planejamento = '<p>Planejamento</p>' || planejamento || '<br/>' || '<p>Reflexões e replanejamento</p>' || reflexoes_replanejamento
 where to_char(criado_em, 'YYYYMM') = '202202'
 and reflexoes_replanejamento <> '';
 
