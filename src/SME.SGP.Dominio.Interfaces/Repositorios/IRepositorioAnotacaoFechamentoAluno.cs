cce6eca6a40715da6c1d2ceb90d8f29a2d35908e
Rafael Losi <rafael.losi@amcom.com.br>
2022-02-22 18:13:17 -0300

    53948 Corrigido Repositorio de Consulta de Anotação

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAnotacaoFechamentoAluno.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioAnotacaoFechamentoAluno.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioAnotacaoFechamentoAluno : IRepositorioBase<AnotacaoFechamentoAluno>
     {
         Task<AnotacaoFechamentoAluno> ObterPorFechamentoEAluno(long fechamentoTurmaDisciplinaId, string alunoCodigo);
+        Task<IEnumerable<FechamentoAlunoAnotacaoConselhoDto>> ObterAnotacoesTurmaAlunoBimestreAsync(string alunoCodigo, string[] turmasCodigos, long periodoId);
     }
 }
