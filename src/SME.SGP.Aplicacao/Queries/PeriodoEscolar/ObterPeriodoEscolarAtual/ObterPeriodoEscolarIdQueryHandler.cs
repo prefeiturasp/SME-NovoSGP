e59cf8b71dc34867f6d5269bcab639da564831ef
Fernando Groeler <fernando.groeler@gmail.com>
2022-03-01 15:52:00 -0400

    Criado novo método para consultar o período escolar atual sem o código da turma.

---
+++ src/SME.SGP.Aplicacao/Queries/PeriodoEscolar/ObterPeriodoEscolarAtual/ObterPeriodoEscolarIdQueryHandler.cs
@@ -0,0 +1,29 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterPeriodoEscolarAtualQueryHandler : IRequestHandler<ObterPeriodoEscolarAtualQuery, PeriodoEscolar>
+    {
+        private readonly IRepositorioPeriodoEscolarConsulta _repositorioPeriodoEscolar;
+        private readonly IRepositorioTurmaConsulta _repositorioTurmaConsulta;
+
+        public ObterPeriodoEscolarAtualQueryHandler(IRepositorioPeriodoEscolarConsulta repositorioPeriodoEscolar, IRepositorioTurmaConsulta repositorioTurmaConsulta)
+        {
+            _repositorioPeriodoEscolar = repositorioPeriodoEscolar;
+            _repositorioTurmaConsulta = repositorioTurmaConsulta;
+        }
+        public async Task<PeriodoEscolar> Handle(ObterPeriodoEscolarAtualQuery request, CancellationToken cancellationToken)
+        {
+            var turma = await _repositorioTurmaConsulta.ObterPorId(request.TurmaId);
+
+            if (turma == null)
+                throw new NegocioException("Turma não encontrada");
+
+            return await _repositorioPeriodoEscolar.ObterPeriodoEscolarAtualAsync(turma.ModalidadeTipoCalendario, request.DataReferencia);
+        }
+    }
+}
