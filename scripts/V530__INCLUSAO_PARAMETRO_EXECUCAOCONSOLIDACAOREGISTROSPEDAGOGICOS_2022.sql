61ee6215508bbbe4e87075a168c14dd26ea1f643
Felipe Abreu <44608765+abreufelipe@users.noreply.github.com>
2022-03-11 14:42:42 -0300

    Update V530__INCLUSAO_PARAMETRO_EXECUCAOCONSOLIDACAOREGISTROSPEDAGOGICOS_2022.sql
    Ajuste na condição da query.

--- scripts/V530__INCLUSAO_PARAMETRO_EXECUCAOCONSOLIDACAOREGISTROSPEDAGOGICOS_2022.sql
+++ scripts/V530__INCLUSAO_PARAMETRO_EXECUCAOCONSOLIDACAOREGISTROSPEDAGOGICOS_2022.sql
@@ -4,8 +4,8 @@ UPDATE public.parametros_sistema set ativo = false, alterado_em = null, valor = 
 
 INSERT INTO public.parametros_sistema
     (nome,tipo,descricao,valor,ano,ativo,criado_em,criado_por,alterado_em,alterado_por,criado_rf,alterado_rf)
-SELECT 'ExecucaoConsolidacaoRegistrosPedagogicos',82,'Data da última execução da rotina de consolidação de registros pedagógicos','',2022,true,current_timestamp,'SISTEMA',null,'Sistema','0','0'
+SELECT 'ExecucaoConsolidacaoRegistrosPedagogicos',82,'Data da Ãºltima execuÃ§Ã£o da rotina de consolidaÃ§Ã£o de registros pedagÃ³gicos','',2022,true,current_timestamp,'SISTEMA',null,'Sistema','0','0'
 WHERE
     NOT EXISTS (
-        SELECT ano FROM public.parametros_sistema WHERE ano = 2022
+        SELECT ano FROM public.parametros_sistema WHERE ano = 2022 and tipo = 82
     );
