1b25b9ab2502cc588f82803fa32acffe3a1c630a
Rafael Losi <rafael.losi@amcom.com.br>
2022-03-16 10:42:24 -0300

    Correção dos testes quebrados

---
+++ src/SME.SGP.Aplicacao/Queries/Aula/ObterAulasPorIds/ObterAulasPorIdsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterAulasPorIdsQueryHandler : IRequestHandler<ObterAulasPorIdsQuery, IEnumerable<Aula>>
+    {
+        private readonly IRepositorioAulaConsulta repositorioAulaConsulta;
+
+        public ObterAulasPorIdsQueryHandler(IRepositorioAulaConsulta repositorioAulaConsulta)
+        {
+            this.repositorioAulaConsulta = repositorioAulaConsulta ?? throw new ArgumentNullException(nameof(repositorioAulaConsulta));
+        }
+
+        public Task<IEnumerable<Aula>> Handle(ObterAulasPorIdsQuery request, CancellationToken cancellationToken)
+            => repositorioAulaConsulta.ObterAulasPorIds(request.AulasIds);
+    }
+}
