df54a8f16ba54dcd7a5eeac2779e4f5f4c371afd
williamamcom <69849971+williamamcom@users.noreply.github.com>
2022-03-21 10:02:21 -0300

    Ajuste no merge de Prod

--- src/SME.SGP.Dados/Repositorios/RepositorioAbrangencia.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioAbrangencia.cs
@@ -765,7 +765,8 @@ namespace SME.SGP.Dados.Repositorios
                             from f_abrangencia_turmas_tipos(@login, @perfil, @consideraHistorico, @modalidade, @semestre, @codigoUe, @anoLetivo, @tipos, @anosInfantilDesconsiderar)
                           order by 5";
 
-            var result = (await database.Conexao.QueryAsync<AbrangenciaTurmaRetorno>(query.ToString(), new { login, perfil, consideraHistorico, modalidade, semestre = periodo, codigoUe, anoLetivo, tipos, anosInfantilDesconsiderar },queryName : "ObterTurmasPorTipos")).AsList();
+            var resultado = await database.Conexao
+                .QueryAsync<AbrangenciaTurmaRetorno>(query.ToString(), new { login, perfil, consideraHistorico, modalidade, semestre = periodo, codigoUe, anoLetivo, tipos, anosInfantilDesconsiderar });
 
             if (perfil == Perfis.PERFIL_SUPERVISOR)
             {
