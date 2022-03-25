3a5eef8e7923b062312296cf76633b21f76c7f1c
Thiago Fernando <thiago.fernando@msn.com>
2022-03-25 18:08:12 -0300

    checkip

--- src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/AcompanhamentoAprendizagem/RelatorioAcompanhamentoAprendizagemUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/AcompanhamentoAprendizagem/RelatorioAcompanhamentoAprendizagemUseCase.cs
@@ -15,6 +15,7 @@ namespace SME.SGP.Aplicacao
         public async Task<bool> Executar(FiltroRelatorioAcompanhamentoAprendizagemDto filtro)
         {
             var usuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+
             return await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.AcompanhamentoAprendizagem, filtro,
                 usuarioLogado, formato: TipoFormatoRelatorio.Html,
                 rotaRelatorio: RotasRabbitSgpRelatorios.RotaRelatoriosSolicitadosRelatorioAcompanhamentoAprendizagem));
