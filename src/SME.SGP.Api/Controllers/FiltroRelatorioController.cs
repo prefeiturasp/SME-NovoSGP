de857734ef398263961cde9f45827ee830ee6b0e
Felipe Abreu <felipe.abreu@amcom.com.br>
2022-04-15 14:00:41 -0300

    Correção dos filtros da tela de boletim. Bug 62446

--- src/SME.SGP.Api/Controllers/FiltroRelatorioController.cs
+++ src/SME.SGP.Api/Controllers/FiltroRelatorioController.cs
@@ -39,9 +39,9 @@ namespace SME.SGP.Api.Controllers
             return Ok(await obterFiltroRelatoriosModalidadesPorUeUseCase.Executar(codigoUe, anoLetivo, consideraHistorico, consideraNovasModalidades));
         }
         [HttpGet("ues/{codigoUe}/modalidades/abrangencias")]
-        public async Task<IActionResult> ObterModalidadesPorUeAbrangencia([FromServices] IObterFiltroRelatoriosModalidadesPorUeAbrangenciaUseCase obterFiltroRelatoriosModalidadesPorUeAbrangenciaUseCase, string codigoUe, bool consideraNovasModalidades = false)
+        public async Task<IActionResult> ObterModalidadesPorUeAbrangencia([FromServices] IObterFiltroRelatoriosModalidadesPorUeAbrangenciaUseCase obterFiltroRelatoriosModalidadesPorUeAbrangenciaUseCase, string codigoUe, bool consideraNovasModalidades = false, bool consideraHistorico = false)
         {
-            return Ok(await obterFiltroRelatoriosModalidadesPorUeAbrangenciaUseCase.Executar(codigoUe, consideraNovasModalidades));
+            return Ok(await obterFiltroRelatoriosModalidadesPorUeAbrangenciaUseCase.Executar(codigoUe, consideraNovasModalidades, consideraHistorico));
         }
 
         [HttpGet("ues/{codigoUe}/modalidades/{modalidade}/anos-escolares")]
