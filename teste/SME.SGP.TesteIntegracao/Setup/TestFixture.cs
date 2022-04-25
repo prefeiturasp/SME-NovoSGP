568f8258c25fc24b05af9f7816e9f76bf910240e
Thiago Fernando <thiago.fernando@msn.com>
2022-04-25 15:06:26 -0300

    Testes de Integração

--- teste/SME.SGP.TesteIntegracao/Setup/TestFixture.cs
+++ teste/SME.SGP.TesteIntegracao/Setup/TestFixture.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Data;
-using System.Text;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using SME.SGP.Dados;
 using SME.SGP.Infra;
+using System;
+using System.Data;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using SME.SGP.Dados.Contexto;
 
 namespace SME.SGP.TesteIntegracao.Setup
 {
-    public class TestFixture: IDisposable
+    public class TestFixture : IDisposable
     {
     //RaphaelDias. Removi o que estava nessa classe para passar pro CollectionFixture. 
     //Se for necessário paralelizar os testes, o ideal é que passe o controle do banco de dados
