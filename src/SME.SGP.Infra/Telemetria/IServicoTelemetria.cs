493b1a48b3bd799f7158dbfde579da29a007e9b1
Rafael Losi <rafael.losi@amcom.com.br>
2022-04-26 10:22:07 -0300

    Incluso parametros da fila rabbit no APM

--- src/SME.SGP.Infra/Telemetria/IServicoTelemetria.cs
+++ src/SME.SGP.Infra/Telemetria/IServicoTelemetria.cs
@@ -8,6 +8,6 @@ namespace SME.SGP.Infra
         Task<dynamic> RegistrarComRetornoAsync<T>(Func<Task<object>> acao, string acaoNome, string telemetriaNome, string telemetriaValor, string parametros = "");
         dynamic RegistrarComRetorno<T>(Func<object> acao, string acaoNome, string telemetriaNome, string telemetriaValor, string parametros = "");
         void Registrar(Action acao, string acaoNome, string telemetriaNome, string telemetriaValor);
-        Task RegistrarAsync(Func<Task> acao, string acaoNome, string telemetriaNome, string telemetriaValor);
+        Task RegistrarAsync(Func<Task> acao, string acaoNome, string telemetriaNome, string telemetriaValor, string parametros = "");
     }
 }
