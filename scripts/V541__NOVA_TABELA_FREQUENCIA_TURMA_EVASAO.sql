f3831592bfa2de7bf67caf0b4121afade9d862ff
Fernando Groeler <fernando.groeler@gmail.com>
2022-05-04 16:08:29 -0400

    Adicionado Indice "mes" e implementado validators.

--- scripts/V541__NOVA_TABELA_FREQUENCIA_TURMA_EVASAO.sql
+++ scripts/V541__NOVA_TABELA_FREQUENCIA_TURMA_EVASAO.sql
@@ -10,5 +10,6 @@ CREATE TABLE public.frequencia_turma_evasao (
 );
 
 CREATE INDEX frequencia_turma_evasao_turma_id_idx ON public.frequencia_turma_evasao USING btree (turma_id);
+CREATE INDEX frequencia_turma_evasao_mes_idx ON public.frequencia_turma_evasao USING btree (mes);
 
 ALTER TABLE public.frequencia_turma_evasao ADD CONSTRAINT frequencia_turma_evasao_fk FOREIGN KEY (turma_id) REFERENCES public.turma(id);
