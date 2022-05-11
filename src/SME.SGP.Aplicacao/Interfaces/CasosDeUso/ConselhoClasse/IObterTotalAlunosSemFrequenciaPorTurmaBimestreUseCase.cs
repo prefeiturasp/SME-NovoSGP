641e0902e086eb5e4fb7436e389e31450011d5a5
DanylloAntunes <danyllo.antunes@gmail.com>
2022-05-11 08:58:37 -0300

    Sistema não está exibindo a quantidade de aulas cadastradas no fechamento de conselho de classe para componentes que não lançam frequência.

---
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/ConselhoClasse/IObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public interface IObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCase
+    {
+        Task<IEnumerable<int>> Executar(string disciplinaId, string codigoTurma, int bimestre);
+    }
+}
