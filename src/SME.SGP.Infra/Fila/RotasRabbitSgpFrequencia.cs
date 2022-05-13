34b4ef43f9b4a03abdfea99b28390b16ce665b05
Rafael Losi <rafael.losi@amcom.com.br>
2022-05-13 12:25:12 -0300

    Ajuste filas de cada flag

--- src/SME.SGP.Infra/Fila/RotasRabbitSgpFrequencia.cs
+++ src/SME.SGP.Infra/Fila/RotasRabbitSgpFrequencia.cs
@@ -2,9 +2,21 @@
 {
     public class RotasRabbitSgpFrequencia
     {
+        public const string RotaValidacaoAusenciaConciliacaoFrequenciaTurma = "sgp.frequencia.turma.conciliacao.validar";
+        public const string RotaCalculoFrequenciaPorTurmaComponente = "sgp.frequencia.turma.componente";
         public const string ConsolidarFrequenciasPorTurma = "sgp.frequencia.turma.consolidar";
         public const string RotaConsolidacaoDashBoardFrequencia = "sgp.consolidacao.frequencia.dashboard";
-        public const string RotaCalculoFrequenciaPorTurmaComponente = "sgp.frequencia.turma.componente";
         public const string ConsolidarFrequenciasTurmasPorUe = "sgp.frequencia.turma.ue.consolidar";
+        public const string ConsolidacaoFrequenciasTurmasCarregar = "sgp.frequencia.turma.carregar";
+        public const string ConsolidarFrequenciasTurmasNoAno = "sgp.frequencia.turma.ano.consolidar";
+        public const string ConsolidarFrequenciasTurmasPorDre = "sgp.frequencia.turma.dre.consolidar";
+        public const string RotaConciliacaoFrequenciaTurmasSync = "sgp.frequencia.turma.conciliacao.sync";
+        public const string RotaConciliacaoFrequenciaTurmaPorPeriodo = "sgp.frequencia.turma.conciliacao.periodo";
+        public const string RotaConciliacaoCalculoFrequenciaPorTurmaComponente = "sgp.frequencia.turma.conciliacao.componente";
+
+        public const string RotaConciliacaoFrequenciaTurmasAlunosSync = "sgp.frequencia.turma.alunos.conciliacao.sync";
+        public const string RotaConciliacaoFrequenciaTurmasAlunosBuscar = "sgp.frequencia.turma.alunos.buscar.sync";
+        public const string RotaCalcularFrequenciaGeralSync = "sgp.frequencia.calcular.geral.sync";
+
     }
 }
