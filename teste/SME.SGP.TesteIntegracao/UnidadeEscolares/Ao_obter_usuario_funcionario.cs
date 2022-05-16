3df11e1036e00658573c40ad68bdb3ef212f0a94
DanylloAntunes <danyllo.antunes@gmail.com>
2022-05-16 17:31:46 -0300

    Criar Endpoint Pesquisa de Funcionarios

---
+++ teste/SME.SGP.TesteIntegracao/UnidadeEscolares/Ao_obter_usuario_funcionario.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao
+{
+    public class Ao_obter_usuario_funcionario : TesteBase
+    {
+        private readonly ItensBasicosBuilder _builder;
+        public Ao_obter_usuario_funcionario(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+            _builder = new ItensBasicosBuilder(this);
+        }
+
+        [Fact]
+        public async Task Deve_retornar_usuario_por_login()
+        {
+            await _builder.CriaItensComunsEja();
+
+            var useCase = ServiceProvider.GetService<IObterUsuarioFuncionarioUseCase>();
+
+            var dto = new FiltroFuncionarioDto()
+            {
+                CodigoRF = "6926886"
+            };
+
+            var retorno = await useCase.Executar(dto);
+
+            retorno.ShouldNotBeEmpty();
+            retorno.First().CodigoRf.ShouldBe("6926886");
+        }
+    }
+}
