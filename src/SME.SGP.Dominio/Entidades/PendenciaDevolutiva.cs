c8e33cc16d6f1786809fa90471ef43852c38aa69
Thiago Fernando <thiago.fernando@msn.com>
2022-05-17 11:57:40 -0300

    Criado Entidade e Mapeamento

---
+++ src/SME.SGP.Dominio/Entidades/PendenciaDevolutiva.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio
+{
+    public class PendenciaDevolutiva : EntidadeBase
+    {
+        public long PedenciaId { get; set; }
+        public long ComponenteCurricularId { get; set; }
+        public long TurmaId { get; set; }
+    }
+}
