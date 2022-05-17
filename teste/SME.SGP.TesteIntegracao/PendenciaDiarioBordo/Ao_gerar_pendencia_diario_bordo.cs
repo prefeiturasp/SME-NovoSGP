722aaf52ccc94f819a510182ab2b3f2d97b8e970
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-05-17 09:16:53 -0300

    Adequação dos testes de integração

--- teste/SME.SGP.TesteIntegracao/PendenciaDiarioBordo/Ao_gerar_pendencia_diario_bordo.cs
+++ teste/SME.SGP.TesteIntegracao/PendenciaDiarioBordo/Ao_gerar_pendencia_diario_bordo.cs
@@ -15,6 +15,21 @@ namespace SME.SGP.TesteIntegracao
 {
     public class Ao_gerar_pendencia_diario_bordo : TesteBase
     {
+        private const string Professor_512_Rf_1001 = "1001";
+        private const string Professor_512_Rf_1002 = "1002";
+        private const string Professor_512_Rf_1003 = "1003";
+        private const string Professor_513_Rf_1004 = "1004";
+        private const string Professor_534_Rf_1005 = "1005";
+        private const string Professor_534_Rf_1006 = "1006";
+
+        private const int Regencia_Infantil_Emei_Manha_534 = 534;
+        private const int Regencia_Infantil_Emei_4h_512 = 512;
+        private const int Regencia_Infantil_Emei_2h_513 = 513;
+
+        private const string Turma_Emei_7P = "7P";
+        private const string Turma_Emei_7A = "7A";
+        private const string Turma_Emei_7B = "7B";
+
         public Ao_gerar_pendencia_diario_bordo(CollectionFixture collectionFixture) : base(collectionFixture)
         {
             
@@ -37,17 +52,17 @@ namespace SME.SGP.TesteIntegracao
 
                 pendenciaRetorno.ShouldBeGreaterThan(0);
 
-                if (item.CodigoRf.Equals("1001"))
+                if (item.CodigoRf.Equals(Professor_512_Rf_1001))
                     pendenciaRetorno.ShouldBe(1);
-                else if (item.CodigoRf.Equals("1002"))
+                else if (item.CodigoRf.Equals(Professor_512_Rf_1002))
                     pendenciaRetorno.ShouldBe(2);
-                else if (item.CodigoRf.Equals("1003"))
+                else if (item.CodigoRf.Equals(Professor_512_Rf_1003))
                     pendenciaRetorno.ShouldBe(3);
-                else if (item.CodigoRf.Equals("1004"))
+                else if (item.CodigoRf.Equals(Professor_513_Rf_1004))
                     pendenciaRetorno.ShouldBe(4);
-                else if (item.CodigoRf.Equals("1005"))
+                else if (item.CodigoRf.Equals(Professor_534_Rf_1005))
                     pendenciaRetorno.ShouldBe(5);
-                else if (item.CodigoRf.Equals("1006"))
+                else if (item.CodigoRf.Equals(Professor_534_Rf_1006))
                     pendenciaRetorno.ShouldBe(6);
             }
         }
@@ -84,14 +99,50 @@ namespace SME.SGP.TesteIntegracao
                 AulaCJ = false,
                 DataAula = new DateTime(2022, 05, 09),
                 ProfessorRf = "Sistema",
-                DisciplinaId = "512",
+                DisciplinaId = Regencia_Infantil_Emei_4h_512.ToString(),
                 Excluido = false,
                 TipoCalendarioId = 1,
                 RecorrenciaAula = RecorrenciaAula.AulaUnica,
                 Quantidade = 1,
                 TipoAula = TipoAula.Normal,
                 UeId = "1",
-                TurmaId = "1234",
+                TurmaId = Turma_Emei_7P,
+                CriadoPor = "",
+                CriadoRF = "",
+                CriadoEm = new DateTime(2022, 05, 09),
+            });
+
+            await InserirNaBase(new Aula()
+            {
+                AulaCJ = false,
+                DataAula = new DateTime(2022, 05, 09),
+                ProfessorRf = "Sistema",
+                DisciplinaId = Regencia_Infantil_Emei_2h_513.ToString(),
+                Excluido = false,
+                TipoCalendarioId = 1,
+                RecorrenciaAula = RecorrenciaAula.AulaUnica,
+                Quantidade = 1,
+                TipoAula = TipoAula.Normal,
+                UeId = "1",
+                TurmaId = Turma_Emei_7A,
+                CriadoPor = "",
+                CriadoRF = "",
+                CriadoEm = new DateTime(2022, 05, 09),
+            });
+
+            await InserirNaBase(new Aula()
+            {
+                AulaCJ = false,
+                DataAula = new DateTime(2022, 05, 09),
+                ProfessorRf = "Sistema",
+                DisciplinaId = Regencia_Infantil_Emei_Manha_534.ToString(),
+                Excluido = false,
+                TipoCalendarioId = 1,
+                RecorrenciaAula = RecorrenciaAula.AulaUnica,
+                Quantidade = 1,
+                TipoAula = TipoAula.Normal,
+                UeId = "1",
+                TurmaId = Turma_Emei_7B,
                 CriadoPor = "",
                 CriadoRF = "",
                 CriadoEm = new DateTime(2022, 05, 09),
@@ -99,9 +150,9 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Usuario()
             {
-                CodigoRf = "1001",
-                Login = "1001",
-                Nome = "Usuario 1001",
+                CodigoRf = Professor_512_Rf_1001,
+                Login = Professor_512_Rf_1001,
+                Nome = Professor_512_Rf_1001,
                 PerfilAtual = Guid.Parse(PerfilUsuario.PROFESSOR.Name()),
                 CriadoPor = "",
                 CriadoRF = "",
@@ -110,9 +161,9 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Usuario()
             {
-                CodigoRf = "1002",
-                Login = "1002",
-                Nome = "Usuario 1002",
+                CodigoRf = Professor_512_Rf_1002,
+                Login = Professor_512_Rf_1002,
+                Nome = Professor_512_Rf_1002,
                 PerfilAtual = Guid.Parse(PerfilUsuario.PROFESSOR.Name()),
                 CriadoPor = "",
                 CriadoRF = "",
@@ -121,9 +172,9 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Usuario()
             {
-                CodigoRf = "1003",
-                Login = "1003",
-                Nome = "Usuario 1003",
+                CodigoRf = Professor_512_Rf_1003,
+                Login = Professor_512_Rf_1003,
+                Nome = Professor_512_Rf_1003,
                 PerfilAtual = Guid.Parse(PerfilUsuario.PROFESSOR.Name()),
                 CriadoPor = "",
                 CriadoRF = "",
@@ -132,9 +183,9 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Usuario()
             {
-                CodigoRf = "1004",
-                Login = "1004",
-                Nome = "Usuario 1004",
+                CodigoRf = Professor_513_Rf_1004,
+                Login = Professor_513_Rf_1004,
+                Nome = Professor_513_Rf_1004,
                 PerfilAtual = Guid.Parse(PerfilUsuario.PROFESSOR.Name()),
                 CriadoPor = "",
                 CriadoRF = "",
@@ -143,9 +194,9 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Usuario()
             {
-                CodigoRf = "1005",
-                Login = "1005",
-                Nome = "Usuario 1005",
+                CodigoRf = Professor_534_Rf_1005,
+                Login = Professor_534_Rf_1005,
+                Nome = Professor_534_Rf_1005,
                 PerfilAtual = Guid.Parse(PerfilUsuario.PROFESSOR.Name()),
                 CriadoPor = "",
                 CriadoRF = "",
@@ -154,9 +205,9 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Usuario()
             {
-                CodigoRf = "1006",
-                Login = "1006",
-                Nome = "Usuario 1006",
+                CodigoRf = Professor_534_Rf_1006,
+                Login = Professor_534_Rf_1006,
+                Nome = Professor_534_Rf_1006,
                 PerfilAtual = Guid.Parse(PerfilUsuario.PROFESSOR.Name()),
                 CriadoPor = "",
                 CriadoRF = "",
@@ -192,9 +243,30 @@ namespace SME.SGP.TesteIntegracao
 
             await InserirNaBase(new Turma()
             {
-                Id = 1,
-                Nome = "1A",
-                CodigoTurma = "1234",
+                Nome = "7P",
+                CodigoTurma = "EI - 7P",
+                Ano = "1",
+                AnoLetivo = 2022,
+                TipoTurma = Dominio.Enumerados.TipoTurma.Regular,
+                ModalidadeCodigo = Modalidade.Fundamental,
+                UeId = 1
+            });
+
+            await InserirNaBase(new Turma()
+            {
+                Nome = "7A",
+                CodigoTurma = "EI - 7A",
+                Ano = "1",
+                AnoLetivo = 2022,
+                TipoTurma = Dominio.Enumerados.TipoTurma.Regular,
+                ModalidadeCodigo = Modalidade.Fundamental,
+                UeId = 1
+            });
+
+            await InserirNaBase(new Turma()
+            {
+                Nome = "7B",
+                CodigoTurma = "EI - 7B",
                 Ano = "1",
                 AnoLetivo = 2022,
                 TipoTurma = Dominio.Enumerados.TipoTurma.Regular,
@@ -211,16 +283,16 @@ namespace SME.SGP.TesteIntegracao
                 {
                     ProfessorComponente = new ProfessorEComponenteInfantilDto()
                     {
-                        CodigoRf = "1001",
+                        CodigoRf = Professor_512_Rf_1001,
                         DescricaoComponenteCurricular = "REGÊNCIA INFANTIL EMEI 4H",
-                        DisciplinaId = 512
+                        DisciplinaId = Regencia_Infantil_Emei_4h_512
                     },
                     Aula = new AulaComComponenteDto()
                     {
                         Id = 1,
                         PeriodoEscolarId = 1
                     },
-                    CodigoTurma = "1234",
+                    CodigoTurma = Turma_Emei_7P,
                     TurmaComModalidade = "EI-7P",
                     NomeEscola = "CEU EMEI PARAISOPOLIS (DRE  CL)"
                 },
@@ -228,29 +300,29 @@ namespace SME.SGP.TesteIntegracao
                 {
                     ProfessorComponente = new ProfessorEComponenteInfantilDto()
                     {
-                        CodigoRf = "1002",
+                        CodigoRf = Professor_512_Rf_1002,
                         DescricaoComponenteCurricular = "REGÊNCIA INFANTIL EMEI 4H",
-                        DisciplinaId = 512
+                        DisciplinaId = Regencia_Infantil_Emei_4h_512
                     },
                     Aula = new AulaComComponenteDto()
                     {
                         Id = 1,
                         PeriodoEscolarId = 1,
                     },
-                    CodigoTurma = "1234",
+                    CodigoTurma = Turma_Emei_7P,
                     TurmaComModalidade = "EI-7P",
                     NomeEscola = "CEU EMEI PARAISOPOLIS (DRE  CL)"
                 },
                 new SalvarPendenciaDiarioBordoCommand()
                 {
-                    CodigoTurma = "1234",
+                    CodigoTurma = Turma_Emei_7P,
                     TurmaComModalidade = "EI-7P",
                     NomeEscola = "CEU EMEI PARAISOPOLIS (DRE  CL)",
                     ProfessorComponente = new ProfessorEComponenteInfantilDto()
                     {
-                        CodigoRf = "1003",
+                        CodigoRf = Professor_512_Rf_1003,
                         DescricaoComponenteCurricular = "REGÊNCIA INFANTIL EMEI 4H",
-                        DisciplinaId = 512
+                        DisciplinaId = Regencia_Infantil_Emei_4h_512
                     },
                     Aula = new AulaComComponenteDto()
                     {
@@ -260,14 +332,14 @@ namespace SME.SGP.TesteIntegracao
                 },
                 new SalvarPendenciaDiarioBordoCommand()
                 {
-                    CodigoTurma = "1234",
+                    CodigoTurma = Turma_Emei_7A,
                     TurmaComModalidade = "EI-7A",
                     NomeEscola = "CEU EMEI PARAISOPOLIS (DRE  CL)",
                     ProfessorComponente = new ProfessorEComponenteInfantilDto()
                     {
-                        CodigoRf = "1004",
+                        CodigoRf = Professor_513_Rf_1004,
                         DescricaoComponenteCurricular = "REGÊNCIA INFANTIL EMEI 2H",
-                        DisciplinaId = 513
+                        DisciplinaId = Regencia_Infantil_Emei_2h_513
                     },
                     Aula = new AulaComComponenteDto()
                     {
@@ -277,14 +349,14 @@ namespace SME.SGP.TesteIntegracao
                 },
                 new SalvarPendenciaDiarioBordoCommand()
                 {
-                    CodigoTurma = "1234",
+                    CodigoTurma = Turma_Emei_7B,
                     TurmaComModalidade = "EI-7B",
                     NomeEscola = "EMEI SAO PAULO",
                     ProfessorComponente = new ProfessorEComponenteInfantilDto()
                     {
-                        CodigoRf = "1005",
+                        CodigoRf = Professor_534_Rf_1005,
                         DescricaoComponenteCurricular = "REGÊNCIA INFANTIL EMEI MANHÃ",
-                        DisciplinaId = 534
+                        DisciplinaId = Regencia_Infantil_Emei_Manha_534
                     },
                     Aula = new AulaComComponenteDto()
                     {
@@ -294,14 +366,14 @@ namespace SME.SGP.TesteIntegracao
                 },
                 new SalvarPendenciaDiarioBordoCommand()
                 {
-                    CodigoTurma = "1234",
+                    CodigoTurma = Turma_Emei_7B,
                     TurmaComModalidade = "EI-7B",
                     NomeEscola = "EMEI SAO PAULO",
                     ProfessorComponente = new ProfessorEComponenteInfantilDto()
                     {
-                        CodigoRf = "1006",
+                        CodigoRf = Professor_534_Rf_1006,
                         DescricaoComponenteCurricular = "REGÊNCIA INFANTIL EMEI MANHÃ",
-                        DisciplinaId = 534
+                        DisciplinaId = Regencia_Infantil_Emei_Manha_534
                     },
                     Aula = new AulaComComponenteDto()
                     {
