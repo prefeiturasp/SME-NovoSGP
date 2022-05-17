13082617d413d6a3ba6d454ce2cbe99531f100ea
Thiago Fernando <thiago.fernando@msn.com>
2022-05-17 14:44:01 -0300

    Ajuste PendenciaDevolutiva

--- src/SME.SGP.Dominio/Entidades/PendenciaDevolutiva.cs
+++ src/SME.SGP.Dominio/Entidades/PendenciaDevolutiva.cs
@@ -3,7 +3,10 @@
     public class PendenciaDevolutiva : EntidadeBase
     {
         public long PedenciaId { get; set; }
+        public Pendencia Pendencia { get; set; }
         public long ComponenteCurricularId { get; set; }
+        public ComponenteCurricular ComponenteCurricular { get; set; }
         public long TurmaId { get; set; }
+        public Turma Turma { get; set; }
     }
 }
