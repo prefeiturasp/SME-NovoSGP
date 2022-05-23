fa34bdee80207ce7beb76a2bec41df750d91f4ac
Thiago Fernando <thiago.fernando@msn.com>
2022-05-23 13:46:07 -0300

    Criação da Query para identificar pendências

---
+++ src/SME.SGP.Aplicacao/Queries/Devolutiva/ExistePendenciaDiarioBordo/ExistePendenciaDiarioBordoQuery.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExistePendenciaDiarioBordoQuery : IRequest<bool>
+    {
+        public ExistePendenciaDiarioBordoQuery(string turmaCodigo, string componenteCodigo)
+        {
+            TurmaCodigo = turmaCodigo;
+            ComponenteCodigo = componenteCodigo;
+        }
+
+        public string TurmaCodigo { get; set;}
+        public string ComponenteCodigo { get; set; }
+    }
+    public class ExistePendenciaDiarioBordoQueryValidator : AbstractValidator<ExistePendenciaDiarioBordoQuery>
+    {
+        public ExistePendenciaDiarioBordoQueryValidator()
+        {
+            RuleFor(a => a.TurmaCodigo)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Turma Código deve ser informado");
+
+            RuleFor(a => a.ComponenteCodigo)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Componente Código deve ser informado");
+        }
+    }
+}
