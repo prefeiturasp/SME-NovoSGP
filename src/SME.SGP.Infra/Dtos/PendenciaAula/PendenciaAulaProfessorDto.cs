358f32ffb345a745e42852d2d1ba91954f7cd311
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-05-27 18:45:24 -0300

    Evitando duplicações e tratamento de CJ

---
+++ src/SME.SGP.Infra/Dtos/PendenciaAula/PendenciaAulaProfessorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Infra
+{
+    public class PendenciaAulaProfessorDto
+    {
+        public long PendenciaId { get; set; }
+        public long AulaId { get; set; }
+        public string CodigoRfProfessor { get; set; }
+    }
+}
