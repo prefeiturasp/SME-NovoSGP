d485879a1ce8f017cac2bcc08acac16748f25766
Thiago Fernando <thiago.fernando@msn.com>
2022-05-27 11:23:51 -0300

    Criado aquivo de testes

---
+++ teste/SME.SGP.TesteIntegracao/PendenciaDevolutiva/Ao_gerar_pendencia_devolutiva.cs
@@ -0,0 +1,30 @@
+﻿using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.PendenciaDevolutiva
+{
+    public class Ao_gerar_pendencia_devolutiva : TesteBase
+    {
+        public Ao_gerar_pendencia_devolutiva(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+
+        }
+
+        [Fact]
+        public async Task Nao_Deve_Gerar_Pendencia_ComParamentro_GerarPendenciaDevolutivaSemDiarioBordo_Desativado()
+        {
+
+        }
+
+        [Fact]
+        public async Task Nao_Deve_Gerar_Pendencia_ComParamentro_DataInicioGeracaoPendencias_Desativado()
+        {
+
+        }
+    }
+}
