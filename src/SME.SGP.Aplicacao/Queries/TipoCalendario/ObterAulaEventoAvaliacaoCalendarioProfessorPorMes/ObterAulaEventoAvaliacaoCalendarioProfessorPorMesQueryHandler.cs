3bc0d4bc62a22ffd8b31cd08a87df0710998f51f
Gabriel Nascimento <gabriel.nascimento@amcom.com.br>
2022-05-31 16:12:45 -0300

    BUG 655573 Ajuste para trazer os fechamentos de bimestres

--- src/SME.SGP.Aplicacao/Queries/TipoCalendario/ObterAulaEventoAvaliacaoCalendarioProfessorPorMes/ObterAulaEventoAvaliacaoCalendarioProfessorPorMesQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/TipoCalendario/ObterAulaEventoAvaliacaoCalendarioProfessorPorMes/ObterAulaEventoAvaliacaoCalendarioProfessorPorMesQueryHandler.cs
@@ -28,11 +28,9 @@ namespace SME.SGP.Aplicacao
 
             var listaRetorno = new List<EventoAulaDiaDto>();
             var turma = await mediator.Send(new ObterTurmaPorCodigoQuery(request.TurmaCodigo));
-            var dataAula = request.Aulas.FirstOrDefault().DataAula;
             var tipoCalendarioId = await mediator.Send(new ObterTipoCalendarioIdPorTurmaQuery(turma));
-            var bimestre = await mediator.Send(new ObterBimestreAtualPorTurmaIdQuery(turma,dataAula));
-            var periodoFechamento = await mediator.Send(new ObterPeriodoFechamentoPorCalendarioIdEBimestreQuery(tipoCalendarioId, turma.EhTurmaInfantil, bimestre));
-            
+            var periodoFechamento = await mediator.Send(new ObterPeriodoFechamentoPorCalendarioIdQuery(tipoCalendarioId));
+
             var usuarioLogado = await servicoUsuario.ObterUsuarioLogado();
             var componentesCurriculares = await servicoEol.ObterComponentesCurricularesPorCodigoTurmaLoginEPerfil(turma.CodigoTurma, usuarioLogado.Login, usuarioLogado.PerfilAtual);
 
@@ -41,16 +39,26 @@ namespace SME.SGP.Aplicacao
             for (int i = 1; i < qntDiasMes + 1; i++)
             {
                 var eventoAula = new EventoAulaDiaDto() { Dia = i };
-                
-                var dataMes = Convert.ToDateTime($"{i}/{request.Mes}/{request.AnoLetivo}");
 
-                if (periodoFechamento == null) { 
-                    if (request.EventosDaUeSME.Any(a => i >= a.DataInicio.Day && i <= a.DataFim.Day))
+                foreach (var item in periodoFechamento.FechamentosBimestres)
+                {
+                    var dataMes = Convert.ToDateTime($"{i}/{request.Mes}/{request.AnoLetivo}");
+
+                    if (periodoFechamento == null)
+                    {
+                        if (request.EventosDaUeSME.Any(a => i >= a.DataInicio.Day && i <= a.DataFim.Day))
+                        {
+                            eventoAula.TemEvento = true;
+                            break;
+                        }
+                    }
+                    else if (dataMes >= item.InicioDoFechamento && dataMes <= item.FinalDoFechamento)
+                    { 
                         eventoAula.TemEvento = true;
+                        break;
+                    }
                 }
-                else if (dataMes >= periodoFechamento.InicioDoFechamento && dataMes <= periodoFechamento.FinalDoFechamento)
-                        eventoAula.TemEvento = true;
-                
+
                 var aulasDoDia = request.Aulas.Where(a => a.DataAula.Day == i);
                 if (aulasDoDia.Any())
                 {
