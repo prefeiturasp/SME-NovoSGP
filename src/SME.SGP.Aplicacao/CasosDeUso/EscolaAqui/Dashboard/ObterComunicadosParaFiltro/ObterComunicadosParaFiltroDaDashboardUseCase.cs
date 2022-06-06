9860a0688cd1d302be2e30696119f6e7e20e0aab
williamamcom <69849971+williamamcom@users.noreply.github.com>
2022-06-06 20:08:04 -0300

    ajuste na query de obter comunicados

--- src/SME.SGP.Aplicacao/CasosDeUso/EscolaAqui/Dashboard/ObterComunicadosParaFiltro/ObterComunicadosParaFiltroDaDashboardUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/EscolaAqui/Dashboard/ObterComunicadosParaFiltro/ObterComunicadosParaFiltroDaDashboardUseCase.cs
@@ -1,34 +1,26 @@
 ﻿using MediatR;
-using SME.SGP.Aplicacao.Queries.EscolaAqui.ObterComunicadosParaFiltro;
 using SME.SGP.Infra.Dtos.EscolaAqui.ComunicadosFiltro;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
-    public class ObterComunicadosParaFiltroDaDashboardUseCase : IObterComunicadosParaFiltroDaDashboardUseCase
+    public class ObterComunicadosParaFiltroDaDashboardUseCase : AbstractUseCase, IObterComunicadosParaFiltroDaDashboardUseCase
     {
-        private readonly IMediator mediator;
-
-        public ObterComunicadosParaFiltroDaDashboardUseCase(IMediator mediator)
+        public ObterComunicadosParaFiltroDaDashboardUseCase(IMediator mediator) : base(mediator)
         {
-            this.mediator = mediator;
         }
 
         public async Task<IEnumerable<ComunicadoParaFiltroDaDashboardDto>> Executar(ObterComunicadosParaFiltroDaDashboardDto obterComunicadosFiltroDto)
-        {
-            var query = new ObterComunicadosParaFiltroDaDashboardQuery(
-                obterComunicadosFiltroDto.AnoLetivo,
-                obterComunicadosFiltroDto.CodigoDre,
-                obterComunicadosFiltroDto.CodigoUe,                
-                obterComunicadosFiltroDto.Modalidades,
-                obterComunicadosFiltroDto.Semestre,
-                obterComunicadosFiltroDto.AnoEscolar,
-                obterComunicadosFiltroDto.CodigoTurma,
-                obterComunicadosFiltroDto.DataEnvioInicial,
-                obterComunicadosFiltroDto.DataEnvioFinal,
-                obterComunicadosFiltroDto.Descricao);
-            return await mediator.Send(query);
-        }
+            => await mediator.Send(new ObterComunicadosParaFiltroDaDashboardQuery(obterComunicadosFiltroDto.AnoLetivo,
+                                                                                  obterComunicadosFiltroDto.CodigoDre,
+                                                                                  obterComunicadosFiltroDto.CodigoUe,
+                                                                                  obterComunicadosFiltroDto.Modalidades,
+                                                                                  obterComunicadosFiltroDto.Semestre,
+                                                                                  obterComunicadosFiltroDto.AnoEscolar,
+                                                                                  obterComunicadosFiltroDto.CodigoTurma,
+                                                                                  obterComunicadosFiltroDto.DataEnvioInicial,
+                                                                                  obterComunicadosFiltroDto.DataEnvioFinal,
+                                                                                  obterComunicadosFiltroDto.Descricao));
     }
 }
