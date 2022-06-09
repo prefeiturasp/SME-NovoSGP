11d2e2e4909cf81adadbcbefea9e92ecfe9b27d6
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-06-09 10:58:04 -0300

    Criando query de pesquisa de responsável atribuído a ue e criação de índices tabela supervisor_escola_dre

---
+++ src/SME.SGP.Aplicacao/Queries/Responsaveis/ExisteResponsavelAtribuidoUePorUeTipo/ExisteResponsavelAtribuidoUePorUeTipoQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExisteResponsavelAtribuidoUePorUeTipoQueryHandler : IRequestHandler<ExisteResponsavelAtribuidoUePorUeTipoQuery, bool>
+    {
+        private readonly IRepositorioSupervisorEscolaDre repositorioSupervisorEscolaDre;
+
+        public ExisteResponsavelAtribuidoUePorUeTipoQueryHandler(IRepositorioSupervisorEscolaDre repositorioSupervisorEscolaDre)
+        {
+            this.repositorioSupervisorEscolaDre = repositorioSupervisorEscolaDre ?? throw new ArgumentNullException(nameof(repositorioSupervisorEscolaDre));
+        }
+
+        public async Task<bool> Handle(ExisteResponsavelAtribuidoUePorUeTipoQuery request, CancellationToken cancellationToken)
+            => await repositorioSupervisorEscolaDre.ExisteResponsavelAtribuidoUePorUeTipo(request.CodigoUe, request.TipoResponsavelAtribuicao);
+    }
+}
