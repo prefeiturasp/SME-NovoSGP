2837dc22bba376c124003e0a094dd8f5854365e5
Danilo Ferreira <shymmi@hotmail.com>
2022-06-21 15:16:05 -0300

    aula recorrente

---
+++ teste/SME.SGP.TesteIntegracao/Aula/Ao_alterar_aula_com_recorrencia.cs
@@ -0,0 +1,26 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.Aula
+{
+    public class Ao_alterar_aula_com_recorrencia : AulaMockComponentePortugues
+    {
+        public Ao_alterar_aula_com_recorrencia(CollectionFixture collectionFixture) : base(collectionFixture) { }
+
+
+
+        [Fact]
+        public async Task Altera_aula_com_recorrencia()
+        {
+            await CriarDadosBasicosAula(ObterPerfilProfessor(), Modalidade.Fundamental, ModalidadeTipoCalendario.FundamentalMedio);
+            await CriarAula(COMPONENTE_CURRICULAR_PORTUGUES_ID_138.ToString(), new System.DateTime(2022, 02, 10), RecorrenciaAula.RepetirBimestreAtual);
+            await CriaAulaRecorrentePortugues(RecorrenciaAula.RepetirBimestreAtual);
+        }
+    }
+}
