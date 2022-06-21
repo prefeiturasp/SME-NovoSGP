8ed332f5354c5c2eebbce049480d32ad3b61ff8d
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-06-21 09:48:19 -0300

    Mock de portugues

---
+++ teste/SME.SGP.TesteIntegracao/Aula/Ao_registrar_aula_normal_repetir_bimestre_atual_excecoes_basicas.cs
@@ -0,0 +1,70 @@
+﻿using Shouldly;
+using SME.SGP.Dominio;
+using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.TestarAulaBimestreAtual
+{
+    public class Ao_registrar_aula_normal_repetir_bimestre_atual_excecoes_basicas : AulaTeste
+    {
+        private const long COMPONENTE_CURRICULAR_PORTUGUES_ID_138 = 138;
+        private const long COMPONENTE_CURRICULAR_DESCONHECIDO_ID_999999 = 999999;
+
+        public Ao_registrar_aula_normal_repetir_bimestre_atual_excecoes_basicas(CollectionFixture collectionFixture) : base(collectionFixture)
+        { }
+
+        [Fact]
+        public async Task Ao_registrar_aula_normal_repetir_no_bimestre_atual_como_professor_cj_com_atribuicao_cj_e_com_atribuicao_espontanea_invalida_modalidade_fundamental()
+        {
+            await CriarDadosBasicosAula(ObterPerfilCJ(), Modalidade.Fundamental, ModalidadeTipoCalendario.FundamentalMedio);
+
+            await CriarAtribuicaoCJ(Modalidade.Fundamental, COMPONENTE_CURRICULAR_PORTUGUES_ID_138);
+
+            await CriarAtribuicaoEsporadica(new DateTime(2022, 2, 15), new DateTime(2022, 2, 15));
+
+            var excecao = await InserirAulaUseCaseSemValidacaoBasica(TipoAula.Normal, RecorrenciaAula.RepetirBimestreAtual, COMPONENTE_CURRICULAR_PORTUGUES_ID_138, new DateTime(2022, 02, 10));
+
+            excecao.ExistemErros.ShouldBeTrue();
+
+            excecao.Mensagens.FirstOrDefault().ShouldNotBeNullOrEmpty();
+
+            excecao.Mensagens.FirstOrDefault().ShouldBeEquivalentTo("Ocorreu um erro ao solicitar a criação de aulas recorrentes, por favor tente novamente. Detalhes: Você não pode criar aulas para essa Turma.");
+        }
+
+        [Fact]
+        public async Task Ao_registrar_aula_normal_repetir_no_bimestre_atual_professor_cj_com_atribuicao_cj_e_com_atribuicao_espontanea_valida_componentes_invalidos_modalidade_fundamental()
+        {
+            await CriarDadosBasicosAula(ObterPerfilCJ(), Modalidade.Fundamental, ModalidadeTipoCalendario.FundamentalMedio);
+
+            await CriarAtribuicaoCJ(Modalidade.Fundamental, COMPONENTE_CURRICULAR_PORTUGUES_ID_138, false);
+
+            await CriarAtribuicaoEsporadica(new DateTime(2022, 2, 10), new DateTime(2022, 2, 10));
+
+            var excecao = await InserirAulaUseCaseSemValidacaoBasica(TipoAula.Normal, RecorrenciaAula.RepetirBimestreAtual, COMPONENTE_CURRICULAR_PORTUGUES_ID_138, new DateTime(2022, 02, 10));
+
+            excecao.ExistemErros.ShouldBeTrue();
+
+            excecao.Mensagens.FirstOrDefault().ShouldNotBeNullOrEmpty();
+
+            excecao.Mensagens.FirstOrDefault().ShouldBeEquivalentTo("Ocorreu um erro ao solicitar a criação de aulas recorrentes, por favor tente novamente. Detalhes: Você não pode criar aulas para essa Turma.");
+        }
+
+        [Fact]
+        public async Task Ao_registrar_aula_normal_repetir_no_bimestre_atual_professor_nao_pode_cadastrar_aula_modalidade_fundamental()
+        {
+            await CriarDadosBasicosAula(ObterPerfilProfessor(), Modalidade.Fundamental, ModalidadeTipoCalendario.FundamentalMedio);
+
+            var excecao = await InserirAulaUseCaseSemValidacaoBasica(TipoAula.Normal, RecorrenciaAula.RepetirBimestreAtual, COMPONENTE_CURRICULAR_DESCONHECIDO_ID_999999, new DateTime(2022, 02, 10));
+
+            excecao.ExistemErros.ShouldBeTrue();
+
+            excecao.Mensagens.FirstOrDefault().ShouldNotBeNullOrEmpty();
+
+            excecao.Mensagens.FirstOrDefault().ShouldBeEquivalentTo("Ocorreu um erro ao solicitar a criação de aulas recorrentes, por favor tente novamente. Detalhes: Você não pode criar aulas para essa Turma.");
+        }
+            
+    }
+}
