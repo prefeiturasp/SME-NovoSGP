dcfb798a5db3fcee8e1f13e9bf8bfdfda1b992b0
DanylloAntunes <danyllo.antunes@gmail.com>
2022-06-22 10:18:27 -0300

    Teste avaliação

---
+++ teste/SME.SGP.TesteIntegracao/Avaliacao/Ao_registrar_avaliacao_para_professor_especialista.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.Avaliacao
+{
+    public class Ao_registrar_avaliacao_para_professor_especialista : TesteAvaliacao
+    {
+        private DateTime dataInicio = new DateTime(2022, 05, 02);
+        private DateTime dataFim = new DateTime(2022, 07, 08);
+
+        public Ao_registrar_avaliacao_para_professor_especialista(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        [Fact]
+        public async Task Registrar_avaliacao_para_professor_especialista()
+        {
+            await CriarItensComuns(true, dataInicio, dataFim, BIMESTRE_2);
+
+            var comando = ServiceProvider.GetService<IComandosAtividadeAvaliativa>();
+            var dto = new AtividadeAvaliativaDto()
+            {
+                UeId = "1",
+                DreId = "1",
+                TurmaId = "1",
+                DisciplinasId = new string[] { COMPONENTE_CURRICULAR_PORTUGUES_ID_138.ToString() },
+                Descricao = "",
+                Nome = "Prova",
+                CategoriaId = CategoriaAtividadeAvaliativa.Normal,
+                DataAvaliacao = new DateTime(),
+                TipoAvaliacaoId = 1
+            };
+
+            await comando.Inserir(dto);
+
+            var atividadeAvaliativas = ObterTodos<AtividadeAvaliativa>();
+
+            atividadeAvaliativas.ShouldNotBeEmpty();
+            atividadeAvaliativas.Count().ShouldBeGreaterThanOrEqualTo(1);
+        }
+    }
+}
---
+++ teste/SME.SGP.TesteIntegracao/Avaliacao/TesteAvaliacao.cs
@@ -0,0 +1,16 @@
+﻿using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.Avaliacao
+{
+    public abstract class TesteAvaliacao : TesteBaseComuns
+    {
+        protected TesteAvaliacao(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+    }
+}
