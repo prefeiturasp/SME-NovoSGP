20666bf0544e64af3ae6f3bb9a62b309c0febe75
Danilo Ferreira <shymmi@hotmail.com>
2022-06-24 16:53:23 -0300

    implementação avaliação

---
+++ teste/SME.SGP.TesteIntegracao/Avaliacao/Ao_alterar_avaliacao_pela_gestao.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.TesteIntegracao.Setup;
+using SME.SGP.TesteIntegracao.TestarAvaliacaoAula;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.TestarAvaliacaoAula
+{
+    public class Ao_alterar_avaliacao_pela_gestao : TesteAvaliacao
+    {
+        private readonly DateTime DATA_24_01 = new(DateTimeExtension.HorarioBrasilia().Year, 01, 24);
+
+        private const long TIPO_CALENDARIO_1 = 1;
+        private const string DESCRICAO = "OUTRA DESCRICAO";
+
+        public Ao_alterar_avaliacao_pela_gestao(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+
+        }
+
+        [Fact]
+        public async Task Alterar_avaliacao_pelo_gestor_Diretor()
+        {
+            await ExecutarTesteParaGestor(ObterPerfilDiretor());
+        }
+
+        [Fact]
+        public async Task Alterar_avaliacao_pelo_gestor_CP()
+        {
+            await ExecutarTesteParaGestor(ObterPerfilCP());
+        }
+
+        [Fact]
+        public async Task Alterar_avaliacao_pelo_gestor_AD()
+        {
+            await ExecutarTesteParaGestor(ObterPerfilAD());
+        }
+
+        [Fact]
+        public async Task Nao_deve_alterar_avaliacao_se_nao_for_gestor()
+        {
+            await ExecutarTesteParaNaoGestor(ObterPerfilProfessor());
+        }
+
+        private async Task ExecutarTesteParaGestor(string perfil)
+        {
+            await CriarDadosBasicos(ObterCriacaoDeDadosDto(perfil));
+            await CriarAula(DATA_24_01, RecorrenciaAula.AulaUnica, TipoAula.Normal, USUARIO_PROFESSOR_CODIGO_RF_1111111, TURMA_CODIGO_1, UE_CODIGO_1, COMPONENTE_REGENCIA_CLASSE_FUND_I_5H_ID_1105.ToString(), TIPO_CALENDARIO_1);
+            var comando = ServiceProvider.GetService<IComandosAtividadeAvaliativa>();
+
+            var atividadeAvaliativa = ObterAtividadeAvaliativaDto(
+            COMPONENTE_REG_CLASSE_SP_INTEGRAL_1A5_ANOS_ID_1213.ToString(),
+            CategoriaAtividadeAvaliativa.Normal,
+            DATA_02_05,
+            TipoAvaliacaoCodigo.AvaliacaoBimestral);
+            atividadeAvaliativa.DisciplinaContidaRegenciaId = new string[] { COMPONENTE_REG_CLASSE_SP_INTEGRAL_1A5_ANOS_ID_1213.ToString() };
+
+            await comando.Inserir(atividadeAvaliativa);
+
+            atividadeAvaliativa.Descricao = DESCRICAO;
+
+            var retorno = await comando.Alterar(atividadeAvaliativa, 1);
+
+            retorno.ShouldNotBeNull();
+
+            var atividadeAvaliativas = ObterTodos<AtividadeAvaliativa>();
+
+            atividadeAvaliativas.ShouldNotBeEmpty();
+
+            atividadeAvaliativas.Count().ShouldBeGreaterThanOrEqualTo(1);
+        }
+
+        private async Task ExecutarTesteParaNaoGestor(string perfil)
+        {
+            await CriarDadosBasicos(ObterCriacaoDeDadosDto(perfil));
+            await CriarAula(DATA_24_01, RecorrenciaAula.AulaUnica, TipoAula.Normal, USUARIO_PROFESSOR_CODIGO_RF_1111111, TURMA_CODIGO_1, UE_CODIGO_1, COMPONENTE_REGENCIA_CLASSE_FUND_I_5H_ID_1105.ToString(), TIPO_CALENDARIO_1);
+            var comando = ServiceProvider.GetService<IComandosAtividadeAvaliativa>();
+
+            var atividadeAvaliativa = ObterAtividadeAvaliativaDto(
+            COMPONENTE_REG_CLASSE_SP_INTEGRAL_1A5_ANOS_ID_1213.ToString(),
+            CategoriaAtividadeAvaliativa.Normal,
+            DATA_02_05,
+            TipoAvaliacaoCodigo.AvaliacaoBimestral);
+            atividadeAvaliativa.DisciplinaContidaRegenciaId = new string[] { COMPONENTE_REG_CLASSE_SP_INTEGRAL_1A5_ANOS_ID_1213.ToString() };
+
+            await comando.Inserir(atividadeAvaliativa);
+
+            atividadeAvaliativa.Descricao = DESCRICAO;
+
+            var retorno = await comando.Alterar(atividadeAvaliativa, 1);
+
+            retorno.ShouldNotBeNull();
+
+            var atividadeAvaliativas = ObterTodos<AtividadeAvaliativa>();
+
+            atividadeAvaliativas.ShouldNotBeEmpty();
+
+            atividadeAvaliativas.Count().ShouldBeGreaterThanOrEqualTo(1);
+        }
+
+        private CriacaoDeDadosDto ObterCriacaoDeDadosDto(string perfil)
+        {
+            return new CriacaoDeDadosDto()
+            {
+                Perfil = perfil,
+                ModalidadeTurma = Modalidade.Fundamental,
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio,
+                TipoCalendarioId = TIPO_CALENDARIO_ID,
+                DataInicio = DATA_02_05,
+                DataFim = DATA_08_07,
+                TipoAvaliacao = TipoAvaliacaoCodigo.AvaliacaoBimestral,
+                Bimestre = BIMESTRE_2
+            };
+        }
+    }
+}
