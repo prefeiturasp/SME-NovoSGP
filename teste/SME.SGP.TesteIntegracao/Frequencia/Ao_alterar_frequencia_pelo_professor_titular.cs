542b620af9515af69e353787d76a4bb607266b8f
Danilo Ferreira <shymmi@hotmail.com>
2022-07-01 14:43:52 -0300

    teste alterar frequencia

---
+++ teste/SME.SGP.TesteIntegracao/Frequencia/Ao_alterar_frequencia_pelo_professor_titular.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.Frequencia
+{
+    internal class Ao_alterar_frequencia_pelo_professor_titular
+    {
+    }
+}
