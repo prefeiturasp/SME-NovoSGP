a0b95a91f2d9de6cf183837162a4eea80419ccef
Helio Barros <helio.barros@amcom.com.br>
2022-07-04 15:20:12 -0300

    Ajuste na consulta de auditoria para anotações do estudante

--- src/SME.SGP.Dados/Repositorios/RepositorioAnotacaoFechamentoAluno.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioAnotacaoFechamentoAluno.cs
@@ -28,16 +28,16 @@ namespace SME.SGP.Dados.Repositorios
         {
             var query = @"select coalesce(afa.anotacao, fa.anotacao) as Anotacao, ftd.disciplina_id,
                           case
-                            when fa.alterado_por is null then fa.criado_por
-                            when fa.alterado_por is not null then fa.alterado_por
+                            when afa.alterado_por is null then afa.criado_por
+                            when afa.alterado_por is not null then afa.alterado_por
                           end as professor,
                           case
-                            when fa.alterado_em is null then fa.criado_em
-                            when fa.alterado_em is not null then fa.alterado_em
+                            when afa.alterado_em is null then afa.criado_em
+                            when afa.alterado_em is not null then afa.alterado_em
                           end as data,
                           case 
-                            when fa.alterado_rf is null then fa.criado_rf
-                            when fa.alterado_rf is not null then fa.alterado_rf
+                            when afa.alterado_rf is null then afa.criado_rf
+                            when afa.alterado_rf is not null thena fa.alterado_rf
                           end as professorrf
                         from fechamento_turma_disciplina ftd 
                         inner join fechamento_aluno fa on fa.fechamento_turma_disciplina_id = ftd.id
@@ -46,7 +46,7 @@ namespace SME.SGP.Dados.Repositorios
                         inner join turma t on ft.turma_id = t.id 
                         where not ftd.excluido and not fa.excluido 
                          and fa.aluno_codigo = @alunoCodigo
-                         and ft.periodo_escolar_id   = @periodoId    
+                         and ft.periodo_escolar_id = @periodoId    
                          and t.turma_id =  ANY(@turmasCodigos)
                          and afa.anotacao is not null;";
 
