c4c3889fbdf5698bca9ba88487cd361fc38cb97d
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-07-04 14:12:44 -0300

    Alteração de predefinição

--- src/SME.SGP.Aplicacao/Commands/Frequencia/InserirRegistrosFrequenciasAlunos/InserirRegistrosFrequenciasAlunosCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Frequencia/InserirRegistrosFrequenciasAlunos/InserirRegistrosFrequenciasAlunosCommandHandler.cs
@@ -66,7 +66,7 @@ namespace SME.SGP.Aplicacao
             await repositorioFrequenciaPreDefinida.InserirVarios(dicionario[INSERIR]);
 
             foreach (var frequenciaPreDefinida in dicionario[ALTERAR])
-                await repositorioFrequenciaPreDefinida.Salvar(frequenciaPreDefinida);
+                await repositorioFrequenciaPreDefinida.Atualizar(frequenciaPreDefinida);
         }
 
 
