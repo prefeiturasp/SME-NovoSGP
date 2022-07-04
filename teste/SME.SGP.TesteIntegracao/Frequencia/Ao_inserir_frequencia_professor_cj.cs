0cb6e55e6487231cd2602471c39ab3d6c289dbc0
DanylloAntunes <danyllo.antunes@gmail.com>
2022-07-04 08:45:25 -0300

    Lançamento CJ - Integração

--- teste/SME.SGP.TesteIntegracao/Frequencia/Ao_inserir_frequencia_professor_cj.cs
+++ teste/SME.SGP.TesteIntegracao/Frequencia/Ao_inserir_frequencia_professor_cj.cs
@@ -9,20 +9,27 @@ namespace SME.SGP.TesteIntegracao.Frequencia
 {
     public class Ao_inserir_frequencia_professor_cj : FrequenciaBase
     {
-        private DateTime DATA_02_05 = new(DateTimeExtension.HorarioBrasilia().Year, 05, 02);
-        private DateTime DATA_07_08 = new(DateTimeExtension.HorarioBrasilia().Year, 07, 08);
-
         public Ao_inserir_frequencia_professor_cj(CollectionFixture collectionFixture) : base(collectionFixture)
         {
         }
 
-        //[Fact]
+        [Fact]
         public async Task Ao_registrar_frenquecia_professor_cj_ensino_fundamental()
         {
-            await CriarDadosBasicos(ObterPerfilCJ(), Modalidade.Fundamental, ModalidadeTipoCalendario.FundamentalMedio, DATA_02_05, DATA_07_08, BIMESTRE_2, DATA_02_05, COMPONENTE_CURRICULAR_PORTUGUES_ID_138.ToString(), false);
+            await CriarDadosBasicos(ObterPerfilCJ(), Modalidade.Fundamental, ModalidadeTipoCalendario.FundamentalMedio, DATA_02_05, DATA_07_08, BIMESTRE_2, DATA_02_05, COMPONENTE_CURRICULAR_PORTUGUES_ID_138.ToString());
             await CriarAtribuicaoCJ(Modalidade.Fundamental, COMPONENTE_CURRICULAR_PORTUGUES_ID_138);
 
             await InserirFrequenciaUseCaseComValidacaoBasica(ObtenhaFrenqueciaDto());
         }
+
+        [Fact]
+        public async Task Ao_registrar_frequencia_professor_CJ_ensino_infantil()
+        {
+            await CriarDadosBasicos(ObterPerfilCJ(), Modalidade.EducacaoInfantil, ModalidadeTipoCalendario.Infantil, DATA_02_05, DATA_07_08, BIMESTRE_2, DATA_02_05, COMPONENTE_REG_CLASSE_SP_INTEGRAL_1A5_ANOS_ID_1213.ToString());
+            await CriarAtribuicaoCJ(Modalidade.EducacaoInfantil, COMPONENTE_REG_CLASSE_SP_INTEGRAL_1A5_ANOS_ID_1213);
+
+            await InserirFrequenciaUseCaseComValidacaoBasica(ObtenhaFrenqueciaDto());
+        }
+
     }
 }
