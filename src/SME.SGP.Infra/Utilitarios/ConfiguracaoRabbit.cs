04581def3f3e26528244d073d8380291a566edc8
Rafael Losi <rafael.losi@amcom.com.br>
2022-07-12 20:43:30 -0300

    67868 Criação de Pool de Conexões com o RabbitMQ

---
+++ src/SME.SGP.Infra/Utilitarios/ConfiguracaoRabbit.cs
@@ -0,0 +1,11 @@
+﻿namespace SME.SGP.Infra.Utilitarios
+{
+    public abstract class ConfiguracaoRabbit
+    {
+        public static string Secao => "";
+        public string HostName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+    }
+}
--- src/SME.SGP.IoC/Extensions/RegistrarRabbit.cs
+++ src/SME.SGP.IoC/Extensions/RegistrarRabbit.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ObjectPool;
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
+using SME.SGP.Aplicacao.Integracoes;
 using SME.SGP.Infra.Utilitarios;
 
 namespace SME.SGP.IoC
@@ -11,9 +13,15 @@ namespace SME.SGP.IoC
         internal static void ConfigurarRabbitParaLogs(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddOptions<ConfiguracaoRabbitLogOptions>()
-                .Bind(configuration.GetSection("ConfiguracaoRabbitLog"), c => c.BindNonPublicProperties = true);
+                .Bind(configuration.GetSection(ConfiguracaoRabbitLogOptions.Secao), c => c.BindNonPublicProperties = true);
 
             services.AddSingleton<ConfiguracaoRabbitLogOptions>();
+            services.AddSingleton<IConexoesRabbitFilasLog>(serviceProvider =>
+            {
+                var options = serviceProvider.GetService<IOptions<ConfiguracaoRabbitLogOptions>>().Value;
+                var provider = serviceProvider.GetService<IOptions<DefaultObjectPoolProvider>>().Value;
+                return new ConexoesRabbitFilasLog(options, provider);
+            });
         }
 
         internal static void ConfigurarRabbit(this IServiceCollection services, IConfiguration configuration)
@@ -22,11 +30,13 @@ namespace SME.SGP.IoC
                 return;
 
             services.AddOptions<ConfiguracaoRabbitOptions>()
-                .Bind(configuration.GetSection(nameof(ConfiguracaoRabbitOptions)), c => c.BindNonPublicProperties = true)
-                .Services.AddSingleton<IConnectionFactory>(serviceProvider =>
-                {
-                    var options = serviceProvider.GetService<IOptions<ConfiguracaoRabbitOptions>>().Value;
+                .Bind(configuration.GetSection(ConfiguracaoRabbitOptions.Secao), c => c.BindNonPublicProperties = true);
 
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<ConfiguracaoRabbitOptions>>().Value;
+
+            services.AddSingleton<IConnectionFactory>(serviceProvider =>
+                {
                     var factory = new ConnectionFactory
                     {
                         HostName = options.HostName,                        
@@ -37,9 +47,14 @@ namespace SME.SGP.IoC
 
                     return factory;
                 });
-
             services.AddSingleton<ConfiguracaoRabbitOptions>();
-            services.AddSingleton<ConfiguracaoRabbitLogOptions>();
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddSingleton<IConexoesRabbitFilasSGP>(serviceProvider =>
+            {
+                var provider = serviceProvider.GetService<IOptions<DefaultObjectPoolProvider>>().Value;
+                return new ConexoesRabbitFilasSGP(options, provider);
+            });
         }
     }
 }
