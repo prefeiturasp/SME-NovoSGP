d467fcf719d277d60a36349834f5505d2ba7664d
Rafael Losi <rafael.losi@amcom.com.br>
2022-07-12 19:43:30 -0400

    67868 Criação de Pool de Conexões com o RabbitMQ

---
+++ src/SME.SGP.Aplicacao/Integracoes/ServicoRabbit/Interface/IConexoesRabbit.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+
+namespace SME.SGP.Aplicacao.Integracoes
+{
+    public interface IConexoesRabbit
+    {
+        IModel Get();
+        void Return(IModel conexao);
+    }
+
+    public interface IConexoesRabbitFilasSGP : IConexoesRabbit { }
+    public interface IConexoesRabbitFilasLog : IConexoesRabbit { }
+}
