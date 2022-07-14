7f46ebb0ee54ca3a78b1472fe02b5c12bf038b46
Rafael Losi <rafael.losi@amcom.com.br>
2022-07-13 23:11:10 -0300

    variavel TempoHeartBeat na ConfiguracaoRabbit

--- src/SME.SGP.Infra/Utilitarios/ConfiguracaoRabbit.cs
+++ src/SME.SGP.Infra/Utilitarios/ConfiguracaoRabbit.cs
@@ -7,5 +7,6 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public string VirtualHost { get; set; }
+        public ushort TempoHeartBeat { get; set; }
     }
 }
