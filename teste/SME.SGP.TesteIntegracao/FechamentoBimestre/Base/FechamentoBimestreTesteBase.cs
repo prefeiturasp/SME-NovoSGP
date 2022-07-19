5502fbad7fc9651037b695f151230ab338eb8d58
Fernando Groeler <fernando.groeler@gmail.com>
2022-07-19 14:30:06 -0400

    Iniciando criação da classe de teste base para fechamento do bimestre.

---
+++ teste/SME.SGP.TesteIntegracao/FechamentoBimestre/Base/FechamentoBimestreTesteBase.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.Nota.ServicosFakes;
+using SME.SGP.TesteIntegracao.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+using System.Collections.Generic;
+
+namespace SME.SGP.TesteIntegracao.FechamentoBimestre
+{
+    public class FechamentoBimestreTesteBase : TesteBase
+    {
+        public FechamentoBimestreTesteBase(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        protected override void RegistrarFakes(IServiceCollection services)
+        {
+            base.RegistrarFakes(services);
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQuery, bool>), typeof(ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQueryHandlerFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAlunosEolPorTurmaQuery, IEnumerable<AlunoPorTurmaResposta>>), typeof(ObterAlunosEolPorTurmaQueryHandlerFake), ServiceLifetime.Scoped));
+        }
+    }
+}
