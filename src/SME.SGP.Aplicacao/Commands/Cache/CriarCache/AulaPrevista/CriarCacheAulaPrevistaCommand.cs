bcdbf96fdca8412818ea5eafecc88f8965a8d97b
Danilo Ferreira <shymmi@hotmail.com>
2022-07-19 15:04:49 -0300

    inclusão de consulta ao cache antes de ir ao repositorio para consultar aulas previstas

---
+++ src/SME.SGP.Aplicacao/Commands/Cache/CriarCache/AulaPrevista/CriarCacheAulaPrevistaCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class CriarCacheAulaPrevistaCommand : IRequest<IEnumerable<AulaPrevista>>
+    {
+        public CriarCacheAulaPrevistaCommand(string nomeChave, long codigoUe)
+        {
+            NomeChave = nomeChave;
+            CodigoUe = codigoUe;
+        }
+
+        public long CodigoUe { get; set; }
+        public string NomeChave { get; set; }
+    }
+}
