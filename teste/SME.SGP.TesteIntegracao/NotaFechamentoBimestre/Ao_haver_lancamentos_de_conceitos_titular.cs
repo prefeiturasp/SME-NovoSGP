b894cc211417b072eb590f85c0c9b2ec12f56e4b
Danilo Ferreira <shymmi@hotmail.com>
2022-07-21 14:09:02 -0300

    implementação teste

--- teste/SME.SGP.TesteIntegracao/NotaFechamentoBimestre/Ao_haver_lancamentos_de_conceitos_titular.cs
+++ teste/SME.SGP.TesteIntegracao/NotaFechamentoBimestre/Ao_haver_lancamentos_de_conceitos_titular.cs
@@ -4,7 +4,6 @@ using SME.SGP.Aplicacao;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
 using SME.SGP.TesteIntegracao.Setup;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,94 +11,28 @@ using Xunit;
 
 namespace SME.SGP.TesteIntegracao.NotaFechamentoBimestre
 {
-    public class Ao_registrar_nota_pelo_cp_ou_diretor : NotaFechamentoBimestreTesteBase
+    public class Ao_haver_lancamentos_de_conceitos_titular : NotaFechamentoBimestreTesteBase
     {
         private const string ALUNO_CODIGO_1 = "1";
+        private const string ALUNO_CODIGO_2 = "2";
+        private const string ALUNO_CODIGO_3 = "3";
 
-        public Ao_registrar_nota_pelo_cp_ou_diretor(CollectionFixture collectionFixture) : base(collectionFixture)
+        public Ao_haver_lancamentos_de_conceitos_titular(CollectionFixture collectionFixture) : base(collectionFixture)
         {
         }
 
         [Fact]
-        public async Task Deve_registrar_nota_numerica_como_cp()
-        {
-            await CriarFechamentoTurma();
-            await CriarFechamentoTurmaDisciplina();
-            await CriarFechamentoAluno();
-            await CriarDadosBase(ObterFiltroFechamentoNotaDto(ObterPerfilCP(), ANO_5));
-            await ExecutarComandoNota();
-        }
-
-        [Fact]
-        public async Task Deve_registrar_nota_conceito_como_cp()
+        public async Task deve_haver_lancamento_com_mais_de_50_porcento_NS_para_inserir_justificativa()
         {
             await CriarDadosBase(ObterFiltroFechamentoNotaDto(ObterPerfilCP(), ANO_1));
             await ExecutarComandoConceito();
         }
 
         [Fact]
-        public async Task Deve_registrar_nota_numerica_como_diretor()
+        public async Task deve_haver_lancamento_com_mais_de_50_porcento_NS_para_inserir_justificativa_regencia()
         {
-            await CriarDadosBase(ObterFiltroFechamentoNotaDto(ObterPerfilDiretor(), ANO_1));
-            await ExecutarComandoNota();
-        }
-        [Fact]
-        public async Task Deve_registrar_nota_conceito_como_diretor()
-        {
-            await CriarDadosBase(ObterFiltroFechamentoNotaDto(ObterPerfilDiretor(), ANO_1));
+            await CriarDadosBase(ObterFiltroFechamentoNotaRegenciaDeClasse(ObterPerfilCP(), ANO_1));
             await ExecutarComandoConceito();
-        }
-
-        private async Task ExecutarTeste(IEnumerable<FechamentoTurmaDisciplinaDto> fechamentoTurma)
-        {
-            var comando = ServiceProvider.GetService<IComandosFechamentoTurmaDisciplina>();
-
-            await comando.Salvar(fechamentoTurma);
-            var notasFechamento = ObterTodos<FechamentoTurmaDisciplina>();
-
-            notasFechamento.ShouldNotBeNull();
-            notasFechamento.ShouldNotBeEmpty();
-            notasFechamento.Count().ShouldBeGreaterThanOrEqualTo(1);
-        }
-
-        private async Task ExecutarComandoNota()
-        {
-            var fechamentoNotaDto = new List<FechamentoNotaDto>()
-            {
-                new FechamentoNotaDto()
-                {
-                    ConceitoIdAnterior = null,
-                    AlteradoEm = DateTimeExtension.HorarioBrasilia(),
-                    CriadoEm = DateTimeExtension.HorarioBrasilia(),
-                    AlteradoPor = "",
-                    AlteradoRf = "",
-                    Anotacao ="",
-                    CodigoAluno = ALUNO_CODIGO_1,
-                    DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
-                    ConceitoId= null,
-                    CriadoPor= "",
-                    CriadoRf= "",
-                    Id= 1,
-                    Nota= 7,
-                    NotaAnterior= 6,
-                    SinteseId= (int)SinteseEnum.Frequente
-                }
-            };
-
-            var fechamentoTurmaDisciplinaDto = new List<FechamentoTurmaDisciplinaDto>()
-            {
-                new FechamentoTurmaDisciplinaDto()
-                {
-                    Bimestre = 1 ,
-                    DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
-                    Id = 1,
-                    Justificativa = "" ,
-                    TurmaId = TURMA_CODIGO_1 ,
-                    NotaConceitoAlunos = fechamentoNotaDto
-                }
-            };
-
-            await ExecutarTeste(fechamentoTurmaDisciplinaDto);
         }
 
         private async Task ExecutarComandoConceito()
@@ -108,7 +41,7 @@ namespace SME.SGP.TesteIntegracao.NotaFechamentoBimestre
             {
                 new FechamentoNotaDto()
                 {
-                    ConceitoIdAnterior = 1,
+                    ConceitoIdAnterior = (int) ConceitoValores.NS,
                     AlteradoEm = DateTimeExtension.HorarioBrasilia(),
                     CriadoEm = DateTimeExtension.HorarioBrasilia(),
                     AlteradoPor = "",
@@ -116,7 +49,43 @@ namespace SME.SGP.TesteIntegracao.NotaFechamentoBimestre
                     Anotacao ="",
                     CodigoAluno = ALUNO_CODIGO_1,
                     DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
-                    ConceitoId= (long) ConceitoValores.P,
+                    ConceitoId= (int) ConceitoValores.NS,
+                    CriadoPor= "",
+                    CriadoRf= "",
+                    Id= 1,
+                    Nota= null,
+                    NotaAnterior= null,
+                    SinteseId= (int)SinteseEnum.Frequente
+                },
+                new FechamentoNotaDto()
+                {
+                    ConceitoIdAnterior = (int) ConceitoValores.NS,
+                    AlteradoEm = DateTimeExtension.HorarioBrasilia(),
+                    CriadoEm = DateTimeExtension.HorarioBrasilia(),
+                    AlteradoPor = "",
+                    AlteradoRf = "",
+                    Anotacao ="",
+                    CodigoAluno = ALUNO_CODIGO_2,
+                    DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
+                    ConceitoId= (int) ConceitoValores.NS,
+                    CriadoPor= "",
+                    CriadoRf= "",
+                    Id= 1,
+                    Nota= null,
+                    NotaAnterior= null,
+                    SinteseId= (int)SinteseEnum.Frequente
+                },
+                new FechamentoNotaDto()
+                {
+                    ConceitoIdAnterior = (int) ConceitoValores.P,
+                    AlteradoEm = DateTimeExtension.HorarioBrasilia(),
+                    CriadoEm = DateTimeExtension.HorarioBrasilia(),
+                    AlteradoPor = "",
+                    AlteradoRf = "",
+                    Anotacao ="",
+                    CodigoAluno = ALUNO_CODIGO_3,
+                    DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
+                    ConceitoId= (int) ConceitoValores.P,
                     CriadoPor= "",
                     CriadoRf= "",
                     Id= 1,
@@ -142,6 +111,20 @@ namespace SME.SGP.TesteIntegracao.NotaFechamentoBimestre
             await ExecutarTeste(fechamentoTurmaDisciplinaDto);
         }
 
+        private async Task ExecutarTeste(IEnumerable<FechamentoTurmaDisciplinaDto> fechamentoTurma)
+        {
+            var comando = ServiceProvider.GetService<IComandosFechamentoTurmaDisciplina>();
+
+            await comando.Salvar(fechamentoTurma);
+            var notasFechamento = ObterTodos<FechamentoNota>();
+
+            notasFechamento.ShouldNotBeNull();
+            notasFechamento.ShouldNotBeEmpty();
+            notasFechamento.Count(x => x.ConceitoId == (long)ConceitoValores.NS).ShouldBeGreaterThan((notasFechamento.Count() / 2));
+        }
+
+
+
         private FiltroFechamentoNotaDto ObterFiltroFechamentoNotaDto(string perfil, string anoTurma, bool consideraAnorAnterior = false)
         {
             return new FiltroFechamentoNotaDto()
@@ -151,6 +134,19 @@ namespace SME.SGP.TesteIntegracao.NotaFechamentoBimestre
                 ConsiderarAnoAnterior = consideraAnorAnterior,
                 Modalidade = Modalidade.Fundamental,
                 TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio,
+                TipoFrequenciaAluno = TipoFrequenciaAluno.PorDisciplina
+            };
+        }
+
+        private FiltroFechamentoNotaDto ObterFiltroFechamentoNotaRegenciaDeClasse(string perfil, string anoTurma, bool consideraAnorAnterior = false)
+        {
+            return new FiltroFechamentoNotaDto()
+            {
+                Perfil = perfil,
+                AnoTurma = anoTurma,
+                ConsiderarAnoAnterior = consideraAnorAnterior,
+                Modalidade = Modalidade.Fundamental,
+                TipoCalendario = ModalidadeTipoCalendario.EJA,
                 TipoFrequenciaAluno = TipoFrequenciaAluno.PorDisciplina
             };
         }
