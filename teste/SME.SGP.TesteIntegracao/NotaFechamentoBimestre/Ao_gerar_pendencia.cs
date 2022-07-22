347a23f6baed1ffb5bc8a532ca7cfd85377e88b0
Danilo Ferreira <shymmi@hotmail.com>
2022-07-22 14:56:54 -0300

    implementando testes para validar pendencias do fechamento

---
+++ teste/SME.SGP.TesteIntegracao/NotaFechamentoBimestre/Ao_gerar_pendencia.cs
@@ -0,0 +1,99 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.NotaFechamentoBimestre.ServicosFakes;
+using SME.SGP.TesteIntegracao.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.NotaFechamentoBimestre
+{
+    public class Ao_gerar_pendencia : NotaFechamentoBimestreTesteBase
+    {
+        private const string ALUNO_CODIGO_1 = "1";
+
+        public Ao_gerar_pendencia(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        protected override void RegistrarFakes(IServiceCollection services)
+        {
+            base.RegistrarFakes(services);
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQuery, bool>), typeof(ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQueryHandlerComPermissaoFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<IncluirFilaGeracaoPendenciasFechamentoCommand, bool>), typeof(IncluirFilaGeracaoPendenciasFechamentoCommandHandlerFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ProfessoresTurmaDisciplinaQuery, List<ProfessorAtribuidoTurmaDisciplinaDTO>>), typeof(ProfessoresTurmaDisciplinaQueryHandlerFakeProfessorPortugues), ServiceLifetime.Scoped));
+        }
+
+        [Fact]
+        public async Task deve_haver_pendencia_com_situacao_valida()
+        {
+            await CriarDadosBase(ObterFiltroFechamentoNotaDto(ObterPerfilCP(), ANO_1));
+            await ExecutarComandoConceito();
+        }
+
+
+
+        private async Task ExecutarComandoConceito()
+        {
+            var fechamentoNotaDto = new List<FechamentoNotaDto>()
+            {
+                new FechamentoNotaDto()
+                {
+                    ConceitoIdAnterior = null,
+                    AlteradoEm = DateTimeExtension.HorarioBrasilia(),
+                    CriadoEm = DateTimeExtension.HorarioBrasilia(),
+                    AlteradoPor = "",
+                    AlteradoRf = "",
+                    Anotacao = "",
+                    CodigoAluno = ALUNO_CODIGO_1,
+                    DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
+                    ConceitoId = null,
+                    CriadoPor = "",
+                    CriadoRf = "",
+                    Nota = null,
+                    NotaAnterior = null,
+                    SinteseId = (int)SinteseEnum.Frequente
+                }
+            };
+
+            var fechamentoTurmaDisciplinaDto = new List<FechamentoTurmaDisciplinaDto>()
+            {
+                new FechamentoTurmaDisciplinaDto()
+                {
+                    Bimestre = 1 ,
+                    DisciplinaId = COMPONENTE_CURRICULAR_PORTUGUES_ID_138,
+                    Justificativa = "" ,
+                    TurmaId = TURMA_CODIGO_1 ,
+                    NotaConceitoAlunos = fechamentoNotaDto
+                }
+            };
+
+            await CriarAula(DATA_29_04, RecorrenciaAula.AulaUnica, TipoAula.Normal, USUARIO_PROFESSOR_CODIGO_RF_1111111, TURMA_CODIGO_1, UE_CODIGO_1, COMPONENTE_CURRICULAR_PORTUGUES_ID_138.ToString(), TIPO_CALENDARIO_1);
+
+            await ExecutarTestePendencia(fechamentoTurmaDisciplinaDto);
+        }
+
+        private async Task ExecutarTestePendencia(IEnumerable<FechamentoTurmaDisciplinaDto> fechamentoTurma)
+        {
+            var comando = ServiceProvider.GetService<IComandosFechamentoTurmaDisciplina>();
+
+            await comando.Salvar(fechamentoTurma);
+            var pendenciasFechamento = ObterTodos<Pendencia>();
+
+            pendenciasFechamento.Any().ShouldBeTrue();
+            pendenciasFechamento.Any
+                (
+                    x => x.Situacao == SituacaoPendencia.Aprovada ||
+                    x.Situacao == SituacaoPendencia.Pendente ||
+                    x.Situacao == SituacaoPendencia.Resolvida
+                ).ShouldBeTrue();
+        }
+    }
+}
