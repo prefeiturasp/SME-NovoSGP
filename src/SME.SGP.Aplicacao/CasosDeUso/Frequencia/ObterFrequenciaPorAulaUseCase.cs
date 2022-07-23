37ae2d28b49ed02aaddba48654462145ebf85305
williamamcom <69849971+williamamcom@users.noreply.github.com>
2022-07-22 22:04:26 -0300

    ajuste na verificação de aluninos inativos para eja

--- src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ObterFrequenciaPorAulaUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Frequencia/ObterFrequenciaPorAulaUseCase.cs
@@ -33,6 +33,8 @@ namespace SME.SGP.Aplicacao
                 throw new NegocioException("Não foram encontrados alunos para a aula/turma informada.");
 
             var alunosDaTurmaFiltrados = alunosDaTurma.GroupBy(x => x.CodigoAluno).SelectMany(y => y.OrderBy(a => a.SituacaoMatricula).Take(1));
+
+            var teste = alunosDaTurmaFiltrados.Where(y => y.CodigoAluno == "685830");
                         
             FrequenciaDto registroFrequenciaDto = await ObterRegistroFrequencia(aula, turma);
 
@@ -82,8 +84,18 @@ namespace SME.SGP.Aplicacao
 
             var turmaPossuiFrequenciaRegistrada = await mediator.Send(new ExisteFrequenciaRegistradaPorTurmaComponenteCurricularQuery(turma.CodigoTurma, aula.DisciplinaId, periodoEscolar.Id));
 
-            var alunosCondicaoFrequencia = alunosDaTurmaFiltrados.Where(a => a.EstaAtivo(aula.DataAula, aula.DataAula) || !a.SituacaoMatricula.Equals(SituacaoMatriculaAluno.VinculoIndevido) &&
-            (a.Inativo && a.DataSituacao >= aula.DataAula)).OrderBy(c => c.NomeAluno);
+            var alunosCondicaoFrequencia = Enumerable.Empty<AlunoPorTurmaResposta>();
+            if (turma.ModalidadeCodigo == Modalidade.EJA)
+            {
+                alunosCondicaoFrequencia = alunosDaTurmaFiltrados.Where(a => !a.Inativo || !a.SituacaoMatricula.Equals(SituacaoMatriculaAluno.VinculoIndevido) &&
+                (a.Inativo && a.DataSituacao >= aula.DataAula)).OrderBy(c => c.NomeAluno);
+            }
+            else
+            {
+                alunosCondicaoFrequencia = alunosDaTurmaFiltrados.Where(a => a.EstaAtivo(aula.DataAula, aula.DataAula) || !a.SituacaoMatricula.Equals(SituacaoMatriculaAluno.VinculoIndevido) &&
+                (a.Inativo && a.DataSituacao >= aula.DataAula)).OrderBy(c => c.NomeAluno);
+            }
+             
 
             foreach (var aluno in alunosCondicaoFrequencia)
             {
