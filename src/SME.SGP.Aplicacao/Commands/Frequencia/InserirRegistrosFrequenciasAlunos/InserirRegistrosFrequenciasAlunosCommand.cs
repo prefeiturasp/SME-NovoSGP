28c899d629678a76e82695360d740212cbc165d2
Thiago Fernando <thiago.fernando@msn.com>
2022-07-25 14:04:50 -0300

    Adicionado Aula ID ao criar frequencia

--- src/SME.SGP.Aplicacao/Commands/Frequencia/InserirRegistrosFrequenciasAlunos/InserirRegistrosFrequenciasAlunosCommand.cs
+++ src/SME.SGP.Aplicacao/Commands/Frequencia/InserirRegistrosFrequenciasAlunos/InserirRegistrosFrequenciasAlunosCommand.cs
@@ -10,17 +10,19 @@ namespace SME.SGP.Aplicacao
 {
     public class InserirRegistrosFrequenciasAlunosCommand : IRequest<bool>
     {
-        public InserirRegistrosFrequenciasAlunosCommand(IList<RegistroFrequenciaAlunoDto> frequencias, long registroFrequenciaId, long turmaId, long componenteCurricularId)
+        public InserirRegistrosFrequenciasAlunosCommand(IList<RegistroFrequenciaAlunoDto> frequencias, long registroFrequenciaId, long turmaId, long componenteCurricularId,long aulaId)
         {
             Frequencias = frequencias;
             RegistroFrequenciaId = registroFrequenciaId;
             TurmaId = turmaId;
             ComponenteCurricularId = componenteCurricularId;
+            AulaId = aulaId;
         }
 
         public IList<RegistroFrequenciaAlunoDto> Frequencias { get; set; }
         public long RegistroFrequenciaId { get; set; }
         public long TurmaId { get; set; }
+        public long AulaId { get; set; }
         public long ComponenteCurricularId { get; set; }
     }
 
@@ -40,6 +42,9 @@ namespace SME.SGP.Aplicacao
             RuleFor(x => x.RegistroFrequenciaId)
               .NotEmpty()
               .WithMessage("O registro de frequência precisa ser informado");
+            RuleFor(x => x.AulaId)
+                .NotEmpty()
+                .WithMessage("A aula precisa ser informada");
         }
     }
 }
