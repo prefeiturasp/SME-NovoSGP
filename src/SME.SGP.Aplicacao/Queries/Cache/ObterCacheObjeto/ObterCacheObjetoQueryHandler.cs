76d987cd8448d5ab6ba6c573274cdec1639b6d53
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-08-01 15:27:48 -0300

    Ajuste de namespace

--- src/SME.SGP.Aplicacao/Queries/Cache/ObterCacheObjeto/ObterCacheObjetoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Cache/ObterCacheObjeto/ObterCacheObjetoQueryHandler.cs
@@ -4,19 +4,22 @@ using System.Threading.Tasks;
 using MediatR;
 using SME.SGP.Dominio.Interfaces;
 
-namespace SME.SGP.Aplicacao;
-
-public class ObterCacheObjetoQueryHandler<T> : IRequestHandler<ObterCacheObjetoQuery<T>,T>
+namespace SME.SGP.Aplicacao
 {
-    private readonly IRepositorioCache repositorioCache;
 
-    public ObterCacheObjetoQueryHandler(IRepositorioCache cache)
+    public class ObterCacheObjetoQueryHandler<T> : IRequestHandler<ObterCacheObjetoQuery<T>, T>
     {
-        repositorioCache = cache ?? throw new ArgumentNullException(nameof(cache));
-    }
+        private readonly IRepositorioCache repositorioCache;
 
-    public async Task<T> Handle(ObterCacheObjetoQuery<T> request, CancellationToken cancellationToken)
-    {
-        return  await repositorioCache.ObterAsync(request.NomeCache,request.BuscarDados,request.MinutosParaExpirar,request.UtilizarGZip);
+        public ObterCacheObjetoQueryHandler(IRepositorioCache cache)
+        {
+            repositorioCache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        public async Task<T> Handle(ObterCacheObjetoQuery<T> request, CancellationToken cancellationToken)
+        {
+            return await repositorioCache.ObterAsync(request.NomeCache, request.BuscarDados, request.MinutosParaExpirar,
+                request.UtilizarGZip);
+        }
     }
 }
