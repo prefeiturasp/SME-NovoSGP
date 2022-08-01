907ced8b1dd9c5cd1f1e50910f60bfccb8ec0881
Thiago Fernando <thiago.fernando@msn.com>
2022-08-01 09:44:01 -0300

    Adicionado cache em ObterUePorIdQuery

--- src/SME.SGP.Aplicacao/Queries/UE/ObterUePorId/ObterUePorIdQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/UE/ObterUePorId/ObterUePorIdQueryHandler.cs
@@ -10,13 +10,17 @@ namespace SME.SGP.Aplicacao
     public class ObterUePorIdQueryHandler : IRequestHandler<ObterUePorIdQuery, Ue>
     {
         private readonly IRepositorioUeConsulta repositorioUe;
+        private readonly IRepositorioCache repositorioCache;
 
-        public ObterUePorIdQueryHandler(IRepositorioUeConsulta repositorioUe)
+
+        public ObterUePorIdQueryHandler(IRepositorioUeConsulta repositorioUe, IRepositorioCache repositorioCache)
         {
             this.repositorioUe = repositorioUe ?? throw new ArgumentNullException(nameof(repositorioUe));
+            this.repositorioCache = repositorioCache ?? throw new ArgumentNullException(nameof(repositorioCache));
         }
 
         public async Task<Ue> Handle(ObterUePorIdQuery request, CancellationToken cancellationToken)
-                => await repositorioUe.ObterUePorId(request.Id);
+            => await repositorioCache.ObterAsync($"ObterUePorId-{request.Id}",
+                async () => await repositorioUe.ObterUePorId(request.Id));
     }
 }
