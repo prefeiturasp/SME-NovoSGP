495e22bdbaadda88d1551dc75e42da400a28ba41
DanylloAntunes <danyllo.antunes@gmail.com>
2022-08-02 16:57:45 -0300

    Utilizar cache na tela de conselho de classe

---
+++ src/SME.SGP.Aplicacao/Cache/ConselhoDeClasseNotaPosConselhoBimestresCache.cs
@@ -0,0 +1,36 @@
+﻿using SME.SGP.Aplicacao.Servicos;
+using SME.SGP.Infra;
+using System.Linq;
+
+namespace SME.SGP.Aplicacao.Cache
+{
+    public class ConselhoDeClasseNotaPosConselhoBimestresCache : ServicoDeGerenciamentoDeCache<ConselhoClasseAlunoNotasConceitosRetornoDto>
+    {
+        private GravarConselhoClasseCommad request;
+
+        public ConselhoDeClasseNotaPosConselhoBimestresCache(GravarConselhoClasseCommad request)
+        {
+            this.request = request;
+        }
+
+        protected override string ObtenhaChave()
+        {
+            return $"NotaConceitoBimestre-{request.ConselhoClasseId}-${request.CodigoAluno}-{request.Bimestre}";
+        }
+
+        protected override ConselhoClasseAlunoNotasConceitosRetornoDto ObtenhaObjetoAtualizado()
+        {
+            var retorno = ObtenhaObjeto();
+
+            if (retorno != null)
+            {
+                var conselhoClasseComponenteFrequenciaDtos = retorno!.NotasConceitos.FirstOrDefault()!.ComponentesCurriculares.FirstOrDefault(x => x.CodigoComponenteCurricular == request.ConselhoClasseNotaDto.CodigoComponenteCurricular);
+                conselhoClasseComponenteFrequenciaDtos!.NotaPosConselho.Nota = request.ConselhoClasseNotaDto.Conceito ?? request.ConselhoClasseNotaDto.Nota;
+
+                return conselhoClasseComponenteFrequenciaDtos;
+            }
+
+            return null;
+        }
+    }
+}
