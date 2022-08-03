eb713ab8b96d79ba3bed6153c2671c351087270c
DanylloAntunes <danyllo.antunes@gmail.com>
2022-08-03 08:04:02 -0300

    Utilizar cache na tela de conselho de classe

---
+++ src/SME.SGP.Aplicacao/Cache/ValorCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Cache
+{
+    public class ValorCache<T> where T : class
+    {
+        public string Chave { get; set; }
+        public T Valor {  get; set; }
+    }
+}
