5a87b9ed29050abc8a0f940789188b119e37fe7e
Fernando Groeler <fernando.groeler@gmail.com>
2022-08-11 16:49:43 -0400

    Removido código não utilizado.

--- teste/SME.SGP.TesteIntegracao/ConselhoDeClasse/Ao_listar_opcoes_impressao.cs
+++ teste/SME.SGP.TesteIntegracao/ConselhoDeClasse/Ao_listar_opcoes_impressao.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using SME.SGP.Aplicacao;
 using SME.SGP.Dominio;
-using SME.SGP.Infra;
 using SME.SGP.TesteIntegracao.Setup;
 using Xunit;
 
