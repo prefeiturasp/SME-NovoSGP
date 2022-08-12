cca52069f8617841a8378e3322f01afc1b2b69c1
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-08-12 12:52:03 -0300

    Status da correção

--- teste/SME.SGP.TesteIntegracao/ConselhoDeClasse/Ao_inserir_nota_numerica_pos_conselho_bimestre.cs
+++ teste/SME.SGP.TesteIntegracao/ConselhoDeClasse/Ao_inserir_nota_numerica_pos_conselho_bimestre.cs
@@ -16,7 +16,9 @@ namespace SME.SGP.TesteIntegracao.ConselhoDeClasse
     {
         public Ao_inserir_nota_numerica_pos_conselho_bimestre(CollectionFixture collectionFixture) : base(collectionFixture)
         {
-            //Revisado
+            //Revisado parcial problemas aqui
+            //Ao_lancar_nota_numerica_pos_conselho_bimestre_fundamental_cp = problema com o usuario
+            //Ao_lancar_nota_pos_conselho_bimestre_numerica_medio_diretor =  problema com o usuario
         }
 
         protected override void RegistrarFakes(IServiceCollection services)
