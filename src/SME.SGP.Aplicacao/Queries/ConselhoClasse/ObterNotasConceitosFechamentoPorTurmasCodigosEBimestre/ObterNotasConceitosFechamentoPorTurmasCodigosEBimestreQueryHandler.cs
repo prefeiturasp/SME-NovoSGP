16da5a4b8365edfb6328b50c29ebb8b8984c11c0
Fernando Groeler <fernando.groeler@gmail.com>
2022-08-12 07:34:39 -0400

    Corrigido build jenkins.

--- src/SME.SGP.Aplicacao/Queries/ConselhoClasse/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestre/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/ConselhoClasse/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestre/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQueryHandler.cs
@@ -8,32 +8,41 @@ using SME.SGP.Dominio.Constantes;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
-namespace SME.SGP.Aplicacao;
-
-public class ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQueryHandler : IRequestHandler<ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery, IEnumerable<NotaConceitoBimestreComponenteDto>>
+namespace SME.SGP.Aplicacao
 {
-    private readonly IRepositorioConselhoClasseNotaConsulta repositorioConselhoClasseNota;
-    private readonly IRepositorioCache repositorioCache;
-
-    public ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQueryHandler(IRepositorioConselhoClasseNotaConsulta repositorioConselhoClasseNota, IRepositorioCache repositorioCache)
+    public class ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQueryHandler : IRequestHandler<
+        ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery, IEnumerable<NotaConceitoBimestreComponenteDto>>
     {
-        this.repositorioConselhoClasseNota = repositorioConselhoClasseNota ?? throw new ArgumentNullException(nameof(repositorioConselhoClasseNota));        
-        this.repositorioCache = repositorioCache ?? throw new ArgumentNullException(nameof(repositorioCache));
-    }
+        private readonly IRepositorioConselhoClasseNotaConsulta repositorioConselhoClasseNota;
+        private readonly IRepositorioCache repositorioCache;
 
-    public async Task<IEnumerable<NotaConceitoBimestreComponenteDto>> Handle(ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery request, CancellationToken cancellationToken)
-    {
-        var retorno = new List<NotaConceitoBimestreComponenteDto>();
-
-        foreach (var turmaCodigo in request.TurmasCodigos)
+        public ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQueryHandler(
+            IRepositorioConselhoClasseNotaConsulta repositorioConselhoClasseNota, IRepositorioCache repositorioCache)
         {
-            var notasConceitosFechamento = (await repositorioCache.ObterAsync(string.Format(NomeChaveCache.CHAVE_NOTA_CONCEITO_FECHAMENTO_TURMA_BIMESTRE, turmaCodigo, request.Bimestre),
-                async () => await repositorioConselhoClasseNota.ObterNotasConceitosFechamentoPorTurmaCodigoEBimestreAsync(turmaCodigo, request.Bimestre))).ToList();
-        
-            if (notasConceitosFechamento.Any())
-                retorno.AddRange(notasConceitosFechamento);
+            this.repositorioConselhoClasseNota = repositorioConselhoClasseNota ??
+                                                 throw new ArgumentNullException(nameof(repositorioConselhoClasseNota));
+            this.repositorioCache = repositorioCache ?? throw new ArgumentNullException(nameof(repositorioCache));
         }
 
-        return await Task.FromResult(retorno);
+        public async Task<IEnumerable<NotaConceitoBimestreComponenteDto>> Handle(
+            ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery request, CancellationToken cancellationToken)
+        {
+            var retorno = new List<NotaConceitoBimestreComponenteDto>();
+
+            foreach (var turmaCodigo in request.TurmasCodigos)
+            {
+                var notasConceitosFechamento = (await repositorioCache.ObterAsync(
+                        string.Format(NomeChaveCache.CHAVE_NOTA_CONCEITO_FECHAMENTO_TURMA_BIMESTRE, turmaCodigo,
+                            request.Bimestre),
+                        async () => await repositorioConselhoClasseNota
+                            .ObterNotasConceitosFechamentoPorTurmaCodigoEBimestreAsync(turmaCodigo, request.Bimestre)))
+                    .ToList();
+
+                if (notasConceitosFechamento.Any())
+                    retorno.AddRange(notasConceitosFechamento);
+            }
+
+            return await Task.FromResult(retorno);
+        }
     }
 }
