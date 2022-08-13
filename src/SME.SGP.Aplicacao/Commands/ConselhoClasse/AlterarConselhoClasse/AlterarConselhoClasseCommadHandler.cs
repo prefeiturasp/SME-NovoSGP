30865802a3e14fc19928493b19054124aeebf160
Felipe Abreu <felipe.abreu@amcom.com.br>
2022-08-13 16:55:19 -0300

    Correção do bug 71987

--- src/SME.SGP.Aplicacao/Commands/ConselhoClasse/AlterarConselhoClasse/AlterarConselhoClasseCommadHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/ConselhoClasse/AlterarConselhoClasse/AlterarConselhoClasseCommadHandler.cs
@@ -71,17 +71,20 @@ namespace SME.SGP.Aplicacao
 
                         conselhoClasseNota.Nota = request.ConselhoClasseNotaDto.Nota.Value;
                     }
-                    else 
-                        conselhoClasseNota.Nota = null;
-
+                    else conselhoClasseNota.Nota = null;
+                    
+                    // Gera histórico de alteração
                     if (request.ConselhoClasseNotaDto.Conceito.HasValue)
                     {
-                        // Gera histórico de alteração
-                        if (conselhoClasseNota.ConceitoId != request.ConselhoClasseNotaDto.Conceito.Value)
+                        if (conselhoClasseNota.ConceitoId != null &&  conselhoClasseNota.ConceitoId != request.ConselhoClasseNotaDto.Conceito.Value)
                             await mediator.Send(new SalvarHistoricoConceitoConselhoClasseCommand(conselhoClasseNota.Id, conselhoClasseNota.ConceitoId, request.ConselhoClasseNotaDto.Conceito.Value), cancellationToken);
-
-                        conselhoClasseNota.ConceitoId = request.ConselhoClasseNotaDto.Conceito.Value;
                     }
+                    else
+                    {
+                        if (conselhoClasseNota.ConceitoId != null && request.ConselhoClasseNotaDto.Conceito == null)
+                            await mediator.Send(new SalvarHistoricoConceitoConselhoClasseCommand(conselhoClasseNota.Id, conselhoClasseNota.ConceitoId, null), cancellationToken);
+                    }
+                    conselhoClasseNota.ConceitoId = request.ConselhoClasseNotaDto.Conceito.HasValue ? request.ConselhoClasseNotaDto.Conceito.Value : null;
                 }
 
                 if (request.Turma.AnoLetivo == 2020)
