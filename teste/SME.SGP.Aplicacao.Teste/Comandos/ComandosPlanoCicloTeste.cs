38f45081f16c0a5b7500de8857b5c2bf81ebf5d3
Thiago Fernando <thiago.fernando@msn.com>
2022-08-16 20:37:49 -0300

    Ajustes ComandosPlanoCicloTeste.cs

--- teste/SME.SGP.Aplicacao.Teste/Comandos/ComandosPlanoCicloTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Comandos/ComandosPlanoCicloTeste.cs
@@ -5,6 +5,7 @@ using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using Microsoft.Extensions.Options;
 using SME.SGP.Infra.Utilitarios;
 using Xunit;
@@ -18,8 +19,9 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
         private readonly Mock<IRepositorioObjetivoDesenvolvimentoPlano> repositorioObjetivoDesenvolvimentoPlano;
         private readonly Mock<IRepositorioPlanoCiclo> repositorioPlanoCiclo;
         private readonly Mock<IUnitOfWork> unitOfWork;
+
         private readonly Mock<IMediator> mediator;
-        private readonly Mock<IOptions<ConfiguracaoArmazenamentoOptions>> configuracaoArmazenamentoOptions;
+        private readonly IOptions<ConfiguracaoArmazenamentoOptions> configuracaoArmazenamentoOptions;
 
         public ComandosPlanoCicloTeste()
         {
@@ -28,20 +30,27 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
             repositorioObjetivoDesenvolvimentoPlano = new Mock<IRepositorioObjetivoDesenvolvimentoPlano>();
             unitOfWork = new Mock<IUnitOfWork>();
             mediator = new Mock<IMediator>();
-            configuracaoArmazenamentoOptions = new Mock<IOptions<ConfiguracaoArmazenamentoOptions>>();
+            //configuracaoArmazenamentoOptions = new Mock<IOptions<ConfiguracaoArmazenamentoOptions>>();
+            configuracaoArmazenamentoOptions = Options.Create<ConfiguracaoArmazenamentoOptions>(new ConfiguracaoArmazenamentoOptions()
+            {
+                BucketArquivos = "Teste",
+                BucketTemp = "Teste",
+                EndPoint = "http://teste/",
+                Port = 0
+            });
             comandosPlanoCiclo = new ComandosPlanoCiclo(repositorioPlanoCiclo.Object,
-                                                        repositorioMatrizSaberPlano.Object,
-                                                        repositorioObjetivoDesenvolvimentoPlano.Object,
-                                                        unitOfWork.Object,mediator.Object, configuracaoArmazenamentoOptions.Object);
+                repositorioMatrizSaberPlano.Object,
+                repositorioObjetivoDesenvolvimentoPlano.Object,
+                unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions);
         }
 
         [Fact(DisplayName = "DeveDispararExcecaoAoInstanciarSemDependencias")]
         public void DeveDispararExcecaoAoInstanciarSemDependencias()
         {
-            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(null, repositorioMatrizSaberPlano.Object, repositorioObjetivoDesenvolvimentoPlano.Object, unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions.Object));
-            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(repositorioPlanoCiclo.Object, null, repositorioObjetivoDesenvolvimentoPlano.Object, unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions.Object));
-            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(repositorioPlanoCiclo.Object, repositorioMatrizSaberPlano.Object, null, unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions.Object));
-            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(repositorioPlanoCiclo.Object, repositorioMatrizSaberPlano.Object, repositorioObjetivoDesenvolvimentoPlano.Object, null, mediator.Object, configuracaoArmazenamentoOptions.Object));
+            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(null, repositorioMatrizSaberPlano.Object, repositorioObjetivoDesenvolvimentoPlano.Object, unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions));
+            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(repositorioPlanoCiclo.Object, null, repositorioObjetivoDesenvolvimentoPlano.Object, unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions));
+            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(repositorioPlanoCiclo.Object, repositorioMatrizSaberPlano.Object, null, unitOfWork.Object, mediator.Object, configuracaoArmazenamentoOptions));
+            Assert.Throws<ArgumentNullException>(() => new ComandosPlanoCiclo(repositorioPlanoCiclo.Object, repositorioMatrizSaberPlano.Object, repositorioObjetivoDesenvolvimentoPlano.Object, null, mediator.Object, configuracaoArmazenamentoOptions));
         }
 
         [Fact(DisplayName = "DeveSalvarPlanoCiclo")]
@@ -49,6 +58,11 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
         {
             repositorioPlanoCiclo.Setup(c => c.Salvar(It.IsAny<PlanoCiclo>()))
                 .Returns(1);
+
+
+            mediator.Setup(a => a.Send(It.IsAny<MoverArquivosTemporariosCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync("Teste");
+
 
             comandosPlanoCiclo.Salvar(new PlanoCicloDto()
             {
@@ -58,11 +72,11 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
                 EscolaId = "1",
                 IdsMatrizesSaber = new List<long>()
                 {
-                    1,2,3
+                    1, 2, 3
                 },
                 IdsObjetivosDesenvolvimento = new List<long>()
                 {
-                    1,2,3
+                    1, 2, 3
                 }
             });
             unitOfWork.Verify(c => c.PersistirTransacao(), Times.Once);
@@ -72,20 +86,24 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
         public void DeveSalvarPlanoCicloAlterandoObjetivosEMatrizDoSaber()
         {
             repositorioObjetivoDesenvolvimentoPlano.Setup(c => c.ObterObjetivosDesenvolvimentoPorIdPlano(It.IsAny<long>()))
-                .Returns(new List<RecuperacaoParalelaObjetivoDesenvolvimentoPlano>() {
-                    new RecuperacaoParalelaObjetivoDesenvolvimentoPlano(){
-                        Id=1,
-                        ObjetivoDesenvolvimentoId=1,
-                        PlanoId=1
+                .Returns(new List<RecuperacaoParalelaObjetivoDesenvolvimentoPlano>()
+                {
+                    new RecuperacaoParalelaObjetivoDesenvolvimentoPlano()
+                    {
+                        Id = 1,
+                        ObjetivoDesenvolvimentoId = 1,
+                        PlanoId = 1
                     }
                 });
 
             repositorioMatrizSaberPlano.Setup(c => c.ObterMatrizesPorIdPlano(It.IsAny<long>()))
-                .Returns(new List<MatrizSaberPlano>() {
-                    new MatrizSaberPlano(){
-                        Id=1,
-                        MatrizSaberId=1,
-                        PlanoId=1
+                .Returns(new List<MatrizSaberPlano>()
+                {
+                    new MatrizSaberPlano()
+                    {
+                        Id = 1,
+                        MatrizSaberId = 1,
+                        PlanoId = 1
                     }
                 });
 
@@ -101,11 +119,11 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
                 EscolaId = "1",
                 IdsMatrizesSaber = new List<long>()
                 {
-                    3,4,5
+                    3, 4, 5
                 },
                 IdsObjetivosDesenvolvimento = new List<long>()
                 {
-                    2,3,4
+                    2, 3, 4
                 }
             });
             unitOfWork.Verify(c => c.PersistirTransacao(), Times.Once);
@@ -128,13 +146,13 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
                     Descricao = "Teste",
                     EscolaId = "1",
                     IdsMatrizesSaber = new List<long>()
-                {
-                    1,2,3
-                },
+                    {
+                        1, 2, 3
+                    },
                     IdsObjetivosDesenvolvimento = new List<long>()
-                {
-                    1,2,3
-                }
+                    {
+                        1, 2, 3
+                    }
                 })).Message);
             unitOfWork.Verify(c => c.PersistirTransacao(), Times.Never);
         }
@@ -151,7 +169,7 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
                     EscolaId = "1",
                     IdsObjetivosDesenvolvimento = new List<long>()
                     {
-                        1,2,3
+                        1, 2, 3
                     }
                 })).Message);
             unitOfWork.Verify(c => c.PersistirTransacao(), Times.Never);
@@ -169,7 +187,7 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
                     EscolaId = "1",
                     IdsMatrizesSaber = new List<long>()
                     {
-                        1,2,3
+                        1, 2, 3
                     }
                 })).Message);
             unitOfWork.Verify(c => c.PersistirTransacao(), Times.Never);
