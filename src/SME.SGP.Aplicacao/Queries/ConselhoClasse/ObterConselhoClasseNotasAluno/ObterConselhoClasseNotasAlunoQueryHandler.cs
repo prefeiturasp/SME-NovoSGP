c66b31216313c1758a2c7bc5650f4064450dc348
Rafael Losi <rafael.losi@amcom.com.br>
2022-08-16 09:36:50 -0300

    Correção carga de notas de conselho de classe

--- src/SME.SGP.Aplicacao/Queries/ConselhoClasse/ObterConselhoClasseNotasAluno/ObterConselhoClasseNotasAlunoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/ConselhoClasse/ObterConselhoClasseNotasAluno/ObterConselhoClasseNotasAlunoQueryHandler.cs
@@ -20,11 +20,8 @@ namespace SME.SGP.Aplicacao
         public async Task<IEnumerable<NotaConceitoBimestreComponenteDto>> Handle(ObterConselhoClasseNotasAlunoQuery request, CancellationToken cancellationToken)
         {
             var turmaCodigo = await mediator.Send(new ObterTurmaPorConselhoClasseIdQuery(request.ConselhoClasseId), cancellationToken); 
-            var turmas = await mediator.Send(new ObterTurmasPorCodigosQuery(new[] { turmaCodigo }), cancellationToken);
-
-            var turmasCodigos = turmas.Select(c => c.CodigoTurma).ToArray();
             
-            return (await mediator.Send(new ObterNotasConceitosConselhoClassePorTurmasCodigosEBimestreQuery(turmasCodigos, request.Bimestre), cancellationToken))
+            return (await mediator.Send(new ObterNotasConceitosConselhoClassePorTurmasCodigosEBimestreQuery(new[] { turmaCodigo }, request.Bimestre), cancellationToken))
                 .Where(c => c.AlunoCodigo == request.AlunoCodigo && c.ConselhoClasseId == request.ConselhoClasseId);
         }
     }
