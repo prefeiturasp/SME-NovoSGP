572d227005e3da6e1d6b9f74ae4c2eccdafd58ef
Fernando Groeler <fernando.groeler@gmail.com>
2022-08-17 11:36:17 -0400

    Ajustado para atualizar o cache ao invés de remover.

--- src/SME.SGP.Aplicacao/Comandos/ComandosFechamentoFinal.cs
+++ src/SME.SGP.Aplicacao/Comandos/ComandosFechamentoFinal.cs
@@ -3,8 +3,10 @@ using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SME.SGP.Dominio.Constantes;
 
 namespace SME.SGP.Aplicacao
@@ -26,12 +28,12 @@ namespace SME.SGP.Aplicacao
             IRepositorioFechamentoTurmaDisciplinaConsulta repositorioFechamentoTurmaDisciplina,
             IMediator mediator)
         {
-            this.servicoFechamentoFinal = servicoFechamentoFinal ?? throw new System.ArgumentNullException(nameof(servicoFechamentoFinal));
-            this.repositorioTurmaConsulta = repositorioTurmaConsulta ?? throw new System.ArgumentNullException(nameof(repositorioTurmaConsulta));
-            this.repositorioFechamentoTurmaDisciplina = repositorioFechamentoTurmaDisciplina ?? throw new System.ArgumentNullException(nameof(repositorioFechamentoTurmaDisciplina));
-            this.repositorioFechamentoTurma = repositorioFechamentoTurma ?? throw new System.ArgumentNullException(nameof(repositorioFechamentoTurma));
-            this.repositorioFechamentoAluno = repositorioFechamentoAluno ?? throw new System.ArgumentNullException(nameof(repositorioFechamentoAluno));
-            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+            this.servicoFechamentoFinal = servicoFechamentoFinal ?? throw new ArgumentNullException(nameof(servicoFechamentoFinal));
+            this.repositorioTurmaConsulta = repositorioTurmaConsulta ?? throw new ArgumentNullException(nameof(repositorioTurmaConsulta));
+            this.repositorioFechamentoTurmaDisciplina = repositorioFechamentoTurmaDisciplina ?? throw new ArgumentNullException(nameof(repositorioFechamentoTurmaDisciplina));
+            this.repositorioFechamentoTurma = repositorioFechamentoTurma ?? throw new ArgumentNullException(nameof(repositorioFechamentoTurma));
+            this.repositorioFechamentoAluno = repositorioFechamentoAluno ?? throw new ArgumentNullException(nameof(repositorioFechamentoAluno));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         public async Task<AuditoriaPersistenciaDto> SalvarAsync(FechamentoFinalSalvarDto fechamentoFinalSalvarDto)
@@ -44,9 +46,8 @@ namespace SME.SGP.Aplicacao
 
             var auditoria = await servicoFechamentoFinal.SalvarAsync(fechamentoTurmaDisciplina, turma, usuarioLogado,
                 fechamentoFinalSalvarDto.Itens, emAprovacao);
-            
-            await RemoverCache(string.Format(NomeChaveCache.CHAVE_FECHAMENTO_NOTA_FINAL_COMPONENTE_TURMA,
-                fechamentoFinalSalvarDto.DisciplinaId, fechamentoFinalSalvarDto.TurmaCodigo));
+
+            await InserirOuAtualizarCache(fechamentoFinalSalvarDto, emAprovacao);
 
             if (!auditoria.EmAprovacao)
             {
@@ -57,11 +58,52 @@ namespace SME.SGP.Aplicacao
 
             return auditoria;
         }
-        
-        private async Task RemoverCache(string nomeChave)
+
+        private async Task InserirOuAtualizarCache(FechamentoFinalSalvarDto fechamentoFinalSalvar, bool emAprovacao)
         {
-            await mediator.Send(new RemoverChaveCacheCommand(nomeChave));
-        }        
+            var disciplinaId = fechamentoFinalSalvar.EhRegencia ? long.Parse(fechamentoFinalSalvar.DisciplinaId) :
+                fechamentoFinalSalvar.Itens.First().ComponenteCurricularCodigo;            
+            
+            var nomeChaveCache = string.Format(NomeChaveCache.CHAVE_FECHAMENTO_NOTA_FINAL_COMPONENTE_TURMA,
+                disciplinaId, fechamentoFinalSalvar.TurmaCodigo);
+            
+            var dadosCache = await mediator.Send(new ObterCacheAsyncQuery(nomeChaveCache, "Obter fechamento nota final"));
+            var retornoCacheMapeado = (await MapearDadosCacheParaDto(dadosCache)).ToList();
+
+            foreach (var fechamentoFinal in fechamentoFinalSalvar.Itens)
+            {
+                var cacheAluno = retornoCacheMapeado.FirstOrDefault(c => c.AlunoCodigo == fechamentoFinal.AlunoRf);
+
+                if (cacheAluno == null)
+                {
+                    retornoCacheMapeado.Add(new FechamentoNotaAlunoAprovacaoDto
+                    {
+                        Bimestre = 0,
+                        Nota = fechamentoFinal.Nota,
+                        AlunoCodigo = fechamentoFinal.AlunoRf,
+                        ConceitoId = fechamentoFinal.ConceitoId,
+                        EmAprovacao = emAprovacao,
+                        ComponenteCurricularId = disciplinaId
+                    });
+
+                    continue;
+                }
+
+                cacheAluno.Nota = fechamentoFinal.Nota;
+                cacheAluno.ConceitoId = fechamentoFinal.ConceitoId;
+                cacheAluno.EmAprovacao = emAprovacao;
+            }
+            
+            await mediator.Send(new SalvarCachePorValorObjetoCommand(nomeChaveCache, retornoCacheMapeado));
+        }
+
+        private static async Task<IEnumerable<FechamentoNotaAlunoAprovacaoDto>> MapearDadosCacheParaDto(string dadosCache)
+        {
+            if (string.IsNullOrEmpty(dadosCache))
+                return Enumerable.Empty<FechamentoNotaAlunoAprovacaoDto>();
+            
+            return await Task.FromResult(JsonConvert.DeserializeObject<IEnumerable<FechamentoNotaAlunoAprovacaoDto>>(dadosCache));
+        }
 
         private Task<Usuario> ObterUsuarioLogado()
             => mediator.Send(new ObterUsuarioLogadoQuery());
@@ -107,8 +149,11 @@ namespace SME.SGP.Aplicacao
             else
                 fechamentoTurmaDisciplina = await repositorioFechamentoTurmaDisciplina.ObterFechamentoTurmaDisciplina(fechamentoFinalSalvarDto.TurmaCodigo, disciplinaId);
 
-            fechamentoTurmaDisciplina ??= new FechamentoTurmaDisciplina()
-                { DisciplinaId = disciplinaId, Situacao = SituacaoFechamento.ProcessadoComSucesso };
+            fechamentoTurmaDisciplina ??= new FechamentoTurmaDisciplina
+            {
+                DisciplinaId = disciplinaId,
+                Situacao = SituacaoFechamento.ProcessadoComSucesso
+            };
 
             fechamentoTurmaDisciplina.FechamentoTurma = fechamentoFinalTurma;
 
