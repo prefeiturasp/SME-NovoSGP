02033e2d9c0ca7ef77801783ae6149ef0097bc5f
Rafael Losi <rafael.losi@amcom.com.br>
2022-08-29 12:52:27 -0300

    Incluso APM no Worker de Notificações

--- src/SME.SGP.Notificacoes.Worker/Startup.cs
+++ src/SME.SGP.Notificacoes.Worker/Startup.cs
@@ -1,3 +1,5 @@
+using Elastic.Apm.AspNetCore;
+using Elastic.Apm.DiagnosticSource;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -43,6 +45,9 @@ namespace SME.SGP.Notificacoes.Worker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseElasticApm(Configuration,
+                new HttpDiagnosticsSubscriber());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
