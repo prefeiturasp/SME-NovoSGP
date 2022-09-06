cf067da94919d08d343aadea95da6b0e56c867e2
Helio Barros <helio.barros@amcom.com.br>
2022-09-06 17:46:27 -0300

    AB#73987  - correção de baixa em compensação de ausência não atualizada ao verificar registro de frequência de alunos inativos.

---
+++ src/SME.SGP.Infra/Dtos/CompensacaoAusencia/TotalCompensacaoAlunoPorCompensacaoIdDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Infra
+{
+    public class TotalCompensacaoAlunoPorCompensacaoIdDto
+    {
+        public long CompensacaoAlunoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
