4f23d519a737f157dda821d9fbd5d7be4a3e457a
Thiago Fernando <thiago.fernando@msn.com>
2022-09-13 08:37:18 -0300

    criado arquivo de testes Ao_copiar_plano_de_aula

---
+++ teste/SME.SGP.TesteIntegracao/PlanoAula/Ao_copiar_plano_de_aula.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dto;
+using SME.SGP.TesteIntegracao.PlanoAula.Base;
+using SME.SGP.TesteIntegracao.PlanoAula.ServicosFakes;
+using SME.SGP.TesteIntegracao.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+
+namespace SME.SGP.TesteIntegracao.PlanoAula
+{
+    public class Ao_copiar_plano_de_aula : PlanoAulaTesteBase
+    {
+        public Ao_copiar_plano_de_aula(CollectionFixture collectionFixture) : base(collectionFixture){}
+        
+        protected override void RegistrarFakes(IServiceCollection services)
+        {
+            base.RegistrarFakes(services);
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAbrangenciaPorTurmaEConsideraHistoricoQuery, AbrangenciaFiltroRetorno>), typeof(ObterAbrangenciaPorTurmaEConsideraHistoricoQueryHandlerFakeFundamental6A), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQuery, bool>), typeof(ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQueryHandlerComPermissaoFake), ServiceLifetime.Scoped));
+
+        }
+    }
+}
