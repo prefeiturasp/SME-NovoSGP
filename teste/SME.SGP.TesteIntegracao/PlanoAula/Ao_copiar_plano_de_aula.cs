3fbae45e53feb2a35ea1c6d8315119915b913da1
Thiago Fernando <thiago.fernando@msn.com>
2022-09-13 08:43:14 -0300

    criado fact dos testes

--- teste/SME.SGP.TesteIntegracao/PlanoAula/Ao_copiar_plano_de_aula.cs
+++ teste/SME.SGP.TesteIntegracao/PlanoAula/Ao_copiar_plano_de_aula.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using System.Threading.Tasks;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using SME.SGP.Aplicacao;
@@ -7,6 +8,7 @@ using SME.SGP.TesteIntegracao.PlanoAula.Base;
 using SME.SGP.TesteIntegracao.PlanoAula.ServicosFakes;
 using SME.SGP.TesteIntegracao.ServicosFakes;
 using SME.SGP.TesteIntegracao.Setup;
+using Xunit;
 
 namespace SME.SGP.TesteIntegracao.PlanoAula
 {
@@ -20,6 +22,24 @@ namespace SME.SGP.TesteIntegracao.PlanoAula
             services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAbrangenciaPorTurmaEConsideraHistoricoQuery, AbrangenciaFiltroRetorno>), typeof(ObterAbrangenciaPorTurmaEConsideraHistoricoQueryHandlerFakeFundamental6A), ServiceLifetime.Scoped));
             services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQuery, bool>), typeof(ObterUsuarioPossuiPermissaoNaTurmaEDisciplinaQueryHandlerComPermissaoFake), ServiceLifetime.Scoped));
 
+        }
+
+        [Fact(DisplayName = "Cópia de plano de aula para outra aula da mesma turma e componente curricular")]
+        public async Task Copiar_plano_para_outra_aula_da_mesma_turma_e_componente()
+        {
+            
+        }
+
+        [Fact(DisplayName = "Cópia de plano de aula para outra turma com o mesmo componente curricular")]
+        public async Task Copiar_plano_para_outra_turma_mesmo_componente()
+        {
+            
+        }
+
+        [Fact(DisplayName = "Cópia de plano de aula para outra turma e componente curricular diferente (não deve permitir)")]
+        public async Task Copiar_plano_para_outra_turma_de_componente_direferente()
+        {
+            
         }
     }
 }
