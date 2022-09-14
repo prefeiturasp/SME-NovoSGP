5b0c3ff3a7b9c324681c1d7573886d5c4086e860
jailsvs <jailsvs@gmail.com>
2022-09-14 11:22:42 -0300

    Revert "Melhorias efetuadas já na queue/rotas SGP Rabbit"
    This reverts commit c1bcf003c5624d86f638ec1930ad0c10b0fe8045.

--- src/SME.SGP.Aplicacao/Workers/WorkerRabbitMQBase.cs
+++ src/SME.SGP.Aplicacao/Workers/WorkerRabbitMQBase.cs
@@ -14,9 +14,7 @@ using SME.SGP.Infra.Excecoes;
 using SME.SGP.Infra.Interfaces;
 using SME.SGP.Infra.Utilitarios;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -95,36 +93,15 @@ namespace SME.SGP.Aplicacao.Workers
 
             foreach (var fila in tipoRotas.ObterConstantesPublicas<string>())
             {
-
-                if (!fila.StartsWith("sgp.notificacao.usuario")) //TODO RESET
-                {
-                    continue;
-                }
-
                 canalRabbit.QueueDeclare(fila, true, false, false, args);
                 canalRabbit.QueueBind(fila, exchange, fila, null);
 
                 if (!string.IsNullOrEmpty(exchangeDeadLetter))
                 {
-                    var argsDlq = new Dictionary<string, object>();
-                    argsDlq.Add("x-dead-letter-exchange", exchange);
-                    argsDlq.Add("x-message-ttl", 10000);
-
                     var filaDeadLetter = $"{fila}.deadletter";
 
-                    canalRabbit.QueueDeclare(filaDeadLetter, true, false, false, argsDlq);
+                    canalRabbit.QueueDeclare(filaDeadLetter, true, false, false, null);
                     canalRabbit.QueueBind(filaDeadLetter, exchangeDeadLetter, fila, null);
-
-                    var queueLimbu = $"{fila}.limbu";
-                    canalRabbit.QueueDeclare(
-                        queue: queueLimbu,
-                        durable: true,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null
-                    );
-
-                    canalRabbit.QueueBind(queueLimbu, exchangeDeadLetter, queueLimbu, null);
                 }
             }
         }
@@ -148,8 +125,6 @@ namespace SME.SGP.Aplicacao.Workers
                     var casoDeUso = scope.ServiceProvider.GetService(comandoRabbit.TipoCasoUso);
 
                     var metodo = UtilMethod.ObterMetodo(comandoRabbit.TipoCasoUso, "Executar");
-
-                    throw new Exception("Erro forçado!");
 
                     await servicoTelemetria.RegistrarAsync(async () =>
                         await metodo.InvokeAsync(casoDeUso, new object[] { mensagemRabbit }),
@@ -186,21 +161,12 @@ namespace SME.SGP.Aplicacao.Workers
                 {
                     transacao?.CaptureException(ex);
 
-                    if (ea.DeliveryTag > comandoRabbit.QuantidadeRetry)
-                    {
-                        canalRabbit.BasicAck(ea.DeliveryTag, false);
-                        
-                        var queueLimbu = $"{ea.RoutingKey}.limbu";
-                        canalRabbit.BasicPublish(ExchangeSgpRabbit.SgpDeadLetter, queueLimbu, null, ea.Body.ToArray());
+                    canalRabbit.BasicReject(ea.DeliveryTag, false);
 
-                    }
-                    else canalRabbit.BasicReject(ea.DeliveryTag, false);
-
-                    //TODO RESET
-                    /*await RegistrarLog(ea, mensagemRabbit, ex, LogNivel.Critico, $"Erros: {ex.Message}");
+                    await RegistrarLog(ea, mensagemRabbit, ex, LogNivel.Critico, $"Erros: {ex.Message}");
 
                     if (mensagemRabbit.NotificarErroUsuario)
-                        NotificarErroUsuario($"Ocorreu um erro interno, por favor tente novamente", mensagemRabbit.UsuarioLogadoRF, comandoRabbit.NomeProcesso);*/
+                        NotificarErroUsuario($"Ocorreu um erro interno, por favor tente novamente", mensagemRabbit.UsuarioLogadoRF, comandoRabbit.NomeProcesso);
                 }
                 finally
                 {
