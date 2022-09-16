e43d71aa506e9be0f5e0b6751b9bdc4a8e377585
Thiago Fernando <thiago.fernando@msn.com>
2022-09-16 14:24:27 -0300

    Criado teste Editar_plano_que_esta_na_situacao_aguardando_parecer_da_coordenacao

--- teste/SME.SGP.TesteIntegracao/PlanoAEE/Ao_realizar_devolucao.cs
+++ teste/SME.SGP.TesteIntegracao/PlanoAEE/Ao_realizar_devolucao.cs
@@ -1,5 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos;
+using SME.SGP.TesteIntegracao.PlanoAEE.ServicosFakes;
 using SME.SGP.TesteIntegracao.Setup;
 using Xunit;
 
@@ -14,16 +25,62 @@ namespace SME.SGP.TesteIntegracao.PlanoAEE
         protected override void RegistrarFakes(IServiceCollection services)
         {
             base.RegistrarFakes(services);
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAlunoPorCodigoEAnoQuery, AlunoReduzidoDto>), typeof(ObterAlunoPorCodigoEAnoQueryHandlerFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterParametroSistemaPorTipoEAnoQuery, ParametrosSistema>), typeof(ObterParametroSistemaPorTipoEAnoQueryHanlerFake), ServiceLifetime.Scoped));
         }
 
         [Fact(DisplayName = "Com o CP editar um plano que está na situação Aguardando parecer da Coordenação e clicar em Devolver, preenchendo o campo de motivo")]
         public async Task Editar_plano_que_esta_na_situacao_aguardando_parecer_da_coordenacao()
         {
-            //Arrange
+            //Criando Plano
+            var salvarPlanoAEEUseCase = ObterServicoSalvarPlanoAEEUseCase();
+
+            await CriarDadosBasicos(new FiltroPlanoAee()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilCP(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio,
+            });
+
+            var planoAEEPersistenciaDto = new PlanoAEEPersistenciaDto()
+            {
+                TurmaId = TURMA_ID_1,
+                AlunoCodigo = ALUNO_CODIGO_1,
+                Situacao = 0,
+                Questoes = ObterQuestoes(),
+                TurmaCodigo = TURMA_CODIGO_1,
+                ResponsavelRF = USUARIO_CP_LOGIN_3333333,
+            };
             
-            //Act
+            var retorno = await salvarPlanoAEEUseCase.Executar(planoAEEPersistenciaDto);
             
-            //Assert
+            //Obter todos os planos
+            var obterTodosPlanoAee = ObterTodos<Dominio.PlanoAEE>();
+            obterTodosPlanoAee.ShouldNotBeNull();
+            
+            //Buscando Plano Criado
+            var filtroObter = new FiltroPesquisaQuestoesPorPlanoAEEIdDto(obterTodosPlanoAee.FirstOrDefault().Id,TURMA_CODIGO_1);
+            
+            var obterPlanoAeeUseCase =  ObterServicoObterPlanoAEEPorIdUseCase();
+            var retornoObter = await obterPlanoAeeUseCase.Executar(filtroObter);
+            retornoObter.ShouldNotBeNull();
+            retornoObter.Situacao.ShouldBeEquivalentTo(SituacaoPlanoAEE.ParecerCP);
+
+            // Devolvendo o Plano Com a Justificativa
+            var filtroDevolver = new DevolucaoPlanoAEEDto(){PlanoAEEId = retornoObter.Id,Motivo = "Motivo Teste"};
+            var devolverUseCase = ObterServicoDevolverPlanoAEEUseCase();
+            var retornoDevolver = await devolverUseCase.Executar(filtroDevolver);
+            retornoDevolver.ShouldBeTrue();
+            
+            //Obter Todas Pendencias
+            var obterTodosPendenciasAee = ObterTodos<Dominio.Pendencia>();
+            obterTodosPendenciasAee.ShouldNotBeNull();
+            obterTodosPendenciasAee.FirstOrDefault().Tipo.ShouldBeEquivalentTo(TipoPendencia.AEE);
+            obterTodosPendenciasAee.FirstOrDefault().Situacao.ShouldBeEquivalentTo(SituacaoPendencia.Resolvida);
+            
+            obterTodosPendenciasAee.LastOrDefault().Tipo.ShouldBeEquivalentTo(TipoPendencia.AEE);
+            obterTodosPendenciasAee.LastOrDefault().Situacao.ShouldBeEquivalentTo(SituacaoPendencia.Pendente);
+
         }
 
         [Fact(DisplayName = "Com o Coordenador do CEFAI editar um plano que está na situação Aguardando atribuição de PAAI e clicar em Devolver, preenchendo o campo de motivo.")]
@@ -43,6 +100,104 @@ namespace SME.SGP.TesteIntegracao.PlanoAEE
             //Act
             
             //Assert
+        }
+        
+        private List<PlanoAEEQuestaoDto> ObterQuestoes()
+        {
+            return new List<PlanoAEEQuestaoDto>()
+            {
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 1,
+                    Resposta = "1",
+                    TipoQuestao = TipoQuestao.PeriodoEscolar
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 2,
+                    Resposta = "4",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 3,
+                    Resposta = "[{\"diaSemana\":\"Segunda\",\"horarioInicio\":\"2022-09-14T08:00:38\",\"horarioTermino\":\"2022-09-14T17:30:38\",\"id\":2},{\"diaSemana\":\"Terça\",\"horarioInicio\":\"2022-09-14T09:00:00\",\"horarioTermino\":\"2022-09-14T18:30:00\",\"id\":2},{\"diaSemana\":\"Quarta\",\"horarioInicio\":\"2022-09-14T12:00:00\",\"horarioTermino\":\"2022-09-14T18:30:00\",\"id\":3},{\"diaSemana\":\"Quinta\",\"horarioInicio\":\"2022-09-14T07:35:00\",\"horarioTermino\":\"2022-09-14T16:45:00\",\"id\":4},{\"diaSemana\":\"Sexta\",\"horarioInicio\":\"2022-09-14T08:45:00\",\"horarioTermino\":\"2022-09-14T16:00:00\",\"id\":5}]",
+                    TipoQuestao = TipoQuestao.FrequenciaEstudanteAEE
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 4,
+                    Resposta = "3",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 5,
+                    Resposta = "4 - Forma de atendimento educacional especializado do estudante",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 6,
+                    Resposta = "5 - Objetivos do AEE",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 7,
+                    Resposta = "6 - Orientações e ações para o desenvolvimento/atividades do AEE",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 8,
+                    Resposta = "14",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 9,
+                    Resposta = "7 - Tem necessidade de recursos de Acessibilidade/Materiais para eliminação de barreiras para a sala regular (Seleção de materiais, equipamentos e mobiliário)",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 10,
+                    Resposta = "1",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 11,
+                    Resposta = "8 - Tem necessidade de recursos de Acessibilidade/Materiais para eliminação de barreiras para a sala de recursos multifuncionais (Seleção de materiais, equipamentos e mobiliário)",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 12,
+                    Resposta = "2",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 13,
+                    Resposta = "9 - Mobilização dos Recursos Humanos da U.E. ou parcerias na unidade educacional",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 14,
+                    Resposta = "6",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 15,
+                    Resposta = "10 - Mobilização dos Recursos Humanos com profissionais externos à unidade educacional",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+            };
+
         }
     }
 }
