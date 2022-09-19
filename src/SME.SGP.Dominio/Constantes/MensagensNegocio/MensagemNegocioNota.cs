d356fc27dccf0487612b6c8f910522882cba3238
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-09-19 13:25:56 -0300

    Alterar use case ValidarMediaAlunosAtividadeAvaliativaUseCase

---
+++ src/SME.SGP.Dominio/Constantes/MensagensNegocio/MensagemNegocioNota.cs
@@ -0,0 +1,7 @@
+﻿namespace SME.SGP.Dominio.Constantes.MensagensNegocio
+{
+    public class MensagemNegocioNota
+    {
+        public const string TIPO_NOTA_NAO_ENCONTRADO = "Não foi encontrado tipo de nota para a avaliação informada";
+    }
+}
