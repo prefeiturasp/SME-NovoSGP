36b2bdd86f3c970effb82f78290436fe06298f47
Thiago Fernando <thiago.fernando@msn.com>
2022-09-19 11:05:43 -0300

    Criado teste Alterar o responsável pelo plano

---
+++ teste/SME.SGP.TesteIntegracao/PlanoAEE/Ao_editar_plano_aee.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos;
+using SME.SGP.TesteIntegracao.PlanoAEE.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.PlanoAEE
+{
+    public class Ao_editar_plano_aee : PlanoAEETesteBase
+    {
+        public Ao_editar_plano_aee(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        protected override void RegistrarFakes(IServiceCollection services)
+        {
+            base.RegistrarFakes(services);
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAlunoPorCodigoEAnoQuery, AlunoReduzidoDto>), typeof(ObterAlunoPorCodigoEAnoQueryHandlerFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterParametroSistemaPorTipoEAnoQuery, ParametrosSistema>), typeof(ObterParametroSistemaPorTipoEAnoQueryHanlerFake), ServiceLifetime.Scoped));
+        }
+        
+        [Fact(DisplayName = "Alterar o responsável pelo plano")]
+        public async Task Alterar_responsável_pelo_plano()
+        {
+            var salvarPlanoAeeUseCase = ObterServicoSalvarPlanoAEEUseCase();
+
+            await CriarDadosBasicos(new FiltroPlanoAee()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilCP(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio,
+            });
+
+            var planoAeePersistenciaDto = new PlanoAEEPersistenciaDto()
+            {
+                TurmaId = TURMA_ID_1,
+                AlunoCodigo = ALUNO_CODIGO_1,
+                Situacao = 0,
+                Questoes = ObterQuestoes(),
+                TurmaCodigo = TURMA_CODIGO_1,
+                ResponsavelRF = USUARIO_CP_LOGIN_3333333,
+            };
+            
+            var planoAeeDto = await salvarPlanoAeeUseCase.Executar(planoAeePersistenciaDto);
+            
+            var filtroObter = new FiltroPesquisaQuestoesPorPlanoAEEIdDto(planoAeeDto.PlanoId,TURMA_CODIGO_1);
+            
+            var obterPlanoAeeUseCase =  ObterServicoObterPlanoAEEPorIdUseCase();
+            var retornoObter = await obterPlanoAeeUseCase.Executar(filtroObter);
+            retornoObter.ShouldNotBeNull();
+            retornoObter.Situacao.ShouldBeEquivalentTo(SituacaoPlanoAEE.ParecerCP);
+
+            var servicoAlterarResponsavelPlano = ObterServicoAtribuirResponsavelGeralDoPlanoUseCase();
+            var alterarResponsavel = await servicoAlterarResponsavelPlano.Executar(planoAeeDto.PlanoId,USUARIO_PROFESSOR_LOGIN_2222222,USUARIO_PROFESSOR_LOGIN_2222222);
+            alterarResponsavel.ShouldBeTrue();
+            
+            var retornoObterPlanoAlterado = await obterPlanoAeeUseCase.Executar(filtroObter);
+            retornoObterPlanoAlterado.ShouldNotBeNull();
+            
+            retornoObterPlanoAlterado.Responsavel.ResponsavelNome.ShouldContain(USUARIO_PROFESSOR_LOGIN_2222222);
+            retornoObterPlanoAlterado.Responsavel.ResponsavelRF.ShouldContain(USUARIO_PROFESSOR_LOGIN_2222222);
+
+        }
+        
+        private List<PlanoAEEQuestaoDto> ObterQuestoes()
+        {
+            return new List<PlanoAEEQuestaoDto>()
+            {
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 1,
+                    Resposta = "1",
+                    TipoQuestao = TipoQuestao.PeriodoEscolar
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 2,
+                    Resposta = "4",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 3,
+                    Resposta = "[{\"diaSemana\":\"Segunda\",\"horarioInicio\":\"2022-09-14T08:00:38\",\"horarioTermino\":\"2022-09-14T17:30:38\",\"id\":2},{\"diaSemana\":\"Terça\",\"horarioInicio\":\"2022-09-14T09:00:00\",\"horarioTermino\":\"2022-09-14T18:30:00\",\"id\":2},{\"diaSemana\":\"Quarta\",\"horarioInicio\":\"2022-09-14T12:00:00\",\"horarioTermino\":\"2022-09-14T18:30:00\",\"id\":3},{\"diaSemana\":\"Quinta\",\"horarioInicio\":\"2022-09-14T07:35:00\",\"horarioTermino\":\"2022-09-14T16:45:00\",\"id\":4},{\"diaSemana\":\"Sexta\",\"horarioInicio\":\"2022-09-14T08:45:00\",\"horarioTermino\":\"2022-09-14T16:00:00\",\"id\":5}]",
+                    TipoQuestao = TipoQuestao.FrequenciaEstudanteAEE
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 4,
+                    Resposta = "3",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 5,
+                    Resposta = "4 - Forma de atendimento educacional especializado do estudante",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 6,
+                    Resposta = "5 - Objetivos do AEE",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 7,
+                    Resposta = "6 - Orientações e ações para o desenvolvimento/atividades do AEE",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 8,
+                    Resposta = "14",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 9,
+                    Resposta = "7 - Tem necessidade de recursos de Acessibilidade/Materiais para eliminação de barreiras para a sala regular (Seleção de materiais, equipamentos e mobiliário)",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 10,
+                    Resposta = "1",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 11,
+                    Resposta = "8 - Tem necessidade de recursos de Acessibilidade/Materiais para eliminação de barreiras para a sala de recursos multifuncionais (Seleção de materiais, equipamentos e mobiliário)",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 12,
+                    Resposta = "2",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 13,
+                    Resposta = "9 - Mobilização dos Recursos Humanos da U.E. ou parcerias na unidade educacional",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 14,
+                    Resposta = "6",
+                    TipoQuestao = TipoQuestao.Radio
+                },
+                new PlanoAEEQuestaoDto()
+                { 
+                    QuestaoId   = 15,
+                    Resposta = "10 - Mobilização dos Recursos Humanos com profissionais externos à unidade educacional",
+                    TipoQuestao = TipoQuestao.Texto
+                },
+            };
+
+        }
+    }
+}
