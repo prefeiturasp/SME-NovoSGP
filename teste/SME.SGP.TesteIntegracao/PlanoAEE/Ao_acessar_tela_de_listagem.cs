bf3b5d180d72046db000c9fe40c1075cb1787361
Danilo Ferreira <shymmi@hotmail.com>
2022-09-18 21:33:23 -0300

    busca por filtros

--- teste/SME.SGP.TesteIntegracao/PlanoAEE/Ao_acessar_tela_de_listagem.cs
+++ teste/SME.SGP.TesteIntegracao/PlanoAEE/Ao_acessar_tela_de_listagem.cs
@@ -5,6 +5,7 @@ using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
 using SME.SGP.TesteIntegracao.Setup;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -24,28 +25,139 @@ namespace SME.SGP.TesteIntegracao.PlanoAEE
         [Fact]
         public async Task Deve_exibir_historico_ao_selecionar_turma_de_2021()
         {
-
-            var servicoCadastrarPlanoAee = ObterServicoSalvarPlanoAEEUseCase();
-            var servicoObterPlanoAEE = ObterServicoObterPlanosAEEUseCase();
-            var filtroPlanoAeeDto = ObterFiltroPlanoAEEDto();
-            var planoAEEPersistenciaDto = ObterPlanoAEEPersistenciaDto();
-
-            await CriarQuestoesPlanoAee();
             await CriarDadosBasicos(new FiltroPlanoAee()
             {
                 Modalidade = Modalidade.Fundamental,
                 Perfil = ObterPerfilProfessor(),
                 TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio
             });
-            // await servicoCadastrarPlanoAee.Executar(planoAEEPersistenciaDto);
-            var questao = ObterTodos<Questao>();
-            var questaoPlano = ObterTodos<PlanoAEEQuestao>();
+
+            await CriarTurma(Modalidade.Fundamental, TURMA_ANO_2, TURMA_CODIGO_2, TipoTurma.Regular, true);
+
+            var servicoCadastrarPlanoAee = ObterServicoSalvarPlanoAEEUseCase();
+            var servicoObterPlanoAEE = ObterServicoObterPlanosAEEUseCase();
+            var filtroPlanoAeeDto = ObterFiltroPlanoAEEDtoTurmaAnoAnterior();
+            var planoAEEPersistenciaDto = ObterPlanoAEEPersistenciaAnoAnteriorDto();
+
+            await servicoCadastrarPlanoAee.Executar(planoAEEPersistenciaDto);
 
             var retorno = await servicoObterPlanoAEE.Executar(filtroPlanoAeeDto);
             retorno.ShouldNotBeNull();
+            var planoAee = retorno.Items.FirstOrDefault();
+            planoAee.Id.ShouldBeGreaterThan(0);
         }
 
-        private FiltroPlanosAEEDto ObterFiltroPlanoAEEDto(SituacaoPlanoAEE situacao = SituacaoPlanoAEE.Validado)
+        //[Fact]
+        //public async Task Deve_pesquisar_por_qualquer_dre_ue_com_usuario_adm_sme()
+        //{
+
+        //}
+
+        [Fact]
+        public async Task Deve_filtrar_por_turma_ano_anterior()
+        {
+            await CriarDadosBasicos(new FiltroPlanoAee()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilProfessor(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio
+            });
+            await CriarTurma(Modalidade.Fundamental, TURMA_ANO_2, TURMA_CODIGO_2, TipoTurma.Regular, true);
+            var filtroPlanoAeeDto = ObterFiltroPlanoAEEDtoTurmaAnoAnterior();
+
+            var servicoCadastrarPlanoAee = ObterServicoSalvarPlanoAEEUseCase();
+            var servicoObterPlanoAEE = ObterServicoObterPlanosAEEUseCase();
+            var planoAEEPersistenciaDto = ObterPlanoAEEPersistenciaAnoAnteriorDto();
+
+            //dto apenas com turma do ano anterior
+            filtroPlanoAeeDto.TurmaId = 2;
+            filtroPlanoAeeDto.Situacao = null;
+
+            await servicoCadastrarPlanoAee.Executar(planoAEEPersistenciaDto);
+            var opa = ObterTodos<SME.SGP.Dominio.PlanoAEE>();
+            var retorno = await servicoObterPlanoAEE.Executar(filtroPlanoAeeDto);
+            retorno.ShouldNotBeNull();
+            retorno.Items.Count().ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Deve_filtrar_por_turma_ano_atual()
+        {
+            await CriarDadosBasicos(new FiltroPlanoAee()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilProfessor(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio
+            });
+
+            var servicoCadastrarPlanoAee = ObterServicoSalvarPlanoAEEUseCase();
+            var servicoObterPlanoAEE = ObterServicoObterPlanosAEEUseCase();
+            var filtroPlanoAeeDto = ObterFiltroPlanoAEEDto();
+            var planoAEEPersistenciaDto = ObterPlanoAEEDto();
+
+            //dto apenas com turma do ano atual
+            filtroPlanoAeeDto.TurmaId = 1;
+            filtroPlanoAeeDto.Situacao = null;
+
+            await servicoCadastrarPlanoAee.Executar(planoAEEPersistenciaDto);
+
+            var retorno = await servicoObterPlanoAEE.Executar(filtroPlanoAeeDto);
+            retorno.ShouldNotBeNull();
+            retorno.Items.Count().ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Deve_filtrar_por_nome_do_estudante()
+        {
+            await CriarDadosBasicos(new FiltroPlanoAee()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilProfessor(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio
+            });
+
+            var servicoCadastrarPlanoAee = ObterServicoSalvarPlanoAEEUseCase();
+            var servicoObterPlanoAEE = ObterServicoObterPlanosAEEUseCase();
+            var filtroPlanoAeeDto = ObterFiltroPlanoAEEDto();
+            var planoAEEPersistenciaDto = ObterPlanoAEEDto();
+
+            //dto apenas com codigo do aluno
+            filtroPlanoAeeDto.TurmaId = 0;
+            filtroPlanoAeeDto.Situacao = null;
+
+            await servicoCadastrarPlanoAee.Executar(planoAEEPersistenciaDto);
+
+            var retorno = await servicoObterPlanoAEE.Executar(filtroPlanoAeeDto);
+            retorno.ShouldNotBeNull();
+            retorno.Items.Count().ShouldBeGreaterThan(0);
+        }
+        [Fact]
+        public async Task Deve_filtrar_por_situacao()
+        {
+            await CriarDadosBasicos(new FiltroPlanoAee()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilProfessor(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio
+            });
+
+            var servicoCadastrarPlanoAee = ObterServicoSalvarPlanoAEEUseCase();
+            var servicoObterPlanoAEE = ObterServicoObterPlanosAEEUseCase();
+            var filtroPlanoAeeDto = ObterFiltroPlanoAEEDto();
+            var planoAEEPersistenciaDto = ObterPlanoAEEDto();
+
+            //dto apenas com situacao
+            filtroPlanoAeeDto.TurmaId = 0;
+            filtroPlanoAeeDto.Situacao = SituacaoPlanoAEE.ParecerCP;
+
+            await servicoCadastrarPlanoAee.Executar(planoAEEPersistenciaDto);
+
+            var retorno = await servicoObterPlanoAEE.Executar(filtroPlanoAeeDto);
+            retorno.ShouldNotBeNull();
+            retorno.Items.Count().ShouldBeGreaterThan(0);
+        }
+
+        private FiltroPlanosAEEDto ObterFiltroPlanoAEEDto(SituacaoPlanoAEE situacao = SituacaoPlanoAEE.ParecerCP)
         {
             return new FiltroPlanosAEEDto()
             {
@@ -57,12 +169,36 @@ namespace SME.SGP.TesteIntegracao.PlanoAEE
             };
         }
 
-        private PlanoAEEPersistenciaDto ObterPlanoAEEPersistenciaDto()
+        private FiltroPlanosAEEDto ObterFiltroPlanoAEEDtoTurmaAnoAnterior(SituacaoPlanoAEE situacao = SituacaoPlanoAEE.ParecerCP)
+        {
+            return new FiltroPlanosAEEDto()
+            {
+                AlunoCodigo = CODIGO_ALUNO_1,
+                DreId = DRE_ID_1,
+                TurmaId = TURMA_ID_2,
+                UeId = UE_ID_1,
+                Situacao = situacao
+            };
+        }
+
+        private PlanoAEEPersistenciaDto ObterPlanoAEEPersistenciaAnoAnteriorDto()
         {
             return new PlanoAEEPersistenciaDto()
             {
                 AlunoCodigo = CODIGO_ALUNO_1,
-                Situacao = SituacaoPlanoAEE.Validado,
+                Situacao = SituacaoPlanoAEE.ParecerCP,
+                TurmaCodigo = TURMA_ID_2.ToString(),
+                TurmaId = TURMA_ID_2,
+                ResponsavelRF = SISTEMA_CODIGO_RF,
+                Questoes = ObterPlanoAeeQuestoes()
+            };
+        }
+        private PlanoAEEPersistenciaDto ObterPlanoAEEDto()
+        {
+            return new PlanoAEEPersistenciaDto()
+            {
+                AlunoCodigo = CODIGO_ALUNO_1,
+                Situacao = SituacaoPlanoAEE.ParecerCP,
                 TurmaCodigo = TURMA_ID_1.ToString(),
                 TurmaId = TURMA_ID_1,
                 ResponsavelRF = SISTEMA_CODIGO_RF,
@@ -75,7 +211,7 @@ namespace SME.SGP.TesteIntegracao.PlanoAEE
             return new List<PlanoAEEQuestaoDto>()
                 { new PlanoAEEQuestaoDto()
                     {   QuestaoId = 2,
-                        Resposta = "teste",
+                        Resposta = "Teste Resposta",
                         RespostaPlanoId = 1,
                         TipoQuestao = TipoQuestao.Frase
                     }
