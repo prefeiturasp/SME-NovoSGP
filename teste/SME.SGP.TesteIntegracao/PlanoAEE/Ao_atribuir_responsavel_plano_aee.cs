455f9a2fd5b15391adc1a55e642965fdc9330d5f
Thiago Fernando <thiago.fernando@msn.com>
2022-09-19 17:17:06 -0300

    criado arquivo de teste Ao_atribuir_responsavel_plano_aee com fact

---
+++ teste/SME.SGP.TesteIntegracao/PlanoAEE/Ao_atribuir_responsavel_plano_aee.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Infra.Dtos;
+using SME.SGP.TesteIntegracao.PlanoAEE.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.PlanoAEE
+{
+    public class Ao_atribuir_responsavel_plano_aee : PlanoAEETesteBase
+    {
+        public Ao_atribuir_responsavel_plano_aee(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+        protected override void RegistrarFakes(IServiceCollection services)
+        {
+            base.RegistrarFakes(services);
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAlunoPorCodigoEAnoQuery, AlunoReduzidoDto>), typeof(ObterAlunoPorCodigoEAnoQueryHandlerFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterParametroSistemaPorTipoEAnoQuery, ParametrosSistema>), typeof(ObterParametroSistemaPorTipoEAnoQueryHanlerFake), ServiceLifetime.Scoped));
+        }
+
+
+        [Fact(DisplayName = "Com o Coordenador do CEFAI realizar atribuição do PAAI.")]
+        public async Task Realizar_atribuicao_paai_com_usuario_cefai()
+        {
+            
+        }
+        
+        [Fact(DisplayName = "Alterar o PAAI atribuído - A pendência deverá ser transferida para o novo PAAI")]
+        public async Task Alterar_o_paai_atribuído()
+        {
+            
+        }
+    }
+}
