8585ead8b2fe79162a5794bb68a23fccf4bf3ca8
DanylloAntunes <danyllo.antunes@gmail.com>
2022-09-20 09:25:33 -0300

    Configuração da base de teste de integração

---
+++ teste/SME.SGP.TesteIntegracao/EncaminhamentoAEE/Ao_Editar_Encaminhamento.cs
@@ -0,0 +1,21 @@
+﻿using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.EncaminhamentoAEE
+{
+    public class Ao_Editar_Encaminhamento : EncaminhamentoAEETesteBase
+    {
+        public Ao_Editar_Encaminhamento(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        public async Task Ao_Editar_Encaminhamento_Rascunho_Professor()
+        {
+
+        }
+    }
+}
---
+++ teste/SME.SGP.TesteIntegracao/EncaminhamentoAEE/Base/EncaminhamentoAEETesteBase.cs
@@ -0,0 +1,70 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.EncaminhamentoAEE
+{
+    public class EncaminhamentoAEETesteBase : TesteBaseComuns
+    {
+        public EncaminhamentoAEETesteBase(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        protected async Task CriarDadosBase(FiltroAEEDto filtro)
+        {
+            await CriarDreUePerfilComponenteCurricular();
+
+            CriarClaimUsuario(filtro.Perfil);
+
+            await CriarUsuarios();
+
+            await CriarTurmaTipoCalendario(filtro);
+
+            if (filtro.CriarPeriodoEscolar)
+                await CriarPeriodoEscolar(filtro.ConsiderarAnoAnterior);
+
+        }
+
+        protected async Task CriarTurmaTipoCalendario(FiltroAEEDto filtro)
+        {
+            await CriarTipoCalendario(filtro.TipoCalendario, filtro.ConsiderarAnoAnterior);
+            await CriarTurma(filtro.Modalidade, filtro.AnoTurma, filtro.ConsiderarAnoAnterior);
+        }
+
+        protected async Task CriarPeriodoEscolar(bool considerarAnoAnterior = false)
+        {
+            await CriarPeriodoEscolar(DATA_03_01_INICIO_BIMESTRE_1, DATA_29_04_FIM_BIMESTRE_1, BIMESTRE_1, TIPO_CALENDARIO_1, considerarAnoAnterior);
+
+            await CriarPeriodoEscolar(DATA_02_05_INICIO_BIMESTRE_2, DATA_08_07_FIM_BIMESTRE_2, BIMESTRE_2, TIPO_CALENDARIO_1, considerarAnoAnterior);
+
+            await CriarPeriodoEscolar(DATA_25_07_INICIO_BIMESTRE_3, DATA_30_09_FIM_BIMESTRE_3, BIMESTRE_3, TIPO_CALENDARIO_1, considerarAnoAnterior);
+
+            await CriarPeriodoEscolar(DATA_03_10_INICIO_BIMESTRE_4, DATA_22_12_FIM_BIMESTRE_4, BIMESTRE_4, TIPO_CALENDARIO_1, considerarAnoAnterior);
+        }
+
+        protected class FiltroAEEDto
+        {
+            public FiltroAEEDto()
+            {
+                TipoCalendarioId = TIPO_CALENDARIO_1;
+                ConsiderarAnoAnterior = false;
+                CriarPeriodoEscolar = true;
+            }
+            public DateTime? DataReferencia { get; set; }
+            public string Perfil { get; set; }
+            public Modalidade Modalidade { get; set; }
+            public ModalidadeTipoCalendario TipoCalendario { get; set; }
+            public int Bimestre { get; set; }
+            public string ComponenteCurricular { get; set; }
+            public long TipoCalendarioId { get; set; }
+            public bool CriarPeriodoEscolar { get; set; }
+            public string AnoTurma { get; set; }
+            public bool ConsiderarAnoAnterior { get; set; }
+            public string ProfessorRf { get; set; }
+        }
+    }
+}
