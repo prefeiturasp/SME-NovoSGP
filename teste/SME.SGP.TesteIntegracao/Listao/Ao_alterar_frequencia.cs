798620b7a54b883a306c691ee3dc785c92065d95
Thiago Fernando <thiago.fernando@msn.com>
2022-09-26 10:53:46 -0300

    Criado Arquivo de teste

---
+++ teste/SME.SGP.TesteIntegracao/Listao/Ao_alterar_frequencia.cs
@@ -0,0 +1,11 @@
+﻿using SME.SGP.TesteIntegracao.Setup;
+
+namespace SME.SGP.TesteIntegracao.Listao
+{
+    public class Ao_alterar_frequencia : ListaoTesteBase
+    {
+        protected Ao_alterar_frequencia(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+    }
+}
