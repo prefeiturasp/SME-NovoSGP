ab76699f7c06b13ecf951b28787f31b17a9fb806
Thiago Fernando <thiago.fernando@msn.com>
2022-09-26 11:11:46 -0300

    Criado Fact dos testes

--- teste/SME.SGP.TesteIntegracao/Listao/Ao_alterar_frequencia.cs
+++ teste/SME.SGP.TesteIntegracao/Listao/Ao_alterar_frequencia.cs
@@ -1,4 +1,6 @@
-﻿using SME.SGP.TesteIntegracao.Setup;
+﻿using System.Threading.Tasks;
+using SME.SGP.TesteIntegracao.Setup;
+using Xunit;
 
 namespace SME.SGP.TesteIntegracao.Listao
 {
@@ -6,6 +8,30 @@ namespace SME.SGP.TesteIntegracao.Listao
     {
         protected Ao_alterar_frequencia(CollectionFixture collectionFixture) : base(collectionFixture)
         {
+        }
+        
+        [Fact(DisplayName = "Listão - Alteração de frequência pelo professor titular.")]
+        public async Task Alteracao_de_frequencia_pelo_professor_titular()
+        {
+            
+        } 
+        
+        [Fact(DisplayName = "Alteração de frequência pelo professor CJ")]
+        public async Task Alteracao_de_frequencia_pelo_professor_cj()
+        {
+            
+        }
+
+        [Fact(DisplayName = "Alteração de frequência pelo CP")]
+        public async Task Alteracao_de_frequencia_pelo_cp()
+        {
+            
+        }
+        
+        [Fact(DisplayName = "Alteração de frequência pelo Diretor")]
+        public async Task Alteracao_de_frequencia_pelo_diretor()
+        {
+            
         }
     }
 }
