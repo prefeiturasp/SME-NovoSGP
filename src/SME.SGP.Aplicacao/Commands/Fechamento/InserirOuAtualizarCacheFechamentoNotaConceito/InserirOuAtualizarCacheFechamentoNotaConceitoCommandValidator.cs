67606e2e26753656da4a2101f2246523ae1a88e4
Fernando Groeler <fernando.groeler@gmail.com>
2022-09-30 13:00:41 -0400

    Ajustada estrutura de classe.

--- src/SME.SGP.Aplicacao/Commands/Fechamento/InserirOuAtualizarCacheFechamentoNotaConceito/InserirOuAtualizarCacheFechamentoNotaConceitoCommandValidator.cs
+++ src/SME.SGP.Aplicacao/Commands/Fechamento/InserirOuAtualizarCacheFechamentoNotaConceito/InserirOuAtualizarCacheFechamentoNotaConceitoCommandValidator.cs
@@ -1,38 +1,42 @@
 using FluentValidation;
 
-namespace SME.SGP.Aplicacao;
-
-public class InserirOuAtualizarCacheFechamentoNotaConceitoCommandValidator : AbstractValidator<InserirOuAtualizarCacheFechamentoNotaConceitoCommand>
+namespace SME.SGP.Aplicacao
 {
-    public InserirOuAtualizarCacheFechamentoNotaConceitoCommandValidator()
+    public class InserirOuAtualizarCacheFechamentoNotaConceitoCommandValidator : AbstractValidator<InserirOuAtualizarCacheFechamentoNotaConceitoCommand>
     {
-        RuleFor(c => c.ComponenteCurricularId)
-            .GreaterThan(0)
-            .WithMessage("O Id do componente curricular deve ser informado para inserir ou atualizar o cache do fechamento.");
+        public InserirOuAtualizarCacheFechamentoNotaConceitoCommandValidator()
+        {
+            RuleFor(c => c.ComponenteCurricularId)
+                .GreaterThan(0)
+                .WithMessage(
+                    "O Id do componente curricular deve ser informado para inserir ou atualizar o cache do fechamento.");
 
-        RuleFor(c => c.TurmaCodigo)
-            .NotNull()
-            .NotEmpty()
-            .WithMessage("O código da turma deve ser informado para inserir ou atualizar o cache do fechamento.");
+            RuleFor(c => c.TurmaCodigo)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("O código da turma deve ser informado para inserir ou atualizar o cache do fechamento.");
 
-        RuleFor(c => c.Bimestre)
-            .InclusiveBetween(0, 4)
-            .When(c => c.Bimestre != null)
-            .WithMessage("Um bimestre válido deve ser informado para inserir ou atualizar o cache do fechamento.");
+            RuleFor(c => c.Bimestre)
+                .InclusiveBetween(0, 4)
+                .When(c => c.Bimestre != null)
+                .WithMessage("Um bimestre válido deve ser informado para inserir ou atualizar o cache do fechamento.");
 
-        RuleFor(c => c.FechamentosNotasConceitos)
-            .NotNull()
-            .WithMessage("As notas ou conceitos devem ser informados para inserir ou atualizar o cache do fechamento.")
-            .DependentRules(() =>
-            {
-                RuleForEach(c => c.FechamentosNotasConceitos)
-                    .ChildRules(c =>
-                    {
-                        c.RuleFor(b => b.CodigoAluno)
-                            .NotEmpty()
-                            .NotNull()
-                            .WithMessage("O código do aluno deve ser informado para inserir ou atualizar o cache de fechamento.");
-                    });
-            });
+            RuleFor(c => c.FechamentosNotasConceitos)
+                .NotNull()
+                .WithMessage(
+                    "As notas ou conceitos devem ser informados para inserir ou atualizar o cache do fechamento.")
+                .DependentRules(() =>
+                {
+                    RuleForEach(c => c.FechamentosNotasConceitos)
+                        .ChildRules(c =>
+                        {
+                            c.RuleFor(b => b.CodigoAluno)
+                                .NotEmpty()
+                                .NotNull()
+                                .WithMessage(
+                                    "O código do aluno deve ser informado para inserir ou atualizar o cache de fechamento.");
+                        });
+                });
+        }
     }
 }
