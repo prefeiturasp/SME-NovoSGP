01ca840e8b2aee7c8ee160f89606afe22eba6005
Fernando Groeler <fernando.groeler@gmail.com>
2022-09-30 08:30:37 -0400

    Criado Command para atualizar o cache do fechamento das notas e conceitos.

---
+++ src/SME.SGP.Infra/Dtos/FechamentoNota/FechamentoNotaConceitoDto.cs
@@ -0,0 +1,9 @@
+namespace SME.SGP.Infra
+{
+    public class FechamentoNotaConceitoDto
+    {
+        public string CodigoAluno { get; set; }
+        public long? ConceitoId { get; set; }
+        public double? Nota { get; set; }
+    }
+}
