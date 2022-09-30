36db80b2a0047cdc6bed9ea3139f85e1751ace63
Fernando Groeler <fernando.groeler@gmail.com>
2022-09-30 13:00:41 -0400

    Ajustada estrutura de classe.

--- src/SME.SGP.Aplicacao/Commands/Fechamento/InserirOuAtualizarCacheFechamentoNotaConceito/InserirOuAtualizarCacheFechamentoNotaConceitoCommand.cs
+++ src/SME.SGP.Aplicacao/Commands/Fechamento/InserirOuAtualizarCacheFechamentoNotaConceito/InserirOuAtualizarCacheFechamentoNotaConceitoCommand.cs
@@ -2,24 +2,25 @@ using System.Collections.Generic;
 using MediatR;
 using SME.SGP.Infra;
 
-namespace SME.SGP.Aplicacao;
-
-public class InserirOuAtualizarCacheFechamentoNotaConceitoCommand : IRequest<bool>
+namespace SME.SGP.Aplicacao
 {
-    public InserirOuAtualizarCacheFechamentoNotaConceitoCommand(long componenteCurricularId,
-        string turmaCodigo, IEnumerable<FechamentoNotaConceitoDto> fechamentosNotasConceitos, 
-        bool emAprovacao, int? bimestre)
+    public class InserirOuAtualizarCacheFechamentoNotaConceitoCommand : IRequest<bool>
     {
-        ComponenteCurricularId = componenteCurricularId;
-        TurmaCodigo = turmaCodigo;
-        FechamentosNotasConceitos = fechamentosNotasConceitos;
-        EmAprovacao = emAprovacao;
-        Bimestre = bimestre;
-    }
+        public InserirOuAtualizarCacheFechamentoNotaConceitoCommand(long componenteCurricularId,
+            string turmaCodigo, IEnumerable<FechamentoNotaConceitoDto> fechamentosNotasConceitos,
+            bool emAprovacao, int? bimestre)
+        {
+            ComponenteCurricularId = componenteCurricularId;
+            TurmaCodigo = turmaCodigo;
+            FechamentosNotasConceitos = fechamentosNotasConceitos;
+            EmAprovacao = emAprovacao;
+            Bimestre = bimestre;
+        }
 
-    public long ComponenteCurricularId { get; }
-    public string TurmaCodigo { get; }
-    public IEnumerable<FechamentoNotaConceitoDto> FechamentosNotasConceitos { get; }
-    public bool EmAprovacao { get; }
-    public int? Bimestre { get; }    
+        public long ComponenteCurricularId { get; }
+        public string TurmaCodigo { get; }
+        public IEnumerable<FechamentoNotaConceitoDto> FechamentosNotasConceitos { get; }
+        public bool EmAprovacao { get; }
+        public int? Bimestre { get; }
+    }
 }
