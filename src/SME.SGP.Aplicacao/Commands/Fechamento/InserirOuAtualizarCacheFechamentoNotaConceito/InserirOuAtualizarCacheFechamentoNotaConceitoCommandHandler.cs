1a56a69a2bb657f4575ad02a532350bbf81bb7e2
Fernando Groeler <fernando.groeler@gmail.com>
2022-09-30 13:00:41 -0400

    Ajustada estrutura de classe.

--- src/SME.SGP.Aplicacao/Commands/Fechamento/InserirOuAtualizarCacheFechamentoNotaConceito/InserirOuAtualizarCacheFechamentoNotaConceitoCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Fechamento/InserirOuAtualizarCacheFechamentoNotaConceito/InserirOuAtualizarCacheFechamentoNotaConceitoCommandHandler.cs
@@ -8,54 +8,61 @@ using SME.SGP.Dominio.Constantes;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 
-namespace SME.SGP.Aplicacao;
-
-public class InserirOuAtualizarCacheFechamentoNotaConceitoCommandHandler : IRequestHandler<InserirOuAtualizarCacheFechamentoNotaConceitoCommand, bool>
+namespace SME.SGP.Aplicacao
 {
-    private readonly IRepositorioCache repositorioCache;
-
-    public InserirOuAtualizarCacheFechamentoNotaConceitoCommandHandler(IRepositorioCache repositorioCache)
+    public class
+        InserirOuAtualizarCacheFechamentoNotaConceitoCommandHandler : IRequestHandler<
+            InserirOuAtualizarCacheFechamentoNotaConceitoCommand, bool>
     {
-        this.repositorioCache = repositorioCache ?? throw new ArgumentNullException(nameof(repositorioCache));
-    }
+        private readonly IRepositorioCache repositorioCache;
 
-    public async Task<bool> Handle(InserirOuAtualizarCacheFechamentoNotaConceitoCommand request, CancellationToken cancellationToken)
-    {
-        var nomeChaveCache = string.Format(NomeChaveCache.CHAVE_FECHAMENTO_NOTA_FINAL_COMPONENTE_TURMA,
-            request.ComponenteCurricularId, request.TurmaCodigo);
-
-        var retornoCacheMapeado = await repositorioCache.ObterObjetoAsync<List<FechamentoNotaAlunoAprovacaoDto>>(nomeChaveCache, "Obter fechamento nota final");
-
-        if (retornoCacheMapeado == null)
-            return false;
-
-        foreach (var fechamentoNotaConceito in request.FechamentosNotasConceitos)
+        public InserirOuAtualizarCacheFechamentoNotaConceitoCommandHandler(IRepositorioCache repositorioCache)
         {
-            var cacheAluno = retornoCacheMapeado.FirstOrDefault(c => c.AlunoCodigo == fechamentoNotaConceito.CodigoAluno && 
-                                                                     c.ComponenteCurricularId == request.ComponenteCurricularId &&
-                                                                     c.Bimestre == request.Bimestre);
-
-            if (cacheAluno == null)
-            {
-                retornoCacheMapeado.Add(new FechamentoNotaAlunoAprovacaoDto
-                {
-                    Bimestre = request.Bimestre,
-                    Nota = fechamentoNotaConceito.Nota,
-                    AlunoCodigo = fechamentoNotaConceito.CodigoAluno,
-                    ConceitoId = fechamentoNotaConceito.ConceitoId,
-                    EmAprovacao = request.EmAprovacao,
-                    ComponenteCurricularId = request.ComponenteCurricularId
-                });
-
-                continue;
-            }
-
-            cacheAluno.Nota = fechamentoNotaConceito.Nota;
-            cacheAluno.ConceitoId = fechamentoNotaConceito.ConceitoId;
-            cacheAluno.EmAprovacao = request.EmAprovacao;
+            this.repositorioCache = repositorioCache ?? throw new ArgumentNullException(nameof(repositorioCache));
         }
 
-        await repositorioCache.SalvarAsync(nomeChaveCache, retornoCacheMapeado);
-        return true;
+        public async Task<bool> Handle(InserirOuAtualizarCacheFechamentoNotaConceitoCommand request,
+            CancellationToken cancellationToken)
+        {
+            var nomeChaveCache = string.Format(NomeChaveCache.CHAVE_FECHAMENTO_NOTA_FINAL_COMPONENTE_TURMA,
+                request.ComponenteCurricularId, request.TurmaCodigo);
+
+            var retornoCacheMapeado =
+                await repositorioCache.ObterObjetoAsync<List<FechamentoNotaAlunoAprovacaoDto>>(nomeChaveCache,
+                    "Obter fechamento nota final");
+
+            if (retornoCacheMapeado == null)
+                return false;
+
+            foreach (var fechamentoNotaConceito in request.FechamentosNotasConceitos)
+            {
+                var cacheAluno = retornoCacheMapeado.FirstOrDefault(c =>
+                    c.AlunoCodigo == fechamentoNotaConceito.CodigoAluno &&
+                    c.ComponenteCurricularId == request.ComponenteCurricularId &&
+                    c.Bimestre == request.Bimestre);
+
+                if (cacheAluno == null)
+                {
+                    retornoCacheMapeado.Add(new FechamentoNotaAlunoAprovacaoDto
+                    {
+                        Bimestre = request.Bimestre,
+                        Nota = fechamentoNotaConceito.Nota,
+                        AlunoCodigo = fechamentoNotaConceito.CodigoAluno,
+                        ConceitoId = fechamentoNotaConceito.ConceitoId,
+                        EmAprovacao = request.EmAprovacao,
+                        ComponenteCurricularId = request.ComponenteCurricularId
+                    });
+
+                    continue;
+                }
+
+                cacheAluno.Nota = fechamentoNotaConceito.Nota;
+                cacheAluno.ConceitoId = fechamentoNotaConceito.ConceitoId;
+                cacheAluno.EmAprovacao = request.EmAprovacao;
+            }
+
+            await repositorioCache.SalvarAsync(nomeChaveCache, retornoCacheMapeado);
+            return true;
+        }
     }
 }
