e8b4e4b9c9f197a7690f11b39c28ab78846f1b6e
robsonamcom <robson.silva@amcom.com.br>
2022-10-06 15:47:54 -0300

    Bug: 76210 Ajustes na consultas de obtenção de notas considerando a disciplina atribuída para nota ao particionar o sequencial dos registros e eleger o último aplicado. Aplicar a funcionalidade de obter último registro de nota atribuída no processo de consolidação para componentes com regência.

--- src/SME.SGP.Dados/Repositorios/RepositorioFechamentoNotaConsulta.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioFechamentoNotaConsulta.cs
@@ -12,18 +12,18 @@ namespace SME.SGP.Dados.Repositorios
 {
     public class RepositorioFechamentoNotaConsulta : RepositorioBase<FechamentoNota>, IRepositorioFechamentoNotaConsulta
     {
-        const string queryPorFechamento = @"select fa.aluno_codigo as AlunoCodigo
+        const string queryPorFechamento = @"with lista as (select fa.aluno_codigo as AlunoCodigo
 	                                        , n.disciplina_id as ComponenteCurricularId
 	                                        , coalesce(wf.nota, n.nota) as Nota
 	                                        , coalesce(wf.conceito_id, n.conceito_id) as ConceitoId
 	                                        , pe.bimestre
 	                                        , wf.id as EmAprovacao
+                                            , row_number() over (partition by ft.turma_id, fa.aluno_codigo, pe.id, n.disciplina_id order by n.id desc) sequencia
                                          from fechamento_nota n
                                         inner join fechamento_aluno fa on fa.id = n.fechamento_aluno_id
                                         inner join fechamento_turma_disciplina ftd on ftd.id = fa.fechamento_turma_disciplina_id 
                                         inner join fechamento_turma ft on ft.id = ftd.fechamento_turma_id 
                                          left join periodo_escolar pe on pe.id = ft.periodo_escolar_id 
-
                                          left join wf_aprovacao_nota_fechamento wf on wf.fechamento_nota_id = n.id
                                         where fa.fechamento_turma_disciplina_id = ANY(@fechamentosTurmaDisciplinaId)";
 
@@ -215,7 +215,7 @@ namespace SME.SGP.Dados.Repositorios
         public Task<IEnumerable<FechamentoNotaAlunoAprovacaoDto>> ObterPorFechamentosTurmaAlunoCodigo(long[] fechamentosTurmaDisciplinaId, string alunoCodigo)
         {
             string query = queryPorFechamento;
-            query += @" and fa.aluno_codigo = @alunoCodigo";
+            query += @" and fa.aluno_codigo = @alunoCodigo) select * from lista where sequencia = 1;";
             return database.Conexao.QueryAsync<FechamentoNotaAlunoAprovacaoDto>(query, new { fechamentosTurmaDisciplinaId, alunoCodigo });
         }
     }
