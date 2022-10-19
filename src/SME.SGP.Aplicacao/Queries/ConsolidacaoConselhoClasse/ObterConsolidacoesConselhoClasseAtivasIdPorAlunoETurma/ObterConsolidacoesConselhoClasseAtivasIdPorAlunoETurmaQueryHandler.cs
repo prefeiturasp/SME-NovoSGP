dc86c8d1f8eb861008fbc1ba4d09a109e1309834
Helio Barros <helio.barros@amcom.com.br>
2022-10-19 16:51:31 -0300

    AB#77243 - correção da rotina de consolidação do total de alunos de conselho para o dashboard (bug: 77073)

---
+++ src/SME.SGP.Aplicacao/Queries/ConsolidacaoConselhoClasse/ObterConsolidacoesConselhoClasseAtivasIdPorAlunoETurma/ObterConsolidacoesConselhoClasseAtivasIdPorAlunoETurmaQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterConsolidacoesConselhoClasseAtivasIdPorAlunoETurmaQueryHandler : IRequestHandler<ObterConsolidacoesConselhoClasseAtivasIdPorAlunoETurmaQuery, IEnumerable<long>>
+    {
+        private readonly IRepositorioConselhoClasseConsolidado repositorioConsolidacaoConselhoClasse;
+
+        public ObterConsolidacoesConselhoClasseAtivasIdPorAlunoETurmaQueryHandler(IRepositorioConselhoClasseConsolidado repositorioConsolidacaoConselhoClasse)
+        {
+            this.repositorioConsolidacaoConselhoClasse = repositorioConsolidacaoConselhoClasse ?? throw new ArgumentNullException(nameof(repositorioConsolidacaoConselhoClasse));
+        }
+        public async Task<IEnumerable<long>> Handle(ObterConsolidacoesConselhoClasseAtivasIdPorAlunoETurmaQuery request, CancellationToken cancellationToken)
+          => await repositorioConsolidacaoConselhoClasse.ObterConsolidacoesAtivasIdPorAlunoETurmaAsync(request.CodigoAluno, request.TurmaId);
+    }
+}
--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConselhoClasseConsolidado.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConselhoClasseConsolidado.cs
@@ -8,9 +8,12 @@ namespace SME.SGP.Dominio
     public interface IRepositorioConselhoClasseConsolidado : IRepositorioBase<ConselhoClasseConsolidadoTurmaAluno>
     {
         Task<IEnumerable<ConselhoClasseConsolidadoTurmaAluno>> ObterConselhosClasseConsolidadoPorTurmaBimestreAsync(long turmaId, int situacaoConselhoClasse);
-        Task<ConselhoClasseConsolidadoTurmaAluno> ObterConselhoClasseConsolidadoPorTurmaBimestreAlunoAsync(long turmaId, string alunoCodigo);        
-        
+        Task<ConselhoClasseConsolidadoTurmaAluno> ObterConselhoClasseConsolidadoPorTurmaBimestreAlunoAsync(long turmaId, string alunoCodigo);
+        Task<IEnumerable<long>> ObterConsolidacoesAtivasIdPorAlunoETurmaAsync(string alunoCodigo, long turmaId);
+
+
         Task<IEnumerable<ConsolidacaoConselhoClasseAlunoMigracaoDto>> ObterFechamentoNotaAlunoOuConselhoClasseAsync(long turmaId);
         Task<long> ObterConselhoClasseConsolidadoPorTurmaAlunoAsync(long turmaId, string alunoCodigo);
+        Task<bool> ExcluirLogicamenteConsolidacaoConselhoClasseAlunoTurmaPorIdConsolidacao(long[] idsConsolidacoes);
     }
 }
