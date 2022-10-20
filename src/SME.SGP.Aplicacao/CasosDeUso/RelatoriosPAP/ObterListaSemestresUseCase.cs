e66f8d4e415b3477e2e0d49fc838a28b1d40a7b3
Helio Barros <helio.barros@amcom.com.br>
2022-10-20 16:09:24 -0300

    AB#77337 - ajuste na tela de consulta e de inserção do relatório PAP para verificar se há reabertura para permitir inserir informações. (bug: 77269)

--- src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/ObterListaSemestresUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/ObterListaSemestresUseCase.cs
@@ -4,6 +4,7 @@ using SME.SGP.Dominio;
 using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
@@ -25,7 +26,34 @@ namespace SME.SGP.Aplicacao
 
             var bimestreAtual = await mediator.Send(new ObterBimestreAtualQuery(DateTime.Today, turma));
 
-            return await mediator.Send(new ObterListaSemestresRelatorioPAPQuery(bimestreAtual));
+            var semestres = await mediator.Send(new ObterListaSemestresRelatorioPAPQuery(bimestreAtual));
+            var tipoCalendarioTurma = await mediator.Send(new ObterTipoCalendarioIdPorTurmaQuery(turma));
+
+            if(tipoCalendarioTurma > 0)
+            {
+                var periodoFechamentoReaberturaVigente = await mediator.Send(new ObterFechamentoReaberturaPorDataTurmaQuery() { DataParaVerificar = DateTime.Now.Date, TipoCalendarioId = tipoCalendarioTurma, UeId = turma.UeId });
+
+                if (periodoFechamentoReaberturaVigente != null)
+                {
+                    var bimestresComReabertura = await mediator.Send(new ObterFechamentoReaberturaBimestrePorFechamentoReaberturaIdQuery(periodoFechamentoReaberturaVigente.Id));
+
+                    if (bimestresComReabertura.Any())
+                    {
+                        var semestresAjustadosComReabertura = semestres.Select(s => new SemestreAcompanhamentoDto()
+                        {
+                            Descricao = s.Descricao,
+                            Semestre = s.Semestre,
+                            PodeEditar = s.Semestre == 1 && bimestresComReabertura.Any(b => b.Bimestre == 2) ? true
+                       : s.Semestre == 2 && bimestresComReabertura.Any(b => b.Bimestre == 4) ? true : false
+                        });
+
+                        return semestresAjustadosComReabertura.ToList();
+                    }           
+                }
+            }
+
+            return semestres;
         }
     }
+            
 }
