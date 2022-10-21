e142918199833a23973ea1447fa4fa5423d3351b
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-10-21 15:47:55 -0300

    Correção dos namespaces para net 5

--- src/SME.SGP.Aplicacao/Queries/ObterPerfilDoToken/ObterPerfilDoTokenQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/ObterPerfilDoToken/ObterPerfilDoTokenQueryHandler.cs
@@ -8,32 +8,33 @@ using System.Threading.Tasks;
 using MediatR;
 using SME.SGP.Infra.Interfaces;
 
-namespace SME.SGP.Aplicacao;
-
-public class ObterPerfilDoTokenQueryHandler : IRequestHandler<ObterPerfilDoTokenQuery, Guid>
+namespace SME.SGP.Aplicacao
 {
-    private readonly IContextoAplicacao contextoAplicacao;
+    public class ObterPerfilDoTokenQueryHandler : IRequestHandler<ObterPerfilDoTokenQuery, Guid>
+    {
+        private readonly IContextoAplicacao contextoAplicacao;
 
-    public ObterPerfilDoTokenQueryHandler(IContextoAplicacao contextoAplicacao)
-    {
-        this.contextoAplicacao = contextoAplicacao ?? throw new ArgumentNullException(nameof(contextoAplicacao));
-    }
+        public ObterPerfilDoTokenQueryHandler(IContextoAplicacao contextoAplicacao)
+        {
+            this.contextoAplicacao = contextoAplicacao ?? throw new ArgumentNullException(nameof(contextoAplicacao));
+        }
 
-    public async Task<Guid> Handle(ObterPerfilDoTokenQuery request, CancellationToken cancellationToken)
-    {
-        return await Task.FromResult(Guid.Parse(
-            ObterClaims(ObterTokenAtual()).FirstOrDefault(claim => claim.Type == "perfil")?.Value ?? string.Empty));
+        public async Task<Guid> Handle(ObterPerfilDoTokenQuery request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Guid.Parse(
+                ObterClaims(ObterTokenAtual()).FirstOrDefault(claim => claim.Type == "perfil")?.Value ?? string.Empty));
+        }
+    
+        private static IEnumerable<Claim> ObterClaims(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+            return tokenS?.Claims;
+        }    
+    
+        private string ObterTokenAtual()
+        {
+            return contextoAplicacao.ObterVariavel<string>("TokenAtual");
+        }    
     }
-    
-    private static IEnumerable<Claim> ObterClaims(string token)
-    {
-        var handler = new JwtSecurityTokenHandler();
-        var tokenS = handler.ReadToken(token) as JwtSecurityToken;
-        return tokenS?.Claims;
-    }    
-    
-    private string ObterTokenAtual()
-    {
-        return contextoAplicacao.ObterVariavel<string>("TokenAtual");
-    }    
 }
