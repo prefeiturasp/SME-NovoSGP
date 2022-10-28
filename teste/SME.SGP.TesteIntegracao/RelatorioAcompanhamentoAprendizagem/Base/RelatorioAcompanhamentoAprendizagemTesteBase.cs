c8bd5768c86a8882455dd0d904197de521695618
Thiago Fernando <thiago.fernando@msn.com>
2022-10-28 07:10:09 -0300

    Ao_Validar_informacoes_Ocorrencias_frequencia

--- teste/SME.SGP.TesteIntegracao/RelatorioAcompanhamentoAprendizagem/Base/RelatorioAcompanhamentoAprendizagemTesteBase.cs
+++ teste/SME.SGP.TesteIntegracao/RelatorioAcompanhamentoAprendizagem/Base/RelatorioAcompanhamentoAprendizagemTesteBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
@@ -17,6 +18,7 @@ namespace SME.SGP.TesteIntegracao.RelatorioAcompanhamentoAprendizagem
         private const string PARAMETRO_QUANTIDADE_IMAGENS_PERCURSO_TURMA_NOME = "QuantidadeImagensPercursoTurma";
         private const string PARAMETRO_QUANTIDADE_IMAGENS_PERCURSO_TURMA_DESCRICAO = "Quantidade de Imagens Permitidas na Seção Percurso Coletivo da Turma";
         private const string PARAMETRO_QUANTIDADE_IMAGENS_PERCURSO_TURMA_VALOR = "2";
+        private const int QUANTIDADE_3 = 3;
         
         private const string PARAMETRO_QUANTIDADE_IMAGENS_PERCURSO_INDIVIDUAL_CRIANCA_NOME = "QuantidadeImagensPercursoIndividualCrianca";
         private const string PARAMETRO_QUANTIDADE_IMAGENS_PERCURSO_INDIVIDUAL_CRIANCA_DESCRICAO = "Quantidade de Imagens permitiras no percurso individual da criança";
@@ -39,8 +41,49 @@ namespace SME.SGP.TesteIntegracao.RelatorioAcompanhamentoAprendizagem
         protected ISalvarAcompanhamentoTurmaUseCase SalvarAcompanhamentoTurmaUseCase()
         {
             return ServiceProvider.GetService<ISalvarAcompanhamentoTurmaUseCase>();
+        }        
+        protected IObterOcorrenciasPorAlunoUseCase ObterOcorrenciasPorAlunoUseCase()
+        {
+            return ServiceProvider.GetService<IObterOcorrenciasPorAlunoUseCase>();
+        }        
+        protected IObterAcompanhamentoAlunoUseCase ObterAcompanhamentoAlunoUseCase()
+        {
+            return ServiceProvider.GetService<IObterAcompanhamentoAlunoUseCase>();
+        }        
+        protected IObterInformacoesDeFrequenciaAlunoPorSemestreUseCase ObterInformacoesDeFrequenciaAlunoPorSemestreUseCase()
+        {
+            return ServiceProvider.GetService<IObterInformacoesDeFrequenciaAlunoPorSemestreUseCase>();
         }
 
+        protected async Task CriarAula(string componenteCurricularCodigo, DateTime dataAula, RecorrenciaAula recorrencia, string rf = USUARIO_PROFESSOR_LOGIN_2222222)
+        {
+            await InserirNaBase(ObterAula(componenteCurricularCodigo, dataAula, recorrencia, rf));
+        }
+        private Dominio.Aula ObterAula(string componenteCurricularCodigo, DateTime dataAula, RecorrenciaAula recorrencia, string rf = USUARIO_PROFESSOR_LOGIN_2222222)
+        {
+            return new Dominio.Aula
+            {
+                UeId = UE_CODIGO_1,
+                DisciplinaId = componenteCurricularCodigo,
+                TurmaId = TURMA_CODIGO_1,
+                TipoCalendarioId = 1,
+                ProfessorRf = rf,
+                Quantidade = QUANTIDADE_3,
+                DataAula = dataAula,
+                RecorrenciaAula = recorrencia,
+                TipoAula = TipoAula.Normal,
+                CriadoEm = DateTime.Now,
+                CriadoPor = SISTEMA_NOME,
+                CriadoRF = SISTEMA_CODIGO_RF,
+                Excluido = false,
+                Migrado = false,
+                AulaCJ = false
+            };
+        }
+        protected IInserirOcorrenciaUseCase InserirOcorrenciaUseCase()
+        {
+            return ServiceProvider.GetService<IInserirOcorrenciaUseCase>();
+        }
         protected async Task CriarDadosBasicos(bool abrirPeriodos = true)
         {
             await CriarDreUePerfil();
@@ -52,6 +95,7 @@ namespace SME.SGP.TesteIntegracao.RelatorioAcompanhamentoAprendizagem
             await CriarUsuarios();
             await CriarTurma(Modalidade.EducacaoInfantil);
             await CriarParametrosSistema();
+            await CriarOcorrenciaTipo();
         }
         
         private async Task CriarPeriodoEscolarTodosBimestres()
@@ -60,8 +104,19 @@ namespace SME.SGP.TesteIntegracao.RelatorioAcompanhamentoAprendizagem
             await CriarPeriodoEscolar(DATA_02_05_INICIO_BIMESTRE_2, DATA_08_07_FIM_BIMESTRE_2, BIMESTRE_2);
             await CriarPeriodoEscolar(DATA_25_07_INICIO_BIMESTRE_3, DATA_30_09_FIM_BIMESTRE_3, BIMESTRE_3);
             await CriarPeriodoEscolar(DATA_03_10_INICIO_BIMESTRE_4, DATA_22_12_FIM_BIMESTRE_4, BIMESTRE_4);
-        }        
-        
+        }
+
+        private async Task CriarOcorrenciaTipo()
+        {
+            await InserirNaBase(new OcorrenciaTipo
+            {
+                Descricao = "Descricao Da Ocorrencia",
+                Excluido = false,
+                CriadoPor = "Sistema",
+                CriadoEm = DateTime.Now,
+                CriadoRF = "1"
+            });
+        }
         private async Task CriarParametrosSistema()
         {
             await InserirNaBase(new ParametrosSistema
