928a8086e7a2823f877e200e0074182dfb355d95
DanylloAntunes <danyllo.antunes@gmail.com>
2022-11-01 16:55:22 -0300

    78007 - Alterações BD - ocorrencia_servidor

---
+++ src/SME.SGP.Dados/Mapeamentos/OcorrenciaServidorMap.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class OcorrenciaServidorMap : DommelEntityMap<OcorrenciaServidor>
+    {
+        public OcorrenciaServidorMap()
+        {
+            ToTable("ocorrencia_servidor");
+            Map(c => c.Id).ToColumn("id").IsIdentity().IsKey();
+            Map(c => c.CodigoRf).ToColumn("rf_codigo");
+            Map(c => c.OcorrenciaId).ToColumn("ocorrencia_id");
+        }
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/OcorrenciaServidor.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.SGP.Dominio
+{ 
+    public class OcorrenciaServidor : EntidadeBase
+    {
+        public string CodigoRf { get; set; }
+        public Ocorrencia Ocorrencia { get; set; }
+        public long OcorrenciaId { get; set; }
+    }
+}
