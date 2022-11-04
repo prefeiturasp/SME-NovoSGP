da0b52e41a553e206f35decb264ab116f56f423f
Thiago Fernando <thiago.fernando@msn.com>
2022-11-04 14:10:54 -0300

    excluir com ue servidor

--- teste/SME.SGP.TesteIntegracao/Ocorrencia/Ao_excluir_ocorrencia.cs
+++ teste/SME.SGP.TesteIntegracao/Ocorrencia/Ao_excluir_ocorrencia.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Shouldly;
 using SME.SGP.Dominio;
@@ -66,6 +67,69 @@ namespace SME.SGP.TesteIntegracao.Ocorrencia
                 Descricao = "Lorem Ipsum é simplesmente uma simulação de texto da",
                 OcorrenciaTipoId = 1,
                 HoraOcorrencia = "17:34",
+            };
+            var useCaseIncluir = InserirOcorrenciaUseCase();
+            await useCaseIncluir.Executar(dtoIncluir);
+            
+            var obterTodasOcorrencias = ObterTodos<Dominio.Ocorrencia>();
+            var useCaseExcluir = ExcluirOcorrenciaUseCase();
+            await useCaseExcluir.Executar(obterTodasOcorrencias.Select(x => x.Id));
+
+            var obterTodasOcorrenciasAposExcluir = ObterTodos<Dominio.Ocorrencia>();
+            obterTodasOcorrenciasAposExcluir.ShouldNotBeNull();
+            obterTodasOcorrenciasAposExcluir.Count(x => !x.Excluido).ShouldBeEquivalentTo(0);
+        }
+
+        [Fact(DisplayName = "Ocorrencia - Excluir Ocorrencia Com Turma e Aluno")]
+        public async Task ExcluirOcorrenciaComTurmaEAluno()
+        {
+            await CriarDadosBasicos();
+            var dtoIncluir = new InserirOcorrenciaDto
+            {
+                AnoLetivo = DateTimeExtension.HorarioBrasilia().Year,
+                DreId = 1,
+                UeId = 1,
+                Modalidade = 5,
+                Semestre = 3,
+                TurmaId = 1,
+                DataOcorrencia = DateTimeExtension.HorarioBrasilia(),
+                Titulo = "Lorem ipsum",
+                Descricao = "Lorem Ipsum é simplesmente uma simulação de texto da",
+                OcorrenciaTipoId = 1,
+                HoraOcorrencia = "17:34",
+                CodigosAlunos = new List<long>(){1,2}
+            };
+            var useCaseIncluir = InserirOcorrenciaUseCase();
+            await useCaseIncluir.Executar(dtoIncluir);
+            
+            var obterTodasOcorrencias = ObterTodos<Dominio.Ocorrencia>();
+            var useCaseExcluir = ExcluirOcorrenciaUseCase();
+            await useCaseExcluir.Executar(obterTodasOcorrencias.Select(x => x.Id));
+
+            var obterTodasOcorrenciasAposExcluir = ObterTodos<Dominio.Ocorrencia>();
+            obterTodasOcorrenciasAposExcluir.ShouldNotBeNull();
+            obterTodasOcorrenciasAposExcluir.Count(x => !x.Excluido).ShouldBeEquivalentTo(0);
+        }
+        
+        
+        [Fact(DisplayName = "Ocorrencia - Excluir Ocorrencia Com Ue e Servidor")]
+        public async Task ExcluirOcorrenciaComUeEServidor()
+        {
+            await CriarDadosBasicos();
+            var dtoIncluir = new InserirOcorrenciaDto
+            {
+                AnoLetivo = DateTimeExtension.HorarioBrasilia().Year,
+                DreId = 1,
+                UeId = 1,
+                Modalidade = 5,
+                Semestre = 3,
+                TurmaId = 1,
+                DataOcorrencia = DateTimeExtension.HorarioBrasilia(),
+                Titulo = "Lorem ipsum",
+                Descricao = "Lorem Ipsum é simplesmente uma simulação de texto da",
+                OcorrenciaTipoId = 1,
+                HoraOcorrencia = "17:34",
+                CodigosServidores = new List<string>(){"rf1","rf2"}
             };
             var useCaseIncluir = InserirOcorrenciaUseCase();
             await useCaseIncluir.Executar(dtoIncluir);
