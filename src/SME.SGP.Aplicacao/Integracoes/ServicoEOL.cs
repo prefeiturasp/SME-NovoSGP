6a7fa9cb2a8bec045a6630b96507c89645aff7a6
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2022-11-04 13:32:12 -0300

    BUG 78188 ajuste para obter a descricao do territorio do Saber que vem do EOL

--- src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
@@ -948,12 +948,13 @@ namespace SME.SGP.Aplicacao.Integracoes
             return JsonConvert.DeserializeObject<bool>(json);
         }
 
-        public async Task<IEnumerable<DisciplinaDto>> ObterDisciplinasPorIdsAgrupadas(long[] ids)
+        public async Task<IEnumerable<DisciplinaDto>> ObterDisciplinasPorIdsAgrupadas(long[] ids,string codigoTurma = null)
         {
             var parametros = JsonConvert.SerializeObject(ids);
 
-            var resposta = await httpClient.PostAsync("disciplinas", new StringContent(parametros, Encoding.UTF8, "application/json-patch+json"));
-
+            var url = $@"disciplinas/turma/{codigoTurma}";
+            var resposta = await httpClient.PostAsync(url, new StringContent(parametros, Encoding.UTF8, "application/json-patch+json"));
+            
             if (!resposta.IsSuccessStatusCode || resposta.StatusCode == HttpStatusCode.NoContent)
             {
                 await RegistrarLogAsync(resposta, "obter as disciplinas", parametros);
