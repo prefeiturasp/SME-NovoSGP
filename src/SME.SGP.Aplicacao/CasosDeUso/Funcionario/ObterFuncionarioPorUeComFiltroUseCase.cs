4781d66a0dda5babdf1ea61c02189d2ff680f44b
DanylloAntunes <danyllo.antunes@gmail.com>
2022-11-04 09:56:07 -0300

    78446 - Endpoint retorno dos servidores

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Funcionario/ObterFuncionarioPorUeComFiltroUseCase.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterFuncionarioPorUeComFiltroUseCase : AbstractUseCase, IObterFuncionarioPorUeComFiltroUseCase
+    {
+        public ObterFuncionarioPorUeComFiltroUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<IEnumerable<UsuarioEolRetornoDto>> Executar((string codigoUe, string filtro) param)
+        {
+            var servidores = await mediator.Send(new ObterFuncionariosPorUeQuery(param.codigoUe));
+
+            if (!string.IsNullOrEmpty(param.filtro))
+                return servidores.Where(servidor => servidor.NomeServidor.IndexOf(param.filtro, StringComparison.OrdinalIgnoreCase) != -1 ||
+                                                    servidor.CodigoRf.IndexOf(param.filtro, StringComparison.OrdinalIgnoreCase) != -1);
+
+            return servidores;
+        }
+    }
+}
