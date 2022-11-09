a22dc178ffc7a5176b3238591b1a15224043df2b
Helio Barros <helio.barros@amcom.com.br>
2022-11-09 13:56:37 -0300

    AB#78776 - ajuste para possibilitar consultar se caso não encontrar atribuições de professor, verificar se o mesmo é CJ e tiver atribuição ativa para a mesma UE informada no filtro

--- src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
+++ src/SME.SGP.Aplicacao/Integracoes/ServicoEOL.cs
@@ -624,7 +624,20 @@ namespace SME.SGP.Aplicacao.Integracoes
                 throw new NegocioException("Ocorreu uma falha ao consultar o professor");
 
             if (resposta.StatusCode == HttpStatusCode.NoContent)
-                throw new NegocioException($"Não foi encontrado professor com RF {codigoRF}");
+            {
+                var dadosUsuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+
+                if (dadosUsuarioLogado.EhProfessorCj())
+                {
+                    var obterAtribuicoesCJAtivas = await mediator.Send(new ObterAtribuicoesCJAtivasQuery(codigoRF));
+                    bool possuiAtribuicaoNaUE = obterAtribuicoesCJAtivas.Any(a => a.UeId == ueId);
+
+                    if (possuiAtribuicaoNaUE)
+                        return new ProfessorResumoDto() { CodigoRF = codigoRF, Nome = dadosUsuarioLogado.Nome, UsuarioId = dadosUsuarioLogado.Id };                           
+                }
+                else
+                    throw new NegocioException($"Não foi encontrado professor com RF {codigoRF}");
+            }
 
             var json = await resposta.Content.ReadAsStringAsync();
 
