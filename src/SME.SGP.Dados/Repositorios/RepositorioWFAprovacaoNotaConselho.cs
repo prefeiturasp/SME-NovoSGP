a1c51167c613b17bdc8c2251d3c8354da9e17003
DanylloAntunes <danyllo.antunes@gmail.com>
2022-11-10 08:31:46 -0300

    62111 - Worker de aprovação de conselho classe

--- src/SME.SGP.Dados/Repositorios/RepositorioWFAprovacaoNotaConselho.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioWFAprovacaoNotaConselho.cs
@@ -16,23 +16,30 @@ namespace SME.SGP.Dados
         {
         }
 
+        public async Task<IEnumerable<WFAprovacaoNotaConselho>> ObterNotaAguardandoAprovacaoPorWorkflow()
+        {
+            var query = ObterQueryNotaEmAprovacaoPorWorkflow();
+            query += " where nwf.wf_aprovacao_id is null";
+
+            return await database.Conexao
+                .QueryAsync<WFAprovacaoNotaConselho, ConselhoClasseNota, ConselhoClasseAluno, ConselhoClasse, FechamentoTurma, Turma, PeriodoEscolar, WFAprovacaoNotaConselho>(query,
+                (wfAprovacao, conselhoClasseNota, conselhoClasseAluno, conselhoClasse, fechamentoTurma, turma, periodoEscolar) =>
+                {
+                    fechamentoTurma.Turma = turma;
+                    conselhoClasse.FechamentoTurma = fechamentoTurma;
+                    conselhoClasse.FechamentoTurma.PeriodoEscolar = periodoEscolar;
+                    conselhoClasseAluno.ConselhoClasse = conselhoClasse;
+                    conselhoClasseNota.ConselhoClasseAluno = conselhoClasseAluno;
+                    wfAprovacao.ConselhoClasseNota = conselhoClasseNota;
+
+                    return wfAprovacao;
+                });
+        }
+
         public async Task<WFAprovacaoNotaConselho> ObterNotaEmAprovacaoPorWorkflow(long workflowId)
         {
-            var query = @"select nwf.*
-	                    , ccn.*
-	                    , cca.*
-	                    , cc.*
-	                    , ft.*
-	                    , t.*
-                        , pe.*
-                      from wf_aprovacao_nota_conselho nwf
-                     inner join conselho_classe_nota ccn on ccn.id = nwf.conselho_classe_nota_id
-                     inner join conselho_classe_aluno cca on cca.id = ccn.conselho_classe_aluno_id 
-                     inner join conselho_classe cc on cc.id = cca.conselho_classe_id 
-                     inner join fechamento_turma ft on ft.id = cc.fechamento_turma_id 
-                     inner join turma t on t.id = ft.turma_id 
-                     inner join periodo_escolar pe on ft.periodo_escolar_id = pe.id 
-                     where nwf.wf_aprovacao_id = @workflowId";
+            var query = ObterQueryNotaEmAprovacaoPorWorkflow();
+            query += " where nwf.wf_aprovacao_id = @workflowId";
 
             return (await database.Conexao
                 .QueryAsync<WFAprovacaoNotaConselho, ConselhoClasseNota, ConselhoClasseAluno, ConselhoClasse, FechamentoTurma, Turma, PeriodoEscolar, WFAprovacaoNotaConselho>(query,
@@ -56,6 +63,24 @@ namespace SME.SGP.Dados
             var query = @"select * from wf_aprovacao_nota_conselho where conselho_classe_nota_id = @conselhoClasseNotaId";
 
             return await database.Conexao.QueryAsync<WFAprovacaoNotaConselho>(query, new { conselhoClasseNotaId });
+        }
+
+        private string ObterQueryNotaEmAprovacaoPorWorkflow()
+        {
+            return @"select nwf.*
+	                    , ccn.*
+	                    , cca.*
+	                    , cc.*
+	                    , ft.*
+	                    , t.*
+                        , pe.*
+                      from wf_aprovacao_nota_conselho nwf
+                     inner join conselho_classe_nota ccn on ccn.id = nwf.conselho_classe_nota_id
+                     inner join conselho_classe_aluno cca on cca.id = ccn.conselho_classe_aluno_id 
+                     inner join conselho_classe cc on cc.id = cca.conselho_classe_id 
+                     inner join fechamento_turma ft on ft.id = cc.fechamento_turma_id 
+                     inner join turma t on t.id = ft.turma_id 
+                     inner join periodo_escolar pe on ft.periodo_escolar_id = pe.id ";
         }
     }
 }
