0cb9cfe49e73682a785791866a086343cfc6e057
jailsvs <jailsvs@gmail.com>
2022-11-18 09:37:38 -0300

    Ajuste compilação classe

--- src/SME.SGP.Dados/Repositorios/RepositorioQuestaoEncaminhamentoNAAPA.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioQuestaoEncaminhamentoNAAPA.cs
@@ -12,4 +12,5 @@ namespace SME.SGP.Dados.Repositorios
         public RepositorioQuestaoEncaminhamentoNAAPA(ISgpContext repositorio, IServicoAuditoria servicoAuditoria) : base(repositorio, servicoAuditoria)
         {
         }
+    }
 }
--- src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoNAAPA.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoNAAPA.cs
@@ -12,4 +12,5 @@ namespace SME.SGP.Dados.Repositorios
         public RepositorioRespostaEncaminhamentoNAAPA(ISgpContext database, IServicoAuditoria servicoAuditoria) : base(database, servicoAuditoria)
         {
         }
+    }
 }
