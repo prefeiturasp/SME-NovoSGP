a9a7f59ba9c19b017172dc461c949e1bde5cbe5e
jailsvs <jailsvs@gmail.com>
2022-11-18 08:10:46 -0300

    78834 - Controller e Repositories

---
+++ src/SME.SGP.Api/Controllers/EncaminhamentoNAAPAController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/encaminhamento-naapa")]
+    [Authorize("Bearer")]
+    public class EncaminhamentoNAAPAController : ControllerBase
+    {  }
+}
---
+++ src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoNAAPA.cs
@@ -0,0 +1,15 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioRespostaEncaminhamentoNAAPA : RepositorioBase<RespostaEncaminhamentoNAAPA>, IRepositorioRespostaEncaminhamentoNAAPA
+    {
+        public RepositorioRespostaEncaminhamentoNAAPA(ISgpContext database, IServicoAuditoria servicoAuditoria) : base(database, servicoAuditoria)
+        {
+        }
+}
