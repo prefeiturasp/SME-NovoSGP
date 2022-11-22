dd84e14b28154b15464a462db7aaa9616de4f508
jailsvs <jailsvs@gmail.com>
2022-11-22 13:32:36 -0300

    79659 - Upload e Exclusão de arquivos/anexo encaminhamento NAAPA

--- src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoNAAPA.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioRespostaEncaminhamentoNAAPA.cs
@@ -19,5 +19,31 @@ namespace SME.SGP.Dados.Repositorios
 
             return await database.Conexao.QueryAsync<RespostaEncaminhamentoNAAPA>(query, new { questaoEncaminhamentoNAAPAId });
         }
+
+        public async Task<bool> RemoverPorArquivoId(long arquivoId)
+        {
+            await Task.CompletedTask;
+
+            var sql =
+                $@"
+					update encaminhamento_naapa_resposta 
+                        set excluido = true,
+                            arquivo_id = null
+					where arquivo_id = @arquivoId 
+                ";
+
+            return (
+                database
+                .Conexao
+                .Execute(sql, new { arquivoId })
+                ) > 0;
+        }
+
+        public async Task<IEnumerable<long>> ObterArquivosPorQuestaoId(long questaoEncaminhamentoNAAPAId)
+        {
+            var query = "select arquivo_id from encaminhamento_naapa_resposta where questao_encaminhamento_id = @questaoEncaminhamentoNAAPAId and arquivo_id is not null";
+
+            return await database.Conexao.QueryAsync<long>(query, new { questaoEncaminhamentoNAAPAId });
+        }
     }
 }
--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRespostaEncaminhamentoNAAPA.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioRespostaEncaminhamentoNAAPA.cs
@@ -7,6 +7,8 @@ namespace SME.SGP.Dominio.Interfaces
 {
     public interface IRepositorioRespostaEncaminhamentoNAAPA : IRepositorioBase<RespostaEncaminhamentoNAAPA>
     {
+        Task<bool> RemoverPorArquivoId(long arquivoId);
         Task<IEnumerable<RespostaEncaminhamentoNAAPA>> ObterPorQuestaoEncaminhamentoId(long requestQuestaoEncaminhamentoNaapaId);
+        Task<IEnumerable<long>> ObterArquivosPorQuestaoId(long questaoEncaminhamentoAEEId);
     }
 }
