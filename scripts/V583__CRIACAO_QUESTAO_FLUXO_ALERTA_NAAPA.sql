d0974cdf6dbef86b56899d53a49dba0dbd8fa478
Rafael Losi <rafael.losi@amcom.com.br>
2022-11-24 18:45:58 -0300

    ajuste encoding

--- scripts/V583__CRIACAO_QUESTAO_FLUXO_ALERTA_NAAPA.sql
+++ scripts/V583__CRIACAO_QUESTAO_FLUXO_ALERTA_NAAPA.sql
@@ -1,4 +1,4 @@
-do $$
+ï»¿do $$
 declare 
 	questionarioId bigint;
 	questaoId bigint;
@@ -11,15 +11,15 @@ begin
 	update questao set ordem = 17 where questionario_id = questionarioId and ordem = 16;
 
 	insert into questao(questionario_id, ordem, nome, placeholder, observacao, obrigatorio, tipo, opcionais, criado_em, criado_por, criado_rf, dimensao, tamanho, mascara)
-		values(questionarioId, 16, 'Aplicação do fluxo de alerta', '', '', true, 9, '', NOW(), 'SISTEMA', '0', 12, null, null)
+		values(questionarioId, 16, 'AplicaÃ§Ã£o do fluxo de alerta', '', '', true, 9, '', NOW(), 'SISTEMA', '0', 12, null, null)
 		RETURNING id INTO questaoId;
 
 	insert into opcao_resposta (questao_id, ordem, nome, criado_em, criado_por, criado_rf)
-		values(questaoId, 1, 'Fluxo de violência', NOW(), 'SISTEMA', '0');
+		values(questaoId, 1, 'Fluxo de violÃªncia', NOW(), 'SISTEMA', '0');
 	insert into opcao_resposta (questao_id, ordem, nome, criado_em, criado_por, criado_rf)
-		values(questaoId, 2, 'Notificação feita pelo NAAPA', NOW(), 'SISTEMA', '0');
+		values(questaoId, 2, 'NotificaÃ§Ã£o feita pelo NAAPA', NOW(), 'SISTEMA', '0');
 	insert into opcao_resposta (questao_id, ordem, nome, criado_em, criado_por, criado_rf)
-		values(questaoId, 3, 'Notificação feita pela escola', NOW(), 'SISTEMA', '0');
+		values(questaoId, 3, 'NotificaÃ§Ã£o feita pela escola', NOW(), 'SISTEMA', '0');
 	insert into opcao_resposta (questao_id, ordem, nome, criado_em, criado_por, criado_rf)
 		values(questaoId, 4, 'Busca ativa escolar', NOW(), 'SISTEMA', '0');
 	insert into opcao_resposta (questao_id, ordem, nome, criado_em, criado_por, criado_rf)
@@ -30,6 +30,6 @@ begin
 	update questao set opcionais = '{"desabilitarDataFutura":true}' where questionario_id = questionarioId and ordem = 0;
 	update questao set tamanho = 100 where questionario_id = questionarioId and ordem in (12,13);
 
-	update secao_encaminhamento_naapa set nome = 'Informações' where questionario_id = questionarioId and ordem = 0;
+	update secao_encaminhamento_naapa set nome = 'InformaÃ§Ãµes' where questionario_id = questionarioId and ordem = 0;
 end $$
 	
