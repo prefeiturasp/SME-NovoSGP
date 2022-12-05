ea1d5606e9fb10e50a71fae5beac1a3f16523b64
jailsvs <jailsvs@gmail.com>
2022-12-05 16:02:08 -0300

    62112 - Ajuste mapping entidade Wf Aprovação Parecer Conclusivo

--- src/SME.SGP.Dados/Mapeamentos/WFAprovacaoParecerConclusivoMap.cs
+++ src/SME.SGP.Dados/Mapeamentos/WFAprovacaoParecerConclusivoMap.cs
@@ -8,7 +8,6 @@ namespace SME.SGP.Dados
         public WFAprovacaoParecerConclusivoMap()
         {
             ToTable("wf_aprovacao_parecer_conclusivo");
-            Map(c => c.Id).ToColumn("id").IsIdentity().IsKey();
             Map(c => c.WfAprovacaoId).ToColumn("wf_aprovacao_id");
             Map(c => c.ConselhoClasseAlunoId).ToColumn("conselho_classe_aluno_id");
             Map(c => c.UsuarioSolicitanteId).ToColumn("usuario_solicitante_id");
