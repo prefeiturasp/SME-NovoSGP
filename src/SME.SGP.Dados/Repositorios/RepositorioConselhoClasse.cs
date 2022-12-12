977801b10b653d320a0e83eca354a1e63bf1bf2a
Danyllo Pereira <danyllo.antunes@gmail.com>
2022-12-12 17:17:26 -0300

    81211 - Notas de fechamento não refletem na tela de Conselho de Classe

--- src/SME.SGP.Dados/Repositorios/RepositorioConselhoClasse.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioConselhoClasse.cs
@@ -2,6 +2,7 @@
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using SME.SGP.Infra.Interface;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
@@ -17,6 +18,18 @@ namespace SME.SGP.Dados.Repositorios
             database.Conexao.Execute("update conselho_classe set situacao = @situacaoConselhoClasse where id = @conselhoClasseId", new { conselhoClasseId, situacaoConselhoClasse = (int)situacaoConselhoClasse });
 
             return Task.FromResult(true);
-        }                      
+        }
+
+        public async Task<IEnumerable<ConselhoClasseAlunosNotaPorFechamentoIdDto>> ObterConselhoClasseAlunosNotaPorFechamentoId(long fechamentoTurmaId)
+        {
+            var query = @"select cc.id ConselhoClasseId, ccn.id ConselhoClasseNotaId, 
+                                cca.aluno_codigo AlunoCodigo, ccn.componente_curricular_codigo ComponenteCurricularCodigo
+                          from conselho_classe cc
+                          join conselho_classe_aluno cca on cc.id = cca.conselho_classe_id
+                          join conselho_classe_nota ccn on ccn.conselho_classe_aluno_id = cca.id
+                          where cc.fechamento_turma_id = @fechamentoTurmaId";
+
+            return await database.Conexao.QueryAsync<ConselhoClasseAlunosNotaPorFechamentoIdDto>(query, new { fechamentoTurmaId });
+        }
     }
 }
