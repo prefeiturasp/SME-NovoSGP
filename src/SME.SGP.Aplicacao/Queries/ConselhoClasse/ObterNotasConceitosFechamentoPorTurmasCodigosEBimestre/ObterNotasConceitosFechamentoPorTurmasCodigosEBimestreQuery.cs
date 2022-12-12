5151b637bfd7fd024574f9132d8d354adf1d57a9
robsonamcom <robson.silva@amcom.com.br>
2022-12-12 13:08:52 -0300

    Bug: 81221 Ajuste na obtenção de notas do aluno com utilização do cache no conselho, logo após o lançamento das notas. Foi necessário especificar o aluno na chave do cache.

--- src/SME.SGP.Aplicacao/Queries/ConselhoClasse/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestre/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/ConselhoClasse/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestre/ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery.cs
@@ -7,16 +7,18 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery : IRequest<IEnumerable<NotaConceitoBimestreComponenteDto>>
     {
-        public ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery(string[] turmasCodigo, int bimestre,
+        public ObterNotasConceitosFechamentoPorTurmasCodigosEBimestreQuery(string[] turmasCodigo, int bimestre, string codigoAluno = null,
             DateTime? dataMatricula = null, DateTime? dataSituacao = null)
         {
             TurmasCodigos = turmasCodigo;
+            CodigoAluno = codigoAluno;
             Bimestre = bimestre;
             DataMatricula = dataMatricula;
             DataSituacao = dataSituacao;
         }
 
         public string[] TurmasCodigos { get; }
+        public string CodigoAluno { get; set; }
         public int Bimestre { get; }
         public DateTime? DataMatricula { get; }
         public DateTime? DataSituacao { get; }
