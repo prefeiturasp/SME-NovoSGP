7c2e9ed91f38c2cd3e380144ecbb4b79badc7ddb
Fernando Groeler <fernando.groeler@gmail.com>
2022-12-12 09:14:58 -0400

    Ajuste do namespace.

--- src/SME.SGP.Aplicacao/Queries/TipoRelatorio/ObterTipoRelatorioPorCodigo/ObterTipoRelatorioPorCodigoQueryValidator.cs
+++ src/SME.SGP.Aplicacao/Queries/TipoRelatorio/ObterTipoRelatorioPorCodigo/ObterTipoRelatorioPorCodigoQueryValidator.cs
@@ -1,14 +1,15 @@
 ﻿using FluentValidation;
 
-namespace SME.SGP.Aplicacao;
-
-public class ObterTipoRelatorioPorCodigoQueryValidator : AbstractValidator<ObterTipoRelatorioPorCodigoQuery>
+namespace SME.SGP.Aplicacao
 {
-    public ObterTipoRelatorioPorCodigoQueryValidator()
+    public class ObterTipoRelatorioPorCodigoQueryValidator : AbstractValidator<ObterTipoRelatorioPorCodigoQuery>
     {
-        RuleFor(c => c.CodigoRelatorio)
-            .NotEmpty()
-            .NotNull()
-            .WithMessage("O código do relatório deve ser informado para obter o tipo do relatório.");
+        public ObterTipoRelatorioPorCodigoQueryValidator()
+        {
+            RuleFor(c => c.CodigoRelatorio)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("O código do relatório deve ser informado para obter o tipo do relatório.");
+        }
     }
 }
