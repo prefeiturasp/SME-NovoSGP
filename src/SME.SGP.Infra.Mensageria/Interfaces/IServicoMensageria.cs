9fefcb521985f978ec2362b3b61b18295815c3d7
Rafael Losi <rafael.losi@amcom.com.br>
2022-12-15 18:53:48 -0300

    Corrigido publicação da Limbo

--- src/SME.SGP.Infra.Mensageria/Interfaces/IServicoMensageria.cs
+++ src/SME.SGP.Infra.Mensageria/Interfaces/IServicoMensageria.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using RabbitMQ.Client;
+using System.Threading.Tasks;
 
 namespace SME.SGP.Infra.Interface
 {
     public interface IServicoMensageria<T>
         where T : class
     {
-        Task<bool> Publicar(T mensagem, string rota, string exchange, string nomeAcao);
+        Task<bool> Publicar(T mensagem, string rota, string exchange, string nomeAcao, IModel canalRabbit = null);
     }
 
     public interface IServicoMensageriaSGP : IServicoMensageria<MensagemRabbit> { }
