31ccd5198f56ccce7a1f58d552c8992bd242a7b0
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2022-12-19 11:52:55 -0300

    Criação do teste de integração de listar documentos

---
+++ teste/SME.SGP.TesteIntegracao/Documento/Ao_listar_documentos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Shouldly;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.PlanoAEE.ServicosFakes;
+using SME.SGP.TesteIntegracao.PlanoAula.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.Documento
+{
+    public class Ao_listar_documentos : DocumentoTesteBase
+    {
+        public Ao_listar_documentos(CollectionFixture collectionFixture) : base(collectionFixture) {}
+
+        [Fact(DisplayName = "Documento - Listando documentos")]
+        public async Task Ao_listar_documentos_com_turma_e_componente()
+        {
+            var filtro = new FiltroDocumentoDto()
+            {
+                Modalidade = Modalidade.Fundamental,
+                Perfil = ObterPerfilProfessor(),
+                TipoCalendario = ModalidadeTipoCalendario.FundamentalMedio,
+            };
+
+            await CriarDadosBasicos(filtro);
+            
+            var obterServicoListarDocumentosUse = ObterServicoListarDocumentosUseCase();
+            var retorno = await obterServicoListarDocumentosUse.Executar(UE_ID_1, (long)Dominio.Enumerados.TipoDocumento.Documento, (long)Dominio.Enumerados.ClassificacaoDocumento.DocumentosTurma);
+
+            // var verificarExistenciaPlanoAee = ObterServicoVerificarExistenciaPlanoAEEPorEstudanteUseCase();
+            // var ex = await Assert.ThrowsAsync<NegocioException>(() => verificarExistenciaPlanoAee.Executar(aluno.Items.FirstOrDefault().Codigo));
+            // ex.Message.ShouldNotBeNullOrEmpty();
+        }
+    }
+}
---
+++ teste/SME.SGP.TesteIntegracao/Documento/Base/DocumentoTesteBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.ServicosFakes;
+using SME.SGP.TesteIntegracao.Setup;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Entidades;
+using SME.SGP.TesteIntegracao.PlanoAula.Base;
+using SME.SGP.Aplicacao.Interfaces.CasosDeUso;
+using SME.SGP.Dominio.Enumerados;
+
+namespace SME.SGP.TesteIntegracao.Documento
+{
+    public abstract class DocumentoTesteBase : TesteBaseComuns
+    {
+        public DocumentoTesteBase(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        protected IListarDocumentosUseCase ObterServicoListarDocumentosUseCase()
+        {
+            return ServiceProvider.GetService<IListarDocumentosUseCase>();
+        }
+
+        protected async Task CriarDadosBasicos(FiltroDocumentoDto filtroDocumentoDto)
+        {
+            await CriarTipoCalendario(filtroDocumentoDto.TipoCalendario);
+
+            await CriarDreUePerfil();
+
+            await CriarPeriodoEscolarTodosBimestres();
+
+            await CriarComponenteCurricular();
+
+            CriarClaimUsuario(filtroDocumentoDto.Perfil);
+
+            await CriarUsuarios();
+
+            await CriarTurma(filtroDocumentoDto.Modalidade);
+        }
+
+        protected async Task CriarPeriodoEscolarTodosBimestres()
+        {
+            await CriarPeriodoEscolar(DATA_01_02_INICIO_BIMESTRE_1, DATA_25_04_FIM_BIMESTRE_1, BIMESTRE_1);
+
+            await CriarPeriodoEscolar(DATA_02_05_INICIO_BIMESTRE_2, DATA_08_07_FIM_BIMESTRE_2, BIMESTRE_2);
+
+            await CriarPeriodoEscolar(DATA_25_07_INICIO_BIMESTRE_3, DATA_30_09_FIM_BIMESTRE_3, BIMESTRE_3);
+
+            await CriarPeriodoEscolar(DATA_03_10_INICIO_BIMESTRE_4, DATA_22_12_FIM_BIMESTRE_4, BIMESTRE_4);
+        }
+
+    }
+}
