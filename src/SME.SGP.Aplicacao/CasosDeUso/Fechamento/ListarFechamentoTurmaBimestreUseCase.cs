fe18009e8d4d1502be2ca960214315e375243861
Thiago Fernando <thiago.fernando@msn.com>
2022-12-28 11:32:34 -0300

    Criado método ObterDadosDoArredondamento

--- src/SME.SGP.Aplicacao/CasosDeUso/Fechamento/ListarFechamentoTurmaBimestreUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Fechamento/ListarFechamentoTurmaBimestreUseCase.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SME.SGP.Aplicacao.Queries;
 using SME.SGP.Infra.Utilitarios;
 
 namespace SME.SGP.Aplicacao
@@ -133,10 +134,13 @@ namespace SME.SGP.Aplicacao
             fechamentoNotaConceitoTurma.AuditoriaAlteracao = AuditoriaUtil.MontarTextoAuditoriaAlteracao(fechamentosTurma.FirstOrDefault(), tipoNotaTurma.EhNota());
             fechamentoNotaConceitoTurma.AuditoriaInclusao = AuditoriaUtil.MontarTextoAuditoriaInclusao(fechamentosTurma.FirstOrDefault(), tipoNotaTurma.EhNota());
 
-            fechamentoNotaConceitoTurma.DadosArredondamento = await mediator.Send(new ObterNotaParametroDtoPorDataAvaliacaoQuery(fechamentoNotaConceitoTurma.PeriodoFim));
+            await ObterDadosDoArredondamento(fechamentoNotaConceitoTurma);
 
             return fechamentoNotaConceitoTurma;
         }
+
+        private async Task ObterDadosDoArredondamento(FechamentoNotaConceitoTurmaDto fechamentoNotaConceitoTurma) 
+            => fechamentoNotaConceitoTurma.DadosArredondamento = await mediator.Send(new ObterNotaParametroDtoPorDataAvaliacaoQuery(fechamentoNotaConceitoTurma.PeriodoFim));
 
         private Task<IEnumerable<string>> ObterAlunosComAnotacaoNoFechamento(long fechamentoId)
             => mediator.Send(new ObterCodigosAlunosComAnotacaoNoFechamentoQuery(fechamentoId));
