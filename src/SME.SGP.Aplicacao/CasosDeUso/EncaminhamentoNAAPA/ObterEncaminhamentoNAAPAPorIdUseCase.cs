677253105919a29edc8ee7d340a91a614f8507de
jailsvs <jailsvs@gmail.com>
2023-01-02 17:26:16 -0300

    83140 - Busca de matricula aluno na turma do encaminhamento naapa gerado, reversão de mudança de DTO e query mediator (com frequencia) efetuados especificamente para a história

--- src/SME.SGP.Aplicacao/CasosDeUso/EncaminhamentoNAAPA/ObterEncaminhamentoNAAPAPorIdUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/EncaminhamentoNAAPA/ObterEncaminhamentoNAAPAPorIdUseCase.cs
@@ -5,6 +5,9 @@ using System;
 using System.Threading.Tasks;
 using SME.SGP.Dominio.Constantes.MensagensNegocio;
 using SME.SGP.Infra;
+using System.Linq;
+using System.Threading;
+using System.ComponentModel.DataAnnotations;
 
 namespace SME.SGP.Aplicacao
 {
@@ -24,9 +27,7 @@ namespace SME.SGP.Aplicacao
             if(encaminhamentoNAAPA == null)
                 throw new NegocioException(MensagemNegocioEncaminhamentoNAAPA.ENCAMINHAMENTO_NAO_ENCONTRADO);
 
-            var aluno = await mediator.Send(new ObterAlunoPorCodigoEAnoQuery(encaminhamentoNAAPA.AlunoCodigo, encaminhamentoNAAPA.Turma.AnoLetivo, true));
-
-            
+            var aluno = await ObterAlunoPorCodigoETurma(encaminhamentoNAAPA.AlunoCodigo, encaminhamentoNAAPA.Turma.CodigoTurma);
             var nomeUe = encaminhamentoNAAPA.Turma.Ue.TipoEscola == TipoEscola.Nenhum ? encaminhamentoNAAPA.Turma.Ue.Nome : 
                             $"{encaminhamentoNAAPA.Turma.Ue.TipoEscola.ObterNomeCurto()} {encaminhamentoNAAPA.Turma.Ue.Nome}";
 
@@ -51,6 +52,49 @@ namespace SME.SGP.Aplicacao
                 DescricaoSituacao = encaminhamentoNAAPA.Situacao.Name(),
                 Modalidade = (int)encaminhamentoNAAPA.Turma.ModalidadeCodigo
             };
+        }
+
+        private async Task<AlunoTurmaReduzidoDto> ObterAlunoPorCodigoETurma(string alunoCodigo, string turmaCodigo)
+        {
+            var aluno = await mediator.Send(new ObterAlunoPorTurmaAlunoCodigoQuery(turmaCodigo, alunoCodigo, true));
+            var frequencia = await mediator.Send(new ObterConsultaFrequenciaGeralAlunoQuery(alunoCodigo, turmaCodigo));
+            var alunoReduzido = new AlunoTurmaReduzidoDto()
+            {
+                Nome = !string.IsNullOrEmpty(aluno.NomeAluno) ? aluno.NomeAluno : aluno.NomeSocialAluno,
+                NumeroAlunoChamada = aluno.ObterNumeroAlunoChamada(),
+                DataNascimento = aluno.DataNascimento,
+                DataSituacao = aluno.DataSituacao,
+                CodigoAluno = aluno.CodigoAluno,
+                CodigoSituacaoMatricula = aluno.CodigoSituacaoMatricula,
+                Situacao = aluno.SituacaoMatricula,
+                TurmaEscola = await ObterNomeTurmaFormatado(aluno.CodigoTurma.ToString()),
+                CodigoTurma = aluno.CodigoTurma.ToString(),
+                CelularResponsavel = aluno.CelularResponsavel,
+                NomeResponsavel = aluno.NomeResponsavel,
+                DataAtualizacaoContato = aluno.DataAtualizacaoContato,
+                TipoResponsavel = aluno.TipoResponsavel,
+                Frequencia = frequencia
+            };
+            return alunoReduzido;
+        }
+
+        private async Task<string> ObterNomeTurmaFormatado(string turmaCodigo)
+        {
+            var turmaNome = "";
+            var turma = await mediator.Send(new ObterTurmaPorCodigoQuery(turmaCodigo));
+
+            if (turma != null)
+            {
+                var nomeTurno = "";
+                if (Enum.IsDefined(typeof(TipoTurnoEOL), turma.TipoTurno))
+                {
+                    TipoTurnoEOL tipoTurno = (TipoTurnoEOL)turma.TipoTurno;
+                    nomeTurno = $"- {tipoTurno.GetAttribute<DisplayAttribute>()?.GetName()}";
+                }
+                turmaNome = $"{turma.ModalidadeCodigo.ShortName()} - {turma.Nome} {nomeTurno}";
+            }
+
+            return turmaNome;
         }
     }
 }
