d904894bcbaab55a1466a34dca4d2660115687a0
Jailson Volnei dos Santos <33402817+Jailsvs@users.noreply.github.com>
2023-01-05 11:24:42 -0300

    Update src/SME.SGP.Aplicacao/Queries/Notas/ObterNotaParametroDtoPorDataAvaliacao/ObterNotaParametroDtoPorDataAvaliacaoQueryHandler.cs
    Co-authored-by: Rafael Losi <57261028+rafalosiamcom@users.noreply.github.com>

--- src/SME.SGP.Aplicacao/Queries/Notas/ObterNotaParametroDtoPorDataAvaliacao/ObterNotaParametroDtoPorDataAvaliacaoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Notas/ObterNotaParametroDtoPorDataAvaliacao/ObterNotaParametroDtoPorDataAvaliacaoQueryHandler.cs
@@ -5,7 +5,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SME.SGP.Aplicacao.Queries
+namespace SME.SGP.Aplicacao
 {
     public class ObterNotaParametroDtoPorDataAvaliacaoQueryHandler : IRequestHandler<ObterNotaParametroDtoPorDataAvaliacaoQuery, NotaParametroDto>
     {
