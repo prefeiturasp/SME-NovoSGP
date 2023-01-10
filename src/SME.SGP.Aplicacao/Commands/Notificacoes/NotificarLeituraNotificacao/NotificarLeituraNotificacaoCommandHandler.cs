c677c15fed6ef34e2c436f0626c1c9a59ce1c330
robsonamcom <robson.silva@amcom.com.br>
2023-01-10 16:22:48 -0300

    Bug: 80654 Ajuste na funcionalidade de aprovação do período através da notificação, incluindo tentativas de obter informações do usuário através da funcionalidade de polly.

--- src/SME.SGP.Aplicacao/Commands/Notificacoes/NotificarLeituraNotificacao/NotificarLeituraNotificacaoCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Notificacoes/NotificarLeituraNotificacao/NotificarLeituraNotificacaoCommandHandler.cs
@@ -1,4 +1,7 @@
 ﻿using MediatR;
+using Microsoft.Win32;
+using Polly;
+using Polly.Registry;
 using SME.SGP.Infra;
 using SME.SGP.Notificacoes.Hub.Interface;
 using System;
@@ -10,15 +13,17 @@ namespace SME.SGP.Aplicacao
     public class NotificarLeituraNotificacaoCommandHandler : AsyncRequestHandler<NotificarLeituraNotificacaoCommand>
     {
         private readonly IMediator mediator;
+        private readonly IAsyncPolicy policy;
 
-        public NotificarLeituraNotificacaoCommandHandler(IMediator mediator)
+        public NotificarLeituraNotificacaoCommandHandler(IMediator mediator, IReadOnlyPolicyRegistry<string> registry)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.policy = registry.Get<IAsyncPolicy>(PoliticaPolly.PublicaFila);
         }
 
         protected override async Task Handle(NotificarLeituraNotificacaoCommand request, CancellationToken cancellationToken)
         {
-            var usuarioRf = await mediator.Send(new ObterUsuarioRfPorIdQuery(request.Notificacao.UsuarioId.Value));
+            var usuarioRf = await policy.ExecuteAsync(() => mediator.Send(new ObterUsuarioRfPorIdQuery(request.Notificacao.UsuarioId.Value)));
 
             await mediator.Send(new PublicarFilaSgpCommand(RotasRabbitSgpNotificacoes.Leitura,
                                                            new MensagemLeituraNotificacaoDto(request.Notificacao.Codigo,
