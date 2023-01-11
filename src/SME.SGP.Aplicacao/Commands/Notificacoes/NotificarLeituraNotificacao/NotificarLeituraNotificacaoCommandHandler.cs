2dae80cdc111855999d0f5d83a4cdaa44f843d04
robsonamcom <robson.silva@amcom.com.br>
2023-01-11 07:02:21 -0300

    Revert "Bug: 80654"
    This reverts commit c677c15fed6ef34e2c436f0626c1c9a59ce1c330.

--- src/SME.SGP.Aplicacao/Commands/Notificacoes/NotificarLeituraNotificacao/NotificarLeituraNotificacaoCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/Notificacoes/NotificarLeituraNotificacao/NotificarLeituraNotificacaoCommandHandler.cs
@@ -1,7 +1,4 @@
 ﻿using MediatR;
-using Microsoft.Win32;
-using Polly;
-using Polly.Registry;
 using SME.SGP.Infra;
 using SME.SGP.Notificacoes.Hub.Interface;
 using System;
@@ -13,17 +10,15 @@ namespace SME.SGP.Aplicacao
     public class NotificarLeituraNotificacaoCommandHandler : AsyncRequestHandler<NotificarLeituraNotificacaoCommand>
     {
         private readonly IMediator mediator;
-        private readonly IAsyncPolicy policy;
 
-        public NotificarLeituraNotificacaoCommandHandler(IMediator mediator, IReadOnlyPolicyRegistry<string> registry)
+        public NotificarLeituraNotificacaoCommandHandler(IMediator mediator)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            this.policy = registry.Get<IAsyncPolicy>(PoliticaPolly.PublicaFila);
         }
 
         protected override async Task Handle(NotificarLeituraNotificacaoCommand request, CancellationToken cancellationToken)
         {
-            var usuarioRf = await policy.ExecuteAsync(() => mediator.Send(new ObterUsuarioRfPorIdQuery(request.Notificacao.UsuarioId.Value)));
+            var usuarioRf = await mediator.Send(new ObterUsuarioRfPorIdQuery(request.Notificacao.UsuarioId.Value));
 
             await mediator.Send(new PublicarFilaSgpCommand(RotasRabbitSgpNotificacoes.Leitura,
                                                            new MensagemLeituraNotificacaoDto(request.Notificacao.Codigo,
