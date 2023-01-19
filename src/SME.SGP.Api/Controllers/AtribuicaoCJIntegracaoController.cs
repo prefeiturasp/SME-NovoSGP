74036f2dc8d8e8e5a9b0d1767f93a5bf394e419c
jailsvs <jailsvs@gmail.com>
2023-01-19 08:43:46 -0300

    84167 - Novo controller de atribuição CJ para integrações

---
+++ src/SME.SGP.Api/Controllers/AtribuicaoCJIntegracaoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Api.Middlewares;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/atribuicoes/cjs/integracoes")]
+    [ChaveIntegracaoSgpApi]
+    [ValidaDto]
+    public class AtribuicaoCJIntegracaoController : ControllerBase
+    {
+        [HttpGet]
+        [ChaveIntegracaoSgpApi]
+        [ProducesResponseType(typeof(IEnumerable<AtribuicaoCJListaRetornoDto>), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> Get([FromQuery]AtribuicaoCJListaFiltroDto atribuicaoCJListaFiltroDto, [FromServices] IListarAtribuicoesCJPorFiltroUseCase useCase)
+        {
+            return Ok(await useCase.Executar(atribuicaoCJListaFiltroDto));
+        }
+
+    }
+}
