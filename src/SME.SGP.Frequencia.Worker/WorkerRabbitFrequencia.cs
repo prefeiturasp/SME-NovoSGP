f481b09ea394cfe8f8ff5a736ac2d88c4f5c836b
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-01-19 15:17:32 -0300

    Tornando o lançamento de frequência sem indicação de origem

--- src/SME.SGP.Frequencia.Worker/WorkerRabbitFrequencia.cs
+++ src/SME.SGP.Frequencia.Worker/WorkerRabbitFrequencia.cs
@@ -65,7 +65,7 @@ namespace SME.SGP.Frequencia.Worker
             Comandos.Add(RotasRabbitSgpFrequencia.RotaTratarCargaRegistroFrequenciaAlunoProcessamento, new ComandoRabbit("Processa a carga referência de aula no registro frequência aluno", typeof(ITratarRegistroFrequenciaAlunoProcessamentoUseCase)));
             Comandos.Add(RotasRabbitSgpFrequencia.RotaTratarFrequenciaRegistradaAlunosInativos, new ComandoRabbit("Tratar frequência registrada para alunos inativos indevidamente", typeof(IVerificaFrequenciaRegistradaAlunosInativosUseCase)));
             
-            Comandos.Add(RotasRabbitSgpFrequencia.RotaFrequenciaLancamentoAulaSync, new ComandoRabbit("Tratar lançamento de frequências provenientes da integração SGA", typeof(ILancarFrequenciaAulaUseCase)));
+            Comandos.Add(RotasRabbitSgpFrequencia.RotaFrequenciaLancamentoAulaSync, new ComandoRabbit("Tratar lançamento de frequências provenientes da integração externa ao SGP", typeof(ILancarFrequenciaAulaUseCase)));
         }
     }
 }
