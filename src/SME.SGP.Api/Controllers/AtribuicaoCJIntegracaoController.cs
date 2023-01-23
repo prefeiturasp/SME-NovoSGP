ae122c97ea25eb36b0b9f7d35c8b89ea617ba74b
Thiago Fernando <thiago.fernando@msn.com>
2023-01-23 13:49:28 -0300

    Correção na Rota do Endpoint de obter professores cj

--- src/SME.SGP.Api/Controllers/AtribuicaoCJIntegracaoController.cs
+++ src/SME.SGP.Api/Controllers/AtribuicaoCJIntegracaoController.cs
@@ -12,13 +12,13 @@ namespace SME.SGP.Api.Controllers
     [ChaveIntegracaoSgpApi]
     public class AtribuicaoCJIntegracaoController : ControllerBase
     {
-        [HttpGet("{ueId}/{anoLetivo}")]
+        [HttpGet("escola/{ueCodigo}/anoLetivo/{anoLetivo}")]
         [ProducesResponseType(typeof(IEnumerable<AtribuicaoCJListaRetornoDto>), 200)]
         [ProducesResponseType(204)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        public async Task<IActionResult> Get(string ueId,int anoLetivo, [FromServices] IListarAtribuicoesCJPorFiltroUseCase useCase)
+        public async Task<IActionResult> Get(string ueCodigo,int anoLetivo, [FromServices] IListarAtribuicoesCJPorFiltroUseCase useCase)
         {
-            return Ok(await useCase.Executar(new AtribuicaoCJListaFiltroDto{UeId = ueId,AnoLetivo = anoLetivo}));
+            return Ok(await useCase.Executar(new AtribuicaoCJListaFiltroDto{UeId = ueCodigo,AnoLetivo = anoLetivo}));
         }
 
     }
