42884de2dde2925843a462d3db6e42d1f361e264
Thiago Fernando <thiago.fernando@msn.com>
2023-01-23 11:29:11 -0300

    Obter se o aluno esta ausente ou não na atividade

--- src/SME.SGP.Aplicacao/Queries/AtividadeAvaliativa/ObterAtividadesNotasAlunoPorTurmaPeriodo/ObterAtividadesNotasAlunoPorTurmaPeriodoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/AtividadeAvaliativa/ObterAtividadesNotasAlunoPorTurmaPeriodo/ObterAtividadesNotasAlunoPorTurmaPeriodoQueryHandler.cs
@@ -3,6 +3,7 @@ using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,13 +12,41 @@ namespace SME.SGP.Aplicacao
     public class ObterAtividadesNotasAlunoPorTurmaPeriodoQueryHandler : IRequestHandler<ObterAtividadesNotasAlunoPorTurmaPeriodoQuery, IEnumerable<AvaliacaoNotaAlunoDto>>
     {
         private readonly IRepositorioAtividadeAvaliativa repositorioAtividadeAvaliativa;
+        private readonly Mediator mediator;
 
-        public ObterAtividadesNotasAlunoPorTurmaPeriodoQueryHandler(IRepositorioAtividadeAvaliativa repositorioAtividadeAvaliativa)
+        public ObterAtividadesNotasAlunoPorTurmaPeriodoQueryHandler(IRepositorioAtividadeAvaliativa repositorioAtividadeAvaliativa, Mediator mediator)
         {
             this.repositorioAtividadeAvaliativa = repositorioAtividadeAvaliativa ?? throw new ArgumentNullException(nameof(repositorioAtividadeAvaliativa));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
-        public Task<IEnumerable<AvaliacaoNotaAlunoDto>> Handle(ObterAtividadesNotasAlunoPorTurmaPeriodoQuery request, CancellationToken cancellationToken)
-            => repositorioAtividadeAvaliativa.ObterAtividadesNotasAlunoPorTurmaPeriodo(request.TurmaId, request.PeriodoEscolarId, request.AlunoCodigo, request.ComponenteCurricular);
+        public async Task<IEnumerable<AvaliacaoNotaAlunoDto>> Handle(ObterAtividadesNotasAlunoPorTurmaPeriodoQuery request, CancellationToken cancellationToken)
+        {
+            var retorno = (await repositorioAtividadeAvaliativa.ObterAtividadesNotasAlunoPorTurmaPeriodo(request.TurmaId, request.PeriodoEscolarId, request.AlunoCodigo, request.ComponenteCurricular)).ToList();
+
+            retorno = await ObterAusencia(request, retorno);
+
+            return retorno;
+        }
+
+        private async Task<List<AvaliacaoNotaAlunoDto>> ObterAusencia(ObterAtividadesNotasAlunoPorTurmaPeriodoQuery request, List<AvaliacaoNotaAlunoDto> listAtividades)
+        {
+            var retorno = new List<AvaliacaoNotaAlunoDto>();
+            var datasDasAtividadesAvaliativas = retorno.Select(x => x.Data).ToArray();
+            var turma = await mediator.Send(new ObterTurmaPorIdQuery(request.TurmaId));
+
+            var ausenciasDasAtividadesAvaliativas = (await mediator.Send(new ObterAusenciasDaAtividadesAvaliativasPorAlunoQuery(turma.CodigoTurma, datasDasAtividadesAvaliativas, request.ComponenteCurricular, request.AlunoCodigo))).ToList();
+
+            foreach (var atividade in listAtividades)
+            {
+                var ausente = ausenciasDasAtividadesAvaliativas
+                    .Any(a => a.AlunoCodigo == request.AlunoCodigo && a.AulaData.Date == atividade.Data.Date);
+
+                atividade.Ausente = ausente;
+                retorno.Add(atividade);
+            }
+
+            return retorno;
+        }
     }
 }
