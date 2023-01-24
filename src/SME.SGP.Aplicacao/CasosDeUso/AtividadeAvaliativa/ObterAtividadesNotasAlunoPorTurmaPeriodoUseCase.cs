86fd55fd8a32f90473a99dd907df4574688fa55e
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-01-24 14:50:16 -0300

    63937 - Incluir os componentes das atividades de notas

--- src/SME.SGP.Aplicacao/CasosDeUso/AtividadeAvaliativa/ObterAtividadesNotasAlunoPorTurmaPeriodoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/AtividadeAvaliativa/ObterAtividadesNotasAlunoPorTurmaPeriodoUseCase.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using MediatR;
+﻿using MediatR;
 using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,21 +12,71 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterAtividadesNotasAlunoPorTurmaPeriodoUseCase : AbstractUseCase, IObterAtividadesNotasAlunoPorTurmaPeriodoUseCase
     {
+        private readonly IMediator mediator;
+
         public ObterAtividadesNotasAlunoPorTurmaPeriodoUseCase(IMediator mediator) : base(mediator)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
-        private readonly IMediator mediator;
-
         public async Task<IEnumerable<AvaliacaoNotaAlunoDto>> Executar(FiltroTurmaAlunoPeriodoEscolarDto param)
         {
-          var retorno =  (await mediator.Send(new ObterAtividadesNotasAlunoPorTurmaPeriodoQuery(param.TurmaId, param.PeriodoEscolarId, param.AlunoCodigo, param.ComponenteCurricular))).ToList();
+            var retorno =  (await mediator.Send(new ObterAtividadesNotasAlunoPorTurmaPeriodoQuery(param.TurmaId, param.PeriodoEscolarId, param.AlunoCodigo, param.ComponenteCurricular))).ToList();
 
-          retorno = await ObterAusencia(param, retorno);
-          return retorno;
+            await CarregarDisciplinasDeRegencia(retorno, long.Parse(param.ComponenteCurricular), param.TurmaId);
+            retorno = await ObterAusencia(param, retorno);
+
+            return retorno;
         }
-        
+
+        private async Task CarregarDisciplinasDeRegencia(
+                                    IEnumerable<AvaliacaoNotaAlunoDto> avaliacoesNotas, 
+                                    long codigoComponenteCurricular,
+                                    long turmaId)
+        {
+            var componentesCurricularesCompletos = await mediator.Send(new ObterComponentesCurricularesPorIdsQuery(new long[] { codigoComponenteCurricular }));
+            if (componentesCurricularesCompletos == null || !componentesCurricularesCompletos.Any())
+                throw new NegocioException("Componente curricular informado não encontrado no EOL");
+
+            var componenteReferencia = componentesCurricularesCompletos.FirstOrDefault(a => a.CodigoComponenteCurricular == codigoComponenteCurricular);
+
+            if (componenteReferencia.Regencia)
+            {
+                var turmaCompleta = await mediator.Send(new ObterTurmaComUeEDrePorIdQuery(turmaId));
+
+                if (turmaCompleta == null)
+                    throw new NegocioException("Não foi possível obter a turma.");
+
+                foreach (var avaliacao in avaliacoesNotas)
+                {
+                    var atividadeDisciplinas = await ObterDisciplinasAtividadeAvaliativa(avaliacao.Id, avaliacao.Regencia);
+                    var idsDisciplinas = atividadeDisciplinas?.Select(a => long.Parse(a.DisciplinaId)).ToArray();
+                    IEnumerable<DisciplinaDto> disciplinas;
+                    if (idsDisciplinas != null && idsDisciplinas.Any())
+                        disciplinas = await ObterDisciplinasPorIds(idsDisciplinas);
+                    else
+                    {
+                        disciplinas = await mediator.Send(new ObterComponentesCurricularesPorProfessorETurmaParaPlanejamentoQuery(
+                                                                                            componenteReferencia.CodigoComponenteCurricular,
+                                                                                            turmaCompleta.CodigoTurma,
+                                                                                            turmaCompleta.TipoTurma == TipoTurma.Programa,
+                                                                                            componenteReferencia.Regencia));
+                    }
+                    var nomesDisciplinas = disciplinas?.Select(d => d.Nome).ToArray();
+                    avaliacao.Disciplinas = nomesDisciplinas;
+                }
+            }
+        }
+
+        public async Task<IEnumerable<AtividadeAvaliativaDisciplina>> ObterDisciplinasAtividadeAvaliativa(long avaliacao_id, bool ehRegencia)
+        {
+            return await mediator.Send(new ObterDisciplinasAtividadeAvaliativaQuery(avaliacao_id, ehRegencia));
+        }
+        public async Task<IEnumerable<DisciplinaDto>> ObterDisciplinasPorIds(long[] ids)
+        {
+            return await mediator.Send(new ObterDisciplinasPorIdsQuery(ids));
+        }
+
         private async Task<List<AvaliacaoNotaAlunoDto>> ObterAusencia(FiltroTurmaAlunoPeriodoEscolarDto request, List<AvaliacaoNotaAlunoDto> listAtividades)
         {
             var retorno = new List<AvaliacaoNotaAlunoDto>();
---
+++ src/SME.SGP.Aplicacao/Queries/Disciplina/ObterComponentesCurricularesPorProfessorETurmaParaPlanejamento/ObterComponentesCurricularesPorProfessorETurmaParaPlanejamentoQueryHandler.cs
@@ -0,0 +1,202 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SME.SGP.Aplicacao.Integracoes.Respostas;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterComponentesCurricularesPorProfessorETurmaParaPlanejamentoQueryHandler : IRequestHandler<ObterComponentesCurricularesPorProfessorETurmaParaPlanejamentoQuery, IEnumerable<DisciplinaDto>>
+    {
+        private static readonly long[] IDS_COMPONENTES_REGENCIA = { 2, 7, 8, 89, 138 };
+        private readonly IMediator mediator;
+        private readonly IRepositorioCache repositorioCache;
+        private readonly IRepositorioAtribuicaoCJ repositorioAtribuicaoCJ;
+        private readonly IRepositorioComponenteCurricularConsulta repositorioComponenteCurricular;
+        private readonly IRepositorioComponenteCurricularJurema repositorioComponenteCurricularJurema;
+        private readonly IConsultasObjetivoAprendizagem consultasObjetivoAprendizagem;
+
+        public ObterComponentesCurricularesPorProfessorETurmaParaPlanejamentoQueryHandler(
+                                                           IMediator mediator,
+                                                           IRepositorioCache repositorioCache,
+                                                           IRepositorioAtribuicaoCJ repositorioAtribuicaoCJ,
+                                                           IRepositorioComponenteCurricularConsulta repositorioComponenteCurricular,
+                                                           IRepositorioComponenteCurricularJurema repositorioComponenteCurricularJurema,
+                                                           IConsultasObjetivoAprendizagem consultasObjetivoAprendizagem)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.repositorioCache = repositorioCache ?? throw new ArgumentNullException(nameof(repositorioCache));
+            this.repositorioAtribuicaoCJ = repositorioAtribuicaoCJ ?? throw new ArgumentNullException(nameof(repositorioAtribuicaoCJ));
+            this.repositorioComponenteCurricular = repositorioComponenteCurricular ?? throw new ArgumentNullException(nameof(repositorioComponenteCurricular));
+            this.repositorioComponenteCurricularJurema = repositorioComponenteCurricularJurema ?? throw new System.ArgumentNullException(nameof(repositorioComponenteCurricularJurema));
+            this.consultasObjetivoAprendizagem = consultasObjetivoAprendizagem ?? throw new System.ArgumentNullException(nameof(consultasObjetivoAprendizagem));
+        }
+
+        public async Task<IEnumerable<DisciplinaDto>> Handle(ObterComponentesCurricularesPorProfessorETurmaParaPlanejamentoQuery request, CancellationToken cancellationToken)
+        {
+            List<DisciplinaDto> disciplinasDto;
+            var usuario = await mediator.Send(new ObterUsuarioLogadoQuery());
+            var dataInicioNovoSGP = await mediator.Send(new ObterParametroSistemaPorTipoQuery(TipoParametroSistema.DataInicioSGP));
+
+            var chaveCache = $"Disciplinas-planejamento-{request.CodigoTurma}-{request.CodigoDisciplina}-{usuario.PerfilAtual}";
+            if (!usuario.EhProfessor() && !usuario.EhProfessorCj() && !usuario.EhProfessorPoa())
+            {
+                var disciplinasCacheString = await repositorioCache.ObterAsync(chaveCache);
+
+                if (!string.IsNullOrWhiteSpace(disciplinasCacheString))
+                {
+                    disciplinasDto = JsonConvert.DeserializeObject<List<DisciplinaDto>>(disciplinasCacheString);
+                    var disciplinas = await TratarRetornoDisciplinasPlanejamento(disciplinasDto, request.CodigoDisciplina, request.Regencia, request.CodigoTurma);
+                    return disciplinas?.OrderBy(c => c.Nome)?.ToList();
+                }
+            }
+
+            var componentesCurricularesJurema = await repositorioCache.ObterAsync("ComponentesJurema", () => Task.FromResult(repositorioComponenteCurricularJurema.Listar()));
+            if (componentesCurricularesJurema == null)
+            {
+                throw new NegocioException("Não foi possível recuperar a lista de componentes curriculares.");
+            }
+
+            var turma = await mediator.Send(new ObterTurmaPorCodigoQuery(request.CodigoTurma));
+            if (turma == null)
+                throw new NegocioException("Não foi possível encontrar a turma");
+
+            if (usuario.EhProfessorCj())
+            {
+                var componentesCJ = await ObterComponentesCJ(null, request.CodigoTurma,
+                    string.Empty,
+                    request.CodigoDisciplina,
+                    usuario.Login);
+                disciplinasDto = MapearParaDto(componentesCJ, request.TurmaPrograma)?.OrderBy(c => c.Nome)?.ToList();
+            }
+            else
+            {
+                var componentesCurriculares = await mediator.Send(new ObterComponentesCurricularesPorCodigoTurmaLoginEPerfilParaPlanejamentoQuery(request.CodigoTurma, usuario.Login, usuario.PerfilAtual));
+
+                if (turma.ModalidadeCodigo == Modalidade.EJA)
+                    componentesCurriculares = RemoverEdFisicaEJA(componentesCurriculares);
+
+                disciplinasDto = (await repositorioComponenteCurricular.ObterDisciplinasPorIds(componentesCurriculares.Select(a => a.Codigo).ToArray()))?.OrderBy(c => c.Nome)?.ToList();
+
+                disciplinasDto.ForEach(d =>
+                {
+                    var componenteEOL = componentesCurriculares.FirstOrDefault(a => a.Codigo == d.CodigoComponenteCurricular);
+                    d.PossuiObjetivos = turma.AnoLetivo < Convert.ToInt32(dataInicioNovoSGP) ? false : componenteEOL.PossuiObjetivosDeAprendizagem(componentesCurricularesJurema, request.TurmaPrograma, turma.ModalidadeCodigo, turma.Ano);
+                    d.Regencia = componenteEOL.Regencia;
+                    d.ObjetivosAprendizagemOpcionais = componenteEOL.PossuiObjetivosDeAprendizagemOpcionais(componentesCurricularesJurema, turma.EnsinoEspecial);
+                });
+            }
+
+            if (!usuario.EhProfessor() && !usuario.EhProfessorCj() && !usuario.EhProfessorPoa())
+                await repositorioCache.SalvarAsync(chaveCache, JsonConvert.SerializeObject(disciplinasDto));
+
+            return await TratarRetornoDisciplinasPlanejamento(disciplinasDto, request.CodigoDisciplina, request.Regencia, request.CodigoTurma, usuario.EhProfessorCj());
+        }
+
+        private async Task<IEnumerable<DisciplinaDto>> TratarRetornoDisciplinasPlanejamento(IEnumerable<DisciplinaDto> disciplinas, long codigoDisciplina, bool regencia, string codigoTurma = "", bool CJ = false)
+        {
+            if (codigoDisciplina == 0 && !regencia)
+                return disciplinas;
+
+            if (regencia)
+            {
+                if (!codigoTurma.Equals(""))
+                {
+                    var regencias = await mediator.Send(new ObterComponentesCurricularesRegenciaPorTurmaCodigoQuery(codigoTurma));
+                    return CJ ? disciplinas.Where(x => regencias.Any(c => c.CodigoComponenteCurricular == x.CodigoComponenteCurricular))
+                        : disciplinas.Where(x => x.Regencia && regencias.Any(c => c.CodigoComponenteCurricular == x.CodigoComponenteCurricular));
+                }
+                return CJ ? disciplinas : disciplinas.Where(x => x.Regencia);
+            }
+
+            return disciplinas.Where(x => x.CodigoComponenteCurricular == codigoDisciplina);
+        }
+
+        private async Task<IEnumerable<DisciplinaResposta>> ObterComponentesCJ(Modalidade? modalidade, string codigoTurma, string ueId, long codigoDisciplina, string rf, bool ignorarDeParaRegencia = false)
+        {
+            IEnumerable<DisciplinaResposta> componentes = null;
+            var atribuicoes = await repositorioAtribuicaoCJ.ObterPorFiltros(modalidade,
+                codigoTurma,
+                ueId,
+                codigoDisciplina,
+                rf,
+                string.Empty,
+                true);
+            if (atribuicoes == null || !atribuicoes.Any())
+                return null;
+
+            var disciplinasEol = await repositorioComponenteCurricular.ObterDisciplinasPorIds(atribuicoes.Select(a => a.DisciplinaId).Distinct().ToArray());
+
+            var componenteRegencia = disciplinasEol?.FirstOrDefault(c => c.Regencia);
+            if (componenteRegencia == null || ignorarDeParaRegencia)
+                return TransformarListaDisciplinaEolParaRetornoDto(disciplinasEol);
+
+            var componentesRegencia = await repositorioComponenteCurricular.ObterDisciplinasPorIds(IDS_COMPONENTES_REGENCIA);
+            if (componentesRegencia != null)
+                return TransformarListaDisciplinaEolParaRetornoDto(componentesRegencia);
+
+            return componentes;
+        }
+
+        private List<DisciplinaDto> MapearParaDto(IEnumerable<DisciplinaResposta> disciplinas, bool turmaPrograma = false, bool ensinoEspecial = false)
+        {
+            var retorno = new List<DisciplinaDto>();
+
+            if (disciplinas != null)
+            {
+                foreach (var disciplina in disciplinas)
+                {
+                    retorno.Add(MapearParaDto(disciplina, turmaPrograma, ensinoEspecial));
+                }
+            }
+            return retorno;
+        }
+
+        private DisciplinaDto MapearParaDto(DisciplinaResposta disciplina, bool turmaPrograma = false, bool ensinoEspecial = false) => new DisciplinaDto()
+        {
+            Id = disciplina.Id,
+            CdComponenteCurricularPai = disciplina.CodigoComponenteCurricularPai,
+            CodigoComponenteCurricular = disciplina.CodigoComponenteCurricular,
+            Nome = disciplina.Nome,
+            NomeComponenteInfantil = disciplina.NomeComponenteInfantil,
+            Regencia = disciplina.Regencia,
+            TerritorioSaber = disciplina.TerritorioSaber,
+            Compartilhada = disciplina.Compartilhada,
+            RegistraFrequencia = disciplina.RegistroFrequencia,
+            LancaNota = disciplina.LancaNota,
+            PossuiObjetivos = !turmaPrograma && consultasObjetivoAprendizagem.DisciplinaPossuiObjetivosDeAprendizagem(disciplina.CodigoComponenteCurricular),
+            ObjetivosAprendizagemOpcionais = consultasObjetivoAprendizagem.ComponentePossuiObjetivosOpcionais(disciplina.CodigoComponenteCurricular, disciplina.Regencia, ensinoEspecial).Result
+        };
+
+        private IEnumerable<DisciplinaResposta> TransformarListaDisciplinaEolParaRetornoDto(IEnumerable<DisciplinaDto> disciplinasEol)
+        {
+            foreach (var disciplinaEol in disciplinasEol)
+            {
+                yield return MapearDisciplinaResposta(disciplinaEol);
+            }
+        }
+
+        private DisciplinaResposta MapearDisciplinaResposta(DisciplinaDto disciplinaEol) => new DisciplinaResposta()
+        {
+            CodigoComponenteCurricular = disciplinaEol.CodigoComponenteCurricular,
+            CodigoComponenteCurricularPai = disciplinaEol.CdComponenteCurricularPai,
+            Nome = disciplinaEol.Nome,
+            Regencia = disciplinaEol.Regencia,
+            Compartilhada = disciplinaEol.Compartilhada,
+            RegistroFrequencia = disciplinaEol.RegistraFrequencia,
+            LancaNota = disciplinaEol.LancaNota,
+            NomeComponenteInfantil = disciplinaEol.NomeComponenteInfantil,
+            Id = disciplinaEol.Id,
+            TerritorioSaber = disciplinaEol.TerritorioSaber
+        };
+
+        private IEnumerable<ComponenteCurricularEol> RemoverEdFisicaEJA(IEnumerable<ComponenteCurricularEol> componentesCurriculares)
+            => componentesCurriculares.Where(c => c.Codigo != 6);
+    }
+}
