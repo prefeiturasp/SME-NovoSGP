c27f1e2b788cfd9404b2fe83e3c1b94607ba02f6
Thiago Fernando <thiago.fernando@msn.com>
2023-01-25 14:38:10 -0300

    add ponto e virgula

--- src/SME.SGP.Dominio/Constantes/MensagensNegocio/MensagemNegocioEOL.cs
+++ src/SME.SGP.Dominio/Constantes/MensagensNegocio/MensagemNegocioEOL.cs
@@ -5,6 +5,6 @@
         public const string NAO_LOCALIZADO_DISCIPLINAS_TURMA_EOL = "Não localizado disciplinas para a turma no EOL!";
         public const string NAO_LOCALIZADO_INFORMACOES_ALUNO_EOL = "Não localizado informações do Aluno no EOL!";
         public const string NAO_LOCALIZADO_INFORMACOES_ALUNO_TURMA_EOL = "Não localizado informações do Aluno e Turma no EOL!";
-        public const string COMPONENTE_CURRICULAR_NAO_LOCALIZADO_INFORMACOES_EOL = "Componente curricular informado não encontrado no EOL"
+        public const string COMPONENTE_CURRICULAR_NAO_LOCALIZADO_INFORMACOES_EOL = "Componente curricular informado não encontrado no EOL";
     }
 }
