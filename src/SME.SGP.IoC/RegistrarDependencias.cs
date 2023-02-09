62ad80a4684dafd51dc8b3096a9e3bf8eaf24c2d
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-02-09 18:08:13 -0300

    Criado endpoint para a consulta de supervisores

--- src/SME.SGP.IoC/RegistrarDependencias.cs
+++ src/SME.SGP.IoC/RegistrarDependencias.cs
@@ -1196,6 +1196,7 @@ namespace SME.SGP.IoC
             services.TryAddScoped<IObterResponsaveisPorDreUseCase, ObterResponsaveisPorDreUseCase>();
             services.TryAddScoped<IAtribuirUeResponsavelUseCase, AtribuirUeResponsavelUseCase>();
             services.TryAddScoped<IObterListaTipoReponsavelUseCase, ObterListaTipoReponsavelUseCase>();
+            services.TryAddScoped<IListarAtribuicoesResponsaveisPorFiltroUseCase, ListarAtribuicoesResponsaveisPorFiltroUseCase>();
 
             // Encaminhamento NAAPA
             services.TryAddScoped<IObterSecoesEncaminhamentosSecaoNAAPAUseCase, ObterSecoesEncaminhamentosSecaoNAAPAUseCase>();
