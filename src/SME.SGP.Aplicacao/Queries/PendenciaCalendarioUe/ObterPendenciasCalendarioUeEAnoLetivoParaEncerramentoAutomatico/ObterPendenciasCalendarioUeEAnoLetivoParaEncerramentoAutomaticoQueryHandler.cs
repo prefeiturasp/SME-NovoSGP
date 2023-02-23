a51abe497b271e18c3e76b313236e7bc08ec53f0
Thiago Fernando <thiago.fernando@msn.com>
2023-02-23 10:18:16 -0300

    Adicionado chaves no name space

--- src/SME.SGP.Aplicacao/Queries/PendenciaCalendarioUe/ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomatico/ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/PendenciaCalendarioUe/ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomatico/ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQueryHandler.cs
@@ -5,20 +5,21 @@ using System.Threading.Tasks;
 using MediatR;
 using SME.SGP.Dominio.Interfaces;
 
-namespace SME.SGP.Aplicacao;
-
-public class ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQueryHandler : IRequestHandler<ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQuery, IEnumerable<long>>
+namespace SME.SGP.Aplicacao
 {
-    public ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQueryHandler(IRepositorioPendenciaCalendarioUe repositorioPendenciaCalendarioUe)
+    public class ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQueryHandler : IRequestHandler<ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQuery, IEnumerable<long>>
     {
-        this.repositorioPendenciaCalendarioUe = repositorioPendenciaCalendarioUe ?? throw new ArgumentNullException(nameof(repositorioPendenciaCalendarioUe));
-    }
+        public ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQueryHandler(IRepositorioPendenciaCalendarioUe repositorioPendenciaCalendarioUe)
+        {
+            this.repositorioPendenciaCalendarioUe = repositorioPendenciaCalendarioUe ?? throw new ArgumentNullException(nameof(repositorioPendenciaCalendarioUe));
+        }
 
-    private readonly IRepositorioPendenciaCalendarioUe repositorioPendenciaCalendarioUe;
+        private readonly IRepositorioPendenciaCalendarioUe repositorioPendenciaCalendarioUe;
 
 
-    public async Task<IEnumerable<long>> Handle(ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQuery request, CancellationToken cancellationToken)
-    {
-        return await repositorioPendenciaCalendarioUe.ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomatico(request.UeId, request.AnoLetivo);
+        public async Task<IEnumerable<long>> Handle(ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomaticoQuery request, CancellationToken cancellationToken)
+        {
+            return await repositorioPendenciaCalendarioUe.ObterPendenciasCalendarioUeEAnoLetivoParaEncerramentoAutomatico(request.UeId, request.AnoLetivo);
+        }
     }
 }
