8d558d527a1b0e6fd56ed6ed0ddff7ca8b47c9f8
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-02-28 12:00:25 -0300

    872727 - Validator

--- src/SME.SGP.Aplicacao/Commands/Fechamento/AtualizarCacheFechamentoNota/AtualizarCacheFechamentoNotaCommand.cs
+++ src/SME.SGP.Aplicacao/Commands/Fechamento/AtualizarCacheFechamentoNota/AtualizarCacheFechamentoNotaCommand.cs
@@ -1,7 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
-using System.Collections.Generic;
 
 namespace SME.SGP.Aplicacao
 {
@@ -23,10 +23,26 @@ namespace SME.SGP.Aplicacao
 
         public FechamentoNota FechamentoNota { get; set; }
         public string CodigoAluno { get; set; }
-        public long FechamentoTurmaDisciplinaId { get; set; }
         public string CodigoTurma { get; set; }
         public bool EmAprovacao { get; set; }
-
         public ConselhoClasseAlunosNotaPorFechamentoIdDto ConselhosClasseAlunos;
+    }
+
+    public class AtualizarCacheFechamentoNotaCommandValidator : AbstractValidator<AtualizarCacheFechamentoNotaCommand>
+    {
+        public AtualizarCacheFechamentoNotaCommandValidator()
+        {
+            RuleFor(a => a.FechamentoNota)
+                .NotEmpty()
+                .WithMessage("O fechamento nota deve ser informado para atualização do cache");
+
+            RuleFor(a => a.CodigoAluno)
+                .NotEmpty()
+                .WithMessage("O código do aluno deve ser informado para atualização do cache");
+
+            RuleFor(a => a.CodigoTurma)
+                .NotEmpty()
+                .WithMessage("O código da turma deve ser informado para atualização do cache");
+        }
     }
 }
