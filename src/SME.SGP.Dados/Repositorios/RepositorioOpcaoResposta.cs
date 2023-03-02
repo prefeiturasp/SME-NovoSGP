5a9a7c89b2273e9c7ef8fa74c60f5311c6d41475
jailsvs <jailsvs@gmail.com>
2023-03-02 17:31:57 -0300

    87413 - Query mediator para busca de opcoes de resposta de questoes

--- src/SME.SGP.Dados/Repositorios/RepositorioOpcaoResposta.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioOpcaoResposta.cs
@@ -14,6 +14,18 @@ namespace SME.SGP.Dados.Repositorios
         {
         }
 
+        public Task<IEnumerable<OpcaoRespostaSimplesDto>> ObterOpcoesRespostaPorNomeComponenteQuestaoTipoQuestionario(string nomeComponente, TipoQuestionario tipoQuestionario)
+        {
+            var query = @"select opcres.id, opcres.ordem, opcres.nome, opcres.questao_id as questaoId, opcres.observacao
+                          from opcao_resposta opcres
+                          inner join questao q on q.id = opcres.questao_id 
+                          inner join questionario q2 on q2.id = q.questionario_id 
+                          where q2.tipo = @tipoQuestionario and not opcres.excluido and not q.excluido 
+                                and q.nome_componente = @nomeComponente";
+
+            return database.Conexao.QueryAsync<OpcaoRespostaSimplesDto>(query, new { nomeComponente, tipoQuestionario = (int)tipoQuestionario });
+        }
+
         public Task<IEnumerable<OpcaoRespostaSimplesDto>> ObterOpcoesRespostaPorQuestaoId(long QuestaoId)
         {
             var query = @"select or2.id, or2.ordem, or2.nome, or2.questao_id as questaoId, or2.observacao
