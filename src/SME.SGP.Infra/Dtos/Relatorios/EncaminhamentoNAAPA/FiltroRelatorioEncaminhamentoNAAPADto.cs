2ac9ce475cddb8438184d3f7c6e3ad8b0d2a05af
jailsvs <jailsvs@gmail.com>
2023-03-03 15:03:54 -0300

    87411 - Filtro relatório

--- src/SME.SGP.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/FiltroRelatorioEncaminhamentoNAAPADto.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/FiltroRelatorioEncaminhamentoNAAPADto.cs
@@ -2,16 +2,12 @@
 {
     public class FiltroRelatorioEncaminhamentoNAAPADto
     {
-        public int AnoLetivo { get; set; }
         public string DreCodigo { get; set; }
         public string UeCodigo { get; set; }
-        public int Modalidade { get; set; }
-        public int Semestre { get; set; }
-        public string[] CodigosTurma { get; set; }
         public int[] SituacaoIds { get; set; }
         public bool ExibirEncerrados { get; set; }
-        public string[] CodigosPAAIResponsavel { get; set; }
-        public string UsuarioNome { get; set; }
-        public string UsuarioRf { get; set; }
+        public int[] FluxoAlertaIds { get; set; }
+        public int[] PortaEntradaIds { get; set; }
+
     }
 }
