3c88ec4eb969ba3336a56d5d0e3dd670e7dfd20c
Juliano Martins <juliano_campos_92@hotmail.com>
2023-03-03 08:37:57 -0400

    Task 86794: Criar o novo campo de observações complementares

---
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioHistoricoEscolarObservacao.cs
@@ -0,0 +1,6 @@
+﻿namespace SME.SGP.Dominio.Interfaces
+{
+    public interface IRepositorioHistoricoEscolarObservacao : IRepositorioBase<HistoricoEscolarObservacao>
+    {
+    }
+}
---
+++ src/SME.SGP.Dominio/Entidades/HistoricoEscolarObservacao.cs
@@ -0,0 +1,8 @@
+namespace SME.SGP.Dominio
+{
+	public class HistoricoEscolarObservacao : EntidadeBase
+	{
+        public string AlunoCodigo { get; set; }
+        public string Observacao { get; set; }
+    }
+}
