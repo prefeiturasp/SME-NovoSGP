be07e7d6e6f800b5cf3d0e9ed044cae14f7da752
Felipe Abreu <felipe.abreu@amcom.com.br>
2023-03-10 11:26:36 -0300

    Revertendo o usuário do parâmetro de turma para obter os componentes por ID.

--- src/SME.SGP.Aplicacao/CasosDeUso/PlanoAula/ObterPlanoAulaUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/PlanoAula/ObterPlanoAulaUseCase.cs
@@ -23,13 +23,11 @@ namespace SME.SGP.Aplicacao
             
             var planoAulaDto = MapearParaDto(planoAula) ?? new PlanoAulaRetornoDto();
 
-            var turma = await mediator.Send(new ObterTurmaPorIdQuery(filtro.TurmaId));
-
             DisciplinaDto disciplinaDto = null;
 
             if (filtro.ComponenteCurricularId.HasValue)
             {
-                var disciplinasRetorno = await mediator.Send(new ObterComponentesCurricularesPorIdsQuery(new long[] { filtro.ComponenteCurricularId.Value }, codigoTurma: turma.CodigoTurma));
+                var disciplinasRetorno = await mediator.Send(new ObterComponentesCurricularesPorIdsQuery(new long[] { filtro.ComponenteCurricularId.Value }));
                 disciplinaDto = disciplinasRetorno.SingleOrDefault();
             }
 
@@ -38,7 +36,7 @@ namespace SME.SGP.Aplicacao
             if (periodoEscolar == null)
                 throw new NegocioException("Período escolar não localizado.");           
 
-            var planejamentoAnualPeriodoId = await mediator.Send(new ExistePlanejamentoAnualParaTurmaPeriodoEComponenteQuery(filtro.TurmaId, periodoEscolar.Id, disciplinaDto != null ? disciplinaDto.Id : long.Parse(aulaDto.DisciplinaId)));
+            var planejamentoAnualPeriodoId = await mediator.Send(new ExistePlanejamentoAnualParaTurmaPeriodoEComponenteQuery(filtro.TurmaId, periodoEscolar.Id, disciplinaDto != null ? disciplinaDto.CodigoComponenteCurricular : long.Parse(aulaDto.DisciplinaId)));
             
             if (planejamentoAnualPeriodoId == 0 
                 && periodoEscolar.TipoCalendario.AnoLetivo.Equals(DateTime.Now.Year) 
