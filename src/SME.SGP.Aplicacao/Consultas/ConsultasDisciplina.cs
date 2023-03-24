4e3156c1effc4f11de76ac6f02e8b0d6519bf6ad
Felipe Abreu <felipe.abreu@amcom.com.br>
2023-03-24 09:55:07 -0300

    Corrigindo código do território para professor.

--- src/SME.SGP.Aplicacao/Consultas/ConsultasDisciplina.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasDisciplina.cs
@@ -138,12 +138,13 @@ namespace SME.SGP.Aplicacao
                         .Send(new ObterComponentesCurricularesEolPorCodigoTurmaLoginEPerfilQuery(codigoTurma, usuarioLogado.Login, usuarioLogado.PerfilAtual, realizarAgrupamentoComponente))).ToList();
 
                     componentesCurriculares ??= (await mediator
-                        .Send(new ObterComponentesCurricularesEolPorCodigoTurmaLoginEPerfilQuery(codigoTurma, usuarioLogado.Login, usuarioLogado.PerfilAtual, realizarAgrupamentoComponente, false))).ToList();
+                        .Send(new ObterComponentesCurricularesEolPorCodigoTurmaLoginEPerfilQuery(codigoTurma, usuarioLogado.Login, usuarioLogado.PerfilAtual, realizarAgrupamentoComponente, false))).ToList();                    
 
                     componentesCurriculares.ForEach(c =>
                     {
+                        var codigoTerritorio = c.Codigo;
                         c.Codigo = c.TerritorioSaber ? c.CodigoComponenteTerritorioSaber : c.Codigo;
-                        c.CodigoComponenteTerritorioSaber = c.TerritorioSaber ? c.CodigoComponenteTerritorioSaber : 0;
+                        c.CodigoComponenteTerritorioSaber = c.TerritorioSaber ? codigoTerritorio : c.CodigoComponenteTerritorioSaber;
                     });
                 }
                 else
@@ -170,7 +171,7 @@ namespace SME.SGP.Aplicacao
                     }                    
                 }
 
-                var idsDisciplinas = componentesCurriculares?.Select(a => a.TerritorioSaber ? a.CodigoComponenteTerritorioSaber : a.Codigo).ToArray();
+                var idsDisciplinas = componentesCurriculares?.Select(a => a.Codigo).ToArray();
 
                 if (usuarioLogado.TemPerfilAdmUE() || usuarioLogado.TemPerfilGestaoUes())
                     idsDisciplinas = await ObterDisciplinasAtribuicaoCJParaTurma(codigoTurma, componentesCurriculares, idsDisciplinas);
@@ -184,7 +185,7 @@ namespace SME.SGP.Aplicacao
 
                 disciplinasDto.ForEach(d =>
                 {
-                    var componenteEOL = componentesCurriculares.FirstOrDefault(a => a.TerritorioSaber ? a.CodigoComponenteTerritorioSaber == d.CodigoComponenteCurricular : a.Codigo == d.CodigoComponenteCurricular);
+                    var componenteEOL = componentesCurriculares.FirstOrDefault(a => a.Codigo == d.CodigoComponenteCurricular);
 
                     d.PossuiObjetivos = turma.AnoLetivo >= Convert.ToInt32(dataInicioNovoSGP) && componenteEOL.PossuiObjetivosDeAprendizagem(componentesCurricularesJurema, turmaPrograma, turma.ModalidadeCodigo, turma.Ano);
                     d.CodigoComponenteCurricular = componenteEOL.Codigo;
