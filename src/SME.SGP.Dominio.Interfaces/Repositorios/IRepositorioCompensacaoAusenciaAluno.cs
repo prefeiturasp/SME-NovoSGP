35f1f46998fb880909be585a1bb6fd91257091eb
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-04-05 17:44:09 -0300

    Retornando método alterado para exclusão não lógica

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioCompensacaoAusenciaAluno.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioCompensacaoAusenciaAluno.cs
@@ -8,6 +8,7 @@ namespace SME.SGP.Dominio.Interfaces
     {
         Task<bool> InserirVarios(IEnumerable<CompensacaoAusenciaAluno> registros, Usuario usuarioLogado);
         Task<bool> AlterarQuantidadeCompensacoesPorCompensacaoAlunoId(long compensacaoAusenciaAlunoId, int quantidade);
+        Task<bool> ExcluirCompensacaoAusenciaAlunoPorId(long id);
         Task<bool> ExclusaoLogicaCompensacaoAusenciaAlunoPorId(long id);
     }
 }
