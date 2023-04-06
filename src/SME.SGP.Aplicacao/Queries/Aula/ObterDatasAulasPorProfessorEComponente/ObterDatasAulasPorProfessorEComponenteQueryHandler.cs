44f4df725ab4cdbc5aa2fb2eba2e2e7817527d25
Helio Barros <helio.barros@amcom.com.br>
2023-04-06 12:33:03 -0300

    AB#91117  - ajuste ao retornar datas corretas do componente e exibir se realmente possui uma ou mais aulas do mesmo componente no dia (bug: 90756)

--- src/SME.SGP.Aplicacao/Queries/Aula/ObterDatasAulasPorProfessorEComponente/ObterDatasAulasPorProfessorEComponenteQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/Aula/ObterDatasAulasPorProfessorEComponente/ObterDatasAulasPorProfessorEComponenteQueryHandler.cs
@@ -59,7 +59,7 @@ namespace SME.SGP.Aplicacao
                         Codigo = componenteCurricularCorrespondente?.Codigo ?? (long.TryParse(request.ComponenteCurricularCodigo, out long codigo) ? codigo : 0),
                         CodigoComponenteCurricularPai = componentesCurricularesDoProfessorCj.Any() ? componentesCurricularesDoProfessorCj.Select(c => long.TryParse(c.codigoComponentePai, out long codigoPai) ? codigoPai : 0).FirstOrDefault() : componenteCurricularCorrespondente?.CodigoComponenteCurricularPai ?? 0,
                         CodigoComponenteTerritorioSaber = componentesCurricularesDoProfessorCj.Any() ? 
-                            componentesCurricularesDoProfessorCj.Select(c => long.TryParse(c.codigoTerritorioSaber, out long codigoTerritorio) ? codigoTerritorio : 0).FirstOrDefault(n => n != 0) 
+                            componentesCurricularesDoProfessorCj.Where(c=> c.codigo == request.ComponenteCurricularCodigo).Select(c => long.TryParse(c.codigoTerritorioSaber, out long codigoTerritorio) ? codigoTerritorio : 0).FirstOrDefault() 
                             : componenteCurricularCorrespondente?.CodigoComponenteTerritorioSaber ?? 0
                     };
 
