a9b6276aa0219fc9f5e1990f75961d7514fb1291
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-04-25 08:17:02 -0300

    91452 - Criar endpoint para obter os alunos e as observações complementares

---
+++ src/SME.SGP.Infra/Dtos/HistoricoEscolar/AlunoComObservacaoDoHistoricoEscolarDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SME.SGP.Infra
+{
+    public class AlunoComObservacaoDoHistoricoEscolarDto
+    {
+        public int NumeroChamada { get; set; }
+        public string CodigoAluno { get; set; }
+        public string NomeAluno { get; set; }
+        public string Observacao { get; set; }
+    }
+}
