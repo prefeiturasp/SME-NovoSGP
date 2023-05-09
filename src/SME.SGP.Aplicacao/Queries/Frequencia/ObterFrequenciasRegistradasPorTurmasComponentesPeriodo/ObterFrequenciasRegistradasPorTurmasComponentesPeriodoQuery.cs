028955c41b1ba1db21e060c6683b2d1c4d94272b
Felipe Abreu <felipe.abreu@amcom.com.br>
2023-05-09 11:42:00 -0300

    Obter quantidade de presenças pelas frequencias registradas para o aluno.

---
+++ src/SME.SGP.Aplicacao/Queries/Frequencia/ObterFrequenciasRegistradasPorTurmasComponentesPeriodo/ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dto;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+
+{
+    public class ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQuery : IRequest<IEnumerable<FrequenciaRegistradaDto>>
+    {
+        public ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQuery(string codigosTurma, long[] componentesCurricularesId, IEnumerable<long> periodosEscolaresIds)
+        {
+            CodigosTurma = codigosTurma;
+            ComponentesCurricularesId = componentesCurricularesId;
+            PeriodosEscolaresId = periodosEscolaresIds;
+        }
+        public string CodigosTurma { get; set; }
+        public long[] ComponentesCurricularesId { get; set; }
+        public IEnumerable<long> PeriodosEscolaresId { get; set; }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/Frequencia/ObterFrequenciasRegistradasPorTurmasComponentesPeriodo/ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Dto;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQueryHandler : IRequestHandler<ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQuery, IEnumerable<FrequenciaRegistradaDto>>
+    {
+        private readonly IRepositorioFrequenciaAlunoDisciplinaPeriodoConsulta repositorioFrequenciaAlunoDisciplinaPeriodo;
+
+        public ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQueryHandler(IRepositorioFrequenciaAlunoDisciplinaPeriodoConsulta repositorioFrequenciaAlunoDisciplinaPeriodo)
+        {
+            this.repositorioFrequenciaAlunoDisciplinaPeriodo = repositorioFrequenciaAlunoDisciplinaPeriodo;
+        }
+
+        public async Task<IEnumerable<FrequenciaRegistradaDto>> Handle(ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQuery request, CancellationToken cancellationToken)
+            => await repositorioFrequenciaAlunoDisciplinaPeriodo.ObterFrequenciaRegistradaPorTurmaDisciplinaEPeriodo(request.CodigosTurma, request.ComponentesCurricularesId.Select(cc => cc).ToArray(), request.PeriodosEscolaresId);
+    }
+}
