8322a9253debb49fc4c7fc12c3a3b19d9d790ccb
jailsvs <jailsvs@gmail.com>
2023-05-11 15:28:37 -0300

    93899 - Revert de fonte gerado no bug 92357 - estava impactando apresentação de aulas no fechamento conselho de classe

--- src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoFrequencia/ObterInformacoesDeFrequenciaAlunosPorBimestreUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoFrequencia/ObterInformacoesDeFrequenciaAlunosPorBimestreUseCase.cs
@@ -54,7 +54,6 @@ namespace SME.SGP.Aplicacao
 
             var alunos = (await mediator.Send(new ObterAlunosDentroPeriodoQuery(turma.CodigoTurma,
                     (bimestreDoPeriodo.PeriodoInicio, bimestreDoPeriodo.PeriodoFim), ehBimestreFinal)))
-                .DistinctBy(a => a.CodigoAluno)
                 .OrderBy(a => a.NomeSocialAluno ?? a.NomeAluno);
 
             if (!alunos?.Any() ?? true)
@@ -227,41 +226,29 @@ namespace SME.SGP.Aplicacao
             var frequenciaAlunosRegistrada = await mediator
                 .Send(new ObterFrequenciaAlunosPorTurmaDisciplinaEPeriodoEscolarQuery(turma, componentesCurricularesId, periodosEscolaresIds, professor));
 
-            var presencasRegistradas = await mediator
-                .Send(new ObterFrequenciasRegistradasPorTurmasComponentesPeriodoQuery(turma.CodigoTurma, componentesCurricularesId, periodosEscolaresIds));
-
             var alunosPorTurma = alunos.ToList();
 
             return frequenciaAlunosRegistrada
                 .GroupBy(x => x.CodigoAluno)
-                .Select(x => ObterFrequenciaAluno(x, alunosPorTurma, presencasRegistradas).Result)
+                .Select(x => ObterFrequenciaAluno(x, alunosPorTurma).Result)
                 .ToList();
         }
 
-        private async Task<FrequenciaAluno> ObterFrequenciaAluno(IGrouping<string, FrequenciaAluno> agrupamentoAluno, IEnumerable<AlunoPorTurmaResposta> alunos, IEnumerable<FrequenciaRegistradaDto> presencasRegistradas)
+        private async Task<FrequenciaAluno> ObterFrequenciaAluno(IGrouping<string, FrequenciaAluno> agrupamentoAluno, IEnumerable<AlunoPorTurmaResposta> alunos)
         {
             var frequenciasAluno = agrupamentoAluno.ToList();
 
             var frequenciasConsideradas = from frequenciaAluno in frequenciasAluno
                                           join aluno in alunos on frequenciaAluno.CodigoAluno equals aluno.CodigoAluno
-                                          join presenca in presencasRegistradas on frequenciaAluno.CodigoAluno equals presenca.CodigoAluno
                                           where aluno.DataMatricula.Date <= frequenciaAluno.PeriodoFim.Date
-                                          select new
-                                          {
-                                              frequenciaAluno.CodigoAluno,
-                                              frequenciaAluno.TotalAulas,
-                                              frequenciaAluno.TotalCompensacoes,
-                                              frequenciaAluno.TotalRemotos,
-                                              frequenciaAluno.TotalAusencias,
-                                              presenca.QuantidadePresencas                                              
-                                          };
+                                          select frequenciaAluno;
             return new FrequenciaAluno
             {
                 CodigoAluno = agrupamentoAluno.Key,
                 TotalAulas = frequenciasConsideradas.Sum(y => y.TotalAulas),
                 TotalAusencias = frequenciasConsideradas.Sum(y => y.TotalAusencias),
                 TotalCompensacoes = frequenciasConsideradas.Sum(y => y.TotalCompensacoes),
-                TotalPresencas = frequenciasConsideradas.Sum(y => y.QuantidadePresencas),
+                TotalPresencas = frequenciasConsideradas.Sum(y => y.TotalPresencas),
                 TotalRemotos = frequenciasConsideradas.Sum(y => y.TotalRemotos),
             };
         }
