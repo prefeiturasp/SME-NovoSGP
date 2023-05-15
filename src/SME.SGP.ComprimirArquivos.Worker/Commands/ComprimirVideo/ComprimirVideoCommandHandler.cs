da74cffc33f3648d95d288a7542f8adf91027c8b
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-05-15 14:37:31 -0300

    Correção das filas e da resolução dos vídeos

--- src/SME.SGP.ComprimirArquivos.Worker/Commands/ComprimirVideo/ComprimirVideoCommandHandler.cs
+++ src/SME.SGP.ComprimirArquivos.Worker/Commands/ComprimirVideo/ComprimirVideoCommandHandler.cs
@@ -31,7 +31,7 @@ namespace SME.SGP.ComprimirArquivos.Worker
 
                 var output = Path.Combine(UtilArquivo.ObterDiretorioCompletoTemporario(), request.NomeArquivo);                
 
-                FFMpeg.Convert(input, output, VideoType.Mp4, Speed.UltraFast, VideoSize.Ld, AudioQuality.Normal, true);
+                FFMpeg.Convert(input, output, VideoType.Mp4, Speed.UltraFast, VideoSize.Hd, AudioQuality.Normal, true);
 
                 await mediator.Send(new MoverExcluirArquivoFisicoCommand(input, output));
             
