497153a2a269c353128927cf5dba40657853a4f0
Thiago Fernando <thiago.fernando@msn.com>
2023-05-16 15:28:15 -0300

    Criado Command de Carga

---
+++ src/SME.SGP.Aplicacao/Commands/Pendencia/CargaPendenciasQuantidadeDiasQuantidadeAulasCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class CargaPendenciasQuantidadeDiasQuantidadeAulasCommand : IRequest<bool>
+    {
+        public CargaPendenciasQuantidadeDiasQuantidadeAulasCommand(CargaAulasDiasPendenciaDto carga)
+        {
+            Carga = carga;
+        }
+
+        public CargaAulasDiasPendenciaDto Carga { get; set; }
+    }
+}
