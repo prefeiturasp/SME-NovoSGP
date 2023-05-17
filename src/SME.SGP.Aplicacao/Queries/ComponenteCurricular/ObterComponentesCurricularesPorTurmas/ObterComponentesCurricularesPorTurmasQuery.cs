6b7f213b271f4e999809c09a5eeb2c1bf7defbae
rafalosiamcom <rafael.losi@amcom.com.br>
2023-05-17 17:32:10 -0300

    94089 - Removido verificação de ausencia na conciliação de frequencia

---
+++ src/SME.SGP.Aplicacao/Queries/ComponenteCurricular/ObterComponentesCurricularesPorTurmas/ObterComponentesCurricularesPorTurmasQuery.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Dominio;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterComponentesCurricularesPorTurmasQuery : IRequest<IEnumerable<ComponenteCurricularEol>>
+    {
+        public ObterComponentesCurricularesPorTurmasQuery(string[] codigosTurmas)
+        {
+            CodigosTurmas = codigosTurmas;
+        }
+
+        public string[] CodigosTurmas { get; }
+    }
+
+    public class ObterComponentesCurricularesPorTurmasQueryValidator : AbstractValidator<ObterComponentesCurricularesPorTurmasQuery>
+    {
+        public ObterComponentesCurricularesPorTurmasQueryValidator()
+        {
+            RuleFor(a => a.CodigosTurmas)
+                .NotEmpty()
+                .WithMessage("Necessário informar os códigos de turmas para consulta de componentes curriculares.");
+        }
+    }
+}
