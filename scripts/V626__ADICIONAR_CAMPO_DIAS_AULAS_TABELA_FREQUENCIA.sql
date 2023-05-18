e49fa159019ad21b6dec027b6aa0dc7c4d3a3614
Thiago Fernando <thiago.fernando@msn.com>
2023-05-18 08:58:48 -0300

    Ajustes nome fila e Script sql

--- scripts/V626__ADICIONAR_CAMPO_DIAS_AULAS_TABELA_FREQUENCIA.sql
+++ scripts/V626__ADICIONAR_CAMPO_DIAS_AULAS_TABELA_FREQUENCIA.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.pendencia ADD qtde_aulas int4 NULL;
-ALTER TABLE public.pendencia ADD qtde_dias int4 NULL;
+ALTER TABLE public.pendencia ADD IF NOT exists qtde_aulas int4 NULL;
+ALTER TABLE public.pendencia ADD IF NOT exists qtde_dias int4 NULL;
