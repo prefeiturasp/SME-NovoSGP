7eee9ad2ba9455580f91eb86d732fc48e9f3f430
jailsvs <jailsvs@gmail.com>
2023-05-24 14:07:39 -0300

    81683 - Ajuste script

--- scripts/V628__ADICIONAR_CAMPOS_NOTA_CONCEITO_OLD_EXCLUIDO_WFS.sql
+++ scripts/V628__ADICIONAR_CAMPOS_NOTA_CONCEITO_OLD_EXCLUIDO_WFS.sql
@@ -5,3 +5,4 @@ ALTER TABLE if exists public.wf_aprovacao_nota_conselho ADD column if not exists
 
 ALTER TABLE public.wf_aprovacao_nota_fechamento ADD IF NOT exists nota_anterior numeric(5, 2) NULL;
 ALTER TABLE public.wf_aprovacao_nota_fechamento ADD IF NOT exists conceito_id_anterior int8 NULL;
+ALTER TABLE public.wf_aprovacao_nota_fechamento ADD CONSTRAINT wf_aprovacao_nota_fechamento_conceito_anterior_fk FOREIGN KEY (conceito_id_anterior) REFERENCES conceito_valores(id);
