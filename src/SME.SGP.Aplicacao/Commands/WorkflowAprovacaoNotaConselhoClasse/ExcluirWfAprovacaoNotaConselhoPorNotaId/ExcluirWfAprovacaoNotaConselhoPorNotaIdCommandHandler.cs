49290e9afa4c6a10f99fdb9d5b4a878125581118
jailsvs <jailsvs@gmail.com>
2023-05-24 16:52:46 -0300

    81683 - Melhoria para caso todos os wf_aprovacao_nota_conselho sejam excluidos (recriados) apagar o wf aprovação (agrupador) e notificação, evitando erro de visualização de notificação pelo CP/Supervisor

--- src/SME.SGP.Aplicacao/Commands/WorkflowAprovacaoNotaConselhoClasse/ExcluirWfAprovacaoNotaConselhoPorNotaId/ExcluirWfAprovacaoNotaConselhoPorNotaIdCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/WorkflowAprovacaoNotaConselhoClasse/ExcluirWfAprovacaoNotaConselhoPorNotaId/ExcluirWfAprovacaoNotaConselhoPorNotaIdCommandHandler.cs
@@ -1,5 +1,9 @@
 ﻿using MediatR;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,8 +23,29 @@ namespace SME.SGP.Aplicacao
         protected override async Task Handle(ExcluirWfAprovacaoNotaConselhoPorNotaIdCommand request, CancellationToken cancellationToken)
         {
             var wfAprovacaoNotas = await repositorioWFAprovacaoNotaConselho.ObterWorkflowAprovacaoNota(request.ConselhoClasseNotaId);
-            foreach(var wfAprovacaoNota in wfAprovacaoNotas)
-                await mediator.Send(new ExcluirWfAprovacaoNotaConselhoClasseCommand(wfAprovacaoNota.Id));
+            foreach (var wfAprovacaoNota in wfAprovacaoNotas)
+            {
+                await repositorioWFAprovacaoNotaConselho.RemoverAsync(wfAprovacaoNota);
+                await PublicarFilaExclusaoWfAprovacao(wfAprovacaoNota.WfAprovacaoId, wfAprovacaoNota.Id);
+            }
+        }
+
+        private async Task PublicarFilaExclusaoWfAprovacao(long? wfAprovacaoId, long wfNotaConselho)
+        {
+            if (wfAprovacaoId.HasValue)
+            {
+                var wfNotasPosConselho = await mediator.Send(new ObterIdsWorkflowPorWfAprovacaoIdQuery(wfAprovacaoId.Value, "wf_aprovacao_nota_conselho"));
+                if (wfNotasPosConselho == null || !wfNotasPosConselho.Except(new long[] { wfNotaConselho } ).Any())
+                {
+                    var usuarioLogado = await mediator.Send(new ObterUsuarioLogadoQuery());
+                    await PulicaFilaSgp(RotasRabbitSgp.WorkflowAprovacaoExcluir, wfAprovacaoId.Value, usuarioLogado);
+                }
+            }
+        }
+
+        private async Task PulicaFilaSgp(string fila, long id, Usuario usuario)
+        {
+            await mediator.Send(new PublicarFilaSgpCommand(fila, new FiltroIdDto(id), Guid.NewGuid(), usuario));
         }
     }
 }
---
+++ src/SME.SGP.Aplicacao/Queries/Workflow/ObterIdsWorkflowPorWfAprovacaoId/ObterIdsWorkflowPorWfAprovacaoIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterIdsWorkflowPorWfAprovacaoIdQuery : IRequest<IEnumerable<long>>
+    {
+        public ObterIdsWorkflowPorWfAprovacaoIdQuery(long workflowId, string tabelaVinculada)
+        {
+            WorkflowId = workflowId;
+            TabelaVinculada = tabelaVinculada;
+        }
+        public long WorkflowId { get; set; }
+        public string TabelaVinculada { get; }
+    }
+}
