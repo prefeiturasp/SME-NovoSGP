5948e011cf2c836bb5878fba01bc267bf509d46a
Thiago Fernando <thiago.fernando@msn.com>
2023-05-26 15:51:29 -0300

    Criado endpoint

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/DashboardNaapa/ObterQuantidadeEncaminhamentoPorSituacaoUseCase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterQuantidadeEncaminhamentoPorSituacaoUseCase : IObterQuantidadeEncaminhamentoPorSituacaoUseCase
+    {
+        private readonly IRepositorioConsolidadoEncaminhamentoNAAPA repositorio;
+        public async Task<IEnumerable<GraficoFrequenciaTurmaEvasaoDto>> Executar(FiltroGraficoEncaminhamentoPorSituacaoDto param)
+        {
+            var graficos = new List<GraficoFrequenciaTurmaEvasaoDto>();
+            var consultaDados = await repositorio.ObterDadosGraficoSitaucaoPorUeAnoLetivo(param.AnoLetivo,param.UeId);
+
+            foreach (var grafico in consultaDados)
+            {
+                var item = new GraficoFrequenciaTurmaEvasaoDto
+                {
+                    Quantidade = int.Parse(grafico.Quantiddade.ToString()),
+                    DataUltimaConsolidacao = grafico.DataUltimaConsolidacao,
+                    Descricao = grafico.Situacao.Name()
+                    
+                };
+                graficos.Add(item);
+            }
+            return graficos;
+        }
+    }
+}
