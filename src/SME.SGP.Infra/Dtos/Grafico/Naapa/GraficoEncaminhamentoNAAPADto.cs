ae75f4efaf94ac4fb36b48c6d1f52eea50d60446
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-05-29 10:32:59 -0300

    64239 - Dto gráfico encaminhamento

---
+++ src/SME.SGP.Infra/Dtos/Grafico/Naapa/GraficoEncaminhamentoNAAPADto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SME.SGP.Infra
+{
+    public class GraficoEncaminhamentoNAAPADto
+    {
+        public DateTime? DataUltimaConsolidacao { get; set; }
+        public List<GraficoBaseDto> Graficos { get; set; }
+    }
+}
