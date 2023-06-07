fe8be91d7328bc85cb1d7d43eca0cd628ad95893
Juliano Martins <juliano_campos_92@hotmail.com>
2023-06-07 19:25:51 -0400

    Correção para que ao carregar o projeto no container não realize o scanning no NFS

--- src/SME.SGP.ComprimirArquivos.Worker/Commands/ComprimirImagem/ComprimirImagemCommandHandler.cs
+++ src/SME.SGP.ComprimirArquivos.Worker/Commands/ComprimirImagem/ComprimirImagemCommandHandler.cs
@@ -1,27 +1,28 @@
-﻿using System;
-using MediatR;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using MediatR;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
-using SME.SGP.Aplicacao;
 using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SME.SGP.ComprimirArquivos.Worker
 {
     public class ComprimirImagemCommandHandler : IRequestHandler<ComprimirImagemCommand, bool>
     {
         private readonly IMediator mediator;
-        
-        public ComprimirImagemCommandHandler(IMediator mediator)
+        private readonly CaminhoArmazenamentoOptions caminhoArmazenamentoOptions;
+
+        public ComprimirImagemCommandHandler(IMediator mediator, CaminhoArmazenamentoOptions caminhoArmazenamentoOptions)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.caminhoArmazenamentoOptions = caminhoArmazenamentoOptions ?? throw new ArgumentNullException(nameof(caminhoArmazenamentoOptions));
         }
         
         public async Task<bool> Handle(ComprimirImagemCommand request, CancellationToken cancellationToken)
@@ -31,12 +32,12 @@ namespace SME.SGP.ComprimirArquivos.Worker
                 if (!request.NomeArquivo.EhArquivoImagemParaOtimizar())
                     return false;
             
-                var input = Path.Combine(UtilArquivo.ObterDiretorioCompletoArquivos(), request.NomeArquivo);
+                var input = Path.Combine(caminhoArmazenamentoOptions.CaminhoFisico, request.NomeArquivo);
 
                 if (!File.Exists(input))
                     await mediator.Send(new SalvarLogViaRabbitCommand($"O arquivo '{request.NomeArquivo}' não foi localizado no endereço '{input}'", LogNivel.Critico, LogContexto.ComprimirArquivos)); 
 
-                var output = Path.Combine(UtilArquivo.ObterDiretorioCompletoTemporario(), request.NomeArquivo);
+                var output = Path.Combine(caminhoArmazenamentoOptions.CaminhoTemporario, request.NomeArquivo);
 
                 using (Image image = Image.Load(input))
                 {
