623d7db9dd44233321e64ca53334edbdbbe066b0
Juliano Martins <juliano.campos@amcom.com.br>
2023-06-29 17:43:23 -0400

    correção teste

--- scripts/V637__INSERIR_COMPONENTE_CURRICULAR_JUREMA.sql
+++ scripts/V637__INSERIR_COMPONENTE_CURRICULAR_JUREMA.sql
@@ -1,8 +1,13 @@
-insert into componente_curricular_jurema (codigo_jurema, codigo_eol, descricao_eol, criado_em, criado_por, criado_rf) 
-values (14, 1322, 'PAP - RECUPERACAO DE APRENDIZAGENS', CURRENT_TIMESTAMP, 'Carga', 'Carga'),
-       (14, 1770, 'PAP PROJETO COLABORATIVO', CURRENT_TIMESTAMP, 'Carga', 'Carga');
+DO $$
+BEGIN
+IF (select count(*) from componente_curricular cc where cc.id in (1322, 1770)) = 2 THEN
+	insert into componente_curricular_jurema (codigo_jurema, codigo_eol, descricao_eol, criado_em, criado_por, criado_rf) 
+	values (14, 1322, 'PAP - RECUPERACAO DE APRENDIZAGENS', CURRENT_TIMESTAMP, 'Carga', 'Carga'),
+		   (14, 1770, 'PAP PROJETO COLABORATIVO', CURRENT_TIMESTAMP, 'Carga', 'Carga');
 
-insert into componente_curriculo_cidade(codigo, componente_curricular_id)
-select ccj.codigo_jurema, ccj.codigo_eol 
-from componente_curricular_jurema ccj
-where ccj.codigo_eol in (1322, 1770);
+	insert into componente_curriculo_cidade(codigo, componente_curricular_id)
+	select ccj.codigo_jurema, ccj.codigo_eol 
+	from componente_curricular_jurema ccj
+	where ccj.codigo_eol in (1322, 1770);
+END if;
+END $$;
