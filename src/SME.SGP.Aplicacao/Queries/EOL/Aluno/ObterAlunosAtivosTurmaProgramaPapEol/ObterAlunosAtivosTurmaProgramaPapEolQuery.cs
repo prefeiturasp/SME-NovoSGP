3c539a2095fe9e292334ccb5e20f07c0a0ccde0c
Thiago Fernando <thiago.fernando@msn.com>
2023-07-05 15:52:05 -0300

    Ajsutes

--- src/SME.SGP.Aplicacao/Queries/EOL/Aluno/ObterAlunosAtivosTurmaProgramaPapEol/ObterAlunosAtivosTurmaProgramaPapEolQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/EOL/Aluno/ObterAlunosAtivosTurmaProgramaPapEol/ObterAlunosAtivosTurmaProgramaPapEolQuery.cs
@@ -7,6 +7,12 @@ namespace SME.SGP.Aplicacao
 {
     public class ObterAlunosAtivosTurmaProgramaPapEolQuery : IRequest<IEnumerable<AlunosTurmaProgramaPapDto>>
     {
+        public ObterAlunosAtivosTurmaProgramaPapEolQuery(int anoLetivo, string[] alunosCodigos)
+        {
+            AnoLetivo = anoLetivo;
+            AlunosCodigos = alunosCodigos;
+        }
+
         public int AnoLetivo { get; set; }
         public string[] AlunosCodigos { get; set; }
     }
