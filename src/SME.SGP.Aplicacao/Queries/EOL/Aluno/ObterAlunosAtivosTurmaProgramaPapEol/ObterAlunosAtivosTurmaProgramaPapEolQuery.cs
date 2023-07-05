3debd72395f774fc5dbb1ad623e6b184bee61376
Thiago Fernando <thiago.fernando@msn.com>
2023-07-05 13:29:44 -0300

    query mediator para checar alunos em turma programa PAP com componentes pré determinados

---
+++ src/SME.SGP.Aplicacao/Queries/EOL/Aluno/ObterAlunosAtivosTurmaProgramaPapEol/ObterAlunosAtivosTurmaProgramaPapEolQuery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentValidation;
+using MediatR;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterAlunosAtivosTurmaProgramaPapEolQuery : IRequest<IEnumerable<AlunosTurmaProgramaPapDto>>
+    {
+        public int AnoLetivo { get; set; }
+        public string[] AlunosCodigos { get; set; }
+    }
+    
+    public class ObterAlunosAtivosTurmaProgramaPapEolQueryValidator : AbstractValidator<ObterAlunosAtivosTurmaProgramaPapEolQuery>
+    {
+        public ObterAlunosAtivosTurmaProgramaPapEolQueryValidator()
+        {
+
+            RuleFor(c => c.AlunosCodigos)
+                .NotEmpty()
+                .WithMessage("O código do aluno deve ser informado para realizar a consulta no EOL.");
+        }
+    }
+}
