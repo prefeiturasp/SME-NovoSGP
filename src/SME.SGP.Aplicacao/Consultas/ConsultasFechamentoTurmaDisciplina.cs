cc37298edde17270339ce2bf650eca22510aaae7
Thiago Fernando <thiago.fernando@msn.com>
2023-07-07 11:56:07 -0300

    Conselho classe

--- src/SME.SGP.Aplicacao/Consultas/ConsultasFechamentoTurmaDisciplina.cs
+++ src/SME.SGP.Aplicacao/Consultas/ConsultasFechamentoTurmaDisciplina.cs
@@ -126,13 +126,26 @@ namespace SME.SGP.Aplicacao
             var dadosAlunos = await consultasTurma.ObterDadosAlunos(turmaCodigo, anoLetivo, periodoEscolar, turma.EhTurmaInfantil);
 
             var dadosAlunosFiltrados = dadosAlunos.Where(d => !d.EstaInativo() || d.EstaInativo() && d.DataSituacao >= primeiroPeriodoDoCalendario).OrderBy(d => d.Nome);
-
-            return dadosAlunosFiltrados.OrderBy(aluno => aluno.CodigoEOL)
+            var matriculadosTurmaPAP = await BuscarAlunosTurmaPAP(dadosAlunosFiltrados.Select(x => x.CodigoEOL).ToArray(), anoLetivo);
+            
+            var listaRetorno = dadosAlunosFiltrados.OrderBy(aluno => aluno.CodigoEOL)
                                        .ThenByDescending(aluno => aluno.DataSituacao)
                                        .GroupBy(aluno => aluno.CodigoEOL)
                                        .Select(aluno => aluno.First())
                                        .OrderBy(aluno => aluno.Nome)
                                        .ToList();
+            
+            return MapearAlunoPap(listaRetorno,matriculadosTurmaPAP);
+        }
+
+        private IEnumerable<AlunoDadosBasicosDto> MapearAlunoPap(List<AlunoDadosBasicosDto> listaAlunos, IEnumerable<AlunosTurmaProgramaPapDto> matriculadosTurmaPAP)
+        {
+            foreach (var aluno in listaAlunos)
+            {
+                aluno.EhMatriculadoTurmaPAP = matriculadosTurmaPAP.Any(x => x.CodigoAluno.ToString() == aluno.CodigoEOL);
+            }
+
+            return listaAlunos;
         }
 
         public async Task<FechamentoTurmaDisciplina> ObterFechamentoTurmaDisciplina(string turmaId, long disciplinaId, int bimestre)
