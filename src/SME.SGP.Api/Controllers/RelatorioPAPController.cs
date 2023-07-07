b67e2bb37f2e04745ec36e7452f8e8a9cf0f44da
DanylloPereira <danyllo.antunes@gmail.com>
2023-07-07 16:25:12 -0300

    95254 - Criação do novo tipo de questão e end point de busca

---
+++ src/SME.SGP.Api/Controllers/RelatorioPAPController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using Polly;
+using SME.SGP.Api.Filtros;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/relatorios/pap")]
+    [Authorize("Bearer")]
+    public class RelatorioPAPController : ControllerBase
+    {
+
+    }
+}
---
+++ teste/SME.SGP.TesteIntegracao/RelatorioPAP/Base/RelatorioPAPTesteBase.cs
@@ -0,0 +1,44 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Enumerados;
+using SME.SGP.TesteIntegracao.Setup;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.RelatorioPAP.Base
+{
+    public class RelatorioPAPTesteBase : TesteBaseComuns
+    {
+        public RelatorioPAPTesteBase(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        protected async Task CriarDadosBase()
+        {
+            await CriarDreUePerfilComponenteCurricular();
+
+            CriarClaimUsuario(ObterPerfilPAP());
+
+            await CriarUsuarios();
+
+            await CriarTurmaTipoCalendario();
+
+            await CriarPeriodoEscolar(false);
+        }
+
+        protected async Task CriarTurmaTipoCalendario()
+        {
+            await CriarTipoCalendario(ModalidadeTipoCalendario.FundamentalMedio, false);
+            await CriarTurma(Modalidade.Fundamental, "7", false, TipoTurma.Programa);
+        }
+
+        protected async Task CriarPeriodoEscolar(bool considerarAnoAnterior = false)
+        {
+            await CriarPeriodoEscolar(DATA_03_01_INICIO_BIMESTRE_1, DATA_01_05_FIM_BIMESTRE_1, BIMESTRE_1, TIPO_CALENDARIO_1, considerarAnoAnterior);
+
+            await CriarPeriodoEscolar(DATA_02_05_INICIO_BIMESTRE_2, DATA_24_07_FIM_BIMESTRE_2, BIMESTRE_2, TIPO_CALENDARIO_1, considerarAnoAnterior);
+
+            await CriarPeriodoEscolar(DATA_25_07_INICIO_BIMESTRE_3, DATA_02_10_FIM_BIMESTRE_3, BIMESTRE_3, TIPO_CALENDARIO_1, considerarAnoAnterior);
+
+            await CriarPeriodoEscolar(DATA_03_10_INICIO_BIMESTRE_4, DATA_22_12_FIM_BIMESTRE_4, BIMESTRE_4, TIPO_CALENDARIO_1, considerarAnoAnterior);
+        }
+    }
+}
