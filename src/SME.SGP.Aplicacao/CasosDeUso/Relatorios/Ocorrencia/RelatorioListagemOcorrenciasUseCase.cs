b12c1397761a813aedb95f54c2c3f218fef7b15c
Juliano Martins <juliano.campos@amcom.com.br>
2023-07-25 10:57:45 -0400

    96925 - Sgp - Criar endpoint para emissão

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Relatorios/Ocorrencia/RelatorioListagemOcorrenciasUseCase.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos.Relatorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class RelatorioListagemOcorrenciasUseCase : AbstractUseCase, IRelatorioListagemOcorrenciasUseCase
+    {
+        public RelatorioListagemOcorrenciasUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(FiltroRelatorioListagemOcorrenciasDto param)
+        {
+            var usuario = await mediator.Send(new ObterUsuarioLogadoQuery()) ?? throw new NegocioException("Não foi possível localizar o usuário.");
+            param.NomeUsuario = usuario.Nome;
+            param.CodigoRf = usuario.CodigoRf;
+
+            return await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.ListagemOcorrencias, param, usuario, rotaRelatorio: RotasRabbitSgpRelatorios.RotaRelatoriosSolicitadosListagemOcorrencias));
+        }
+    }
+}
