c3962b7dfed2fbe15738541c2b46b852bc47a933
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-07-27 22:16:41 -0300

    Ajustando a nomenclatura

--- src/SME.SGP.Aplicacao/CasosDeUso/ConsolidacaoFrequenciaTurma/ConsolidarFrequenciaPorTurmaMensalUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/ConsolidacaoFrequenciaTurma/ConsolidarFrequenciaPorTurmaMensalUseCase.cs
@@ -10,8 +10,8 @@ namespace SME.SGP.Aplicacao
 {
     public class ConsolidarFrequenciaPorTurmaMensalUseCase : ConsolidarFrequenciaPorTurmaAbstractUseCase, IConsolidarFrequenciaPorTurmaMensalUseCase
     {
-        private DateTime _dataInicio;
-        private DateTime _dataFim;
+        private DateTime DataInicio;
+        private DateTime DataFim;
 
         public ConsolidarFrequenciaPorTurmaMensalUseCase(IMediator mediator) : base(mediator)
         {
@@ -19,14 +19,14 @@ namespace SME.SGP.Aplicacao
 
         protected override TipoConsolidadoFrequencia TipoConsolidado => TipoConsolidadoFrequencia.Mensal;
 
-        protected override (DateTime?, DateTime?) Periodos => (_dataInicio, _dataFim);
+        protected override (DateTime?, DateTime?) Periodos => (DataInicio, DataFim);
 
         protected override async Task<IEnumerable<FrequenciaAlunoDto>> ObterFrequenciaConsideradas(string codigoTurma)
         {
-            _dataInicio = new DateTime(Filtro.Data.Year, Filtro.Data.Month, 01);
-            _dataFim = _dataInicio.AddMonths(1).AddDays(-1);
-            var alunos = await mediator.Send(new ObterAlunosDentroPeriodoQuery(codigoTurma, (_dataInicio, _dataFim)));
-            var frequenciaTurma = await mediator.Send(new ObterFrequenciaPorTurmaPeriodoQuery(codigoTurma, _dataInicio, _dataFim));
+            DataInicio = new DateTime(Filtro.Data.Year, Filtro.Data.Month, 01);
+            DataFim = DataInicio.AddMonths(1).AddDays(-1);
+            var alunos = await mediator.Send(new ObterAlunosDentroPeriodoQuery(codigoTurma, (DataInicio, DataFim)));
+            var frequenciaTurma = await mediator.Send(new ObterFrequenciaPorTurmaPeriodoQuery(codigoTurma, DataInicio, DataFim));
 
             return from ft in frequenciaTurma
                    join a in alunos on ft.AlunoCodigo equals a.CodigoAluno
