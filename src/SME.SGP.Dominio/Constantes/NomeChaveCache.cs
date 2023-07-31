d14ffbf356f87f3fb300044475b3e75ca679f7ad
Helio Barros <helio.barros@amcom.com.br>
2023-07-31 14:38:09 -0300

    removendo duplicidade de chave cache

--- src/SME.SGP.Dominio/Constantes/NomeChaveCache.cs
+++ src/SME.SGP.Dominio/Constantes/NomeChaveCache.cs
@@ -16,7 +16,6 @@
         public const string CHAVE_NOTA_CONCEITO_CONSELHO_CLASSE_TURMA_BIMESTRE = "nota-conceito-conselho-classe-turmacodigo-bimestre:{0}-{1}";
         public const string CHAVE_FECHAMENTO_NOTA_TURMA_BIMESTRE = "fechamento-nota-turmacodigo-bimestre:{0}-{1}";
         public const string CHAVE_ALUNOS_TURMA = "todos-alunos-turma:{0}/codigo-aluno:{1}";
-        public const string CHAVE_AUTENTICACAO_FREQUENCIA = "autenticacao-frequencia:{0}";
         public const string CHAVE_COMPONENTES_PROFESSOR_TURMA = "componentes-professor-turma-agrupado:{0}-{1}-{2}-{3}";
         public const string NOME_CHAVE_COMPENSACAO_TURMA_BIMESTRE = "compensacao-ausencia-turma:turma-{0}-bimestre-{1}";
         public const string CHAVE_NOTA_CONSOLIDACAO_CONSELHO_CLASSE_TURMA_COMPONENTE_BIMESTRE_ALUNO = "nota-consolidacao-conselho-classe-turma-componente-bimestre-aluno:{0}-{1}-{2}-{3}";
