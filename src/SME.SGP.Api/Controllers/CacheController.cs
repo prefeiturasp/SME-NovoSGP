3a5e32371e3ac1d06f656097fc2b48e62b28707f
rafalosiamcom <rafael.losi@amcom.com.br>
2023-08-09 18:07:01 -0300

    Criado listagem de chaves de cache

---
+++ src/SME.SGP.Api/Controllers/CacheController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Dominio.Constantes;
+using SME.SGP.Infra.Utilitarios;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/cache")]
+    public class CacheController : Controller
+    {
+        [HttpGet("chaves")]
+        [AllowAnonymous]
+        public IActionResult ObterChaves()
+        {
+            return Ok(typeof(NomeChaveCache).ObterConstantesPublicas<string>());
+        }
+    }
+}
