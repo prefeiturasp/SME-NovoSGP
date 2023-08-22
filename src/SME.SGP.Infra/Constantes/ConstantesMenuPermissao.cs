6e4aaadd70c57d1d0124ea2e18b9dc24fc6bdbc2
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-08-22 09:01:21 -0300

    Ordenação dos menus

---
+++ src/SME.SGP.Infra/Constantes/ConstantesMenuPermissao.cs
@@ -0,0 +1,25 @@
+﻿namespace SME.SGP.Infra.Constantes
+{
+    public static class ConstantesMenuPermissao
+    {
+        public const string MENU_PLANO_ANUAL = "Plano Anual";
+        public const string MENU_PLANO_DE_CICLO = "Plano de Ciclo";
+        public const string MENU_CARTA_DE_INTENCOES = "Carta de Intenções";
+        public const string MENU_DOCUMENTOS_E_PLANOS_DE_TRABALHO = "Documentos e planos de trabalho";
+        public const string MENU_TERRITORIO_DO_SABER = "Território do Saber";
+        public const string MENU_REGISTRO_POA = "Registro POA";
+        
+        public const string AGRUPAMENTO_PLANEJAMENTO = "Planejamento";
+        
+        public const string ROTA_PLANEJAMENTO_PLANO_ANUAL = "/planejamento/plano-anual";
+        public const string ROTA_PLANEJAMENTO_PLANO_CICLO = "/planejamento/plano-ciclo";
+        public const string ROTA_PLANEJAMENTO_CARTA_DE_INTENCOES = "/planejamento/carta-intencoes";
+        public const string ROTA_GESTAO_DOCUMENTOS_PLANOS_TRABALHO = "/gestao/documentos-planos-trabalho";
+        public const string ROTA_PLANEJAMENTO_PLANO_ANUAL_TERRITORIO_SABER = "/planejamento/plano-anual-territorio-saber";
+        public const string ROTA_DIARIO_CLASSE_REGISTRO_POA = "/diario-classe/registro-poa";
+        
+        public const string ICONE_FAS_FA_LIST_ALT = "fas fa-list-alt";
+        public const string ICONE_FAR_FA_CALENDAR_MINUS = "far fa-calendar-minus";
+        public const string ICONE_FAR_FA_ENVELOPE_OPEN = "far fa-envelope-open";
+    }
+}
