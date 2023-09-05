eee463412437b98a27d9ac584da631040f4a9ae7
DanylloPereira <danyllo.antunes@gmail.com>
2023-09-05 16:27:31 -0300

    102702 - Cria Query para buscar os encaminhamentos com inatividade de atendimento

---
+++ src/SME.SGP.Aplicacao/Commands/EncaminhamentoNAAPA/AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPA/AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPACommand.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SME.SGP.Aplicacao
+{
+    public class AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPACommand : IRequest<bool>
+    {
+        public AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPACommand(long encaminhamentoId)
+        {
+            EncaminhamentoId = encaminhamentoId;
+        }
+        public long EncaminhamentoId { get; set; }
+    }
+
+    public class AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPACommandValidator : AbstractValidator<AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPACommand>
+    {
+        public AlterarDataUltimaNotificacaoInatividadeAtendimentoNAAPACommandValidator()
+        {
+            RuleFor(a => a.EncaminhamentoId)
+                .NotEmpty()
+                .WithMessage("É id do encaminhamento naapa deve ser informado para atualização da última data de notificação de inatividade atendimento.");
+        }
+    }
+}
