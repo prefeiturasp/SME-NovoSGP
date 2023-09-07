40b25442a4cfe682e40a2faa6c45bd7fe8e0fd72
Helio Barros <helio.barros@amcom.com.br>
2023-09-07 12:25:17 -0300

    ajuste nos testes de integração - freq. global

---
+++ teste/SME.SGP.TesteIntegracao/Frequencia/ServicosFakes/ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaFreqGeralQueryHandlerFake.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.Frequencia.ServicosFakes
+{
+    public class ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaFreqGeralQueryHandlerFake : IRequestHandler<ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery, string[]>
+    {
+        private string CODIGO_TURMA_1 = "1";
+        private string CODIGO_TURMA_2 = "2";
+        public async Task<string[]> Handle(ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery request, CancellationToken cancellationToken)
+        {
+            return new string[] { CODIGO_TURMA_1, CODIGO_TURMA_2 };
+        }
+    }
+}
--- teste/SME.SGP.TesteIntegracao/ServicosFakes/ObterTurmasPorCodigosQueryHandlerFake.cs
+++ teste/SME.SGP.TesteIntegracao/ServicosFakes/ObterTurmasPorCodigosQueryHandlerFake.cs
@@ -13,14 +13,20 @@ namespace SME.SGP.TesteIntegracao.ServicosFakes
     public class ObterTurmasPorCodigosQueryHandlerFake : IRequestHandler<ObterTurmasPorCodigosQuery, IEnumerable<Turma>>
     {
         private const string CODIGO_TURMA_1 = "1";
+        private const string CODIGO_TURMA_2 = "2";
 
         public async Task<IEnumerable<Turma>> Handle(ObterTurmasPorCodigosQuery request, CancellationToken cancellationToken)
         {
             return new List<Turma> { 
-                new Turma{
-                CodigoTurma = CODIGO_TURMA_1,
-                Id = 1,
-                TipoTurma = Dominio.Enumerados.TipoTurma.Regular
+                new Turma {
+                    CodigoTurma = CODIGO_TURMA_1,
+                    Id = 1,
+                    TipoTurma = Dominio.Enumerados.TipoTurma.Regular
+                },
+                new Turma {
+                    CodigoTurma = CODIGO_TURMA_2,
+                    Id = 2,
+                    TipoTurma = Dominio.Enumerados.TipoTurma.Regular
                 }
             };
         }
