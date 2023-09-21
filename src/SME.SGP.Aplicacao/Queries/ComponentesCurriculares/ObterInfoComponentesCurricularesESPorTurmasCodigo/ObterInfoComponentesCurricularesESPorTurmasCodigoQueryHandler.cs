f86a4cfa23e9e6f925a75f4bd34436c41350403a
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2023-09-21 12:40:56 -0300

    99745 ajuste na listagem de situações do conselho de classe

---
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterInfoComponentesCurricularesESPorTurmasCodigo/ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Minio.DataModel;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler : IRequestHandler<ObterInfoComponentesCurricularesESPorTurmasCodigoQuery, IEnumerable<InfoComponenteCurricular>>
+    {
+        private readonly IMediator mediator;
+        public ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<IEnumerable<InfoComponenteCurricular>> Handle(ObterInfoComponentesCurricularesESPorTurmasCodigoQuery request, CancellationToken cancellationToken)
+        {
+            var turmas = await mediator.Send(new ObterTurmasDreUePorCodigosQuery(request.CodigosDeTurmas));
+
+            var qtdeRegistros = 0;
+            var qtdeRegistrosIgnorados = 0;
+            int? bimestre = null;
+            var componentesDaTurma = new List<RetornoConsultaListagemTurmaComponenteDto>();
+            foreach (var turma in turmas)
+            {
+                componentesDaTurma.AddRange((await mediator.Send(new ObterTurmasComComponentesQuery(turma.Ue.CodigoUe, turma.Ue.Dre.CodigoDre, turma.CodigoTurma, turma.AnoLetivo, qtdeRegistros, qtdeRegistrosIgnorados, bimestre, turma.ModalidadeCodigo, turma.Semestre, false, string.Empty, false, DateTime.Now, string.Empty))).Items);
+
+            }
+            var codigosComponentes = componentesDaTurma.Select(x => x.ComponenteCurricularCodigo).Distinct().ToArray();
+            
+            var componentesCurricularesSgp = await mediator.Send(new ObterInfoPedagogicasComponentesCurricularesPorIdsQuery(codigosComponentes));
+            return componentesCurricularesSgp;
+        }
+    }
+}
