c6c9c063b88a5841eed46ffec8a8fbb4ec48eff1
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-09-27 11:00:07 -0300

    Correção testes

--- teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasAtribuicaoEsporadicaTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Consultas/ConsultasAtribuicaoEsporadicaTeste.cs
@@ -31,24 +31,23 @@ namespace SME.SGP.Aplicacao.Teste.Comandos
         {
             //ARRANGE
             var atribuicaoEsporadica = new AtribuicaoEsporadica() { Id = 1, Excluido = false };
-            repositorioAtribuicaoEsporadica.Setup(a => a.ObterPorIdAsync(atribuicaoEsporadica.Id)).Returns(Task.FromResult(atribuicaoEsporadica));
+            repositorioAtribuicaoEsporadica.Setup(a => a.ObterPorId(atribuicaoEsporadica.Id)).Returns(atribuicaoEsporadica);
 
             //ASSERT && //ACT
             Assert.NotNull(consultasAtribuicaoEsporadica.ObterPorId(1));
-            repositorioAtribuicaoEsporadica.Verify(c => c.ObterPorIdAsync(1), Times.Once);
+            repositorioAtribuicaoEsporadica.Verify(c => c.ObterPorId(1), Times.Once);
         }
 
         [Fact(DisplayName = "Não Deve Consultar Atribuição não existe")]
-        public async Task Nao_Deve_Consultar_Atribuicao_Nao_Existe()
+        public void Nao_Deve_Consultar_Atribuicao_Nao_Existe()
         {
             //ARRANGE
             var atribuicaoEsporadica = new AtribuicaoEsporadica() { Id = 1, Excluido = false };
-            repositorioAtribuicaoEsporadica.Setup(a => a.ObterPorIdAsync(atribuicaoEsporadica.Id)).Returns(Task.FromResult(atribuicaoEsporadica));
+            repositorioAtribuicaoEsporadica.Setup(a => a.ObterPorId(atribuicaoEsporadica.Id)).Returns(atribuicaoEsporadica);
 
             //ASSERT && //ACT
-            var resultado = consultasAtribuicaoEsporadica.ObterPorId(2);
-            Assert.Null(resultado);
-            repositorioAtribuicaoEsporadica.Verify(c => c.ObterPorIdAsync(2).Result, Times.Once);
+            Assert.Null(consultasAtribuicaoEsporadica.ObterPorId(2));
+            repositorioAtribuicaoEsporadica.Verify(c => c.ObterPorId(2), Times.Once);
         }
     }
 }
