ff2ecc28cfe090996a4cf4f3948cebda1eeb7a2d
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2023-10-04 15:59:23 -0300

    Revert "99745 removendo consulta não utilizada"
    This reverts commit 9c96cd7380ef8fa7eae9786f3c6d76e7a6b1cc4d.

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConselhoClasseConsolidadoConsulta.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConselhoClasseConsolidadoConsulta.cs
@@ -8,6 +8,7 @@ namespace SME.SGP.Dominio
     public interface IRepositorioConselhoClasseConsolidadoConsulta : IRepositorioBase<ConselhoClasseConsolidadoTurmaAluno>
     {
         Task<IEnumerable<ConselhoClasseConsolidadoTurmaAluno>> ObterConselhosClasseConsolidadoPorTurmaBimestreAsync(long turmaId, int situacaoConselhoClasse, int bimestre);
+        Task<IEnumerable<AlunoSituacaoConselhoDto>> ObterStatusConsolidacaoConselhoClasseAlunoTurma(long turmaId, int bimestre);
         Task<ConselhoClasseConsolidadoTurmaAluno> ObterConselhoClasseConsolidadoPorTurmaBimestreAlunoAsync(long turmaId, string alunoCodigo);
         Task<IEnumerable<long>> ObterConsolidacoesAtivasIdPorAlunoETurmaAsync(string alunoCodigo, long turmaId);
         Task<IEnumerable<ConsolidacaoConselhoClasseAlunoMigracaoDto>> ObterFechamentoNotaAlunoOuConselhoClasseAsync(long turmaId);
