63ec186e6c15f54c3e6cc289f6a8ec5c32dac7b0
DanylloPereira <danyllo.antunes@gmail.com>
2023-10-10 09:03:03 -0300

    103757 - Revertendo classes semestral

---
+++ src/SME.SGP.Dados/Mapeamentos/RelatorioSemestralPAPAlunoMap.cs
@@ -0,0 +1,16 @@
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class RelatorioSemestralPAPAlunoMap : BaseMap<RelatorioSemestralPAPAluno>
+    {
+        public RelatorioSemestralPAPAlunoMap()
+        {
+            ToTable("relatorio_semestral_pap_aluno");
+            Map(c => c.RelatorioSemestralTurmaPAPId).ToColumn("relatorio_semestral_turma_pap_id");
+            Map(c => c.AlunoCodigo).ToColumn("aluno_codigo");
+            Map(c => c.Migrado).ToColumn("migrado");
+            Map(c => c.Excluido).ToColumn("excluido");
+        }
+    }
+}
---
+++ src/SME.SGP.Dados/Mapeamentos/RelatorioSemestralTurmaPAPMap.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using SME.SGP.Dominio;
+
+namespace SME.SGP.Dados.Mapeamentos
+{
+    public class RelatorioSemestralTurmaPAPMap : DommelEntityMap<RelatorioSemestralTurmaPAP>
+    {
+        public RelatorioSemestralTurmaPAPMap()
+        {
+            ToTable("relatorio_semestral_turma_pap");
+            Map(c => c.Id).ToColumn("id").IsIdentity().IsKey();
+            Map(c => c.TurmaId).ToColumn("turma_id");
+            Map(c => c.Semestre).ToColumn("semestre");
+        }
+    }
+}
