78d2074c0a1a0938d3b59b9345c5f2c53c6ffc84
DanylloPereira <danyllo.antunes@gmail.com>
2023-10-10 09:51:34 -0300

    103757 - Criar fila de migração das informações

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/ExecutarMigracaoRelatorioPapPorAnoEletivoUseCase.cs
@@ -0,0 +1,17 @@
+﻿using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExecutarMigracaoRelatorioPapPorAnoEletivoUseCase : IExecutarMigracaoRelatorioPapPorAnoEletivoUseCase
+    {
+        public Task<bool> Executar(MensagemRabbit param)
+        {
+            var anoSemestre = param.ObterObjetoMensagem<AnoSemestreDto>();
+
+            throw new NotImplementedException();
+        }
+    }
+}
