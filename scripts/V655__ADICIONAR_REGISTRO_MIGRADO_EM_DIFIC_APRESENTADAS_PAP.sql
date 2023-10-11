c51568a2dd2e760cecb760fff47c47ad47575144
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-10-11 13:27:39 -0300

    Ajustando pela ordem e não pelo id

--- scripts/V655__ADICIONAR_REGISTRO_MIGRADO_EM_DIFIC_APRESENTADAS_PAP.sql
+++ scripts/V655__ADICIONAR_REGISTRO_MIGRADO_EM_DIFIC_APRESENTADAS_PAP.sql
@@ -5,7 +5,7 @@ declare
 begin
 	
 	SELECT id INTO questaoId FROM questao WHERE nome_componente = 'DIFIC_APRESENTADAS';	
-    SELECT max(id)+1 INTO ultimoId FROM opcao_resposta WHERE questao_id = questaoId;
+    SELECT max(ordem)+1 INTO ultimoId FROM opcao_resposta WHERE questao_id = questaoId;
     
 	INSERT INTO opcao_resposta (questao_id, ordem, nome, criado_em, criado_por, criado_rf)
 	select questaoId, ultimoId, 'Registro migrado', NOW(), 'SISTEMA', '0'
