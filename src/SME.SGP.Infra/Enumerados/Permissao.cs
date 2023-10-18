68f138bf5685aeb54fbe5884092f7138082f7a3c
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-10-18 18:20:23 -0300

    Ajuste de permissão de novo menu

--- src/SME.SGP.Infra/Enumerados/Permissao.cs
+++ src/SME.SGP.Infra/Enumerados/Permissao.cs
@@ -1224,7 +1224,43 @@ namespace SME.SGP.Infra
             Url = ConstantesMenuPermissao.ROTA_GESTAO_OCORRENCIAS,
             Icone = ConstantesMenuPermissao.ICONE_FAS_FA_TASKS,
             EhAlteracao = true)]
-            OCO_A = 196,          
+            OCO_A = 196,
+            
+        [PermissaoMenu(Agrupamento = ConstantesMenuPermissao.AGRUPAMENTO_GESTAO, 
+            Menu = ConstantesMenuPermissao.MENU_CADASTRO_DE_ABAE, 
+            OrdemAgrupamento = ConstantesMenuPermissao.ORDEM_AGRUPAMENTO_GESTAO,
+            OrdemMenu = ConstantesMenuPermissao.ORDEM_MENU_CADASTRO_DE_ABAE,
+            Url = ConstantesMenuPermissao.ROTA_GESTAO_CADASTRO_DE_ABAE,
+            Icone = ConstantesMenuPermissao.ICONE_FAS_FA_TASKS,
+            EhAlteracao = true)]
+            ABA_A = 251,    
+            
+        [PermissaoMenu(Agrupamento = ConstantesMenuPermissao.AGRUPAMENTO_GESTAO, 
+            Menu = ConstantesMenuPermissao.MENU_CADASTRO_DE_ABAE, 
+            OrdemAgrupamento = ConstantesMenuPermissao.ORDEM_AGRUPAMENTO_GESTAO,
+            OrdemMenu = ConstantesMenuPermissao.ORDEM_MENU_CADASTRO_DE_ABAE,
+            Url = ConstantesMenuPermissao.ROTA_GESTAO_CADASTRO_DE_ABAE,
+            Icone = ConstantesMenuPermissao.ICONE_FAS_FA_TASKS,
+            EhExclusao = true)]
+            ABA_E = 252, 
+        
+        [PermissaoMenu(Agrupamento = ConstantesMenuPermissao.AGRUPAMENTO_GESTAO, 
+            Menu = ConstantesMenuPermissao.MENU_CADASTRO_DE_ABAE, 
+            OrdemAgrupamento = ConstantesMenuPermissao.ORDEM_AGRUPAMENTO_GESTAO,
+            OrdemMenu = ConstantesMenuPermissao.ORDEM_MENU_CADASTRO_DE_ABAE,
+            Url = ConstantesMenuPermissao.ROTA_GESTAO_CADASTRO_DE_ABAE,
+            Icone = ConstantesMenuPermissao.ICONE_FAS_FA_TASKS,
+            EhInclusao = true)]
+            ABA_I = 253, 
+        
+        [PermissaoMenu(Agrupamento = ConstantesMenuPermissao.AGRUPAMENTO_GESTAO, 
+            Menu = ConstantesMenuPermissao.MENU_CADASTRO_DE_ABAE, 
+            OrdemAgrupamento = ConstantesMenuPermissao.ORDEM_AGRUPAMENTO_GESTAO,
+            OrdemMenu = ConstantesMenuPermissao.ORDEM_MENU_CADASTRO_DE_ABAE,
+            Url = ConstantesMenuPermissao.ROTA_GESTAO_CADASTRO_DE_ABAE,
+            Icone = ConstantesMenuPermissao.ICONE_FAS_FA_TASKS,
+            EhConsulta = true)]
+            ABA_C = 254, 
 
         [PermissaoMenu(Agrupamento = ConstantesMenuPermissao.AGRUPAMENTO_AEE, 
             Menu = ConstantesMenuPermissao.MENU_ENCAMINHAMENTO, 
