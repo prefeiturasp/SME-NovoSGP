c4be6bba2b60738be49f9ad545194790a3191f8d
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-10-20 15:38:23 -0300

    End point busca ABAEs

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/ABAE/ObterPaginadoCadastroAcessoABAEUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+
+namespace SME.SGP.Aplicacao;
+
+public class ObterPaginadoCadastroAcessoABAEUseCase: AbstractUseCase, IObterPaginadoCadastroAcessoABAEUseCase
+{
+    public ObterPaginadoCadastroAcessoABAEUseCase(IMediator mediator) : base(mediator)
+    {}
+
+    public async Task<PaginacaoResultadoDto<DreUeNomeSituacaoABAEDto>> Executar(FiltroDreIdUeIdNomeSituacaoABAEDto filtro)
+    {
+        var retorno = await mediator.Send(new ObterPaginadoCadastroAcessoABAEPorFiltroQuery(filtro));
+        
+        return new PaginacaoResultadoDto<DreUeNomeSituacaoABAEDto>()
+        {
+            Items = retorno.Items.Select(s => new DreUeNomeSituacaoABAEDto()
+            {
+                Id = s.Id,
+                Dre = s.Dre,
+                Ue = $"{s.TipoEscola.ObterNomeCurto()} {s.Ue}",
+                Nome = s.Nome,
+                Situacao = s.Situacao
+            }),
+            TotalRegistros = retorno.TotalRegistros,
+            TotalPaginas = retorno.TotalPaginas
+        };
+    }
+}
