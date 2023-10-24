4b62d8821ae43af3ccab8df6aa5524606f3df66c
DanylloPereira <danyllo.antunes@gmail.com>
2023-10-24 18:07:47 -0300

    107215 - revertendo

--- scripts/V659__ADICIONAR_CAMPO_SEMESTRE_TIPO_CALENDARIO.sql
+++ scripts/V659__ADICIONAR_CAMPO_SEMESTRE_TIPO_CALENDARIO.sql
@@ -3,8 +3,10 @@ ALTER TABLE public.tipo_calendario ADD column IF NOT exists SEMESTRE int4 NULL;
 
 --> Definindo semestre conforme nome do tipo de calendário
 update  tipo_calendario tc
-	set semestre = case when nome like '% 1%' then 1 else 2 end
-where tc.modalidade = 2;
+	set semestre = case when nome like '%1º%' then 1 else 2 end
+where tc.modalidade = 2 
+      and not excluido 
+	  and semestre is null;
 	  
 --> Inserindo parâmetro CelpDiasLetivos
 INSERT INTO public.parametros_sistema (nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, criado_rf)
