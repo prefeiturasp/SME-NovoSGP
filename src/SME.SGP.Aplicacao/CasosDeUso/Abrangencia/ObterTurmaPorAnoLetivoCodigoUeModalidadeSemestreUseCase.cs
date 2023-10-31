d8759506289aacec8e9d3eeaaa833c602a4559d8
Thiago Fernando <thiago.fernando@msn.com>
2023-10-31 14:14:37 -0300

    Use the default parameter value defined in the overridden method.

--- src/SME.SGP.Aplicacao/CasosDeUso/Abrangencia/ObterTurmaPorAnoLetivoCodigoUeModalidadeSemestreUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Abrangencia/ObterTurmaPorAnoLetivoCodigoUeModalidadeSemestreUseCase.cs
@@ -3,9 +3,8 @@ using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Aplicacao.Queries.Abrangencia.ObterTurmaPorAnoLetivoCodigoUeModalidade;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao.CasosDeUso.Abrangencia
@@ -17,9 +16,9 @@ namespace SME.SGP.Aplicacao.CasosDeUso.Abrangencia
 
         }
 
-        public async Task<IEnumerable<OpcaoDropdownDto>> Executar(string codigoUe, int anoLetivo, Modalidade? modalidade, int semestre, bool consideraNovosAnosInfantil = false)
+        public async Task<IEnumerable<OpcaoDropdownDto>> Executar(string codigoUe, int anoLetivo, Modalidade? modalidade, int semestre, bool desconsideraNovosAnosInfantil)
         {
-            var anosInfantilDesconsiderar = modalidade == Modalidade.EducacaoInfantil && !consideraNovosAnosInfantil ?
+            var anosInfantilDesconsiderar = modalidade == Modalidade.EducacaoInfantil && !desconsideraNovosAnosInfantil ?
                 await mediator.Send(new ObterParametroTurmaFiltroPorAnoLetivoEModalidadeQuery(anoLetivo, Modalidade.EducacaoInfantil)) : null;
             return await mediator.Send(new ObterTurmaPorAnoLetivoCodigoUeModalidadeSemestreQuery(anoLetivo, codigoUe, modalidade, semestre, anosInfantilDesconsiderar));
         }
