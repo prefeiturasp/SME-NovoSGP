dfeee7488debd733e54f375efae5b823f0825fab
Thiago Fernando <thiago.fernando@msn.com>
2023-10-31 14:16:32 -0300

    Rename parameter 'codigoFoto' to 'param' to match the interface declaration

--- src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoAluno/ExcluirFotoAlunoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoAluno/ExcluirFotoAlunoUseCase.cs
@@ -13,9 +13,9 @@ namespace SME.SGP.Aplicacao
         {
         }
 
-        public async Task<AuditoriaDto> Executar(Guid codigoFoto)
+        public async Task<AuditoriaDto> Executar(Guid param)
         {
-            var foto = await mediator.Send(new ObterFotoSemestreAlunoPorCodigoQuery(codigoFoto));
+            var foto = await mediator.Send(new ObterFotoSemestreAlunoPorCodigoQuery(param));
 
             if (foto.EhNulo())
                 throw new NegocioException("Código da foto não licalizado");
