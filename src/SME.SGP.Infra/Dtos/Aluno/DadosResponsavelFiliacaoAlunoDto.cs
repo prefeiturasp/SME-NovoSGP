86f64c25b88b5ceed03431295ef4c4fd4469d1b0
DanylloPereira <danyllo.antunes@gmail.com>
2023-11-07 15:37:27 -0300

    105978 - End point busca de dados adicionais das filiações do aluno

---
+++ src/SME.SGP.Infra/Dtos/Aluno/DadosResponsavelFiliacaoAlunoDto.cs
@@ -0,0 +1,15 @@
+﻿using SME.SGP.Dto;
+using System.Collections.Generic;
+
+namespace SME.SGP.Infra.Dtos.Aluno
+{
+    public class DadosResponsavelFiliacaoAlunoDto
+    {
+        public string CodigoAluno { get; set; }
+        public string NomeFiliacao1 { get; set; }
+        public List<TelefonesDto> TelefonesFiliacao1 { get; set; }
+        public string NomeFiliacao2 { get; set; }
+        public List<TelefonesDto> TelefonesFiliacao2 { get; set; }
+        public EnderecoRespostaDto Endereco { get; set; }
+    }
+}
