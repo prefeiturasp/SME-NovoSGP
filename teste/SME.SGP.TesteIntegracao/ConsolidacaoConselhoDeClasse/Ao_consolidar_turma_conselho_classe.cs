016a3c26939b971fc0a11bcf503e6a394cf9c504
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2023-11-16 12:16:40 -0300

    Bug: 107921 Renomeando classe fake para corrigir o erro de ambiguidade.

--- teste/SME.SGP.TesteIntegracao/ConsolidacaoConselhoDeClasse/Ao_consolidar_turma_conselho_classe.cs
+++ teste/SME.SGP.TesteIntegracao/ConsolidacaoConselhoDeClasse/Ao_consolidar_turma_conselho_classe.cs
@@ -27,7 +27,7 @@ namespace SME.SGP.TesteIntegracao.ConsolidacaoConselhoDeClasse
             base.RegistrarFakes(services);
             services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQuery, string[]>), typeof(ObterTurmaCodigosAlunoPorAnoLetivoAlunoTipoTurmaQueryHandlerFake), ServiceLifetime.Scoped));
             services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterAlunosAtivosPorTurmaCodigoQuery, IEnumerable<AlunoPorTurmaResposta>>), typeof(ObterAlunosAtivosPorTurmaCodigoQueryHandlerFake), ServiceLifetime.Scoped));
-            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterMatriculasAlunoNaTurmaQuery, IEnumerable<AlunoPorTurmaResposta>>), typeof(ObterMatriculasAlunoNaTurmaQueryHandlerFake), ServiceLifetime.Scoped));
+            services.Replace(new ServiceDescriptor(typeof(IRequestHandler<ObterMatriculasAlunoNaTurmaQuery, IEnumerable<AlunoPorTurmaResposta>>), typeof(ObterMatriculasAlunoNaTurmaQueryHandlerFakeConslidacaoConselho), ServiceLifetime.Scoped));
 
         }
 
