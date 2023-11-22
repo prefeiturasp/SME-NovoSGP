6a1a36ccd05d8e0653d1b21c9ff4d690a24051c4
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-11-22 14:53:22 -0300

    109165 - Resolução Sonar: Refactor this method to reduce its Cognitive Complexity from x to the y allowed

--- src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoAluno/SyncAjusteImagensAcompanhamentoAprendizagemAlunoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/AcompanhamentoAluno/SyncAjusteImagensAcompanhamentoAprendizagemAlunoUseCase.cs
@@ -2,6 +2,7 @@
 using SME.SGP.Aplicacao.Interfaces;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
+using SME.SGP.Infra.Extensoes;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -24,52 +25,30 @@ namespace SME.SGP.Aplicacao
         {
             var acompanhamentoAluno = mensagem.ObterObjetoMensagem<FiltroAjusteImagensRAADto>();
             var imagens = await repositorio.ObterImagensParaAjusteRota(acompanhamentoAluno.Id);
-
             var listaAtualizacao = new List<(long id, string nomeAnterior, string nomeAtual)>();
 
             foreach (var imagem in imagens)
             {
                 using (var client = new WebClient())
                 {
-                    try
+                    byte[] arquivo = null;
+                    Exception excecao = null;
+                    if (!client.TryDownloadData(imagem.NomeCompleto, out arquivo, out excecao))
                     {
-                        var arquivo = client.DownloadData(new Uri(imagem.NomeCompleto));
-                    }
-                    catch (Exception)
-                    {
-                        try
-                        {
-                            var arquivo = client.DownloadData(new Uri(imagem.NomeCompletoAlternativo));
+                        if (client.TryDownloadData(imagem.NomeCompletoAlternativo, out arquivo, out excecao))
                             listaAtualizacao.Add((imagem.Id, imagem.NomeCompleto, imagem.NomeCompletoAlternativo));
-                        }
-                        catch (Exception)
-                        {
-                            try
-                            {
-                                var arquivo = client.DownloadData(new Uri(imagem.NomeCompletoAlternativo2));
-                                listaAtualizacao.Add((imagem.Id, imagem.NomeCompleto, imagem.NomeCompletoAlternativo2));
-                            }
-                            catch (Exception)
-                            {
-                                try
-                                {
-                                    var arquivo = client.DownloadData(new Uri(imagem.NomeCompletoAlternativo3));
-                                    listaAtualizacao.Add((imagem.Id, imagem.NomeCompleto, imagem.NomeCompletoAlternativo3));
-                                }
-                                catch (Exception)
-                                {
-                                    throw;
-                                }
-                            }
-                        }
+                        else if (client.TryDownloadData(imagem.NomeCompletoAlternativo2, out arquivo, out excecao))
+                            listaAtualizacao.Add((imagem.Id, imagem.NomeCompleto, imagem.NomeCompletoAlternativo2));
+                        else if (client.TryDownloadData(imagem.NomeCompletoAlternativo3, out arquivo, out excecao))
+                            listaAtualizacao.Add((imagem.Id, imagem.NomeCompleto, imagem.NomeCompletoAlternativo3));
+                        else
+                            throw excecao;
                     }
                 }
             }
 
             foreach(var atualizar in listaAtualizacao)
-            {
                 await repositorio.AtualizarLinkImagem(atualizar.id, atualizar.nomeAnterior, atualizar.nomeAtual);
-            }
 
             return true;
         }
