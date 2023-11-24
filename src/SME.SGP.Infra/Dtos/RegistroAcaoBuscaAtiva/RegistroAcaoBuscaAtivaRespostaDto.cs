dbbbeae6ee53a95d8bd087c086412c134de76ac6
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-11-24 15:39:01 -0300

    103716 - Ajuste adição campo semestre na turma do registro de ação

--- src/SME.SGP.Infra/Dtos/RegistroAcaoBuscaAtiva/RegistroAcaoBuscaAtivaRespostaDto.cs
+++ src/SME.SGP.Infra/Dtos/RegistroAcaoBuscaAtiva/RegistroAcaoBuscaAtivaRespostaDto.cs
@@ -14,7 +14,8 @@ namespace SME.SGP.Infra.Dtos
         public long TurmaId { get; set; }
         public string TurmaNome { get; set; }
         public string TurmaCodigo { get; set; }
-        
+        public int TurmaSemestre { get; set; }
+
         public int AnoLetivo { get; set; }
         public AlunoTurmaReduzidoDto Aluno { get; set; }
         public int Modalidade { get; set; }
