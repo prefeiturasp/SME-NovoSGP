2ca184868805f3ea2c027ede7b00ab1fdbba43eb
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2023-11-28 16:15:13 -0300

    Bug: 107921 Complemento do bug tratando erro de objeto nulo. Teste unitário para cobertura do ajuste.

--- src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterInfoComponentesCurricularesESPorTurmasCodigo/ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/ComponentesCurriculares/ObterInfoComponentesCurricularesESPorTurmasCodigo/ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using Minio.DataModel;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
 using System;
@@ -20,30 +19,36 @@ namespace SME.SGP.Aplicacao
 
         public async Task<IEnumerable<InfoComponenteCurricular>> Handle(ObterInfoComponentesCurricularesESPorTurmasCodigoQuery request, CancellationToken cancellationToken)
         {
-            var turmas = await mediator.Send(new ObterTurmasDreUePorCodigosQuery(request.CodigosDeTurmas));
+            var turmas = await mediator.Send(new ObterTurmasDreUePorCodigosQuery(request.CodigosDeTurmas), cancellationToken);
             var componentesDaTurma = new List<RetornoConsultaListagemTurmaComponenteDto>();
+
             foreach (var turma in turmas)
             {
-                var dataReferenciaInicioAnoLetivo = turma.Historica ? new DateTime(turma.AnoLetivo, 2, 5) : DateTimeExtension.HorarioBrasilia();
-                componentesDaTurma.AddRange((await mediator.Send(new ObterTurmasComComponentesQuery(turma.Ue.CodigoUe, 
-                                                                                                    turma.Ue.Dre.CodigoDre, 
-                                                                                                    turma.CodigoTurma, 
-                                                                                                    turma.AnoLetivo, 
-                                                                                                    qtdeRegistros:0, 
-                                                                                                    qtdeRegistrosIgnorados:0, 
-                                                                                                    bimestre: null, 
-                                                                                                    turma.ModalidadeCodigo, 
-                                                                                                    turma.Semestre, 
-                                                                                                    false, 
-                                                                                                    string.Empty, 
-                                                                                                    turma.Historica, 
-                                                                                                    dataReferenciaInicioAnoLetivo, 
-                                                                                                    string.Empty))).Items);
+                var dataReferenciaInicioAnoLetivo = turma.Historica ? new DateTime(turma.AnoLetivo, 2, 5) : DateTimeExtension.HorarioBrasilia().Date;
+                var componentesObtidos = (await mediator.Send(new ObterTurmasComComponentesQuery(turma.Ue.CodigoUe,
+                                                                                                 turma.Ue.Dre.CodigoDre,
+                                                                                                 turma.CodigoTurma,
+                                                                                                 turma.AnoLetivo,
+                                                                                                 qtdeRegistros: 0,
+                                                                                                 qtdeRegistrosIgnorados: 0,
+                                                                                                 bimestre: null,
+                                                                                                 turma.ModalidadeCodigo,
+                                                                                                 turma.Semestre,
+                                                                                                 false,
+                                                                                                 string.Empty,
+                                                                                                 turma.Historica,
+                                                                                                 dataReferenciaInicioAnoLetivo,
+                                                                                                 string.Empty), cancellationToken))?.Items;
+
+                if (componentesObtidos != null && componentesObtidos.Any())
+                    componentesDaTurma.AddRange(componentesObtidos);
+                else
+                    throw new NegocioException($"Não foi possível obter turma com componentes da turma: {turma.CodigoTurma}.");
             }
 
             var codigosComponentes = componentesDaTurma.Select(x => x.ComponenteCurricularCodigo).Distinct().ToArray();
-            
-            var componentesCurricularesSgp = await mediator.Send(new ObterInfoPedagogicasComponentesCurricularesPorIdsQuery(codigosComponentes));
+
+            var componentesCurricularesSgp = await mediator.Send(new ObterInfoPedagogicasComponentesCurricularesPorIdsQuery(codigosComponentes), cancellationToken);
             return componentesCurricularesSgp;
         }
     }
---
+++ teste/SME.SGP.Aplicacao.Teste/Queries/ComponentesCurriculares/ObterInfoComponentesCurricularesESPorTurmasCodigo/ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandlerTeste.cs
@@ -0,0 +1,103 @@
+﻿using MediatR;
+using Moq;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.Aplicacao.Teste.Queries.ComponentesCurriculares.ObterInfoComponentesCurricularesESPorTurmasCodigo
+{
+    public class ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandlerTeste
+    {
+
+        private readonly Mock<IMediator> mediator;
+        private readonly ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler obterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler;
+
+        public ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandlerTeste()
+        {
+            mediator = new Mock<IMediator>();
+            obterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler = new ObterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler(mediator.Object);
+        }
+
+        [Fact(DisplayName = "Componentes Curriculares - ObterComponentesCurricularesPorTurmasCodigoComSucesso")]
+        public async Task ObterComponentesCurricularesPorTurmasCodigoComSucesso()
+        {
+            var dre = new Dre() { CodigoDre = "1" };
+            var ue = new Ue() { CodigoUe = "1", Dre = dre };
+
+            var turmas = new List<Turma>()
+            {
+                new Turma() {  CodigoTurma = "1", AnoLetivo = DateTimeExtension.HorarioBrasilia().Year, Ue = ue, ModalidadeCodigo = Modalidade.Fundamental },
+                new Turma() {  CodigoTurma = "2", AnoLetivo = DateTimeExtension.HorarioBrasilia().Year, Ue = ue, ModalidadeCodigo = Modalidade.Fundamental }
+            };
+
+            var codigosTurmas = turmas.Select(t => t.CodigoTurma).ToArray();
+
+            mediator.Setup(x => x.Send(It.Is<ObterTurmasDreUePorCodigosQuery>(y => y.Codigos == codigosTurmas), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(turmas);
+
+            mediator.Setup(x => x.Send(It.Is<ObterTurmasComComponentesQuery>(y => y.TurmaCodigo == turmas[0].CodigoTurma), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new PaginacaoResultadoDto<RetornoConsultaListagemTurmaComponenteDto>()
+                {
+                    TotalPaginas = 1,
+                    TotalRegistros = 1,
+                    Items = new List<RetornoConsultaListagemTurmaComponenteDto>() { new RetornoConsultaListagemTurmaComponenteDto() { ComponenteCurricularCodigo = 1 } }
+                });
+
+            mediator.Setup(x => x.Send(It.Is<ObterTurmasComComponentesQuery>(y => y.TurmaCodigo == turmas[1].CodigoTurma), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(new PaginacaoResultadoDto<RetornoConsultaListagemTurmaComponenteDto>()
+               {
+                   TotalPaginas = 1,
+                   TotalRegistros = 1,
+                   Items = new List<RetornoConsultaListagemTurmaComponenteDto>() { new RetornoConsultaListagemTurmaComponenteDto() { ComponenteCurricularCodigo = 2 } }
+               });
+
+            mediator.Setup(x => x.Send(It.Is<ObterInfoPedagogicasComponentesCurricularesPorIdsQuery>(y => y.Ids.Contains(1) && y.Ids.Contains(2)), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<InfoComponenteCurricular>() { new InfoComponenteCurricular(), new InfoComponenteCurricular() }.AsEnumerable());
+
+            var query = new ObterInfoComponentesCurricularesESPorTurmasCodigoQuery(codigosTurmas);
+
+            var resultado = await obterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler.Handle(query, It.IsAny<CancellationToken>());
+
+            Assert.NotEmpty(resultado);
+            Assert.Equal(2, resultado.Count());
+        }
+
+        [Fact(DisplayName = "Componentes Curriculares - ObterComponentesCurricularesPorTurmasCodigoComExcecaoComponenteNaoLocalizadoParaTurma")]
+        public async Task ObterComponentesCurricularesPorTurmasCodigoComExcecaoComponenteNaoLocalizadoParaTurma()
+        {
+            var dre = new Dre() { CodigoDre = "1" };
+            var ue = new Ue() { CodigoUe = "1", Dre = dre };
+
+            var turmas = new List<Turma>()
+            {
+                new Turma() {  CodigoTurma = "1", AnoLetivo = DateTimeExtension.HorarioBrasilia().Year, Ue = ue, ModalidadeCodigo = Modalidade.Fundamental },
+                new Turma() {  CodigoTurma = "2", AnoLetivo = DateTimeExtension.HorarioBrasilia().Year, Ue = ue, ModalidadeCodigo = Modalidade.Fundamental }
+            };
+
+            var codigosTurmas = turmas.Select(t => t.CodigoTurma).ToArray();
+
+            mediator.Setup(x => x.Send(It.Is<ObterTurmasDreUePorCodigosQuery>(y => y.Codigos == codigosTurmas), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(turmas);
+
+            mediator.Setup(x => x.Send(It.Is<ObterTurmasComComponentesQuery>(y => y.TurmaCodigo == turmas[0].CodigoTurma), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new PaginacaoResultadoDto<RetornoConsultaListagemTurmaComponenteDto>()
+                {
+                    TotalPaginas = 1,
+                    TotalRegistros = 1,
+                    Items = new List<RetornoConsultaListagemTurmaComponenteDto>() { new RetornoConsultaListagemTurmaComponenteDto() { ComponenteCurricularCodigo = 1 } }
+                });
+
+            mediator.Setup(x => x.Send(It.Is<ObterTurmasComComponentesQuery>(y => y.TurmaCodigo == turmas[1].CodigoTurma), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(new PaginacaoResultadoDto<RetornoConsultaListagemTurmaComponenteDto>() { Items = Enumerable.Empty<RetornoConsultaListagemTurmaComponenteDto>() });
+
+            var query = new ObterInfoComponentesCurricularesESPorTurmasCodigoQuery(codigosTurmas);
+
+            Assert.Equal("Não foi possível obter turma com componentes da turma: 2.", 
+                (await Assert.ThrowsAsync<NegocioException>(async () => await obterInfoComponentesCurricularesESPorTurmasCodigoQueryHandler.Handle(query, It.IsAny<CancellationToken>()))).Message);            
+        }
+    }
+}
