51850059cc134214c5ff1015291a1fb81d44e01f
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-04 16:39:12 -0300

    107629 - Consulta Registros ação grid

--- src/SME.SGP.Api/Controllers/BuscaAtivaController.cs
+++ src/SME.SGP.Api/Controllers/BuscaAtivaController.cs
@@ -14,7 +14,7 @@ namespace SME.SGP.Api.Controllers
     [ApiController]
     [Route("api/v1/busca-ativa")]
     [ValidaDto]
-    [Authorize("Bearer")]
+    //[Authorize("Bearer")]
     public class BuscaAtivaController : ControllerBase
     {
         [HttpPost("registros-acao")]
@@ -60,6 +60,16 @@ namespace SME.SGP.Api.Controllers
         [Permissao(Permissao.CCEA_NAAPA_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterRegistrosAcaoCriancaEstudanteAusente([FromQuery] FiltroRegistrosAcaoCriancasEstudantesAusentesDto filtro,
             [FromServices] IObterRegistrosAcaoCriancaEstudanteAusenteUseCase useCase)
+        {
+            return Ok(await useCase.Executar(filtro));
+        }
+
+        [HttpGet("registros-acao")]
+        [ProducesResponseType(typeof(PaginacaoResultadoDto<RegistroAcaoBuscaAtivaCriancaEstudanteAusenteDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        //[Permissao(Permissao.RABA_NAAPA_C, Policy = "Bearer")]
+        public async Task<IActionResult> ObterRegistrosAcao([FromQuery] FiltroRegistrosAcaoDto filtro,
+            [FromServices] IObterRegistrosAcaoUseCase useCase)
         {
             return Ok(await useCase.Executar(filtro));
         }
---
+++ src/SME.SGP.Aplicacao/Queries/BuscaAtiva/ObterRegistrosAcao/ObterRegistrosAcaoQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterRegistrosAcaoQueryHandler : ConsultasBase, IRequestHandler<ObterRegistrosAcaoQuery, PaginacaoResultadoDto<RegistroAcaoBuscaAtivaListagemDto>>
+    {
+        public IMediator mediator { get; }
+        public IRepositorioRegistroAcaoBuscaAtiva repositorioRegistroAcao { get; }
+
+
+        public ObterRegistrosAcaoQueryHandler(IContextoAplicacao contextoAplicacao, IMediator mediator, IRepositorioRegistroAcaoBuscaAtiva repositorioRegistroAcao) : base(contextoAplicacao)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.repositorioRegistroAcao = repositorioRegistroAcao ?? throw new ArgumentNullException(nameof(repositorioRegistroAcao));
+        }
+
+        public async Task<PaginacaoResultadoDto<RegistroAcaoBuscaAtivaListagemDto>> Handle(ObterRegistrosAcaoQuery request, CancellationToken cancellationToken)
+        {
+            return await repositorioRegistroAcao.ListarPaginado(request.Filtros.AnoLetivo,
+                                                                            request.Filtros.DreId,
+                                                                            request.Filtros.UeId,
+                                                                            request.Filtros.TurmaId,
+                                                                            request.Filtros.Modalidade,
+                                                                            request.Filtros.Semestre,
+                                                                            request.Filtros.NomeAluno,
+                                                                            request.Filtros.DataRegistroInicio,
+                                                                            request.Filtros.DataRegistroFim,
+                                                                            request.Filtros.ProcedimentoRealizado,
+                                                                              Paginacao);
+        }
+    }
+}
