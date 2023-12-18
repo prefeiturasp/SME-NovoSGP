ff3626a17190bbe9a817abbc98b1b114014c5975
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-18 11:42:20 -0300

    110406 - Removendo comment autorização API

--- src/SME.SGP.Api/Controllers/RelatorioDinamicoNAAPAController.cs
+++ src/SME.SGP.Api/Controllers/RelatorioDinamicoNAAPAController.cs
@@ -4,6 +4,7 @@ using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
 using SME.SGP.Aplicacao.Interfaces.CasosDeUso;
 using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos.Questionario;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,13 +12,13 @@ namespace SME.SGP.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/relatorio-dinamico-naapa")]
-    //[Authorize("Bearer")]
+    [Authorize("Bearer")]
     public class RelatorioDinamicoNAAPAController : ControllerBase
     {
         [HttpPost]
         [ProducesResponseType(typeof(RelatorioDinamicoNAAPADto), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        //[Permissao(Permissao.RDNAAPA_C, Policy = "Bearer")]
+        [Permissao(Permissao.RDNAAPA_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterEncaminhamentosNAAPA(
                                             [FromBody] FiltroRelatorioDinamicoNAAPADto filtro,
                                             [FromServices] IRelatorioDinamicoObterEncaminhamentoNAAPAUseCase useCase)
@@ -26,9 +27,9 @@ namespace SME.SGP.Api.Controllers
         }
 
         [HttpGet("questoes")] 
-        [ProducesResponseType(typeof(IEnumerable<QuestaoDto>), 200)]
+        [ProducesResponseType(typeof(IEnumerable<SecaoQuestoesDTO>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        //[Permissao(Permissao.RDNAAPA_C, Policy = "Bearer")]
+        [Permissao(Permissao.RDNAAPA_C, Policy = "Bearer")]
         public async Task<IActionResult> ObterQuestoesPorModalidadesParaRelatorioDinamico(
                                             [FromQuery] int[] modalidadesId,
                                             [FromServices] IObterQuestoesRelatorioDinamicoEncaminhamentoNAAPAPorModalidadesUseCase useCase)
