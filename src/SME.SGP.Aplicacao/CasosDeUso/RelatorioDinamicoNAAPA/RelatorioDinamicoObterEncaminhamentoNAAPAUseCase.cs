60aeea73608671d90c7c6cf7b679ef4b33fe1b59
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-19 14:19:35 -0300

    110405 - Ajuste tratamento -99 array modalidades

--- src/SME.SGP.Aplicacao/CasosDeUso/RelatorioDinamicoNAAPA/RelatorioDinamicoObterEncaminhamentoNAAPAUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/RelatorioDinamicoNAAPA/RelatorioDinamicoObterEncaminhamentoNAAPAUseCase.cs
@@ -7,16 +7,12 @@ namespace SME.SGP.Aplicacao
 {
     public class RelatorioDinamicoObterEncaminhamentoNAAPAUseCase : AbstractUseCase, IRelatorioDinamicoObterEncaminhamentoNAAPAUseCase
     {
-        private const string OPCAO_TODOS = "-99";
-
         public RelatorioDinamicoObterEncaminhamentoNAAPAUseCase(IMediator mediator) : base(mediator)
         {
         }
 
         public Task<RelatorioDinamicoNAAPADto> Executar(FiltroRelatorioDinamicoNAAPADto param)
         {
-            if (param.Modalidades.PossuiRegistros(x => x.ToString().Equals(OPCAO_TODOS)))
-                param.Modalidades = new Modalidade[] { };
             return mediator.Send(new ObterEncaminhamentoNAAPAParaRelatorioDinamicoQuery(param));
         }
     }
