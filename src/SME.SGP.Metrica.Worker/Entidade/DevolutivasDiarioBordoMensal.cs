a48052469b9845e842b794d31d55c33433747a28
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-20 17:27:51 -0300

    108474 - Ajuste campo entidade elastic

--- src/SME.SGP.Metrica.Worker/Entidade/DevolutivasDiarioBordoMensal.cs
+++ src/SME.SGP.Metrica.Worker/Entidade/DevolutivasDiarioBordoMensal.cs
@@ -6,10 +6,12 @@ namespace SME.SGP.Metrica.Worker.Entidade
     {
         public DevolutivasDiarioBordoMensal(DateTime data, int quantidade) : base(data.ToString("yyyyMM"))
         {
+            Data = data.ToUniversalTime();
             Quantidade = quantidade;
             Ano = data.Year;
             Mes = data.Month;
         }
+        public DateTime Data { get; set; }
         public int Ano { get; set; }
         public int Mes { get; set; }
         public int Quantidade { get; set; }
