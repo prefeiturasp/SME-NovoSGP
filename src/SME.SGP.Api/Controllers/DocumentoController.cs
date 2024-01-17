648062e2bc3097659f2838b87b2a5bde362bcf7a
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-01-17 09:34:23 -0300

    109592 - Tratamento para filtrar listagem de documentos - todas em dre e ue

--- src/SME.SGP.Api/Controllers/DocumentoController.cs
+++ src/SME.SGP.Api/Controllers/DocumentoController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SME.SGP.Api.Filtros;
 using SME.SGP.Aplicacao;
+using SME.SGP.Dto;
 using SME.SGP.Infra;
 using System;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace SME.SGP.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/armazenamento/documentos")]
-    [Authorize("Bearer")]
+    //[Authorize("Bearer")]
     public class DocumentoController : ControllerBase
     {
         [HttpGet("{documentoId}/tipo-documento/{tipoDocumentoId}/classificacao/{classificacaoId}/usuario/{usuarioId}/ue/{ueId}/anoLetivo/{anoLetivo}")]
@@ -47,12 +48,11 @@ namespace SME.SGP.Api.Controllers
         [HttpGet("")]
         [ProducesResponseType(typeof(PaginacaoResultadoDto<DocumentoDto>), 200)]
         [ProducesResponseType(401)]
-        [Permissao(Permissao.DPU_C, Policy = "Bearer")]
+        //[Permissao(Permissao.DPU_C, Policy = "Bearer")]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        public async Task<IActionResult> ListarDocumentos([FromQuery] long ueId, [FromQuery] long tipoDocumentoId,
-            [FromQuery] long classificacaoId, [FromQuery] int? anoLetivo, [FromServices] IListarDocumentosUseCase useCase)
+        public async Task<IActionResult> ListarDocumentos([FromQuery] FiltroListagemDocumentosDto filtro, [FromServices] IListarDocumentosUseCase useCase)
         {
-            return Ok(await useCase.Executar(anoLetivo, ueId, tipoDocumentoId, classificacaoId));
+            return Ok(await useCase.Executar(filtro));
         }
 
         [HttpPost("upload")]
