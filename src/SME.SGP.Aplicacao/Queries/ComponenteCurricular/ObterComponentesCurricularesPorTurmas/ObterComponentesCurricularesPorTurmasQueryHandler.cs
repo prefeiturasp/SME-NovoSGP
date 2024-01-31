7f870a3a4eb9adc481a166b9e7b53162f17c49e6
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-01-31 15:34:38 -0300

    Bug: 109112 Complemento do bug desfazendo a remoção do query mediator referente ao endpoint que não retorna as informações de território.

---
+++ src/SME.SGP.Aplicacao/Queries/ComponenteCurricular/ObterComponentesCurricularesPorTurmas/ObterComponentesCurricularesPorTurmasQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterComponentesCurricularesPorTurmasQueryHandler : IRequestHandler<ObterComponentesCurricularesPorTurmasQuery, IEnumerable<ComponenteCurricularEol>>
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IMediator mediator;
+
+        public ObterComponentesCurricularesPorTurmasQueryHandler(IHttpClientFactory httpClientFactory, IMediator mediator)
+        {
+            this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator)); ;
+        }
+
+        public async Task<IEnumerable<ComponenteCurricularEol>> Handle(ObterComponentesCurricularesPorTurmasQuery request, CancellationToken cancellationToken)
+        {
+            var codigosTurmas = string.Join("&codigoTurmas=", request.CodigosTurmas);
+
+            var httpClient = httpClientFactory.CreateClient(ServicosEolConstants.SERVICO);
+            var resposta = await httpClient.GetAsync(ServicosEolConstants.URL_COMPONENTES_CURRICULARES_TURMAS_REGULARES + $"?codigoTurmas={codigosTurmas}");
+            if (resposta.IsSuccessStatusCode)
+            {
+                var json = await resposta.Content.ReadAsStringAsync();
+                var retorno = JsonConvert.DeserializeObject<List<ComponenteCurricularEol>>(json);
+
+                var componentesCurricularesSgp = await mediator.Send(new ObterInfoPedagogicasComponentesCurricularesPorIdsQuery(retorno.ObterCodigos()));
+                retorno.PreencherInformacoesPegagogicasSgp(componentesCurricularesSgp);
+                return retorno;
+            }
+
+            return Enumerable.Empty<ComponenteCurricularEol>();
+        }
+    }
+}
