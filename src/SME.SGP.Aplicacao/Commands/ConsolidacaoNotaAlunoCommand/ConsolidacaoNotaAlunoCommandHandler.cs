773168427a1a104ccd63c843ca402aadd1e7d826
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2024-02-07 17:00:34 -0300

    BUG 109778: Adicionar validação se o componente lança nota antes de acessar o fluxo de consolidação do conselho de classe para impedir componentes que não lançam nota.

--- src/SME.SGP.Aplicacao/Commands/ConsolidacaoNotaAlunoCommand/ConsolidacaoNotaAlunoCommandHandler.cs
+++ src/SME.SGP.Aplicacao/Commands/ConsolidacaoNotaAlunoCommand/ConsolidacaoNotaAlunoCommandHandler.cs
@@ -20,18 +20,20 @@ namespace SME.SGP.Aplicacao
 
         public async Task<bool> Handle(ConsolidacaoNotaAlunoCommand request, CancellationToken cancellationToken)
         {
-            await mediator.Send(new SalvarCacheConselhoClasseOuFechamentoNotaConceitoCommand(request.AlunoCodigo, request.Bimestre ?? 0, request.TurmaId, request.ComponenteCurricularId,
+            if (await mediator.Send(new ObterComponenteLancaNotaQuery(request.ComponenteCurricularId)))
+            {
+                await mediator.Send(new SalvarCacheConselhoClasseOuFechamentoNotaConceitoCommand(request.AlunoCodigo, request.Bimestre ?? 0, request.TurmaId, request.ComponenteCurricularId,
                                                                                              request.Nota, request.ConceitoId,
                                                                                              request.ConselhoClasse ? TipoAlteracao.ConselhoClasseNota : TipoAlteracao.FechamentoNota));
 
-            var mensagemConsolidacaoConselhoClasseAluno = new MensagemConsolidacaoConselhoClasseAlunoDto(request.AlunoCodigo, 
-                                                                                                         request.TurmaId, 
-                                                                                                         request.Bimestre, 
-                                                                                                         request.Inativo,
-                                                                                                         request.ComponenteCurricularId);
+                var mensagemConsolidacaoConselhoClasseAluno = new MensagemConsolidacaoConselhoClasseAlunoDto(request.AlunoCodigo,
+                                                                                                             request.TurmaId,
+                                                                                                             request.Bimestre,
+                                                                                                             request.Inativo,
+                                                                                                             request.ComponenteCurricularId);
 
-            await mediator.Send(new PublicarFilaSgpCommand(RotasRabbitSgpFechamento.ConsolidarTurmaConselhoClasseAlunoTratar, mensagemConsolidacaoConselhoClasseAluno, Guid.NewGuid(), null));
-
+                await mediator.Send(new PublicarFilaSgpCommand(RotasRabbitSgpFechamento.ConsolidarTurmaConselhoClasseAlunoTratar, mensagemConsolidacaoConselhoClasseAluno, Guid.NewGuid(), null));
+            }
             return true;
         }
     }
