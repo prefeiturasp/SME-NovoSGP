2462f924dc9b28829a5c3b3ee7a35e21077336ee
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-02-09 08:52:26 -0300

    110416 - Removendo comment

--- src/SME.SGP.Aplicacao/CasosDeUso/DashboardBuscaAtiva/ConsolidarReflexoFrequenciaBuscaAtivaUeUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/DashboardBuscaAtiva/ConsolidarReflexoFrequenciaBuscaAtivaUeUseCase.cs
@@ -21,8 +21,8 @@ namespace SME.SGP.Aplicacao
         {
             var filtro = mensagem.ObterObjetoMensagem<FiltroIdAnoLetivoDto>();
             var ue = await mediator.Send(new ObterCodigoUEDREPorIdQuery(filtro.Id));
-            //await mediator.Send(new ExcluirConsolidacoesReflexoFrequenciaBuscaAtivaUeMesCommand(ue.UeCodigo, filtro.Data.Month, filtro.Data.Year));
-            //await mediator.Send(new ExcluirConsolidacoesReflexoFrequenciaBuscaAtivaUeMesCommand(ue.UeCodigo, ANUAL, filtro.Data.Year));
+            await mediator.Send(new ExcluirConsolidacoesReflexoFrequenciaBuscaAtivaUeMesCommand(ue.UeCodigo, filtro.Data.Month, filtro.Data.Year));
+            await mediator.Send(new ExcluirConsolidacoesReflexoFrequenciaBuscaAtivaUeMesCommand(ue.UeCodigo, ANUAL, filtro.Data.Year));
 
             var registrosBuscaAtiva = await repositorioBuscaAtiva.ObterIdsRegistrosAlunoResponsavelContatado(filtro.Data.Date, filtro.Id, filtro.Data.Year);
             foreach (var idBuscaAtiva in registrosBuscaAtiva)
