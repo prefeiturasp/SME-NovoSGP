a5f5cae3485dcd3e5aacfd7a949721f4ef912422
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-02-12 15:54:40 -0300

    112629 - publicação fila reconsolidação conselho classe

--- src/SME.SGP.Metrica.Worker/UseCases/ConselhoClasseNaoConsolidadoUEUseCase.cs
+++ src/SME.SGP.Metrica.Worker/UseCases/ConselhoClasseNaoConsolidadoUEUseCase.cs
@@ -1,6 +1,10 @@
-﻿using SME.SGP.Infra;
+﻿using MediatR;
+using SME.SGP.Infra;
+using SME.SGP.Metrica.Worker.Commands;
 using SME.SGP.Metrica.Worker.Repositorios.Interfaces;
 using SME.SGP.Metrica.Worker.UseCases.Interfaces;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Metrica.Worker.UseCases
@@ -9,12 +13,15 @@ namespace SME.SGP.Metrica.Worker.UseCases
     {
         private readonly IRepositorioSGPConsulta repositorioSGP;
         private readonly IRepositorioConselhoClasseNaoConsolidado repositorioConselhoNaoConsolidado;
+        private readonly IMediator mediator;
 
         public ConselhoClasseNaoConsolidadoUEUseCase(IRepositorioSGPConsulta repositorioSGP,
-                                                     IRepositorioConselhoClasseNaoConsolidado repositorioConselhoNaoConsolidado)
+                                                     IRepositorioConselhoClasseNaoConsolidado repositorioConselhoNaoConsolidado,
+                                                     IMediator mediator)
         {
             this.repositorioSGP = repositorioSGP ?? throw new System.ArgumentNullException(nameof(repositorioSGP));
             this.repositorioConselhoNaoConsolidado = repositorioConselhoNaoConsolidado ?? throw new System.ArgumentNullException(nameof(repositorioConselhoNaoConsolidado));
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
         }
 
         public async Task<bool> Executar(MensagemRabbit mensagem)
@@ -24,6 +31,17 @@ namespace SME.SGP.Metrica.Worker.UseCases
             var conselhosNaoConsolidados = await repositorioSGP.ObterConselhosClasseNaoConsolidados(ue.Id);
             foreach (var conselhoNaoConsolidado in conselhosNaoConsolidados)
                 await repositorioConselhoNaoConsolidado.InserirAsync(conselhoNaoConsolidado);
+
+            foreach (var conselhoDivergenteReconsolidar in conselhosNaoConsolidados.GroupBy(cc => new { cc.TurmaId, cc.AlunoCodigo, cc.Bimestre }))
+            {
+                var mensagemConsolidacaoConselhoClasseAluno = new MensagemConsolidacaoConselhoClasseAlunoDto(conselhoDivergenteReconsolidar.Key.AlunoCodigo,
+                                                                                                         conselhoDivergenteReconsolidar.Key.TurmaId,
+                                                                                                         conselhoDivergenteReconsolidar.Key.Bimestre,
+                                                                                                         false);
+
+                await mediator.Send(new PublicarFilaCommand(RotasRabbitSgpFechamento.ConsolidarTurmaConselhoClasseAlunoTratar, mensagemConsolidacaoConselhoClasseAluno));
+            }    
+
 
             return true;
         }
