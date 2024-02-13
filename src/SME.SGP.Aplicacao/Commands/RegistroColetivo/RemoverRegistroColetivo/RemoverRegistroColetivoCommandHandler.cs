0a125a23605dfb9235dc848a414fb34d6c8e4149
DanylloPereira <danyllo.antunes@gmail.com>
2024-02-13 14:58:30 -0300

    98803 - Criar endpoint de Novo/Editar RegistroColetivo

---
+++ src/SME.SGP.Aplicacao/Commands/RegistroColetivo/RemoverRegistroColetivo/RemoverRegistroColetivoCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Constantes.MensagensNegocio;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class RemoverRegistroColetivoCommandHandler : IRequestHandler<RemoverRegistroColetivoCommand, bool>
+    {
+        private readonly IMediator mediator;
+        private readonly IRepositorioRegistroColetivo repositorio;
+        private readonly IUnitOfWork unitOfWork;
+
+        public RemoverRegistroColetivoCommandHandler(IMediator mediator,
+                                                     IRepositorioRegistroColetivo repositorio,
+                                                     IUnitOfWork unitOfWork)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.repositorio = repositorio ?? throw new ArgumentNullException(nameof(repositorio));
+            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        public async Task<bool> Handle(RemoverRegistroColetivoCommand request, CancellationToken cancellationToken)
+        {
+            var registroColetivo = await repositorio.ObterPorIdAsync(request.RegistroColetivoId);
+
+            if (registroColetivo.EhNulo())
+                throw new NegocioException(MensagemNegocioComuns.REGISTRO_COLETIVO_NAO_ENCONTRADA);
+
+            unitOfWork.IniciarTransacao();
+            try
+            {
+                if (registroColetivo.Anexos.NaoEhNulo() && registroColetivo.Anexos.Any())
+                {
+                    await RemoverArquivos(registroColetivo.Anexos);
+                    await mediator.Send(new RemoverRegistroColetivoAnexoCommand(registroColetivo.Id));
+                }
+
+                await mediator.Send(new RemoverRegistroColetivoUeCommand(registroColetivo.Id));
+
+                await repositorio.RemoverAsync(registroColetivo);
+
+                unitOfWork.PersistirTransacao();
+            }
+            catch (Exception)
+            {
+                unitOfWork.Rollback();
+            }
+
+            return true;
+        }
+
+        private async Task RemoverArquivos(IEnumerable<Arquivo> arquivos)
+        {
+            foreach (var arquivo in arquivos)
+                await mediator.Send(new ExcluirArquivoPorIdCommand(arquivo.Id));
+        }
+    }
+}
---
+++ teste/SME.SGP.TesteIntegracao/RegistroColetivoNAAPA/Ao_alterar_registro_coletivo.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SME.SGP.Aplicacao;
+using SME.SGP.Dominio;
+using SME.SGP.Infra;
+using SME.SGP.TesteIntegracao.RegistroColetivoNAAPA.Base;
+using SME.SGP.TesteIntegracao.Setup;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.TesteIntegracao.RegistroColetivoNAAPA
+{
+    public class Ao_alterar_registro_coletivo : RegistroColetivoTesteBase
+    {
+        public Ao_alterar_registro_coletivo(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+
+        [Fact(DisplayName = "Registro Coletivo - Alterar registro coletivo sem anexo")]
+        public async Task Ao_alterar_registro_coletivo_sem_anexo()
+        {
+            await CriaBase();
+
+             await InserirNaBase(new RegistroColetivo()
+            {
+                DreId = DRE_ID_2,
+                TipoReuniaoId = TipoReuniaoConstants.ATENDIMENTO_NAO_PRESENCIAL_ID,
+                DataRegistro = DateTimeExtension.HorarioBrasilia().Date,
+                QuantidadeCuidadores = 1,
+                QuantidadeEducadores = 2,
+                QuantidadeEducandos = 3,
+                QuantidadeParticipantes = 4,
+                Descricao = "Registro coletivo",
+                Observacao = "Observação",
+                CriadoEm = DateTime.Now,
+                CriadoPor = SISTEMA_NOME,
+                CriadoRF = SISTEMA_CODIGO_RF,
+            });
+
+            await InserirNaBase(new RegistroColetivoUe()
+            {
+                RegistroColetivoId = 1,
+                UeId = UE_ID_2,
+                CriadoEm = DateTime.Now,
+                CriadoPor = SISTEMA_NOME,
+                CriadoRF = SISTEMA_CODIGO_RF,
+            });
+
+            await InserirNaBase(new RegistroColetivoUe()
+            {
+                RegistroColetivoId = 1,
+                UeId = UE_ID_3,
+                CriadoEm = DateTime.Now,
+                CriadoPor = SISTEMA_NOME,
+                CriadoRF = SISTEMA_CODIGO_RF,
+            });
+
+            var dto = new RegistroColetivoDto()
+            {
+                Id = 1,
+                DreId = DRE_ID_2,
+                UeIds = new List<long>() { UE_ID_2, UE_ID_3 },
+                TipoReuniaoId = TipoReuniaoConstants.ATENDIMENTO_NAO_PRESENCIAL_ID,
+                DataRegistro = DateTimeExtension.HorarioBrasilia().Date,
+                QuantidadeCuidadores = 1,
+                QuantidadeEducadores = 2,
+                QuantidadeEducandos = 3,
+                QuantidadeParticipantes = 4,
+                Descricao = "Registro coletivo",
+                Observacao = "Observação"
+            };
+            var useCase = ServiceProvider.GetService<ISalvarRegistroColetivoUseCase>();
+        }
+    }
+}
