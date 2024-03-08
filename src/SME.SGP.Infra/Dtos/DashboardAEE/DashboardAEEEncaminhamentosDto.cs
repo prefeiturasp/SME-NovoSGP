d311a1bd34c980841ae8cac3f8aa16073f69deba
DanylloPereira <danyllo.antunes@gmail.com>
2024-03-08 09:22:56 -0300

    98793 - Ajuste

--- src/SME.SGP.Infra/Dtos/DashboardAEE/DashboardAEEEncaminhamentosDto.cs
+++ src/SME.SGP.Infra/Dtos/DashboardAEE/DashboardAEEEncaminhamentosDto.cs
@@ -5,7 +5,6 @@ namespace SME.SGP.Infra.Dtos
     public class DashboardAEEEncaminhamentosDto
     {
         public long QtdeEncaminhamentosSituacao { get; set; }
-
         public long TotalEncaminhamentosAnalise { get; set; }
         public IEnumerable<AEESituacaoEncaminhamentoDto> SituacoesEncaminhamentoAEE { get; set; }
     }
