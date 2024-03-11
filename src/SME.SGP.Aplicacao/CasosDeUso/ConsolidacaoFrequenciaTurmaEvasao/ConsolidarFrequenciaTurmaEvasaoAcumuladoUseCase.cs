af9f81c58dce40cbf67732c929890b8b26e239bf
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-03-11 11:02:13 -0300

    106651 - Persistencia de alunos de frequencia turma evasão

--- src/SME.SGP.Aplicacao/CasosDeUso/ConsolidacaoFrequenciaTurmaEvasao/ConsolidarFrequenciaTurmaEvasaoAcumuladoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/ConsolidacaoFrequenciaTurmaEvasao/ConsolidarFrequenciaTurmaEvasaoAcumuladoUseCase.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
+using Minio.DataModel;
 using SME.SGP.Dominio;
 using SME.SGP.Infra;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
@@ -21,8 +23,6 @@ namespace SME.SGP.Aplicacao
         public async Task<bool> Executar(MensagemRabbit mensagem)
         {
             var filtro = mensagem.ObterObjetoMensagem<FiltroConsolidacaoFrequenciaTurmaEvasaoAcumulado>();
-            int quantidadeAlunosAbaixo50Porcento;
-            int quantidadeAlunox0Porcento;
             var dataConsulta = filtro.Ano == DateTime.Now.Year ? DateTime.Today : new DateTime(filtro.Ano, 12, 31);
 
             if (filtro.TurmaId == 0)
@@ -36,81 +36,73 @@ namespace SME.SGP.Aplicacao
                     foreach (var ue in ues)
                     {
                         var turmas = await mediator.Send(new ObterTurmasComModalidadePorAnoUEQuery(filtro.Ano, ue));
-
                         foreach (var turma in turmas)
-                        {                            
-                            quantidadeAlunosAbaixo50Porcento = 0;
-                            quantidadeAlunox0Porcento = 0;
-
-                            var alunos = await mediator.Send(new ObterAlunosAtivosPorTurmaCodigoQuery(turma.TurmaCodigo, dataConsulta));
-                            alunos = alunos.Where(s => s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Ativo ||
-                                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Rematriculado ||
-                                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.PendenteRematricula ||
-                                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.SemContinuidade ||
-                                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Concluido);
-                            if (alunos.EhNulo() || !alunos.Any())
-                                continue;
-
-                            var alunosFrequencia = await mediator.Send(new ObterFrequenciaGeralPorAlunosTurmaQuery(alunos.Select(a => a.CodigoAluno).ToArray(), turma.TurmaCodigo));
-
-                            quantidadeAlunosAbaixo50Porcento = alunosFrequencia.Count(a => a.PercentualFrequencia < 50);
-                            quantidadeAlunox0Porcento = alunosFrequencia.Count(a => a.PercentualFrequencia == 0);                           
-
-                            unitOfWork.IniciarTransacao();
-                            try
-                            {
-                                await mediator.Send(new LimparFrequenciaTurmaEvasaoPorTurmasEMesesCommand(new long[] { turma.TurmaId }, new int[] { 0 }));
-                                await RegistrarFrequenciaTurmaEvasao(turma.TurmaId, 0, quantidadeAlunosAbaixo50Porcento, quantidadeAlunox0Porcento);
-
-                                unitOfWork.PersistirTransacao();
-                            }
-                            catch
-                            {
-                                unitOfWork.Rollback();
-                                throw;
-                            }
-                        }
+                            await GerarFrequenciaTurmaEvasao(turma.TurmaCodigo, turma.TurmaId, dataConsulta);
                     }
                 }
             }
             else
             {
                 var turma = await mediator.Send(new ObterTurmaPorIdQuery(filtro.TurmaId));
-
-                var alunos = await mediator.Send(new ObterAlunosAtivosPorTurmaCodigoQuery(turma.CodigoTurma, dataConsulta));
-                alunos = alunos.Where(s => s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Ativo ||
-                                        s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Rematriculado ||
-                                        s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.PendenteRematricula ||
-                                        s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.SemContinuidade ||
-                                        s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Concluido);
-                if (alunos.EhNulo() || !alunos.Any())
-                    throw new NegocioException($"Não foram encontrados alunos para a turma {turma.CodigoTurma} no Eol");
-
-                var alunosFrequencia = await mediator.Send(new ObterFrequenciaGeralPorAlunosTurmaQuery(alunos.Select(a => a.CodigoAluno).ToArray(), turma.CodigoTurma));
-
-                quantidadeAlunosAbaixo50Porcento = alunosFrequencia.Count(a => a.PercentualFrequencia < 50);
-                quantidadeAlunox0Porcento = alunosFrequencia.Count(a => a.PercentualFrequencia == 0);
-               
-                unitOfWork.IniciarTransacao();
-                try
-                {
-                    await mediator.Send(new LimparFrequenciaTurmaEvasaoPorTurmasEMesesCommand(new long[] { filtro.TurmaId }, new int[] { 0 }));
-                    await RegistrarFrequenciaTurmaEvasao(filtro.TurmaId, 0, quantidadeAlunosAbaixo50Porcento, quantidadeAlunox0Porcento);
-
-                    unitOfWork.PersistirTransacao();
-                }
-                catch
-                {
-                    unitOfWork.Rollback();
-                    throw;
-                }
+                await GerarFrequenciaTurmaEvasao(turma.CodigoTurma, turma.Id, dataConsulta);
             }
             return true;
         }
 
-        private async Task RegistrarFrequenciaTurmaEvasao(long turmaId, int mes, int quantidadeAlunosAbaixo50Porcento, int quantidadeAlunos0Porcento)
+        private IEnumerable<(string AlunoCodigo, 
+                             string AlunoNome, 
+                             double PercentualFrequencia)> ObterAlunosFrequenciaInferior50(IEnumerable<AlunoPorTurmaResposta> alunos, 
+                                                                                           IEnumerable<FrequenciaAluno> frequencias)
         {
-            await mediator.Send(new RegistrarFrequenciaTurmaEvasaoCommand(turmaId, mes, quantidadeAlunosAbaixo50Porcento, quantidadeAlunos0Porcento));
+            foreach (var frequencia in frequencias.Where(a => a.PercentualFrequencia < 50))
+            {
+                var aluno = alunos.FirstOrDefault(a => a.CodigoAluno.Equals(frequencia.CodigoAluno));
+                yield return (aluno?.CodigoAluno, aluno?.NomeAluno, frequencia.PercentualFrequencia);
+            }
+        }
+
+        private async Task GerarFrequenciaTurmaEvasao(string codigoTurma, long idTurma, DateTime dataConsulta)
+        {
+            var alunos = await mediator.Send(new ObterAlunosAtivosPorTurmaCodigoQuery(codigoTurma, dataConsulta));
+            alunos = alunos.Where(s => s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Ativo ||
+                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Rematriculado ||
+                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.PendenteRematricula ||
+                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.SemContinuidade ||
+                                    s.CodigoSituacaoMatricula == SituacaoMatriculaAluno.Concluido);
+            if (alunos.EhNulo() || !alunos.Any())
+                throw new NegocioException($"Não foram encontrados alunos para a turma {codigoTurma} no Eol");
+
+            var alunosFrequencia = await mediator.Send(new ObterFrequenciaGeralPorAlunosTurmaQuery(alunos.Select(a => a.CodigoAluno).ToArray(), codigoTurma));
+
+            int quantidadeAlunosAbaixo50Porcento = alunosFrequencia.Count(a => a.PercentualFrequencia < 50);
+            int quantidadeAlunox0Porcento = alunosFrequencia.Count(a => a.PercentualFrequencia == 0);
+
+            unitOfWork.IniciarTransacao();
+            try
+            {
+                await mediator.Send(new LimparFrequenciaTurmaEvasaoAlunoPorTurmasEMesesCommand(new long[] { idTurma }, new int[] { 0 }));
+                await mediator.Send(new LimparFrequenciaTurmaEvasaoPorTurmasEMesesCommand(new long[] { idTurma }, new int[] { 0 }));
+                var id = await RegistrarFrequenciaTurmaEvasao(idTurma, 0, quantidadeAlunosAbaixo50Porcento, quantidadeAlunox0Porcento);
+                await RegistrarFrequenciaTurmaEvasaoAluno(id, ObterAlunosFrequenciaInferior50(alunos, alunosFrequencia));
+                unitOfWork.PersistirTransacao();
+            }
+            catch (Exception e)
+            {
+                _ = e.Message;
+                unitOfWork.Rollback();
+                throw;
+            }
+        }
+
+        private async Task<long> RegistrarFrequenciaTurmaEvasao(long turmaId, int mes, int quantidadeAlunosAbaixo50Porcento, int quantidadeAlunos0Porcento)
+        {
+            return await mediator.Send(new RegistrarFrequenciaTurmaEvasaoCommand(turmaId, mes, quantidadeAlunosAbaixo50Porcento, quantidadeAlunos0Porcento));
+        }
+
+        private async Task RegistrarFrequenciaTurmaEvasaoAluno(long frequenciaTurmaEvasaoId, IEnumerable<(string AlunoCodigo, string AlunoNome, double PercentualFrequencia)> frequenciasAlunos)
+        {
+            foreach (var freqAluno in frequenciasAlunos)
+                await mediator.Send(new RegistrarFrequenciaTurmaEvasaoAlunoCommand(frequenciaTurmaEvasaoId, freqAluno.AlunoCodigo, freqAluno.AlunoNome, freqAluno.PercentualFrequencia));
         }
     }
 }
