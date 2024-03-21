3199f98ccfbab3bfe1303e0d95ed9d0d505d3b34
DanylloPereira <danyllo.antunes@gmail.com>
2024-03-21 16:37:18 -0300

    107657 - Criar fila de exclusão de notificações

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Notificacoes/ExecutarExclusaoNotificacaoUseCase.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso
+{
+    public class ExecutarExclusaoNotificacaoUseCase : AbstractUseCase, IExecutarExclusaoNotificacaoUseCase
+    {
+        public ExecutarExclusaoNotificacaoUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(MensagemRabbit param)
+        {
+            long.TryParse(param.Mensagem.ToString(), out long notificacaoId);
+
+            if (notificacaoId > 0)
+                await mediator.Send(new ExcluirNotificacaoTotalPorIdCommand(notificacaoId));
+
+            return true;
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Notificacoes/ExecutarExclusaoNotificacoesPeriodicamenteUseCase.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SGP.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ExecutarExclusaoNotificacoesPeriodicamenteUseCase : AbstractUseCase, IExecutarExclusaoNotificacoesPeriodicamenteUseCase
+    {
+        public ExecutarExclusaoNotificacoesPeriodicamenteUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(MensagemRabbit param)
+        {
+            var idsNotificacoes = await mediator.Send(new ObterIdsDasNotificacoesParaExclusaoPorDiasQuery());
+
+            foreach (var id in idsNotificacoes)
+                await mediator.Send(new PublicarFilaSgpCommand(RotasRabbitSgp.RotaExecutarExclusaoDaNotificacao, id, Guid.NewGuid()));
+
+            return true;
+        }
+    }
+}
