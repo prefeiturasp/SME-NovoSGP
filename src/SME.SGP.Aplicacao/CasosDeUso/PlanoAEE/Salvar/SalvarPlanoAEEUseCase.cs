8580aeca843948a76c9c4360fca1699c69a54dd1
Igor Hebling Sallowicz <igor.sallo@hotmail.com>
2024-04-02 09:24:23 -0300

    Adicionado validação de Periodo Escolar e ajustado tratamento para matriculas que não contem turma regular no plano aee AB#118339

--- src/SME.SGP.Aplicacao/CasosDeUso/PlanoAEE/Salvar/SalvarPlanoAEEUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/PlanoAEE/Salvar/SalvarPlanoAEEUseCase.cs
@@ -4,6 +4,7 @@ using SME.SGP.Infra;
 using System;
 using System.Threading.Tasks;
 using SME.SGP.Dominio.Constantes.MensagensNegocio;
+using System.Linq;
 
 namespace SME.SGP.Aplicacao.CasosDeUso
 {
@@ -33,7 +34,47 @@ namespace SME.SGP.Aplicacao.CasosDeUso
 
             await mediator.Send(new SalvarPlanoAEETurmaAlunoCommand(planoAeePersistidoDto.PlanoId, aluno.CodigoAluno));
 
+            await ValidaQuestaoPeriodoEscolarSeEstaNoPeriodoCorreto(planoAeePersistidoDto);
+
             return planoAeePersistidoDto;
-        }     
+        }
+
+        private async Task ValidaQuestaoPeriodoEscolarSeEstaNoPeriodoCorreto(RetornoPlanoAEEDto questaoPeriodoEscolar)
+        {
+            var planoAee = await mediator.Send(new ObterPlanoAEEPorIdQuery(questaoPeriodoEscolar.PlanoId));
+            
+            if(planoAee.EhNulo())
+                throw new NegocioException(MensagemNegocioPlanoAee.Plano_aee_nao_encontrado);
+
+            var ultimaVersaoPlanoAee = await mediator.Send(new ObterVersaoPlanoAEEPorIdQuery(questaoPeriodoEscolar.PlanoVersaoId));
+
+            if(ultimaVersaoPlanoAee.NaoEhNulo() && ultimaVersaoPlanoAee.Numero > 1)
+            {
+                var turma = await mediator.Send(new ObterTurmaPorIdQuery(planoAee.TurmaId));
+
+                if (turma.EhNulo())
+                    throw new NegocioException(MensagemNegocioTurma.TURMA_NAO_ENCONTRADA);
+
+                var periodoEscolarPlano = await mediator.Send(new ObterPeriodoEscolarAtualPorTurmaQuery(turma, DateTime.Now));
+
+                if(periodoEscolarPlano.EhNulo())
+                    throw new NegocioException(MensagemNegocioPeriodo.PERIODO_ESCOLAR_NAO_ENCONTRADO);
+
+                var respostaQuestoesPlanoAee = await mediator.Send(new ObterRespostasPlanoAEEPorVersaoQuery(ultimaVersaoPlanoAee.Id));
+
+                if(respostaQuestoesPlanoAee.NaoEhNulo() && respostaQuestoesPlanoAee.Any())
+                {
+                    //Obtem resposta da questao Bimestre de vigência do plano
+                    var questaoAValidar = respostaQuestoesPlanoAee.FirstOrDefault(x => x.QuestaoId == 41);
+
+                    if (questaoAValidar.NaoEhNulo() && (long)Convert.ToDouble(questaoAValidar.Texto) != periodoEscolarPlano.Id)
+                    {
+                        var planoAEEQuestaoId = await mediator.Send(new SalvarPlanoAEEQuestaoCommand(planoAee.Id, 41, ultimaVersaoPlanoAee.Id));
+
+                        await mediator.Send(new SalvarPlanoAEERespostaCommand(planoAee.Id, planoAEEQuestaoId, periodoEscolarPlano.Id.ToString(), TipoQuestao.PeriodoEscolar));
+                    }
+                }
+            }
+        }
     }
 }
