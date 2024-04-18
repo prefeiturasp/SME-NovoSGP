51d4e1103c50de7e0822e3215983138ddc18c002
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-04-18 16:22:46 -0300

    Ajuste padronizacao end point

--- src/SME.SGP.Api/Controllers/MapeamentoEstudanteController.cs
+++ src/SME.SGP.Api/Controllers/MapeamentoEstudanteController.cs
@@ -14,10 +14,19 @@ using System.Threading.Tasks;
 namespace SME.SGP.Api.Controllers
 {
     [ApiController]
-    [Route("api/v1/mapeamento-estudante")]
+    [Route("api/v1/mapeamentos-estudantes")]
     [Authorize("Bearer")]
     public class MapeamentoEstudanteController : ControllerBase
     {
+        [HttpPost()]
+        [ProducesResponseType(typeof(IEnumerable<ResultadoRegistroAcaoBuscaAtivaDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Permissao(Permissao.ME_I, Permissao.ME_A, Policy = "Bearer")]
+        public async Task<IActionResult> RegistrarMapeamentoEstudante([FromBody] RegistroAcaoBuscaAtivaDto registroAcaoDto, [FromServices] IRegistrarRegistroAcaoUseCase registrarRegistroAcaoUseCase)
+        {
+            return Ok(await registrarRegistroAcaoUseCase.Executar(registroAcaoDto));
+        }
+
         [HttpGet("secoes")]
         [ProducesResponseType(typeof(IEnumerable<SecaoQuestionarioDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
@@ -28,7 +37,7 @@ namespace SME.SGP.Api.Controllers
             return Ok(await obterSecoesMapeamentoSecaoUseCase.Executar(mapeamentoEstudanteId));
         }
 
-        [HttpGet("questionario/{questionarioId}")]
+        [HttpGet("questionarios/{questionarioId}")]
         [ProducesResponseType(typeof(IEnumerable<QuestaoDto>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [Permissao(Permissao.ME_C, Policy = "Bearer")]
