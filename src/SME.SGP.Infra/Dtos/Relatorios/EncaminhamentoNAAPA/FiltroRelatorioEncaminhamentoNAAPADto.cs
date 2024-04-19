d1d1b6e5c3edd7886a657d46689c859bebf7bd8b
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-04-19 16:00:40 -0300

    Bug: 120361 Ajuste no filtro de encaminhamento NAAPA considerando corretamente os filtros enviados pelo front com os ajustes realizados para sereme considerados na impressão.

--- src/SME.SGP.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/FiltroRelatorioEncaminhamentoNAAPADto.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/EncaminhamentoNAAPA/FiltroRelatorioEncaminhamentoNAAPADto.cs
@@ -1,15 +1,20 @@
-﻿namespace SME.SGP.Infra
+﻿using SME.SGP.Dominio;
+
+namespace SME.SGP.Infra
 {
     public class FiltroRelatorioEncaminhamentoNAAPADto
     {
+        public bool ConsideraHistorico { get; set; }
+        public int AnoLetivo { get; set; }
         public string DreCodigo { get; set; }
         public string UeCodigo { get; set; }
+        public Modalidade[] Modalidades { get; set; }
+        public string[] AnosEscolaresCodigos { get; set; }
         public int[] SituacaoIds { get; set; }
         public bool ExibirEncerrados { get; set; }
         public int[] FluxoAlertaIds { get; set; }
         public int[] PortaEntradaIds { get; set; }
         public string UsuarioNome { get; set; }
         public string UsuarioRf { get; set; }
-        public long[] Ids { get; set; }
     }
 }
