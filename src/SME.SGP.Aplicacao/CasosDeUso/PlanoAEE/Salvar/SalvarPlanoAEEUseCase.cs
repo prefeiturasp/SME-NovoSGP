a4beb096fab836e0b5c6bfcda77e8fc0440e3281
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-04-23 16:35:49 -0300

    Revert "Bug: 120344"
    This reverts commit 80e4a50dae96b1bc355fad4e71172018140875c2.

--- src/SME.SGP.Aplicacao/CasosDeUso/PlanoAEE/Salvar/SalvarPlanoAEEUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/PlanoAEE/Salvar/SalvarPlanoAEEUseCase.cs
@@ -1,22 +1,19 @@
 ﻿using MediatR;
 using SME.SGP.Dominio;
-using SME.SGP.Dominio.Constantes.MensagensNegocio;
-using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
 using System;
 using System.Threading.Tasks;
+using SME.SGP.Dominio.Constantes.MensagensNegocio;
 
 namespace SME.SGP.Aplicacao.CasosDeUso
 {
     public class SalvarPlanoAEEUseCase : ISalvarPlanoAEEUseCase
     {
         private readonly IMediator mediator;
-        private readonly IUnitOfWork unitOfWork;
 
-        public SalvarPlanoAEEUseCase(IMediator mediator, IUnitOfWork unitOfWork)
+        public SalvarPlanoAEEUseCase(IMediator mediator)
         {
             this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         }
 
         public async Task<RetornoPlanoAEEDto> Executar(PlanoAEEPersistenciaDto planoAeeDto)
@@ -32,26 +29,11 @@ namespace SME.SGP.Aplicacao.CasosDeUso
             if (aluno.EhNulo())
                 throw new NegocioException(MensagemNegocioAluno.ESTUDANTE_NAO_ENCONTRADO);
 
-            using var transacao = unitOfWork.IniciarTransacao();
+            var planoAeePersistidoDto = await mediator.Send(new SalvarPlanoAeeCommand(planoAeeDto, turma.Id, aluno.NomeAluno, aluno.CodigoAluno, aluno.ObterNumeroAlunoChamada()));
 
-            try
-            {
-                var planoAeePersistidoDto = await mediator
-                    .Send(new SalvarPlanoAeeCommand(planoAeeDto, turma.Id, aluno.NomeAluno, aluno.CodigoAluno, aluno.ObterNumeroAlunoChamada()));
+            await mediator.Send(new SalvarPlanoAEETurmaAlunoCommand(planoAeePersistidoDto.PlanoId, aluno.CodigoAluno));
 
-                await mediator
-                    .Send(new SalvarPlanoAEETurmaAlunoCommand(planoAeePersistidoDto.PlanoId, aluno.CodigoAluno));
-
-                unitOfWork.PersistirTransacao();
-
-                return planoAeePersistidoDto;
-            }
-            catch (Exception ex)
-            {
-                await mediator.Send(new SalvarLogViaRabbitCommand($"Não foi possível salvar o Plano AEE {planoAeeDto.Id}", LogNivel.Negocio, LogContexto.Geral, ex.Message, rastreamento: ex.StackTrace, innerException: ex.InnerException.ToString()));
-                unitOfWork.Rollback();
-                throw;
-            }
-        }
+            return planoAeePersistidoDto;
+        }     
     }
 }
