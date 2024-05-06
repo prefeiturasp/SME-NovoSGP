ccd27a4f105dd248c967c4fefad932fb44b821e9
Thiago Fernando <thiago.fernando@msn.com>
2024-05-06 12:00:11 -0300

    Criado Commands do SalvarRelatorioPAP

--- src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/SalvarRelatorioPAPUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/SalvarRelatorioPAPUseCase.cs
@@ -30,13 +30,13 @@ namespace SME.SGP.Aplicacao
             if (relatorioPAPDto.PAPTurmaId.HasValue)
                 return await AlterarRelatorioPAP(relatorioPAPDto);
 
-            return await SalvarRelatorioPAP(relatorioPAPDto);
+            return await  mediator.Send(new SalvarRelatorioPAPCommand(relatorioPAPDto));
         }
 
         private async Task<ResultadoRelatorioPAPDto> AlterarRelatorioPAP(RelatorioPAPDto relatorioPAPDto)
         {
             var resultado = new ResultadoRelatorioPAPDto();
-            var relatorioAluno = await PersistirRelatorioAluno(relatorioPAPDto, relatorioPAPDto.PAPTurmaId.Value);
+            var relatorioAluno = await mediator.Send(new PersistirRelatorioAlunoCommand(relatorioPAPDto, relatorioPAPDto.PAPTurmaId.Value));
 
             resultado.PAPTurmaId = relatorioPAPDto.PAPTurmaId.Value;
             resultado.PAPAlunoId = relatorioAluno.Id;
@@ -51,74 +51,34 @@ namespace SME.SGP.Aplicacao
 
         private async Task<ResultadoRelatorioPAPSecaoDto> AlterarSecao(RelatorioPAPSecaoDto secao, long relatorioAlunoId)
         {
-            var relatorioSecao = await PersistirRelatorioSecao(secao, relatorioAlunoId);
+            var relatorioSecao = await mediator.Send(new PersistirRelatorioSecaoCommand(secao, relatorioAlunoId));
             
             foreach (var questaoRespostas in secao.Respostas.GroupBy(q => q.QuestaoId))
             {
                 var questaoExistente = relatorioSecao.Questoes.FirstOrDefault(q => q.QuestaoId == questaoRespostas.Key);
 
                 if (questaoExistente.EhNulo())
-                    await SalvarQuestao(relatorioSecao.Id, questaoRespostas.Key, questaoRespostas);
+                    await mediator.Send(new SalvarQuestaoCommand(relatorioSecao.Id, questaoRespostas.Key, questaoRespostas));
                 else
                     await AlterarQuestao(questaoExistente, questaoRespostas);
             }
 
-            return ObterResultadoSecao(relatorioSecao, secao.SecaoId);
-        }
-
-        private async Task<RelatorioPeriodicoPAPSecao> PersistirRelatorioSecao(RelatorioPAPSecaoDto secao, long relatorioAlunoId)
-        {
-            if (!secao.Respostas.Any())
-                throw new NegocioException(string.Format(MensagemNegocioComuns.NENHUMA_QUESTAO_FOI_ENCONTRADA_NA_SECAO_X, secao.SecaoId));
-
-            if (secao.Id.HasValue)
+            return new ResultadoRelatorioPAPSecaoDto()
             {
-                var relatorioSecao = await mediator.Send(new ObterRelatorioPeriodicoSecaoPAPQuery(secao.Id.Value));
-
-                await mediator.Send(new AlterarRelatorioPeriodicoSecaoPAPCommand(relatorioSecao));
-
-                return relatorioSecao;
-            }
-
-            return await mediator.Send(new SalvarRelatorioPeriodicoSecaoPAPCommand(secao.SecaoId, relatorioAlunoId)); 
+                SecaoId = relatorioSecao.Id,
+                Auditoria = new AuditoriaDto()
+                {
+                    Id = secao.SecaoId,
+                    CriadoEm = relatorioSecao.CriadoEm,
+                    CriadoPor = relatorioSecao.CriadoPor,
+                    CriadoRF = relatorioSecao.CriadoRF,
+                    AlteradoEm = relatorioSecao.AlteradoEm,
+                    AlteradoPor = relatorioSecao.AlteradoPor,
+                    AlteradoRF = relatorioSecao.AlteradoRF
+                }
+            }; 
         }
-
-        private async Task<ResultadoRelatorioPAPDto> SalvarRelatorioPAP(RelatorioPAPDto relatorioPAPDto)
-        {
-            var resultado = new ResultadoRelatorioPAPDto();
-            var relatorioTurma = await mediator.Send(new SalvarRelatorioPeriodicoTurmaPAPCommand(relatorioPAPDto.TurmaId, relatorioPAPDto.periodoRelatorioPAPId));
-            var relatorioAluno = await PersistirRelatorioAluno(relatorioPAPDto, relatorioTurma.Id);
-
-            resultado.PAPTurmaId = relatorioTurma.Id;
-            resultado.PAPAlunoId = relatorioAluno.Id;
-
-            foreach (var secao in relatorioPAPDto.Secoes)
-            {
-                resultado.Secoes.Add(await SalvarSecao(secao, relatorioAluno.Id));
-            }
-
-            return resultado;
-        }
-
-        private async Task<ResultadoRelatorioPAPSecaoDto> SalvarSecao(RelatorioPAPSecaoDto secao, long relatorioAlunoId)
-        {
-            var relatorioSecao = await PersistirRelatorioSecao(secao, relatorioAlunoId);
-
-            foreach (var questoes in secao.Respostas.GroupBy(q => q.QuestaoId))
-            {
-                await SalvarQuestao(relatorioSecao.Id, questoes.Key, questoes);
-            }
-
-            return ObterResultadoSecao(relatorioSecao, secao.SecaoId);
-        }
-
-        private async Task SalvarQuestao(long relatorioSecaoId, long questaoId, IEnumerable<RelatorioPAPRespostaDto> respostas)
-        {
-            var relatorioQuestao = await mediator.Send(new SalvarRelatorioPeriodicoQuestaoPAPCommand(relatorioSecaoId, questaoId));
-
-            await SalvarResposta(respostas, relatorioQuestao.Id);
-        }
-
+        
         private async Task AlterarQuestao(RelatorioPeriodicoPAPQuestao questaoExistente, IEnumerable<RelatorioPAPRespostaDto> respostas)
         {
             if (questaoExistente.Excluido)
@@ -160,42 +120,6 @@ namespace SME.SGP.Aplicacao
             foreach (var questao in repostasIncluidas)
                 await mediator.Send(new SalvarRelatorioPeriodicoRespostaPAPCommand(questao.Resposta, questao.TipoQuestao, questaoExistente.Id));
 
-        }
-
-        private ResultadoRelatorioPAPSecaoDto ObterResultadoSecao(RelatorioPeriodicoPAPSecao relatorioSecao, long secaoId)
-        {
-            return new ResultadoRelatorioPAPSecaoDto()
-            {
-                SecaoId = relatorioSecao.Id,
-                Auditoria = new AuditoriaDto()
-                {
-                    Id = secaoId,
-                    CriadoEm = relatorioSecao.CriadoEm,
-                    CriadoPor = relatorioSecao.CriadoPor,
-                    CriadoRF = relatorioSecao.CriadoRF,
-                    AlteradoEm = relatorioSecao.AlteradoEm,
-                    AlteradoPor = relatorioSecao.AlteradoPor,
-                    AlteradoRF = relatorioSecao.AlteradoRF
-                }
-            }; 
-        }
-
-        private async Task SalvarResposta(IEnumerable<RelatorioPAPRespostaDto> respostas, long relatorioQuestaoId)
-        {
-            foreach (var resposta in respostas)
-            {
-                await mediator.Send(new SalvarRelatorioPeriodicoRespostaPAPCommand(resposta.Resposta, resposta.TipoQuestao, relatorioQuestaoId));
-            }
-        }
-
-        private async Task<RelatorioPeriodicoPAPAluno> PersistirRelatorioAluno(RelatorioPAPDto relatorioPAPDto, long relatorioTurmaId)
-        {
-            if (relatorioPAPDto.PAPAlunoId.HasValue)
-                return await mediator.Send(new ObterRelatorioPeriodicoAlunoPAPQuery(relatorioPAPDto.PAPAlunoId.Value));
-
-            return await mediator.Send(new SalvarRelatorioPeriodicoAlunoPAPCommand(relatorioPAPDto.AlunoCodigo, 
-                                                                                   relatorioPAPDto.AlunoNome,
-                                                                                   relatorioTurmaId));
         }
 
         private async Task ValidarQuestoesObrigatorias(RelatorioPAPDto relatorioPAPDto, string turmaCodigo)
---
+++ src/SME.SGP.Aplicacao/Commands/RelatorioPAP/PersistirRelatorioSecao/PersistirRelatorioSecaoCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Constantes.MensagensNegocio;
+
+namespace SME.SGP.Aplicacao
+{
+    public class PersistirRelatorioSecaoCommandHandler : IRequestHandler<PersistirRelatorioSecaoCommand,RelatorioPeriodicoPAPSecao>
+    {
+        private readonly IMediator mediator;
+
+        public PersistirRelatorioSecaoCommandHandler(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<RelatorioPeriodicoPAPSecao> Handle(PersistirRelatorioSecaoCommand request, CancellationToken cancellationToken)
+        {
+            var secao = request.Secao;
+            if (!secao.Respostas.Any())
+                throw new NegocioException(string.Format(MensagemNegocioComuns.NENHUMA_QUESTAO_FOI_ENCONTRADA_NA_SECAO_X, secao.SecaoId));
+
+            if (!secao.Id.HasValue)
+                return await mediator.Send(
+                    new SalvarRelatorioPeriodicoSecaoPAPCommand(secao.SecaoId, request.RelatorioAlunoId),
+                    cancellationToken);
+            var relatorioSecao = await mediator.Send(new ObterRelatorioPeriodicoSecaoPAPQuery(secao.Id.Value), cancellationToken);
+
+            await mediator.Send(new AlterarRelatorioPeriodicoSecaoPAPCommand(relatorioSecao), cancellationToken);
+
+            return relatorioSecao;
+
+        }
+    }
+}
