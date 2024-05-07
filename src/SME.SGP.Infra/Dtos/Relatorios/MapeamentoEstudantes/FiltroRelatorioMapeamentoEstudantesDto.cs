601ac4d8c66580b44b428d7755c213015ddeea88
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-05-07 09:00:45 -0300

    118351 - Ajuste nullables filtros relatorio

--- src/SME.SGP.Infra/Dtos/Relatorios/MapeamentoEstudantes/FiltroRelatorioMapeamentoEstudantesDto.cs
+++ src/SME.SGP.Infra/Dtos/Relatorios/MapeamentoEstudantes/FiltroRelatorioMapeamentoEstudantesDto.cs
@@ -12,14 +12,14 @@ namespace SME.SGP.Infra
         public string[] TurmasCodigo { get; set; }
         public string AlunoCodigo { get; set; }
         public long[] PareceresConclusivosIdAnoAnterior { get; set; }
-        public long OpcaoRespostaIdDistorcaoIdadeAnoSerie { get; set; }
-        public long OpcaoRespostaIdPossuiPlanoAEE { get; set; }
-        public long OpcaoRespostaIdAcompanhadoNAAPA { get; set; }
+        public long? OpcaoRespostaIdDistorcaoIdadeAnoSerie { get; set; }
+        public long? OpcaoRespostaIdPossuiPlanoAEE { get; set; }
+        public long? OpcaoRespostaIdAcompanhadoNAAPA { get; set; }
         public string OpcaoRespostaPossuiPAP { get; set; }
-        public long OpcaoRespostaIdProgramaSPIntegral { get; set; }
+        public long? OpcaoRespostaIdProgramaSPIntegral { get; set; }
         public string OpcaoRespostaHipoteseEscrita { get; set; }
         public string OpcaoRespostaAvaliacaoExternaProvaSP { get; set; }
-        public long OpcaoRespostaIdFrequencia { get; set; }
+        public long? OpcaoRespostaIdFrequencia { get; set; }
         public string UsuarioNome { get; set; }
         public string UsuarioRf { get; set; }
     }
