9a6969dce442a33e55b82c8cf932db3254831be2
Thiago Fernando <thiago.fernando@msn.com>
2024-05-08 11:05:28 -0300

    Refact ICopiarRelatorioPAPUseCase

--- src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/CopiarRelatorioPAPUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/RelatoriosPAP/CopiarRelatorioPAPUseCase.cs
@@ -15,45 +15,51 @@ namespace SME.SGP.Aplicacao
         public async Task<bool> Executar(CopiarPapDto copiarPapDto)
         {
             var turma = await mediator.Send(new ObterTurmaPorCodigoQuery(copiarPapDto.CodigoTurma));
+            var obterSecoesOrigem = await mediator.Send(new ObterSecoesPAPQuery(copiarPapDto.CodigoTurmaOrigem, copiarPapDto.CodigoAlunoOrigem, copiarPapDto.PeriodoRelatorioPAPId));
             foreach (var estudante in copiarPapDto.Estudantes)
             {
 
-                var obterSecoesOrigem = await mediator.Send(new ObterSecoesPAPQuery(copiarPapDto.CodigoTurmaOrigem, copiarPapDto.CodigoAlunoOrigem, copiarPapDto.PeriodoRelatorioPAPId));
+                var obterSecoesDestino = await mediator.Send(new ObterSecoesPAPQuery(copiarPapDto.CodigoTurma, estudante.AlunoCodigo, copiarPapDto.PeriodoRelatorioPAPId));
                 var relatorioPAPDto = new RelatorioPAPDto()
                 {
                     periodoRelatorioPAPId = copiarPapDto.PeriodoRelatorioPAPId,
                     TurmaId = turma.Id,
                     AlunoCodigo = estudante.AlunoCodigo,
                     AlunoNome = estudante.AlunoNome,
-                    PAPTurmaId = obterSecoesOrigem.PAPTurmaId,
-                    PAPAlunoId = obterSecoesOrigem.PAPAlunoId
+                    PAPTurmaId = obterSecoesDestino.PAPTurmaId,
+                    PAPAlunoId = obterSecoesDestino.PAPAlunoId
                 };
                 
-                foreach (var questionarioSecaoId in obterSecoesOrigem.Secoes)
+                foreach (var questionarioSecaoId in obterSecoesDestino.Secoes)
                 {
                     var sessao = new RelatorioPAPSecaoDto()
                     {
-                        Id = questionarioSecaoId.Id,
+                        Id = questionarioSecaoId.PAPSecaoId,
                         SecaoId = questionarioSecaoId.Id
                     };
-                    var questoes = await mediator.Send(new ObterQuestionarioPAPPorPeriodoQuery(copiarPapDto.CodigoTurmaOrigem,
+                    var secaoOrigem = obterSecoesOrigem.Secoes.FirstOrDefault(x => x.Id == questionarioSecaoId.Id);
+                    var questoesOrigem = await mediator.Send(new ObterQuestionarioPAPPorPeriodoQuery(copiarPapDto.CodigoTurmaOrigem,
                                                                                                                         copiarPapDto.CodigoAlunoOrigem,
                                                                                                                         copiarPapDto.PeriodoRelatorioPAPId,
-                                                                                                                        questionarioSecaoId.QuestionarioId,
-                                                                                                                        questionarioSecaoId.Id));
+                                                                                                                        secaoOrigem.QuestionarioId,
+                                                                                                                        secaoOrigem.PAPSecaoId));
+                    
+                    var questoesDestino = await mediator.Send(new ObterQuestionarioPAPPorPeriodoQuery(copiarPapDto.CodigoTurma,
+                                                                                                                                        estudante.AlunoCodigo,
+                                                                                                                                        copiarPapDto.PeriodoRelatorioPAPId,
+                                                                                                                                        questionarioSecaoId.QuestionarioId,
+                                                                                                                                        questionarioSecaoId.PAPSecaoId));
 
-                    foreach (var questao in questoes)
+                    foreach (var questao in questoesDestino)
                     {
-                        foreach (var respostas in questao.Resposta)
+                        if (copiarPapDto.Secoes.SelectMany(s => s.QuestoesIds).Contains(questao.Id))
                         {
-                            var resposta = new RelatorioPAPRespostaDto()
-                            {
-                                RelatorioRespostaId = respostas.Id,
-                                Resposta = respostas.Texto, 
-                                QuestaoId = questao.Id,
-                                TipoQuestao = questao.TipoQuestao
-                            };
-                            sessao.Respostas.Add(resposta);
+                            var questaoOrigem = questoesOrigem.FirstOrDefault(x => x.Id == questao.Id);
+                            CriarRelatorioPAPResposta(questaoOrigem, sessao, true);
+                        }
+                        else
+                        {
+                            CriarRelatorioPAPResposta(questao, sessao, false);
                         }
                     }
                     
@@ -63,6 +69,21 @@ namespace SME.SGP.Aplicacao
                 await mediator.Send(new PersistirRelatorioPAPCommand(relatorioPAPDto));
             }
             return true;
+        }
+
+        private static void CriarRelatorioPAPResposta(QuestaoDto questao, RelatorioPAPSecaoDto sessao, bool ehquestaoOrigem)
+        {
+            foreach (var respostas in questao.Resposta)
+            {
+                var resposta = new RelatorioPAPRespostaDto()
+                {
+                    RelatorioRespostaId = ehquestaoOrigem ? null : respostas.Id,
+                    Resposta = respostas.OpcaoRespostaId?.ToString() ?? respostas.Texto,  
+                    QuestaoId = questao.Id,
+                    TipoQuestao = questao.TipoQuestao
+                };
+                sessao.Respostas.Add(resposta);
+            }
         }
     }
 }
