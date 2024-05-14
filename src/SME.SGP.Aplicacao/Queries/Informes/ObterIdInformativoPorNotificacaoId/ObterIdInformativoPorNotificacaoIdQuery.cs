8c901b4f7af0a095c19f22038daf87c4478a4812
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-05-14 18:17:12 -0300

    120317 - Busca de notificação com anexos

---
+++ src/SME.SGP.Aplicacao/Queries/Informes/ObterIdInformativoPorNotificacaoId/ObterIdInformativoPorNotificacaoIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterIdInformativoPorNotificacaoIdQuery : IRequest<long>
+    {
+        public ObterIdInformativoPorNotificacaoIdQuery(long notificacaoId)
+        {
+            NotificacaoId = notificacaoId;
+        }
+        public long NotificacaoId { get; }
+    }
+
+    public class ObterIdInformativoPorNotificacaoIdQueryValidator : AbstractValidator<ObterIdInformativoPorNotificacaoIdQuery>
+    {
+        public ObterIdInformativoPorNotificacaoIdQueryValidator()
+        {
+            RuleFor(c => c.NotificacaoId)
+                .GreaterThan(0)
+                .WithMessage("O Id da notificação deve ser informado para a pesquisa.");
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/Informes/ObterIdInformativoPorNotificacaoId/ObterIdInformativoPorNotificacaoIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterIdInformativoPorNotificacaoIdQueryHandler : IRequestHandler<ObterIdInformativoPorNotificacaoIdQuery, long>
+    {
+        private readonly IRepositorioInformativoNotificacao repositorio;
+
+        public ObterIdInformativoPorNotificacaoIdQueryHandler(IRepositorioInformativoNotificacao repositorio)
+        {
+            this.repositorio = repositorio ?? throw new ArgumentNullException(nameof(repositorio));
+        }
+
+        public Task<long> Handle(ObterIdInformativoPorNotificacaoIdQuery request, CancellationToken cancellationToken)
+        {
+            return repositorio.ObterIdInformativoPorNotificacaoIdAsync(request.NotificacaoId);
+        }
+    }
+}
