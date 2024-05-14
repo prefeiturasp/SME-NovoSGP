76ab0dfc3b2fba388a33798fbb79e978f15d096e
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-05-14 10:09:03 -0300

    120303 - Salvar anexo informativo ao salvar informe

---
+++ src/SME.SGP.Aplicacao/Commands/Informes/SalvarInformesAnexos/SalvarInformesAnexosCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using SME.SGP.Dados.Repositorios;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class SalvarInformesAnexosCommandHandler : IRequestHandler<SalvarInformesAnexosCommand, bool>
+    {
+        private readonly IRepositorioInformativoAnexo repositorio;
+        private readonly IMediator mediator;
+
+        public SalvarInformesAnexosCommandHandler(IRepositorioInformativoAnexo repositorio, IMediator mediator)
+        {
+            this.repositorio = repositorio ?? throw new ArgumentNullException(nameof(repositorio));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<bool> Handle(SalvarInformesAnexosCommand request, CancellationToken cancellationToken)
+        {
+            var arquivos = (await mediator.Send(new ObterArquivosPorCodigosQuery(request.CodigosAnexo))).ToList();
+            foreach (var documentoArquivo in arquivos.Select(arquivo => new InformativoAnexo
+            {
+                ArquivoId = arquivo.Id,
+                InformativoId = request.InformativoId
+            }))
+            {
+                await repositorio.SalvarAsync(documentoArquivo);
+            }
+
+            return true;
+        }
+    }
+}
