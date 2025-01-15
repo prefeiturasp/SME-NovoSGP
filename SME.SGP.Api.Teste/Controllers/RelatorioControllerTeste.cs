d3845f3c0ffb641b25c6ac083b09078a6d71a458
Flavio-Spassu <flavio.souza@spassu.com.br>
2025-01-15 11:37:48 -0300

    removido comentários

--- SME.SGP.Api.Teste/Controllers/RelatorioControllerTeste.cs
+++ SME.SGP.Api.Teste/Controllers/RelatorioControllerTeste.cs
@@ -6,8 +6,6 @@ using SME.SGP.Infra;
 using System.Threading.Tasks;
 using Xunit;
 
-// Teste do comportamento do controlador
-
 namespace SME.SGP.Api.Teste.Controllers
 {
     public class RelatorioControllerTeste
@@ -15,26 +13,21 @@ namespace SME.SGP.Api.Teste.Controllers
         [Fact]
         public async Task BuscaAtiva_DeveRetornarOk()
         {
-            // Arrange: Preparar o filtro e o resultado esperado
-            var filtro = new FiltroRelatorioBuscasAtivasDto(); // Pode ser um objeto vazio para simplificar o teste.
+            var filtro = new FiltroRelatorioBuscasAtivasDto();
             var resultadoEsperado = true;
 
-            // Mock do UseCase
             var mockRelatorioUseCase = new Mock<IRelatorioBuscasAtivasUseCase>();
             mockRelatorioUseCase
                 .Setup(x => x.Executar(It.IsAny<FiltroRelatorioBuscasAtivasDto>()))
                 .ReturnsAsync(resultadoEsperado);
 
-            // Instância do Controller
             var controller = new RelatorioController();
 
-            // Act: Chamar o método a ser testado
             var result = await controller.BuscaAtiva(filtro, mockRelatorioUseCase.Object);
 
-            // Assert: Verificar o resultado
-            var okResult = Assert.IsType<OkObjectResult>(result); // Verifica se o retorno é 200 OK.
-            Assert.NotNull(okResult.Value);                      // Verifica se há um valor no retorno.
-            Assert.Equal(resultadoEsperado, okResult.Value);     // Compara o valor retornado com o esperado.
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+            Assert.Equal(resultadoEsperado, okResult.Value);
         }
     }
 }
