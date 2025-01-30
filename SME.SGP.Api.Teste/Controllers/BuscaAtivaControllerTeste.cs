e29a13f0effdd96ae98505732138e1a7e9c6b9c0
Flavio-Spassu <flavio.souza@spassu.com.br>
2025-01-30 09:20:03 -0300

    Adicionado teste do busca ativa controller

---
+++ SME.SGP.Api.Teste/Controllers/BuscaAtivaControllerTeste.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using SME.SGP.Aplicacao;
+using SME.SGP.Infra;
+using SME.SGP.Api.Controllers;
+
+namespace SME.SGP.Api.Teste.Controllers
+{
+    public class BuscaAtivaControllerTeste
+    {
+        [Fact]
+        public async Task ObterRegistrosAcao_DeveRetornarOkComResultado()
+        {
+            var filtro = new FiltroRegistrosAcaoDto();
+            var resultadoEsperado = new PaginacaoResultadoDto<RegistroAcaoBuscaAtivaListagemDto>
+            {
+            };
+
+            var useCaseMock = new Mock<IObterRegistrosAcaoUseCase>();
+            useCaseMock
+                .Setup(u => u.Executar(filtro))
+                .ReturnsAsync(resultadoEsperado);
+
+            var controller = new BuscaAtivaController();
+
+            var resultado = await controller.ObterRegistrosAcao(filtro, useCaseMock.Object);
+
+            var okResult = Assert.IsType<OkObjectResult>(resultado);
+            Assert.NotNull(okResult.Value);
+            Assert.Equal(resultadoEsperado, okResult.Value);
+        }
+    }
+}
