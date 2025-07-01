abb3558be9f364f5ef8aa6624bb293350a9f6385
Lincoln <link7-7@hotmail.com>
2025-07-01 15:17:27 -0300

    tratando turmas duplicadas no filtro principal

---
+++ src/SME.SGP.Aplicacao/Queries/EOL/Turma/ObterTurmasApiEol/ObterTurmasApiEolHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SME.SGP.Infra.Dtos;
+using SME.SGP.Infra;
+using System.Linq;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterTurmasApiEolHandler : IRequestHandler<ObterTurmasApiEolQuery, IList<TurmaApiEolDto>>
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public ObterTurmasApiEolHandler(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
+        public async Task<IList<TurmaApiEolDto>> Handle(ObterTurmasApiEolQuery request, CancellationToken cancellationToken)
+        {
+            var listaTurmas = new List<TurmaApiEolDto>();
+            var filtro = JsonConvert.SerializeObject(request.CodigosTurmas);
+
+            using (var httpClient = httpClientFactory.CreateClient(ServicosEolConstants.SERVICO))
+            {
+                var resposta = await httpClient.PostAsync("turmas/listar-turmas", new StringContent(filtro, Encoding.UTF8, "application/json-patch+json"));
+
+                if (!resposta.IsSuccessStatusCode || resposta.StatusCode == HttpStatusCode.NoContent)
+                    return listaTurmas;
+
+                var json = await resposta.Content.ReadAsStringAsync();
+
+                listaTurmas = JsonConvert.DeserializeObject<List<TurmaApiEolDto>>(json);
+
+            }
+
+            return listaTurmas?.Where(t => t.Extinta == false).ToList() ?? new List<TurmaApiEolDto>();
+        }
+    }
+}
