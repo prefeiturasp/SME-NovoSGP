2e092b8a5608e16ede6b9b04cda2467c2625e2fd
Max Fernandes <xaamax@gmail.com>
2025-08-22 10:49:20 -0300

    Estrutura inicial importacao arquivos

---
+++ src/SME.SGP.Api/Controllers/ImportarArquivoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SME.SGP.Api.Middlewares;
+using SME.SGP.Aplicacao.Interfaces.CasosDeUso.ImportarArquivo;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos.ImportarArquivo;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [Route("api/v1/importar-arquivo")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class ImportarArquivoController : ControllerBase
+    {
+        [HttpPost("ideb")]
+        [ProducesResponseType(typeof(RetornoDto), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 400)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        public async Task<IActionResult> ImportarArquivoIdeb(IFormFile arquivo, [FromServices] ICasoDeUsoImportacaoArquivoIdeb casoDeUsoImportacaoArquivo)
+        {
+            return Ok(await casoDeUsoImportacaoArquivo.Executar(arquivo));
+        }
+
+    //    [HttpPost]
+    //    [ProducesResponseType(typeof(RetornoDto), 200)]
+    //    [ProducesResponseType(typeof(RetornoBaseDto), 400)]
+    //    [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+    //    public async Task<IActionResult> ImportarArquivoIDEB(IFormFile arquivo,
+    //[FromServices] ICasoDeUsoImportacaoArquivo casoDeUsoImportacaoArquivo)
+    //    {
+    //        return Ok(await casoDeUsoImportacaoArquivo.Executar(arquivo));
+    //    }
+    }
+}
---
+++ src/SME.SGP.Infra/Dtos/ImportarArquivo/ImportacaoLogErroDto.cs
@@ -0,0 +1,12 @@
+﻿public class ImportacaoLogErroDto
+{
+    public ImportacaoLogErroDto(long importacaoLogId, long linhaArquivo, string motivoFalha)
+    {
+        ImportacaoLogId = importacaoLogId;
+        LinhaArquivo = linhaArquivo;
+        MotivoFalha = motivoFalha;
+    }
+    public long ImportacaoLogId { get; set; }
+    public long LinhaArquivo { get; set; }
+    public string MotivoFalha { get; set; }
+}
