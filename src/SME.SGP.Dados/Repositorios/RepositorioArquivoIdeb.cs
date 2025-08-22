5d98a14bdfd4289b65af7e87d6f0073df2708561
Max Fernandes <xaamax@gmail.com>
2025-08-22 10:49:20 -0300

    Estrutura inicial importacao arquivos

---
+++ src/SME.SGP.Dados/Repositorios/RepositorioArquivoIdeb.cs
@@ -0,0 +1,18 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Entidades;
+using SME.SGP.Dominio.Interfaces;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interface;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioArquivoIdeb : RepositorioBase<ArquivoIdeb>, IRepositorioArquivoIdeb
+    {
+        public RepositorioArquivoIdeb(ISgpContext database, IServicoAuditoria servicoAuditoria) : base(database, servicoAuditoria)
+        {
+        }
+    }
+}
