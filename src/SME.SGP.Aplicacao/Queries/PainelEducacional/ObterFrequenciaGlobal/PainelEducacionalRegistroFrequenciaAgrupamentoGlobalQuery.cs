e3fe5b81c5383d06a987bf877b3218d700c0fe12
cicero.santos <cicero.santos@spassu.com.br>
2025-08-25 17:31:39 -0300

    Consolidações

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterFrequenciaGlobal/PainelEducacionalRegistroFrequenciaAgrupamentoGlobalQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterFrequenciaGlobal/PainelEducacionalRegistroFrequenciaAgrupamentoGlobalQuery.cs
@@ -6,5 +6,13 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterFrequenciaGlobal
 {
     public class PainelEducacionalRegistroFrequenciaAgrupamentoGlobalQuery : IRequest<IEnumerable<PainelEducacionalRegistroFrequenciaAgrupamentoGlobalDto>>
     {
+        public PainelEducacionalRegistroFrequenciaAgrupamentoGlobalQuery(string codigoDre, string codigoUe)
+        {
+            CodigoDre = codigoDre;
+            CodigoUe = codigoUe;
+        }
+
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
     }
 }
