41e2350dbea04f13019368f4420cce3627150504
cicero.santos <cicero.santos@spassu.com.br>
2025-08-25 17:10:11 -0300

    Frequencia em Ranking

---
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterFrequenciaMensal/PainelEducacionalRegistroFrequenciaAgrupamentoMensalQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using SME.SGP.Infra.Dtos.PainelEducacional;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterFrequenciaMensal
+{
+    public class PainelEducacionalRegistroFrequenciaAgrupamentoMensalQuery : IRequest<IEnumerable<PainelEducacionalRegistroFrequenciaAgrupamentoMensalDto>>
+    {
+    }
+}
