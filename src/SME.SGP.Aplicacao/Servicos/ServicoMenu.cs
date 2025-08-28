a1e0b5daa30b5fb3a818af956202281d71af439b
cicero.santos <cicero.santos@spassu.com.br>
2025-08-28 14:51:26 -0300

    Ajuste de permissao

--- src/SME.SGP.Aplicacao/Servicos/ServicoMenu.cs
+++ src/SME.SGP.Aplicacao/Servicos/ServicoMenu.cs
@@ -23,7 +23,6 @@ namespace SME.SGP.Aplicacao
         {
             var ajudas = (await mediator.Send(ObterAjudasDoSistemaQuery.Instance)).ToList();
 
-            var teste = ajudas.Where(x => x.IdModulo == 114);
             var permissoes = servicoUsuario.ObterPermissoes();
 
             var agrupamentos = permissoes.Where(c => Enum.IsDefined(typeof(Permissao), c)).GroupBy(item => new
