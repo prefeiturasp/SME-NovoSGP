4fc3af168feed1dabcd247b47bf91bb0718fc7b6
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-09-03 17:03:07 -0300

    command e handler criado

--- src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsolidarIdepPainelEducacionalUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsolidarIdepPainelEducacionalUseCase.cs
@@ -1,35 +1,23 @@
 ﻿using MediatR;
+using SME.SGP.Aplicacao.Commands.PainelEducacional.ObterIdep;
 using SME.SGP.Aplicacao.Interfaces.CasosDeUso.PainelEducacional;
-using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao.CasosDeUso.PainelEducacional
 {
     public class ConsolidarIdepPainelEducacionalUseCase : AbstractUseCase, IConsolidarIdepPainelEducacionalUseCase
     {
-        private readonly IRepositorioIdepPainelEducacionalConsulta repositorioIdep;
 
-        public ConsolidarIdepPainelEducacionalUseCase(IMediator mediator, IRepositorioIdepPainelEducacionalConsulta repositorioIdep) : base(mediator)
+        public ConsolidarIdepPainelEducacionalUseCase(IMediator mediator) : base(mediator)
         {
-            this.repositorioIdep = repositorioIdep;
         }
 
         public async Task<bool> Executar(MensagemRabbit param)
         {
-            var registrosIdep = repositorioIdep.ObterTodosIdep();
+            var registrosIdep = await mediator.Send(new ObterIdepCommand());
 
-            //Precisa usar a função Inserir() do RepositorioIdepPainelEducacionalConsolidacao
-            //Porem precisa ver o padrao a ser seguido, se vai usar mediator ou  direto o repositorio
-
-
-            //await repositorioIdep.Inserir(registrosIdep);
-
-            throw new NotImplementedException();
+            return await mediator.Send(new InserirConsolidacaoIdepCommand(registrosIdep));
         }
     }
 }
---
+++ src/SME.SGP.Aplicacao/Commands/PainelEducacional/ObterIdep/ObterIdepCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Commands.PainelEducacional.ObterIdep;
+using SME.SGP.Dominio.Entidades;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao
+{
+    public class ObterIdepCommandHandler : IRequestHandler<ObterIdepCommand, IEnumerable<PainelEducacionalIdep>>
+    {
+        private readonly IRepositorioIdepPainelEducacionalConsulta repositorioIdepConsulta;
+
+        public ObterIdepCommandHandler(IRepositorioIdepPainelEducacionalConsulta repositorioIdepConsulta)
+        {
+            this.repositorioIdepConsulta = repositorioIdepConsulta ?? throw new ArgumentNullException(nameof(repositorioIdepConsulta));
+        }
+
+        public async Task<IEnumerable<PainelEducacionalIdep>> Handle(ObterIdepCommand request, CancellationToken cancellationToken)
+        {
+            return await repositorioIdepConsulta.ObterTodosIdep();
+        }
+    }
+}
