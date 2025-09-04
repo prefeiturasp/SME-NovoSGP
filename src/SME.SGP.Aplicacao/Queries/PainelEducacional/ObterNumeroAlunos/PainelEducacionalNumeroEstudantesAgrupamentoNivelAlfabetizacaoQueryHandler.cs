02d91036a66c72aca3e3a0b7384d2a2e2f0432f4
Diego Moreno <dierudito@gmail.com>
2025-09-04 14:24:50 -0300

    Refatora consulta de alunos por nível de alfabetização
    Substitui o repositório e entidade de agrupamento de estudantes por nível de alfabetização por uma nova implementação baseada em ConsolidacaoAlfabetizacaoNivelEscrita. Atualiza interfaces, DTOs, injeção de dependências e testes para refletir a nova estrutura, removendo as implementações antigas e adicionando testes para o novo handler.

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterNumeroAlunos/PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterNumeroAlunos/PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQueryHandler.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Entidades;
 using SME.SGP.Dominio.Interfaces.Repositorios;
+using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos.PainelEducacional;
 using System.Collections.Generic;
 using System.Threading;
@@ -10,33 +12,42 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterNumeroAlunos
 {
     public class PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQueryHandler : IRequestHandler<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQuery, IEnumerable<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto>>
     {
-        private readonly IRepositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao repositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao;
+        private readonly IRepositorioConsolidacaoAlfabetizacaoNivelEscrita repositorioConsolidacaoAlfabetizacaoNivelEscrita;
 
-        public PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQueryHandler(IRepositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao repositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao)
+        public PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQueryHandler(IRepositorioConsolidacaoAlfabetizacaoNivelEscrita repositorioConsolidacaoAlfabetizacaoNivelEscrita)
         {
-            this.repositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao = repositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao;
+            this.repositorioConsolidacaoAlfabetizacaoNivelEscrita = repositorioConsolidacaoAlfabetizacaoNivelEscrita;
         }
 
         public async Task<IEnumerable<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto>> Handle(PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoQuery request, CancellationToken cancellationToken)
         {
-            var registros = await repositorioPainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao.ObterNumeroAlunos(request.AnoLetivo, request.Periodo, request.CodigoDre, request.CodigoUe);
+            var registros = await repositorioConsolidacaoAlfabetizacaoNivelEscrita.ObterNumeroAlunos(request.AnoLetivo, request.Periodo, request.CodigoDre, request.CodigoUe);
 
             return MapearParaDto(registros);
         }
 
-        private IEnumerable<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto> MapearParaDto(IEnumerable<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacao> registros)
+        private IEnumerable<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto> MapearParaDto(IEnumerable<ConsolidacaoAlfabetizacaoNivelEscrita> registros)
         {
             var numeroAlunos = new List<PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto>();
             foreach (var item in registros)
             {
+                var nivelAlfabetizacao = item.NivelEscrita.Trim().ToUpper() switch
+                {
+                    "PS" => NivelAlfabetizacao.PreSilabico,
+                    "SSV" => NivelAlfabetizacao.SilabicoSemValor,
+                    "SCV" => NivelAlfabetizacao.SilabicoComValor,
+                    "SA" => NivelAlfabetizacao.SilabicoAlfabetico,
+                    "A" => NivelAlfabetizacao.Alfabetico
+                };
+                var nivelAlfabetizacaoDescricao = nivelAlfabetizacao.Description();
                 numeroAlunos.Add(new PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto()
                 {
-                    NivelAlfabetizacao = item.NivelAlfabetizacao,
-                    NivelAlfabetizacaoDescricao = item.NivelAlfabetizacaoDescricao,
-                    Dre = item.Dre,
-                    Ue = item.Ue,
-                    Ano = item.Ano,
-                    TotalAlunos = item.TotalAlunos,
+                    NivelAlfabetizacao = nivelAlfabetizacao,
+                    NivelAlfabetizacaoDescricao = nivelAlfabetizacaoDescricao,
+                    Dre = item.DreCodigo,
+                    Ue = item.UeCodigo,
+                    Ano = item.AnoLetivo,
+                    TotalAlunos = item.Quantidade,
                     Periodo = item.Periodo
                 });
             }
---
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioConsolidacaoAlfabetizacaoNivelEscrita.cs
@@ -0,0 +1,13 @@
+﻿using SME.SGP.Dominio.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorioConsolidacaoAlfabetizacaoNivelEscrita
+    {
+        Task ExcluirConsolidacaoNivelEscrita();
+        Task<bool> SalvarConsolidacaoNivelEscrita(ConsolidacaoAlfabetizacaoNivelEscrita entidade);
+        Task<IEnumerable<ConsolidacaoAlfabetizacaoNivelEscrita>> ObterNumeroAlunos(int anoLetivo, int periodo, string codigoDre = null, string codigoUe = null);
+    }
+}
