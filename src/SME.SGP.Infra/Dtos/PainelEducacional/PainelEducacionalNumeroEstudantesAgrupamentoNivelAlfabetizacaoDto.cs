92ff8671e954e207c36a740c0893841a41bcbb0d
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-09-05 15:06:37 -0300

    Retornar o nível de alfabetização, e o código do nível de alfabetização também / Ajustes nos testes unitários

--- src/SME.SGP.Infra/Dtos/PainelEducacional/PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto.cs
+++ src/SME.SGP.Infra/Dtos/PainelEducacional/PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto.cs
@@ -4,7 +4,8 @@ namespace SME.SGP.Infra.Dtos.PainelEducacional
 {
     public class PainelEducacionalNumeroEstudantesAgrupamentoNivelAlfabetizacaoDto
     {
-        public NivelAlfabetizacao NivelAlfabetizacao { get; set; }
+        public string NivelAlfabetizacao { get; set; }
+        public int CodigoNivelAlfabetizacao { get; set; }
         public string NivelAlfabetizacaoDescricao { get; set; }
         public string Ue { get; set; }
         public string Dre { get; set; }
