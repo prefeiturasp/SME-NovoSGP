d16561e1325cdb5f1a73413f2eecfcadf9ea16f5
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-09-08 12:02:59 -0300

    Revert "3) Na tabela de análise de frequência quando estiver selecionado todas em DRE, deverá apresentar a DRE entre parênteses após o nome da UE e a ordenação deverá ser pela DRE."
    This reverts commit 0cc2f17eed242d03102657ecc37a62c3a92bb16e.

--- src/SME.SGP.Dominio/Entidades/PainelEducacionalRegistroFrequenciaAgrupamentoEscola.cs
+++ src/SME.SGP.Dominio/Entidades/PainelEducacionalRegistroFrequenciaAgrupamentoEscola.cs
@@ -10,7 +10,6 @@
         public string CodigoDre { get; set; }
         public string CodigoUe { get; set; }
         public string UE { get; set; }
-        public string DRE { get; set; }
         public int Mes { get; set; }
     }
 }
