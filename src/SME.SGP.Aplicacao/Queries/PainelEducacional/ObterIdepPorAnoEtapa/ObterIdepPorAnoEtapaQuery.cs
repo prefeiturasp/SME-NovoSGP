196ef3d24e96091417873b85e36631284e7460e9
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-09-08 16:10:11 -0300

    Estrutura inicial

---
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterIdepPorAnoEtapa/ObterIdepPorAnoEtapaQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Entidades;
+using SME.SGP.Infra.Dtos.PainelEducacional;
+using System.Collections.Generic;
+
+namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterIdepPorAnoEtapa
+{
+    public class ObterIdepPorAnoEtapaQuery : IRequest<IEnumerable<PainelEducacionalConsolidacaoIdep>>
+    {
+        public ObterIdepPorAnoEtapaQuery(int ano, int etapa)
+        {
+            Ano = ano;
+            Etapa = etapa;
+        }
+        public int Ano { get; set; }
+        public int Etapa { get; set; }
+    }
+}
