3790c421862ce8580ba4e64ea93eaaff69cf3ea0
Diego Moreno <dierudito@gmail.com>
2025-09-09 12:07:38 -0300

    Adicionar lógica de abreviação do nome Dre e atualizar o uso
    Introduz a propriedade PrefixoDoNomeAbreviado à entidade Dre para gerar nomes abreviados. Atualiza o manipulador de comandos e os testes relacionados para usar a nova propriedade, garantindo que os nomes das escolas sejam formatados com o prefixo correto. Adiciona um teste unitário para a lógica de abreviação.

--- src/SME.SGP.Dominio/Entidades/Dre.cs
+++ src/SME.SGP.Dominio/Entidades/Dre.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace SME.SGP.Dominio
 {
-    [ExcludeFromCodeCoverage]
     public class Dre
     {
         public string Abreviacao { get; set; }
@@ -11,5 +9,17 @@ namespace SME.SGP.Dominio
         public DateTime DataAtualizacao { get; set; }
         public long Id { get; set; }
         public string Nome { get; set; }
+
+        public string PrefixoDoNomeAbreviado
+        {
+            get
+            {
+                string novaSigla = "DRE";
+                string textoParaSubstituir = "DIRETORIA REGIONAL DE EDUCACAO";
+                var nomeFormatado = Nome.ToUpper().Replace(textoParaSubstituir, novaSigla).Trim();
+                return nomeFormatado;
+            }
+            private set { }
+        }
     }
 }
