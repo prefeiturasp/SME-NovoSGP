8718ec53dd18812cdf197b64094602f3635f5c89
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-09-11 16:31:08 -0300

    Desfazendo a coluna codigo_ue da tbl de consolidação do IDEP

--- src/SME.SGP.Dados/Repositorios/RepositorioIdepPainelEducacionalConsolidacao.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioIdepPainelEducacionalConsolidacao.cs
@@ -17,18 +17,17 @@ namespace SME.SGP.Dados.Repositorios
         {
             var sql = @"
              INSERT INTO painel_educacional_consolidacao_idep
-                 (ano_letivo, etapa, faixa, quantidade, media_geral, criado_por, criado_rf, alterado_em, alterado_por, alterado_rf, codigo_dre, codigo_ue)
+                 (ano_letivo, etapa, faixa, quantidade, media_geral, criado_por, criado_rf, alterado_em, alterado_por, alterado_rf, codigo_dre)
              VALUES
-                 (@AnoLetivo, @Etapa, @Faixa, @Quantidade, @MediaGeral, @CriadoPor, @CriadoRF, @UltimaAtualizacao, @AlteradoPor, @AlteradoRF, @CodigoDre, @CodigoUe)
-             ON CONFLICT (ano_letivo, etapa, faixa, codigo_dre, codigo_ue)
+                 (@AnoLetivo, @Etapa, @Faixa, @Quantidade, @MediaGeral, @CriadoPor, @CriadoRF, @UltimaAtualizacao, @AlteradoPor, @AlteradoRF, @CodigoDre)
+             ON CONFLICT (ano_letivo, etapa, faixa)
              DO UPDATE SET
                  quantidade   = EXCLUDED.quantidade,
                  media_geral  = EXCLUDED.media_geral,
                  alterado_em  = EXCLUDED.alterado_em,
                  alterado_por = EXCLUDED.alterado_por,
                  alterado_rf  = EXCLUDED.alterado_rf,
-                 codigo_dre   = EXCLUDED.codigo_dre,
-                 codigo_ue    = EXCLUDED.codigo_ue
+                 codigo_dre   = EXCLUDED.codigo_dre
              WHERE painel_educacional_consolidacao_idep.alterado_em < EXCLUDED.alterado_em;";
 
             using var conn = database.Conexao;
