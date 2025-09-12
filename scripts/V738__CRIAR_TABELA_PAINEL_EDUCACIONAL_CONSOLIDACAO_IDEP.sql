1bd6eec9c9ba5fdd48b8406da081ccf1fa3dd3af
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-09-12 16:35:52 -0300

    Script da tabela de consolidação do idep corrigida

--- scripts/V738__CRIAR_TABELA_PAINEL_EDUCACIONAL_CONSOLIDACAO_IDEP.sql
+++ scripts/V738__CRIAR_TABELA_PAINEL_EDUCACIONAL_CONSOLIDACAO_IDEP.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS painel_educacional_consolidacao_idep (
+CREATE TABLE painel_educacional_consolidacao_idep (
   id              SERIAL PRIMARY KEY,  
   ano_letivo      INT          NOT NULL,
   etapa           VARCHAR(20)  NOT NULL,
@@ -13,32 +13,12 @@ CREATE TABLE IF NOT EXISTS painel_educacional_consolidacao_idep (
   criado_rf       VARCHAR(200) NULL,
   alterado_em     TIMESTAMP    NOT NULL DEFAULT NOW(),
   alterado_por    VARCHAR(200) NULL,
-  alterado_rf     VARCHAR(200) NULL
+  alterado_rf     VARCHAR(200) NULL,
+  
+  CONSTRAINT uq_painel_ano_etapa_faixa UNIQUE (ano_letivo, etapa, faixa)
 );
 
-DO $$ 
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint 
-        WHERE conname = 'uq_painel_ano_etapa_faixa' 
-        AND conrelid = 'painel_educacional_consolidacao_idep'::regclass
-    ) THEN
-        ALTER TABLE painel_educacional_consolidacao_idep
-        ADD CONSTRAINT uq_painel_ano_etapa_faixa
-        UNIQUE (ano_letivo, etapa, faixa);
-    END IF;
-END $$;
-
-DO $$ 
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_indexes 
-        WHERE indexname = 'ix_painel_ano_etapa' 
-        AND tablename = 'painel_educacional_consolidacao_idep'
-    ) THEN
-        CREATE INDEX ix_painel_ano_etapa 
-        ON painel_educacional_consolidacao_idep (ano_letivo, etapa);
-    END IF;
-END $$;
+CREATE INDEX ix_painel_ano_etapa 
+ON painel_educacional_consolidacao_idep (ano_letivo, etapa);
 
 COMMIT;
