252a1a24a7dc36d446f8965c00483d283ef7f902
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-09-16 10:34:32 -0300

    Consolidação do ideb para o visão geral do painel educacional

--- src/SME.SGP.Infra.Mensageria/Rotas/RotasRabbitSgpPainelEducacional.cs
+++ src/SME.SGP.Infra.Mensageria/Rotas/RotasRabbitSgpPainelEducacional.cs
@@ -7,6 +7,7 @@
         public const string ConsolidarNivelEscritaAlfabetizacaoCritico = "sgp.consolidar.nivel.escrita.alfabetizacao.critico";
         public const string ConsolidarInformacoesPapPainelEducacional = "sgp.consolidar.informacoes.pap.painel.educacional";
         public const string ConsolidarVisaoGeralPainelEducacional = "sgp.consolidacao.visao.geral.painel.educacional";
+        public const string ConsolidarIdebPainelEducacional = "sgp.consolidacao.ideb.painel.educacional";
     }
 }
 
--- src/SME.SGP.PainelEducacional.Worker/WorkerRabbitPainelEducacional.cs
+++ src/SME.SGP.PainelEducacional.Worker/WorkerRabbitPainelEducacional.cs
@@ -33,7 +33,7 @@ namespace SME.SGP.PainelEducacional.Worker
             Comandos.Add(RotasRabbitSgpPainelEducacional.ConsolidarNivelEscritaAlfabetizacaoCritico, new ComandoRabbit("Sincronização e Consolidação dos Dados da Sondagem da Alfabetização Crítica Escrita", typeof(IConsolidarInformacoesAlfabetizacaoCriticaEscritaPainelEducacionalUseCase), false));
             Comandos.Add(RotasRabbitSgpPainelEducacional.ConsolidarInformacoesPapPainelEducacional, new ComandoRabbit("Sincronização e Consolidação dos Dados de PAP", typeof(IConsolidarInformacoesPapPainelEducacionalUseCase)));
             Comandos.Add(RotasRabbitSgpPainelEducacional.ConsolidarVisaoGeralPainelEducacional, new ComandoRabbit("Consolidar Visão Geral para o Painel Educacional", typeof(IConsolidarVisaoGeralPainelEducacionalUseCase)));
-            Comandos.Add(RotasRabbitSgpPainelEducacional.ConsolidarIdepPainelEducacional, new ComandoRabbit("Consolidar idep para painel educacional", typeof(IConsolidarIdepPainelEducacionalUseCase)));
+            Comandos.Add(RotasRabbitSgpPainelEducacional.ConsolidarIdebPainelEducacional, new ComandoRabbit("Consolidar Ideb para o Painel Educacional", typeof(IConsolidarIdebPainelEducacionalUseCase)));
         }
     }
 }
