228f8750d97eb4e9cd99368e8d0ac4be5384af4e
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-09-19 11:00:20 -0300

    Alteração do nome do arquivo de script pra manter sequencia em release / Correções que o Diego havia sugerido ontem

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterIndicadoresPap/ObterIndicadoresPapQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterIndicadoresPap/ObterIndicadoresPapQuery.cs
@@ -1,10 +1,11 @@
 using MediatR;
 using SME.SGP.Dominio;
+using SME.SGP.Dominio.Entidades;
 using System.Collections.Generic;
 
 namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterIndicadoresPap
 {
-    public class ObterIndicadoresPapQuery : IRequest<IEnumerable<ConsolidacaoInformacoesPap>>
+    public class ObterIndicadoresPapQuery : IRequest<IEnumerable<PainelEducacionalInformacoesPapDto>>
     {
         public ObterIndicadoresPapQuery(string codigoDre = null, string codigoUe = null)
         {
