b89cf0dff850ddf78df9bc043c6cca2cf36b8b80
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-09-22 16:47:44 -0300

    Remover método não utilizado

--- src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioIdebPainelEducacionalConsulta.cs
+++ src/SME.SGP.Dominio.Interfaces/Repositorios/IRepositorioIdebPainelEducacionalConsulta.cs
@@ -7,7 +7,6 @@ namespace SME.SGP.Dominio.Interfaces.Repositorios
 {
     public interface IRepositorioIdebPainelEducacionalConsulta : IRepositorioBase<PainelEducacionalIdeb>
     {
-        Task<IEnumerable<PainelEducacionalConsolidacaoIdeb>> ObterTodosIdeb();
         Task<IEnumerable<PainelEducacionalIdebDto>> ObterIdebPorAnoSerie(int anoLetivo, string serie, string codigoDre, string codigoUe);
         Task<int?> ObterAnoMaisRecenteIdeb(string serie, string codigoDre, string codigoUe);
     }
