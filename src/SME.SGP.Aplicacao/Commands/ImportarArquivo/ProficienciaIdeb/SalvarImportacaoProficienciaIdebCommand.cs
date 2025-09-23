55278f1e35968cbb5b800b8e49a95c93ee3e80ff
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-09-23 15:12:55 -0300

    SalvarImportacaoProficienciaIdebCommand

---
+++ src/SME.SGP.Aplicacao/Commands/ImportarArquivo/ProficienciaIdeb/SalvarImportacaoProficienciaIdebCommand.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Infra.Dtos.ImportarArquivo;
+
+namespace SME.SGP.Aplicacao.Commands.ImportarArquivo.ProficienciaIdeb
+{
+    public class SalvarImportacaoProficienciaIdebCommand : IRequest<bool>
+    {
+        public SalvarImportacaoProficienciaIdebCommand(ProficienciaIdebDto proficienciaIdeb)
+        {
+            ProficienciaIdeb = proficienciaIdeb;
+        }
+
+        public ProficienciaIdebDto ProficienciaIdeb { get; }
+
+        public class SalvarImportacaoProficienciaIdebCommandValidator : AbstractValidator<SalvarImportacaoProficienciaIdebCommand>
+        {
+            public SalvarImportacaoProficienciaIdebCommandValidator()
+            {
+                RuleFor(x => x.ProficienciaIdeb.AnoLetivo)
+                    .GreaterThan(0).WithMessage("Ano Letivo inválido");
+
+                RuleFor(x => x.ProficienciaIdeb.CodigoEOLEscola)
+                    .NotEmpty().WithMessage("Código EOL da UE inválido")
+                    .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("Código EOL da UE inválido");
+            }
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Commands/ImportarArquivo/ProficienciaIdeb/SalvarImportacaoProficienciaIdebCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Commands.ImportarArquivo.ProficienciaIdeb
+{
+    public class SalvarImportacaoProficienciaIdebCommandHandler : IRequestHandler<SalvarImportacaoProficienciaIdebCommand, bool>
+    {
+        private readonly IRepositorioProficienciaIdeb _repositorioProficienciaIdeb;
+        public async Task<bool> Handle(SalvarImportacaoProficienciaIdebCommand request, CancellationToken cancellationToken)
+        {
+            var proficienciaIdeb = MapearParaEntidade(request);
+
+            await _repositorioProficienciaIdeb.SalvarAsync(proficienciaIdeb);
+
+            return proficienciaIdeb.Id > 0;
+        }
+
+        private Dominio.Entidades.ProficienciaIdeb MapearParaEntidade(SalvarImportacaoProficienciaIdebCommand request)
+           => new Dominio.Entidades.ProficienciaIdeb()
+           {
+               AnoLetivo = request.ProficienciaIdeb.AnoLetivo,
+               CodigoEOLEscola = request.ProficienciaIdeb.CodigoEOLEscola,
+           };
+    }
+}
