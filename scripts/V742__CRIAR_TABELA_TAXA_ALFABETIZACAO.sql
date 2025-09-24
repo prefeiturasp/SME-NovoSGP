6191044531824da82328b8672da776f0a85f93ca
Flavio-Spassu <flavio.souza@spassu.com.br>
2025-09-24 09:12:35 -0300

    atualizado o script para manter o padrão das outras tabelas

--- scripts/V742__CRIAR_TABELA_TAXA_ALFABETIZACAO.sql
+++ scripts/V742__CRIAR_TABELA_TAXA_ALFABETIZACAO.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS public."taxa-alfabetizacao" (
-    "codigo-escola"        VARCHAR(15)   NOT NULL,
-    "taxa-alfabetizacao"   NUMERIC(11,2) NOT NULL,
-    "ano-letivo"           BIGINT        NOT NULL,
+CREATE TABLE IF NOT EXISTS public.taxa_alfabetizacao (
+    "codigo_eol_escola"        VARCHAR(15)   NOT NULL,
+    "taxa_alfabetizacao"   NUMERIC(11,2) NOT NULL,
+    "ano_letivo"           BIGINT        NOT NULL,
     "criado_em"            TIMESTAMP     NOT NULL,
     "criado_por"           VARCHAR(200)  NOT NULL,
     "alterado_em"          TIMESTAMP     NOT NULL,
