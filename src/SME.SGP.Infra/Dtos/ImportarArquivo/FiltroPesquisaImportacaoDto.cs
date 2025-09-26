df2aabbf996a183f355fe9e4f626302dfd56a2a2
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-09-26 15:22:42 -0300

    Ajuste para paginação ser feita corretamente / Caso não sejam enviados valores para paginação e número de registros por página, assumir valores default

--- src/SME.SGP.Infra/Dtos/ImportarArquivo/FiltroPesquisaImportacaoDto.cs
+++ src/SME.SGP.Infra/Dtos/ImportarArquivo/FiltroPesquisaImportacaoDto.cs
@@ -3,5 +3,7 @@
     public class FiltroPesquisaImportacaoDto
     {
         public long? ImportacaoLogId { get; set; }
+        public int NumeroPagina { get; set; } = 1;
+        public int NumeroRegistros { get; set; } = 10;
     }
 }
