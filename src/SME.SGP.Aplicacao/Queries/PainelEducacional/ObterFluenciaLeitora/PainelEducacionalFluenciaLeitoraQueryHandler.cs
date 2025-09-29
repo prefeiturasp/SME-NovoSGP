2f7049ed210e3436a3d1bd87c27bac137c3870ab
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-09-29 09:13:02 -0300

    Ajustes em agrupamentos + teste unitário para o endpoint

---
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterFluenciaLeitora/PainelEducacionalFluenciaLeitoraQueryHandler.cs
@@ -0,0 +1,72 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Entidades;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterFluenciaLeitora
+{
+    public class PainelEducacionalFluenciaLeitoraQueryHandler : IRequestHandler<PainelEducacionalFluenciaLeitoraQuery, IEnumerable<PainelEducacionalFluenciaLeitoraDto>>
+    {
+        private readonly IRepositorioPainelEducacionalFluenciaLeitoraConsulta repositorioPainelEducacionalFluenciaLeitoraConsulta;
+        
+        public PainelEducacionalFluenciaLeitoraQueryHandler(IRepositorioPainelEducacionalFluenciaLeitoraConsulta repositorioPainelEducacionalFluenciaLeitoraConsulta)
+        {
+            this.repositorioPainelEducacionalFluenciaLeitoraConsulta = repositorioPainelEducacionalFluenciaLeitoraConsulta;
+        }
+
+        public async Task<IEnumerable<PainelEducacionalFluenciaLeitoraDto>> Handle(PainelEducacionalFluenciaLeitoraQuery request, CancellationToken cancellationToken)
+        {
+            var registros = await repositorioPainelEducacionalFluenciaLeitoraConsulta.ObterFluenciaLeitora(request.Periodo, request.AnoLetivo, request.CodigoDre);
+
+            var ehConsolidacaoSme = string.IsNullOrWhiteSpace(request.CodigoDre) || request.CodigoDre == "0";
+            
+            if (ehConsolidacaoSme)
+            {
+                return registros
+                    .GroupBy(r => new { r.NomeFluencia })
+                    .Select(g => new PainelEducacionalFluenciaLeitoraDto
+                    {
+                        NomeFluencia = g.Key.NomeFluencia,
+                        DescricaoFluencia = g.First().DescricaoFluencia,
+                        DreCodigo = g.First().DreCodigo ?? "",
+                        Percentual = CalcularPercentual(g),
+                        QuantidadeAlunos = g.Sum(x => x.QuantidadeAlunos),
+                        Ano = g.First().Ano,
+                        Periodo = g.First().Periodo
+                    })
+                    .OrderBy(r => r.NomeFluencia)
+                    .ThenBy(r => r.DreCodigo);
+            }
+            else
+            {
+                return registros
+                    .GroupBy(r => new { r.NomeFluencia, r.DreCodigo })
+                    .Select(g => new PainelEducacionalFluenciaLeitoraDto
+                    {
+                        NomeFluencia = g.Key.NomeFluencia,
+                        DescricaoFluencia = g.First().DescricaoFluencia,
+                        DreCodigo = g.Key.DreCodigo ?? "",
+                        Percentual = CalcularPercentual(g),
+                        QuantidadeAlunos = g.Sum(x => x.QuantidadeAlunos),
+                        Ano = g.First().Ano,
+                        Periodo = g.First().Periodo
+                    })
+                    .OrderBy(r => r.NomeFluencia)
+                    .ThenBy(r => r.DreCodigo);
+            }
+        }
+
+        private static decimal CalcularPercentual(IEnumerable<PainelEducacionalFluenciaLeitoraDto> grupo)
+        {
+            var totalAlunos = grupo.Sum(x => x.QuantidadeAlunos);
+            if (totalAlunos == 0) return 0;
+            
+            var somaPonderada = grupo.Sum(x => x.Percentual * x.QuantidadeAlunos);
+            return Math.Round(somaPonderada / totalAlunos, 2);
+        }
+    }
+}
---
+++ src/SME.SGP.Dados/Repositorios/RepositorioPainelEducacionalFluenciaLeitoraConsulta.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using SME.SGP.Dominio.Entidades;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using SME.SGP.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioPainelEducacionalFluenciaLeitoraConsulta : IRepositorioPainelEducacionalFluenciaLeitoraConsulta
+    {
+        private readonly ISgpContextConsultas database;
+
+        public RepositorioPainelEducacionalFluenciaLeitoraConsulta(ISgpContextConsultas database)
+        {
+            this.database = database;
+        }
+
+        public async Task<IEnumerable<PainelEducacionalFluenciaLeitoraDto>> ObterFluenciaLeitora(int periodo, int anoLetivo, string codigoDre)
+        {
+            var query = new StringBuilder(@"
+                                           SELECT fluencia as NomeFluencia,
+                                           descricao_fluencia as DescricaoFluencia,
+                                           dre_codigo as DreCodigo,
+                                           percentual, 
+                                           quantidade_alunos as QuantidadeAlunos,
+                                           ano,
+                                           periodo
+                                           FROM consolidacao_painel_educacional_fluencia_leitora");
+
+            var condicoes = new List<string>();
+            var parametros = new DynamicParameters();
+
+            if (periodo != 0)
+            {
+                condicoes.Add("periodo = @periodo");
+                parametros.Add("periodo", periodo);
+            }
+
+            if (anoLetivo != 0 && anoLetivo != 0)
+            {
+                condicoes.Add("ano = @anoLetivo");
+                parametros.Add("anoLetivo", anoLetivo);
+            }
+
+            if (!string.IsNullOrWhiteSpace(codigoDre) && codigoDre != "0")
+            {
+                condicoes.Add("dre_codigo = @codigoDre");
+                parametros.Add("codigoDre", codigoDre);
+            }
+
+            if (condicoes.Any())
+            {
+                query.Append(" WHERE ");
+                query.Append(string.Join(" AND ", condicoes));
+            }
+
+            return await database.QueryAsync<PainelEducacionalFluenciaLeitoraDto>(
+                query.ToString(),
+                parametros
+            );
+        }
+    }
+}
