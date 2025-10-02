a934dd0b4fe3b0efc50f38b0f9d83fac28134f5a
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-02 11:18:41 -0300

    Criar endpoint para retornar informações da UE e Diretor

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsultasProficienciaEscolaDadosUseCase.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces.CasosDeUso.PainelEducacional;
+using SME.SGP.Aplicacao.Queries.PainelEducacional.ObterProficienciaEscolaDados;
+using SME.SGP.Dominio;
+using SME.SGP.Infra.Dtos.PainelEducacional;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso.PainelEducacional
+{
+    public class ConsultasProficienciaEscolaDadosUseCase : IConsultasProficienciaEscolaDadosUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ConsultasProficienciaEscolaDadosUseCase(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<PainelEducacionalProficienciaEscolaDadosDto> ObterProficienciaEscolaDados(string codigoUe)
+        {
+            if (string.IsNullOrWhiteSpace(codigoUe))
+                throw new NegocioException("Informe a unidade escolar");
+
+            var proficienciaEscolaDados = await mediator.Send(new ObterProficienciaEscolaDadosQuery(codigoUe));
+
+            return proficienciaEscolaDados;
+        }
+    }
+}
