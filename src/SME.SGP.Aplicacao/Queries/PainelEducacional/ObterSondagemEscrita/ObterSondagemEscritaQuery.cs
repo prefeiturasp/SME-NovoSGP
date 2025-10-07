d358a70682c9eaf6da153df1edd83104227a8072
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-07 11:56:14 -0300

    Ajustes no PR: Adicionar validação para parâmetros obrigatórios, alterar o nome do parâmetro de período para bimestre, e corrigir teste unitário

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterSondagemEscrita/ObterSondagemEscritaQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterSondagemEscrita/ObterSondagemEscritaQuery.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using SME.SGP.Infra.Dtos.PainelEducacional.SondagemEscrita;
 using System.Collections.Generic;
 
@@ -19,5 +20,27 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterSondagemEscrita
         public int AnoLetivo { get; set; }
         public int Bimestre { get; set; }
         public int SerieAno { get; set; }
+    }
+
+    public class ObterSondagemEscritaQueryValidator : AbstractValidator<ObterSondagemEscritaQuery>
+    {
+        public ObterSondagemEscritaQueryValidator()
+        {
+            RuleFor(c => c.CodigoDre)
+            .NotEmpty()
+            .WithMessage("O código da DRE deve ser informado.");
+
+            RuleFor(c => c.CodigoUe)
+            .NotEmpty()
+            .WithMessage("O código da UE deve ser informado.");
+
+            RuleFor(c => c.AnoLetivo)
+           .NotEmpty()
+           .WithMessage("O ano letivo deve ser informado.");
+
+            RuleFor(c => c.Bimestre)
+           .NotEmpty()
+           .WithMessage("O bimestre deve ser informado.");
+        }
     }
 }
