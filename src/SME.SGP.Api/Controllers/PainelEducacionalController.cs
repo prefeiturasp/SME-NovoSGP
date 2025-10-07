ccf1d0ee6295325b4348df6f79ac36ce75b976d4
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-07 11:56:14 -0300

    Ajustes no PR: Adicionar validação para parâmetros obrigatórios, alterar o nome do parâmetro de período para bimestre, e corrigir teste unitário

--- src/SME.SGP.Api/Controllers/PainelEducacionalController.cs
+++ src/SME.SGP.Api/Controllers/PainelEducacionalController.cs
@@ -127,7 +127,7 @@ namespace SME.SGP.Api.Controllers
         [ProducesResponseType(typeof(RetornoBaseDto), 500)]
         [ProducesResponseType(typeof(RetornoBaseDto), 601)]
         [Permissao(Permissao.FB_C, Policy = "Bearer")]
-        public async Task<IActionResult> ObterSondagemEscrita([FromQuery] FiltroPainelEducacionalAnoLetivoPeriodo filtro, [FromServices] IConsultasSondagemEscritaUseCase consultasSondagemEscritaUseCase)
-    => Ok(await consultasSondagemEscritaUseCase.ObterSondagemEscrita(filtro.CodigoDre, filtro.CodigoUe, filtro.AnoLetivo, filtro.Periodo, filtro.SerieAno));
+        public async Task<IActionResult> ObterSondagemEscrita([FromQuery] FiltroPainelEducacionalAnoLetivoBimestre filtro, [FromServices] IConsultasSondagemEscritaUseCase consultasSondagemEscritaUseCase)
+    => Ok(await consultasSondagemEscritaUseCase.ObterSondagemEscrita(filtro.CodigoDre, filtro.CodigoUe, filtro.AnoLetivo, filtro.Bimestre, filtro.SerieAno));
     }
 }
