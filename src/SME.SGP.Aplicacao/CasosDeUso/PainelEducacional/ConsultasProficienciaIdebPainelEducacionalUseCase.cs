2e6ea28766cf8f228298cf77cdd07a5324d986e2
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-07 16:46:52 -0300

    Ajustes no endpoint de proficiência IDEP: Se não passar o ano letivo, deve retornar apenas 5 anos para trás, ordenando em ordem decrescente (Critério de aceite)

--- src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsultasProficienciaIdebPainelEducacionalUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsultasProficienciaIdebPainelEducacionalUseCase.cs
@@ -6,6 +6,7 @@ using SME.SGP.Infra.Dtos.PainelEducacional;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SME.SGP.Aplicacao.CasosDeUso.PainelEducacional
 {
@@ -24,6 +25,14 @@ namespace SME.SGP.Aplicacao.CasosDeUso.PainelEducacional
                 throw new NegocioException("Informe a unidade escolar");
 
             var proficienciaIdeb = await mediator.Send(new ObterProficienciaIdepQuery(anoLetivo, codigoUe));
+
+            if (anoLetivo <= 0)
+            {
+                proficienciaIdeb = proficienciaIdeb
+                    .OrderByDescending(p => p.AnoLetivo) 
+                    .Take(5)
+                    .ToList();
+            }
 
             return proficienciaIdeb;
         }
