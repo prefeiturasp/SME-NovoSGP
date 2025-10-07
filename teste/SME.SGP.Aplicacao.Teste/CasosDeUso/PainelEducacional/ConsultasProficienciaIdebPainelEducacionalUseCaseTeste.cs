81f6d3353a35869e56940c4b6abf8098071392ee
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-07 16:46:52 -0300

    Ajustes no endpoint de proficiência IDEP: Se não passar o ano letivo, deve retornar apenas 5 anos para trás, ordenando em ordem decrescente (Critério de aceite)

--- teste/SME.SGP.Aplicacao.Teste/CasosDeUso/PainelEducacional/ConsultasProficienciaIdebPainelEducacionalUseCaseTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/CasosDeUso/PainelEducacional/ConsultasProficienciaIdebPainelEducacionalUseCaseTeste.cs
@@ -5,6 +5,7 @@ using SME.SGP.Aplicacao.Queries.PainelEducacional.ObterProficienciaIdep;
 using SME.SGP.Dominio;
 using SME.SGP.Infra.Dtos.PainelEducacional;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -53,6 +54,40 @@ namespace SME.SGP.Aplicacao.Teste.CasosDeUso.PainelEducacional
 
             Assert.NotNull(resultado);
             Assert.Equal(mockRetorno, resultado);
+            mediatorMock.Verify(m => m.Send(It.Is<ObterProficienciaIdepQuery>(q => q.AnoLetivo == anoLetivo && q.CodigoUe == codigoUe),
+                                            It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Executar_Quando_Ano_Nao_Informado_Deve_Retornar_Ultimos_5_Anos()
+        {
+            var anoLetivo = 0; 
+            var codigoUe = "123456";
+            var mockRetorno = new List<PainelEducacionalProficienciaIdepDto>
+            {
+                new PainelEducacionalProficienciaIdepDto { AnoLetivo = 2025 },
+                new PainelEducacionalProficienciaIdepDto { AnoLetivo = 2024 },
+                new PainelEducacionalProficienciaIdepDto { AnoLetivo = 2023 },
+                new PainelEducacionalProficienciaIdepDto { AnoLetivo = 2022 },
+                new PainelEducacionalProficienciaIdepDto { AnoLetivo = 2021 },
+                new PainelEducacionalProficienciaIdepDto { AnoLetivo = 2020 } 
+            };
+
+            mediatorMock.Setup(m => m.Send(It.Is<ObterProficienciaIdepQuery>(q => q.AnoLetivo == anoLetivo && q.CodigoUe == codigoUe),
+                                           It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(mockRetorno);
+
+            var resultado = await useCase.ObterProficienciaIdep(anoLetivo, codigoUe);
+
+            Assert.NotNull(resultado);
+            Assert.Equal(5, resultado.Count()); 
+            Assert.Collection(resultado,
+                item => Assert.Equal(2025, item.AnoLetivo),
+                item => Assert.Equal(2024, item.AnoLetivo),
+                item => Assert.Equal(2023, item.AnoLetivo),
+                item => Assert.Equal(2022, item.AnoLetivo),
+                item => Assert.Equal(2021, item.AnoLetivo)); 
+
             mediatorMock.Verify(m => m.Send(It.Is<ObterProficienciaIdepQuery>(q => q.AnoLetivo == anoLetivo && q.CodigoUe == codigoUe),
                                             It.IsAny<CancellationToken>()), Times.Once);
         }
