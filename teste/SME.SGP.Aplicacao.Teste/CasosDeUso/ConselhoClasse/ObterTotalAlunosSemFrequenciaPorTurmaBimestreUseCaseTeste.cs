a72c72ff4999c6c60cdaebe018d5399186f1b9c2
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-08 14:52:49 -0300

    Testes - ainda não finalizado (contém erros no build!!!!!!!)

---
+++ teste/SME.SGP.Aplicacao.Teste/CasosDeUso/ConselhoClasse/ObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCaseTeste.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.Aplicacao.Teste.CasosDeUso.ConselhoClasse
+{
+    public class ObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCaseTeste
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCase _useCase;
+
+        public ObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCaseTeste()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _useCase = new ObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCase(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public void Construtor_Quando_Mediator_Nulo_Deve_Lancar_ArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ObterTotalAlunosSemFrequenciaPorTurmaBimestreUseCase(null));
+        }
+
+        [Fact]
+        public async Task Executar_Quando_Chamado_Deve_Enviar_Query_E_Retornar_Resultado()
+        {
+            var disciplinaId = "COMP-123";
+            var codigoTurma = "TURMA-456";
+            var bimestre = 2;
+            var dataMatricula = new DateTime(2025, 10, 7);
+            var retornoEsperado = new List<int> { 10, 20 };
+
+            _mediatorMock.Setup(m => m.Send(
+                It.Is<ObterTotalAlunosSemFrequenciaPorTurmaBimestreQuery>(q =>
+                    q.DisciplinaId == disciplinaId &&
+                    q.CodigoTurma == codigoTurma &&
+                    q.Bimestre == bimestre &&
+                    q.DataMatricula == dataMatricula),
+                It.IsAny<CancellationToken>()))
+                .ReturnsAsync(retornoEsperado);
+
+            var resultado = await _useCase.Executar(disciplinaId, codigoTurma, bimestre, dataMatricula);
+
+            Assert.NotNull(resultado);
+            Assert.Equal(retornoEsperado, resultado);
+
+            _mediatorMock.Verify(m => m.Send(
+                It.Is<ObterTotalAlunosSemFrequenciaPorTurmaBimestreQuery>(q =>
+                    q.DisciplinaId == disciplinaId &&
+                    q.CodigoTurma == codigoTurma &&
+                    q.Bimestre == bimestre &&
+                    q.DataMatricula == dataMatricula),
+                It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
---
+++ teste/SME.SGP.Aplicacao.Teste/CasosDeUso/ConselhoClasse/ObterTotalAulasNaoLancamNotaUseCaseTeste.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Moq;
+using SME.SGP.Infra.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SME.SGP.Aplicacao.Teste.CasosDeUso.ConselhoClasse
+{
+    public class ObterTotalAulasNaoLancamNotaUseCaseTeste
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ObterTotalAulasNaoLancamNotaUseCase _useCase;
+
+        public ObterTotalAulasNaoLancamNotaUseCaseTeste()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _useCase = new ObterTotalAulasNaoLancamNotaUseCase(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public void Construtor_Quando_Mediator_Nulo_Deve_Lancar_ArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ObterTotalAulasNaoLancamNotaUseCase(null));
+        }
+
+        [Fact]
+        public async Task Executar_Quando_Chamado_Deve_Enviar_Query_E_Retornar_Resultado()
+        {
+            var codigoTurma = "123456";
+            var bimestre = 2;
+            var codigoAluno = "123999";
+            var retornoEsperado = new List<TotalAulasNaoLancamNotaDto>
+            {
+                new TotalAulasNaoLancamNotaDto { DisciplinaId = 1, TotalAulas = "10" }
+            };
+
+            _mediatorMock.Setup(m => m.Send(
+                It.Is<ObterTotalAulasNaoLancamNotaQuery>(q =>
+                    q.CodigoTurma == codigoTurma &&
+                    q.Bimestre == bimestre &&
+                    q.CodigoAluno == codigoAluno),
+                It.IsAny<CancellationToken>()))
+                .ReturnsAsync(retornoEsperado);
+
+            var resultado = await _useCase.Executar(codigoTurma, bimestre, codigoAluno);
+
+            Assert.NotNull(resultado);
+            Assert.Equal(retornoEsperado, resultado);
+
+            _mediatorMock.Verify(m => m.Send(
+                It.Is<ObterTotalAulasNaoLancamNotaQuery>(q =>
+                    q.CodigoTurma == codigoTurma &&
+                    q.Bimestre == bimestre &&
+                    q.CodigoAluno == codigoAluno),
+                It.IsAny<CancellationToken>()), Moq.Times.Once);
+        }
+    }
+}
