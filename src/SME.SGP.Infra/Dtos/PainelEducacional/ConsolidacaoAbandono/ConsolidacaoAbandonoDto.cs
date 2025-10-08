fb0b410f6e4ef78efd3ea49af3da6db87f98f646
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-10-08 16:52:21 -0300

    Registros consolidados

---
+++ src/SME.SGP.Infra/Dtos/PainelEducacional/ConsolidacaoAbandono/ConsolidacaoAbandonoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SME.SGP.Infra.Dtos.PainelEducacional.ConsolidacaoAbandono
+{
+    public class ConsolidacaoAbandonoDto
+    {
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public string Ano { get; set; }
+        public string Modalidade { get; set; }
+        public int QuantidadeDesistencias { get; set; }
+        public int AnoLetivo { get; set; }
+    }
+}
