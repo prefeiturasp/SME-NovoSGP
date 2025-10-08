d68ee0ab1066b84883d8ff137ccf30a2306d3b8a
Diego Moreno <dierudito@gmail.com>
2025-10-08 15:54:15 -0300

    Adiciona testes e refatora queries de indicadores PAP
    Foram adicionados diversos testes unitários para casos de uso, comandos e queries relacionados ao Painel Educacional e indicadores PAP. As classes de handler e validator da query ObterDadosBrutosParaConsolidacaoIndicadoresPap foram extraídas para arquivos separados, melhorando a organização do código.

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterDadosBrutosParaConsolidacaoIndicadoresPap/ObterDadosBrutosParaConsolidacaoIndicadoresPapQuery.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterDadosBrutosParaConsolidacaoIndicadoresPap/ObterDadosBrutosParaConsolidacaoIndicadoresPapQuery.cs
@@ -1,17 +1,8 @@
-using FluentValidation;
 using MediatR;
-using SME.SGP.Aplicacao.Queries.Aluno.ObterAlunosTurmaPap;
-using SME.SGP.Aplicacao.Queries.ConsolidacaoFrequenciaTurma.ObterFrequenciaPorLimitePercentual;
-using SME.SGP.Aplicacao.Queries.PainelEducacional.ObterDificuldadesIndicadoresPap;
-using SME.SGP.Dominio.Entidades.MapeamentoPap;
-using SME.SGP.Infra.Consts;
 using SME.SGP.Infra.Dtos.Aluno;
 using SME.SGP.Infra.Dtos.ConsolidacaoFrequenciaTurma;
 using SME.SGP.Infra.Dtos.PainelEducacional.IndicadoresPap;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterDadosBrutosParaConsolidacaoIndicadoresPap
 {
@@ -24,46 +15,6 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterDadosBrutosParaConsol
         public ObterDadosBrutosParaConsolidacaoIndicadoresPapQuery(int anoLetivo)
         {
             this.AnoLetivo = anoLetivo;
-        }
-    }
-    public class ObterDadosBrutosParaConsolidacaoIndicadoresPapQueryHandler : IRequestHandler<ObterDadosBrutosParaConsolidacaoIndicadoresPapQuery, (IEnumerable<DadosMatriculaAlunoTipoPapDto> dadosAlunoTurma, IEnumerable<ContagemDificuldadeIndicadoresPapPorTipoDto> dificuldadesPap, IEnumerable<QuantitativoAlunosFrequenciaBaixaPorTurmaDto> frequenciaBaixa)>
-    {
-        private readonly IMediator mediator;
-        public ObterDadosBrutosParaConsolidacaoIndicadoresPapQueryHandler(IMediator mediator)
-        {
-            this.mediator = mediator;
-        }
-        public async Task<(
-            IEnumerable<DadosMatriculaAlunoTipoPapDto> dadosAlunoTurma,
-            IEnumerable<ContagemDificuldadeIndicadoresPapPorTipoDto> dificuldadesPap,
-            IEnumerable<QuantitativoAlunosFrequenciaBaixaPorTurmaDto> frequenciaBaixa)> Handle(ObterDadosBrutosParaConsolidacaoIndicadoresPapQuery request, CancellationToken cancellationToken)
-        {
-            var dadosAlunosTurmasPap = await mediator.Send(new ObterAlunosTurmaPapQuery(request.AnoLetivo), cancellationToken);
-            if (dadosAlunosTurmasPap == null || !dadosAlunosTurmasPap.Any())
-                return (null, null, null);
-
-            dadosAlunosTurmasPap = DefinirTipoPap(dadosAlunosTurmasPap);
-            var indicadoresPap = await mediator.Send(new ObterDificuldadesIndicadoresPapQuery(dadosAlunosTurmasPap), cancellationToken);
-            var dadosFrequencia = await mediator.Send(new ObterQuantidadeAbaixoFrequenciaPorTurmaQuery(request.AnoLetivo), cancellationToken);
-
-            return (dadosAlunosTurmasPap, indicadoresPap, dadosFrequencia);
-        }
-
-        private static IEnumerable<DadosMatriculaAlunoTipoPapDto> DefinirTipoPap(IEnumerable<DadosMatriculaAlunoTipoPapDto> dadosMatriculaAlunos)
-        {
-            foreach (var dado in dadosMatriculaAlunos)
-            {
-                dado.TipoPap = PapComponenteCurricularMap.ObterTipoPapPorComponente(dado.ComponenteCurricularId);
-                yield return dado;
-            }
-        }
-    }
-    public class ObterDadosBrutosParaConsolidacaoIndicadoresPapQueryValidator : AbstractValidator<ObterDadosBrutosParaConsolidacaoIndicadoresPapQuery>
-    {
-        public ObterDadosBrutosParaConsolidacaoIndicadoresPapQueryValidator()
-        {
-            RuleFor(c => c.AnoLetivo)
-                .GreaterThanOrEqualTo(PainelEducacionalConstants.ANO_LETIVO_MIM_LIMITE).WithMessage($"O ano letivo deve ser maior ou igual {PainelEducacionalConstants.ANO_LETIVO_MIM_LIMITE}.");
         }
     }
 }
--- teste/SME.SGP.Aplicacao.Teste/Queries/PainelEducacional/ObterIdepPorAnoEtapaQueryHandlerTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Queries/PainelEducacional/ObterIdepPorAnoEtapaQueryHandlerTeste.cs
@@ -3,9 +3,7 @@ using SME.SGP.Aplicacao.Queries.PainelEducacional.ObterIdepPorAnoEtapa;
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra.Dtos.PainelEducacional;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -38,16 +36,7 @@ namespace SME.SGP.Aplicacao.Teste.Queries.PainelEducacional.ObterIdepPorAnoEtapa
         {
             var dados = new List<PainelEducacionalIdepDto>
             {
-                new PainelEducacionalIdepDto
-                {
-                    AnoLetivo = 2023,
-                    Etapa = PainelEducacionalIdepEtapa.AnosIniciais,
-                    Faixa = "5.0-6.9",
-                    CodigoDre = "123456",
-                    Quantidade = 10,
-                    MediaGeral = 5.5m,
-                    UltimaAtualizacao = new DateTime(2023, 10, 1)
-                }
+                new PainelEducacionalIdepDto { AnoLetivo = 2023, Etapa = PainelEducacionalIdepEtapa.AnosIniciais }
             };
 
             var query = new ObterIdepPorAnoEtapaQuery(2023, PainelEducacionalIdepEtapa.AnosIniciais.ToString(), "123456");
@@ -56,103 +45,8 @@ namespace SME.SGP.Aplicacao.Teste.Queries.PainelEducacional.ObterIdepPorAnoEtapa
                        .ReturnsAsync(dados);
 
             var resultado = await _handler.Handle(query, CancellationToken.None);
-            var resultadoLista = resultado.ToList();
 
-            Assert.Single(resultadoLista);
-            var item = resultadoLista.First();
-            Assert.Equal(2023, item.AnoLetivo);
-            Assert.Equal(PainelEducacionalIdepEtapa.AnosIniciais, item.Etapa);
-            Assert.Equal("5.0-6.9", item.Faixa);
-            Assert.Equal("123456", item.CodigoDre);
-            Assert.Equal(10, item.Quantidade);
-            Assert.Equal(5.5m, item.MediaGeral);
-            Assert.Equal(new DateTime(2023, 10, 1), item.UltimaAtualizacao);
-        }
-
-        [Fact]
-        public async Task Deve_Retornar_Dados_Agrupados_E_Ordenados()
-        {
-            var dadosRepositorio = new List<PainelEducacionalIdepDto>
-            {
-                new PainelEducacionalIdepDto 
-                { 
-                    AnoLetivo = 2023, 
-                    Etapa = PainelEducacionalIdepEtapa.AnosIniciais,
-                    Faixa = "0-4.9",
-                    CodigoDre = "123456",
-                    Quantidade = 10,
-                    MediaGeral = 4.5m,
-                    UltimaAtualizacao = new DateTime(2023, 10, 1)
-                },
-                new PainelEducacionalIdepDto 
-                { 
-                    AnoLetivo = 2023, 
-                    Etapa = PainelEducacionalIdepEtapa.AnosIniciais,
-                    Faixa = "0-4.9",
-                    CodigoDre = "123456",
-                    Quantidade = 5,
-                    MediaGeral = 3.5m,
-                    UltimaAtualizacao = new DateTime(2023, 10, 15)
-                },
-                new PainelEducacionalIdepDto 
-                { 
-                    AnoLetivo = 2023, 
-                    Etapa = PainelEducacionalIdepEtapa.AnosIniciais,
-                    Faixa = "5.0-6.9",
-                    CodigoDre = "123456",
-                    Quantidade = 8,
-                    MediaGeral = 6.0m,
-                    UltimaAtualizacao = new DateTime(2023, 10, 10)
-                }
-            };
-
-            var query = new ObterIdepPorAnoEtapaQuery(2023, PainelEducacionalIdepEtapa.AnosIniciais.ToString(), "123456");
-
-            _repositorio.Setup(r => r.ObterIdepPorAnoEtapa(2023, PainelEducacionalIdepEtapa.AnosIniciais.ToString(), "123456"))
-                       .ReturnsAsync(dadosRepositorio);
-
-            var resultado = await _handler.Handle(query, CancellationToken.None);
-            var resultadoLista = resultado.ToList();
-
-            Assert.Equal(2, resultadoLista.Count);
-
-            var primeiroGrupo = resultadoLista.First(x => x.Faixa == "0-4.9");
-            Assert.Equal(2023, primeiroGrupo.AnoLetivo);
-            Assert.Equal(PainelEducacionalIdepEtapa.AnosIniciais, primeiroGrupo.Etapa);
-            Assert.Equal("123456", primeiroGrupo.CodigoDre);
-            Assert.Equal(15, primeiroGrupo.Quantidade); 
-            Assert.Equal(4.0m, primeiroGrupo.MediaGeral); 
-            Assert.Equal(new DateTime(2023, 10, 15), primeiroGrupo.UltimaAtualizacao); 
-
-            var segundoGrupo = resultadoLista.First(x => x.Faixa == "5.0-6.9");
-            Assert.Equal(2023, segundoGrupo.AnoLetivo);
-            Assert.Equal(PainelEducacionalIdepEtapa.AnosIniciais, segundoGrupo.Etapa);
-            Assert.Equal("123456", segundoGrupo.CodigoDre);
-            Assert.Equal(8, segundoGrupo.Quantidade);
-            Assert.Equal(6.0m, segundoGrupo.MediaGeral);
-            Assert.Equal(new DateTime(2023, 10, 10), segundoGrupo.UltimaAtualizacao);
-
-            Assert.Equal("0-4.9", resultadoLista[0].Faixa);
-            Assert.Equal("5.0-6.9", resultadoLista[1].Faixa);
-        }
-
-        [Fact]
-        public async Task Deve_Retornar_Lista_Vazia_Quando_Repositorio_Nao_Retorna_Dados()
-        {
-            var query = new ObterIdepPorAnoEtapaQuery(2023, PainelEducacionalIdepEtapa.AnosIniciais.ToString(), "123456");
-
-            _repositorio.Setup(r => r.ObterIdepPorAnoEtapa(2023, PainelEducacionalIdepEtapa.AnosIniciais.ToString(), "123456"))
-                       .ReturnsAsync(new List<PainelEducacionalIdepDto>());
-
-            var resultado = await _handler.Handle(query, CancellationToken.None);
-
-            Assert.Empty(resultado);
-        }
-
-        [Fact]
-        public void Deve_Lancar_ArgumentNullException_Quando_Repositorio_For_Null()
-        {
-            Assert.Throws<ArgumentNullException>(() => new ObterIdepPorAnoEtapaQueryHandler(null));
+            Assert.Equal(dados, resultado);
         }
     }
 }
