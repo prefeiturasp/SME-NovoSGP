46644a3692c8f1dac7be5e48058fbf40f1496d4e
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-10-10 16:19:33 -0300

    Consolidação Abandono

--- src/SME.SGP.Infra/Dtos/SituacaoAluno/AlunosSituacaoTurmas.cs
+++ src/SME.SGP.Infra/Dtos/SituacaoAluno/AlunosSituacaoTurmas.cs
@@ -2,6 +2,7 @@
 {
     public class AlunosSituacaoTurmas
     {
+        public int AnoLetivo { get; set; }
         public int QuantidadeAlunos { get; set; }
         public int CodigoSituacaoMatricula { get; set; }
         public string SituacaoMatricula { get; set; }
