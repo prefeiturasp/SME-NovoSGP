c7e7ca691f653c404f6487c5ac02e702b45e2dc3
Diego Moreno <dierudito@gmail.com>
2025-10-13 14:36:33 -0300

    Adiciona visão UE Consolidação Abandono Painel Educacional
    Implementa a consolidação de abandono nos níveis de UE, incluindo novas tabelas de banco de dados, entidades, DTOs, consultas e métodos de repositório. Atualiza a lógica de casos de uso e comandos para processar e persistir indicadores de abandono para UEs. Adiciona testes unitários relacionados para novas funcionalidades.

---
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterAbandonoUltimoAnoConsolidado/ObterAbandonoUltimoAnoConsolidadoQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterAbandonoUltimoAnoConsolidado
+{
+    public class ObterAbandonoUltimoAnoConsolidadoQueryHandler : IRequestHandler<ObterAbandonoUltimoAnoConsolidadoQuery, int>
+    {
+        private readonly IRepositorioPainelEducacionalConsolidacaoAbandono _repositorio;
+
+        public ObterAbandonoUltimoAnoConsolidadoQueryHandler(IRepositorioPainelEducacionalConsolidacaoAbandono repositorio)
+        {
+            _repositorio = repositorio;
+        }
+        public async Task<int> Handle(ObterAbandonoUltimoAnoConsolidadoQuery request, CancellationToken cancellationToken)
+        {
+            var ultimoAno = await _repositorio.ObterUltimoAnoConsolidadoAsync();
+            return ultimoAno ?? 0;
+        }
+    }
+}
--- src/SME.SGP.Dados/Repositorios/RepositorioPainelEducacionalConsolidacaoAbandono.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioPainelEducacionalConsolidacaoAbandono.cs
@@ -4,13 +4,13 @@ using NpgsqlTypes;
 using SME.SGP.Dominio.Entidades;
 using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra;
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Dados.Repositorios
 {
+    [ExcludeFromCodeCoverage]
     public class RepositorioPainelEducacionalConsolidacaoAbandono : IRepositorioPainelEducacionalConsolidacaoAbandono
     {
         private readonly IConfiguration configuration;
@@ -29,14 +29,13 @@ namespace SME.SGP.Dados.Repositorios
 
             await using var writer = conn.BeginBinaryImport(@"
                 COPY painel_educacional_consolidacao_abandono 
-                    (id, codigo_dre, ano, modalidade, quantidade_desistencias, ano_letivo, criado_em)
+                    (codigo_dre, ano, modalidade, quantidade_desistencias, ano_letivo, criado_em)
                 FROM STDIN (FORMAT BINARY)
             ");
 
             foreach (var item in indicadores)
             {
                 await writer.StartRowAsync();
-                await writer.WriteAsync(DateTime.UtcNow.Ticks, NpgsqlDbType.Bigint);
                 await writer.WriteAsync(item.CodigoDre, NpgsqlDbType.Varchar);
                 await writer.WriteAsync(item.Ano, NpgsqlDbType.Varchar);
                 await writer.WriteAsync(item.Modalidade, NpgsqlDbType.Varchar);
@@ -48,15 +47,52 @@ namespace SME.SGP.Dados.Repositorios
             await writer.CompleteAsync();
         }
 
-        public async Task LimparConsolidacao()
+        public async Task BulkInsertAsync(IEnumerable<PainelEducacionalConsolidacaoAbandonoUe> indicadores)
         {
-            var sql = "DELETE FROM painel_educacional_consolidacao_abandono";
-            await database.ExecuteAsync(sql);
+            await using var conn = new NpgsqlConnection(configuration.GetConnectionString("SGP_Postgres"));
+            await conn.OpenAsync();
+            await using var writer = conn.BeginBinaryImport(@"
+                COPY painel_educacional_consolidacao_abandono_ue 
+                    (ano_letivo, codigo_dre, codigo_ue, codigo_turma, nome_turma, modalidade, quantidade_desistencias, criado_em)
+                FROM STDIN (FORMAT BINARY)
+            ");
+            foreach (var item in indicadores)
+            {
+                await writer.StartRowAsync();
+                await writer.WriteAsync(item.AnoLetivo, NpgsqlDbType.Integer);
+                await writer.WriteAsync(item.CodigoDre, NpgsqlDbType.Varchar);
+                await writer.WriteAsync(item.CodigoUe, NpgsqlDbType.Varchar);
+                await writer.WriteAsync(item.CodigoTurma, NpgsqlDbType.Varchar);
+                await writer.WriteAsync(item.NomeTurma, NpgsqlDbType.Varchar);
+                await writer.WriteAsync(item.Modalidade, NpgsqlDbType.Varchar);
+                await writer.WriteAsync(item.QuantidadeDesistencias, NpgsqlDbType.Integer);
+                await writer.WriteAsync(item.CriadoEm, NpgsqlDbType.TimestampTz);
+            }
+            await writer.CompleteAsync();
         }
 
-        public Task<IEnumerable<PainelEducacionalConsolidacaoAbandono>> ObterConsolidacaoAsync(int anoLetivo, string codigoDre, string codigoUe)
+        public async Task LimparConsolidacao(int ano)
         {
-            throw new NotImplementedException();
+            var sql = @"DELETE FROM painel_educacional_consolidacao_abandono WHERE ano_letivo = @ano;
+                        DELETE FROM painel_educacional_consolidacao_abandono_ue WHERE ano_letivo = @ano;
+                        -- sincronizar a sequence das tabelas
+                        SELECT setval(
+                            pg_get_serial_sequence('painel_educacional_consolidacao_abandono', 'id'),
+                            (SELECT MAX(id) FROM painel_educacional_consolidacao_abandono)
+                        );
+                        SELECT setval(
+                            pg_get_serial_sequence('painel_educacional_consolidacao_abandono_ue', 'id'),
+                            (SELECT MAX(id) FROM painel_educacional_consolidacao_abandono_ue)
+                        );
+                        ";
+
+            await database.ExecuteAsync(sql, new { ano });
+        }
+
+        public async Task<int?> ObterUltimoAnoConsolidadoAsync()
+        {
+            var sql = @"SELECT MAX(ano_letivo) FROM painel_educacional_consolidacao_abandono";
+            return await database.QueryFirstOrDefaultAsync<int?>(sql);
         }
     }
 }
