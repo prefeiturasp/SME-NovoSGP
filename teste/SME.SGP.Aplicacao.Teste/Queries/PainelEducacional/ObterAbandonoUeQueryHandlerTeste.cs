2b75956cd85abbc6af611ec8b2c42848fe5edb99
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-10-13 17:00:49 -0300

    testes unitarios AB#134796

--- teste/SME.SGP.Aplicacao.Teste/Queries/PainelEducacional/ObterAbandonoUeQueryHandlerTeste.cs
+++ teste/SME.SGP.Aplicacao.Teste/Queries/PainelEducacional/ObterAbandonoUeQueryHandlerTeste.cs
@@ -1,12 +1,12 @@
-using Moq;
-using SME.SGP.Aplicacao.Queries.PainelEducacional.ObterAbandono;
-using SME.SGP.Dominio;
-using SME.SGP.Dominio.Interfaces.Repositorios;
-using SME.SGP.Infra;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Moq;
 using Xunit;
+using SME.SGP.Aplicacao.Queries.PainelEducacional.ObterAbandono;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using SME.SGP.Infra.Dtos.PainelEducacional;
 
 namespace SME.SGP.Aplicacao.Teste.Queries.PainelEducacional
 {
@@ -16,22 +16,19 @@ namespace SME.SGP.Aplicacao.Teste.Queries.PainelEducacional
         public async Task DeveRetornarDtoCorretoAoConsultarRepositorio()
         {
             var mockRepo = new Mock<IRepositorioPainelEducacionalAbandonoUe>();
-            var paginacaoResultado = new PaginacaoResultadoDto<PainelEducacionalAbandonoUe>
+            var resultadoRepo = new List<PainelEducacionalAbandonoUe>
             {
-                Items = new List<PainelEducacionalAbandonoUe>
-                {
-                    new PainelEducacionalAbandonoUe {
-                        CodigoTurma = "T1",
-                        NomeTurma = "Turma 1",
-                        Modalidade = "CIEJA",
-                        QuantidadeDesistencias = 5
-                    }
-                },
-                TotalPaginas = 1,
-                TotalRegistros = 1
+                new PainelEducacionalAbandonoUe {
+                    CodigoTurma = "T1",
+                    NomeTurma = "Turma 1",
+                    Modalidade = "CIEJA",
+                    QuantidadeDesistencias = 5,
+                    TotalPaginas = 1,
+                    TotalRegistros = 1
+                }
             };
             mockRepo.Setup(r => r.ObterAbandonoUe(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
-                .ReturnsAsync(paginacaoResultado);
+                .ReturnsAsync(resultadoRepo);
 
             var handler = new ObterAbandonoUeQueryHandler(mockRepo.Object);
             var query = new ObterAbandonoUeQuery(2025, "dre", "ue", "CIEJA", 1, 10);
