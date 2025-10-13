5d0f47f3f405d50874ae54f03020fae8f3e37f71
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-10-13 14:21:33 -0300

    Repositorio e handler alterado para o novo dto

--- src/SME.SGP.Infra/Dtos/PainelEducacional/PainelEducacionalAbandonoTurmaDto.cs
+++ src/SME.SGP.Infra/Dtos/PainelEducacional/PainelEducacionalAbandonoTurmaDto.cs
@@ -1,9 +1,10 @@
-using System;
-
 namespace SME.SGP.Infra.Dtos.PainelEducacional
 {
     public class PainelEducacionalAbandonoTurmaDto : PainelEducacionalAbandonoBaseDto
     {
         public string Turma { get; set; }
+        public string Modalidade { get; set; }
+        public string CodigoTurma { get; set; }
+        public int QuantidadeDesistencias { get; set; }
     }
 }
