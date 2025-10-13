de419a20c801679d49af90475499887073afcd62
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-10-13 14:21:33 -0300

    Repositorio e handler alterado para o novo dto

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterAbandono/ObterAbandonoUeQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterAbandono/ObterAbandonoUeQueryHandler.cs
@@ -1,6 +1,9 @@
 using MediatR;
+using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra.Dtos.PainelEducacional;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,28 +12,36 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterAbandono
     public class ObterAbandonoUeQueryHandler : IRequestHandler<ObterAbandonoUeQuery, PainelEducacionalAbandonoUeDto>
     {
         private readonly IRepositorioPainelEducacionalAbandonoUe repositorio;
-        
+
         public ObterAbandonoUeQueryHandler(IRepositorioPainelEducacionalAbandonoUe repositorio)
         {
             this.repositorio = repositorio;
         }
-        
+
         public async Task<PainelEducacionalAbandonoUeDto> Handle(ObterAbandonoUeQuery request, CancellationToken cancellationToken)
         {
-            var (modalidades, totalPaginas, totalRegistros) = await repositorio.ObterAbandonoUe(
-                request.AnoLetivo,
-                request.CodigoDre,
-                request.CodigoUe,
-                request.Modalidade,
-                request.NumeroPagina,
-                request.NumeroRegistros);
-            
-            return new PainelEducacionalAbandonoUeDto
+            var resultado = await repositorio.ObterAbandonoUe(request.AnoLetivo, request.CodigoDre, request.CodigoUe, request.Modalidade, request.NumeroPagina, request.NumeroRegistros);
+
+            return MapearParaDto(resultado);
+        }
+
+        private static PainelEducacionalAbandonoUeDto MapearParaDto(IEnumerable<PainelEducacionalAbandonoUe> resultado)
+        {
+            var dto = new PainelEducacionalAbandonoUeDto
             {
-                Modalidades = new System.Collections.Generic.List<PainelEducacionalAbandonoTurmaDto>(modalidades),
-                TotalPaginas = totalPaginas,
-                TotalRegistros = totalRegistros
+                Modalidades = resultado.Select(r => new PainelEducacionalAbandonoTurmaDto
+                {
+                    Modalidade = r.Modalidade,
+                    CodigoTurma = r.CodigoTurma,
+                    Turma = r.NomeTurma,
+                    QuantidadeDesistencias = r.QuantidadeDesistencias
+                }).ToList(),
+
+                TotalPaginas = resultado.First().TotalPaginas,
+                TotalRegistros = resultado.First().TotalRegistros
             };
+
+            return dto;
         }
     }
 }
