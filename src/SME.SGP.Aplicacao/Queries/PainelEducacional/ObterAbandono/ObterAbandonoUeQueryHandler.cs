ed86fb9e7182740cf53cceb0acdc4522010c188c
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-10-14 11:15:10 -0300

    Paginação ajustado seguindo o padrao de RepositorioCadastroAcessoABAEConsulta

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterAbandono/ObterAbandonoUeQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterAbandono/ObterAbandonoUeQueryHandler.cs
@@ -1,8 +1,6 @@
 using MediatR;
-using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra.Dtos.PainelEducacional;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,22 +19,17 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterAbandono
         public async Task<PainelEducacionalAbandonoUeDto> Handle(ObterAbandonoUeQuery request, CancellationToken cancellationToken)
         {
             var resultado = await repositorio.ObterAbandonoUe(request.AnoLetivo, request.CodigoDre, request.CodigoUe, request.Modalidade, request.NumeroPagina, request.NumeroRegistros);
-            return MapearParaDto(resultado);
-        }
-
-        private static PainelEducacionalAbandonoUeDto MapearParaDto(IEnumerable<PainelEducacionalAbandonoUe> resultado)
-        {
-            var dto = new PainelEducacionalAbandonoUeDto
+            return new PainelEducacionalAbandonoUeDto
             {
-                Modalidades = resultado.Select(r => new PainelEducacionalAbandonoTurmaDto
+                Modalidades = resultado.Items.Select(r => new PainelEducacionalAbandonoTurmaDto
                 {
                     Turma = r.NomeTurma,
                     QuantidadeDesistentes = r.QuantidadeDesistencias
                 }).ToList(),
-                TotalPaginas = resultado.FirstOrDefault()?.TotalPaginas ?? 0,
-                TotalRegistros = resultado.FirstOrDefault()?.TotalRegistros ?? 0
+
+                TotalPaginas = resultado.TotalPaginas,
+                TotalRegistros = resultado.TotalRegistros
             };
-            return dto;
         }
     }
 }
