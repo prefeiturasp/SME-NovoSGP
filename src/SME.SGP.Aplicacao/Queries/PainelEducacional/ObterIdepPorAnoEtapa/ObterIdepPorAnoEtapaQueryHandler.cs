7d61946ac80776982cf6aeca9151d11b90025198
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-14 10:49:03 -0300

    Adicionar agrupamento ao endpoint de idep e revisar teste unitário

--- src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterIdepPorAnoEtapa/ObterIdepPorAnoEtapaQueryHandler.cs
+++ src/SME.SGP.Aplicacao/Queries/PainelEducacional/ObterIdepPorAnoEtapa/ObterIdepPorAnoEtapaQueryHandler.cs
@@ -3,6 +3,7 @@ using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra.Dtos.PainelEducacional;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,7 +18,25 @@ namespace SME.SGP.Aplicacao.Queries.PainelEducacional.ObterIdepPorAnoEtapa
         }
         public async Task<IEnumerable<PainelEducacionalIdepDto>> Handle(ObterIdepPorAnoEtapaQuery request, CancellationToken cancellationToken)
         {
-            return await repositorioIdepConsulta.ObterIdepPorAnoEtapa(request.AnoLetivo, request.Etapa, request.CodigoDre);
+            var dadosIdep = await repositorioIdepConsulta.ObterIdepPorAnoEtapa(request.AnoLetivo, request.Etapa, request.CodigoDre);
+            
+            var dadosAgrupados = dadosIdep
+                .GroupBy(x => new { x.AnoLetivo, x.Etapa, x.Faixa })
+                .Select(g => new PainelEducacionalIdepDto
+                {
+                    AnoLetivo = g.Key.AnoLetivo,
+                    Etapa = g.Key.Etapa,
+                    Faixa = g.Key.Faixa,
+                    CodigoDre = g.FirstOrDefault().CodigoDre ?? null,
+                    Quantidade = g.Sum(x => x.Quantidade),
+                    MediaGeral = g.Average(x => x.MediaGeral),
+                    UltimaAtualizacao = g.Max(x => x.UltimaAtualizacao)
+                })
+                .OrderBy(x => x.AnoLetivo)
+                .ThenBy(x => x.Etapa)
+                .ThenBy(x => x.Faixa);
+
+            return dadosAgrupados;
         }
     }
 }
