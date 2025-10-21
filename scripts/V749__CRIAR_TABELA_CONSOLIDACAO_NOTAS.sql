5737640aa94c8cb264e7ef2112d49a46a060480b
Diego Moreno <dierudito@gmail.com>
2025-10-21 15:22:09 -0300

    Update V749__CRIAR_TABELA_CONSOLIDACAO_NOTAS.sql

--- scripts/V749__CRIAR_TABELA_CONSOLIDACAO_NOTAS.sql
+++ scripts/V749__CRIAR_TABELA_CONSOLIDACAO_NOTAS.sql
@@ -1,11 +1,10 @@
-DROP TABLE IF exists  painel_educacional_consolidacao_nota_ue;
 CREATE TABLE IF NOT exists  painel_educacional_consolidacao_nota_ue (
     id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
     ano_letivo int4 NOT NULL,
     codigo_dre VARCHAR(15) NOT NULL,
 	codigo_ue VARCHAR(15) NOT NULL,
     bimestre smallint NULL,
-    modalidade int4 NOT NULL,
+    modalidade int NOT NULL,
     serie_turma VARCHAR(20) NOT NULL,
     quantidade_abaixo_media_portugues int NOT NULL,
     quantidade_acima_media_portugues int NOT NULL,
@@ -16,15 +15,13 @@ CREATE TABLE IF NOT exists  painel_educacional_consolidacao_nota_ue (
 	criado_em timestamptz NOT NULL,
 	CONSTRAINT painel_educacional_consolidacao_nota_ue_pk PRIMARY KEY (id)
 );
-DROP INDEX IF EXISTS idx_painel_educacional_consolidacao_nota_ue;
 CREATE INDEX IF NOT EXISTS idx_painel_educacional_consolidacao_nota_ue ON painel_educacional_consolidacao_nota_ue (ano_letivo, codigo_dre, codigo_ue, bimestre, modalidade, serie_turma);
-DROP TABLE IF EXISTS painel_educacional_consolidacao_nota;
 CREATE TABLE IF NOT EXISTS painel_educacional_consolidacao_nota (
     id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
     ano_letivo int4 NOT NULL,
     codigo_dre VARCHAR(15) NOT null,
     bimestre smallint NULL,
-    modalidade int4 NOT NULL,
+    modalidade int NOT NULL,
     ano_turma CHAR(1) NULL,
     quantidade_abaixo_media_portugues int NOT NULL,
     quantidade_acima_media_portugues int NOT NULL,
@@ -35,5 +32,5 @@ CREATE TABLE IF NOT EXISTS painel_educacional_consolidacao_nota (
 	criado_em timestamptz NOT NULL,
 	CONSTRAINT painel_educacional_consolidacao_nota_pk PRIMARY KEY (id)
 );
-DROP INDEX IF EXISTS idx_painel_educacional_consolidacao_nota;
+
 CREATE INDEX IF NOT EXISTS idx_painel_educacional_consolidacao_nota ON painel_educacional_consolidacao_nota (ano_letivo, codigo_dre, bimestre, modalidade, ano_turma);
