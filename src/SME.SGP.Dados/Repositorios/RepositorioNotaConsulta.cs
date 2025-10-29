32a8b776b50c876fd1ff8e99be4374f7cc3265c8
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-29 09:37:34 -0300

    Incluir no filtro paginação, adicionar nome da modalidade e tornar obrigatório envio da modalidade pelo filtro

--- src/SME.SGP.Dados/Repositorios/RepositorioNotaConsulta.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioNotaConsulta.cs
@@ -1,6 +1,5 @@
 ﻿using Dapper;
 using SME.SGP.Dominio;
-using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Dominio.Interfaces.Repositorios;
 using SME.SGP.Infra;
 using SME.SGP.Infra.Dtos.PainelEducacional.Notas.VisaoSmeDre;
