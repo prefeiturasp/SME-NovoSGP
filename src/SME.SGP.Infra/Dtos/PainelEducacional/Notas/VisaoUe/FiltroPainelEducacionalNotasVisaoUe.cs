32a534f2a2fa58f6abbf2f41961d1e5ba1e8b4c2
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-29 09:37:34 -0300

    Incluir no filtro paginação, adicionar nome da modalidade e tornar obrigatório envio da modalidade pelo filtro

--- src/SME.SGP.Infra/Dtos/PainelEducacional/Notas/VisaoUe/FiltroPainelEducacionalNotasVisaoUe.cs
+++ src/SME.SGP.Infra/Dtos/PainelEducacional/Notas/VisaoUe/FiltroPainelEducacionalNotasVisaoUe.cs
@@ -8,5 +8,7 @@ namespace SME.SGP.Infra.Dtos.PainelEducacional.Notas.VisaoUe
         public int AnoLetivo { get; set; } 
         public Modalidade Modalidade { get; set; }
         public int Bimestre { get; set; }
+        public int NumeroPagina { get; set; }
+        public int NumeroRegistros { get; set; }
     }
 }
