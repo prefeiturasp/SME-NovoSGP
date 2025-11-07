138ae235e7e9d7aaba53de9832f27754ff2e2b98
Diego Moreno <dierudito@gmail.com>
2025-11-07 12:00:52 -0300

    Cria endpoints para buscar usuários da sme
    AB#137316

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/Usuarios/ObterUsuariosCoreSsoPorRfUseCase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces.CasosDeUso.Usuarios;
+using SME.SGP.Aplicacao.Queries.EOL.Usuarios.ObterUsuariosCoreSsoPorRf;
+using SME.SGP.Infra;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso.Usuarios
+{
+    public class ObterUsuariosCoreSsoPorRfUseCase : IObterUsuariosCoreSsoPorRfUseCase
+    {
+        private readonly IMediator _mediator;
+        public ObterUsuariosCoreSsoPorRfUseCase(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<UsuarioCoreSsoDto> Executar(string codigoRf)
+        {
+            return await _mediator.Send(new ObterUsuariosCoreSsoPorRfQuery(codigoRf));
+        }
+    }
+}
---
+++ src/SME.SGP.Aplicacao/Queries/EOL/Usuarios/ObterUsuariosCoreSsoPorRf/ObterUsuariosCoreSsoPorRfQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace SME.SGP.Aplicacao.Queries.EOL.Usuarios.ObterUsuariosCoreSsoPorRf
+{
+    public class ObterUsuariosCoreSsoPorRfQueryValidator : AbstractValidator<ObterUsuariosCoreSsoPorRfQuery>
+    {
+        public ObterUsuariosCoreSsoPorRfQueryValidator()
+        {
+            RuleFor(c => c.CodigoRf)
+                .NotEmpty()
+                .WithMessage("O código RF do usuário é obrigatório.");
+        }
+    }
+}
