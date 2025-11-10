0856fb910ecb65b746be3354ad6bbd24ff0f67ca
Diego Moreno <dierudito@gmail.com>
2025-11-10 15:37:01 -0300

    Adiciona filtro por RF e nome para obter usuario da sme paginado

--- src/SME.SGP.Infra.Consts/ServicosEolConstants.cs
+++ src/SME.SGP.Infra.Consts/ServicosEolConstants.cs
@@ -260,6 +260,6 @@
         public const string URL_MATRICULA_TURMA_ESCOLA = "turmas/matricula-turma-escola";
         public const string URL_MATRICULA_TURMA_ESCOLA_ALUNO = "turmas/matricula-turma-escola-aluno";
         public const string URL_USUARIOS_CORESSO_POR_LOGIN = "v1/usuarios/coresso/login/{0}";
-        public const string URL_USUARIOS_CORESSO_PAGINADO = "v1/usuarios/coresso/paginado?paginaAtual={0}&registrosPorPagina={1}";
+        public const string URL_USUARIOS_CORESSO_PAGINADO = "v1/usuarios/coresso/todos?paginaAtual={0}&registrosPorPagina={1}";
     }
 }
