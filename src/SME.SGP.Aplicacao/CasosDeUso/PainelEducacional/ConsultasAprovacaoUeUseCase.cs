43f064af7b92b47fe6a0347db6c22db00e2f86f7
Flavio-Spassu <flavio.souza@spassu.com.br>
2025-11-11 11:41:23 -0300

    adicionado filtrar modalidade por id + teste unitario

--- src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsultasAprovacaoUeUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/PainelEducacional/ConsultasAprovacaoUeUseCase.cs
@@ -16,9 +16,9 @@ namespace SME.SGP.Aplicacao.CasosDeUso.PainelEducacional
             this.mediator = mediator;
         }
 
-        public async Task<PaginacaoResultadoDto<PainelEducacionalAprovacaoUeDto>> ObterAprovacao(int anoLetivo, string codigoUe, string modalidade, int numeroPagina, int numeroRegistros)
+        public async Task<PaginacaoResultadoDto<PainelEducacionalAprovacaoUeDto>> ObterAprovacao(int anoLetivo, string codigoUe, int modalidadeId, int numeroPagina, int numeroRegistros)
         {
-            return await mediator.Send(new PainelEducacionalAprovacaoUeQuery(anoLetivo, codigoUe, modalidade, numeroPagina, numeroRegistros));
+            return await mediator.Send(new PainelEducacionalAprovacaoUeQuery(anoLetivo, codigoUe, modalidadeId, numeroPagina, numeroRegistros));
         }
     }
 }
