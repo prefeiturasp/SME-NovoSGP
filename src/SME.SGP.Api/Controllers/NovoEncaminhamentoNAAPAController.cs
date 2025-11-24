8ea0c2c0bb648ae714c7ae42d4bc1401f3312102
wesleygyn <wesley_gyn@yahoo.com.br>
2025-11-24 17:14:44 -0300

    AB#138561

---
+++ src/SME.SGP.Api/Controllers/NovoEncaminhamentoNAAPAController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/novo-encaminhamento-naapa")]
+    [Authorize("Bearer")]
+    public class NovoEncaminhamentoNAAPAController : ControllerBase
+    {
+
+    }
+}
---
+++ src/SME.SGP.Dados/Repositorios/RepositorioNovoEncaminhamentoNAAPA.cs
@@ -0,0 +1,20 @@
+﻿using SME.SGP.Dominio;
+using SME.SGP.Dominio.Interfaces.Repositorios;
+using SME.SGP.Infra;
+using SME.SGP.Infra.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Dados.Repositorios
+{
+    public class RepositorioNovoEncaminhamentoNAAPA : RepositorioBase<EncaminhamentoNAAPA>, IRepositorioNovoEncaminhamentoNAAPA
+    {
+        public RepositorioNovoEncaminhamentoNAAPA(ISgpContext database, IServicoAuditoria servicoAuditoria) : base(database, servicoAuditoria)
+        {
+        }
+
+    }
+}
