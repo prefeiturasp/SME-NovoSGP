c00eea9d7c1758be791b30a4b602328b6e029435
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-12-01 14:32:21 -0300

    sgp.abrangencia.worker.tratar

---
+++ src/SME.SGP.Aplicacao/CasosDeUso/AbrangenciaSync/AbrangenciaSyncUseCase.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao.Interfaces.CasosDeUso.AbrangenciaSync;
+using SME.SGP.Aplicacao.Queries.ObterUsuariosPerfis;
+using SME.SGP.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.CasosDeUso.AbrangenciaSync
+{
+    public class AbrangenciaSyncUseCase : AbstractUseCase, IAbrangenciaSyncUseCase
+    {
+        public AbrangenciaSyncUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(MensagemRabbit mensagem)
+        {
+            var usuarios = await mediator.Send(new ObterUsuariosPerfisQuery());
+            foreach (var usuario in usuarios)
+            {
+                await mediator.Send(new PublicarFilaSgpCommand("sgp.abrangencia.worker.tratar", usuario, Guid.NewGuid(), null));
+            }
+
+            return true;
+        }
+    }
+}
