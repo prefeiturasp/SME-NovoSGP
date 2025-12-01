4aa17b5977ea285544bb49af6c445d2d6954fcec
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-12-01 15:44:52 -0300

    processamento pelo worker de abrangencia

--- src/SME.SGP.Infra.Mensageria/Rotas/RotasRabbitSgp.cs
+++ src/SME.SGP.Infra.Mensageria/Rotas/RotasRabbitSgp.cs
@@ -143,6 +143,5 @@
         public const string ExecutarGravarObservacaoHistorioEscolar = "sgp.executar.gravar.observacao.historico.escolar";
 
         public const string SincronizarAbrangencia = "sgp.abrangencia.worker.sync";
-        //public const string SincronizarAbrangenciaTratar = "sgp.abrangencia.tratar.worker";
     }
 }
