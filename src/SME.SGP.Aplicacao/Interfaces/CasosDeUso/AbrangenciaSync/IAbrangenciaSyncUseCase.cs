3010c2472fac24af35535cb37a9e5d93c03ad361
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-12-01 14:32:21 -0300

    sgp.abrangencia.worker.tratar

---
+++ src/SME.SGP.Aplicacao/Interfaces/CasosDeUso/AbrangenciaSync/IAbrangenciaSyncUseCase.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace SME.SGP.Aplicacao.Interfaces.CasosDeUso.AbrangenciaSync
+{
+    public interface IAbrangenciaSyncUseCase : IRabbitUseCase
+    {
+    }
+}
--- src/SME.SGP.Dados/Repositorios/RepositorioUsuario.cs
+++ src/SME.SGP.Dados/Repositorios/RepositorioUsuario.cs
@@ -2,6 +2,7 @@
 using SME.SGP.Dominio;
 using SME.SGP.Dominio.Interfaces;
 using SME.SGP.Infra;
+using SME.SGP.Infra.Dtos.Abrangencia;
 using SME.SGP.Infra.Interface;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,18 @@ namespace SME.SGP.Dados.Repositorios
             var query = "select * from usuario where id = any(@ids)";
 
             return await database.Conexao.QueryAsync<Usuario>(query, new { ids });
+        }
+
+        public async Task<IEnumerable<AbrangenciaUsuarioPerfilDto>> ObterUsuariosPerfis()
+        {
+            var query = @"select  u.rf_codigo Login,
+                                       a.perfil Perfil 
+                                from usuario u
+                                inner join (select distinct(a.usuario_id ),
+	                                   a.perfil 
+                                from abrangencia a ) a  on u.id = a.usuario_id ";
+
+            return await database.Conexao.QueryAsync<AbrangenciaUsuarioPerfilDto>(query);
         }
     }
 }
