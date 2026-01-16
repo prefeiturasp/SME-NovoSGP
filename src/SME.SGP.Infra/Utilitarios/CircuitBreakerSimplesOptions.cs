a611ce09d75b6004db17c5e83d1e8cb28190e109
Mario Junior <mariosj9@gmail.com>
2026-01-16 08:19:58 -0300

    Criação circuit breaker RepositorioCacheRedis

---
+++ src/SME.SGP.Infra/Utilitarios/CircuitBreakerSimplesOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace SME.SGP.Infra.Utilitarios
+{
+    public class CircuitBreakerSimplesOptions
+    {
+        public const string Secao = "CircuitBreakerSimples";
+
+        public int LimiteFalhas { get; set; } = 5;
+        public int TempoAberturaSegundos { get; set; } = 30;
+    }
+}
