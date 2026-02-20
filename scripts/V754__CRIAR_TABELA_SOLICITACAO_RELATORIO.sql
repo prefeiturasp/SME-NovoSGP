8d2a5fc3d96d1f85a9a0f1d6126414c849279e9b
Thiago Fernando <thiago.fernando@msn.com>
2026-02-20 09:25:51 -0300

    Criar estrutura de tabela  para controle de solicitações de relatorios

---
+++ scripts/V754__CRIAR_TABELA_SOLICITACAO_RELATORIO.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS solicitacao_relatorio (
+    id int8 GENERATED ALWAYS AS IDENTITY,
+    filtros_usados varchar NOT NULL,
+    tipo_relatorio int4 NOT NULL,
+    usuario_que_solicitou varchar NOT NULL,
+    status_solicitacao int4 NOT NULL,
+    excluido bool DEFAULT false NOT NULL,
+    criado_em timestamp NOT NULL,
+    criado_por varchar(200) NOT NULL,
+    alterado_em timestamp NULL,
+    alterado_por varchar(200) NULL,
+    criado_rf varchar(200) NOT NULL,
+    alterado_rf varchar(200) NULL,
+    CONSTRAINT solicitacao_relatorios_pk PRIMARY KEY (id)
+);
