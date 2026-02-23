3ee556240010601496f236dc9262b488b1290d75
Thiago Fernando <thiago.fernando@msn.com>
2026-02-23 14:44:24 -0300

    #143294 Implementar no SGP controle de solicitações

---
+++ src/SME.SGP.Aplicacao/Queries/SolicitacaoRelatorio/RelatorioJaSolicitado/RelatorioJaSolicitadoAsyncQuery.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.SGP.Dominio;
+using SME.SGP.Dominio.Entidades;
+
+namespace SME.SGP.Aplicacao.Queries.SolicitacaoRelatorio.RelatorioJaSolicitado
+{
+    public class RelatorioJaSolicitadoAsyncQuery : IRequest<bool>
+    {
+        public RelatorioJaSolicitadoAsyncQuery(FiltroRelatorioBase filtros, TipoRelatorio tipoRelatorio, string usuarioQueSolicitou)
+        {
+            Filtros = filtros;
+            TipoRelatorio = tipoRelatorio;
+            UsuarioQueSolicitou = usuarioQueSolicitou;
+        }
+
+        public FiltroRelatorioBase Filtros { get; set; }
+        public TipoRelatorio TipoRelatorio { get; set; }
+        public string UsuarioQueSolicitou { get; set; }
+    }
+
+    public class RelatorioJaSolicitadoAsyncQueryValidator : AbstractValidator<RelatorioJaSolicitadoAsyncQuery>
+    {
+        public RelatorioJaSolicitadoAsyncQueryValidator()
+        {
+            RuleFor(c => c.UsuarioQueSolicitou).NotEmpty().NaoEhNulo();
+            RuleFor(c => c.TipoRelatorio).NotEmpty().NaoEhNulo();
+            RuleFor(c => c.Filtros).NotEmpty().NaoEhNulo();
+        }
+    }
+}
