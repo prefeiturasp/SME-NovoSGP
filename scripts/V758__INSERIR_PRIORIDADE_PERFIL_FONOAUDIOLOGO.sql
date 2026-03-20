ffe3c7fc130e7db1d3b1954bbe273e4fbacf4b1e
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-03-20 10:05:08 -0300

    Inserir prioridade perfil fono

---
+++ scripts/V758__INSERIR_PRIORIDADE_PERFIL_FONOAUDIOLOGO.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.prioridade_perfil (ordem, tipo, nome_perfil, codigo_perfil, criado_em, criado_por, alterado_em, alterado_por, criado_rf, alterado_rf) 
+VALUES('399', 2, 'Fonoaudiólogo', '90e1e074-37d6-e911-abd6-f81654fe895d', NOW(), 'Carga', NULL, NULL, 'Carga', NULL);
+
+-- o tipo é o mesmo do assistente social
+-- a ordem menor é que define quem vai aparecer primeiro quando logar (perfil)
+
