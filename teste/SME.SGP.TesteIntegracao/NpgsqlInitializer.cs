ce23c683b9f72c19766b01f8bfbb26676a18ab79
Thiago Fernando <thiago.fernando@msn.com>
2026-06-30 10:40:48 -0400

    Task 149785: Atualizar Npgsql

---
+++ teste/SME.SGP.TesteIntegracao/NpgsqlInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace SME.SGP.TesteIntegracao
+{
+    public static class NpgsqlInitializer
+    {
+        [ModuleInitializer]
+        public static void Initialize()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+    }
+}
