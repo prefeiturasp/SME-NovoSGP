f9bc8fc5a8f07d45b8cf8b7776f55ebee06d6e6f
Thiago Fernando <thiago.fernando@msn.com>
2026-07-03 07:18:02 -0400

    Task 152344: Ajustar Teste em Ambiente de QA

---
+++ teste/SME.SGP.TesteIntegracao/RelatorioAcompanhamentoAprendizagem/ServicosFake/TurmaEmPeriodoAbertoQueryFake.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SME.SGP.Aplicacao;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SGP.TesteIntegracao.RelatorioAcompanhamentoAprendizagem.ServicosFake
+{
+    public class TurmaEmPeriodoAbertoQueryFake : IRequestHandler<TurmaEmPeriodoAbertoQuery, bool>
+    {
+        public static bool ShouldReturnPeriodoAberto = true;
+        public  Task<bool> Handle(TurmaEmPeriodoAbertoQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(ShouldReturnPeriodoAberto);
+        }
+    }
+}
