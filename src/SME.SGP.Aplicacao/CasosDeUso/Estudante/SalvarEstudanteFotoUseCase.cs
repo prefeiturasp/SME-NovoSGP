a449e89118f5fcbbbfc081dd2d4c71caf3f7cb80
Thiago Fernando <thiago.fernando@msn.com>
2026-09-09 09:28:56 -0300

    Capturar erro ao salvar foto

--- src/SME.SGP.Aplicacao/CasosDeUso/Estudante/SalvarEstudanteFotoUseCase.cs
+++ src/SME.SGP.Aplicacao/CasosDeUso/Estudante/SalvarEstudanteFotoUseCase.cs
@@ -1,11 +1,14 @@
 ﻿using MediatR;
 using SME.SGP.Aplicacao.Interfaces;
+using SME.SGP.Dominio.Enumerados;
 using SME.SGP.Infra;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace SME.SGP.Aplicacao
 {
+    [ExcludeFromCodeCoverage]
     public class SalvarFotoEstudanteUseCase : AbstractUseCase, ISalvarFotoEstudanteUseCase
     {
         public SalvarFotoEstudanteUseCase(IMediator mediator) : base(mediator)
@@ -13,6 +16,16 @@ namespace SME.SGP.Aplicacao
         }
 
         public async Task<Guid> Executar(EstudanteFotoDto dto)
-            => await mediator.Send(new SalvarFotoEstudanteCommand(dto.File, dto.AlunoCodigo));
+        {
+            try
+            {
+                return await mediator.Send(new SalvarFotoEstudanteCommand(dto.File, dto.AlunoCodigo));
+            }
+            catch (Exception e)
+            {
+                await mediator.Send(new SalvarLogViaRabbitCommand($"=========== Erro ao salvar foto do Aluno SalvarFotoEstudanteUseCase: {e.Message}, {e.StackTrace?.ToString()}, {e.InnerException} , {e}", LogNivel.Critico, LogContexto.Geral));
+                throw;
+            }
+        }
     }
 }
